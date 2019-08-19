d059d5c4dc1857cf47226b1eaa79732a8b578ceb
Yang Guo <yangguo@chromium.org>
2019-08-19 13:43:00 +0200

    Update readme
    Also fix Python formatting.
    
    TBR=machenbach@chromium.org
    
    Change-Id: If2a7b4f4ebda9af948135bdfa806d3b89b09de4f

--- scripts/devtools_paths.py
+++ scripts/devtools_paths.py
@@ -1,20 +1,21 @@
 # Copyright 2019 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """
 Helper to find the path to the correct third_party directory
 """
 
 from os import path
 
+
 def root_path():
-  SCRIPTS_PATH = path.dirname(path.abspath(__file__))
-  ABS_DEVTOOLS_PATH = path.dirname(SCRIPTS_PATH)
-  if 'renderer' in ABS_DEVTOOLS_PATH:
-    return path.normpath(path.join(ABS_DEVTOOLS_PATH, '..', '..', '..', '..'))
-  else:
-    return ABS_DEVTOOLS_PATH
+    SCRIPTS_PATH = path.dirname(path.abspath(__file__))
+    ABS_DEVTOOLS_PATH = path.dirname(SCRIPTS_PATH)
+    if 'renderer' in ABS_DEVTOOLS_PATH:
+        return path.normpath(path.join(ABS_DEVTOOLS_PATH, '..', '..', '..', '..'))
+    else:
+        return ABS_DEVTOOLS_PATH
+
 
 def third_party_path():
-  return path.join(root_path(), 'third_party')
+    return path.join(root_path(), 'third_party')
