ce2b6c7f58f27f3cf13dfa1621272bb00040f031
Yang Guo <yangguo@google.com>
2019-09-19 10:59:03 +0200

    Switch to new repository

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -173,24 +173,36 @@ def _CheckCSSViolations(input_api, output_api):
                 results.append(output_api.PresubmitError(("%s:%d uses ::shadow selector") % (f.LocalPath(), line_number)))
     return results
 
+def _CommonChecks(input_api, output_api):
+  """Checks common to both upload and commit."""
+  checks = [
+    input_api.canned_checks.CheckOwnersFormat,
+    input_api.canned_checks.CheckOwners,
+    input_api.canned_checks.CheckChangeHasNoCrAndHasOnlyOneEol,
+    input_api.canned_checks.CheckChangeHasNoStrayWhitespace,
+    input_api.canned_checks.CheckGenderNeutral,
+    _CheckDevtoolsLocalizableResources,
+    _CheckDevtoolsLocalization,
+  ]
+
+  return sum([check(input_api, output_api) for check in checks], [])
 
 def CheckChangeOnUpload(input_api, output_api):
     results = []
     results.extend(_CheckBuildGN(input_api, output_api))
     results.extend(_CheckFormat(input_api, output_api))
-    results.extend(_CheckDevtoolsLocalizableResources(input_api, output_api))
-    results.extend(_CheckDevtoolsLocalization(input_api, output_api))
     results.extend(_CheckDevtoolsStyle(input_api, output_api))
-    results.extend(_CompileDevtoolsFrontend(input_api, output_api))
     results.extend(_CheckOptimizeSVGHashes(input_api, output_api))
     results.extend(_CheckCSSViolations(input_api, output_api))
+    results.extend(_CommonChecks(input_api, output_api))
     return results
 
 
 def CheckChangeOnCommit(input_api, output_api):
     results = []
-    results.extend(_CheckDevtoolsLocalizableResources(input_api, output_api))
-    results.extend(_CheckDevtoolsLocalization(input_api, output_api))
+    results.extend(_CommonChecks(input_api, output_api))
+    results.extend(input_api.canned_checks.CheckChangeHasDescription(
+      input_api, output_api))
     return results
 
 
