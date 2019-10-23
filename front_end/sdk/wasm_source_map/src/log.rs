390e09bbc30b47d7b1daf48ab969db2c9ff89a39
Ingvar Stepanyan <rreverser@google.com>
2019-10-23 13:48:35 +0100

    Add native DWARF support
    This adds native location resolver, written in Rust and compiled to
    WebAssembly, that can parse DWARF information from other given
    WebAssembly modules and resolve addresses <-> source location mappings
    in both directions.
    
    From V8 side, this depends on recently added Debugger.getWasmBytecode
    command and on a special "wasm://dwarf" source map URL reported for Wasm
    modules that contain DWARF information.
    
    On the JavaScript side, this uses autogenerated JavaScript bindings by
    wasm-bindgen to an internal context which contains all parsed data and
    lazily creates SDK.SourceMapEntry when requested. These bindings are
    further wrapped into a public class that implements SDK.SourceMap
    interface.
    
    ---
    
    Always auto-step over Wasm DWARF
    
    Wasm doesn't have any other source when DWARF is used for source
    mapping, so it will step into an empty source pane.
    
    This is not very useful, so let's always auto-step to the next resolved
    location.
    
    There are talks about stabilising auto-stepping for JS source maps as
    well, but for now limiting to just the new functionality should be
    safer.
    
    Change-Id: I6e981dcd33a03a5db2f2b1e39a93372c5ba09b97
    Bug: 1016772

---
+++ front_end/sdk/wasm_source_map/src/log.rs
@@ -0,0 +1,23 @@
+#![allow(unused)]
+
+#[cfg(feature = "log")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "log")]
+#[wasm_bindgen]
+extern "C" {
+  #[wasm_bindgen(js_namespace = console)]
+  pub(crate) fn log(x: String);
+}
+
+#[cfg(feature = "log")]
+macro_rules! log {
+    ($($args:tt)*) => {
+        crate::log::log(format!($($args)*))
+    };
+}
+
+#[cfg(not(feature = "log"))]
+macro_rules! log {
+  ($($args:tt)*) => {};
+}
