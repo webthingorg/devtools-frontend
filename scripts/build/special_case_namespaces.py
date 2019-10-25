7ad92fa7fc073d7f79daec8de2e263293c3398fa
Yang Guo <yangguo@chromium.org>
2019-10-25 13:25:47 +0200

    Move test scripts to scripts/test
    R=aerotwist@chromium.org, liviurau@chromium.org, machenbach@chromium.org
    
    Bug: 1018122
    Change-Id: I7cc3a1a9ab01c649b3c7fe8ba5d5254adc631634

---
+++ scripts/build/special_case_namespaces.py
@@ -0,0 +1,9 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from os import path
+
+special_case_namespaces_path = path.join(path.dirname(path.abspath(__file__)), 'special_case_namespaces.json')
+with open(special_case_namespaces_path) as json_file:
+    special_case_namespaces = json.load(json_file)
