487eb036df6e1f5490717704b6dbf33a01e7fcf8
Tim van der Lippe <tvanderlippe@google.com>
2019-10-28 13:01:05 +0000

    Rollup workers and migrate dependent modules
    Since workers do not support modules [1], it is not possible to
    currently import any ESM DevTools module. To be able to migrate
    text_utils/ to ESM, we would need to duplicate a large portion of
    text_utils/, which would be unmaintainable.
    
    We already had the same situation when we migrated platform/ to ESM and
    decided to copy the relevant functions, but this time around that is no
    longer an option.
    
    Thus, the 2 workers (heap_snapshot_worker and formatter_worker) are now
    being bundled on build time. This means that in a release build, the
    respective entrypoints are bundled and inserted in the output.
    
    To bundle, we use `rollup`, which is a bundler only concerned with
    rolling up ES modules. All other functionality of rollup (such as
    tree-shaking) is unused. We can revisit later if we need a bundler for
    the rest of devtools, but since we are in active migration to ESM that
    is infeasible at this point in time.
    
    As part of this CL, the following folders are migrated to ESM:
    - cm_headless/
    - formatter_worker/
    - heap_snapshot_model/
    - heap_snapshot_worker/
    - text_utils/
    
    Since text_utils is also an autostart module for the shell, this is the
    only module that is imported from root.js.
    
    Some of these modules also include files that are annotated with
    skip_compilation and thus run with dummy files during Closure
    compilation.
    
    Note that, because of the usages of import-statements in the workers
    and the blocking bug [1], the workers are generated even when Chromium
    is built with `debug_devtools=true`.
    
    Since heap_snapshot_model/ also used in the profiler, we have to eagerly
    load this module in root.js. Once all other dependencies of the profiler
    have been migrated to ESM, we can remove this import-statement.
    
    [1]: crbug.com/680046
    
    Bug:1013129,1006759
    Change-Id: I4c03c7b8a1f351ae9693cbcd922412083dd34bba

--- front_end/formatter_worker.unbundled.js
+++ front_end/formatter_worker.unbundled.js
@@ -1,7 +1,12 @@
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-self.importScripts('platform/utilities.js');
+
+import './platform/utilities.js';
+import './text_utils/text_utils.js';
+import './cm_headless/cm_headless.js';
+import './formatter_worker/formatter_worker.js';
+
 // Release build has Runtime.js bundled.
 if (!self.Root || !self.Root.Runtime) {
   self.importScripts('Runtime.js');
