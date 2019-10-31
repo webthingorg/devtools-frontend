900a1c5c76930d6bab69f658f476d1ee6efd47b0
Paul Lewis <aerotwist@chromium.org>
2019-10-31 15:23:05 +0000

    Move data_grid over to ESM
    Bug: 1006759
    Change-Id: I02a7fc01bff612cf057c918df7449734bba241b7

--- front_end/root.js
+++ front_end/root.js
@@ -19,3 +19,6 @@ import './persistence/persistence.js';
 import './browser_sdk/browser_sdk.js';
 import './extensions/extensions.js';
 import './console_counters/console_counters.js';
+
+// TODO: Make lazy-loaded.
+import './data_grid/data_grid.js';
