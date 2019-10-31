d7fbdb8cb1bd7b82b3921ead04cf8af0ed319941
Hongchan Choi <hongchan@chromium.org>
2019-10-31 10:23:46 -0700

    [WIP] WebAudio Graph Visualizer
    This is a working prototype of WebAudio graph visualizer. This is for
    the reference purpose only; not for review or merge.
    
    Design doc: go/web-audio-geiger-viz (googler-only)
    
    Tricium: disable
    Bug: 974343
    Change-Id: I06cf678cf06e27f4dd1a8ca8aa022dbf4ae9076f

--- front_end/web_audio/graph_visualizer/GraphStyle.js
+++ front_end/web_audio/graph_visualizer/GraphStyle.js
@@ -28,3 +28,63 @@ WebAudio.GraphVisualizer.GraphStyles.TotalParamPortHeight =
 
 WebAudio.GraphVisualizer.GraphStyles.NodeLabelFontStyle = '14px Segoe UI, Arial';
 WebAudio.GraphVisualizer.GraphStyles.ParamLabelFontStyle = '12px Segoe UI, Arial';
+
+/**
+ * Computes the color that should be used to visualize an AudioNode.
+ * @param {string} nodeType The type of the node. With the Node suffix removed.
+ * @param {?boolean=} isOffline Whether this node pertains to an
+ *     OfflineAudioContext.
+ * @return {string} The hex color used to visualize the AudioNode.
+ */
+WebAudio.GraphVisualizer.computeNodeColor = function(nodeType, isOffline) {
+  // To make the label concise, remove the suffix "Node" from the nodeType.
+  if (nodeType.endsWith('Node'))
+    {nodeType = nodeType.slice(0, nodeType.length - 4);}
+
+  // AudioNodes are grouped into color categories based on their purposes.
+  switch (nodeType) {
+    case 'AudioDestination':
+      // The destination nodes of OfflineAudioContexts are brown. Those of
+      // "non-offline" AudioContexts are a dark grey.
+      return isOffline ? '#5D4037' : '#90a4ad';
+    case 'AudioBufferSource':
+    case 'Oscillator':
+      return '#009688';
+    case 'Gain':
+    case 'BiquadFilter':
+    case 'Convolver':
+    case 'Delay':
+    case 'DynamicsCompressor':
+    case 'IIRFilter':
+    case 'Panner':
+    case 'StereoPanner':
+    case 'ChannelMerger':
+    case 'ChannelSplitter':
+    case 'WaveShaper':
+      return '#64b5f6';
+    case 'Analyser':
+      return '#f48fb1';
+    case 'MediaElementAudioSource':
+    case 'MediaStreamAudioDestination':
+    case 'MediaStreamAudioSource':
+      return '#ba68c8';
+    case 'ScriptProcessor':  // Deprecated.
+      return '#e0e0e0';
+    case 'AudioWorklet':
+      return '#9fa8da';
+    case 'Listener':
+      return '#bcaaa4';
+    default:
+      // Nothing matched. Odd. Highlight this node in dark red.
+      console.warn(`Unknown nodeType: ${nodeType}`);
+      return '#C62828';
+  }
+};
+
+WebAudio.GraphVisualizer.textColor = '#263238';
+WebAudio.GraphVisualizer.paramTextColor = '#303942';
+WebAudio.GraphVisualizer.inputPortColor = '#455a63';
+WebAudio.GraphVisualizer.outputPortColor = '#455a63';
+WebAudio.GraphVisualizer.edgeColor = '#455a63';
+WebAudio.GraphVisualizer.paramPortColor = '#ffa726';
+WebAudio.GraphVisualizer.unconnectedPortColor = '#fff';
