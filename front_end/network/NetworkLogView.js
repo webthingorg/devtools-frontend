a606b0102cb37d6c95b85ef66a23775afb8cf819
Amanda Baker <ambake@microsoft.com>
2019-11-05 10:55:30 -0800

    Reland "DevTools: Hide Network datagrid from SR when landing page is visible"
    Previously, when navigating the Network landing page with a screen
    reader in browse/scan mode, the user would be able to reach the
    DataGrid elements that are not visible. This content is reached before
    reaching the landing page, so a user wouldn't know to keep navigating
    past the grid to reach the shortcut suggestions.
    
    This change hides the datagrid and toolbar from the screen reader using
    the aria-hidden attribute, so focus moves straight to
    the landing page content.
    
    Original CL (reverted):
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1880306
    
    Bug: 963183
    Change-Id: I056e1a6aaafdcca86d22f358e99faa7e417d24bd

--- front_end/network/NetworkLogView.js
+++ front_end/network/NetworkLogView.js
@@ -645,15 +645,26 @@ Network.NetworkLogView = class extends UI.VBox {
     }
     hintText.createChild('br');
     hintText.appendChild(UI.XLink.create(
-        'https://developers.google.com/web/tools/chrome-devtools/network/?utm_source=devtools&utm_campaign=2019Q1',
+        'https://developers.google.cfom/web/tools/chrome-devtools/network/?utm_source=devtools&utm_campaign=2019Q1',
         'Learn more'));
+
+    this._setHidden(true);
   }
 
   _hideRecordingHint() {
+    this._setHidden(false);
     if (this._recordingHint) {
       this._recordingHint.remove();
     }
     this._recordingHint = null;
+  }
+
+  /**
+   * @param {boolean} value
+   */
+  _setHidden(value) {
+    this._columns.setHidden(value);
+    UI.ARIAUtils.setHidden(this._summaryToolbar.element, value);
   }
 
   /**
