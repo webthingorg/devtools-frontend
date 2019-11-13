402d430db7266dbc2acb0f764ef0903f7e571679
Changhao Han <changhaohan@chromium.org>
2019-11-13 21:35:13 +0100

    Fixes focus issue when navigating in Memory Object panel.
    Bug: chromium:1005449
    Change-Id: I11a2cf3cebddaa6a7f2dfb262067322ade2abf58

--- front_end/data_grid/DataGrid.js
+++ front_end/data_grid/DataGrid.js
@@ -988,6 +988,16 @@ export default class DataGridImpl extends Common.Object {
       nextSelectedNode.reveal();
       nextSelectedNode.select();
     }
+
+    if ((event.key === 'ArrowUp' || event.key === 'ArrowDown' || event.key === 'ArrowLeft' ||
+         event.key === 'ArrowRight') &&
+        document.activeElement !== this.element) {
+      // crbug.com/1005449
+      // navigational keys pressed but current DataGrid panel has lost focus;
+      // re-focus to ensure subsequent keydowns can be registered within this DataGrid
+      this.element.focus();
+    }
+
     if (handled) {
       event.consume(true);
     }
