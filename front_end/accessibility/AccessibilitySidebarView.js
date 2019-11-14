e0f875f8956a2bcb8dd7bb83aed41add50e76403
Alice Boxhall <aboxhall@chromium.org>
2019-11-14 14:32:56 +1100

    Update Accessibility model when Accessibility pane is shown.
    Bug: 1024700
    Change-Id: I789300ee0bd280ce01e5a58d7d26daaa49c27a0c

--- front_end/accessibility/AccessibilitySidebarView.js
+++ front_end/accessibility/AccessibilitySidebarView.js
@@ -96,12 +96,6 @@ export default class AccessibilitySidebarView extends UI.ThrottledWidget {
   wasShown() {
     super.wasShown();
 
-    this._breadcrumbsSubPane.setNode(this.node());
-    this._breadcrumbsSubPane.setAXNode(this.axNode());
-    this._axNodeSubPane.setNode(this.node());
-    this._axNodeSubPane.setAXNode(this.axNode());
-    this._ariaSubPane.setNode(this.node());
-
     SDK.targetManager.addModelListener(SDK.DOMModel, SDK.DOMModel.Events.AttrModified, this._onAttrChange, this);
     SDK.targetManager.addModelListener(SDK.DOMModel, SDK.DOMModel.Events.AttrRemoved, this._onAttrChange, this);
     SDK.targetManager.addModelListener(
@@ -120,6 +114,9 @@ export default class AccessibilitySidebarView extends UI.ThrottledWidget {
         SDK.DOMModel, SDK.DOMModel.Events.CharacterDataModified, this._onNodeChange, this);
     SDK.targetManager.removeModelListener(
         SDK.DOMModel, SDK.DOMModel.Events.ChildNodeCountUpdated, this._onNodeChange, this);
+
+    // Ensure we refresh the AXNode data next time the pane is shown.
+    this._updateWhenVisible = true;
   }
 
   _pullNode() {
