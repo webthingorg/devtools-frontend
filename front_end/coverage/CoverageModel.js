71cb4632854edabac3bb5c9bb3c398e1ea1f259e
Jan Scheffler <janscheffler@chromium.org>
2019-11-19 12:41:34 +0100

    Refractor of URLCoverageInfo
    This cl adds a single method to change information about
    the collected coverage of a url.
    
    Change-Id: Ia55b12a5d2241597115a70342a7c1ca30c44ec76

--- front_end/coverage/CoverageModel.js
+++ front_end/coverage/CoverageModel.js
@@ -240,8 +240,7 @@ Coverage.CoverageModel = class extends SDK.SDKModel {
       if (!urlEntry || !urlEntry._coverageInfoByLocation.delete(key)) {
         continue;
       }
-      urlEntry._size -= entry._size;
-      urlEntry._usedSize -= entry._usedSize;
+      urlEntry._addToSizes(-entry._usedSize, -entry._size);
       if (!urlEntry._coverageInfoByLocation.size) {
         this._coverageByURL.delete(entry.url());
       }
@@ -479,7 +478,7 @@ Coverage.CoverageModel = class extends SDK.SDKModel {
     if (!isNewUrlCoverage && coverageInfo._usedSize === oldUsedSize) {
       return null;
     }
-    urlCoverage._usedSize += coverageInfo._usedSize - oldUsedSize;
+    urlCoverage._addToSizes(coverageInfo._usedSize - oldUsedSize, 0);
     return coverageInfo;
   }
 
@@ -568,11 +567,13 @@ Coverage.CoverageModel._coveragePollingPeriodMs = 200;
 
 SDK.SDKModel.register(Coverage.CoverageModel, SDK.Target.Capability.None, false);
 
-Coverage.URLCoverageInfo = class {
+Coverage.URLCoverageInfo = class extends Common.Object {
   /**
    * @param {string} url
    */
   constructor(url) {
+    super();
+
     this._url = url;
     /** @type {!Map<string, !Coverage.CoverageInfo>} */
     this._coverageInfoByLocation = new Map();
@@ -629,6 +630,11 @@ Coverage.URLCoverageInfo = class {
     return this._coverageInfoByLocation.values();
   }
 
+  _addToSizes(usedSize, size) {
+    this._usedSize += usedSize;
+    this._size += size;
+  }
+
   /**
    * @param {!Common.ContentProvider} contentProvider
    * @param {number} contentLength
@@ -657,7 +663,7 @@ Coverage.URLCoverageInfo = class {
 
     entry = new Coverage.CoverageInfo(contentProvider, contentLength, lineOffset, columnOffset, type);
     this._coverageInfoByLocation.set(key, entry);
-    this._size += contentLength;
+    this._addToSizes(0, contentLength);
 
     return entry;
   }
