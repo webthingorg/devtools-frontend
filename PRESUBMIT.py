8b708d6f60ec23e26b1dd85b5d7236ddcaa659e6
Tim van der Lippe <tvanderlippe@chromium.org>
2019-11-19 17:04:56 +0000

    Allow changes in the .gitignore for new third_party inclusions
    Otherwise the upload fails as it would include a change in both a `third_party` directory as well as the `.gitignore`.
    
    Bug:1011811
    Change-Id: Ifeee519cb0a1f76d1d5abb24a0297d43c0f6864a

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -58,6 +58,9 @@ def _CheckChangesAreExclusiveToDirectory(input_api, output_api):
     for dirs in EXCLUSIVE_CHANGE_DIRECTORIES:
         affected_in_dir = filter(lambda f: FileIsInDir(f, dirs), affected_files)
         num_in_dir = len(affected_in_dir)
+        # Addition of new third_party folders must have a new entry in `.gitignore`
+        if '.gitignore' in affected_files:
+            num_in_dir = num_in_dir + 1
         if num_in_dir == 0:
             continue
         if num_in_dir < num_affected:
