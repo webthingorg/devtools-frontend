3116a0383f9a027c781aa46b26104e0ecea071f3
Tim van der Lippe <tvanderlippe@chromium.org>
2019-11-25 16:08:35 +0000

    Migrate cm_modes/ to ESM
    This also makes cm_modes/ non-remote and uses proper dynamic imports to
    load the code (rather than an `eval`). This increases the binary size by
    104 KB from 11756K to 11860K which is an increase of 0.8%.
    
    The stylus cm_mode has been removed. This mode was the largest mode
    (30KB), seems unmaintained [1] and this syntax is likely rarely used (if
    at all). It is unclear how often the other modes are installed and used.
    
    roll CodeMirror
    
    [1]: https://github.com/codemirror/CodeMirror/commits/master/mode/stylus
    
    Bug:1006759,1011466
    Change-Id: If4d81d01318e08a63faaffee8b0cc9ab5954d4c6

--- scripts/test/run_type_check.py
+++ scripts/test/run_type_check.py
@@ -284,6 +284,15 @@ GENERATED_SKIP_COMPILATION_FILES = [
     'cm/brace-fold.js',
     'audits/lighthouse/report.js',
     'audits/lighthouse/report-generator.js'
+    'cm_modes/clike.js',
+    'cm_modes/coffeescript.js',
+    'cm_modes/php.js',
+    'cm_modes/python.js',
+    'cm_modes/shell.js',
+    'cm_modes/livescript.js',
+    'cm_modes/markdown.js',
+    'cm_modes/clojure.js',
+    'cm_modes/jsx.js',
 ]
 
 
