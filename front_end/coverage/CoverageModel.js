e61d0634745fe2433ef9425a6e2053cc989f0dfc
Jan Scheffler <janscheffler@chromium.org>
2019-12-02 09:12:04 +0100

    Add coverage information to status bar
    This patch adds a new statusbar item to the sources panel
    to inform developers about the coverage of the current file
    or to open the coverage panel if no coverage has been reported yet.
    
    https://imgur.com/a/axWRJvJ
    
    Fixed: 1022013
    Change-Id: Ibd57bc6fa8a0730574abe8e009f67b245f43d0e3

--- front_end/coverage/CoverageModel.js
+++ front_end/coverage/CoverageModel.js
@@ -107,6 +107,7 @@ export default class CoverageModel extends SDK.SDKModel {
   reset() {
     this._coverageByURL = new Map();
     this._coverageByContentProvider = new Map();
+    this.dispatchEventToListeners(CoverageModel.Events.CoverageReset);
   }
 
   /**
@@ -223,6 +224,15 @@ export default class CoverageModel extends SDK.SDKModel {
    */
   entries() {
     return Array.from(this._coverageByURL.values());
+  }
+
+  /**
+   *
+   * @param {string} url
+   * @return {?Coverage.URLCoverageInfo}
+   */
+  getCoverageForUrl(url) {
+    return this._coverageByURL.get(url);
   }
 
   /**
@@ -565,16 +575,24 @@ export default class CoverageModel extends SDK.SDKModel {
   }
 }
 
+/** @enum {symbol} */
+CoverageModel.Events = {
+  CoverageUpdated: Symbol('CoverageUpdated'),
+  CoverageReset: Symbol('CoverageReset'),
+};
+
 SDK.SDKModel.register(CoverageModel, SDK.Target.Capability.None, false);
 
 /**
  * @unrestricted
  */
-export class URLCoverageInfo {
+export class URLCoverageInfo extends Common.Object {
   /**
    * @param {string} url
    */
   constructor(url) {
+    super();
+
     this._url = url;
     /** @type {!Map<string, !Coverage.CoverageInfo>} */
     this._coverageInfoByLocation = new Map();
@@ -656,6 +674,10 @@ export class URLCoverageInfo {
   _addToSizes(usedSize, size) {
     this._usedSize += usedSize;
     this._size += size;
+
+    if (usedSize !== 0 || size !== 0) {
+      this.dispatchEventToListeners(Coverage.URLCoverageInfo.Events.SizesChanged);
+    }
   }
 
   /**
@@ -691,6 +713,11 @@ export class URLCoverageInfo {
     return entry;
   }
 }
+
+/** @enum {symbol} */
+URLCoverageInfo.Events = {
+  SizesChanged: Symbol('SizesChanged')
+};
 
 /**
  * @unrestricted
@@ -837,9 +864,6 @@ Coverage = Coverage || {};
  * @constructor
  */
 Coverage.CoverageModel = CoverageModel;
-
-/** @enum {symbol} */
-Coverage.CoverageModel.Events = Events;
 
 /**
  * @enum {number}
