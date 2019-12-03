0bc88ea54c5dabea7275897fde218920f33e38f1
Mike Jackson <mjackson@microsoft.com>
2019-12-02 22:00:25 -0800

    Allow user to tab to the inspect link on the remote devices panel.
    This also collapses the deprecation message into a single
    resource string.
    
    Bug: 1029499
    Change-Id: I05046e21359dd2e3007ddc8158600d4ab46be752

--- front_end/devices/DevicesView.js
+++ front_end/devices/DevicesView.js
@@ -6,20 +6,14 @@ Devices.DevicesView = class extends UI.VBox {
   constructor() {
     super(true);
 
-    const startDeprecationMessage = ls`This panel has been deprecated in favor of the `;
-    const button = UI.Fragment.build`<a href="#">chrome://inspect/#devices</a>`;
-    const endDeprecationmessage = ls` interface, which has equivalent functionality.`;
+    const discoveryFooter = this.contentElement.createChild('span');
+    const documentationLink = UI.XLink.create('chrome://inspect/#devices');
 
-    const deprecationNotice = UI.Fragment.build`
-      <div style="user-select: text; padding: 5px;">
-        ${startDeprecationMessage}${button}${endDeprecationmessage}
-      </div>
-    `;
+    discoveryFooter.style.padding = '5px';
+    discoveryFooter.appendChild(UI.formatLocalized(
+        'This panel has been deprecated in favor of the %s interface, which has equivalent functionality.',
+        [documentationLink]));
 
-    this.contentElement.appendChild(deprecationNotice.element());
-
-    this.contentElement.querySelector('a').addEventListener('click', () => {
-      SDK.targetManager.mainTarget().pageAgent().navigate('chrome://inspect/#devices');
-    });
+    this.setDefaultFocusedElement(documentationLink);
   }
 };
