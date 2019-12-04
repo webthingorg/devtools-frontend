7bf95a3b3d767c30abbafdbd4f5302ba27d15c5f
Rob Paveza <Rob.Paveza@microsoft.com>
2019-12-04 09:54:36 -0700

    Squelch CSS race condition error on edits
    There are minor race conditions in which the StylePropertyTreeElement
    is tracking its own CSSProperty and the property is deleted from the
    CSSStyleDeclaration to which it belongs. The conditions are particularly
    difficult to reproduce, and the user experience does not seem to raise
    an issue, probably because changes to the underlying SDK model cause
    immediate refreshes of the entire tree).
    
    This change modifies the behavior to not cause an error in the event
    that the race condition is encountered by treating edits as having the
    entire string, so that user edits aren't dropped.
    
    Change-Id: I2777196cb88fa2928f8a5471398b0a58c8c67853

--- front_end/elements/StylePropertyTreeElement.js
+++ front_end/elements/StylePropertyTreeElement.js
@@ -932,7 +932,9 @@ export default class StylePropertyTreeElement extends UI.TreeElement {
     this._removePrompt();
     this.editingEnded(context);
     const isEditingName = context.isEditingName;
-    const nameValueEntered = isEditingName && this.nameElement.textContent.includes(':');
+    // If the underlying property has been ripped out, always assume that the value having been entered was
+    // a name-value pair and attempt to process it via the SDK.
+    const nameValueEntered = (isEditingName && this.nameElement.textContent.includes(':')) || !this.property;
 
     // Determine where to move to before making changes
     let createNewProperty, moveToSelector;
@@ -1096,7 +1098,8 @@ export default class StylePropertyTreeElement extends UI.TreeElement {
    * @return {!Promise}
    */
   async _innerApplyStyleText(styleText, majorChange, property) {
-    if (!this.treeOutline) {
+    // this.property might have been nulled at the end of the last _innerApplyStyleText
+    if (!this.treeOutline || !this.property) {
       return;
     }
 
