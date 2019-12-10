18f12a1df74a1aa4dd1177909d49b3123d1c28c9
Al Muthanna Athamina <almuthanna@google.com>
2019-12-10 14:22:05 +0100

    Add test files for existing classes
    This CL attempts to add Karma test files with minimal tests to make some
    classes appear in coverage reports and to use them as placeholders
    for future tests
    
    Bug: chromium:1032561
    Change-Id: Id9f60efad1387b13a30c7937bccda275f8e20bca

---
+++ test/unittests/front_end/ui/Context.ts
@@ -0,0 +1,14 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+
+import {default as Context} from '../../../../front_end/ui/Context.js';
+
+describe('Context', () => {
+  it('can be instantiated without issues', () => {
+    const context = new Context();
+    assert.equal(context.flavors().size, 0, 'context should not have any flavors upon instantiation');
+  });
+});
