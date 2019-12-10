5e124ec9f471b7b68cdbb9a60088d76a43eafb49
Tim van der Lippe <tvanderlippe@chromium.org>
2019-12-10 17:18:51 +0000

    Introduce front_end/common/BUILD.gn for TypeScript type-checking
    This adds a `BUILD.gn` for the `common/` folder to allow type-checking
    with TypeScript. The build configuration has been fixed such that
    type-checking happens on both DevTools and Chromium with the correct
    paths.
    
    Most of the files in `common/` fail to be tested with TypeScript, as
    TypeScript does not support `@interface`. Trie.js was the easiest and
    smallest file I could find that (with minimal changes) would pass the
    TypeScript checker.
    
    Bug: 1011811
    Change-Id: I482761bfbcbf459478dc7f51f2df6576ac872de6
    DISABLE_THIRD_PARTY_CHECK=TypeScript build configuration changes

--- third_party/typescript/ts_library.py
+++ third_party/typescript/ts_library.py
@@ -11,7 +11,7 @@ from os import path
 _CURRENT_DIR = path.join(path.dirname(__file__))
 TSC_LOCATION = path.join(_CURRENT_DIR, '..', '..', 'node_modules', 'typescript', 'bin', 'tsc')
 
-ROOT_TS_CONFIG_LOCATION = path.join(_CURRENT_DIR, 'tsconfig.json')
+ROOT_TS_CONFIG_LOCATION = path.join(_CURRENT_DIR, '..', '..', 'tsconfig.json')
 
 
 def runTsc(tsconfig_location):
@@ -24,7 +24,7 @@ def runTsc(tsconfig_location):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sources', nargs='*', required=True, help='List of TypeScript source files')
-    parser.add_argument('-b', '--tsconfig_location', required=True)
+    parser.add_argument('-b', '--tsconfig_output_location', required=True)
     opts = parser.parse_args()
     with open(ROOT_TS_CONFIG_LOCATION) as root_tsconfig:
         try:
@@ -33,7 +33,7 @@ def main():
             print('Encountered error while loading root tsconfig:')
             print(e)
             return 1
-    tsconfig_output_location = path.join(os.getcwd(), opts.tsconfig_location)
+    tsconfig_output_location = path.join(os.getcwd(), opts.tsconfig_output_location)
     tsconfig['files'] = [path.join(os.getcwd(), src) for src in opts.sources]
     tsconfig['compilerOptions']['outDir'] = path.dirname(tsconfig_output_location)
     with open(tsconfig_output_location, 'w') as generated_tsconfig:
