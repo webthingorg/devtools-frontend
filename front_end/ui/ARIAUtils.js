87160b4c17ba2258000f3d2881044b4ecfd2c290
Jack Lynch <jalyn@microsoft.com>
2019-12-16 14:00:41 -0800

    Mark Changes drawer as tabpanel
    This CL marks the list of files in the Changes drawer [1] as a tablist
    rather than a tree. This provides clarity for screen reader users
    because the current behavior better matches the Tab pattern as defined
    in the WAI-ARIA Authoring Practices [2]:
    
      > Tabs are a set of layered sections of content, known as tab panels,
      > that display one panel of content at a time. Each tab panel has an
      > associated tab element, that when activated, displays the panel.
      > The list of tab elements is arranged along one edge of the
      > currently displayed panel, most commonly the top edge.
    
    [1] https://gyazo.com/12ee1829f0660b77523eb51bf59481e1
    [2] https://www.w3.org/TR/wai-aria-practices-1.1/#tabpanel
    
    Bug: 963183
    Change-Id: Ic79c22ad55941e4b340c23c5979ec02adf60b080

--- front_end/ui/ARIAUtils.js
+++ front_end/ui/ARIAUtils.js
@@ -102,6 +102,13 @@ export function markAsTab(element) {
 /**
  * @param {!Element} element
  */
+export function markAsTablist(element) {
+  element.setAttribute('role', 'tablist');
+}
+
+/**
+ * @param {!Element} element
+ */
 export function markAsTabpanel(element) {
   element.setAttribute('role', 'tabpanel');
 }
