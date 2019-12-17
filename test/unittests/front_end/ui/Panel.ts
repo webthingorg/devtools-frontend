404a54b1407a5c1198dc225effee4fcfb3d04e16
Al Muthanna Athamina <almuthanna@google.com>
2019-12-17 16:16:10 +0100

    Add test files for classes depending on UI
    This CL attempts to add Karma test files with minimal tests to make some
    classes depending on UI appear in coverage reports and to use them as
    placeholders for future tests
    
    Bug: chromium:1032561
    Change-Id: Ic0fcc7ec18ce6843e7db55b37152bedcb82cd3bc

---
+++ test/unittests/front_end/ui/Panel.ts
@@ -0,0 +1,16 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+
+import {Panel} from '/front_end/ui/Panel.js';
+
+describe('Panel', () => {
+  it('can create a span element successfully', () => {
+    const panel = new Panel();
+    console.log(panel);
+  });
+
+  // TODO continue writing tests here or use another describe block
+});
