365e335481729e166c6332cf8f33d89e7fdbf993
Jan Scheffler <janscheffler@chromium.org>
2019-12-17 11:47:39 +0100

    Fix waterfall column shift in network panel
    This cl fixes the shifting of the waterfall column in the network panel
    that can happen when undocking the devtools window because toggling of a
    class does not immediatley lead to a redraw and therefor to a wrong
    offsetTop value in js.
    
    Fixed: chromium:1019723
    Change-Id: Ibb27391c2ba3af14d73adaf71e745d54d99c799d

--- front_end/network/NetworkLogViewColumns.js
+++ front_end/network/NetworkLogViewColumns.js
@@ -253,7 +253,14 @@ export default class NetworkLogViewColumns {
     this._waterfallColumn.setRowHeight(largeRows ? 41 : 21);
     this._waterfallScroller.classList.toggle('small', !largeRows);
     this._waterfallHeaderElement.classList.toggle('small', !largeRows);
-    this._waterfallColumn.setHeaderHeight(this._waterfallScroller.offsetTop);
+
+    // Request an animation frame because under certain conditions
+    // (see crbug.com/1019723) this._waterfallScroller.offsetTop does
+    // not return the value it's supposed to return as of the applied
+    // css classes.
+    window.requestAnimationFrame(() => {
+      this._waterfallColumn.setHeaderHeight(this._waterfallScroller.offsetTop);
+    });
   }
 
   /**
