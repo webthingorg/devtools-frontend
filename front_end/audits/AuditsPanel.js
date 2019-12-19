fc44138521b192748d8f69b07a6ce0a9deb5634e
Connor Clark <cjamcl@chromium.org>
2019-12-19 15:50:00 -0800

    test
    Change-Id: I8c1fff635d63d5aff53f2970ca348b53e78943e8

--- front_end/audits/AuditsPanel.js
+++ front_end/audits/AuditsPanel.js
@@ -215,15 +215,15 @@ export default class AuditsPanel extends UI.Panel {
    * @param {!ReportRenderer.RunnerResultArtifacts=} artifacts
    */
   _buildReportUI(lighthouseResult, artifacts) {
-    if (lighthouseResult === null) {
-      return;
-    }
+    // if (lighthouseResult === null) {
+    //   return;
+    // }
 
-    const optionElement = new Audits.ReportSelector.Item(
-        lighthouseResult, () => this._renderReport(lighthouseResult, artifacts), this._renderStartView.bind(this));
-    this._reportSelector.prepend(optionElement);
-    this._refreshToolbarUI();
-    this._renderReport(lighthouseResult);
+    // const optionElement = new Audits.ReportSelector.Item(
+    //     lighthouseResult, () => this._renderReport(lighthouseResult, artifacts), this._renderStartView.bind(this));
+    // this._reportSelector.prepend(optionElement);
+    // this._refreshToolbarUI();
+    // this._renderReport(lighthouseResult);
   }
 
   /**
@@ -285,10 +285,10 @@ export default class AuditsPanel extends UI.Panel {
 
       Host.userMetrics.actionTaken(Host.UserMetrics.Action.AuditsFinished);
 
-      await this._resetEmulationAndProtocolConnection();
+      // await this._resetEmulationAndProtocolConnection();
       this._buildReportUI(lighthouseResponse.lhr, lighthouseResponse.artifacts);
     } catch (err) {
-      await this._resetEmulationAndProtocolConnection();
+      // await this._resetEmulationAndProtocolConnection();
       if (err instanceof Error) {
         this._statusView.renderBugReport(err);
       }
@@ -297,7 +297,7 @@ export default class AuditsPanel extends UI.Panel {
 
   async _cancelAudit() {
     this._statusView.updateStatus(ls`Cancelling`);
-    await this._resetEmulationAndProtocolConnection();
+    // await this._resetEmulationAndProtocolConnection();
     this._renderStartView();
   }
 
