b8cec661460059a2e451e00161ae134e7258f8ce
Jack Lynch <jalyn@microsoft.com>
2019-12-30 19:16:51 -0800

    Highlight DOM breakpoints when hit
    This CL fixes an issue where one or both of the two instances of the DOM
    breakpoints pane (in Sources and Elements) fail to update when a
    breakpoint is hit.
    
    Bug: 973251
    Change-Id: I288dea794518605f8f93c28c7826372dbaea2e1f

--- front_end/ui/ViewManager.js
+++ front_end/ui/ViewManager.js
@@ -202,6 +202,7 @@ export class ContainerWidget extends VBox {
       const shouldFocus = this.element.hasFocus();
       this.setDefaultFocusedElement(null);
       this._view[widgetSymbol] = widget;
+      this._widget = widget;
       widget.show(this.element);
       if (shouldFocus) {
         widget.focus();
@@ -216,6 +217,7 @@ export class ContainerWidget extends VBox {
    */
   wasShown() {
     this._materialize().then(() => {
+      this._widget.show(this.element);
       this._wasShownForTest();
     });
   }
@@ -253,6 +255,16 @@ export class _ExpandableContainerWidget extends VBox {
     UI.ARIAUtils.setControls(this._titleElement, this.contentElement.createChild('slot'));
     this._view = view;
     view[_ExpandableContainerWidget._symbol] = this;
+  }
+
+  /**
+   * @override
+   */
+  async wasShown() {
+    if (this._widget) {
+      await this._materializePromise;
+      this._widget.show(this.element);
+    }
   }
 
   /**
