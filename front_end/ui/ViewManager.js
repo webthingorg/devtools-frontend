8bc89d80c0a314e27dc9d584fe2719474e61ae2c
Jack Lynch <jalyn@microsoft.com>
2020-01-06 14:49:06 -0800

    Highlight DOM breakpoints when hit
    This CL fixes an issue where one or both of the two instances of the DOM
    breakpoints pane (in Sources and Elements) fail to update when a
    breakpoint is hit. There's a video in the linked bug showing more
    specific repro steps, but the issue stems from three different
    extensions in browser_debugger.json that reference
    DOMBreakpointsSidebarPane: two views and a ContextFlavorListener. Unlike
    every other view in the devtools, these ones define a factoryName rather
    than a className. This makes the devtools create separate instances for
    each rather than using runtime.sharedInstance(), which breaks
    UI.Context._dispatchFlavorChange(), which does rely on
    runtime.sharedInstance().
    
    In order to make _dispatchFlavorChange() work properly with both the
    Elements and Sources panel views, I replaced their factoryNames with
    classNames, i.e. made them share an instance. Some minor changes to
    ContainerWidget and _ExpandableContainerWidget were needed to make sure
    the shared instance actually shows up where needed, but everything now
    works as expected. An alternative would be to rework
    Extension.instance() and ContextFlavorListener, but that seems more
    invasive.
    
    Bug: 973251
    Change-Id: I288dea794518605f8f93c28c7826372dbaea2e1f

--- front_end/ui/ViewManager.js
+++ front_end/ui/ViewManager.js
@@ -216,6 +216,7 @@ export class ContainerWidget extends VBox {
    */
   wasShown() {
     this._materialize().then(() => {
+      this._view[widgetSymbol].show(this.element);
       this._wasShownForTest();
     });
   }
@@ -253,6 +254,15 @@ export class _ExpandableContainerWidget extends VBox {
     UI.ARIAUtils.setControls(this._titleElement, this.contentElement.createChild('slot'));
     this._view = view;
     view[_ExpandableContainerWidget._symbol] = this;
+  }
+
+  /**
+   * @override
+   */
+  wasShown() {
+    if (this._widget) {
+      this._materializePromise.then(() => this._widget.show(this.element));
+    }
   }
 
   /**
