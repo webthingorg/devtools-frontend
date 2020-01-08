41c416df61d1971454fc3f7c5f0701de33204911
Michael Liao <michael.liao@microsoft.com>
2020-01-07 16:36:04 -0800

    [Profiler]: Adding SR functionality for heap size and trends in Isolate Selector list item
    Changes:
    - Adding Heap size and trend text of isolate list item to accessible label
    
    Image of isolate item to SR text:
    https://imgur.com/ync6kTI
    
    The trend sizes are read as "Up arrow 6.0 KB slash S" or "Down arrow 6.0 KB slash S"
    
    Bug: 963183
    Change-Id: I2eebdf2516288f16eb3b1d3088f0e6a26ae98556

--- front_end/profiler/IsolateSelector.js
+++ front_end/profiler/IsolateSelector.js
@@ -235,6 +235,7 @@ Profiler.IsolateSelector.ListItem = class {
   updateStats() {
     this._heapDiv.textContent = Number.bytesToString(this._isolate.usedHeapSize());
     Profiler.IsolateSelector._formatTrendElement(this._isolate.usedHeapSizeGrowRate(), this._trendDiv);
+    this.updateTitle();
   }
 
   updateTitle() {
@@ -257,6 +258,10 @@ Profiler.IsolateSelector.ListItem = class {
       titleDiv.textContent = title;
       titleDiv.title = title;
     }
-    UI.ARIAUtils.setAccessibleName(this.element, titles.join(' '));
+    const trendText = this._trendDiv.textContent;
+    const heapSize = this._heapDiv.textContent;
+    const isolateItemLabel = trendText ? ls`${titles.join(' ')}. Size: ${heapSize} Trend: ${trendText}` :
+                                         ls`${titles.join(' ')}. Size: ${heapSize}`;
+    UI.ARIAUtils.setAccessibleName(this.element, isolateItemLabel);
   }
 };
