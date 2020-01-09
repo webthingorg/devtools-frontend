6a8f7b6cc1440caad44cc7030a1a1707c9c0299b
Paul Lewis <aerotwist@chromium.org>
2020-01-09 14:35:27 +0000

    Migrates audits/ to ESM internally
    Bug: 1006759
    Change-Id: I8a8d88f00c2c039dd9effe8335f3e7c1bc5bb4a4

--- front_end/audits/audits.js
+++ front_end/audits/audits.js
@@ -2,17 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './lighthouse/report.js';
-import './lighthouse/report-generator.js';
-import './RadioSetting.js';
-import './AuditsPanel.js';
-import './AuditsController.js';
-import './AuditsReportSelector.js';
-import './AuditsReportRenderer.js';
-import './AuditsStartView.js';
-import './AuditsStatusView.js';
-import './AuditsProtocolService.js';
-
 import * as AuditsController from './AuditsController.js';
 import * as AuditsPanel from './AuditsPanel.js';
 import * as AuditsProtocolService from './AuditsProtocolService.js';
