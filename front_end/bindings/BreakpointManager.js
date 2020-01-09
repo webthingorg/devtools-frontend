82f9471258a33d90e1dd9a79c3a2aadb338546ae
Simon Zünd <szuend@chromium.org>
2020-01-09 13:47:17 +0100

    Properly await {Breakpoint._refreshBreakpoint}
    Breakpoint._refreshBreakpoint has one use-site left where the result is
    not awaited. This CL fixes that.
    
    Bug: chromium:1032016
    Change-Id: Ib7979105d5ab7be9812b6cc3f01dea8effc36d09

--- front_end/bindings/BreakpointManager.js
+++ front_end/bindings/BreakpointManager.js
@@ -317,12 +317,12 @@ export class Breakpoint {
     this._breakpointManager._targetManager.observeModels(SDK.DebuggerModel, this);
   }
 
-  refreshInDebugger() {
+  async refreshInDebugger() {
     if (this._isRemoved) {
       return;
     }
     for (const breakpoint of this._modelBreakpoints.values()) {
-      breakpoint._refreshBreakpoint();
+      await breakpoint._refreshBreakpoint();
     }
   }
 
