229a50378f0d971186c95609d15c6cee57428259
Ted Meyer <tmathmeyer@chromium.org>
2020-01-10 16:26:34 -0800

    Rolled MediaTable into EventDisplayTable
    Also cleaned up some the table elements to get json views - this
    required a small change to the constructor for the JSONView class
    which allows the collapsible viewer to start collapsed instead of
    expanded. Since it's an optional arg, it shouldn't doesn't affect
    any other uses of the class.
    
    Bug: 794255
    Change-Id: Ib83636a358e5eeeb47f958fd8046e80b0896a424

--- front_end/media/EventDisplayTable.js
+++ front_end/media/EventDisplayTable.js
@@ -22,6 +22,13 @@ Media.EventDisplayColumnConfig;
  */
 Media.Event;
 
+/** @enum {string} */
+Media.MediaEventColumnKeys = {
+  Timestamp: 'timestamp',
+  Event: 'event',
+  Value: 'value'
+};
+
 /**
  * @unrestricted
  */
@@ -40,8 +47,15 @@ Media.EventNode = class extends DataGrid.SortableDataGridNode {
    */
   createCell(columnId) {
     const cell = this.createTD(columnId);
-    const cellData = /** @type string */ (this.data[columnId]);
-    cell.createTextChild(cellData);
+    const cellData = this.data[columnId];
+    if (columnId === Media.MediaEventColumnKeys.Value) {
+      const area = new SourceFrame.JSONView(new SourceFrame.ParsedJSON(cellData, '', ''), true);
+      area.markAsRoot();
+      area.show(cell);
+    } else {
+      cell.classList.add('event-display-table-basic-text-table-entry');
+      cell.createTextChild(cellData);
+    }
     return cell;
   }
 
@@ -57,23 +71,30 @@ Media.EventNode = class extends DataGrid.SortableDataGridNode {
 /**
  * @unrestricted
  */
-Media.EventDisplayTable = class extends UI.VBox {
-  /**
-   * @param {!Array.<!Media.EventDisplayColumnConfig>} headerDescriptors
-   * @param {?string=} uniqueColumn
-   * @param {?string=} defaultSortingColumnId
-   */
-  constructor(headerDescriptors, uniqueColumn, defaultSortingColumnId) {
+Media.PlayerEventsView = class extends UI.VBox {
+  constructor() {
     super();
 
     // Set up element styles.
     this.registerRequiredCSS('media/eventDisplayTable.css');
     this.contentElement.classList.add('event-display-table-contents-table-container');
 
-    this._uniqueColumnEntryKey = uniqueColumn;
-    this._uniqueColumnMap = new Map();
+    this._dataGrid = this._createDataGrid(
+        [
+          {
+            id: Media.MediaEventColumnKeys.Timestamp,
+            title: ls`Timestamp`,
+            weight: 1,
+            sortable: true,
+            sortingFunction:
+                DataGrid.SortableDataGrid.NumericComparator.bind(null, Media.MediaEventColumnKeys.Timestamp)
+          },
+          {id: Media.MediaEventColumnKeys.Event, title: ls`Event Name`, weight: 2, sortable: false},
+          {id: Media.MediaEventColumnKeys.Value, title: ls`Value`, weight: 7, sortable: false}
+        ],
+        Media.MediaEventColumnKeys.Timestamp);
 
-    this._dataGrid = this._createDataGrid(headerDescriptors, defaultSortingColumnId);
+    this._firstEventTime = 0;
     this._dataGrid.setStriped(true);
     this._dataGrid.asWidget().show(this.contentElement);
   }
@@ -87,7 +108,7 @@ Media.EventDisplayTable = class extends UI.VBox {
     const gridColumnDescs = [];
     const sortFunctionMap = new Map();
     for (const headerDesc of headers) {
-      gridColumnDescs.push(Media.EventDisplayTable._convertToGridDescriptor(headerDesc));
+      gridColumnDescs.push(Media.PlayerEventsView._convertToGridDescriptor(headerDesc));
       if (headerDesc.sortable) {
         sortFunctionMap.set(headerDesc.id, headerDesc.sortingFunction);
         if (!default_sort) {
@@ -112,11 +133,17 @@ Media.EventDisplayTable = class extends UI.VBox {
   }
 
   /**
-   * @param {!Array.<!Media.Event>} events
+   * @param {string} playerID
+   * @param {!Array.<!Media.Event>} changes
+   * @param {!Media.MediaModel.MediaChangeTypeKeys} change_type
    */
-  addEvents(events) {
-    for (const event of events) {
-      this.addEvent(event);
+  renderChanges(playerID, changes, change_type) {
+    if (this._firstEventTime === 0 && changes.length > 0) {
+      this._firstEventTime = changes[0].timestamp;
+    }
+
+    for (const event of changes) {
+      this.addEvent(this._subtractFirstEventTime(event));
     }
   }
 
@@ -124,18 +151,20 @@ Media.EventDisplayTable = class extends UI.VBox {
    * @param {!Media.Event} event
    */
   addEvent(event) {
-    if (this._uniqueColumnEntryKey) {
-      const eventValue = event[this._uniqueColumnEntryKey];
-      if (this._uniqueColumnMap.has(eventValue)) {
-        this._uniqueColumnMap.get(eventValue).data = event;
-        return;
-      }
-    }
+    const json = JSON.parse(event.value);
+    event.event = json.event;
+    delete json['event'];
+    event.value = json;
     const node = new Media.EventNode(event);
     this._dataGrid.rootNode().insertChildOrdered(node);
-    if (this._uniqueColumnEntryKey) {
-      this._uniqueColumnMap.set(event[this._uniqueColumnEntryKey], node);
-    }
+  }
+
+  /**
+   * @param {!Media.Event} event
+   */
+  _subtractFirstEventTime(event) {
+    event.timestamp = (event.timestamp - this._firstEventTime).toFixed(3);
+    return event;
   }
 
   /**
