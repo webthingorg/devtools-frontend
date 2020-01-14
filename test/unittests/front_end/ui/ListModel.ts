04ba7ece9be76ccfda6091360ff9db5aae82f4a8
Mathias Bynens <mathias@chromium.org>
2020-01-14 17:07:23 +0100

    Add tests for ListModel operations
    This patch imports the tests from
    chromium/src/third_party/blink/web_tests/http/tests/devtools/unit/list-model.js.
    
    Change-Id: I94e8cbc84a839fc07421dcd1ca5b86e153a0bce0

--- test/unittests/front_end/ui/ListModel.ts
+++ test/unittests/front_end/ui/ListModel.ts
@@ -20,5 +20,40 @@ describe('ListModel', () => {
     assert.equal(listModel.length, 0, 'length of list model should be 0');
   });
 
-  // TODO continue writing tests here or use another describe block
+  it('supports various list operations', () => {
+    const model = new ListModel();
+
+    model.replaceAll([0, 1, 2]);
+    assert.deepEqual([...model], [0, 1, 2]);
+
+    model.replaceRange(0, 1, [5, 6, 7]);
+    assert.deepEqual([...model], [5, 6, 7, 1, 2]);
+
+    model.insert(model.length, 10);
+    assert.deepEqual([...model], [5, 6, 7, 1, 2, 10]);
+
+    model.remove(model.length - 1);
+    assert.deepEqual([...model], [5, 6, 7, 1, 2]);
+
+    model.remove(4);
+    assert.deepEqual([...model], [5, 6, 7, 1]);
+
+    model.insert(1, 8);
+    assert.deepEqual([...model], [5, 8, 6, 7, 1]);
+
+    model.replaceAll([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+    assert.deepEqual([...model], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+
+    model.replaceRange(7, 8, [27]);
+    assert.deepEqual([...model], [0, 1, 2, 3, 4, 5, 6, 27, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+
+    model.replaceRange(18, 20, [28, 29]);
+    assert.deepEqual([...model], [0, 1, 2, 3, 4, 5, 6, 27, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 28, 29]);
+
+    model.replaceRange(1, 4, [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43]);
+    assert.deepEqual([...model], [0, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 4, 5, 6, 27, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 28, 29]);
+
+    model.replaceRange(0, 29, []);
+    assert.deepEqual([...model], [29]);
+  });
 });
