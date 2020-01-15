929bb43128786f9b450e28c921e74bfbd74f76e8
Paul Lewis <aerotwist@chromium.org>
2020-01-15 16:00:20 +0000

    Migrates sources/ to ESM internally
    Bug: 1006759
    Change-Id: Ie1ca01eaadde94191431217f297b108dde5e8adf

--- front_end/sources/UISourceCodeFrame.js
+++ front_end/sources/UISourceCodeFrame.js
@@ -26,7 +26,16 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-export default class UISourceCodeFrame extends SourceFrame.SourceFrame {
+import {CoveragePlugin} from './CoveragePlugin.js';
+import {CSSPlugin} from './CSSPlugin.js';
+import {DebuggerPlugin} from './DebuggerPlugin.js';
+import {GutterDiffPlugin} from './GutterDiffPlugin.js';
+import {JavaScriptCompilerPlugin} from './JavaScriptCompilerPlugin.js';
+import {ScriptOriginPlugin} from './ScriptOriginPlugin.js';
+import {SnippetsPlugin} from './SnippetsPlugin.js';
+import {SourcesPanel} from './SourcesPanel.js';
+
+export class UISourceCodeFrame extends SourceFrame.SourceFrame {
   /**
    * @param {!Workspace.UISourceCode} uiSourceCode
    */
@@ -294,7 +303,7 @@ export default class UISourceCodeFrame extends SourceFrame.SourceFrame {
     if (this._isSettingContent) {
       return;
     }
-    Sources.SourcesPanel.instance().updateLastModificationTime();
+    SourcesPanel.instance().updateLastModificationTime();
     this._muteSourceCodeEvents = true;
     if (this.isClean()) {
       this._uiSourceCode.resetWorkingCopy();
@@ -339,27 +348,27 @@ export default class UISourceCodeFrame extends SourceFrame.SourceFrame {
     const pluginUISourceCode = binding ? binding.network : this._uiSourceCode;
 
     // The order of these plugins matters for toolbar items
-    if (Sources.DebuggerPlugin.accepts(pluginUISourceCode)) {
-      this._plugins.push(new Sources.DebuggerPlugin(this.textEditor, pluginUISourceCode, this.transformer()));
+    if (DebuggerPlugin.accepts(pluginUISourceCode)) {
+      this._plugins.push(new DebuggerPlugin(this.textEditor, pluginUISourceCode, this.transformer()));
     }
-    if (Sources.CSSPlugin.accepts(pluginUISourceCode)) {
-      this._plugins.push(new Sources.CSSPlugin(this.textEditor));
+    if (CSSPlugin.accepts(pluginUISourceCode)) {
+      this._plugins.push(new CSSPlugin(this.textEditor));
     }
-    if (!this.pretty && Sources.JavaScriptCompilerPlugin.accepts(pluginUISourceCode)) {
-      this._plugins.push(new Sources.JavaScriptCompilerPlugin(this.textEditor, pluginUISourceCode));
+    if (!this.pretty && JavaScriptCompilerPlugin.accepts(pluginUISourceCode)) {
+      this._plugins.push(new JavaScriptCompilerPlugin(this.textEditor, pluginUISourceCode));
     }
-    if (Sources.SnippetsPlugin.accepts(pluginUISourceCode)) {
-      this._plugins.push(new Sources.SnippetsPlugin(this.textEditor, pluginUISourceCode));
+    if (SnippetsPlugin.accepts(pluginUISourceCode)) {
+      this._plugins.push(new SnippetsPlugin(this.textEditor, pluginUISourceCode));
     }
-    if (Sources.ScriptOriginPlugin.accepts(pluginUISourceCode)) {
-      this._plugins.push(new Sources.ScriptOriginPlugin(this.textEditor, pluginUISourceCode));
+    if (ScriptOriginPlugin.accepts(pluginUISourceCode)) {
+      this._plugins.push(new ScriptOriginPlugin(this.textEditor, pluginUISourceCode));
     }
     if (!this.pretty && Root.Runtime.experiments.isEnabled('sourceDiff') &&
-        Sources.GutterDiffPlugin.accepts(pluginUISourceCode)) {
-      this._plugins.push(new Sources.GutterDiffPlugin(this.textEditor, pluginUISourceCode));
+        GutterDiffPlugin.accepts(pluginUISourceCode)) {
+      this._plugins.push(new GutterDiffPlugin(this.textEditor, pluginUISourceCode));
     }
-    if (Sources.CoveragePlugin.accepts(pluginUISourceCode)) {
-      this._plugins.push(new Sources.CoveragePlugin(this.textEditor, pluginUISourceCode));
+    if (CoveragePlugin.accepts(pluginUISourceCode)) {
+      this._plugins.push(new CoveragePlugin(this.textEditor, pluginUISourceCode));
     }
 
     this.dispatchEventToListeners(Events.ToolbarItemsChanged);
@@ -607,17 +616,17 @@ export default class UISourceCodeFrame extends SourceFrame.SourceFrame {
   }
 }
 
-export const _iconClassPerLevel = {};
-_iconClassPerLevel[Workspace.UISourceCode.Message.Level.Error] = 'smallicon-error';
-_iconClassPerLevel[Workspace.UISourceCode.Message.Level.Warning] = 'smallicon-warning';
+export const iconClassPerLevel = {};
+iconClassPerLevel[Workspace.UISourceCode.Message.Level.Error] = 'smallicon-error';
+iconClassPerLevel[Workspace.UISourceCode.Message.Level.Warning] = 'smallicon-warning';
 
-export const _bubbleTypePerLevel = {};
-_bubbleTypePerLevel[Workspace.UISourceCode.Message.Level.Error] = 'error';
-_bubbleTypePerLevel[Workspace.UISourceCode.Message.Level.Warning] = 'warning';
+export const bubbleTypePerLevel = {};
+bubbleTypePerLevel[Workspace.UISourceCode.Message.Level.Error] = 'error';
+bubbleTypePerLevel[Workspace.UISourceCode.Message.Level.Warning] = 'warning';
 
-export const _lineClassPerLevel = {};
-_lineClassPerLevel[Workspace.UISourceCode.Message.Level.Error] = 'text-editor-line-with-error';
-_lineClassPerLevel[Workspace.UISourceCode.Message.Level.Warning] = 'text-editor-line-with-warning';
+export const lineClassPerLevel = {};
+lineClassPerLevel[Workspace.UISourceCode.Message.Level.Error] = 'text-editor-line-with-error';
+lineClassPerLevel[Workspace.UISourceCode.Message.Level.Warning] = 'text-editor-line-with-warning';
 
 /**
  * @unrestricted
@@ -631,10 +640,10 @@ export class RowMessage {
     this._repeatCount = 1;
     this.element = createElementWithClass('div', 'text-editor-row-message');
     this._icon = this.element.createChild('label', '', 'dt-icon-label');
-    this._icon.type = _iconClassPerLevel[message.level()];
+    this._icon.type = iconClassPerLevel[message.level()];
     this._repeatCountElement =
         this.element.createChild('span', 'text-editor-row-message-repeat-count hidden', 'dt-small-bubble');
-    this._repeatCountElement.type = _bubbleTypePerLevel[message.level()];
+    this._repeatCountElement.type = bubbleTypePerLevel[message.level()];
     const linesContainer = this.element.createChild('div');
     const lines = this._message.text().split('\n');
     for (let i = 0; i < lines.length; ++i) {
@@ -737,7 +746,7 @@ export class RowMessageBucket {
     }
     const editorLineNumber = position.lineNumber;
     if (this._level) {
-      this.textEditor.toggleLineClass(editorLineNumber, _lineClassPerLevel[this._level], false);
+      this.textEditor.toggleLineClass(editorLineNumber, lineClassPerLevel[this._level], false);
     }
     if (this._decorationStartColumn !== null) {
       this.textEditor.removeDecoration(this._decoration, editorLineNumber);
@@ -817,15 +826,15 @@ export class RowMessageBucket {
       return;
     }
     if (this._level) {
-      this.textEditor.toggleLineClass(editorLineNumber, _lineClassPerLevel[this._level], false);
+      this.textEditor.toggleLineClass(editorLineNumber, lineClassPerLevel[this._level], false);
       this._icon.type = '';
     }
     this._level = maxMessage.level();
     if (!this._level) {
       return;
     }
-    this.textEditor.toggleLineClass(editorLineNumber, _lineClassPerLevel[this._level], true);
-    this._icon.type = _iconClassPerLevel[this._level];
+    this.textEditor.toggleLineClass(editorLineNumber, lineClassPerLevel[this._level], true);
+    this._icon.type = iconClassPerLevel[this._level];
   }
 }
 
@@ -903,28 +912,3 @@ export class Plugin {
 export const Events = {
   ToolbarItemsChanged: Symbol('ToolbarItemsChanged')
 };
-
-/* Legacy exported object */
-self.Sources = self.Sources || {};
-
-/* Legacy exported object */
-Sources = Sources || {};
-
-/** @constructor */
-Sources.UISourceCodeFrame = UISourceCodeFrame;
-
-Sources.UISourceCodeFrame._iconClassPerLevel = _iconClassPerLevel;
-Sources.UISourceCodeFrame._bubbleTypePerLevel = _bubbleTypePerLevel;
-Sources.UISourceCodeFrame._lineClassPerLevel = _lineClassPerLevel;
-
-/** @constructor */
-Sources.UISourceCodeFrame.RowMessage = RowMessage;
-
-/** @constructor */
-Sources.UISourceCodeFrame.RowMessageBucket = RowMessageBucket;
-
-/** @constructor */
-Sources.UISourceCodeFrame.Plugin = Plugin;
-
-/** @enum {symbol} */
-Sources.UISourceCodeFrame.Events = Events;
