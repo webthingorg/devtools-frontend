3cd0fe7d41c3e04d0740c77859f1b7971b3a2f5b
Chandani Shrestha <chshrest@microsoft.com>
2020-01-21 10:23:26 -0800

    [Elements]: Keyboard and screen reader accessibility of swatch drag element in color picker
    This change makes color swatch which lives color-picker keyboard accessible.
    Earlier, keyboard user couldn't move drag element in color-picker to select different color.
    In this change user can tab into drag element on first tab when color-picker is open then, using arrow keys it could be moved UP, DOWN, LEFT and RIGHT to select different colors.
    The change is tested against both mouse and keydown handler.
    
    Gif showing the change https://imgur.com/MZWaWgC
    
    Bug: 963183
    Change-Id: I9d073b9b462a37e74e9d01adf30083b28015d0ca

--- front_end/color_picker/Spectrum.js
+++ front_end/color_picker/Spectrum.js
@@ -52,10 +52,15 @@ export class Spectrum extends UI.VBox {
 
     super(true);
     this.registerRequiredCSS('color_picker/spectrum.css');
-    this.contentElement.tabIndex = 0;
-    this.setDefaultFocusedElement(this.contentElement);
 
     this._colorElement = this.contentElement.createChild('div', 'spectrum-color');
+    this._colorElement.tabIndex = 0;
+    this.setDefaultFocusedElement(this._colorElement);
+    this._colorElement.addEventListener('keydown', this._onSliderKeydown.bind(this, positionColor.bind(this)));
+    const swatchAriaText = ls
+    `Press arrow keys with or without modifiers to move swatch position. Alt moves tiny bit, Ctrl moves less and Shift moves large`;
+    UI.ARIAUtils.setAccessibleName(this._colorElement, swatchAriaText);
+    UI.ARIAUtils.markAsApplication(this._colorElement);
     this._colorDragElement = this._colorElement.createChild('div', 'spectrum-sat fill')
                                  .createChild('div', 'spectrum-val fill')
                                  .createChild('div', 'spectrum-dragger');
@@ -88,6 +93,7 @@ export class Spectrum extends UI.VBox {
 
     // RGBA/HSLA display.
     this._displayContainer = toolsContainer.createChild('div', 'spectrum-text source-code');
+    UI.ARIAUtils.markAsPoliteLiveRegion(this._displayContainer, true);
     this._textValues = [];
     for (let i = 0; i < 4; ++i) {
       const inputValue = UI.createInput('spectrum-text-value');
@@ -103,6 +109,7 @@ export class Spectrum extends UI.VBox {
 
     // HEX display.
     this._hexContainer = toolsContainer.createChild('div', 'spectrum-text spectrum-text-hex source-code');
+    UI.ARIAUtils.markAsPoliteLiveRegion(this._hexContainer, true);
     this._hexValue = UI.createInput('spectrum-text-value');
     this._hexContainer.appendChild(this._hexValue);
     this._hexValue.maxLength = 9;
@@ -262,10 +269,53 @@ export class Spectrum extends UI.VBox {
      */
     function positionColor(event) {
       const hsva = this._hsv.slice();
-      hsva[1] = Number.constrain((event.x - this._colorOffset.left) / this.dragWidth, 0, 1);
-      hsva[2] = Number.constrain(1 - (event.y - this._colorOffset.top) / this.dragHeight, 0, 1);
+      const colorPosition = getUpdatedColorPosition(this._colorDragElement, event);
+      this._colorOffset = this._colorElement.totalOffset();
+      hsva[1] = Number.constrain((colorPosition.x - this._colorOffset.left) / this.dragWidth, 0, 1);
+      hsva[2] = Number.constrain(1 - (colorPosition.y - this._colorOffset.top) / this.dragHeight, 0, 1);
 
       this._innerSetColor(hsva, '', undefined /* colorName */, undefined, ChangeSource.Other);
+    }
+
+    /**
+     * @param {!Element} dragElement
+     * @param {!Event} event
+     * @return {{x: number, y: number}}
+     */
+    function getUpdatedColorPosition(dragElement, event) {
+      const elementPosition = dragElement.getBoundingClientRect();
+      const verticalX = elementPosition.x + elementPosition.width / 2;
+      const horizontalY = elementPosition.y + elementPosition.width / 2;
+      const defaultUnit = elementPosition.width / 4;
+      const unit = getUnitToMove(defaultUnit, event);
+      switch (event.key) {
+        case 'ArrowLeft':
+          return {x: elementPosition.left - unit, y: horizontalY};
+        case 'ArrowRight':
+          return {x: elementPosition.right + unit, y: horizontalY};
+        case 'ArrowDown':
+          return {x: verticalX, y: elementPosition.bottom + unit};
+        case 'ArrowUp':
+          return {x: verticalX, y: elementPosition.top - unit};
+        default:
+          return {x: event.x, y: event.y};
+      }
+    }
+
+    /**
+     * @param {number} unit
+     * @param {!Event} event
+     * @return {number}
+     */
+    function getUnitToMove(unit, event) {
+      if (event.altKey) {
+        unit = 1;
+      } else if (event.ctrlKey) {
+        unit = 10;
+      } else if (event.shiftKey) {
+        unit = 20;
+      }
+      return unit;
     }
   }
 
