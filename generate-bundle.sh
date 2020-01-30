56928ec53a8424a24e35841342f0a7852eefc828
Kunihiko Sakamoto <ksakamoto@chromium.org>
2020-01-30 16:23:42 +0900

    [DO NOT COMMIT] WebBundle build (for loading performance testing)
    The generate-bundle.sh script creates a web bundle from the
    front_end/ directory.
    
    The CSP directive in devtools_app.html is removed so that the
    WebBundle can be loaded from an opaque origin (file://).
    
    Change-Id: Ibf48d0ca44e7a01ee9a9b997da7465832b24184f

---
+++ generate-bundle.sh
@@ -0,0 +1,6 @@
+gen-bundle \
+    -dir front_end \
+    -baseURL https://devtools.wbn/ \
+    -primaryURL https://devtools.wbn/devtools_app.html \
+    -headerOverride 'Access-Control-Allow-Origin: *' \
+    -o devtools.wbn
