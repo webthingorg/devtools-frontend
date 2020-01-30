ac20aa2388205c8a4cb08e2af36adebd22a1a725
Kunihiko Sakamoto <ksakamoto@chromium.org>
2020-01-30 15:53:14 +0900

    [DO NOT COMMIT] Webpack build (for loading performance testing)
    This adds webpack.config.js that bundles modules under front_end/.
    The resulting build can be loaded from browser by running server.js and
    navigating to http://localhost:8080/devtools_app.html.
    
    To be able to build with webpack, the following changes have been made:
    
    - Removed AMD/CommonJS supporting code with require() calls because that
      confuse Webpack (https://github.com/webpack/webpack/issues/8826).
      - For modules under cm/ and cm_web_modes/, this is done by a webpack
        plugin (replace-loader.js).
    - Stopped wrapping import() with eval, so that Webpack understands it.
      - And its argument must end with ".js" so that Webpack will not try
        to ingest non-js files.
    
    Change-Id: I49eb96bcf5ddc99dc8552ff93405550a8548926d

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const handlers = [
+  express.static("bundle"),
+  express.static("front_end"),
+];
+app.use(...handlers);
+
+const listener = app.listen(process.env.PORT || 8080, () => {
+  console.log('The server is listening on port ' + listener.address().port);
+});
