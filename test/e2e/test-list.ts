4db82220783f11d6519bd57360d0ddc92aebef6b
Tim van der Lippe <tvanderlippe@chromium.org>
2020-02-05 14:55:31 +0000

    Remove hello world e2etest
    This one is replaced by the console tests that do more checks.
    
    Bug: 1047671
    Change-Id: I541c5fa984ada7f6260f2c32bb4f6e6f1768ddcb

--- test/e2e/test-list.ts
+++ test/e2e/test-list.ts
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 export const testList = [
-  './hello-world/hello-world.js',
   './sources/script-in-multiple-workers.js',
   './console/console-message-format.js',
 ];
