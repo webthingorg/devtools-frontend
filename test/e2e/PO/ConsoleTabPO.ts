bfd5210b004483ae5fa125e2d803d9f7d7ca876a
Al Muthanna Athamina <almuthanna@chromium.org>
2020-02-07 14:03:13 +0100

    Refactor Console E2E test to use PO pattern
    This CL attempts to modify the way we access
    selectors for the Console E2E tests in order
    to reduce duplication.
    
    Change-Id: I15c007240143313f2edb8b9c21836492ed60aa7d

---
+++ test/e2e/PO/ConsoleTabPO.ts
@@ -0,0 +1,10 @@
+class ConsoleTabPO {
+  get tabConsole() {return '#tab-console'}
+  get defaultLevel() {return '[aria-label="Log level: Default levels"]'}
+  get verboseLevel() {return '[aria-label="Verbose, unchecked"]'}
+  get loggedConsoleMessages() {return '.console-group-messages'}
+  get firstMessageFromConsole() {return '.console-group-messages .source-code .console-message-text'}
+}
+
+export default new ConsoleTabPO();
+
