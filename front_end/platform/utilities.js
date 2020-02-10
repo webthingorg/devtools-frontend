df7e54cea611b6076a99652255afc32a4698e926
Simon Zünd <szuend@chromium.org>
2020-02-10 14:00:58 +0100

    [utilities] Replace {valuesArray} with {values} in for .. of loops
    This CL is part of the effort to get rid of prototype hijacking in
    utilties.js.
    
    R=tvanderlippe@chromium.org
    
    Bug: chromium:1050549
    Change-Id: I7f0fed58f2a30f19403e7cd3e9d83101370de1dd

--- front_end/platform/utilities.js
+++ front_end/platform/utilities.js
@@ -1164,7 +1164,7 @@ const Multimap = class {
     const result = [];
     const keys = this.keysArray();
     for (let i = 0; i < keys.length; ++i) {
-      result.push(...this.get(keys[i]).valuesArray());
+      result.push(...this.get(keys[i]).values());
     }
     return result;
   }
