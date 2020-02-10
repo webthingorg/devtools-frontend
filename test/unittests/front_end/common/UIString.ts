2240bc909239168f797351e5cbaa1c6e89a6eb9c
Jack Franklin <jacktfranklin@chromium.org>
2020-02-10 12:17:17 +0000

    [Unittest] Add unit tests to UIString
    Bug: 1009443
    Change-Id: I4f82d785e5d2f6ab2b2d3a6b4e735961ff906c0e

--- test/unittests/front_end/common/UIString.ts
+++ test/unittests/front_end/common/UIString.ts
@@ -6,12 +6,35 @@ const {assert} = chai;
 
 import * as Common from '/front_end/common/common.js';
 
-describe('UIString', () => {
+describe.only('UIString', () => {
   it('serializes UI strings', () => {
     const output = Common.UIString.serializeUIString('foo');
     assert.equal(output, JSON.stringify({
       messageParts: ['foo'],
       values: [],
     }));
+  });
+
+  it('serializes UI strings and includes any values', () => {
+    const output = Common.UIString.serializeUIString('a string', ['value1', 'value2']);
+    assert.equal(output, JSON.stringify({
+      messageParts: ['a string'],
+      values: ['value1', 'value2'],
+    }));
+  });
+
+  it('deserializes UI strings', () => {
+    const inputString = 'a string';
+    const serializedString = Common.UIString.serializeUIString(inputString);
+    const deserializedString = Common.UIString.deserializeUIString(serializedString);
+    assert.deepEqual(deserializedString, {
+      messageParts: [inputString],
+      values: [],
+    });
+  });
+
+  it('returns an empty object if no string is given to deserialize', () => {
+    const output = Common.UIString.deserializeUIString();
+    assert.deepEqual(output, {});
   });
 });
