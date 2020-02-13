1132a1f2085890e85c7b5cba0af3567600fc04e0
Jan Scheffler <janscheffler@chromium.org>
2020-02-13 12:10:45 +0100

    Add affected resources to issues panel
    Screenshot: https://imgur.com/a/eV9V0t4
    Change-Id: I13fe4a02f0dc6af95e37bc8530019afced8449c0

--- front_end/issues/IssuesPane.js
+++ front_end/issues/IssuesPane.js
@@ -2,18 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {Events} from '../sdk/Issue.js';
+
 class IssueView extends UI.Widget {
   constructor(parent, issue) {
     super(false);
     this._parent = parent;
     this._issue = issue;
     this._details = issueDetails[issue.code];
+    this._affectedCookies = null;
+    this._affectedCookiesLabel = null;
+    this._affectedResources = null;
 
     this.contentElement.classList.add('issue');
-    this.contentElement.classList.add('collapsed');
 
     this.appendHeader();
     this.appendBody();
+
+    this._issue.addEventListener(Events.InstanceAdded, this._handleInstanceAdded.bind(this));
+    this._issue.addEventListener(Events.CookieAdded, this._handleCookieAdded.bind(this));
   }
 
   appendHeader() {
@@ -55,9 +62,85 @@ class IssueView extends UI.Widget {
     const linkIcon = UI.Icon.create('largeicon-link', 'link-icon');
     link.prepend(linkIcon);
 
+    this.appendAffectedResources(body);
+
     const bodyWrapper = createElementWithClass('div', 'body-wrapper');
     bodyWrapper.appendChild(body);
     this.contentElement.appendChild(bodyWrapper);
+  }
+
+  appendAffectedResources(body) {
+    const wrapper = createElementWithClass('div', 'affected-resources');
+    const label = createElementWithClass('div', 'affected-resources-label');
+    label.innerText = 'Affected Resources';
+    wrapper.appendChild(label);
+    this._affectedResources = wrapper;
+
+    this.appendAffectedCookies();
+
+    body.appendChild(wrapper);
+  }
+
+
+  _updateAffectedCookiesCounter() {
+    this._affectedCookiesLabel.textContent = ls`${this._issue.numberOfCookies()} cookies`;
+  }
+
+  appendAffectedCookies() {
+    const wrapper = createElementWithClass('div', 'affected-cookies');
+    const label = createElementWithClass('div', 'affected-cookies-label');
+    label.addEventListener('click', () => {
+      wrapper.classList.toggle('expanded');
+    });
+    wrapper.appendChild(label);
+    this._affectedCookiesLabel = label;
+    this._updateAffectedCookiesCounter();
+
+    const body2 = createElementWithClass('div', 'affected-cookies-wrapper');
+    const body = createElementWithClass('table', 'affected-cookies-cookies');
+    const header = createElementWithClass('tr');
+
+    const name = createElementWithClass('td', 'affected-cookies-header');
+    name.innerText = 'Name';
+    header.appendChild(name);
+
+    const info = createElementWithClass('td', 'affected-cookies-header affected-cookies-header-info');
+    // Prepend a space to align them better with cookie domains starting with a "."
+    info.innerText = '\u2009Context';
+    header.appendChild(info);
+
+    body.appendChild(header);
+    body2.appendChild(body);
+    wrapper.appendChild(body2);
+    this._affectedCookies = body;
+
+    for (const instance of this._issue.instances()) {
+      this.appendAffectedCookie(instance.cookie);
+    }
+
+    this._affectedResources.appendChild(wrapper);
+  }
+
+  appendAffectedCookie(cookie) {
+    this._updateAffectedCookiesCounter();
+    const element = createElementWithClass('tr', 'affected-cookies-cookie');
+    const name = createElementWithClass('td', '');
+    name.appendChild(Components.Linkifier.linkifyRevealable(new SDK.Cookie(cookie.name, cookie.value), cookie.name));
+    const info = createElementWithClass('td', 'affected-cookies-cookie-info');
+
+    // Prepend a space for all domains not starting with a "." to align them better.
+    info.innerText = (cookie.domain[0] !== '.' ? '\u2008' : '') + cookie.domain + cookie.path;
+
+    element.appendChild(name);
+    element.appendChild(info);
+    this._affectedCookies.appendChild(element);
+  }
+
+  _handleCookieAdded(event) {
+    this.appendAffectedCookie(event.data);
+  }
+
+  _handleInstanceAdded(event) {
   }
 
   _handleSelect() {
