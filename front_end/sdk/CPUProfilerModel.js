8e79ee937b522d02d2069964fb7f8d9b6a94d7d1
Sigurd Schneider <sigurds@chromium.org>
2020-02-13 09:43:11 +0100

    [coverage] Enable processing of triggered coverage events
    Bug: chromium:1042927, chromium:1022031
    Change-Id: I41ecdd42178ea66d7e207e6959e33cb0c3c82634

--- front_end/sdk/CPUProfilerModel.js
+++ front_end/sdk/CPUProfilerModel.js
@@ -148,7 +148,8 @@ export class CPUProfilerModel extends SDKModel {
   startPreciseCoverage(jsCoveragePerBlock, preciseCoverageDeltaUpdateCallback) {
     const callCount = false;
     this._preciseCoverageDeltaUpdateCallback = preciseCoverageDeltaUpdateCallback;
-    return this._profilerAgent.startPreciseCoverage(callCount, jsCoveragePerBlock);
+    const allowTriggeredUpdated = true;
+    return this._profilerAgent.startPreciseCoverage(callCount, jsCoveragePerBlock, allowTriggeredUpdated);
   }
 
   /**
