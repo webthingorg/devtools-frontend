db40f16f6ffb0b4f77f5cc081e5a2482dfd3df00
Jack Franklin <jacktfranklin@chromium.org>
2020-02-26 17:19:41 +0100

    Typescriptify Common/Progress.js
    Bug: 1011811
    Change-Id: I9a7198b8bf017cf51bef92afa07ec8e494ae17ef

--- front_end/common/Progress.js
+++ front_end/common/Progress.js
@@ -77,6 +77,7 @@ export class CompositeProgress {
    */
   constructor(parent) {
     this._parent = parent;
+    /** @type {!Array.<!SubProgress>} */
     this._children = [];
     this._childrenDone = 0;
     this._parent.setTotalWork(1);
@@ -128,6 +129,9 @@ export class SubProgress {
     this._composite = composite;
     this._weight = weight || 1;
     this._worked = 0;
+
+    /** @type {number} */
+    this._totalWork = 0;
   }
 
   /**
@@ -156,7 +160,7 @@ export class SubProgress {
 
   /**
    * @override
-   * @param {number} totalWork
+   * @param {!number} totalWork
    */
   setTotalWork(totalWork) {
     this._totalWork = totalWork;
@@ -165,7 +169,7 @@ export class SubProgress {
 
   /**
    * @override
-   * @param {number} worked
+   * @param {!number} worked
    * @param {string=} title
    */
   setWorked(worked, title) {
