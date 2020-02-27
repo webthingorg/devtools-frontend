aff7dc314d23e4f937fcda8b614fcbcba9ddf1ae
Al Muthanna Athamina <almuthanna@chromium.org>
2020-02-27 10:28:26 +0100

    Refactor Console E2E test to use PO pattern
    This CL attempts to modify the way we access
    selectors for the Console E2E tests in order
    to reduce duplication.
    
    Change-Id: I15c007240143313f2edb8b9c21836492ed60aa7d

---
+++ test/e2e/page-objects/HeaderPageObject.ts
@@ -0,0 +1,28 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as puppeteer from 'puppeteer';
+import {click} from '../../shared/helper.js';
+
+export class HeaderPageObject {
+  frontend: puppeteer.Page;
+
+  constructor(frontend: puppeteer.Page) {
+    this.frontend = frontend;
+  }
+
+  get elementsTab() {return '#tab-elements';}
+  get consoleTab() {return '#tab-console';}
+  get sourcesTab() {return '#tab-sources';}
+  get networkTab() {return '#tab-network';}
+  get performanceTab() {return '#tab-timeline';}
+  get memoryTab() {return '#tab-heap_profiler';}
+  get applicationTab() {return '#tab-resources';}
+  get securityTab() {return '#tab-security';}
+  get auditsTab() {return '#tab-audits';}
+
+  async clickConsoleTab() {
+    await click(this.consoleTab);
+  }
+}
