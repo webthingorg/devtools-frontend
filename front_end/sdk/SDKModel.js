c00f13700d06d3ea43a4ca6a93dd7d48c6ac9294
Simon Zünd <szuend@chromium.org>
2020-03-02 11:27:30 +0100

    [utilities] Use Set instead of Array for SDKModel._targets
    This CL is part of the cleanup effort to get rid of Array#remove and
    use Sets instead of Arrays where appropriate.
    
    Drive-by: Use existing {models} helper to find a specific non-null
    model class for a target.
    
    R=mathias@chromium.org
    
    Bug: chromium:1050549
    Change-Id: I4fccadcc56e79edf7182765903a9769ecbe48e6d

--- front_end/sdk/SDKModel.js
+++ front_end/sdk/SDKModel.js
@@ -349,8 +349,8 @@ export const Type = {
 export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
   constructor() {
     super();
-    /** @type {!Array.<!Target>} */
-    this._targets = [];
+    /** @type {!Set.<!Target>} */
+    this._targets = new Set();
     /** @type {!Array.<!Observer>} */
     this._observers = [];
     /** @type {!Platform.Multimap<symbol, !{modelClass: !Function, thisObject: (!Object|undefined), listener: function(!Common.EventTarget.EventTargetEvent)}>} */
@@ -370,7 +370,7 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
     }
     this._isSuspended = true;
     this.dispatchEventToListeners(Events.SuspendStateChanged);
-    return Promise.all(this._targets.map(target => target.suspend(reason)));
+    return Promise.all(Array.from(this._targets.values(), target => target.suspend(reason)));
   }
 
   /**
@@ -382,7 +382,7 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
     }
     this._isSuspended = false;
     this.dispatchEventToListeners(Events.SuspendStateChanged);
-    return Promise.all(this._targets.map(target => target.resume()));
+    return Promise.all(Array.from(this._targets.values(), target => target.resume()));
   }
 
   /**
@@ -399,8 +399,8 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
    */
   models(modelClass) {
     const result = [];
-    for (let i = 0; i < this._targets.length; ++i) {
-      const model = this._targets[i].model(modelClass);
+    for (const target of this._targets) {
+      const model = target.model(modelClass);
       if (model) {
         result.push(model);
       }
@@ -412,7 +412,8 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
    * @return {string}
    */
   inspectedURL() {
-    return this._targets[0] ? this._targets[0].inspectedURL() : '';
+    const mainTarget = this.mainTarget();
+    return mainTarget ? mainTarget.inspectedURL() : '';
   }
 
   /**
@@ -466,11 +467,8 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
    * @param {!Object=} thisObject
    */
   addModelListener(modelClass, eventType, listener, thisObject) {
-    for (let i = 0; i < this._targets.length; ++i) {
-      const model = this._targets[i].model(modelClass);
-      if (model) {
-        model.addEventListener(eventType, listener, thisObject);
-      }
+    for (const model of this.models(modelClass)) {
+      model.addEventListener(eventType, listener, thisObject);
     }
     this._modelListeners.set(eventType, {modelClass: modelClass, thisObject: thisObject, listener: listener});
   }
@@ -486,11 +484,8 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
       return;
     }
 
-    for (let i = 0; i < this._targets.length; ++i) {
-      const model = this._targets[i].model(modelClass);
-      if (model) {
-        model.removeEventListener(eventType, listener, thisObject);
-      }
+    for (const model of this.models(modelClass)) {
+      model.removeEventListener(eventType, listener, thisObject);
     }
 
     for (const info of this._modelListeners.get(eventType)) {
@@ -537,7 +532,7 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
       target.pageAgent().waitForDebugger();
     }
     target.createModels(new Set(this._modelObservers.keysArray()));
-    this._targets.push(target);
+    this._targets.add(target);
 
     const copy = this._observers.slice(0);
     for (const observer of copy) {
@@ -564,11 +559,11 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
    * @param {!Target} target
    */
   removeTarget(target) {
-    if (!this._targets.includes(target)) {
+    if (!this._targets.has(target)) {
       return;
     }
 
-    this._targets.remove(target);
+    this._targets.delete(target);
     for (const modelClass of target.models().keys()) {
       this._modelRemoved(target, modelClass, target.models().get(modelClass));
     }
@@ -592,7 +587,7 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
    * @return {!Array.<!Target>}
    */
   targets() {
-    return this._targets.slice();
+    return [...this._targets];
   }
 
   /**
@@ -601,19 +596,14 @@ export class TargetManager extends Common.ObjectWrapper.ObjectWrapper {
    */
   targetById(id) {
     // TODO(dgozman): add a map id -> target.
-    for (let i = 0; i < this._targets.length; ++i) {
-      if (this._targets[i].id() === id) {
-        return this._targets[i];
-      }
-    }
-    return null;
+    return this.targets().find(target => target.id() === id) || null;
   }
 
   /**
    * @return {?Target}
    */
   mainTarget() {
-    return this._targets[0] || null;
+    return this._targets.size ? this._targets.values().next().value : null;
   }
 }
 
