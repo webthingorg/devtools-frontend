6c16a7608dd4506e0543cb903c5490449f474f7a
Eric Leese <leese@chromium.org>
2020-03-04 15:46:41 +0100

    More tests for scripts shared between workers
    This change fixes a flaky test, adds a substitute for the webtest
    breakpoints-ui-restored-breakpoint.js so that test can be removed
    from blink, and adds testing of breaking in multiple workers.
    
    Bug: chromium:670180
    Change-Id: I3695b4239be1a064e886fa3912843febe2fde9a3

--- test/e2e/sources/script-in-multiple-workers.ts
+++ test/e2e/sources/script-in-multiple-workers.ts
@@ -4,96 +4,230 @@
 
 import {assert} from 'chai';
 import {describe, it} from 'mocha';
+import * as puppeteer from 'puppeteer';
 
-import {getBrowserAndPages, getElementPosition, resetPages, resourcesPath, $} from '../../shared/helper.js';
+import {click, getBrowserAndPages, resetPages, resourcesPath, $, waitFor} from '../../shared/helper.js';
+
+import {doubleClickSourceTreeItem, addBreakpointForLine} from '../helpers/sources-helpers.js';
+
+const WORKER_SELECTORS = createSelectorsForFile('multi-workers.js', 1);
+const WORKER2_SELECTORS = createSelectorsForFile('multi-workers.js', 2);
+const WORKER3_SELECTORS = createSelectorsForFile('multi-workers.js', 3);
+const WORKER4_SELECTORS = createSelectorsForFile('multi-workers.js', 4);
+
+type NestedFileSelector = {
+  rootSelector: string,
+  domainSelector: string,
+  folderSelector: string,
+  fileSelector: string,
+};
+
+function createSelectorsForFile(fileName: string, workerIndex: number): NestedFileSelector {
+  const rootSelector = new Array(workerIndex).fill(`[aria-label="${fileName}, worker"]`).join(' ~ ');
+  const domainSelector = `${rootSelector} + ol > [aria-label="localhost:8090, domain"]`;
+  const folderSelector = `${domainSelector} + ol > [aria-label="test/e2e/resources/sources, nw-folder"]`;
+  const fileSelector = `${folderSelector} + ol > [aria-label="${fileName}, file"]`;
+
+  return {
+    rootSelector,
+    domainSelector,
+    folderSelector,
+    fileSelector,
+  };
+}
+
+async function openNestedWorkerFile(selectors: NestedFileSelector) {
+  await doubleClickSourceTreeItem(selectors.rootSelector);
+  await doubleClickSourceTreeItem(selectors.domainSelector);
+  await doubleClickSourceTreeItem(selectors.folderSelector);
+  await waitFor(selectors.fileSelector);
+  await click(selectors.fileSelector);
+}
+
+async function validateNavigationTree() {
+  const workers = new Array(10).fill('[aria-label="multi-workers.js, worker"]');
+  // Create a selector for 10 workers that are siblings.
+  await waitFor(workers.join(' ~ '));
+}
+
+async function validateSourceTabs(frontend: puppeteer.Page) {
+  // Verifies there is exactly one source open.
+  const sourceTabPane = await frontend.waitForSelector('#sources-panel-sources-view .tabbed-pane');
+  const sourceTabs = (await $('.tabbed-pane-header-tabs', sourceTabPane)).asElement();
+  if (!sourceTabs) {
+    assert.fail('Unable to locate source tabs');
+    return;
+  }
+  const openSources = await sourceTabs.$$eval('.tabbed-pane-header-tab', nodes => nodes.map(n => n.getAttribute('aria-label')));
+  assert.deepEqual(openSources, ['multi-workers.js']);
+}
+
+async function typeIntoConsole(frontend: puppeteer.Page, console: puppeteer.ElementHandle, message: string) {
+  await console.type(message);
+
+  // Wait for autocomplete text to catch up.
+  const line = (await console.$('.CodeMirror-activeline'))!.asElement()!;
+  const autocomplete = (await line.$('.auto-complete-text'))!.asElement()!;
+  await frontend.waitFor(
+      (msg, ln, ac) => ln.textContent === msg && ac.textContent === '',
+      undefined, message, line, autocomplete);
+
+  await console.press('Enter');
+}
+
+async function validateBreakpoints(frontend: puppeteer.Page) {
+  const bps = await frontend.$$eval('.cm-breakpoint .CodeMirror-linenumber', nodes => nodes.map(n => n.textContent));
+  assert.deepEqual(bps, ['6', '10']);
+  const disabled = await frontend.$$eval('.cm-breakpoint-disabled .CodeMirror-linenumber', nodes => nodes.map(n => n.textContent));
+  assert.deepEqual(disabled, ['6']);
+}
+
+async function validateBreakpointsWithoutDisabled(frontend: puppeteer.Page) {
+  // Currently breakpoints do not get copied to workers if they are disabled.
+  // This behavior is enforced by a web test, which this test will replace.
+  // TODO(leese): Once breakpoint copying issue is fixed, remove this function.
+  const bps = await frontend.$$eval('.cm-breakpoint .CodeMirror-linenumber', nodes => nodes.map(n => n.textContent));
+  assert.deepEqual(bps, ['10']);
+  const disabled = await frontend.$$eval('.cm-breakpoint-disabled .CodeMirror-linenumber', nodes => nodes.map(n => n.textContent));
+  assert.deepEqual(disabled, []);
+}
 
 describe('Multi-Workers', async () => {
   beforeEach(async () => {
     await resetPages();
   });
 
-  // Test is flaky: https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket.appspot.com/8887838790062903056/+/steps/E2E_tests/0/stdout
-  it.skip('loads scripts exactly once', async () => {
+  it('loads scripts exactly once on reload', async () => {
     const {target, frontend} = getBrowserAndPages();
 
     // Have the target load the page.
-    await target.goto(`${resourcesPath}/pages/multi-workers.html`);
+    await target.goto(`${resourcesPath}/sources/multi-workers.html`);
 
-    // Locate the button for switching to the sources tab.
-    const sourcesTabButtonLocation = await getElementPosition('#tab-sources');
-    if (!sourcesTabButtonLocation) {
-      assert.fail('Unable to locate sources tab button.');
-    }
+    await click('#tab-sources');
+    await validateNavigationTree();
+    await openNestedWorkerFile(WORKER_SELECTORS);
 
-    // Click on the button and wait for the sources to load. The reason we use this method
-    // rather than elementHandle.click() is because the frontend attaches the behavior to
-    // a 'mousedown' event (not the 'click' event). To avoid attaching the test behavior
-    // to a specific event we instead locate the button in question and ask Puppeteer to
-    // click on it instead.
-    await frontend.mouse.click(sourcesTabButtonLocation.x, sourcesTabButtonLocation.y);
+    // Look at source tabs
+    await validateSourceTabs(frontend);
 
-    const expectedWorkers = new Array(10).fill('multi-workers.js');
+    // Reload page
+    await target.goto(`${resourcesPath}/sources/multi-workers.html`);
+
+    // Check workers again
+    await validateNavigationTree();
+
+    // Look at source tabs again
+    await validateSourceTabs(frontend);
+  });
+
+  // TODO(leese): Enable once chromium:670180 is fixed.
+  it.skip('loads scripts exactly once on break', async () => {
+    const {target, frontend} = getBrowserAndPages();
+
+    // Have the target load the page.
+    await target.goto(`${resourcesPath}/sources/multi-workers.html`);
+
+    await click('#tab-sources');
+
+    await validateNavigationTree();
+
+    // Open console tab
+    await click('#tab-console');
+
     {
-      const pane = await frontend.waitForSelector('.navigator-tabbed-pane div[aria-label="Page panel"] div.vbox');
-      const tree = (await $('.tree-outline', pane)).asElement()!;
-      // Check that all 10 workers have appeared
-      const workers = await tree.$$eval('.navigator-worker-tree-item span.tree-element-title', nodes => nodes.map(n => n.innerHTML));
-      assert.deepEqual(workers, expectedWorkers);
+      // Send message to a worker by evaluating in the console
+      const console = await frontend.waitForSelector('#console-prompt');
+      await typeIntoConsole(frontend, console, 'workers[3].postMessage({});');
 
-      // Navigate down to one of the workers
-      await tree.press('ArrowDown');
-      await tree.press('ArrowDown');
-      await tree.press('ArrowDown');
-      await tree.press('ArrowDown');
-      await tree.press('ArrowDown');
-      // Expand worker
-      await tree.press('ArrowRight');
-      await tree.press('ArrowDown');
-      // Expand domain
-      await tree.press('ArrowRight');
-      await tree.press('ArrowDown');
-      // Expand folder
-      await tree.press('ArrowRight');
-      await tree.press('ArrowDown');
-      // Open script
-      await tree.press('Enter');
+      // Should automatically switch to sources tab.
+
+      // Validate that we are paused
+      const pauseButton = (await waitFor('[aria-label="Pause script execution"][aria-pressed="true"]'))!.asElement();
+      if (!pauseButton) {
+        assert.fail('Could not find pause/resume button');
+        return;
+      }
 
       // Look at source tabs
-      const sourceTabPane = await frontend.waitForSelector('#sources-panel-sources-view .tabbed-pane');
-      const sourceTabs = (await $('.tabbed-pane-header-tabs', sourceTabPane)).asElement();
-      if (!sourceTabs) {
-        assert.fail('Unable to located source tabs');
-        return;
-      }
+      await validateSourceTabs(frontend);
 
-      const openSources = await sourceTabs.$$eval('.tabbed-pane-header-tab', nodes => nodes.map(n => n.getAttribute('aria-label')));
-      assert.deepEqual(openSources, ['multi-workers.js']);
+      // Continue
+      await pauseButton.click();
+      // Verify that we have continued.
+      await waitFor('[aria-label="Pause script execution"][aria-pressed="false"]');
     }
-    // Reload page
-    await target.goto(`${resourcesPath}/pages/multi-workers.html`);
 
-    {
-      // Check workers again
-      const pane = await frontend.waitForSelector('.navigator-tabbed-pane div[aria-label="Page panel"] div.vbox');
-      const tree = (await $('.tree-outline', pane)).asElement();
-      if (!tree) {
-        assert.fail('Unable to locate tree');
-        return;
-      }
+    await click('#tab-console');
+    const console = await frontend.waitForSelector('#console-prompt');
 
-      const workers = await tree.$$eval('.navigator-worker-tree-item span.tree-element-title', nodes => nodes.map(n => n.innerHTML));
-      assert.deepEqual(workers, expectedWorkers);
-
-      // Look at source tabs again
-      const sourceTabPane = await frontend.waitForSelector('#sources-panel-sources-view .tabbed-pane');
-      const sourceTabs = (await $('.tabbed-pane-header-tabs', sourceTabPane)).asElement();
-      if (!sourceTabs) {
-        assert.fail('Unable to locate source tabs');
-        return;
-      }
-
-      const openSources = await sourceTabs.$$eval('.tabbed-pane-header-tab', nodes => nodes.map(n => n.getAttribute('aria-label')));
-      assert.deepEqual(openSources, ['multi-workers.js']);
+    // Switch back to top execution context
+    const dropdown = (await $('[aria-label^="JavaScript context:"]')).asElement();
+    if (!dropdown) {
+      assert.fail('Unable to locate dropdown');
+      return;
     }
+    await dropdown.press('Space');
+    await frontend.keyboard.press('Home');
+    await frontend.keyboard.press('Space');
+
+    // Send message to a different worker
+    await console.click();
+    await typeIntoConsole(frontend, console, 'workers[7].postMessage({});');
+
+    // Validate that we are paused
+    await waitFor('[aria-label="Pause script execution"][aria-pressed="true"]');
+
+    // Look at source tabs
+    await validateSourceTabs(frontend);
+  });
+
+  it('copies breakpoints between workers', async () => {
+    const {target, frontend} = getBrowserAndPages();
+
+    // Have the target load the page.
+    await target.goto(`${resourcesPath}/sources/multi-workers.html`);
+
+    await click('#tab-sources');
+    // Wait for all workers to load
+    await validateNavigationTree();
+    // Open file from second worker
+    await openNestedWorkerFile(WORKER2_SELECTORS);
+    // Set two breakpoints
+    await addBreakpointForLine(frontend, 6);
+    // Disable first breakpoint
+    const bpEntry = await waitFor('.breakpoint-entry');
+    const bpCheckbox = await waitFor('input', bpEntry);
+    if (!bpCheckbox) {
+      assert.fail('Could not find checkbox to disable breakpoint');
+      return;
+    }
+    await bpCheckbox.evaluate(n => (n as HTMLElement).click());
+    await frontend.waitFor('.cm-breakpoint-disabled');
+    // Add another breakpoint
+    await addBreakpointForLine(frontend, 10);
+
+    // Check breakpoints
+    await validateBreakpoints(frontend);
+
+    // Close tab
+    await click('[aria-label="Close multi-workers.js"]');
+
+    // Open different worker
+    await openNestedWorkerFile(WORKER3_SELECTORS);
+
+    // Check breakpoints
+    await validateBreakpointsWithoutDisabled(frontend);
+
+    // Close tab
+    await click('[aria-label="Close multi-workers.js"]');
+
+    // Reload
+    await target.goto(`${resourcesPath}/sources/multi-workers.html`);
+
+    // Open different worker
+    await openNestedWorkerFile(WORKER4_SELECTORS);
+
+    // Check breakpoints
+    await validateBreakpointsWithoutDisabled(frontend);
   });
 });
