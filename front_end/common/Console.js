3e9f1124e06d2dc0204efa56176d8781caf0a3b5
Paul Lewis <aerotwist@chromium.org>
2020-03-04 12:40:02 +0000

    [global] Remaps self.Common.console
    This CL changes references to self.Common.console (the global instance
    of Common.Console) over to Common.Console.Console.instance(). To keep
    both TypeScript and Closure happy we must make a method on the Console
    class itself, since it only allows private constructors to be accessed
    by static methods on the class.
    
    Bug: 1058320
    Change-Id: Icd4ac8aea76383588a5664a96643179d91812698

--- front_end/common/Console.js
+++ front_end/common/Console.js
@@ -6,13 +6,31 @@ import {ObjectWrapper} from './Object.js';
 import {reveal} from './Revealer.js';
 
 /**
+ * @type {!Console}
+ */
+let consoleInstance;
+
+/**
  * @unrestricted
  */
 export class Console extends ObjectWrapper {
+  /**
+   * Instantiable via the instance() factory below.
+   *
+   * @private
+   */
   constructor() {
     super();
     /** @type {!Array.<!Message>} */
     this._messages = [];
+  }
+
+  static instance({forceNew = false} = {}) {
+    if (!consoleInstance || forceNew) {
+      consoleInstance = new Console();
+    }
+
+    return consoleInstance;
   }
 
   /**
