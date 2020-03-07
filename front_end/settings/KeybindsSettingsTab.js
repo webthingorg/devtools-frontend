0808c879e78a21bfe0cac91012ef207f9b77b8bc
Jack Lynch <jalyn@microsoft.com>
2020-03-06 19:44:29 -0800

    Add experimental shortcuts settings tab
    Bug: 174309
    Change-Id: I1301cdf9337f0282b3a28d33dd5a4129954763b1

---
+++ front_end/settings/KeybindsSettingsTab.js
@@ -0,0 +1,112 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {Descriptor, KeyboardShortcut, Keys} from '../ui/KeyboardShortcut.js';  // eslint-disable-line no-unused-vars
+import {Delegate, Editor, ListWidget, ValidatorResult} from '../ui/ListWidget.js';  // eslint-disable-line no-unused-vars
+import {createControlForSetting} from '../ui/SettingsUI.js';
+import {VBox} from '../ui/Widget.js';
+
+/**
+ * @implements {Delegate}
+ */
+export class KeybindsSettingsTab extends VBox {
+  constructor() {
+    super(true);
+    this.registerRequiredCSS('settings/keybindsSettingsTab.css');
+    this._actions = new Map();
+    for (const extension of self.runtime.extensions('action')) {
+      const descriptor = extension.descriptor();
+      this._actions.set(descriptor.actionId, descriptor);
+    }
+
+    const header = this.contentElement.createChild('header');
+    header.createChild('h1').textContent = ls`Custom keyboard shortcuts`;
+    const keybindsSetSetting = self.Common.settings.moduleSetting('activeKeybindSet');
+    keybindsSetSetting.addChangeListener(this.update, this);
+    const keybindsSetSelect = createControlForSetting(keybindsSetSetting);
+    keybindsSetSelect.classList.add('keybinds-set-select');
+    keybindsSetSelect.insertBefore(createTextNode(ls`Import shortcuts from`), keybindsSetSelect.firstElementChild);
+    this.contentElement.appendChild(keybindsSetSelect);
+
+    const listHeader = this.contentElement.createChild('div', 'keybinds-list-item keybinds-header');
+    listHeader.createChild('div', 'keybinds-list-text').textContent = ls`Action`;
+    listHeader.createChild('div', 'keybinds-list-text').textContent = ls`Keyboard input`;
+    this._list = new ListWidget(this);
+    this._list.registerRequiredCSS('settings/keybindsSettingsTab.css');
+    this._list.show(this.contentElement);
+    this.update();
+  }
+
+  /**
+   * @override
+   * @param {!KeyboardShortcut} item
+   * @return {!Element}
+   */
+  renderItem(item) {
+    const itemElement = createElementWithClass('div', 'keybinds-list-item');
+
+    if (item.isCategory) {
+      itemElement.classList.add('keybinds-category-header');
+      itemElement.textContent = item.name;
+    } else {
+      const descriptor = this._actions.get(item.name);
+      const options = descriptor.options;
+      let title;
+      if (options) {
+        title = options.map(option => option.title).join('/');
+      } else {
+        title = descriptor.title;
+      }
+      itemElement.createChild('div', 'keybinds-list-text').textContent = title;
+      const keysElement = itemElement.createChild('div', 'keybinds-list-text');
+      item.shortcuts.forEach(
+          shortcut => keysElement.createChild('span', 'keybinds-key').textContent = shortcut.descriptor.name);
+    }
+
+    return itemElement;
+  }
+
+  /**
+   * @override
+   * @param {!KeyboardShortcut} item
+   * @param {number} index
+   */
+  removeItemRequested(item, index) {
+  }
+  /**
+   * None of the items are editable, so this method will never be called
+   * @override
+   * @param {!KeyboardShortcut} item
+   * @return {!Editor<!KeyboardShortcut>}
+   */
+  beginEdit(item) {
+    return new Editor();
+  }
+
+  /**
+   * @override
+   * @param {!KeyboardShortcut} item
+   * @param {!Editor<!KeyboardShortcut>} editor
+   * @param {boolean} isNew
+   */
+  commitEdit(item, editor, isNew) {
+  }
+
+  update() {
+    this._list.clear();
+    let currentCategory;
+    for (const action of [...this._actions.keys()].sort()) {
+      const category = action.split('.')[0];
+      if (category !== currentCategory) {
+        this._list.appendItem(
+            {name: `${category[0].toUpperCase()}${category.substr(1).replace(/[-_]/g, ' ')}`, isCategory: true}, false);
+      }
+      this._list.appendItem({name: action, shortcuts: self.UI.shortcutRegistry.shortcutsForAction(action)}, false);
+      currentCategory = category;
+    }
+  }
+}
+
+/** @typedef {{name: string, shortcuts: (!Array<KeyboardShortcut>|undefined), isCategory: (boolean|undefined)}} */
+let KeybindItem;  // eslint-disable-line no-unused-vars
