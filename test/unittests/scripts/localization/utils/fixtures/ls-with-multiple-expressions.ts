dbf15692977461e254256d50d4496bd8a2322d5d
Jack Franklin <jacktfranklin@chromium.org>
2020-03-09 15:02:42 +0000

    Extend GRDP tooling with TypeScript support
    This CL introduces support for parsing and pulling translations out of
    `*.ts` files.
    
    It introduces code to parse a TypeScript AST to look for `ls` calls and
    pass them back into the existing GRDP tooling.
    
    As a purposeful limitation, it _only looks for `ls` calls_.
    
    Change-Id: I3f91793165e11f0382d6266f4e2c2443701b299a

---
+++ test/unittests/scripts/localization/utils/fixtures/ls-with-multiple-expressions.ts
@@ -0,0 +1,6 @@
+const myFunc = () => {
+  const x = 1;
+  const y = 2;
+  const z = 3;
+  return ls`blah blah ${x} and ${y}, ${z}`;
+};
---
+++ test/unittests/scripts/localization/utils/fixtures/no-ls.ts
@@ -0,0 +1,3 @@
+const myFunc = () => {
+  return 'blah blah';
+};
