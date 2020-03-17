ae39bef070ada378e76f8d2feed91a55c604e959
Eric Leese <leese@chromium.org>
2020-03-17 18:13:04 +0100

    Deduplicate tabs with same URL
    Opening the same file in multiple workers results in only one copy
    open. Further deduplication work in the BreakpointManager results
    in disabled breakpoints being visible in all workers.
    
    Bug: chromium:670180
    Change-Id: I57c7cf2aab46dcc2dc9d6622819df55bdd75d0a2

--- front_end/sources/TabbedEditorContainer.js
+++ front_end/sources/TabbedEditorContainer.js
@@ -91,7 +91,7 @@ export class TabbedEditorContainer extends Common.ObjectWrapper.ObjectWrapper {
 
     this._previouslyViewedFilesSetting = setting;
     this._history = History.fromObject(this._previouslyViewedFilesSetting.get());
-    this._historyUriToUISourceCode = new Map();
+    this._uriToUISourceCode = new Map();
   }
 
   /**
@@ -188,7 +188,7 @@ export class TabbedEditorContainer extends Common.ObjectWrapper.ObjectWrapper {
    * @param {!Workspace.UISourceCode.UISourceCode} uiSourceCode
    */
   showFile(uiSourceCode) {
-    this._innerShowFile(uiSourceCode, true);
+    this._innerShowFile(_canonicalUISourceCode(uiSourceCode), true);
   }
 
   /**
@@ -213,7 +213,7 @@ export class TabbedEditorContainer extends Common.ObjectWrapper.ObjectWrapper {
     const result = [];
     const uris = this._history._urls();
     for (const uri of uris) {
-      const uiSourceCode = this._historyUriToUISourceCode.get(uri);
+      const uiSourceCode = this._uriToUISourceCode.get(uri);
       if (uiSourceCode) {
         result.push(uiSourceCode);
       }
@@ -379,8 +379,24 @@ export class TabbedEditorContainer extends Common.ObjectWrapper.ObjectWrapper {
 
   /**
    * @param {!Workspace.UISourceCode.UISourceCode} uiSourceCode
+   * @return {!Workspace.UISourceCode.UISourceCode}
+   */
+  _canonicalUISourceCode(uiSourceCode) {
+    // Check if we have already a UISourceCode for this url
+    if (this._uriToUISourceCode.has(uiSourceCode.url())) {
+      // Ignore incoming uiSourceCode, we already have this file.
+      return this._uriToUISourceCode.get(uiSourceCode.url());
+    } else {
+      this._uriToUISourceCode.set(uiSourceCode.url(), uiSourceCode);
+      return uiSourceCode;
+    }
+  }
+
+  /**
+   * @param {!Workspace.UISourceCode.UISourceCode} uiSourceCode
    */
   addUISourceCode(uiSourceCode) {
+    uiSourceCode = _canonicalUISourceCode(uiSourceCode);
     const binding = self.Persistence.persistence.binding(uiSourceCode);
     uiSourceCode = binding ? binding.fileSystem : uiSourceCode;
     if (this._currentFile === uiSourceCode) {
@@ -392,12 +408,6 @@ export class TabbedEditorContainer extends Common.ObjectWrapper.ObjectWrapper {
     if (index === -1) {
       return;
     }
-
-    // Check if we have already opened a tab for this uri....
-    if (this._historyUriToUISourceCode.has(uiSourceCode.url())) {
-      return;
-    }
-    this._historyUriToUISourceCode.set(uiSourceCode.url(), uiSourceCode);
 
     if (!this._tabIds.has(uiSourceCode)) {
       this._appendFileTab(uiSourceCode, false);
@@ -437,8 +447,8 @@ export class TabbedEditorContainer extends Common.ObjectWrapper.ObjectWrapper {
       if (tabId) {
         tabIds.push(tabId);
       }
-      if (this._historyUriToUISourceCode.get(uiSourceCode.url()) === uiSourceCode) {
-        this._historyUriToUISourceCode.delete(uiSourceCode.url());
+      if (this._uriToUISourceCode.get(uiSourceCode.url()) === uiSourceCode) {
+        this._uriToUISourceCode.delete(uiSourceCode.url());
       }
     }
     this._tabbedPane.closeTabs(tabIds);
