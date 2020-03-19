91323a507b4afa7c8399b7f7de9bed14f0459ff4
Jack Lynch <jalyn@microsoft.com>
2020-03-18 17:23:56 -0700

    Add experimental shortcuts settings tab
    This CL adds a new "Custom Keyboard Shortcuts" settings tab hidden
    behind an experiment. Currently it just displays existing shortcuts, but
    future CLs will add the ability for users to choose from preset sets of
    shortcuts from other editors (e.g. VS Code) and set custom shortcuts for
    individual actions.
    
    Custom keyboard shortcuts design doc: https://docs.google.com/document/d/1oOPSWPxCHvMoBZ0Fw9jwFZt6gP4lrsrsl8DEAp-Hy7o/edit#heading=h.2xpjzz3fl1ju
    
    Screenshot: https://gyazo.com/c5c915f351932ed8a1233770b84eb0ef
    
    Bug: 174309
    Change-Id: I1301cdf9337f0282b3a28d33dd5a4129954763b1

--- front_end/settings/SettingsScreen.js
+++ front_end/settings/SettingsScreen.js
@@ -59,9 +59,11 @@ export class SettingsScreen extends UI.Widget.VBox {
     tabbedPane.leftToolbar().appendToolbarItem(new UI.Toolbar.ToolbarItem(settingsLabelElement));
     tabbedPane.setShrinkableTabs(false);
     tabbedPane.makeVerticalTabLayout();
-    const shortcutsView = new UI.View.SimpleView(ls`Shortcuts`);
-    self.UI.shortcutsScreen.createShortcutsTabView().show(shortcutsView.element);
-    this._tabbedLocation.appendView(shortcutsView);
+    if (!Root.Runtime.experiments.isEnabled('customKeyboardShortcuts')) {
+      const shortcutsView = new UI.View.SimpleView(ls`Shortcuts`);
+      self.UI.shortcutsScreen.createShortcutsTabView().show(shortcutsView.element);
+      this._tabbedLocation.appendView(shortcutsView);
+    }
     tabbedPane.show(this.contentElement);
   }
 
@@ -319,7 +321,11 @@ export class ActionDelegate {
             UI.UIUtils.addReferrerToURL('https://developers.google.com/web/tools/chrome-devtools/'));
         return true;
       case 'settings.shortcuts':
-        SettingsScreen._showSettingsScreen({name: ls`Shortcuts`, focusTabHeader: true});
+        if (Root.Runtime.experiments.isEnabled('customKeyboardShortcuts')) {
+          SettingsScreen._showSettingsScreen({name: 'keybinds', focusTabHeader: true});
+        } else {
+          SettingsScreen._showSettingsScreen({name: ls`Shortcuts`, focusTabHeader: true});
+        }
         return true;
     }
     return false;
