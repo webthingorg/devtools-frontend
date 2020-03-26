2337c3d5f6f765c16de5de06d57a29cb0944eeae
Tim van der Lippe <tvanderlippe@chromium.org>
2020-03-26 14:29:47 +0000

    Remove unused global references in workspace-legacy.js
    These are not used in our layout tests or module.json files
    
    R=aerotwist@chromium.org
    
    Bug: 1064958
    Change-Id: Ia80d231758e202de92daa2bf53ae9c0e5476f545

--- front_end/workspace/workspace-legacy.js
+++ front_end/workspace/workspace-legacy.js
@@ -8,53 +8,17 @@ self.Workspace = self.Workspace || {};
 Workspace = Workspace || {};
 
 /** @constructor */
-Workspace.FileManager = WorkspaceModule.FileManager.FileManager;
-
-/** @enum {symbol} */
-Workspace.FileManager.Events = WorkspaceModule.FileManager.Events;
-
-/** @constructor */
 Workspace.UISourceCode = WorkspaceModule.UISourceCode.UISourceCode;
-
-/** @enum {symbol} */
-Workspace.UISourceCode.Events = WorkspaceModule.UISourceCode.Events;
-
-/** @constructor */
-Workspace.UISourceCode.Message = WorkspaceModule.UISourceCode.Message;
-
-/** @constructor */
-Workspace.UISourceCode.LineMarker = WorkspaceModule.UISourceCode.LineMarker;
 
 /** @constructor */
 Workspace.UILocation = WorkspaceModule.UISourceCode.UILocation;
 
-/** @constructor */
-Workspace.UISourceCodeMetadata = WorkspaceModule.UISourceCode.UISourceCodeMetadata;
-
-/** @constructor */
-Workspace.Workspace = WorkspaceModule.Workspace.WorkspaceImpl;
-
-/** @enum {symbol} */
-Workspace.Workspace.Events = WorkspaceModule.Workspace.Events;
-
-/** @interface */
-Workspace.ProjectSearchConfig = WorkspaceModule.Workspace.ProjectSearchConfig;
-
-/** @interface */
-Workspace.Project = WorkspaceModule.Workspace.Project;
-
-/** @enum {string} */
-Workspace.projectTypes = WorkspaceModule.Workspace.projectTypes;
-
-/** @constructor */
-Workspace.ProjectStore = WorkspaceModule.Workspace.ProjectStore;
-
 /**
- * @type {?Workspace.FileManager}
+ * @type {?WorkspaceModule.FileManager.FileManager}
  */
 self.Workspace.fileManager;
 
 /**
- * @type {!Workspace.Workspace}
+ * @type {!WorkspaceModule.Workspace.WorkspaceImpl}
  */
 self.Workspace.workspace;
