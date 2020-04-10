abbb1b5edfe90a7215cda8db19481a8a35ec518e
vidorteg <vidorteg@microsoft.com>
2020-04-10 14:00:08 -0700

    Localization: Example CL for migrating a module from v1 to v2 [Documentation WIP]
    Hi everyone,
    
    Our plan for implementing Localization V2 considers 3 phases:
    
    Infrastructure, Migration & Cleaning
    
    This CL is part of the Migration phase: "Migrate Settings module",
    the intention is to get early feedback on the v2 localization API
    usage and design.
    
    Prerequisites: Infrastructure phase is complete and checked in
    
    This PR consist in it considers 4 main changes:
    - Changes to *.js files:
          * Replaces localization v1 API for v2 API calls.
          * Creates UIStrings resource object.
    - Remove resources from grdp files (they are inside the *.js files
    - en-XL.json: Sample file with localized resources. Any language file
        should be provided in this format
    - moduleUIStrings.js: module.json files can contain UI resources, this
        file contains the UIStrings structure for those resources.
    
    Phase 1 Sample CLs:
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2090459
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2101283
    
    Change-Id: Ie82b13b36c2036bf59f24a8ceb05bfdefe016ec6

---
+++ front_end/settings/moduleUIStrings.js
@@ -0,0 +1,35 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export const UIStrings = {
+  /**
+  *@description Name of the Settings view
+  */
+  IDS_DEVTOOLS_f4f70727dc34561dfde1a3c529b6205c: 'Settings',
+  /**
+  *@description Text for the documentation of something
+  */
+  IDS_DEVTOOLS_5b6cf869265c13af8566f192b4ab3d2a: 'Documentation',
+  /**
+  *@description Text for keyboard shortcuts
+  */
+  IDS_DEVTOOLS_29e9d8e295b1d5b3002d2d3d5baa4c37: 'Shortcuts',
+  /**
+  *@description Text in Settings Screen of the Settings
+  */
+  IDS_DEVTOOLS_d0834fcec6337785ee749c8f5464f6f6: 'Preferences',
+  /**
+  *@description Text in Settings Screen of the Settings
+  */
+  IDS_DEVTOOLS_4829262cecb9828817b33e0f9c907f91: 'Experiments',
+  /**
+  *@description Title of Blackboxing settings
+  */
+  IDS_DEVTOOLS_c9deece3e6de26d07ef6409b96f21fd0: 'Blackboxing',
+  /**
+  *@description Tab header for the custom keyboard shortcuts settings tab
+  */
+  IDS_DEVTOOLS_dd9d37c902c417ff340e28a19e2ff00d: 'Custom Shortcuts',
+};
+Common.i18n.registerUIStrings('settings/moduleUIStrings.js', UIStrings);
