69b4540f8b970527067b3cfa070b17dd50bd6c49
Jan Scheffler <janscheffler@chromium.org>
2020-04-15 10:16:32 +0200

    Show cookies for unreachable frames
    This cl fixes the regression introduced in crrev.com/c/2035930 by
    removing the changes and instead showing cookies for the unreachableUrl
    in Application > Cookies too.
    
    Bug: chromium:1047348, chromium:1057948, chromium:1066415
    Change-Id: Ie25bc4df6f5e432debef1958ef7d27c09a44b183

---
+++ test/e2e/helpers/lighthouse-helpers.ts
@@ -0,0 +1,21 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as puppeteer from 'puppeteer';
+
+import {$, click, resourcesPath, waitFor} from '../../shared/helper.js';
+
+export async function navigateToLighthouseTab(target: puppeteer.Page, testName: string) {
+  await target.goto(`${resourcesPath}/lighthouse/${testName}.html`);
+  await click('#tab-lighthouse');
+  // Make sure the lighthouse start view is shown
+  await waitFor('.lighthouse-start-view');
+}
+
+export async function isGenerateReportButtonDisabled() {
+  const button = await $('.lighthouse-start-view .primary-button');
+  const disabledProperty = await button.getProperty('disabled');
+  const disabled = await disabledProperty.jsonValue();
+  return disabled;
+}
