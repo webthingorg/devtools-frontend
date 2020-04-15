1ece456a59bac95bfbd6799f29fdd0cc79e2e426
Almothana Athamneh <almuthanna@google.com>
2020-04-15 23:45:13 +0200

    [Experimental] - Create a step function
    Create a step function that provides
    a documentation for the part the test
    failed in.
    
    Change-Id: I224474e25c4e8aaa1f7e3df2f2f1e18c8ef220e6

--- test/e2e/application/session-storage.ts
+++ test/e2e/application/session-storage.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assert} from 'chai';
+import {assert, AssertionError} from 'chai';
 import {describe, it} from 'mocha';
 import * as puppeteer from 'puppeteer';
 
@@ -10,6 +10,18 @@ import {$$, click, getBrowserAndPages, resetPages, resourcesPath, waitFor} from 
 
 const SESSION_STORAGE_SELECTOR = '[aria-label="Session Storage"]';
 const DOMAIN_SELECTOR = `${SESSION_STORAGE_SELECTOR} + ol > [aria-label="http://localhost:8090"]`;
+
+async function step(description: string, step: Function) {
+  try {
+    return await step();
+  } catch (error) {
+    if (error instanceof AssertionError) {
+      throw new AssertionError('Unexpected Result in Step: ' + description, error);
+    } else {
+      throw {step: description, error: error};
+    }
+  }
+}
 
 async function navigateToApplicationTab(target: puppeteer.Page, testName: string) {
   await target.goto(`${resourcesPath}/application/${testName}.html`);
@@ -30,35 +42,118 @@ describe('The Application Tab', async () => {
 
   it('shows Session Storage keys and values', async () => {
     const {target} = getBrowserAndPages();
-    await navigateToApplicationTab(target, 'session-storage');
 
-    await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
-    await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+    await step('navigate to session-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'session-storage');
+    });
 
-    // Wait for Storage data-grid to show up
-    await waitFor('.storage-view table');
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+    });
 
-    const dataGridNodes = await $$('.data-grid-data-grid-node');
-    const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
-      return {
-        key: row.querySelector('.key-column')!.textContent,
-        value: row.querySelector('.value-column')!.textContent,
-      };
-    }));
+    await step('check that storage data values are correct', async () => {
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
 
-    assert.deepEqual(dataGridRowValues, [
-      {
-        key: 'firstKey',
-        value: 'firstValue',
-      },
-      {
-        key: 'secondKey',
-        value: '{"field":"complexValue","primitive":2}',
-      },
-      {
-        key: '',
-        value: '',
-      },
-    ]);
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'firstKey',
+          value: 'firstValue',
+        },
+        {
+          key: 'secondKey',
+          value: '{"field":"complexValue","primitive":2}',
+        },
+        {
+          key: '',
+          value: '',
+        },
+      ]);
+    });
+  });
+
+  it('shows Session Storage keys and values (but has an assertion error)', async () => {
+    const {target} = getBrowserAndPages();
+
+    await step('navigate to session-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'session-storage');
+    });
+
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+    });
+
+    await step('check that storage data values are correct', async () => {
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
+
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'fail',
+          value: 'fail',
+        },
+      ]);
+    });
+  });
+
+  it('shows Session Storage keys and values (but has a generic error)', async () => {
+    const {target} = getBrowserAndPages();
+
+    await step('navigate to session-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'session-storage');
+    });
+
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+    });
+
+    await step('check that storage data values are correct', async () => {
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
+
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'firstKey',
+          value: 'firstValue',
+        },
+        {
+          key: 'secondKey',
+          value: '{"field":"complexValue","primitive":2}',
+        },
+        {
+          key: '',
+          value: '',
+        },
+      ]);
+
+      throw new Error('Generic Error');
+    });
   });
 });
