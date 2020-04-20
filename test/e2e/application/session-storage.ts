51bb2cd3176a43e83cb17ccee8f03897626c6c1d
Almothana Athamneh <almuthanna@google.com>
2020-04-20 16:25:54 +0200

    [Experimental] - Create a step function
    Create a step function that provides
    a documentation for the part the test
    failed in.
    
    Change-Id: I224474e25c4e8aaa1f7e3df2f2f1e18c8ef220e6

--- test/e2e/application/session-storage.ts
+++ test/e2e/application/session-storage.ts
@@ -4,10 +4,9 @@
 
 import {assert, AssertionError} from 'chai';
 import {describe, it} from 'mocha';
+import * as puppeteer from 'puppeteer';
 
-import {getBrowserAndPages} from '../../shared/helper.js';
-
-import {doubleClickSourceTreeItem, getDataGridData, navigateToApplicationTab} from '../helpers/application-helpers.js';
+import {$$, click, getBrowserAndPages, resetPages, resourcesPath, waitFor} from '../../shared/helper.js';
 
 const SESSION_STORAGE_SELECTOR = '[aria-label="Session Storage"]';
 const DOMAIN_SELECTOR = `${SESSION_STORAGE_SELECTOR} + ol > [aria-label="http://localhost:8090"]`;
@@ -44,7 +43,23 @@ async function step(description: string, step: Function) {
   }
 }
 
+async function navigateToApplicationTab(target: puppeteer.Page, testName: string) {
+  await target.goto(`${resourcesPath}/application/${testName}.html`);
+  await click('#tab-resources');
+  // Make sure the application navigation list is shown
+  await waitFor('.storage-group-list-item');
+}
+
+async function doubleClickSourceTreeItem(selector: string) {
+  await waitFor(selector);
+  await click(selector, {clickOptions: {clickCount: 2}});
+}
+
 describe('The Application Tab', async () => {
+  beforeEach(async () => {
+    await resetPages();
+  });
+
   it('shows Session Storage keys and values', async () => {
     const {target} = getBrowserAndPages();
 
@@ -58,7 +73,17 @@ describe('The Application Tab', async () => {
     });
 
     await step('check that storage data values are correct', async () => {
-      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
+
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
       assert.deepEqual(dataGridRowValues, [
         {
           key: 'firstKey',
@@ -89,7 +114,17 @@ describe('The Application Tab', async () => {
     });
 
     await step('check that storage data values are correct', async () => {
-      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
+
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
       assert.deepEqual(dataGridRowValues, [
         {
           key: 'fail',
@@ -112,7 +147,17 @@ describe('The Application Tab', async () => {
     });
 
     await step('check that storage data values are correct', async () => {
-      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
+
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
       assert.deepEqual(dataGridRowValues, [
         {
           key: 'firstKey',
@@ -150,7 +195,17 @@ describe('The Application Tab', async () => {
     });
 
     await step('check that storage data values are correct', async () => {
-      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
+
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
       assert.deepEqual(dataGridRowValues, [
         {
           key: 'firstKey',
@@ -181,7 +236,17 @@ describe('The Application Tab', async () => {
     });
 
     await step('check that storage data values are correct', async () => {
-      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      // Wait for Storage data-grid to show up
+      await waitFor('.storage-view table');
+
+      const dataGridNodes = await $$('.data-grid-data-grid-node');
+      const dataGridRowValues = await dataGridNodes.evaluate(nodes => nodes.map((row: Element) => {
+        return {
+          key: row.querySelector('.key-column')!.textContent,
+          value: row.querySelector('.value-column')!.textContent,
+        };
+      }));
+
       assert.deepEqual(dataGridRowValues, [
         {
           key: 'firstKey',
