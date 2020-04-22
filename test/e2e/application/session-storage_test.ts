8d017395838ca6c3980b265b21b0fdd06b85ddfe
Al Muthanna Athamina <almuthanna@chromium.org>
2020-04-22 12:59:07 +0200

    [Experimental] - Create a step function
    Create a step function that provides
    a documentation for the part the test
    failed in.
    
    Change-Id: I224474e25c4e8aaa1f7e3df2f2f1e18c8ef220e6

--- test/e2e/application/session-storage_test.ts
+++ test/e2e/application/session-storage_test.ts
@@ -5,35 +5,137 @@
 import {assert} from 'chai';
 import {describe, it} from 'mocha';
 
-import {getBrowserAndPages} from '../../shared/helper.js';
+import {getBrowserAndPages, step} from '../../shared/helper.js';
 
 import {doubleClickSourceTreeItem, getDataGridData, navigateToApplicationTab} from '../helpers/application-helpers.js';
 
 const SESSION_STORAGE_SELECTOR = '[aria-label="Session Storage"]';
 const DOMAIN_SELECTOR = `${SESSION_STORAGE_SELECTOR} + ol > [aria-label="http://localhost:8090"]`;
 
-describe('The Application Tab', async () => {
+describe.only('The Application Tab', async () => {
   it('shows Session Storage keys and values', async () => {
     const {target} = getBrowserAndPages();
-    await navigateToApplicationTab(target, 'session-storage');
 
-    await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
-    await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+    await step('navigate to session-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'session-storage');
+    });
 
-    const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
-    assert.deepEqual(dataGridRowValues, [
-      {
-        key: 'firstKey',
-        value: 'firstValue',
-      },
-      {
-        key: 'secondKey',
-        value: '{"field":"complexValue","primitive":2}',
-      },
-      {
-        key: '',
-        value: '',
-      },
-    ]);
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+    });
+
+    await step('check that storage data values are correct', async () => {
+      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'firstKey',
+          value: 'firstValue',
+        },
+        {
+          key: 'secondKey',
+          value: '{"field":"complexValue","primitive":2}',
+        },
+        {
+          key: '',
+          value: '',
+        },
+      ]);
+    });
+  });
+
+  it('shows Session Storage keys and values (but has an assertion error)', async () => {
+    const {target} = getBrowserAndPages();
+
+    await step('navigate to session-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'session-storage');
+    });
+
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+      assert.equal(1, 2, 'this will raise an assertion error');
+    });
+
+    await step('check that storage data values are correct', async () => {
+      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'firstKey',
+          value: 'firstValue',
+        },
+        {
+          key: 'secondKey',
+          value: '{"field":"complexValue","primitive":2}',
+        },
+        {
+          key: '',
+          value: '',
+        },
+      ]);
+    });
+  });
+
+  it('shows Session Storage keys and values (but has a generic error)', async () => {
+    const {target} = getBrowserAndPages();
+
+    await step('navigate to session-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'session-storage');
+    });
+
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+      throw new Error('Generic Error');
+    });
+
+    await step('check that storage data values are correct', async () => {
+      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'firstKey',
+          value: 'firstValue',
+        },
+        {
+          key: 'secondKey',
+          value: '{"field":"complexValue","primitive":2}',
+        },
+        {
+          key: '',
+          value: '',
+        },
+      ]);
+    });
+  });
+
+  it('shows Session Storage keys and values (but has a timeout)', async () => {
+    const {target} = getBrowserAndPages();
+
+    await step('navigate to session-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'session-storage');
+    });
+
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SESSION_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem('aaaaa');
+    });
+
+    await step('check that storage data values are correct', async () => {
+      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'firstKey',
+          value: 'firstValue',
+        },
+        {
+          key: 'secondKey',
+          value: '{"field":"complexValue","primitive":2}',
+        },
+        {
+          key: '',
+          value: '',
+        },
+      ]);
+    });
   });
 });
