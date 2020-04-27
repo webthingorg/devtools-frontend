03733e854c4453f94dbae51a6ea60e0972e94b0d
Jack Franklin <jacktfranklin@chromium.org>
2020-04-27 11:30:32 +0100

    Move `UIString` into `Platform`
    Platform is the "lowest" part of DevTools - e.g. it depends on nothing.
    
    We have code we want to move to Platform but cannot because it needs
    `ls` and therefore creates a circular dependency. So this CL moves
    UIString into Platform.
    
    To avoid rewriting a lot of imports we re-export it from `common.js`.
    
    Change-Id: I399bbe8593e043148c4c7e51f598cbd73a89f639

--- front_end/platform/platform.js
+++ front_end/platform/platform.js
@@ -33,6 +33,7 @@ import './utilities.js';
 import * as ArrayUtilities from './array-utilities.js';
 import * as NumberUtilities from './number-utilities.js';
 import * as StringUtilities from './string-utilities.js';
+import * as UIString from './UIString.js';
 
 export {Multimap} from './utilities.js';
-export {ArrayUtilities, NumberUtilities, StringUtilities};
+export {ArrayUtilities, NumberUtilities, StringUtilities, UIString};
