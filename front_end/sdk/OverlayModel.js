501c951cbd33c71d935caeb14057ac91475f71e6
Alex Rudenko <alexrudenko@chromium.org>
2020-04-30 10:07:34 +0200

    Forward the color format preference to the backend
    Another CL[1] adds the backend support for the color format. This
    CL adds forwarding of the current color format preference to the
    setInspectMode call. The 'original' format is not supported by the
    backend and, therefore, the only allowed formats are RGB, HEX and HSL.
    
    Doc: https://goo.gle/devtools-color-format-overlay
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/2172823
    
    DISABLE_THIRD_PARTY_CHECK=protocol update needed for tests in another CL
    
    Bug: 924698
    Change-Id: Ib6b36fcaa0396f6495c939bcef97162caafe9721

--- front_end/sdk/OverlayModel.js
+++ front_end/sdk/OverlayModel.js
@@ -298,6 +298,7 @@ export class OverlayModel extends SDKModel {
    */
   _buildHighlightConfig(mode = 'all', showStyles = false) {
     const showRulers = Common.Settings.Settings.instance().moduleSetting('showMetricsRulers').get();
+    const colorFormat = Common.Settings.Settings.instance().moduleSetting('colorFormat').get();
     const highlightConfig =
         {showInfo: mode === 'all', showRulers: showRulers, showStyles, showExtensionLines: showRulers};
     if (mode === 'all' || mode === 'content') {
@@ -324,6 +325,11 @@ export class OverlayModel extends SDKModel {
 
     if (mode === 'all') {
       highlightConfig.cssGridColor = Common.Color.PageHighlight.CssGrid.toProtocolRGBA();
+    }
+
+    const supportedColorFormats = new Set(['rgb', 'hsl', 'hex']);
+    if (supportedColorFormats.has(colorFormat)) {
+      highlightConfig.colorFormat = colorFormat;
     }
 
     return highlightConfig;
