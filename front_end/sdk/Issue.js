76314abf749a95a0c9e57af438194c349ac83d72
John Emau <John.Emau@microsoft.com>
2020-04-29 21:32:57 -0700

    [issues] Add affected sources to Issue view
    This change adds 'sources' as an affected resource for issues.
    If provided the sources will render as a linkified file name with line
    number:
    
          https://cdn.foo.example/js/my-script.js -> script.js:5
    
    Activating the link will open the sources panel to the line and column
    number of the affected source (note that only the line number is displayed).
    
    The affected sources will be useful for future issues such as [deprecation
    of synchronous XMLHttpRequest on the main thread][1].
    
    [1]: https://imgur.com/MYpi6cY
    
    The below images are using dummy data found only in patchset 9 and
    removed in patchset 10:
    https://crrev.com/c/2168811/8..9/front_end/sdk/Issue.js
    
    Screenshot: https://imgur.com/6Xiqs75
    Gif: https://imgur.com/NY92K1w
    Keyboard accessibility gif: https://imgur.com/fsG2ZhT
    Bug: chromium:1070376
    Change-Id: I37906f24e731cffff5da59a6e0084df1a2413732

--- front_end/sdk/Issue.js
+++ front_end/sdk/Issue.js
@@ -30,6 +30,15 @@ export const IssueKind = {
 export let IssueDescription;  // eslint-disable-line no-unused-vars
 
 /**
+ * @typedef {{
+ *            columnNumber: (number|undefined),
+ *            lineNumber: number,
+ *            url:string
+ *          }}
+ */
+export let AffectedSource;  // eslint-disable-line no-unused-vars
+
+/**
  * @abstract
  */
 export class Issue extends Common.ObjectWrapper.ObjectWrapper {
@@ -74,6 +83,13 @@ export class Issue extends Common.ObjectWrapper.ObjectWrapper {
    * @return {!Iterable<!Protocol.Audits.AffectedRequest>}
    */
   requests() {
+    return [];
+  }
+
+  /**
+   * @returns {!Iterable<!AffectedSource>}
+   */
+  sources() {
     return [];
   }
 
