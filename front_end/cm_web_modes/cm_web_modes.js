f10cf7e1ea777229620dc91b9e6a5141f2cf649a
Tim van der Lippe <tvanderlippe@chromium.org>
2020-05-04 13:20:44 +0100

    Port cm_web_modes files to third_party/codemirror usages
    roll CodeMirror
    
    R=mathias@chromium.org
    
    Bug: 1076825
    Change-Id: I7b5a15bcc9db51b4a38be7db3d2da97aa9deb0f6

--- front_end/cm_web_modes/cm_web_modes.js
+++ front_end/cm_web_modes/cm_web_modes.js
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file is intentionally left empty, as cm_web_modes needs to both operate
-// on cm_headless and cm. We can't know statically which file to import.
-// Instead, if you need to depend on cm_web_modes, you need to import either
-// cm_web_modes_cm.js or cm_web_modes_headless.js
-export default {}
+import '../third_party/codemirror/package/mode/css/css.js';
+import '../third_party/codemirror/package/mode/javascript/javascript.js';
+import '../third_party/codemirror/package/mode/xml/xml.js';
+import '../third_party/codemirror/package/mode/htmlmixed/htmlmixed.js';
+import '../third_party/codemirror/package/mode/htmlembedded/htmlembedded.js';
