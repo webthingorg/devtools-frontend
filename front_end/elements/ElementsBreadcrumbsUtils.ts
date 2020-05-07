7cb5518c662e7e796c9fc3663724a6940fc4f913
Jack Franklin <jacktfranklin@chromium.org>
2020-05-07 11:50:50 +0100

    Rewrite ElementsBreadcrumbs as a TypeScript custom element
    This is the first attempt at building a piece of UI in the devtools with
    TypeScript and custom elements. This CL also explores how we can interface
    between the old code and the new, using events as required.
    
    The _bridge.js file is auto-generated using the component bridges generator.
    
    DISABLE_THIRD_PARTY_CHECK=needed a ts library change
    
    Change-Id: I2ad7d3ab52606b92aa23438f259a253812f55a83

---
+++ front_end/elements/ElementsBreadcrumbsUtils.ts
@@ -0,0 +1,85 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {ls} from '../platform/platform.js';
+
+export interface DOMNode {
+  parentNode?: DOMNode;
+  id: number;
+  nodeType: number;
+  pseudoType?: string;
+  shadowRootType: string|null;
+  nodeName: string;
+  nodeNameNicelyCased: string;
+  legacyDomNode: unknown;
+  highlightNode: () => void;
+  clearHighlight: () => void;
+  getAttribute: (attr: string) => string | undefined;
+}
+
+export type UserScrollPosition = 'start'|'middle'|'end';
+
+export interface Crumb {
+  title: CrumbTitle;
+  selected: boolean;
+  node: DOMNode;
+  originalNode: unknown;
+}
+
+export interface CrumbTitle {
+  main: string;
+  extras: {id?: string; classes?: string[];};
+}
+
+
+const makeCrumbTitle = (main: string, extras = {}): CrumbTitle => {
+  return {
+    main,
+    extras,
+  };
+};
+
+export class NodeSelectedEvent extends Event {
+  public data: unknown
+
+      public constructor(private node: DOMNode) {
+    super('node-selected', {});
+    this.data = node.legacyDomNode;
+  }
+}
+
+export const determineElementTitle = (domNode: DOMNode): CrumbTitle => {
+  switch (domNode.nodeType) {
+    case Node.ELEMENT_NODE: {
+      if (domNode.pseudoType) {
+        return makeCrumbTitle('::' + domNode.pseudoType);
+      }
+      const crumbTitle: CrumbTitle = makeCrumbTitle(domNode.nodeNameNicelyCased);
+
+      const id = domNode.getAttribute('id');
+      if (id) {
+        crumbTitle.extras.id = id;
+      }
+
+      const classAttribute = domNode.getAttribute('class');
+      if (classAttribute) {
+        const classes = new Set(classAttribute.split(/\s+/));
+        crumbTitle.extras.classes = Array.from(classes);
+      }
+
+      return crumbTitle;
+    }
+
+    case Node.TEXT_NODE:
+      return makeCrumbTitle(ls`(text)`);
+    case Node.COMMENT_NODE:
+      return makeCrumbTitle('<!-->');
+    case Node.DOCUMENT_TYPE_NODE:
+      return makeCrumbTitle('<!doctype>');
+    case Node.DOCUMENT_FRAGMENT_NODE:
+      return makeCrumbTitle(domNode.shadowRootType ? '#shadow-root' : domNode.nodeNameNicelyCased);
+    default:
+      return makeCrumbTitle(domNode.nodeNameNicelyCased);
+  }
+};
