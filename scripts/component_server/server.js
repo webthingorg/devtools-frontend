5d92cba4b1e8ad04ea2681062115c72b02190163
Jack Franklin <jacktfranklin@chromium.org>
2020-05-14 11:49:07 +0100

    [WIP] Components dev server
    This introduces a prototype of a developer server that we would use to view components locally.
    
    This won't be landed in its current form.
    
    The process would be:
    
    1. Create your component.
    2. Create `front_end/component_docs/your-component` folder.
    3. Add examples as self contained HTML file in `front_end/component_docs/your-component/X.html`
    4. Update `front_end/component_docs/BUILD.gn` with references to those HTML files.
    5. Run `node scripts/component_server/server.js` (we'd map this to an `npm` command)
    6. Visit `localhost:8090/your-component`.
    7. See all the examples in one file via iframes.
    
    If we were to accept this proposal I'd like to write the server in TypeScript and tidy up the logging/error handling/etc but hopefully this is enough for the purposes of the proposal.
    
    Change-Id: I6cd7588f045c3cd46e57f1f44441ffa95f0b70bb

---
+++ scripts/component_server/server.js
@@ -0,0 +1,142 @@
+// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const parseURL = require('url').parse;
+
+const serverPort = parseInt(process.env.PORT, 10) || 8090;
+const devtoolsFrontendFolder = path.resolve(path.join(__dirname, '..', '..', 'out', 'Default', 'gen', 'front_end'));
+
+http.createServer(requestHandler).listen(serverPort);
+console.log(`Started components server at http://localhost:${serverPort}\n`);
+
+function createComponentIndexFile(componentName, componentExamples) {
+  // clang-format off
+  return `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width" />
+      <title>DevTools component: ${componentName.substring(1)}</title>
+      <style>
+        h1 { text-transform: capitalize; }
+
+        .example {
+          padding: 5px;
+          margin: 10px;
+        }
+        iframe { display: block; width: 100%; }
+      </style>
+    </head>
+    <body>
+      <h1>${componentName.substring(1)}</h1>
+      ${componentExamples.map(example => {
+        const fullPath = path.join('component_docs', componentName, example);
+        return `<div class="example">
+          <h3>${example}</h3>
+          <iframe src="${fullPath}"></iframe>
+        </div>`;
+      }).join('\n')}
+    </body>
+  </html>`;
+  // clang-format on
+}
+
+function createServerIndexFile(componentNames) {
+  // clang-format off
+  return `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width" />
+      <title>DevTools components</title>
+      <style>
+        a { text-transform: capitalize; }
+      </style>
+    </head>
+    <body>
+      <h1>DevTools components</h1>
+      <ul>
+        ${componentNames.map(name => {
+          return `<li><a href='/${name}'>${name}</a></li>`;
+        }).join('\n')}
+      </ul>
+    </body>
+  </html>`;
+  // clang-format on
+}
+
+async function getExamplesForPath(filePath) {
+  const componentDirectory = path.join(devtoolsFrontendFolder, 'component_docs', filePath);
+
+  const contents = await fs.promises.readdir(componentDirectory);
+
+  return createComponentIndexFile(filePath, contents);
+}
+
+function respondWithHtml(response, html) {
+  response.setHeader('Content-Type', 'text/html; charset=utf-8');
+  response.writeHead(200);
+  response.write(html, 'utf8');
+  response.end();
+}
+
+async function requestHandler(request, response) {
+  const filePath = parseURL(request.url).pathname;
+
+  if (filePath === '/favicon.ico') {
+    sendResponse(404, '404, no favicon');
+    return;
+  }
+
+  if (filePath === '/' || filePath === '/index.html') {
+    const components = await fs.promises.readdir(path.join(devtoolsFrontendFolder, 'component_docs'));
+    const html = createServerIndexFile(components);
+    respondWithHtml(response, html);
+  } else if (path.extname(filePath) === '') {
+    // This means it's a component path like /breadcrumbs
+    const componentHtml = await getExamplesForPath(filePath);
+    respondWithHtml(response, componentHtml);
+  } else {
+    const fullPath = path.join(devtoolsFrontendFolder, filePath);
+    const errorsAccesingFile = await fs.promises.access(fullPath, fs.constants.R_OK);
+
+    if (errorsAccesingFile) {
+      console.error(`File ${fullPath} does not exist.`);
+      sendResponse(404, '404 - File not found');
+      return;
+    }
+
+    let encoding = 'utf8';
+    if (fullPath.endsWith('.wasm') || fullPath.endsWith('.png') || fullPath.endsWith('.jpg')) {
+      encoding = 'binary';
+    }
+
+    const fileContents = await fs.promises.readFile(fullPath, encoding);
+
+    encoding = 'utf8';
+    if (fullPath.endsWith('.js')) {
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
+    } else if (fullPath.endsWith('.css')) {
+      response.setHeader('Content-Type', 'text/css; charset=utf-8');
+    } else if (fullPath.endsWith('.wasm')) {
+      response.setHeader('Content-Type', 'application/wasm');
+      encoding = 'binary';
+    } else if (fullPath.endsWith('.svg')) {
+      response.setHeader('Content-Type', 'image/svg+xml; charset=utf-8');
+    } else if (fullPath.endsWith('.png')) {
+      response.setHeader('Content-Type', 'image/png');
+      encoding = 'binary';
+    } else if (fullPath.endsWith('.jpg')) {
+      response.setHeader('Content-Type', 'image/jpg');
+      encoding = 'binary';
+    }
+
+    response.writeHead(200);
+    response.write(fileContents, encoding);
+    response.end();
+  }
+}
