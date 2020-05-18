933c6acc294c2f0eeffb47bf2d3a00e930a980ee
Sigurd Schneider <sigurds@chromium.org>
2020-05-18 16:21:25 +0200

    Typescriptify ServiceWorkerCacheModel
    Currently blocked on https://crbug.com/1081686
    
    Bug: chromium:1011811, chromium:1079233
    Change-Id: I03130db3b66dff45784a56d33d64f095236812cb

--- front_end/protocol_client/InspectorBackend.js
+++ front_end/protocol_client/InspectorBackend.js
@@ -674,6 +674,20 @@ export class TargetBase {
   performanceAgent() {
     throw new Error('Implemented in InspectorBackend.js');
   }
+
+  /**
+   * @return {!ProtocolProxyApi.CacheStorageApi}
+   */
+  cacheStorageAgent() {
+    throw new Error('Implemented in InspectorBackend.js');
+  }
+
+  /**
+   * @return {!ProtocolProxyApi.StorageApi}
+   */
+  storageAgent() {
+    throw new Error('Implemented in InspectorBackend.js');
+  }
 }
 
 /**
