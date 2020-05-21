17720d1545747984227cfb99326a8c0403abe8bc
Jose Leal Chapa <joselea@microsoft.com>
2020-05-21 15:11:08 -0700

    Animations end to end test update
    Bug: 1082735
    Change-Id: I82b3bda336af42ed2b1a834a6b4ca68f79d1cc57

--- test/e2e/helpers/animations-helpers.ts
+++ test/e2e/helpers/animations-helpers.ts
@@ -7,11 +7,12 @@ import * as puppeteer from 'puppeteer';
 import {click, resourcesPath, waitFor} from '../../shared/helper.js';
 import {openPanelViaMoreTools} from './settings-helpers.js';
 
+const ANIMATIONS_PANE_SELECTOR = 'div[aria-label="Animations panel"]';
+
 export async function waitForAnimationsPanelToLoad() {
   // Open panel and wait for content
   await openPanelViaMoreTools('Animations');
-  await waitFor('div[aria-label="Animations panel"]');
-  await waitFor('div.animations-timeline');
+  await waitFor(ANIMATIONS_PANE_SELECTOR);
 }
 
 export async function navigateToSiteWithAnimation(target: puppeteer.Page) {
@@ -22,8 +23,12 @@ export async function navigateToSiteWithAnimation(target: puppeteer.Page) {
 
 export async function waitForAnimationContent() {
   const first_animation_preview_selector = '.animation-buffer-preview[aria-label="Animation Preview 1"]';
+  const animation_timeline_buffer_children_selector = '.animation-timeline-buffer';
+
+  await waitFor(animation_timeline_buffer_children_selector);
   await waitFor(first_animation_preview_selector);
   await click(first_animation_preview_selector);
+
   await waitFor('.animation-node-row');
   await waitFor('svg.animation-ui');
 }
