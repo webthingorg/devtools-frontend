fd15bf30c11565440ba5010622fd6711621cc803
Changhao Han <changhaohan@chromium.org>
2020-05-26 22:24:32 +0200

    e2e test for media query inspector; add canDock option in ReloadDevTools
    Add e2e test for Device Toolbar's media query inspector, as a follow-up
    to an earlier issue that media query inspector did not show media query
    rules from style tag attributes.
    When adding this e2e test for Device Toolbar, it's required that the
    e2e test driver can reload DevTools frontend page with "can_dock=true"
    parameter in its URL. This CL also adds "canDock" as an option when
    reloading DevTools frontend in e2e testing.
    
    Bug: chromium:1069467, chromium:1086159
    Change-Id: Ic8685fc231fa7b01554e05a1121b3c2394fb7131

---
+++ test/e2e/emulation/media-query-inspector_test.ts
@@ -0,0 +1,54 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {describe, it} from 'mocha';
+
+import {$, $$, click, getBrowserAndPages, resourcesPath, waitFor} from '../../shared/helper.js';
+import {openDeviceToolbar, reloadDockableFrontEnd} from '../helpers/emulation-helpers.js';
+
+const DEVICE_TOOLBAR_OPTIONS_SELECTOR = '.device-mode-toolbar .device-mode-toolbar-options';
+const MEDIA_QUERY_INSPECTOR_SELECTOR = '.media-inspector-view';
+const MEDIA_INSPECTOR_MARKER_SELECTOR = '.media-inspector-marker';
+
+const showMediaQueryInspector = async () => {
+  const inspector = await $(MEDIA_QUERY_INSPECTOR_SELECTOR);
+  const isOpen = await inspector.evaluate(e => !!e);
+  if (isOpen) {
+    return;
+  }
+
+  await click(DEVICE_TOOLBAR_OPTIONS_SELECTOR);
+  const {frontend} = getBrowserAndPages();
+  await frontend.keyboard.press('ArrowDown');
+  await frontend.keyboard.press('Enter');
+  await waitFor(MEDIA_QUERY_INSPECTOR_SELECTOR);
+};
+
+describe('Media query inspector', async () => {
+  beforeEach(async function() {
+    await reloadDockableFrontEnd();
+
+    const {target} = getBrowserAndPages();
+    await target.goto(`${resourcesPath}/emulation/media-query-inspector.html`);
+    await waitFor('.tabbed-pane-left-toolbar');
+    await openDeviceToolbar();
+    await showMediaQueryInspector();
+  });
+
+  it('lists all the media queries', async () => {
+    const inspectorMarkers = await $$(MEDIA_INSPECTOR_MARKER_SELECTOR);
+    const markersContent = await inspectorMarkers.evaluate((nodes: HTMLElement[]) => {
+      return nodes.map((node: HTMLElement) => node.textContent);
+    });
+    assert.includeMembers(
+        markersContent,
+        [
+          '100px300px',
+          '500px800px',
+          '801px',
+        ],
+        'missed media query rule(s)');
+  });
+});
---
+++ test/e2e/helpers/emulation-helpers.ts
@@ -0,0 +1,21 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {click, reloadDevTools, waitFor} from '../../shared/helper.js';
+
+const DEVICE_TOOLBAR_TOGGLER_SELECTOR = '[aria-label="Toggle device toolbar"]';
+const DEVICE_TOOLBAR_SELECTOR = '.device-mode-toolbar';
+
+export const reloadDockableFrontEnd = async () => {
+  await reloadDevTools({canDock: true});
+};
+
+export const openDeviceToolbar = async () => {
+  const deviceToolbarToggler = await waitFor(DEVICE_TOOLBAR_TOGGLER_SELECTOR);
+  const isOpen = (await deviceToolbarToggler.evaluate(e => e.getAttribute('aria-pressed'))) === 'true';
+  if (isOpen) {
+    return;
+  }
+  await click(deviceToolbarToggler);
+  await waitFor(DEVICE_TOOLBAR_SELECTOR);
+};
