878891cf2739c0c4e1be55c3d46c2b109fc77ac7
Philip Pfaffe <pfaffe@chromium.org>
2020-05-29 14:38:11 +0200

    Implement a Chrome Extension Wrapper for the Symbol Server
    Bug: chromium:1082697
    Change-Id: Id1261ff3dbdf40bfc463a33930d1c7c3d3ca54e9

--- back_end/CXXDWARFSymbols/tests/FormatValues/FormatVariable.js
+++ back_end/CXXDWARFSymbols/tests/FormatValues/FormatVariable.js
@@ -2,44 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// REQUIRES: formatters
-// RUN: %p/Inputs/lsp-encode %p/Inputs/formatArray.json \
+// REQUIRES: formatters && !wasm_plugin
+// RUN: %p/Inputs/lsp-encode %p/Inputs/FormatVariable.json \
 // RUN: | %symbol-server 2>/dev/null | node %s | FileCheck %s
 
 // CHECK-NOT: Didn't consume
-// CHECK: Reading 4 bytes from offset 1024
-// CHECK: Reading 4 bytes from offset 1028
-// CHECK: Reading 4 bytes from offset 1032
-// CHECK: Reading 4 bytes from offset 1036
-// CHECK: Result at: {{[0-9]+}}
-// FIXME: Canonical types?
-// CHECK: Result: {"type":"int","name":"A","value":[{"type":"int32_t","name":"A[0]","value":"0"},{"type":"int32_t","name":"A[1]","value":"4"},{"type":"int32_t","name":"A[2]","value":"8"},{"type":"int32_t","name":"A[3]","value":"12"}]}
+// CHECK: Got 3 responses
+// CHECK: ["global.c"]
+// CHECK: {"name":"I","scope":"GLOBAL","type":"int"}
 
-tests = require('./tests.js')
+// CHECK: Reading 4 bytes from offset 1024
+// CHECK: Result at: {{[0-9]+}}
+// CHECK: Result: {"type":"int32_t","name":"I","value":"256"}
+
+tests = require('../NodeTests.js')
+
+var heap;
 
 // void __getMemory(uint32_t offset, uint32_t size, void* result);
 function proxyGetMemory(offset, size, result) {
-  console.log('Reading ' + size + ' bytes from offset ' + offset);
+  console.log('Reading ' + size + ' bytes from offset ' + offset + ' into ' + result);
   // Expecting size 4, so "read" 4 bytes from the engine:
-  Heap[result] = offset % 256;
-  Heap[result + 1] = 0;
-  Heap[result + 2] = 0;
-  Heap[result + 3] = 0;
+  heap[result] = 0;
+  heap[result + 1] = 1;
+  heap[result + 2] = 0;
+  heap[result + 3] = 0;
 }
 
 (async () => {
   const data = await tests.readStdIn();
   const input = [...tests.parseInput(data)];
+  console.log('Got ' + input.length + ' responses.');
 
   for (const message of input) {
     if (message.result.value) {
       const buf = Uint8Array.from(tests.decodeBase64(message.result.value.code));
       const module = new WebAssembly.Module(buf);
       const instance = tests.makeInstance(module, {getMemory: proxyGetMemory});
-      Heap = new Uint8Array(instance.exports.memory.buffer);
+      heap = new Uint8Array(instance.exports.memory.buffer);
       const result_offset = instance.exports.wasm_format();
       console.log('Result at: ' + result_offset);
-      console.log('Result: ' + tests.toString(Heap, result_offset));
+      console.log('Result: ' + tests.toString(heap, result_offset));
     } else {
       console.log(JSON.stringify(message));
     }
