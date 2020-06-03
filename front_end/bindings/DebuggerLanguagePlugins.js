217c7b98f9379615f8d31e0613cfd14e0fe94875
Philip Pfaffe <pfaffe@chromium.org>
2020-06-03 16:50:12 +0200

    [wasm debugging] Add a js_type to evaluator results
    Adding a js_type to the evaluator result allows the scope view to decide
    how to show the values on its own. Effectively that moves special casing
    for the type mapping out of javascript into the symbol server.
    
    Change-Id: I07c8b896fcaa2bedc6c38b6275fe37c37c8049e2

--- front_end/bindings/DebuggerLanguagePlugins.js
+++ front_end/bindings/DebuggerLanguagePlugins.js
@@ -16,7 +16,8 @@ class SourceVariable extends SDK.RemoteObject.RemoteObjectImpl {
    */
   constructor(callFrame, variable, plugin, location) {
     super(
-        callFrame.debuggerModel.runtimeModel(), /* objectId=*/ undefined, /* type=*/ variable.type,
+        callFrame.debuggerModel.runtimeModel(), /* objectId=*/ undefined,
+        /* type=*/ variable.type.replace(/[ \[\]]/g, '_'),
         /* subtype=*/ undefined, /* value=*/ null, /* unserializableValue=*/ undefined,
         /* customPreview=*/ variable.type);
     this._variable = variable;
@@ -32,7 +33,64 @@ class SourceVariable extends SDK.RemoteObject.RemoteObjectImpl {
    * @return {string}
    */
   get type() {
-    return this._variable.type;
+    return this._variable.type.replace(/[ \[\]]/g, '_');
+  }
+
+  /** Get the representation when value contains a string
+   * @param {!VariableValue} value
+   */
+  _reprString(value) {
+    return value.value;
+  }
+
+  /** Get the representation when value contains a number
+   * @param {!VariableValue} value
+   */
+  _reprNumber(value) {
+    return Number(value.value);
+  }
+
+  /** Get the representation when value is a compound value
+   * @param {!VariableValue} value
+   */
+  _reprCompound(value) {
+    const result = {};
+    for (const property of value.value) {
+      result[property.name] = this._repr(property);
+    }
+    return result;
+  }
+
+  /** Get the representation when value contains an array of values
+   * @param {!VariableValue} value
+   */
+  _reprArray(value) {
+    return value.value.map(v => this._repr(v));
+  }
+
+  /** Get the representation for a variable value
+   * @param {!VariableValue} value
+   */
+  _repr(value) {
+    if (value.js_type === 'array') {
+      return this._reprArray(value);
+    }
+    if (value.js_type === 'object') {
+      return this._reprArray(value);
+    }
+    if (value.js_type === 'number') {
+      return this._reprNumber(value);
+    }
+    return this._reprString(value);
+  }
+
+  /** Produce a language specific representation of a variable value
+   * @override
+   * @param {!VariableValue} value
+   * @return {!SDK.RemoteObject.RemoteObject}
+   */
+  _getRepresentation(value) {
+    return new SDK.RemoteObject.LocalJSONObject(this._repr(value));
   }
 
   /**
@@ -43,18 +101,7 @@ class SourceVariable extends SDK.RemoteObject.RemoteObjectImpl {
    * @return {!Promise<!SDK.RemoteObject.GetPropertiesResult>}
    */
   async doGetProperties(ownProperties, accessorPropertiesOnly, generatePreview) {
-    if (accessorPropertiesOnly) {
-      return /** @type {!SDK.RemoteObject.GetPropertiesResult} */ ({properties: [], internalProperties: []});
-    }
-
-    const repr = await getRepr(this._evaluator, this._callFrame, this._plugin);
-    return /** @type {!SDK.RemoteObject.GetPropertiesResult} */ ({
-      properties: [new SDK.RemoteObject.RemoteObjectProperty(
-          'value', repr, /* enumerable=*/ false, /* writable=*/ false, /* isOwn=*/ true, /* wasThrown=*/ false)],
-      internalProperties: []
-    });
-
-    async function getRepr(evaluatorPromise, callFrame, plugin) {
+    const getRepr = async (evaluatorPromise, callFrame, plugin) => {
       try {
         const evaluator = await evaluatorPromise;
         const evaluateResponse = await callFrame.debuggerModel._agent.invoke_executeWasmEvaluator(
@@ -67,7 +114,7 @@ class SourceVariable extends SDK.RemoteObject.RemoteObjectImpl {
         }
 
         const value = JSON.parse(evaluateResponse.result.value);
-        return await plugin.getRepresentation(value);
+        return this._getRepresentation(value);
       } catch (error) {
         if (!(error instanceof DebuggerLanguagePluginError)) {
           throw error;
@@ -75,7 +122,18 @@ class SourceVariable extends SDK.RemoteObject.RemoteObjectImpl {
         console.error('Error in debugger language plugin: ' + error.message + ' (' + error.code + ')');
         return null;
       }
+    };
+
+    if (accessorPropertiesOnly) {
+      return /** @type {!SDK.RemoteObject.GetPropertiesResult} */ ({properties: [], internalProperties: []});
     }
+
+    const repr = await getRepr(this._evaluator, this._callFrame, this._plugin);
+    return /** @type {!SDK.RemoteObject.GetPropertiesResult} */ ({
+      properties: [new SDK.RemoteObject.RemoteObjectProperty(
+          'value', repr, /* enumerable=*/ false, /* writable=*/ false, /* isOwn=*/ true, /* wasThrown=*/ false)],
+      internalProperties: []
+    });
   }
 }
 
@@ -660,12 +718,5 @@ export class DebuggerLanguagePlugin {
    * @throws {DebuggerLanguagePluginError}
   */
   async evaluateVariable(name, location) {
-  }
-
-  /** Produce a language specific representation of a variable value
-   * @param {!VariableValue} value
-   * @return {!Promise<!SDK.RemoteObject.RemoteObject>}
-   */
-  async getRepresentation(value) {
   }
 }
