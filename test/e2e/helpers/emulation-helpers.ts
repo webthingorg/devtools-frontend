b6f12044364c93a168c9eded1eb51148ee43bb67
Songtao Xia <soxia@microsoft.com>
2020-06-02 19:26:58 -0700

    [e2e] e2e tests for dual screen emulation.
    Test the experimental feature of dual screen emulation.
    
    Test the dual screen span mode exists for a dual screen device but
    not for a non-dual-screen device.
    
    Change-Id: I72b0cb4e77bb58305f112b32fd7b040436ec42c9

--- test/e2e/helpers/emulation-helpers.ts
+++ test/e2e/helpers/emulation-helpers.ts
@@ -1,7 +1,10 @@
 // Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import {$, click, getBrowserAndPages, reloadDevTools, waitFor} from '../../shared/helper.js';
+import {assert} from 'chai';
+import * as puppeteer from 'puppeteer';
+
+import {$, click, enableExperiment, getBrowserAndPages, reloadDevTools, resourcesPath, waitFor} from '../../shared/helper.js';
 
 const DEVICE_TOOLBAR_TOGGLER_SELECTOR = '[aria-label="Toggle device toolbar"]';
 const DEVICE_TOOLBAR_SELECTOR = '.device-mode-toolbar';
@@ -35,4 +38,53 @@ export const showMediaQueryInspector = async () => {
   await frontend.keyboard.press('ArrowDown');
   await frontend.keyboard.press('Enter');
   await waitFor(MEDIA_QUERY_INSPECTOR_SELECTOR);
+};
+
+export const startEmulationWithDualScreenFlag = async () => {
+  await enableExperiment('dualScreenSupport', {canDock: true});
+  const {target} = getBrowserAndPages();
+  await target.goto(`${resourcesPath}/emulation/dual-screen-inspector.html`);
+  await waitFor('.tabbed-pane-left-toolbar');
+  await openDeviceToolbar();
+};
+
+const deviceListSelector = '.toolbar-button';
+const duoSelector = '[aria-label*="Surface Duo"]';
+const spanButtonSelector = '[aria-label="Toggle dual-screen mode"]';
+
+const assertButtonDisabled = async (spanButton: puppeteer.JSHandle, expected: boolean) => {
+  const isDisabled = await spanButton.evaluate((e: HTMLButtonElement) => {
+    return e.disabled;
+  });
+  assert(isDisabled === expected);
+};
+
+// Test if span button is clickable when emulating a dual screen device.
+export const testSpanForDualScreen = async () => {
+  const toolbar = await $(DEVICE_TOOLBAR_SELECTOR);
+  const button = await $(deviceListSelector, toolbar);
+  await click(button);
+
+  const duo = await $(duoSelector);
+  await click(duo);
+
+  // make sure the span button is clickable.
+  const spanButton = await $(spanButtonSelector);
+  await assertButtonDisabled(spanButton, false);
+  await click(spanButton);
+};
+
+const nonDualSelector = '[aria-label*="iPad"]';
+// Test if span button is clickable when emulating a non-dual-screen device.
+export const testSpanForNonDualScreen = async () => {
+  const toolbar = await $(DEVICE_TOOLBAR_SELECTOR);
+  const button = await $(deviceListSelector, toolbar);
+  await click(button);
+
+  const nonDual = await $(nonDualSelector);
+  await click(nonDual);
+
+  // make sure the span button is disabled.
+  const spanButton = await $(spanButtonSelector);
+  await assertButtonDisabled(spanButton, true);
 };
