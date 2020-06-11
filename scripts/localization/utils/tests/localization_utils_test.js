38a7ab50509ac15daaa965f2fe43411ecd9aead8
Christy Chen <chrche@microsoft.com>
2020-06-11 00:29:29 -0700

    [Loc] Presubmit#1: Check unused resources in UIStrings
    This check will detect (and autofix) any string resources that are not used in the code anymore. If a developer deletes a localization call but forgets to delete it from the UIStrings structure, the check will warn/remove it.
    
    Testing steps
    1. Add the following to the CoverageView.js right after import statements
    export const UIStrings = {
      /**
      *@description Text in Coverage List View of the Coverage tab
      */
      perFunction: 'Per function',
      /**
      *@description Text in Coverage List View of the Coverage tab
      */
      perBlock: 'Per block',
    };
    
    2. Change: 
            label: ls`Per function`,
     To the Loc V2 API call:
            label: Common.i18n.getLocalizedString(str_, UIStrings.perFunction),
    
    3. Change:
            label: ls`Per block`,
     To not use any localization API
            label: "Per block",
    
    4. Run
    node check_localizable_resources.js --autofix
    
    The Loc V1 check would remove the two entries from grdp
    The Loc V2 check would remove perBlock entry from UIStrings
    
    
    
    
    Bug: 941561
    Change-Id: Ic5f3ee6e9c1586bb3226593c32bc7af7e49a547a

--- scripts/localization/utils/tests/localization_utils_test.js
+++ scripts/localization/utils/tests/localization_utils_test.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const {isLocalizationCall, espree, getLocalizationCase} = require('../localization_utils');
+const {isLocalizationCall, espree, getLocalizationCase, isLocalizationV2Call} = require('../localization_utils');
 const {assert} = require('chai');
 
 const parseCode = code => espree.parse(code, {ecmaVersion: 11, sourceType: 'module', range: true, loc: true});
@@ -44,6 +44,28 @@ describe('isLocalizationCall', () => {
   });
 });
 
+describe('isLocalizationV2Call', () => {
+  it('is true for a call to Common.i18n.getLocalizedString', () => {
+    const ast = parseCode('Common.i18n.getLocalizedString(_str, UIStrings.fakeID)');
+    assert.isTrue(isLocalizationV2Call(ast.body[0].expression));
+  });
+
+  it('is true for a call to Common.i18n.getFormatLocalizedString', () => {
+    const ast = parseCode('Common.i18n.getFormatLocalizedString(_str, UIStrings.fakeID)');
+    assert.isTrue(isLocalizationV2Call(ast.body[0].expression));
+  });
+
+  it('is true for a declaration of UIStrings', () => {
+    const ast = parseCode('const UIStrings = {fakeID: "Hello World"}');
+    assert.isTrue(isLocalizationV2Call(ast.body[0].declarations[0]));
+  });
+
+  it('is false for a tagged template expression', () => {
+    const ast = parseCode('ls`foo`');
+    assert.isFalse(isLocalizationV2Call(ast.body[0].expression));
+  });
+});
+
 describe('getLocalizationCase', () => {
   it('returns correctly for a tagged template', () => {
     const ast = parseCode('ls`foo`');
@@ -73,5 +95,17 @@ describe('getLocalizationCase', () => {
   it('returns Platform.UIString for UIString', () => {
     const ast = parseCode('UIString(\'blahblah %s, 2\')');
     assert.strictEqual(getLocalizationCase(ast.body[0].expression), 'Platform.UIString');
+  });
+  it('returns Common.i18n.getLocalizedString for Common.i18n.getLocalizedString', () => {
+    const ast = parseCode('Common.i18n.getLocalizedString(_str, UIStrings.fakeID)');
+    assert.strictEqual(getLocalizationCase(ast.body[0].expression), 'Common.i18n.getLocalizedString');
+  });
+  it('returns Common.i18n.getFormatLocalizedString for Common.i18n.getFormatLocalizedString', () => {
+    const ast = parseCode('Common.i18n.getFormatLocalizedString(_str, UIStrings.fakeID)');
+    assert.strictEqual(getLocalizationCase(ast.body[0].expression), 'Common.i18n.getFormatLocalizedString');
+  });
+  it('returns UIStrings for UIStrings', () => {
+    const ast = parseCode('const UIStrings = {fakeID: "Hello World"}');
+    assert.strictEqual(getLocalizationCase(ast.body[0].declarations[0]), 'UIStrings');
   });
 });
