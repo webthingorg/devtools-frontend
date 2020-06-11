77d81367867766a8867814dc2d27405f72b1a7f5
Brandon Goddard <brgoddar@microsoft.com>
2020-06-11 08:30:28 -0700

    Grid: Add settings for grid highlight config behind experiment
    This CL adds a new section to the preferences settings page to allow
    users to configure a few options of the GridHighlightConfig used
    for css grid tooling highlighting. These settings default to the current
    configuration.
    
    New setting section: https://imgur.com/xzD3uNL
    
    Examples of different configurations: https://imgur.com/a/2k9lfqs
    
    Follow up work: add settings for line number tooltips once that
    has landed
    
    Bug: 1047356
    Change-Id: I234ab6254653a079ba4aa1d055f7d35961ca7d85

--- front_end/sdk/OverlayModel.js
+++ front_end/sdk/OverlayModel.js
@@ -310,6 +310,70 @@ export class OverlayModel extends SDKModel {
   }
 
   /**
+   * @return {!Protocol.Overlay.GridHighlightConfig}
+   */
+  _buildGridHighlightConfig() {
+    const showGridRowGaps = Common.Settings.Settings.instance().moduleSetting('showGridRowGaps').get();
+    const showGridColumnGaps = Common.Settings.Settings.instance().moduleSetting('showGridColumnGaps').get();
+    const gridBorderSetting = Common.Settings.Settings.instance().moduleSetting('showGridBorder').get();
+    let showGridBorder;
+    let gridBorderDashed;
+    switch (gridBorderSetting) {
+      case 'dashed':
+        showGridBorder = true;
+        gridBorderDashed = true;
+        break;
+      case 'solid':
+        showGridBorder = true;
+        break;
+      default:
+        break;
+    }
+    const showGridLinesSetting = Common.Settings.Settings.instance().moduleSetting('showGridLines').get();
+    let showGridLines;
+    let gridLinesDashed;
+    let showGridExtensionLines;
+    switch (showGridLinesSetting) {
+      case 'dashed':
+        showGridLines = true;
+        gridLinesDashed = true;
+        break;
+      case 'solid':
+        showGridLines = true;
+        break;
+      case 'extended-dashed':
+        showGridLines = true;
+        gridLinesDashed = true;
+        showGridExtensionLines = true;
+        break;
+      case 'extended-solid':
+        showGridLines = true;
+        showGridExtensionLines = true;
+        break;
+      default:
+        break;
+    }
+    // Add background to help distinguish rows/columns when cell borders are not outlined
+    const addBackgroundsToGaps = !showGridLines;
+
+    return {
+      rowGapColor: (showGridRowGaps && addBackgroundsToGaps) ?
+          Common.Color.PageHighlight.GridRowGapBackground.toProtocolRGBA() :
+          undefined,
+      rowHatchColor: showGridRowGaps ? Common.Color.PageHighlight.GridRowGapHatch.toProtocolRGBA() : undefined,
+      columnGapColor: (showGridColumnGaps && addBackgroundsToGaps) ?
+          Common.Color.PageHighlight.GridColumnGapBackground.toProtocolRGBA() :
+          undefined,
+      columnHatchColor: showGridColumnGaps ? Common.Color.PageHighlight.GridColumnGapHatch.toProtocolRGBA() : undefined,
+      gridBorderColor: showGridBorder ? Common.Color.PageHighlight.GridBorder.toProtocolRGBA() : undefined,
+      gridBorderDash: gridBorderDashed,
+      cellBorderColor: showGridLines ? Common.Color.PageHighlight.GridCellBorder.toProtocolRGBA() : undefined,
+      cellBorderDash: gridLinesDashed,
+      showGridExtensionLines: showGridExtensionLines
+    };
+  }
+
+  /**
    * @param {string=} mode
    * @param {boolean=} showStyles
    * @return {!Protocol.Overlay.HighlightConfig}
@@ -343,12 +407,7 @@ export class OverlayModel extends SDKModel {
 
     if (mode === 'all') {
       if (this._gridFeaturesExperimentEnabled) {
-        highlightConfig.gridHighlightConfig = {
-          rowHatchColor: Common.Color.PageHighlight.GridRowGapHatch.toProtocolRGBA(),
-          columnHatchColor: Common.Color.PageHighlight.GridColumnGapHatch.toProtocolRGBA(),
-          cellBorderColor: Common.Color.PageHighlight.GridCellBorder.toProtocolRGBA(),
-          cellBorderDash: true
-        };
+        highlightConfig.gridHighlightConfig = this._buildGridHighlightConfig();
       } else {
         // Support for the legacy grid cell highlight.
         highlightConfig.cssGridColor = Common.Color.PageHighlight.CssGrid.toProtocolRGBA();
