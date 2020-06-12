72ae970c90c8ff051c6ffc6bf9ff226fd88e615c
Jack Franklin <jacktfranklin@chromium.org>
2020-06-12 14:51:37 +0100

    Expose all of the lit-html directives
    Change-Id: I37dae3b447c6cf44ef4a87294a4f45fab0dceb09

--- front_end/lit_html/directives.ts
+++ front_end/lit_html/directives.ts
@@ -2,4 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export * from '../third_party/lit-html/package/directives/async-append.js';
+export * from '../third_party/lit-html/package/directives/async-replace.js';
+export * from '../third_party/lit-html/package/directives/cache.js';
 export * from '../third_party/lit-html/package/directives/class-map.js';
+export * from '../third_party/lit-html/package/directives/guard.js';
+export * from '../third_party/lit-html/package/directives/if-defined.js';
+export * from '../third_party/lit-html/package/directives/repeat.js';
+export * from '../third_party/lit-html/package/directives/style-map.js';
+export * from '../third_party/lit-html/package/directives/unsafe-html.js';
+export * from '../third_party/lit-html/package/directives/until.js';
