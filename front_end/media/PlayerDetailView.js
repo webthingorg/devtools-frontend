8fb8fae205ed5670f23a96d2fb35ea7c916555c9
Ted Meyer <tmathmeyer@chromium.org>
2020-06-12 14:32:42 -0700

    Add a live-updating timeline view to the Media component in devtools
    I had to add a small helper to the FlameChart.js file to be able to add
    a scroll wheel listener for zooming in and out.
    
    Right now I've got the play/pause/ended/destroyed events hooked up,
    as well as the audio/video buffering delays. I want to next hook up
    some of the new work the team has been doing on smoothness and cadence
    of frame presentation.
    
    Example usage:
    https://files.tedm.io/dtss/timeline/example_usage1.mp4
    
    Example with throttled internet:
    https://files.tedm.io/dtss/timeline/example_usage2.mp4
    
    Change-Id: I515acb76196d127059a958388168f0794e190274

--- front_end/media/PlayerDetailView.js
+++ front_end/media/PlayerDetailView.js
@@ -6,6 +6,7 @@ import * as Common from '../common/common.js';
 import * as UI from '../ui/ui.js';
 
 import {PlayerEventsView} from './EventDisplayTable.js';
+import {PlayerEventsTimeline} from './EventTimelineView.js';
 import {TriggerHandler} from './MainView.js';  // eslint-disable-line no-unused-vars
 import {PlayerEvent} from './MediaModel.js';   // eslint-disable-line no-unused-vars
 import {PlayerPropertiesView} from './PlayerPropertiesView.js';
@@ -16,6 +17,7 @@ import {PlayerPropertiesView} from './PlayerPropertiesView.js';
 export const PlayerDetailViewTabs = {
   Events: 'events',
   Properties: 'properties',
+  Timeline: 'timeline'
 };
 
 /**
@@ -28,6 +30,7 @@ export class PlayerDetailView extends UI.TabbedPane.TabbedPane {
 
     this._eventView = new PlayerEventsView();
     this._propertyView = new PlayerPropertiesView();
+    this._timelineView = new PlayerEventsTimeline();
 
     this.appendTab(
         PlayerDetailViewTabs.Properties, Common.UIString.UIString('Properties'), this._propertyView,
@@ -36,6 +39,10 @@ export class PlayerDetailView extends UI.TabbedPane.TabbedPane {
     this.appendTab(
         PlayerDetailViewTabs.Events, Common.UIString.UIString('Events'), this._eventView,
         Common.UIString.UIString('Player events'));
+
+    this.appendTab(
+        PlayerDetailViewTabs.Timeline, Common.UIString.UIString('Timeline'), this._timelineView,
+        Common.UIString.UIString('Player timeline'));
   }
 
   /**
@@ -66,5 +73,6 @@ export class PlayerDetailView extends UI.TabbedPane.TabbedPane {
    */
   onEvent(event) {
     this._eventView.onEvent(event);
+    this._timelineView.onEvent(event);
   }
 }
