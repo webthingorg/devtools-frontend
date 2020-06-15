38f20a1b00357fa4af3b7b8e21dbdf6fab7ddc15
Philip Pfaffe <pfaffe@chromium.org>
2020-06-15 10:52:57 +0200

    Implement a Chrome Extension Wrapper for the Symbol Server
    Bug: chromium:1082697
    Change-Id: Id1261ff3dbdf40bfc463a33930d1c7c3d3ca54e9

---
+++ back_end/CXXDWARFSymbols/driver/wasm-plugin/Extension/DevToolsPlugin.js
@@ -0,0 +1,73 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class Plugin {
+  constructor() {
+    this._nextRequestId = 0;
+    this._callbacks = new Map();
+    this._port = null;
+    this._connect();
+  }
+
+  _connect() {
+    this._port = chrome.runtime.connect({name: 'SymbolServer'});
+    this._port.onDisconnect.addListener(() => {
+      console.error('Disconnected');
+      const error = chrome.runtime.lastError;
+      if (error) {
+        throw error;
+      }
+      this._connect();
+    });
+    this._port.onMessage.addListener(this._onMessage.bind(this));
+  }
+
+  _onMessage(message) {
+    if (!this._callbacks.has(message.requestId)) {
+      throw `No pending request ${message.requestId}`;
+    }
+    const callback = this._callbacks.get(message.requestId);
+    this._callbacks.delete(message.requestId);
+    callback(message.value);
+  }
+
+  _call(method, parameters) {
+    return new Promise(resolve => {
+      const requestId = this._nextRequestId++;
+      this._callbacks.set(requestId, resolve);
+      const request = {method, requestId, parameters};
+      this._port.postMessage(request);
+    });
+  }
+
+  id() {
+    return 'TestPlugin';
+  }
+
+  handleScript(scriptId, isWasm, sourceURL, sourceMapURL, debugSymbols) {
+    return this._call('handleScript', {scriptId, isWasm, sourceURL, sourceMapURL, debugSymbols});
+  }
+
+  addRawModule(rawModuleId, symbols, rawModule) {
+    return this._call('addRawModule', {rawModuleId, symbols, rawModule});
+  }
+
+  sourceLocationToRawLocation(sourceLocation) {
+    return this._call('sourceLocationToRawLocation', {sourceLocation});
+  }
+
+  rawLocationToSourceLocation(rawLocation) {
+    return this._call('rawLocationToSourceLocation', {rawLocation});
+  }
+
+  listVariablesInScope(rawLocation) {
+    return this._call('listVariablesInScope', {rawLocation});
+  }
+
+  evaluateVariable(name, location) {
+    return this._call('evaluateVariable', {name, location});
+  }
+}
+
+chrome.devtools.panels.sources.registerLanguagePluginExtension(new Plugin());
--- back_end/CXXDWARFSymbols/tests/FormatValues/FormatArray.js
+++ back_end/CXXDWARFSymbols/tests/FormatValues/FormatArray.js
@@ -2,58 +2,41 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// REQUIRES: formatters
-// RUN: %p/Inputs/lsp-encode %p/Inputs/formatString.json \
+// REQUIRES: formatters && !wasm_plugin
+// RUN: %p/Inputs/lsp-encode %p/Inputs/FormatArray.json \
 // RUN: | %symbol-server 2>/dev/null | node %s | FileCheck %s
 
 // CHECK-NOT: Didn't consume
+// CHECK: Reading 4 bytes from offset 1024
 // CHECK: Reading 4 bytes from offset 1028
+// CHECK: Reading 4 bytes from offset 1032
+// CHECK: Reading 4 bytes from offset 1036
 // CHECK: Result at: {{[0-9]+}}
-// CHECK: Result: {"type":"const char*","name":"String","value":"ABCD"}
+// FIXME: Canonical types?
+// CHECK: Result: {"type":"int","name":"A","value":[{"type":"int32_t","name":"A[0]","value":"0"},{"type":"int32_t","name":"A[1]","value":"4"},{"type":"int32_t","name":"A[2]","value":"8"},{"type":"int32_t","name":"A[3]","value":"12"}]}
 
-tests = require('./tests.js')
+tests = require('../NodeTests.js')
 
 // void __getMemory(uint32_t offset, uint32_t size, void* result);
 function proxyGetMemory(offset, size, result) {
   console.log('Reading ' + size + ' bytes from offset ' + offset);
   // Expecting size 4, so "read" 4 bytes from the engine:
-  switch (offset) {
-    case 1028:  // Deref char*;
-      Heap[result] = 0;
-      Heap[result + 1] = 0;
-      Heap[result + 2] = 1;
-      Heap[result + 3] = 0;
-      break;
-    case 65536:
-      Heap[result] = 65;
-      'A'
-      break;
-    case 65537:
-      Heap[result] = 66;
-      'B'
-      break;
-    case 65538:
-      Heap[result] = 67;
-      'C'
-      break;
-    case 65539:
-      Heap[result] = 68;
-      'D'
-      break;
-  }
+  Heap[result] = offset % 256;
+  Heap[result + 1] = 0;
+  Heap[result + 2] = 0;
+  Heap[result + 3] = 0;
 }
 
 (async () => {
   const data = await tests.readStdIn();
   const input = [...tests.parseInput(data)];
-  console.log(JSON.stringify(input));
+
   for (const message of input) {
     if (message.result.value) {
       const buf = Uint8Array.from(tests.decodeBase64(message.result.value.code));
       const module = new WebAssembly.Module(buf);
       const instance = tests.makeInstance(module, {getMemory: proxyGetMemory});
       Heap = new Uint8Array(instance.exports.memory.buffer);
-
       const result_offset = instance.exports.wasm_format();
       console.log('Result at: ' + result_offset);
       console.log('Result: ' + tests.toString(Heap, result_offset));
