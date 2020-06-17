3875f06275e1af06bbebaa6f48b953eb5c2bf2c0
Ng Zhi An <zhin@chromium.org>
2020-06-17 14:36:11 -0700

    Use acorn-logical-assignment
    Bug: chromium:1080569
    Change-Id: Id65b11e9df68f1e04ec2b84b18b8684db3452f37

--- front_end/formatter_worker/Acorn.js
+++ front_end/formatter_worker/Acorn.js
@@ -2,8 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import logicalAssignment from '../third_party/acorn-logical-assignment/package/dist/acorn-logical-assignment.mjs';
 import * as Acorn from '../third_party/acorn/package/dist/acorn.mjs';
+
+// Extensions return a new Parser class (no mutation).
+const ExtendedParser = Acorn.Parser.extend(logicalAssignment);
+
+const CustomAcorn = {
+  ...Acorn,
+  Parser: ExtendedParser,
+  // Re-export convenient functions on the acorn namespace.  Binding is needed
+  // since the functions uses `new this(...)`, and `this` will be the exports
+  // object if not bound to ExtendedParser.
+  tokenizer: ExtendedParser.tokenizer.bind(ExtendedParser),
+  parse: ExtendedParser.parse.bind(ExtendedParser),
+};
 
 // Simple wrapper around Acorn so that we can add extensions to it, such as ES
 // features that are not merged to Acorn yet.
-export {Acorn};
+export {CustomAcorn as Acorn};
