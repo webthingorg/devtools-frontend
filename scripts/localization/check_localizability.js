cf2cc71105730fc87046e99af461d3882da07007
Christy Chen <chrche@microsoft.com>
2020-06-17 01:55:18 -0700

    [Loc] Presubmit#3: LocV2 Checking the shape of localization API calls
    Checking the shape of localization API calls, for example:
    if the node is Common.i18n.getLocalizedString, the first argument should be the string instance function 'str_', the second argument should reference the UIStrings object like 'UIStrings.url'
    The code should look like:
    Common.i18n.getLocalizedString(str_, UIStrings.url);
    
    Bug: 941561
    Change-Id: I1336867b06dd727f5164e6b1d7ac7ae6ed6ea446

--- scripts/localization/check_localizability.js
+++ scripts/localization/check_localizability.js
@@ -203,6 +203,9 @@ function analyzeNode(parentNode, node, filePath, errors) {
   if (locVersion === 1) {
     runLocV1APIChecks(locCase, node, parentNode, code, errors, filePath);
   }
+  if (locVersion === 2) {
+    runLocV2APIChecks(locCase, node, errors, filePath);
+  }
   for (const key of objKeys) {
     // recursively parse all the child nodes
     analyzeNode(node, node[key], filePath, errors);
@@ -262,6 +265,47 @@ function runLocV1APIChecks(locCase, node, parentNode, code, errors, filePath) {
     default: {
       // String concatenation to localization call(s) should be changed
       checkConcatenation(parentNode, node, filePath, errors);
+      break;
+    }
+  }
+}
+
+function runLocV2APIChecks(locCase, node, errors, filePath) {
+  switch (locCase) {
+    case 'Common.i18n.getLocalizedString':
+    case 'Common.i18n.getFormatLocalizedString': {
+      // For example,
+      // node: Common.i18n.getLocalizedString(str_, UIStrings.url)
+      // firstArg : str_
+      // secondArg : UIStrings.url
+      const firstArg = node.arguments[0];
+      if (firstArg.type !== espreeTypes.IDENTIFIER || firstArg.name !== 'str_') {
+        addError(
+            `${localizationUtils.getRelativeFilePathFromSrc(filePath)}${
+                localizationUtils.getLocationMessage(node.loc)}: first argument should be 'str_'`,
+            errors);
+      }
+      const secondArg = node.arguments[1];
+      const isCallingUIStringsObject = (secondArg.object && secondArg.object.name === 'UIStrings');
+      const isPropertyAnIdentifier = (secondArg.property && secondArg.property.type === espreeTypes.IDENTIFIER);
+      if (secondArg.type !== espreeTypes.MEMBER_EXPR || !isCallingUIStringsObject || !isPropertyAnIdentifier) {
+        addError(
+            `${localizationUtils.getRelativeFilePathFromSrc(filePath)}${
+                localizationUtils.getLocationMessage(
+                    node.loc)}: second argument should reference an identifier in UIStrings object`,
+            errors);
+      }
+      break;
+    }
+    case 'UIStrings': {
+      break;
+    }
+    default: {
+      addError(
+          `${localizationUtils.getRelativeFilePathFromSrc(filePath)}${
+              localizationUtils.getLocationMessage(
+                  node.loc)}: Localization API error detected. Please report the problem.`,
+          errors);
       break;
     }
   }
