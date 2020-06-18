9908f2102a3c30dc3ca9bb9b8922ae3c3a49625d
Patrick Brosset <patrick.brosset@microsoft.com>
2020-06-18 02:39:32 -0700

    Split computed-pane-properties_tests.ts to avoid timeouts
    This test has been failing in CI environments because of timeout issues.
    Because this test was doing 2 things: testing a property on one node and
    then another property on another node, it's simple enough to split this
    in 2 test cases, and therefore avoid exceeding the per-test-case
    timeout.
    
    Bug: 1096439
    Change-Id: Ie7c94ad9db4a34d86dbc3f443e68611739b0531b

--- test/e2e/elements/computed-pane-properties_test.ts
+++ test/e2e/elements/computed-pane-properties_test.ts
@@ -4,43 +4,69 @@
 
 import {assert} from 'chai';
 import {describe, it} from 'mocha';
+import {Page} from 'puppeteer';
 
 import {getBrowserAndPages, goToResource} from '../../shared/helper.js';
 import {focusElementsTree, getAllPropertiesFromComputedPane, getContentOfComputedPane, navigateToSidePane, waitForComputedPaneChange, waitForElementsComputedSection} from '../helpers/elements-helpers.js';
 
 describe('The Computed pane', async () => {
-  it('can display the CSS properties of the selected element', async () => {
-    const {frontend} = getBrowserAndPages();
+  // The test cases here are very similar, a node gets selected, and some styles in the computed pane are asserted.
+  // This is why they are listed in an array below, so that we don't have to repeat the testing logic every time.
+  // Also, this makes sure only one node is tested per test case, which keeps the tests running fast and avoiding
+  // timeouts.
+  const TESTS = [
+    {
+      description: 'can display a simple color property value',
+      resource: 'elements/simple-styled-page.html',
+      selectNode: async (frontend: Page) => {
+        // Select the H1 element by pressing down once.
+        await frontend.keyboard.press('ArrowDown');
+      },
+      expectedPropertyCount: 10,
+      assertProperties: [{
+        name: 'color',
+        value: 'rgb(255, 0, 102)',
+      }],
+    },
+    {
+      description: 'can display a simple background-color property value',
+      resource: 'elements/simple-styled-page.html',
+      selectNode: async (frontend: Page) => {
+        // Select the H2 element by pressing down twice.
+        await frontend.keyboard.press('ArrowDown');
+        await frontend.keyboard.press('ArrowDown');
+      },
+      expectedPropertyCount: 11,
+      assertProperties: [{
+        name: 'background-color',
+        value: 'rgb(255, 215, 0)',
+      }],
+    },
+  ];
 
-    await goToResource('elements/simple-styled-page.html');
-    await navigateToSidePane('Computed');
-    await waitForElementsComputedSection();
+  for (const {description, resource, selectNode, expectedPropertyCount, assertProperties} of TESTS) {
+    it(description, async () => {
+      const {frontend} = getBrowserAndPages();
 
-    // Note that navigating to the computed pane moved focus away from the elements pane. Restore it.
-    await focusElementsTree();
+      await goToResource(resource);
+      await navigateToSidePane('Computed');
+      await waitForElementsComputedSection();
 
-    // Select the H1 element and wait for the computed pane to change.
-    let content = await getContentOfComputedPane();
-    await frontend.keyboard.press('ArrowDown');
-    await waitForComputedPaneChange(content);
+      // Note that navigating to the computed pane moved focus away from the elements pane. Restore it.
+      await focusElementsTree();
 
-    const h1Properties = await getAllPropertiesFromComputedPane();
-    assert.strictEqual(h1Properties.length, 10, 'There should be 10 computed properties on the H1 element');
+      const content = await getContentOfComputedPane();
+      await selectNode(frontend);
+      await waitForComputedPaneChange(content);
 
-    const colorProperty = h1Properties.find(property => property && property.name === 'color');
-    assert.isTrue(!!colorProperty, 'H1 element should have a color computed property');
-    assert.strictEqual(colorProperty && colorProperty.value, 'rgb(255, 0, 102)');
+      const properties = await getAllPropertiesFromComputedPane();
+      assert.strictEqual(properties.length, expectedPropertyCount, 'The right number of properties was found');
 
-    // Select the H2 element by pressing down again.
-    content = await getContentOfComputedPane();
-    await frontend.keyboard.press('ArrowDown');
-    await waitForComputedPaneChange(content);
-
-    const h2Properties = await getAllPropertiesFromComputedPane();
-    assert.strictEqual(h2Properties.length, 11, 'There should be 11 computed properties on the H2 element');
-
-    const backgroundProperty = h2Properties.find(property => property && property.name === 'background-color');
-    assert.isTrue(!!backgroundProperty, 'H2 element should have a background-color computed property');
-    assert.strictEqual(backgroundProperty && backgroundProperty.value, 'rgb(255, 215, 0)');
-  });
+      for (const {name, value} of assertProperties) {
+        const property = properties.find(property => property && property.name === name);
+        assert.isTrue(!!property, `The element has the ${name} computed property`);
+        assert.strictEqual(property && property.value, value, `The property has the right value: ${value}`);
+      }
+    });
+  }
 });
