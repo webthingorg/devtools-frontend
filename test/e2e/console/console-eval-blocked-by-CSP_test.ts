66922c6da44f1202251b7350ed7c1d310556c894
Peter Marshall <petermarshall@chromium.org>
2020-06-19 14:58:28 +0200

    Don't bypass CSP restrictions on eval from the console
    Previously the console would allow evals even when the target page
    blocked evals via Content Security Policy. This made it hard to debug
    CSP-related issues on the page because users couldn't reproduce CSP
    eval errors in the console.
    
    Use the new flag to Runtime.evaluate() that will cause CSP failures
    for eval from the console, if CSP on the page prevents unsafe eval.
    
    Add a test.
    
    Bug: chromium:1084558
    Change-Id: I916f36a2c02fbc1843de50851b72b54d2a3a7b8c

---
+++ test/e2e/console/console-eval-blocked-by-CSP_test.ts
@@ -0,0 +1,52 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {describe, it} from 'mocha';
+import {click, getBrowserAndPages, goToResource, typeText} from '../../shared/helper.js';
+import {CONSOLE_TAB_SELECTOR, focusConsolePrompt} from '../helpers/console-helpers.js';
+
+describe('The Console Tab', async function() {
+  it('eval in console succeeds for pages with no CSP', async () => {
+    const {frontend} = getBrowserAndPages();
+    await click(CONSOLE_TAB_SELECTOR);
+    await focusConsolePrompt();
+
+    await typeText('eval("1+1");');
+    await frontend.keyboard.press('Enter');
+
+    await frontend.waitForFunction(() => {
+      return document.querySelectorAll('.console-user-command-result').length === 1;
+    });
+
+    const evaluateResult = await frontend.evaluate(() => {
+      return document.querySelectorAll('.console-user-command-result')[0].textContent;
+    });
+
+    assert.strictEqual(evaluateResult, '2', 'Eval result was not correct');
+  });
+
+  it('eval in console fails for pages with CSP that blocks eval', async () => {
+    await goToResource('console/CSP-blocks-eval.html');
+
+    const {frontend} = getBrowserAndPages();
+    await click(CONSOLE_TAB_SELECTOR);
+    await focusConsolePrompt();
+
+    await typeText('eval("1+1");');
+    await frontend.keyboard.press('Enter');
+
+    await frontend.waitForFunction(() => {
+      return document.querySelectorAll('.console-user-command-result').length === 1;
+    });
+
+    const evaluateResult = await frontend.evaluate(() => {
+      return document.querySelectorAll('.console-user-command-result')[0].textContent;
+    });
+
+    assert.include(
+        evaluateResult || '', '\'unsafe-eval\' is not an allowed source of script',
+        'Didn\'t find expected CSP error message');
+  });
+});
