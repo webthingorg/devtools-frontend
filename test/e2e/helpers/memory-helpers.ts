79036da1a7d2629ac7af7a218f70b3962f4fd7e4
Jose Leal Chapa <joselea@microsoft.com>
2020-06-22 18:58:00 -0700

    DevTools personalization - Make Memory tab closeable
    To be careful of not introducing breaking changes the closeable tabs work will be staggered in different CLs.
    This CL targets the Memory panel, and includes new e2e tests that validate:
    - the panel is open by default
    - the panel exists under More tools
    - the panel can be opened from the command menu
    - the panel can be opened with the query param "panel"
    - closing the tab doesn't crash the devtools
    - reopening devtools persist the newly closed tab
    
    Explainer:
    https://docs.google.com/document/d/1TC1FxzSgRIaXmChpjL05T7sZuduRos3IDPDG5vCS1Oc/edit?usp=sharing
    
    Bug: 1074732
    Change-Id: I1fff522048775c78f7f7990931ce6e77645e2faf

--- test/e2e/helpers/memory-helpers.ts
+++ test/e2e/helpers/memory-helpers.ts
@@ -4,18 +4,21 @@
 
 import {assert} from 'chai';
 
-import {$$, click, goToResource, waitFor, waitForNone} from '../../shared/helper.js';
+import {$$, click, closePanelTab, getBrowserAndPages, goToResource, typeText, waitFor, waitForNone} from '../../shared/helper.js';
+
+import {openCommandMenu} from './quick_open-helpers.js';
+import {openPanelViaMoreTools} from './settings-helpers.js';
 
 const NEW_HEAP_SNAPSHOT_BUTTON = 'button[aria-label="Take heap snapshot"]';
 const MEMORY_PANEL_CONTENT = 'div[aria-label="Memory panel"]';
 const PROFILE_TREE_SIDEBAR = 'div.profiles-tree-sidebar';
-export const MEMORY_TAB_ID = '#tab-heap_profiler';
+const MEMORY_PANEL_TITLE = 'Memory';
+export const MEMORY_TAB_SELECTOR = '#tab-heap_profiler';
 
 export async function navigateToMemoryTab() {
   await goToResource('memory/default.html');
-  await click(MEMORY_TAB_ID);
-  await waitFor(MEMORY_PANEL_CONTENT);
-  await waitFor(PROFILE_TREE_SIDEBAR);
+  await click(MEMORY_TAB_SELECTOR);
+  await memoryPanelContentIsLoaded();
 }
 
 export async function takeHeapSnapshot() {
@@ -36,4 +39,37 @@ export async function getCountOfDataGridRows(selector: string) {
   const grid = await waitFor(selector);
   const dataGridNodes = await $$('.data-grid-data-grid-node', grid);
   return await dataGridNodes.evaluate(nodes => nodes.length);
+}
+
+export async function memoryTabExists() {
+  await waitFor(MEMORY_TAB_SELECTOR);
+}
+
+export async function memoryTabDoesNotExist() {
+  await waitForNone(MEMORY_TAB_SELECTOR);
+}
+
+export async function memoryPanelContentIsLoaded() {
+  await waitFor(MEMORY_PANEL_CONTENT);
+  await waitFor(PROFILE_TREE_SIDEBAR);
+}
+
+export async function closeMemoryTab() {
+  await closePanelTab(MEMORY_TAB_SELECTOR);
+  await memoryTabDoesNotExist();
+}
+
+export async function openMemoryPanelFromMoreTools() {
+  await openPanelViaMoreTools(MEMORY_PANEL_TITLE);
+  await memoryTabExists();
+  await memoryPanelContentIsLoaded();
+}
+
+export async function openMemoryPanelFromCommandMenu() {
+  const {frontend} = getBrowserAndPages();
+  await openCommandMenu();
+  await typeText('Show Memory');
+  await frontend.keyboard.press('Enter');
+  await memoryTabExists();
+  await memoryPanelContentIsLoaded();
 }
--- test/e2e/memory/memory_test.ts
+++ test/e2e/memory/memory_test.ts
@@ -4,7 +4,8 @@
 
 import {describe, it} from 'mocha';
 
-import {navigateToMemoryTab, takeHeapSnapshot, waitForHeapSnapshotData} from '../helpers/memory-helpers.js';
+import {reloadDevTools} from '../../shared/helper.js';
+import {closeMemoryTab, memoryPanelContentIsLoaded, memoryTabDoesNotExist, memoryTabExists, navigateToMemoryTab, openMemoryPanelFromCommandMenu, openMemoryPanelFromMoreTools, takeHeapSnapshot, waitForHeapSnapshotData} from '../helpers/memory-helpers.js';
 
 describe('The Memory Panel', async () => {
   it('Loads content', async () => {
@@ -17,5 +18,34 @@ describe('The Memory Panel', async () => {
     await waitForHeapSnapshotData();
     await takeHeapSnapshot();
     await waitForHeapSnapshotData();
+  });
+
+  it('is open by default when devtools initializes', async () => {
+    await navigateToMemoryTab();
+  });
+
+  it('closes without crashing and stays closed after reloading tools', async () => {
+    await closeMemoryTab();
+    await reloadDevTools();
+    await memoryTabDoesNotExist();
+  });
+
+  it('appears under More tools after being closed', async () => {
+    await closeMemoryTab();
+    await openMemoryPanelFromMoreTools();
+    await reloadDevTools({selectedPanel: {name: 'memory'}});
+    await memoryTabExists();
+  });
+
+  it('can be opened from command menu after being closed', async () => {
+    await closeMemoryTab();
+    await openMemoryPanelFromCommandMenu();
+  });
+
+  it('opens if the query param "panel" is set', async () => {
+    await closeMemoryTab();
+    await reloadDevTools({queryParams: {panel: 'heap_profiler'}});
+    await memoryTabExists();
+    await memoryPanelContentIsLoaded();
   });
 });
