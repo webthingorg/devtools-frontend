11350212d8ab19357b2a8bab21fe0c6d00644fb8
Brandon Goddard <brgoddar@microsoft.com>
2020-06-23 13:07:36 -0700

    Grid: Add telemetry to track Grid settings changes
    This cl adds an enumerated histogram to track changes to grid settings.
    
    Corresponding backend CL: https://crrev.com/c/2261137
    
    Bug: 1047356
    
    Change-Id: Iee8dfb74130d206c596eeb6ba5347b66b2558e55

--- test/e2e/host/user-metrics_test.ts
+++ test/e2e/host/user-metrics_test.ts
@@ -9,7 +9,7 @@ import * as puppeteer from 'puppeteer';
 import {$, click, enableExperiment, getBrowserAndPages, platform, reloadDevTools, waitFor} from '../../shared/helper.js';
 import {clickToggleButton, selectDualScreen, startEmulationWithDualScreenFlag} from '../helpers/emulation-helpers.js';
 import {closeSecurityTab, navigateToSecurityTab} from '../helpers/security-helpers.js';
-import {openPanelViaMoreTools} from '../helpers/settings-helpers.js';
+import {openPanelViaMoreTools, openSettingsTab} from '../helpers/settings-helpers.js';
 
 interface UserMetric {
   name: string;
@@ -38,6 +38,7 @@ declare global {
     __issuesPanelOpenedFrom: (evt: Event) => void;
     __keybindSetSettingChanged: (evt: Event) => void;
     __dualScreenDeviceEmulated: (evt: Event) => void;
+    __gridSettingChanged: (evt: Event) => void;
     Host: {UserMetrics: UserMetrics; userMetrics: {actionTaken(name: number): void;}};
     UI: {inspectorView: {_showDrawer(show: boolean): void; showView(name: string): void;}};
   }
@@ -85,6 +86,11 @@ async function beginCatchEvents(frontend: puppeteer.Page) {
       window.__caughtEvents.push({name: 'DevTools.DualScreenDeviceEmulated', value: customEvt.detail.value});
     };
 
+    window.__gridSettingChanged = (evt: Event) => {
+      const customEvt = evt as CustomEvent;
+      window.__caughtEvents.push({name: 'DevTools.GridSettingChanged', value: customEvt.detail.value});
+    };
+
     window.__caughtEvents = [];
     window.__beginCatchEvents = () => {
       window.addEventListener('DevTools.PanelShown', window.__panelShown);
@@ -95,6 +101,7 @@ async function beginCatchEvents(frontend: puppeteer.Page) {
       window.addEventListener('DevTools.IssuesPanelOpenedFrom', window.__issuesPanelOpenedFrom);
       window.addEventListener('DevTools.KeybindSetSettingChanged', window.__keybindSetSettingChanged);
       window.addEventListener('DevTools.DualScreenDeviceEmulated', window.__dualScreenDeviceEmulated);
+      window.addEventListener('DevTools.GridSettingChanged', window.__gridSettingChanged);
     };
 
     window.__endCatchEvents = () => {
@@ -106,6 +113,7 @@ async function beginCatchEvents(frontend: puppeteer.Page) {
       window.removeEventListener('DevTools.IssuesPanelOpenedFrom', window.__issuesPanelOpenedFrom);
       window.removeEventListener('DevTools.KeybindSetSettingChanged', window.__keybindSetSettingChanged);
       window.removeEventListener('DevTools.DualScreenDeviceEmulated', window.__dualScreenDeviceEmulated);
+      window.removeEventListener('DevTools.GridSettingChanged', window.__gridSettingChanged);
     };
 
     window.__beginCatchEvents();
@@ -361,6 +369,33 @@ describe('User Metrics', () => {
           histogramName: 'DevTools.Launch.Timeline',
           panelName: 'timeline',
         },
+      },
+    ]);
+  });
+
+  it('dispatches an event when the CSS Grid overlay setting changes', async () => {
+    const {frontend} = getBrowserAndPages();
+    await enableExperiment('cssGridFeatures');
+    // enableExperiment reloads the DevTools and removes our listeners
+    await beginCatchEvents(frontend);
+
+    await openSettingsTab('Preferences');
+
+    const gridBorderSelect = await $('[aria-label="Grid"] p select') as puppeteer.ElementHandle<HTMLSelectElement>;
+    gridBorderSelect.select('solid');
+
+    await assertCapturedEvents([
+      {
+        name: 'DevTools.ActionTaken',
+        value: 37,  // Settings opened from menu
+      },
+      {
+        name: 'DevTools.PanelShown',
+        value: 29,  // settings-preferences
+      },
+      {
+        name: 'DevTools.GridSettingChanged',
+        value: 2,  // showGridBorder.solid
       },
     ]);
   });
