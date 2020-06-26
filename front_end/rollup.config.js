4471a6bc53de71bc269d72dcccb5bf0bf2c8320e
Tim van der Lippe <tvanderlippe@chromium.org>
2020-06-26 17:01:33 +0100

    Extract external module computation into rollup.config.js
    This allows us to remove the pre-computation of the external modules
    from the Python script, into the Rollup module. This is a necessary
    change for the future, where we can define an entrypoint in any
    location. This could be in a folder which is not a direct
    sub-folder of `front_end`.
    
    To do so, we operate on the assumption that the directory that the
    input file is in, is the entrypoint. If you resolve an import that
    is in that directory or in a subdirectory, we treat it as internal.
    If it is outside of that folder, we treat it as external.
    
    This largely mimics the implementation of the ESLint rule for
    ES modules.
    
    R=aerotwist@chromium.org,jacktfranklin@chromium.org
    
    Also-By: janscheffler@chromium.org
    Also-By: alexrudenko@chromium.org
    Bug: 1098730
    Change-Id: I416b4c9cd9997b9fc2a50aea41077c41fea59af0

--- front_end/rollup.config.js
+++ front_end/rollup.config.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as path from 'path';
+
 // eslint-disable-next-line import/no-default-export
 export default {
   treeshake: false,
@@ -9,4 +11,58 @@ export default {
   output: {
     format: 'esm',
   },
+  plugins:
+      [
+        (() => {
+          let entrypointDirectory;
+          return {
+            name: 'devtools-plugin',
+            buildStart(options) {
+              let inputFile = options.input;
+              if (Array.isArray(inputFile)) {
+                if (inputFile.length !== 1) {
+                  throw new Error(`Invalid multiple inputs: ${JSON.stringify(inputFile)}`);
+                }
+                inputFile = inputFile[0];
+              } else {
+                throw new Error(`Invalid input file type specified: ${JSON.stringify(options.input)}`);
+              }
+              const absoluteInputPath = path.normalize(path.join(process.cwd(), inputFile));
+              entrypointDirectory = path.dirname(absoluteInputPath);
+            },
+            resolveId(source, importer) {
+              if (!importer) {
+                return null;
+              }
+              const currentDirectory = path.normalize(path.dirname(importer));
+              const importedFilelocation = path.normalize(path.join(currentDirectory, source));
+              const importedFileDirectory = path.dirname(importedFilelocation);
+
+              // Generated files are part of other directories, as they are only imported once
+              if (path.basename(importedFileDirectory) === 'generated') {
+                return null;
+              }
+
+              // We currently still have to import third_party packages and put them in separate
+              // folders with the `module.json` files.
+              //
+              // Note that we can't do a simple check for only `third_party`, as in Chromium
+              // our full path is `third_party/devtools-frontend/src/`, which thus *always*
+              // includes third_party. It also not possible to use the current directory
+              // as a check for the import, as the import will be different in Chromium and
+              // would therefore not match the path of `__dirname`.
+              if (importedFileDirectory.includes(path.join('front_end', 'third_party'))) {
+                return null;
+              }
+
+              const isExternal = !importedFileDirectory.startsWith(entrypointDirectory);
+
+              return {
+                id: importedFilelocation,
+                external: isExternal,
+              };
+            }
+          };
+        })(),
+      ]
 };
