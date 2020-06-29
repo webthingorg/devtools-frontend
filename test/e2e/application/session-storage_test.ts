5bb8c20cdd2fc0c1d74103e3496e55e2a590faf0
Jan Scheffler <janscheffler@chromium.org>
2020-06-29 10:46:54 +0200

    [Cookies] Only delete visible cookies
    This patch changes the default behaviour of the clear all button
    in the cookies table to only delete the ones that are currently
    visible due to filtering instead of all of them.
    
    Fixed: chromium:978059
    Change-Id: Ifbc969a9ccc271b34b91926898277dae3397c491

--- test/e2e/application/session-storage_test.ts
+++ test/e2e/application/session-storage_test.ts
@@ -6,8 +6,7 @@ import {assert} from 'chai';
 import {describe, it} from 'mocha';
 
 import {getBrowserAndPages, step} from '../../shared/helper.js';
-
-import {doubleClickSourceTreeItem, getDataGridData, navigateToApplicationTab} from '../helpers/application-helpers.js';
+import {doubleClickSourceTreeItem, getStorageItemsData, navigateToApplicationTab} from '../helpers/application-helpers.js';
 
 const SESSION_STORAGE_SELECTOR = '[aria-label="Session Storage"]';
 const DOMAIN_SELECTOR = `${SESSION_STORAGE_SELECTOR} + ol > [aria-label="http://localhost:8090"]`;
@@ -26,7 +25,7 @@ describe('The Application Tab', async () => {
     });
 
     await step('check that storage data values are correct', async () => {
-      const dataGridRowValues = await getDataGridData('.storage-view table', ['key', 'value']);
+      const dataGridRowValues = await getStorageItemsData(['key', 'value']);
       assert.deepEqual(dataGridRowValues, [
         {
           key: 'firstKey',
