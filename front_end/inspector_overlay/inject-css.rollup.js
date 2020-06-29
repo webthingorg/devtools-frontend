b8c53997e04eef20a7748bcb25de511e2035ef66
Alex Rudenko <alexrudenko@chromium.org>
2020-06-29 08:59:12 +0200

    Extract CSS to separate files in inspector_overlay
    This CL moves inline CSS to separate files. The styles will be
    injected using a new Rollup plugin. The sources will not be directly
    executable and, therefore, debug files have to import from
    `out/Default/resources/inspector_overlay` instead of the source folders.
    To see the changes one has to rebuild using `autoninja -C out/Default`.
    Since the build step would be required after the full transition to
    TypeScript, it seems appropriate to leverage the build step to allow
    for more maintainable approach with regards to CSS files.
    
    Change-Id: I5b1b8a40a76c9c51aa9ca95f70a377b8488caa2d

---
+++ front_end/inspector_overlay/inject-css.rollup.js
@@ -0,0 +1,27 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
+
+module.exports = function injectCSS() {
+  let styleId = 0;
+  return {
+    name: 'injectCSS',
+    transform(code, id) {
+      if (id.endsWith('.css')) {
+        styleId++;
+        return {
+          code: `
+            const styleTag${styleId} = document.createElement('style');
+            styleTag${styleId}.textContent = ${JSON.stringify(code)};
+            document.head.append(styleTag${styleId});
+          `,
+          map: null
+        };
+      }
+      return;
+    }
+  };
+};
