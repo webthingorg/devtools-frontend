f5eadef713e9ebc8b38a5478ef06c77b32e7fc75
Simon Zünd <szuend@chromium.org>
2020-07-02 10:07:48 +0200

    [lit-html] Allow usage of lit-html from JavaScript land
    This CL adds a new LitHtmlJs module, that can be imported similar to
    other modules:
    
    import * as LitHtmlJs from '../lit_html_js/lit_html_js.js';
    
    Shortcuts for 'html' and 'render' are recommended afterwards:
    
    const html = LitHtmlJs.html;
    const render = LitHtmlJs.render;
    
    R=tvanderlippe@chromium.org
    
    Change-Id: I7d0326fd5b7a411d8f6a9d39fb34d800362c0325

---
+++ front_end/lit_html_js/lit_html_js.js
@@ -0,0 +1,7 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html, render} from '../third_party/lit-html/package/lit-html.js';
+
+export {html, render};
