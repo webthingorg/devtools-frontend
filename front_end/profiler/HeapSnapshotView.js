284c4679d7a557fae5cb7b00277b8dbbea4dfde7
Brian Cui <brcui@microsoft.com>
2020-07-07 16:02:39 -0700

    Settings Search Initial Experiment
    This CL adds a new experiment, Settings Search, which adds a new search
    box to the Settings pane for filtering options.
    
    This initial CL adds filter support for the Preferences and Experiments
    tabs. As the user types in the search box, controls are filtered
    in real time and the relevant keywords are highlighted.
    
    Future CLs will enable filtering for other tabs.
    
    To try the experiment, visit Settings -> Experiments and enable the
    Settings Search checkbox. Then, reload DevTools, and open Settings.
    
    One-pager: https://docs.google.com/document/d/1nRh5Y7JqNOowkOqpbYfJMbSZ1rqSHhRh_7HqzqhuHwY/edit?ts=5e99dbbf
    
    Video Demo: https://i.imgur.com/kJtfzFj.mp4
    
    Bug: 1101117
    Change-Id: I53b205b44df546a73bcdedd8e7bf2997b93486af

--- front_end/profiler/HeapSnapshotView.js
+++ front_end/profiler/HeapSnapshotView.js
@@ -1169,10 +1169,10 @@ export class HeapSnapshotProfileType extends ProfileType {
    * @return {?Element}
    */
   customContent() {
-    const checkboxSetting = UI.SettingsUI.createSettingCheckbox(
+    const checkboxSetting = UI.SettingsUI.SettingElement.createCheckbox(
         ls
         `Treat global objects as roots (recommended, unchecking this exposes internal nodes and introduces excessive detail, but might help debugging cycles in retaining paths)`,
-        this._treatGlobalObjectsAsRoots, true);
+        this._treatGlobalObjectsAsRoots, true).element();
         this._customContent = /** @type {!UI.UIUtils.CheckboxLabel} */ (checkboxSetting);
         const showOptionToNotTreatGlobalObjectsAsRoots =
             Root.Runtime.experiments.isEnabled('showOptionToNotTreatGlobalObjectsAsRoots');
@@ -1389,8 +1389,11 @@ export class TrackingHeapSnapshotProfileType extends HeapSnapshotProfileType {
    * @return {?Element}
    */
   customContent() {
-    const checkboxSetting = UI.SettingsUI.createSettingCheckbox(
-        ls`Record allocation stacks (extra performance overhead)`, this._recordAllocationStacksSetting, true);
+    const checkboxSetting =
+        UI.SettingsUI.SettingElement
+            .createCheckbox(
+                ls`Record allocation stacks (extra performance overhead)`, this._recordAllocationStacksSetting, true)
+            .element();
     this._customContent = /** @type {!UI.UIUtils.CheckboxLabel} */ (checkboxSetting);
     return checkboxSetting;
   }
