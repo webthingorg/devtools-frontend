27b7e67d91d030ad4b88dcc89c3102a91cebf850
Olivia Flynn <t-olfly@microsoft.com>
2020-07-08 12:45:24 -0700

    WIP: Source Order Viewer UI
    This CL adds:
    - UI to turn the source order view on/off for an element
    - Routing of node information from UI -> Overlay Model -> CDP
    - An experiment dedicated to this tool
    
    Gif: https://imgur.com/a/8bduCOY
    Corresponding back-end changes are on Gerrit, #2250504
    
    This CL is part of creating a Source Order Viewer, which was identified
    as a customer ask on The Web We Want
    The Web We Want submission: https://webwewant.fyi/wants/64
    Explainer: https://docs.google.com/document/d/1t33UTIpW46MGor70W8mdBJV
    Rio8eo3p76w3FH8MjV7A/edit?usp=sharing
    
    Change-Id: Ic494fa3c50bfb55d1837d3903c8124f89089acc2

---
+++ front_end/accessibility/SourceOrderView.js
@@ -0,0 +1,69 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as SDK from '../sdk/sdk.js';  // eslint-disable-line no-unused-vars
+import * as UI from '../ui/ui.js';
+
+import {AccessibilitySubPane} from './AccessibilitySubPane.js';
+
+export class SourceOrderPane extends AccessibilitySubPane {
+  constructor() {
+    super(ls`Source Order Viewer`);
+
+    this._noNodeInfo = this.createInfo(ls`No source order information avaiable`);
+    this._checked = false;
+    this._checkboxLabel = UI.UIUtils.CheckboxLabel.create(/* title */ ls`Show source order`, /* checked */ false);
+    this._checkboxElement = this._checkboxLabel.checkboxElement;
+
+    this._checkboxLabel.style.margin = '5px';
+    this._checkboxElement.addEventListener('click', this._checkboxClicked.bind(this), false);
+    this.element.appendChild(this._checkboxLabel);
+  }
+
+  /**
+     * @override
+     * @param {?SDK.DOMModel.DOMNode} node
+     */
+  async setNode(node) {
+    const persistCheckbox = this._checkboxElement.checked;
+    this._checkboxElement.checked = false;
+    this._checkboxClicked();
+    super.setNode(node);
+    if (!this._node) {
+      this._overlayModel = null;
+      return;
+    }
+
+    let foundSourceOrder = false;
+    if (this._node.childNodeCount() > 0) {
+      if (!this._node.children()) {
+        await this._node.getSubtree(1, false);
+      }
+      const children = /** @type {!SDK.DOMModel.DOMNode[]} */ (this._node.children());
+      foundSourceOrder = children.some(child => child.nodeType() === Node.ELEMENT_NODE);
+    }
+
+    this._noNodeInfo.classList.toggle('hidden', foundSourceOrder);
+    this._checkboxLabel.classList.toggle('hidden', !foundSourceOrder);
+    if (foundSourceOrder) {
+      this._overlayModel = this._node.domModel().overlayModel();
+      this._checkboxElement.checked = persistCheckbox;
+      this._checkboxClicked();
+    } else {
+      this._overlayModel = null;
+    }
+  }
+
+  _checkboxClicked() {
+    if (!this._node || !this._overlayModel) {
+      return;
+    }
+
+    if (this._checkboxElement.checked) {
+      this._overlayModel.highlightSourceOrderInOverlay(this._node);
+    } else {
+      this._overlayModel.hideSourceOrderInOverlay();
+    }
+  }
+}
