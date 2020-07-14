00738231d1293ec70cda32ff194ca5506ffe4af8
Tim van der Lippe <tvanderlippe@chromium.org>
2020-07-14 17:21:14 +0100

    Let test_runners use modules instead of scripts
    These should be equivalent, with the difference that they are now
    using dynamic imports to load, rather than evals.
    
    R=aerotwist@chromium.org
    
    Bug: 1105476
    Change-Id: Iacc1d7bc8666dc16f77eb0d6dc820016d5f52dd5

---
+++ front_end/axe_core_test_runner/axe_core_test_runner.js
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../third_party/axe-core/axe.js';
+import './AxeCoreTestRunner.js';
