5f962d166f090c9b05acbcfc489468c864a3b600
Changhao Han <changhaohan@chromium.org>
2020-07-17 00:26:06 +0200

    Add e2e test for computed tab's "Show all" functionality
    Bug: chromium:1106251
    Change-Id: I17054651a1ad4d83dd9bd6a0f90e38c3857796e4

--- test/e2e/elements/computed-pane-properties_test.ts
+++ test/e2e/elements/computed-pane-properties_test.ts
@@ -5,22 +5,27 @@
 import {assert} from 'chai';
 import {describe, it} from 'mocha';
 
-import {getBrowserAndPages, goToResource} from '../../shared/helper.js';
+import {$, click, getBrowserAndPages, goToResource} from '../../shared/helper.js';
 import {focusElementsTree, getAllPropertiesFromComputedPane, getContentOfComputedPane, navigateToSidePane, waitForComputedPaneChange, waitForElementsComputedSection} from '../helpers/elements-helpers.js';
 
-describe('The Computed pane', async () => {
-  it('can display the CSS properties of the selected element', async () => {
-    const {frontend} = getBrowserAndPages();
+const COMPUTED_STYLES_PANEL_SELECTOR = '[aria-label="Computed panel"]';
+const COMPUTED_STYLES_SHOW_ALL_SELECTOR = '[aria-label="Show all"]';
 
+describe('The Computed pane', async () => {
+  beforeEach(async function() {
     await goToResource('elements/simple-styled-page.html');
     await navigateToSidePane('Computed');
     await waitForElementsComputedSection();
+    await focusElementsTree();
+  });
+
+  it('can display the CSS properties of the selected element', async () => {
+    const {frontend} = getBrowserAndPages();
 
     // Note that navigating to the computed pane moved focus away from the elements pane. Restore it.
-    await focusElementsTree();
+    const content = await getContentOfComputedPane();
 
     // Select the H1 element and wait for the computed pane to change.
-    let content = await getContentOfComputedPane();
     await frontend.keyboard.press('ArrowDown');
     await waitForComputedPaneChange(content);
 
@@ -32,7 +37,6 @@ describe('The Computed pane', async () => {
     assert.strictEqual(colorProperty && colorProperty.value, 'rgb(255, 0, 102)');
 
     // Select the H2 element by pressing down again.
-    content = await getContentOfComputedPane();
     await frontend.keyboard.press('ArrowDown');
     await waitForComputedPaneChange(content);
 
@@ -42,5 +46,22 @@ describe('The Computed pane', async () => {
     const backgroundProperty = h2Properties.find(property => property && property.name === 'background-color');
     assert.isTrue(!!backgroundProperty, 'H2 element should have a background-color computed property');
     assert.strictEqual(backgroundProperty && backgroundProperty.value, 'rgb(255, 215, 0)');
+  });
+
+  it('can display inherited CSS properties of the selected element', async () => {
+    const {frontend} = getBrowserAndPages();
+    const computedPanel = await $(COMPUTED_STYLES_PANEL_SELECTOR);
+    const content = await getContentOfComputedPane();
+
+    // Select the H1 element and wait for the computed pane to change.
+    await frontend.keyboard.press('ArrowDown');
+    const showAllButton = await $(COMPUTED_STYLES_SHOW_ALL_SELECTOR, computedPanel);
+    await click(showAllButton);
+    await waitForComputedPaneChange(content);
+
+    const allH1Properties = await getAllPropertiesFromComputedPane();
+    const alignContentProperty = allH1Properties.find(property => property && property.name === 'align-content');
+    assert.isTrue(!!alignContentProperty, 'H1 element should inherit the align-content computed property');
+    assert.strictEqual(alignContentProperty && alignContentProperty.value, 'normal');
   });
 });
