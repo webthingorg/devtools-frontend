aa83496b76808cb9980a65673d2520b2c8195c35
Ilya Sherman <isherman@chromium.org>
2020-07-15 17:38:03 -0700

    Add a utility for parsing the X-Client-Data header
    This header contains serialized and base64-encoded protocol buffers.
    This utility simply base64-decodes the serialized data, and then
    parses the proto via compiled JSPB parsing.
    
    See https://chromium-review.googlesource.com/c/chromium/src/+/2301121
    for the parser implementation. The "gen" code in this CL is the compiled
    output from that CL.
    
    Bug: 1103854
    Change-Id: Ib0e04ba2ac02cfb88200054041fc32f86fd2ede8

---
+++ test/unittests/scripts/component_bridges/client_variations_proto_test.js
@@ -0,0 +1,23 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {assert} from 'chai';
+import {parseClientVariations} from '../../../../scripts/component_bridges/client_variations_proto.js';
+
+describe('parseClientVariations', () => {
+  it('return empty lists for unparseable text', () => {
+    const result = parseClientVariations('gibberish');
+    assert.deepEqual(result, {
+      'variationIds': [],
+      'triggerVariationIds': [],
+    });
+  });
+
+  it('parses a valid serialized proto', () => {
+    const result = parseClientVariations('CG8I3gEIzQIYvAMYqwQ=');
+    assert.deepEqual(result, {
+      'variationIds': ['111', '222', '333'],
+      'triggerVariationIds': ['444', '555'],
+    });
+  });
+});
