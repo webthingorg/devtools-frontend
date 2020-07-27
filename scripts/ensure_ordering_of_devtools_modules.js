63df4a1b5cb69a1ba30ebb59510998867211dc1d
Jan Scheffler <janscheffler@chromium.org>
2020-07-27 16:51:30 +0200

    Add script to ensure sorting in .gni files
    This cl adds a simple script to ensure sorting in
    all_devtools_modules.gni and devtools_module_entrypoints.gni
    
    Change-Id: I21962895f7b72f1052c89b30cefdc843f7b36624

---
+++ scripts/ensure_ordering_of_devtools_modules.js
@@ -0,0 +1,37 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const fs = require('fs');
+
+function sortAndDeduplicateLines(string) {
+  return string.trimRight()
+      .split('\n')
+      // Sort
+      .sort((a, b) => a.trim().localeCompare(b.trim()))
+      // Deduplicate
+      .filter((l, i, a) => a.indexOf(l) === i)
+      .join('\n');
+}
+
+function sortVar(data, name) {
+  const regexp = new RegExp(`${name} = \\[\\n([^\\[]*)\\]`, 'm');
+  const match = data.match(regexp);
+  if (!match) {
+    throw new Error(`Could not find variable ${name}`);
+  }
+  const lines = sortAndDeduplicateLines(match[1]);
+  return data.replace(match[1], lines + '\n');
+}
+
+// all_devtools_modules
+let modules = fs.readFileSync('all_devtools_modules.gni', {encoding: 'utf8'});
+modules = sortVar(modules, 'all_devtools_modules');
+modules = sortVar(modules, 'all_typescript_modules');
+fs.writeFileSync('all_devtools_modules.gni', modules);
+
+// devtools_module_entrypoint
+let entrypoints = fs.readFileSync('devtools_module_entrypoints.gni', {encoding: 'utf8'});
+entrypoints = sortVar(entrypoints, 'devtools_module_entrypoints');
+entrypoints = sortVar(entrypoints, 'generated_typescript_entrypoints');
+fs.writeFileSync('devtools_module_entrypoints.gni', entrypoints);
