60639fd85f26c29d9bccaf9de14fc39deae9dae4
Alex Rudenko <alexrudenko@chromium.org>
2020-07-27 11:34:46 +0000

    Layout pane WIP
    Change-Id: I7b5620ce6818df68d1f9cb36975be3679ca16837

---
+++ front_end/elements/LayoutPane.ts
@@ -0,0 +1,99 @@
+// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as LitHtml from '../third_party/lit-html/lit-html.js';
+
+import {BooleanSetting, DOMNode, EnumSetting, Setting} from './LayoutPaneUtils.js';
+
+export class SettingChangedEvent extends Event {
+  data: {setting: string, value: string|boolean};
+
+  constructor(setting: string, value: string|boolean) {
+    super('setting-changed', {});
+    this.data = {setting, value};
+  }
+}
+
+export class LayoutPane extends HTMLElement {
+  private readonly shadow = this.attachShadow({mode: 'open'});
+  private settings: Readonly<Setting[]>|null = null;
+  private selectedDOMNode: Readonly<DOMNode>|null = null;
+
+  set data(data: {selectedNode: DOMNode|null, settings: Setting[]}) {
+    this.selectedDOMNode = data.selectedNode;
+    this.settings = data.settings;
+    this.update();
+  }
+
+  private update() {
+    this.render();
+  }
+
+  private render() {
+    // Disabled until https://crbug.com/1079231 is fixed.
+    // clang-format off
+    LitHtml.render(LitHtml.html`
+      <style>
+        .settings {
+          display: flex;
+          flex-direction: column;
+        }
+        label {
+          display: flex;
+          flex-direction: row;
+          align-items: baseline;
+        }
+      </style>
+      <h2>CSS Grid</h2>
+      <div class="settings">
+        ${(this.settings || []).map(setting => setting.type === 'boolean' ?
+          this.renderBooleanSetting(setting) : this.renderEnumSetting(setting))}
+      </div>
+    `, this.shadow, {
+      eventContext: this,
+    });
+    // clang-format on
+  }
+
+  private onBooleanSettingChange(setting: BooleanSetting) {
+    return (event: Event&{target: HTMLInputElement}) => {
+      event.preventDefault();
+      this.dispatchEvent(new SettingChangedEvent(setting.name, event.target.checked));
+    };
+  }
+
+  private onEnumSettingChange(setting: EnumSetting) {
+    return (event: Event&{target: HTMLInputElement}) => {
+      event.preventDefault();
+      this.dispatchEvent(new SettingChangedEvent(setting.name, event.target.value));
+    };
+  }
+
+  private renderBooleanSetting(setting: BooleanSetting) {
+    return LitHtml.html`<label title=${setting.title}>
+      <input type="checkbox" .checked=${setting.value} @change=${this.onBooleanSettingChange(setting)} />
+      ${setting.title}
+    </label>`;
+  }
+
+  private renderEnumSetting(setting: EnumSetting) {
+    return LitHtml.html`<label title=${setting.title}>
+      ${setting.title}
+      <select @change=${this.onEnumSettingChange(setting)}>
+        ${
+        setting.options.map(
+            opt => LitHtml.html`<option value=${opt.value} .selected=${setting.value === opt.value}>${
+                opt.title}</option>`)}
+      </select>
+    </label>`;
+  }
+}
+
+customElements.define('devtools-layout-pane', LayoutPane);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'devtools-layout-pane': LayoutPane;
+  }
+}
---
+++ front_end/elements/LayoutSidebarPane.js
@@ -0,0 +1,79 @@
+// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Common from '../common/common.js';
+import * as SDK from '../sdk/sdk.js';
+import * as UI from '../ui/ui.js';
+
+import {createLayoutPane} from './LayoutPane_bridge.js';
+
+/**
+ * @unrestricted
+ */
+export class LayoutSidebarPane extends UI.ThrottledWidget.ThrottledWidget {
+  constructor() {
+    super(false /* isWebComponent */);
+    this._layoutPane = createLayoutPane();
+    this.contentElement.appendChild(this._layoutPane);
+    this._settings = [
+      'showGridBorder', 'showGridLines', 'showGridLineNumbers', 'showGridGaps', 'showGridAreas', 'showGridTrackSizes'
+    ];
+    this._layoutPane.addEventListener('setting-changed', event => {
+      Common.Settings.Settings.instance().moduleSetting(event.data.setting).set(event.data.value);
+    });
+    this._settings.forEach(setting => {
+      Common.Settings.Settings.instance().moduleSetting(setting).addChangeListener(this.update, this);
+    });
+  }
+
+  /**
+   * @override
+   * @protected
+   * @return {!Promise<void>}
+   */
+  async doUpdate() {
+    const node = self.UI.context.flavor(SDK.DOMModel.DOMNode);
+    this._layoutPane.data = {
+      selectedNode: node,
+      settings: this._settings
+                    .map(settingName => {
+                      const setting = Common.Settings.Settings.instance().moduleSetting(settingName);
+                      const ext = setting.extension();
+                      if (!ext) {
+                        return null;
+                      }
+                      const descriptor = ext.descriptor();
+                      return {
+                        type: descriptor.settingType,
+                        name: descriptor.settingName,
+                        title: descriptor.title,
+                        value: setting.get(),
+                        options: descriptor.options.map(opt => ({
+                                                          title: opt.text,
+                                                          value: opt.value,
+                                                        }))
+                      };
+                    })
+                    .filter(descriptor => descriptor !== null)
+    };
+  }
+
+  /**
+   * @override
+   */
+  wasShown() {
+    self.UI.context.addFlavorChangeListener(SDK.DOMModel.DOMNode, this.update, this);
+    this.update();
+  }
+
+  /**
+   * @override
+   */
+  willHide() {
+    self.UI.context.removeFlavorChangeListener(SDK.DOMModel.DOMNode, this.update, this);
+    this._settings.forEach(setting => {
+      Common.Settings.Settings.instance().moduleSetting(setting).removeChangeListener(this.update.bind, this);
+    });
+  }
+}
