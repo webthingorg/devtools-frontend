d04054db9ef21de06fab78ed1823fa30b700370c
Maksim Sadym <sadym@chromium.org>
2020-08-04 12:02:31 +0000

    e2e tests for Idle Emulation
    Bug: 1090802
    Change-Id: Icebb86770dcde77bc267470cc26426b2439f9164

--- test/e2e/sensors/idle_test.ts
+++ test/e2e/sensors/idle_test.ts
@@ -4,14 +4,44 @@
 
 import {assert} from 'chai';
 import {describe, it} from 'mocha';
+import {JSHandle} from 'puppeteer';
 
-import {$} from '../../shared/helper.js';
+import {$, clearPermissionsOverride, getBrowserAndPages, goToResource, overridePermissions} from '../../shared/helper.js';
 import {openPanelViaMoreTools} from '../helpers/settings-helpers.js';
 
-describe('Sensors panel', () => {
+describe('Idle Emulation on Sensors panel', () => {
   beforeEach(async () => {
+    await overridePermissions(['notifications']);
     await openPanelViaMoreTools('Sensors');
   });
+  afterEach(async () => {
+    await clearPermissionsOverride();
+  });
+
+  async function selectOption(select: JSHandle<HTMLSelectElement>, value: string) {
+    await select.evaluate(async (node, _value) => {
+      node.value = _value;
+      const event = document.createEvent('HTMLEvents');
+      event.initEvent('change', false, true);
+      node.dispatchEvent(event);
+    }, value);
+  }
+
+  async function getState() {
+    const {target} = getBrowserAndPages();
+    return await target.evaluate(() => {
+      const state = document.getElementById('state');
+      return state ? state.innerText : '';
+    });
+  }
+
+  async function waitForState(state: string) {
+    const {target} = getBrowserAndPages();
+    await target.waitForFunction(_state => {
+      const stateEl = document.getElementById('state');
+      return _state === (stateEl ? stateEl.innerText : '');
+    }, undefined, state);
+  }
 
   it('includes UI for emulating an idle state', async () => {
     const select = await $('.idle-section select');
@@ -25,5 +55,29 @@ describe('Sensors panel', () => {
       'User idle, screen locked',
     ].join('');
     assert.deepEqual(actual, expected);
+  });
+
+  it('changing idle state emulation causes change of the IdleDetector state', async () => {
+    await goToResource('sensors/idle-detector.html');
+    const select = await $('.idle-section select');
+
+    // InitialState can be idle as well.
+    const initialState = await getState();
+
+    // Emulate Idle states and verify IdleDetector updates state accordingly.
+    await selectOption(select, '{"isUserActive":false,"isScreenUnlocked":false}');
+    await waitForState('Idle state: idle, locked.');
+
+    await selectOption(select, '{"isUserActive":true,"isScreenUnlocked":false}');
+    await waitForState('Idle state: active, locked.');
+
+    await selectOption(select, '{"isUserActive":true,"isScreenUnlocked":true}');
+    await waitForState('Idle state: active, unlocked.');
+
+    await selectOption(select, '{"isUserActive":false,"isScreenUnlocked":true}');
+    await waitForState('Idle state: idle, unlocked.');
+
+    await selectOption(select, 'none');
+    await waitForState(initialState);
   });
 });
