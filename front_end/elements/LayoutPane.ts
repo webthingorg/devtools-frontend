c7f75bf7fa15c0013b1a5e8aa3b4d587d0f072b0
Alex Rudenko <alexrudenko@chromium.org>
2020-08-05 12:14:30 +0000

    Layout pane
    This CL adds a new layout panel to the sidebar in Elements. Currently,
    the pane only displays CSS Grid settings. In the future, the list of
    all CSS grids will show up above the settings. The new panel is behind
    the CSS Grid experiment flag.
    
    Screenshot: https://i.imgur.com/T5Kd5Wi.png
    
    Bug: 1109177
    Change-Id: I7b5620ce6818df68d1f9cb36975be3679ca16837

---
+++ front_end/elements/LayoutPane.ts
@@ -0,0 +1,158 @@
+// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as LitHtml from '../third_party/lit-html/lit-html.js';
+import * as UI from '../ui/ui.js';
+
+import {BooleanSetting, EnumSetting, Setting, SettingType} from './LayoutPaneUtils.js';
+
+export class SettingChangedEvent extends Event {
+  data: {setting: string, value: string|boolean};
+
+  constructor(setting: string, value: string|boolean) {
+    super('setting-changed', {});
+    this.data = {setting, value};
+  }
+}
+
+export class LayoutPane extends HTMLElement {
+  private readonly shadow = this.attachShadow({mode: 'open'});
+  private settings: Readonly<Setting[]>|null = null;
+
+  set data(data: {settings: Setting[]}) {
+    this.settings = data.settings;
+    this.update();
+  }
+
+  private update() {
+    this.render();
+  }
+
+  private render() {
+    // Disabled until https://crbug.com/1079231 is fixed.
+    // clang-format off
+    LitHtml.render(LitHtml.html`
+      <style>
+        .header {
+          align-items: center;
+          background-color: var(--toolbar-bg-color, #f3f3f3);
+          border-bottom: var(--divider-border, 1px solid #d0d0d0);
+          border-top: var(--divider-border, 1px solid #d0d0d0);
+          display: flex;
+          line-height: 1.6;
+          overflow: hidden;
+          padding: 0 5px;
+          white-space: nowrap;
+        }
+        .content-section {
+          padding: 16px;
+        }
+        .content-section-title {
+          font-size: 12px;
+          font-weight: 500;
+          line-height: 1.1;
+          margin: 0;
+          padding: 0;
+        }
+        .checkbox-settings {
+          margin-top: 8px;
+          display: grid;
+          grid-template-columns: 1fr;
+          gap: 5px;
+        }
+        .checkbox-label {
+          display: flex;
+          flex-direction: row;
+        }
+        .checkbox-label input {
+          margin: 0 6px 0 0;
+          padding: 0;
+        }
+        .select-settings {
+          margin-top: 16px;
+          display: grid;
+          grid-template-columns: repeat(auto-fill, 150px);
+          gap: 16px;
+        }
+        .select-label {
+          display: flex;
+          flex-direction: column;
+        }
+        .select-label span {
+          margin-bottom: 4px;
+        }
+      </style>
+      <details open>
+        <summary class="header">
+          Grid
+        </summary>
+        <div class="content-section">
+          <h3 class="content-section-title">Overlay display settings</h3>
+          <div class="checkbox-settings">
+            ${this.getBooleanSettings().map(setting => this.renderBooleanSetting(setting))}
+          </div>
+          <div class="select-settings">
+            ${this.getEnumSettings().map(setting => this.renderEnumSetting(setting))}
+          </div>
+        </div>
+      </details>
+    `, this.shadow, {
+      eventContext: this,
+    });
+    // clang-format on
+    if (this.shadowRoot) {
+      UI.Utils.injectCoreStyles(this.shadowRoot);
+    }
+  }
+
+  private getEnumSettings(): EnumSetting[] {
+    return (this.settings || []).filter((setting): setting is EnumSetting => setting.type === SettingType.enum);
+  }
+
+  private getBooleanSettings(): BooleanSetting[] {
+    return (this.settings || []).filter((setting): setting is BooleanSetting => setting.type === SettingType.boolean);
+  }
+
+  private onBooleanSettingChange(setting: BooleanSetting) {
+    return (event: Event&{target: HTMLInputElement}) => {
+      event.preventDefault();
+      this.dispatchEvent(new SettingChangedEvent(setting.name, event.target.checked));
+    };
+  }
+
+  private onEnumSettingChange(setting: EnumSetting) {
+    return (event: Event&{target: HTMLInputElement}) => {
+      event.preventDefault();
+      this.dispatchEvent(new SettingChangedEvent(setting.name, event.target.value));
+    };
+  }
+
+  private renderBooleanSetting(setting: BooleanSetting) {
+    return LitHtml.html`<label data-boolean-setting="true" class="checkbox-label" title=${setting.title}>
+      <input data-input="true" type="checkbox" .checked=${setting.value} @change=${
+        this.onBooleanSettingChange(setting)} />
+      <span data-label="true">${setting.title}</span>
+    </label>`;
+  }
+
+  private renderEnumSetting(setting: EnumSetting) {
+    return LitHtml.html`<label data-enum-setting="true" class="select-label" title=${setting.title}>
+      <span data-label="true">${setting.title}</span>
+      <select class="chrome-select" data-input="true" @change=${this.onEnumSettingChange(setting)}>
+        ${
+        setting.options.map(
+            opt => LitHtml.html`<option value=${opt.value} .selected=${setting.value === opt.value}>${
+                opt.title}</option>`)}
+      </select>
+    </label>`;
+  }
+}
+
+customElements.define('devtools-layout-pane', LayoutPane);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'devtools-layout-pane': LayoutPane;
+  }
+}
