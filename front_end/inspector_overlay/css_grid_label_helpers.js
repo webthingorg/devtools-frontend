be5a760a10a453b02ceeb25eb6c90a403b5580c0
Patrick Brosset <patrick.brosset@microsoft.com>
2020-08-06 02:51:57 -0700

    Grid: Always place grid area labels in the top/left corner of the area
    Grid area name labels are supposed to be displayed in the top/left
    corner of the corresponding grid areas. And they are, most of the time.
    
    The way this was achieved was by using the minX and minY coordinates
    from the area bounds.
    However, when the grid container is transformed (e.g. rotated), the minX
    and minY might not correspond to the top/left corner anymore.
    
    So now we use the bounds.allPoints[0] data, which always gives us the
    x/y coordinates of the top/left corner.
    
    Before: https://imgur.com/aE9Gx3U.jpg
    After: https://i.imgur.com/Xi6bNaq.jpg
    Bug: 1100384
    Change-Id: Icb0acb1f4b1a4d77c0e7df6af18e48bb96608cc7

--- front_end/inspector_overlay/css_grid_label_helpers.js
+++ front_end/inspector_overlay/css_grid_label_helpers.js
@@ -5,7 +5,7 @@
 // @ts-nocheck
 // TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
-import {AreaBounds, Bounds} from './common.js';  // eslint-disable-line no-unused-vars
+import {AreaBounds, Bounds, Position} from './common.js';  // eslint-disable-line no-unused-vars
 
 /**
  * There are 12 different types of arrows for labels.
@@ -69,9 +69,6 @@ let GridPositionNormalizedData;  // eslint-disable-line no-unused-vars
 
 /** @typedef {!{computedSize: number, x: number, y: number}} */
 let TrackSize;  // eslint-disable-line no-unused-vars
-
-/** @typedef {!{x: number, y: number}} */
-let Position;  // eslint-disable-line no-unused-vars
 
 /** @typedef {!{
  * rotationAngle?: number,
@@ -390,8 +387,10 @@ export function drawGridAreaNames(container, areaBounds) {
   for (const {name, bounds} of areaBounds) {
     const element = _createLabelElement(container, name);
 
-    element.style.left = bounds.minX + 'px';
-    element.style.top = bounds.minY + 'px';
+    const topLeftCorner = bounds.allPoints[0];
+
+    element.style.left = topLeftCorner.x + 'px';
+    element.style.top = topLeftCorner.y + 'px';
   }
 }
 
