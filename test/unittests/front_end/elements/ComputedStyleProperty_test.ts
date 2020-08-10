5f03222a0a31f7051cca3e944bd0117892f9b741
Changhao Han <changhaohan@chromium.org>
2020-08-10 12:27:37 +0200

    TS-componentize Computed tab's style property and style trace elements
    This also helps future work, for example, Computed style grouping,
    to be TS-componentized as well.
    
    Bug: chromium:1106251, chromium:1096230
    Change-Id: I2f8a977f4e3f701905354cb4edb15702cb8cd2a3

---
+++ test/unittests/front_end/elements/ComputedStyleProperty_test.ts
@@ -0,0 +1,91 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {ComputedStyleProperty} from '../../../../front_end/elements/ComputedStyleProperty.js';
+import {assertShadowRoot, renderElementIntoDOM} from '../helpers/DOMHelpers.js';
+
+const {assert} = chai;
+
+describe('ComputedStyleProperty', () => {
+  it('renders inherited vs. non-inherited property correctly', () => {
+    const component = new ComputedStyleProperty();
+    renderElementIntoDOM(component);
+    const data = {
+      inherited: true,
+      expanded: false,
+      onNavigateToSource: () => {},
+    };
+    component.data = data;
+
+    assertShadowRoot(component.shadowRoot);
+    const inheritedSlots = Array.from(component.shadowRoot.querySelectorAll('.inherited slot'));
+    assert.deepEqual(
+        inheritedSlots.map(slot => slot.getAttribute('name')),
+        [
+          'property-name',
+          'property-value',
+        ],
+        'should contain name and value slots under .inherited selector');
+
+    data.inherited = false;
+    component.data = data;
+    const nonInheritedSlots = Array.from(component.shadowRoot.querySelectorAll('details summary slot'));
+    assert.deepEqual(
+        nonInheritedSlots.map(slot => slot.getAttribute('name')),
+        [
+          'property-name',
+          'property-value',
+        ],
+        'should contain name and value slots under details summary selector');
+  });
+
+  it('renders expanded vs. collapsed property correctly', () => {
+    const component = new ComputedStyleProperty();
+    renderElementIntoDOM(component);
+    const data = {
+      inherited: false,
+      expanded: true,
+      onNavigateToSource: () => {},
+    };
+    component.data = data;
+
+    assertShadowRoot(component.shadowRoot);
+    const detailsElement = component.shadowRoot.querySelector('details');
+    const summaryElement = component.shadowRoot.querySelector('summary');
+    assert.exists(detailsElement, 'non-inherited property should be wrapped in a <details> element');
+    assert.exists(summaryElement, 'non-inherited property should contain a <summary> element');
+    if (!detailsElement || !summaryElement) {
+      return;
+    }
+    assert.isTrue(detailsElement.open, 'details tag should be open when the property is expanded');
+
+    data.expanded = false;
+    component.data = data;
+    assert.isFalse(detailsElement.open, 'details tag should not be open when the property is collapsed');
+
+    summaryElement.click();
+    assert.isTrue(component.isExpanded(), 'component should be expanded after clicking while closed');
+    assert.isTrue(detailsElement.open, 'details tag should be open after clicking while closed');
+  });
+
+  it('renders a clickable goto icon when it contains traces', () => {
+    const component = new ComputedStyleProperty();
+    renderElementIntoDOM(component);
+    let isClicked = false;
+    const data = {
+      inherited: false,
+      expanded: false,
+      onNavigateToSource: () => {
+        isClicked = true;
+      },
+    };
+    component.data = data;
+
+    assertShadowRoot(component.shadowRoot);
+    const goto: HTMLElement|null = component.shadowRoot.querySelector('.goto');
+    assert.exists(goto);
+    goto !.click();
+    assert.isTrue(isClicked, 'goto icon should be clickable');
+  });
+});
