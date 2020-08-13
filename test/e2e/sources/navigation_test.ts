67c6db713816776c2ca95974485cbf111dbf3ff2
Peter Marshall <petermarshall@chromium.org>
2020-08-13 16:36:54 +0200

    WIP: Show how it.repeat() could work
    Bug: 1101782
    Change-Id: I15dbb71493e60064288bfc050ac3f818a5c6821f

--- test/e2e/sources/navigation_test.ts
+++ test/e2e/sources/navigation_test.ts
@@ -3,14 +3,14 @@
 // found in the LICENSE file.
 
 import {assert} from 'chai';
-import {describe, it} from 'mocha';
 
 import {waitFor} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
 import {clickOnContextMenu, openSourceCodeEditorForFile} from '../helpers/sources-helpers.js';
 
 describe('The Sources Tab', async () => {
   describe('Navigation', () => {
-    it('should show a "search in folder" option in the context menu of folders', async () => {
+    it.repeat(5, 'should show a "search in folder" option in the context menu of folders', async () => {
       await openSourceCodeEditorForFile('index.html', 'navigation/index.html');
 
       await clickOnContextMenu('[aria-label="test/e2e/resources/sources/navigation, nw-folder"]', 'Search in folder');
