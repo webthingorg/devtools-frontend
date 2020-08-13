9e16aab543991464304c8f66fe912c867578499d
Alex Rudenko <alexrudenko@chromium.org>
2020-08-13 06:02:36 +0000

    Show list of grid elements in Layout pane
    This CL adds a list of grid elements to the Layout pane and
    synchronises the overlay state with adorners. The ability to navigate
    to an element or configure the colors will be done in follow-up CLs.
    
    Screenshot: https://i.imgur.com/y8bsLzk.png
    
    Bug: 1109177
    Change-Id: I7700a37e1e8b03b48e3e4394f3e6c95a41d543f0

--- front_end/elements/LayoutPaneUtils.ts
+++ front_end/elements/LayoutPaneUtils.ts
@@ -27,3 +27,11 @@ export interface BaseSetting {
 export type BooleanSetting = BaseSetting&{type: SettingType.BOOLEAN, options: BooleanSettingOption[], value: boolean};
 export type EnumSetting = BaseSetting&{type: SettingType.ENUM, options: EnumSettingOption[], value: string};
 export type Setting = EnumSetting|BooleanSetting;
+
+export interface Element {
+  id: number;
+  name: string;
+  domId?: string;
+  domClasses?: string[];
+  enabled: boolean;
+}
