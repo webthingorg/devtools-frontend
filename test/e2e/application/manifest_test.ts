bfe46f2cd03bf8b2866b7d02d78dae51e1b425ae
Mike Jackson <mjackson@microsoft.com>
2020-08-14 12:16:17 -0700

    PWA: Update installability failure to account for display_override
    Update devtools to show the appropriate error message when
    a PWA is not installable due to a value in the display_override
    field in the manifest.
    
    For example, if the developer specifies:
      display: "standalone"
      display_override: ["browser"]
    
    This would not be installable. Without this change in DevTools
    an error is shown to the user, but with no text, and is thus inactionable
    for the user.
    
    To test the change use: chrome --enable-features=WebAppManifestDisplayOverride
    Example websites:
     - (not installable) https://mwjacksonmsft.github.io/pwa/display-override-browser/index.html
     - (installable) https://mwjacksonmsft.github.io/pwa/display-override-custom/index.html
    
    Screenshot after change:
    https://imgur.com/a/7NhHScW
    
    Explainer: https://github.com/WICG/display-override/blob/master/explainer.md
    Design document: https://docs.google.com/document/d/1hEmbGVHMN38q1YTaaGccQ-Y5CHr7xIURYPRWXTuvZLo/edit?usp=sharing
    I2P: https://groups.google.com/a/chromium.org/d/topic/blink-dev/WvIeZT8uSzw/discussion
    
    While I was here, I noticed two other reasons were missing, and
    I've added them as well.
    
    Bug: 1109520
    Change-Id: Id5687843876a71ddddee88edbf05d950b1e6e171

---
+++ test/e2e/application/manifest_test.ts
@@ -0,0 +1,24 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {describe, it} from 'mocha';
+
+import {click, getBrowserAndPages, getHostedModeServerPort, goToResource, waitFor} from '../../shared/helper.js';
+import {doubleClickSourceTreeItem, getDataGridData, navigateToApplicationTab} from '../helpers/application-helpers.js';
+
+function shouldRunTest() {
+  const features = process.env['CHROME_FEATURES'];
+  return features !== undefined && features.includes('WebAppManifestDisplayOverride');
+}
+
+describe('The Application Tab - Manifest', async () => {
+  before(async () => {});
+
+  it('display_override installability check', async () => {
+    if (!shouldRunTest()) {
+      return;
+    }
+  });
+});
