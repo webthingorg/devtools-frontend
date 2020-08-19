de375b604529b875c9cfe1742cb3169f4da9ab44
Christy Chen <chrche@microsoft.com>
2020-08-19 01:11:10 -0700

    Fix background services recording button tooltip not updating
    When hitting the record button, the tooltip did not get updated from "start recording" to "stop recording"
    https://imgur.com/a/KhHFT3k
    This is due to that there are multiple views sharing a same action, so we can't toggle the action to change the tooltip like the ones in other tools (related change https://chromium-review.googlesource.com/c/chromium/src/+/1609707)
    
    This CL update the button tooltip whenever the button is clicked
    https://imgur.com/a/vyguTDV
    
    Bug: 1117410
    Change-Id: I53e71ce7f58ea94c04315027dcf63ec3a21538b3

--- front_end/ui/Toolbar.js
+++ front_end/ui/Toolbar.js
@@ -447,13 +447,13 @@ export class ToolbarItem extends Common.ObjectWrapper.ObjectWrapper {
   /**
    * @param {string} title
    */
-  setTitle(title) {
+  setTitle(title, actionId = undefined) {
     if (this._title === title) {
       return;
     }
     this._title = title;
     ARIAUtils.setAccessibleName(this.element, title);
-    Tooltip.install(this.element, title, undefined, {
+    Tooltip.install(this.element, title, actionId, {
       anchorTooltipAtElement: true,
     });
   }
