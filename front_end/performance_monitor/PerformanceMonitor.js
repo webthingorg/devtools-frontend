6cc977572d0461a0c9a61b150b13dc98a83528b6
Alex Rudenko <alexrudenko@chromium.org>
2020-08-24 08:42:16 +0000

    Revert "[Perfmon] Improve contrast of labels in dark theme"
    This reverts commit 2f852116281b1b8673b304c9c3f5637826de799f.
    
    Reason for revert: breaks colors in the Ctrl + Shift + P dialog (crbug.com/1119722)
    
    Original change's description:
    > [Perfmon] Improve contrast of labels in dark theme
    > 
    > The logic for tracking colors for the dark theme did not test the labels
    > for the Perfmon charting, but rather the chart lines themselves. This
    > CL ensures the labels are checked, but also bumps the minimum cap value
    > so that the labels in the Perfmon obtain a contrast of > 7.0.
    > 
    > See
    > 
    > Bug: 1098637
    > Change-Id: I6b366d672fca971b134261fa1de2477ef52345c3
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2264116
    > Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    > Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    
    TBR=aerotwist@chromium.org,jacktfranklin@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1098637
    Change-Id: Icfef08b6ad8fe7037eabaa7c6d277038e488d7fb

--- front_end/performance_monitor/PerformanceMonitor.js
+++ front_end/performance_monitor/PerformanceMonitor.js
@@ -449,9 +449,6 @@ export class ControlPane extends Common.ObjectWrapper.ObjectWrapper {
       {title: Common.UIString.UIString('Style recalcs / sec'), metrics: [{name: 'RecalcStyleCount', color: 'deeppink'}]}
     ];
     for (const info of this._chartsInfo) {
-      if (info.color) {
-        info.color = self.UI.themeSupport.patchColorText(info.color, UI.UIUtils.ThemeSupport.ColorUsage.Foreground);
-      }
       for (const metric of info.metrics) {
         metric.color = self.UI.themeSupport.patchColorText(metric.color, UI.UIUtils.ThemeSupport.ColorUsage.Foreground);
       }
@@ -602,8 +599,7 @@ export let MetricInfo;
  *   max: (number|undefined),
  *   currentMax: (number|undefined),
  *   format: (!Format|undefined),
- *   smooth: (boolean|undefined),
- *   color: (string|undefined)
+ *   smooth: (boolean|undefined)
  * }}
  */
 export let ChartInfo;
