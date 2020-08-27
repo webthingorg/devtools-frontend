b8d4df71b8e328d9a90fd1149e5b625cf52f4cda
Simon Zünd <szuend@chromium.org>
2020-08-27 13:54:19 +0200

    [ts] Type-check sdk/CPUProfilerModel.js with TypeScript
    R=tvanderlippe@chromium.org
    
    Bug: chromium:1011811
    Change-Id: I2004b28568e24d6dbf5e6c8b37a83a4c9baf6abc

--- front_end/protocol_client/InspectorBackend.js
+++ front_end/protocol_client/InspectorBackend.js
@@ -727,6 +727,13 @@ export class TargetBase {
   }
 
   /**
+   * @return {!ProtocolProxyApi.ProfilerApi}
+   */
+  profilerAgent() {
+    throw new Error('Implemented in InspectorBackend.js');
+  }
+
+  /**
    * @return {!ProtocolProxyApi.PerformanceApi}
    */
   performanceAgent() {
@@ -824,6 +831,13 @@ export class TargetBase {
    * @param {!ProtocolProxyApi.PageDispatcher} dispatcher
    */
   registerPageDispatcher(dispatcher) {
+    throw new Error('Implemented in InspectorBackend.js');
+  }
+
+  /**
+   * @param {!ProtocolProxyApi.ProfilerDispatcher} dispatcher
+   */
+  registerProfilerDispatcher(dispatcher) {
     throw new Error('Implemented in InspectorBackend.js');
   }
 
