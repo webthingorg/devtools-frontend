39dab9913dd1acfebc4ae67ebf513fc3668ff983
Paul Lewis <aerotwist@chromium.org>
2020-09-01 17:19:37 +0100

    it# Enter a description of the change. Update Closure Compiler and fix associated errors
    Change-Id: I07cc1ea6b1b135571359136dfdec120ab4649b9e

--- scripts/test/run_type_check.py
+++ scripts/test/run_type_check.py
@@ -282,7 +282,7 @@ common_closure_args = [
     '--warning_level',
     'VERBOSE',
     '--language_in=ECMASCRIPT_NEXT',
-    '--language_out=ES5_STRICT',
+    '--language_out=ECMASCRIPT_NEXT',
     '--extra_annotation_name',
     'suppressReceiverCheck',
     '--extra_annotation_name',
