a1e8b1f4c056edebd67186f55315fe68d079131b
Tim van der Lippe <tvanderlippe@chromium.org>
2020-09-03 11:55:06 +0100

    [globals] self.SDK.domDebuggerManager
    R=aerotwist@chromium.org
    
    Bug: 1058320
    Change-Id: I523b32d769c6e240ce678e6657a66cd54e2c4c8d

--- front_end/main/MainImpl.js
+++ front_end/main/MainImpl.js
@@ -251,7 +251,7 @@ export class MainImpl {
     self.SDK.consoleModel = SDK.ConsoleModel.ConsoleModel.instance();
     self.UI.dockController = new UI.DockController.DockController(canDock);
     self.SDK.multitargetNetworkManager = SDK.NetworkManager.MultitargetNetworkManager.instance({forceNew: true});
-    self.SDK.domDebuggerManager = new SDK.DOMDebuggerModel.DOMDebuggerManager();
+    self.SDK.domDebuggerManager = SDK.DOMDebuggerModel.DOMDebuggerManager.instance({forceNew: true});
     SDK.SDKModel.TargetManager.instance().addEventListener(
         SDK.SDKModel.Events.SuspendStateChanged, this._onSuspendStateChanged.bind(this));
 
