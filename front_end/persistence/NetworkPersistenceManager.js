d70b121a0f182fc367d870f277a9a495aaccbc84
Eric Leese <leese@chromium.org>
2020-09-07 12:01:02 +0200

    Reland "Fix path escaping"
    This reverts commit a292e76f9ca0c1cd65fed699d552ea703e245a8a.
    This relands commit 10892e33a22289e7a8c2006448962ce48e4d5867.
    
    Inconsistent URL escaping was breaking snippets, which by default
    have characters (space and #) which need to be escaped.
    
    As a general rule, anything called 'url' or 'path' is now url-encoded,
    and anything called 'name' or 'platformPath' is not.
    
    Updated to match upper and lower case drive letters in Node on Windows.
    
    Bug: chromium:1094436
    Change-Id: Ib82bc34ad6769dfc9df6cef80c77401cee2a0783

--- front_end/persistence/NetworkPersistenceManager.js
+++ front_end/persistence/NetworkPersistenceManager.js
@@ -255,6 +255,15 @@ export class NetworkPersistenceManager extends Common.ObjectWrapper.ObjectWrappe
   }
 
   /**
+   * @param {string} url
+   * @return {string}
+   */
+  _fileUrlFromNetworkUrl(url) {
+    return Common.ParsedURL.ParsedURL.relativePlatformPathToURL(
+        this._encodedPathFromUrl(url), /** @type {!FileSystem} */ (this._project).fileSystemPath() + '/');
+  }
+
+  /**
    * @param {string} path
    * @return {string}
    */
@@ -381,9 +390,7 @@ export class NetworkPersistenceManager extends Common.ObjectWrapper.ObjectWrappe
     }
     const url = Common.ParsedURL.ParsedURL.urlWithoutHash(uiSourceCode.url());
     this._networkUISourceCodeForEncodedPath.set(this._encodedPathFromUrl(url), uiSourceCode);
-
-    const fileSystemUISourceCode = this._project.uiSourceCodeForURL(
-        /** @type {!FileSystem} */ (this._project).fileSystemPath() + '/' + this._encodedPathFromUrl(url));
+    const fileSystemUISourceCode = this._project.uiSourceCodeForURL(this._fileUrlFromNetworkUrl(url));
     if (fileSystemUISourceCode) {
       await this._bind(uiSourceCode, fileSystemUISourceCode);
     }
@@ -523,8 +530,7 @@ export class NetworkPersistenceManager extends Common.ObjectWrapper.ObjectWrappe
     if (!this._active || (method !== 'GET' && method !== 'POST')) {
       return;
     }
-    const path = /** @type {!FileSystem} */ (this._project).fileSystemPath() + '/' +
-        this._encodedPathFromUrl(interceptedRequest.request.url);
+    const path = this._fileUrlFromNetworkUrl(interceptedRequest.request.url);
     const fileSystemUISourceCode = this._project.uiSourceCodeForURL(path);
     if (!fileSystemUISourceCode) {
       return;
