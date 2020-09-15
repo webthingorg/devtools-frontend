6f0959f6fecaed3106d6c9d477e9e8455d09c3ca
Patrick Brosset <patrick.brosset@microsoft.com>
2020-09-15 03:09:27 -0700

    Grid: Set a light background color on the persistent grid overlay
    In order to make the persistent grid overlays stand out more, especially
    when there are several of them on at the same time, we want a light
    color to be applied to its background.
    
    Before: https://imgur.com/ZFinACO.gif
    After: https://imgur.com/k7s6Wdy.gif
    
    This depends on the backend CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2405997/
    
    Bug: 1047356
    Change-Id: Id1435fa8423611939e800b9ef57ce026cb41bd4e

--- front_end/sdk/OverlayModel.js
+++ front_end/sdk/OverlayModel.js
@@ -845,6 +845,7 @@ class DefaultPersistentGridHighlighter {
    */
   _buildGridHighlightConfig(nodeId) {
     const mainColor = this.colorOfGrid(nodeId);
+    const background = mainColor.setAlpha(0.1);
     const gapBackground = mainColor.setAlpha(0.3);
     const gapHatch = mainColor.setAlpha(0.8);
 
@@ -870,6 +871,7 @@ class DefaultPersistentGridHighlighter {
       showAreaNames: /** @type {boolean} */ (this._showGridAreasSetting.get()),
       showTrackSizes: /** @type {boolean} */ (this._showGridTrackSizesSetting.get()),
       areaBorderColor: mainColor.toProtocolRGBA(),
+      gridBackgroundColor: background.toProtocolRGBA(),
     };
   }
 
