082a5f4271a5bbba0a85ea6529ad4edcdf758278
Stephen Sigwart <ssigwart@gmail.com>
2020-09-24 23:06:05 -0400

    Add clear current line in history option
    This gives you the option to clear a single line from the console
    history instead of all of history.
    
    Change-Id: Ie01df1daaea4edea59b4c4293b37e33e0caa492a

--- front_end/console/ConsoleView.js
+++ front_end/console/ConsoleView.js
@@ -379,6 +379,15 @@ export class ConsoleView extends UI.Widget.VBox {
     this._prompt.history().setHistoryData([]);
   }
 
+  _clearLineFromHistory() {
+    const text = this._prompt.text();
+    if (text !== '') {
+      this._prompt.history().removeHistoryItem(text);
+      this._consoleHistorySetting.set(this._prompt.history().historyData());
+      this._prompt.setText('');
+    }
+  }
+
   _consoleHistoryAutocompleteChanged() {
     this._prompt.setAddCompletionsFromHistory(this._consoleHistoryAutocompleteSetting.get());
   }
@@ -1672,6 +1681,9 @@ export class ActionDelegate {
         return true;
       case 'console.clear.history':
         ConsoleView.instance()._clearHistory();
+        return true;
+      case 'console.clear.history-line':
+        ConsoleView.instance()._clearLineFromHistory();
         return true;
       case 'console.create-pin':
         ConsoleView.instance()._pinPane.addPin('', true /* userGesture */);
