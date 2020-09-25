0c010267fae06584df3346d3592c6587dae8531b
Eric Leese <leese@chromium.org>
2020-09-25 15:29:40 +0200

    Support virtual inline call frames in DevTools with the plugin
    This change adds support for a new DebuggerPlugin RPC that turns
    a V8 callframe into potentially multiple virtual call frames representing
    inline frames.
    
    Bug: chromium:1126943
    
    
    Adding inline support; WIP
    
    Change-Id: I3b9682d13f39c6698b796bf494dbc12ac3f2f424

--- front_end/bindings/DebuggerLanguagePlugins.js
+++ front_end/bindings/DebuggerLanguagePlugins.js
@@ -487,7 +487,8 @@ export class DebuggerLanguagePluginManager {
       rawModuleId: script.sourceURL,
       // RawLocation.columnNumber is the byte offset in the full raw wasm module. Plugins expect the offset in the code
       // section, so subtract the offset of the code section in the module here.
-      codeOffset: rawLocation.columnNumber - (script.codeOffset() || 0)
+      codeOffset: rawLocation.columnNumber - (script.codeOffset() || 0),
+      inlineFrameIndex: rawLocation.inlineFrameIndex
     };
 
     let sourceLocations;
@@ -730,10 +731,11 @@ export class DebuggerLanguagePluginManager {
     }
     /** @type {!Map<string, !SourceScope>} */
     const scopes = new Map();
-    /** @type {!RawLocation}} */
+    /** @type {!RawLocation} */
     const location = {
       'rawModuleId': script.sourceURL,
-      'codeOffset': callFrame.location().columnNumber - (script.codeOffset() || 0)
+      'codeOffset': callFrame.location().columnNumber - (script.codeOffset() || 0),
+      'inlineFrameIndex': callFrame.inlineFrameIndex
     };
 
     try {
@@ -747,6 +749,39 @@ export class DebuggerLanguagePluginManager {
         scope.object().variables.push(variable);
       }
       return Array.from(scopes.values());
+    } catch (error) {
+      if (!(error instanceof DebuggerLanguagePluginError)) {
+        throw error;
+      }
+      Common.Console.Console.instance().warn(ls`Error in debugger language plugin: ${error.message} (${error.code})`);
+      return null;
+    }
+  }
+
+  /**
+   * @param {!SDK.DebuggerModel.CallFrame} callFrame
+   * @return {!Promise<?{frames: !Array<!FunctionInfo>}>}
+   */
+  async getFunctionInfo(callFrame) {
+    if (!callFrame) {
+      return null;
+    }
+    const script = callFrame.script;
+    const plugin = await this._getPluginForScript(script);
+    if (!plugin) {
+      return null;
+    }
+    /** @type {!RawLocation}} */
+    const location = {
+      'rawModuleId': script.sourceURL,
+      'codeOffset': callFrame.location().columnNumber - (script.codeOffset() || 0),
+      // TODO: Once closure->typescript migration is complete, delete this and change
+      // type definition to show that this field is optional.
+      'inlineFrameIndex': undefined
+    };
+
+    try {
+      return await plugin.getFunctionInfo(location);
     } catch (error) {
       if (!(error instanceof DebuggerLanguagePluginError)) {
         throw error;
@@ -818,8 +853,9 @@ export let RawLocationRange;
 
 /** Offsets in raw modules
  * @typedef {{
- *            rawModuleId:string,
- *            codeOffset:number
+ *            rawModuleId: string,
+ *            codeOffset: number,
+ *            inlineFrameIndex: (number|undefined)
  *          }}
  */
 // @ts-ignore typedef
@@ -867,6 +903,14 @@ export let VariableValue;
  */
 // @ts-ignore typedef
 export let EvaluatorModule;
+
+/** Either the code of an evaluator module or a constant representation of a variable
+ * @typedef {{
+ *            name: !string
+ *          }}
+ */
+// @ts-ignore typedef
+export let FunctionInfo;
 
 /**
  * @interface
@@ -938,6 +982,15 @@ export class DebuggerLanguagePlugin {
    * @throws {Bindings.DebuggerLanguagePlugins.DebuggerLanguagePluginError}
    */
   removeRawModule(rawModuleId) {
+    throw new Error('Not implemented yet');
+  }
+
+  /** Find locations in source files from a location in a raw module
+   * @param {!RawLocation} rawLocation
+   * @return {!Promise<?{frames: !Array<!FunctionInfo>}>}
+   * @throws {DebuggerLanguagePluginError}
+  */
+  async getFunctionInfo(rawLocation) {
     throw new Error('Not implemented yet');
   }
 }
