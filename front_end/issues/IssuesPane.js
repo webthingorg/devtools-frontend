f4fd71786ff4aab544b9cf89ccb8e6461650c72f
Michael Liao <michael.liao@microsoft.com>
2020-10-08 09:06:03 -0700

    Revert "[Issues Pane]: Fixing focus when navigating to issues panel via infobar"
    **Keeping this CL in case we want to revert before checking in the change
    CL that fixes regression:
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2461788
    
    
    This reverts commit 5e507ffce398d2935face3ecb6b3e98c3f74b1d3.
    
    Reason for revert: Causing focus regression
    
    Original change's description:
    > [Issues Pane]: Fixing focus when navigating to issues panel via infobar
    >
    > Issue:
    > - focus appears lost when navigating to the issues panel
    >
    > Changes:
    > - set default focus element to the issues treeOutline and select the first tree element upon focus
    >
    > Before: https://imgur.com/bCaAMsz
    >
    > After: https://imgur.com/z6iiufy
    >
    > Bug: 1133476
    > Change-Id: I80e792a27c4a7db876e35b4a554a352832522c2c
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2438653
    > Commit-Queue: Michael Liao <michael.liao@microsoft.com>
    > Reviewed-by: Jack Lynch <jalyn@microsoft.com>
    > Reviewed-by: Patrick Brosset <patrick.brosset@microsoft.com>
    
    TBR=jalyn@microsoft.com,michael.liao@microsoft.com,patrick.brosset@microsoft.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1133476
    Change-Id: Ic049756a928293b2b06d77e866c9f3a8afef934f

--- front_end/issues/IssuesPane.js
+++ front_end/issues/IssuesPane.js
@@ -1273,7 +1273,6 @@ export class IssuesPaneImpl extends UI.Widget.VBox {
 
     this._categoryViews = new Map();
     this._issueViews = new Map();
-    this._showThirdPartyCheckbox = null;
 
     const {toolbarContainer, updateToolbarIssuesCount} = this._createToolbars();
     this._issuesToolbarContainer = toolbarContainer;
@@ -1284,13 +1283,6 @@ export class IssuesPaneImpl extends UI.Widget.VBox {
     this._issuesTree.setShowSelectionOnKeyboardFocus(true);
     this._issuesTree.contentElement.classList.add('issues');
     this.contentElement.appendChild(this._issuesTree.element);
-
-    // Setting the default focused element to the issuesTree container which
-    // will delegate focus to either the first issue in the pane or the checkbox
-    // if no issue is available. We add an event listener to delegate focus
-    // since issues and the checkbox are not populated at this point.
-    this.setDefaultFocusedElement(this._issuesTree.contentElement);
-    this._issuesTree.contentElement.addEventListener('focus', this._selectFirstChildOrCheckbox.bind(this), false);
 
     this._noIssuesMessageDiv = document.createElement('div');
     this._noIssuesMessageDiv.classList.add('issues-pane-no-issues');
@@ -1336,10 +1328,10 @@ export class IssuesPaneImpl extends UI.Widget.VBox {
     });
 
     const thirdPartySetting = SDK.Issue.getShowThirdPartyIssuesSetting();
-    this._showThirdPartyCheckbox = new UI.Toolbar.ToolbarSettingCheckbox(
+    const showThirdPartyCheckbox = new UI.Toolbar.ToolbarSettingCheckbox(
         thirdPartySetting, ls`Include cookie Issues caused by third-party sites`,
         ls`Include third-party cookie issues`);
-    rightToolbar.appendToolbarItem(this._showThirdPartyCheckbox);
+    rightToolbar.appendToolbarItem(showThirdPartyCheckbox);
 
     rightToolbar.appendSeparator();
     const toolbarWarnings = document.createElement('div');
@@ -1477,15 +1469,6 @@ export class IssuesPaneImpl extends UI.Widget.VBox {
     if (issueView) {
       issueView.expand();
       issueView.reveal();
-    }
-  }
-
-  _selectFirstChildOrCheckbox() {
-    const firstChild = this._issuesTree.firstChild();
-    if (firstChild) {
-      firstChild.select();
-    } else if (this._showThirdPartyCheckbox) {
-      this._showThirdPartyCheckbox.inputElement.focus();
     }
   }
 }
