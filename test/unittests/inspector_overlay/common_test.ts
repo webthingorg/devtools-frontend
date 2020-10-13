ed2ebcfc03da6dbb263991779d516d7f4d69f428
Sigurd Schneider <sigurds@chromium.org>
2020-10-13 10:06:08 +0200

    Remove DOM prototype patching of createTextChild(ren)
    Bug: chromium:1011811
    Change-Id: I44a66c10efd317dcb7d5a4f58ab4be0fded4b215

--- test/unittests/inspector_overlay/common_test.ts
+++ test/unittests/inspector_overlay/common_test.ts
@@ -4,6 +4,8 @@
 
 const {assert} = chai;
 
+import * as UI from '../../../front_end/ui/ui.js';
+
 import {createElement, Overlay} from '../../../inspector_overlay/common.js';
 
 // Make sure typescript knows about the custom properties that are set on the window object.
@@ -29,8 +31,7 @@ describe('common helper', () => {
     assert.instanceOf(child, HTMLSpanElement, 'The right span element got created');
     assert.strictEqual(child.className, 'child', 'The right className got set');
 
-    assert.isTrue(!!wrapper.createTextChild, 'createTextChild is available on DOM elements');
-    const textChild = wrapper.createTextChild('hello world');
+    const textChild = UI.UIUtils.createTextChild(wrapper, 'hello world');
 
     assert.instanceOf(textChild, Text, 'The right text node got created');
     assert.strictEqual(textChild.textContent, 'hello world', 'The right text content got set');
