8bb821d95bf51e5f653a1426b01ae141e9315463
Jack Lynch <jalyn@microsoft.com>
2020-10-14 18:05:33 -0700

    Remove dependencies on ShortcutsScreen
    This CL removes all dependencies on ShortcutsScreen in preparation for
    its removal. Since CSSPlugin and TransformController actually store
    their shortcuts in ShortcutsScreen, those shortcuts have to be moved
    into the module.jsons so that they can be managed by ShortcutRegistry,
    as most other shortcuts in the DevTools are.
    
    enums.xml changes to match userMetrics changes: https://chromium-review.googlesource.com/c/chromium/src/+/2464047
    
    Custom shortcuts explainer: https://docs.google.com/document/d/1oOPSWPxCHvMoBZ0Fw9jwFZt6gP4lrsrsl8DEAp-Hy7o/edit#
    
    Bug: 174309
    Change-Id: I5d9ae2e28c555c88c5549fb830575b36878e0f96

--- front_end/ui/ShortcutRegistry.js
+++ front_end/ui/ShortcutRegistry.js
@@ -184,6 +184,7 @@ export class ShortcutRegistry {
   /**
    * @param {!Element} element
    * @param {!Object.<string, function():Promise.<boolean>>} handlers
+   * @return {function(!Event): void}
    */
   addShortcutListener(element, handlers) {
     // We only want keys for these specific actions to get handled this
@@ -194,7 +195,10 @@ export class ShortcutRegistry {
       allowlistKeyMap.addKeyMapping(shortcut.descriptors.map(descriptor => descriptor.key), shortcut.action);
     });
 
-    element.addEventListener('keydown', event => {
+    /**
+     * @param {!Event} event
+     */
+    const listener = event => {
       const key = KeyboardShortcut.makeKeyFromEvent(/** @type {!KeyboardEvent} */ (event));
       const keyMap = this._activePrefixKey ? allowlistKeyMap.getNode(this._activePrefixKey.key()) : allowlistKeyMap;
       if (!keyMap) {
@@ -203,7 +207,9 @@ export class ShortcutRegistry {
       if (keyMap.getNode(key)) {
         this.handleShortcut(/** @type {!KeyboardEvent} */ (event), handlers);
       }
-    });
+    };
+    element.addEventListener('keydown', listener);
+    return listener;
   }
 
   /**
