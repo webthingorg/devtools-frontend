4e369a04571c0ac1f1b4e9bbf0acfc1bdbad087c
chait pinnamaneni <cpinnamaneni@microsoft.com>
2020-10-16 12:37:11 -0700

    Service worker button to show network panel in drawer.
    Explainer for this feature is https://docs.google.com/document/d/14qJ1rZyMtnIa8hPJc1V8DJRxk4lVI3hlWYGvBDICog0/edit?ts=5f7c13ce
    
    Button to show network panel in drawer
    https://i.imgur.com/vnStScF.png
    
    Network panel in drawer
    https://i.imgur.com/3SmdLFK.png
    
    Closing the drawer will move it back to main tabs unless user moved it to bottom.
    
    Change-Id: I7af563869304da912bf10a85376835b9b8e0417a

--- front_end/ui/InspectorView.js
+++ front_end/ui/InspectorView.js
@@ -242,6 +242,13 @@ export class InspectorView extends VBox {
   }
 
   /**
+   * @param {boolean} isDrawerOpen
+   */
+  _emitDrawerOpenEvent(isDrawerOpen) {
+    Common.EventTarget.fireEvent(Events.DrawerChange, {isDrawerOpen}, document.body);
+  }
+
+  /**
    * @param {string} tabId
    * @return {?TabbedPane}
    */
@@ -281,6 +288,7 @@ export class InspectorView extends VBox {
     } else {
       this._focusRestorer = null;
     }
+    this._emitDrawerOpenEvent(true);
   }
 
   /**
@@ -298,6 +306,8 @@ export class InspectorView extends VBox {
       this._focusRestorer.restore();
     }
     this._drawerSplitWidget.hideSidebar(true);
+
+    this._emitDrawerOpenEvent(false);
   }
 
   /**
@@ -536,3 +546,8 @@ export class InspectorViewTabDelegate {
     }
   }
 }
+
+/** @enum {string} */
+export const Events = {
+  DrawerChange: 'drawerchange',
+};
