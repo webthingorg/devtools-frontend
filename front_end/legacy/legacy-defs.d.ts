c699e70b4615e648da7ab917ee9b762c3b436996
Andres Olivares <andoli@chromium.org>
2020-10-19 12:01:08 -0500

    [ts] Typecheck data-grid/DataGrid.js with TypeScript
    Bug: chromium:1011811
    
    
    Revert "Make template type of data_grid/DataGrid.js more specific to prepare for migragtion to TS"
    
    This reverts commit 06559156297a283063d43dadc1d3b2e2dc107764.
    
    Change-Id: I47191f2b5bb2ca315fca352ede47721ce3358387

--- front_end/legacy/legacy-defs.d.ts
+++ front_end/legacy/legacy-defs.d.ts
@@ -145,6 +145,7 @@ interface Event {
 
 interface Node {
   enclosingNodeOrSelfWithClass(className: string, stayWithin?: Element): Element;
+  enclosingNodeOrSelfWithNodeName(nodeName: string): Node|null;
   getComponentRoot(): Document|DocumentFragment|null;
   getComponentSelection(): Selection|null;
   hasSameShadowRoot(other: Node): boolean;
