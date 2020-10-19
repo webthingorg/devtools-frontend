504614833c745da2359ab7c3faa7c3c121c1b76d
Tim van der Lippe <tvanderlippe@chromium.org>
2020-10-19 15:38:53 +0100

    Move startup files to GN
    There previously were two special files that were handled
    by `build_release_applications`: root.js and RuntimeInstantiator.js.
    Both files are explicitly part of the startup process of DevTools
    and its various entrypoints.
    
    To remove the copying from `build_release_applications`, we have
    to move these to the relevant `devtools_entrypoint`. Since a
    `devtools_entrypoints` bundles all subdirectories, we can't keep
    these files in `front_end/` directly. Instead, we move these files
    to `startup/` to denote their special-casing in the startup process.
    
    Next to that, we have to fix all usages of these files in the entrypoints.
    For all JavaScript entrypoint files, all side-effect legacy files
    that are loaded by `startup.js` (previously known as `root.js` and renamed
    to prevent confusion with the `root/` module) are removed. All
    "additional" legacy files that a particular entrypoint requires are
    still loaded as-is.
    
    The RuntimeInstantiator is moved to become an implementation detail
    of `startup/`. Therefore, all of the usages that were previously
    importing from `RuntimeInstantiator` now import via `startup.js`.
    
    In the end, the special-casing of these files are removed and renamed
    for clarity. In the future, we want to remove the complicated
    entrypoints startup process, but we are not ready for that yet.
    That will require additional cleanups with `resources` in `module.json`
    before that change can happen.
    
    R=aerotwist@chromium.org
    
    Bug: 1131500
    Change-Id: I198d5a62d2aab70f842c68d9b4c0871fde1587a4

--- front_end/shell.js
+++ front_end/shell.js
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './RuntimeInstantiator.js';
+import './startup/startup.js';
 // We generate the descriptors in this file, which depend on the runtime.
