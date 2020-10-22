284664676ce5811ab49516ed78f93fe0d0c32e6e
Kim-Anh Tran <kimanh@chromium.org>
2020-10-22 14:21:36 +0200

    Add linear memory inspector viewer component
    This adds the viewer component of the linear memory inspector that
    allows the user to view the content of an array buffer.
    
    Bug: chromium:1110202
    Change-Id: I8ebc36f09a5b15516df54895bb16b2fcd47310b1

--- test/unittests/front_end/linear_memory_inspector/LinearMemoryInspector_test.ts
+++ test/unittests/front_end/linear_memory_inspector/LinearMemoryInspector_test.ts
@@ -5,37 +5,54 @@
 import {LinearMemoryInspector} from '../../../../front_end/linear_memory_inspector/LinearMemoryInspector.js';
 import {toHexString} from '../../../../front_end/linear_memory_inspector/LinearMemoryInspectorUtils.js';
 import {LinearMemoryNavigator} from '../../../../front_end/linear_memory_inspector/LinearMemoryNavigator.js';
+import {LinearMemoryViewer} from '../../../../front_end/linear_memory_inspector/LinearMemoryViewer.js';
 import {assertElement, assertShadowRoot, renderElementIntoDOM} from '../helpers/DOMHelpers.js';
 
 const {assert} = chai;
 
 describe('LinearMemoryInspector', () => {
-  it('renders navigator', async () => {
-    const component = new LinearMemoryInspector();
-    renderElementIntoDOM(component);
-    const size = 128;
-    const memory = [];
-    for (let i = 0; i < size; ++i) {
-      memory[i] = i;
+  describe('renders the', () => {
+    function setUpComponent() {
+      const component = new LinearMemoryInspector();
+      renderElementIntoDOM(component);
+      const size = 128;
+      const memory = [];
+      for (let i = 0; i < size; ++i) {
+        memory[i] = i;
+      }
+      component.data = {
+        memory: new Uint8Array(memory),
+        address: 20,
+      };
+      return component;
     }
-    component.data = {
-      memory: new Uint8Array(memory),
-      address: 20,
-    };
 
-    const shadowRoot = component.shadowRoot;
-    assertShadowRoot(shadowRoot);
-    const input = shadowRoot.querySelector('devtools-linear-memory-inspector-navigator');
-    assertElement(input, LinearMemoryNavigator);
+    it('navigator component', async () => {
+      const component = setUpComponent();
+      const shadowRoot = component.shadowRoot;
+      assertShadowRoot(shadowRoot);
+      const input = shadowRoot.querySelector('devtools-linear-memory-inspector-navigator');
+      assertElement(input, LinearMemoryNavigator);
+    });
+
+    it('viewer component', async () => {
+      const component = setUpComponent();
+      const shadowRoot = component.shadowRoot;
+      assertShadowRoot(shadowRoot);
+      const input = shadowRoot.querySelector('devtools-linear-memory-inspector-viewer');
+      assertElement(input, LinearMemoryViewer);
+    });
   });
 
-  it('can format a hexadecimal number', async () => {
-    const number = 23;
-    assert.strictEqual(toHexString(number, 0), '17');
-  });
+  describe('formats', () => {
+    it('a hexadecimal number', async () => {
+      const number = 23;
+      assert.strictEqual(toHexString(number, 0), '17');
+    });
 
-  it('can format a hexadecimal number and add padding', async () => {
-    const decimalNumber = 23;
-    assert.strictEqual(toHexString(decimalNumber, 5), '00017');
+    it('a hexadecimal number and add padding', async () => {
+      const decimalNumber = 23;
+      assert.strictEqual(toHexString(decimalNumber, 5), '00017');
+    });
   });
 });
