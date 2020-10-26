179487e1080fbd5ae08cbedf43d71515fc0eb75a
Philip Pfaffe <pfaffe@chromium.org>
2020-10-26 12:55:57 +0100

    Add a DebuggerPaused DOM event and some tooling to await it in tests
    I will use it to get rid of a timeout() in the sources panel tests, but
    that will be a followup CL to simplify reverting it if necessary.
    
    Change-Id: I4362135e1dac306d67f29474d1d6aea4ef0f1102

--- front_end/sdk/DebuggerModel.js
+++ front_end/sdk/DebuggerModel.js
@@ -755,6 +755,8 @@ export class DebuggerModel extends SDKModel {
       } else {
         this.stepInto();
       }
+    } else {
+      Common.EventTarget.fireEvent('DevTools.DebuggerPaused');
     }
 
     _scheduledPauseOnAsyncCall = null;
