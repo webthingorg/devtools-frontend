cc089923c8e1e54319317505b09efaf1c9173180
Andres Olivares <andoli@chromium.org>
2020-10-26 08:36:03 -0500

    Migrate Map.proto.inverse into MapUtilities.
    Bug: 1050549
    Change-Id: If3b142918be6a4e01bd899d685671a5018c5e0c6

--- front_end/platform/utilities.js
+++ front_end/platform/utilities.js
@@ -455,18 +455,6 @@ Set.prototype.firstValue = function() {
 };
 
 /**
- * @return {!Platform.Multimap<!KEY, !VALUE>}
- */
-Map.prototype.inverse = function() {
-  const result = new Platform.Multimap();
-  for (const key of this.keys()) {
-    const value = this.get(key);
-    result.set(value, key);
-  }
-  return result;
-};
-
-/**
  * @template K, V
  */
 export class Multimap {
