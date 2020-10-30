b42d18be6e55f8a2b4f9e202e6421bbd67c35a09
Jan Scheffler <janscheffler@chromium.org>
2020-10-30 12:15:11 +0100

    [Recorder] Add recording to DevTools
    Bug: chromium:1144127
    Change-Id: I58a5add98bf08a8113ecdf674e08911a79f7b12b

---
+++ front_end/recorder/recorder.js
@@ -0,0 +1,9 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as RecorderModel from './RecorderModel.js';
+import * as RecordingFileSystem from './RecordingFileSystem.js';
+
+export const project = RecordingFileSystem.project;
+export {RecordingFileSystem, RecorderModel};
