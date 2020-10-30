aad62ccf640d2231858dd092a4a72f6357f451aa
Paul Lewis <aerotwist@chromium.org>
2020-10-30 11:45:25 +0000

    [WIP] Support fake connections to the backend
    Many unit tests are blocked by the fact that they require the SDK but
    the SDK requires a live connection to the backend. This CL introduces a
    fake connection that can be used instead, and instantiates an
    SDK.CookieModel.CookieModel to demonstrate how it can be used.
    
    While this CL has side-effects, due to other migrations they are far
    less problematic. Specifically, creating a Root.Runtime.Runtime.instance
    and a Common.Settings.Settings.instance is now overwritable thanks to
    the forceNew flag (whereas previously it was not). This means that
    anyone who wishes to reinstantiate these entities for a different test
    can do so easily. Nonetheless there is clean-up code here for those
    instances to remove them at the end of the test.
    
    Change-Id: I6734c7f69f4f6b8e6433976aa5f38a3dd2e2f99a

---
+++ test/unittests/front_end/sdk/CookieModel_test.ts
@@ -0,0 +1,46 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+
+import * as TestConnection from '../helpers/TestConnection.js';
+import {Protocol} from '../../../../front_end/protocol_client/protocol_client.js';
+import {CookieModel} from '../../../../front_end/sdk/CookieModel.js';
+
+describe('CookieModel', () => {
+  before(() => {
+    TestConnection.enable();
+  });
+
+  after(() => {
+    TestConnection.disable();
+  });
+
+  it('can retrieve cookies', async () => {
+    // CDP Connection mock: for Network.getCookies, respond with a single cookie.
+    TestConnection.setResponseHandler('Network.getCookies', () => {
+      return {
+        cookies: [{
+          domain: '.example.com',
+          name: 'name',
+          path: '/test',
+          size: 23,
+          value: 'value',
+          expires: 42,
+          httpOnly: false,
+          secure: false,
+          session: true,
+          priority: Protocol.Network.CookiePriority.Medium,
+        }],
+      };
+    });
+
+    const target = TestConnection.createTarget();
+    const model = new CookieModel(target);
+    const cookies = await model.getCookies(['https://www.google.com']);
+    assert.isArray(cookies);
+    assert.lengthOf(cookies, 1);
+    assert.strictEqual(cookies[0].domain(), '.example.com');
+  });
+});
