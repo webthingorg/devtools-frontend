73ec437230bfd5f519b35fa7d46871b63ce97825
Paul Lewis <aerotwist@chromium.org>
2020-11-02 16:33:18 +0000

    TypeScriptify AccessibilitySubPane.js
    R=tvanderlippe@chromium.org
    
    Bug: 1011811
    Change-Id: I7b315743deb213d62748236b6e038162c13fbb77

--- front_end/accessibility/AccessibilitySubPane.js
+++ front_end/accessibility/AccessibilitySubPane.js
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 import * as SDK from '../sdk/sdk.js';  // eslint-disable-line no-unused-vars
 import * as UI from '../ui/ui.js';
 
@@ -38,7 +35,7 @@ export class AccessibilitySubPane extends UI.View.SimpleView {
    * @return {?SDK.DOMModel.DOMNode}
    */
   node() {
-    return this._node;
+    return this._node || null;
   }
 
   /**
