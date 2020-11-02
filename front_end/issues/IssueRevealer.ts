d7f54773648527e292c2ce1a5eaf2d28e64a4d2c
Tim van der Lippe <tvanderlippe@chromium.org>
2020-11-02 12:31:19 +0000

    Use TypeScript-authored files in issues/
    Since we have changed `build_release_applications` to ignore the files
    listed in the `modules` array (
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2461783)
    the files listed in `modules` are only used for Closure. This means
    that if we remove the files from `modules`, Closure will no longer
    know about it.
    
    There is one caveat: the runtime still does the contains-check for
    the legacy file. Therefore, we have to include the `issues-legacy.js`
    file in the `modules` array (until we have removed its extensions, see
    crbug.com/1134103). To ignore Closure warnings, we also have to
    add the legacy file to the `skip_compilations` array.
    
    Note that we can only do this for modules which satisfy these
    requirements:
    1. They are referenced directly by the entrypoint JSON files
    2. There are no modules that are still type checked by Closure
    that depend on this module
    
    R=aerotwist@chromium.org,szuend@chromium.org
    
    Bug: 1011811, 1127902
    Change-Id: I348f2735312fc6147915a88b1377e85c02161d8a

--- front_end/issues/IssueRevealer.ts
+++ front_end/issues/IssueRevealer.ts
@@ -2,29 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Common from '../common/common.js';  // eslint-disable-line no-unused-vars
+import type * as Common from '../common/common.js';
 import * as SDK from '../sdk/sdk.js';
 import * as UI from '../ui/ui.js';
-import {IssuesPaneImpl} from './IssuesPane.js';  // eslint-disable-line no-unused-vars
+import type {IssuesPaneImpl} from './IssuesPane.js';
 
-/**
- * @implements {Common.Revealer.Revealer}
- * @unrestricted
- */
-export class IssueRevealer {
-  /**
-   * @override
-   * @param {!Object} issue
-   * @return {!Promise<void>}
-   */
-  async reveal(issue) {
+export class IssueRevealer implements Common.Revealer.Revealer {
+  async reveal(issue: unknown): Promise<void> {
     if (!(issue instanceof SDK.Issue.Issue)) {
       throw new Error('Internal error: not a issue');
     }
     await UI.ViewManager.ViewManager.instance().showView('issues-pane');
     const view = UI.ViewManager.ViewManager.instance().view('issues-pane');
     if (view) {
-      const issuesPane = /** @type {!IssuesPaneImpl} */ (await view.widget());
+      const issuesPane = (await view.widget()) as IssuesPaneImpl;
       issuesPane.revealByCode(issue.code());
     }
   }
