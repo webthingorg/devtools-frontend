b7fd5478f5562578a1d8ec79063206c5255f5495
Sigurd Schneider <sigurds@chromium.org>
2020-11-04 16:06:53 +0100

    DevTools: Allow users to override storage quota size.
    This patch adds an interface at DevTools>Application to allow users to
    override the quota size for the current origin. At the backend, the
    QuotaManager maintains a map from origins to emulated quota size.
    
    Corresponding Chromium CL: https://crrev.com/c/2122473
    Doc: https://docs.google.com/document/d/1gSZRjwgz4T0Dl8GSrN8BTFKVvw8-dCehIQOJsBVsOgc/edit#
    Bug:945786
    Change-Id: I2d19d7b749ebec26202039f29477145299904a41

--- front_end/resources/ClearStorageView.js
+++ front_end/resources/ClearStorageView.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import * as Common from '../common/common.js';
+import * as i18n from '../i18n/i18n.js';
 import * as PerfUI from '../perf_ui/perf_ui.js';
 import * as Platform from '../platform/platform.js';
 import * as SDK from '../sdk/sdk.js';
@@ -12,6 +13,28 @@ import {ApplicationCacheModel} from './ApplicationCacheModel.js';
 import {DatabaseModel} from './DatabaseModel.js';
 import {DOMStorageModel} from './DOMStorageModel.js';
 import {IndexedDBModel} from './IndexedDBModel.js';
+
+export const UIStrings = {
+  /**
+   * @description Text in the Storage View that expresses the amout of used and available storage quota
+   * @example {1.5 MB} PH1
+   * @example {123.1 MB} PH2
+   */
+  storageQuotaUsed: '{PH1} used out of {PH2} storage quota',
+  /**
+   * @description Tooltip in the Storage View that expresses the precise amout of used and available storage quota
+   * @example {200} PH1
+   * @example {400} PH2
+   */
+  storageQuotaUsedWithBytes: '{PH1} bytes used out of {PH2} bytes storage quota',
+  /**
+   * @description Fragment indicating that a certain data size has been custom configured
+   * @example {1.5 MB} PH1
+   */
+  storageWithCustomMarker: '{PH1} (custom)',
+};
+const str_ = i18n.i18n.registerUIStrings('coverage/CoverageView.js', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 
 /**
  * @implements {SDK.SDKModel.Observer}
@@ -32,7 +55,7 @@ export class ClearStorageView extends UI.ThrottledWidget.ThrottledWidget {
       [types.Websql, 'rgb(203, 220, 56)'],           // lime
     ]);
 
-    this._reportView = new UI.ReportView.ReportView(Common.UIString.UIString('Clear storage'));
+    this._reportView = new UI.ReportView.ReportView(Common.UIString.UIString('Storage'));
     this._reportView.registerRequiredCSS('resources/clearStorageView.css', {enableLegacyPatching: true});
     this._reportView.element.classList.add('clear-storage-header');
     this._reportView.show(this.contentElement);
@@ -48,6 +71,7 @@ export class ClearStorageView extends UI.ThrottledWidget.ThrottledWidget {
 
     const quota = this._reportView.appendSection(Common.UIString.UIString('Usage'));
     this._quotaRow = quota.appendSelectableRow();
+    this._quotaRow.classList.add('quota-usage-row');
     const learnMoreRow = quota.appendRow();
     const learnMore = UI.XLink.XLink.create(
         'https://developers.google.com/web/tools/chrome-devtools/progressive-web-apps#opaque-responses',
@@ -59,6 +83,33 @@ export class ClearStorageView extends UI.ThrottledWidget.ThrottledWidget {
     const usageBreakdownRow = quota.appendRow();
     usageBreakdownRow.classList.add('usage-breakdown-row');
     usageBreakdownRow.appendChild(this._pieChart);
+
+    this._quotaOverridden = false;
+    /** @type {string} */
+    this._previousOverrideFieldValue = '';
+    const quotaOverrideCheckboxRow = quota.appendRow();
+    this._quotaOverrideCheckbox = UI.UIUtils.CheckboxLabel.create('Simulate custom storage quota', false, '');
+    quotaOverrideCheckboxRow.appendChild(this._quotaOverrideCheckbox);
+    this._quotaOverrideCheckbox.checkboxElement.addEventListener('click', this._onClickCheckbox.bind(this), false);
+    this._quotaOverrideControlRow = quota.appendRow();
+    /** @type {!HTMLInputElement} */
+    this._quotaOverrideEditor = /** @type {!HTMLInputElement} */ (
+        this._quotaOverrideControlRow.createChild('input', 'quota-override-notification-editor'));
+    this._quotaOverrideControlRow.appendChild(UI.UIUtils.createLabel(Common.UIString.UIString('MB')));
+    this._quotaOverrideControlRow.classList.add('hidden');
+    this._quotaOverrideEditor.addEventListener('keyup', event => {
+      if (isEnterKey(event)) {
+        this._applyQuotaOverrideFromInputField();
+        event.consume(true);
+      }
+    });
+    this._quotaOverrideEditor.addEventListener('focusout', event => {
+      this._applyQuotaOverrideFromInputField();
+      event.consume(true);
+    });
+
+    const errorMessageRow = quota.appendRow();
+    this._quotaOverrideErrorMessage = errorMessageRow.createChild('div', 'quota-override-error');
 
     const clearButtonSection = this._reportView.appendSection('', 'clear-storage-button').appendRow();
     this._clearButton = UI.UIUtils.createTextButton(ls`Clear site data`, this._clear.bind(this));
@@ -137,7 +188,8 @@ export class ClearStorageView extends UI.ThrottledWidget.ThrottledWidget {
    * @param {string} mainOrigin
    * @param {?string} unreachableMainOrigin
    */
-  _updateOrigin(mainOrigin, unreachableMainOrigin) {
+  async _updateOrigin(mainOrigin, unreachableMainOrigin) {
+    const oldOrigin = this._securityOrigin;
     if (unreachableMainOrigin) {
       this._securityOrigin = unreachableMainOrigin;
       this._reportView.setSubtitle(ls`${unreachableMainOrigin} (failed to load)`);
@@ -146,7 +198,85 @@ export class ClearStorageView extends UI.ThrottledWidget.ThrottledWidget {
       this._reportView.setSubtitle(mainOrigin);
     }
 
+    if (oldOrigin !== this._securityOrigin) {
+      this._quotaOverrideControlRow.classList.add('hidden');
+      this._quotaOverrideCheckbox.checkboxElement.checked = false;
+      this._quotaOverrideErrorMessage.textContent = '';
+      if (this._target && oldOrigin) {
+        // TODO(chromim:945786): Properly withdraw from upholding the quota instead of resetting it.
+        await this._clearQuotaForOrigin(this._target, oldOrigin);
+      }
+    }
     this.doUpdate();
+  }
+
+  async _applyQuotaOverrideFromInputField() {
+    this._quotaOverrideErrorMessage.textContent = '';
+    const editorString = this._quotaOverrideEditor.value;
+    if (!editorString && this._target) {
+      await this._overrideQuotaForOrigin(this._target, this._securityOrigin, 0);
+      return;
+    }
+    const quota = parseFloat(editorString);
+    if (!Number.isFinite(quota)) {
+      this._quotaOverrideErrorMessage.textContent = ls`Please enter a number`;
+      return;
+    }
+    if (quota < 0) {
+      this._quotaOverrideErrorMessage.textContent = ls`Number must be non-negative`;
+      return;
+    }
+    const bytesPerMB = 1000 * 1000;
+    const quotaInBytes = Math.round(quota * bytesPerMB);
+    const quotaFieldValue = `${quotaInBytes / bytesPerMB}`;
+    this._quotaOverrideEditor.value = quotaFieldValue;
+    this._previousOverrideFieldValue = quotaFieldValue;
+    if (this._target) {
+      await this._overrideQuotaForOrigin(this._target, this._securityOrigin, quotaInBytes);
+    }
+  }
+
+  /**
+   * @param {!SDK.SDKModel.Target} target
+   * @param {?string} origin
+   * @param {number} quotaInBytes
+   */
+  async _overrideQuotaForOrigin(target, origin, quotaInBytes) {
+    if (!origin) {
+      return;
+    }
+
+    let quotaSize;
+    if (quotaInBytes && quotaInBytes > -1) {
+      quotaSize = quotaInBytes;
+    }
+    await target.storageAgent().invoke_overrideQuotaForOrigin({origin, quotaSize});
+  }
+
+  /**
+   * @param {!SDK.SDKModel.Target} target
+   * @param {string} origin
+   */
+  async _clearQuotaForOrigin(target, origin) {
+    await target.storageAgent().invoke_overrideQuotaForOrigin({origin});
+  }
+
+  async _onClickCheckbox() {
+    if (this._quotaOverrideControlRow.classList.contains('hidden')) {
+      this._quotaOverrideControlRow.classList.remove('hidden');
+      this._quotaOverrideCheckbox.checkboxElement.checked = true;
+      this._quotaOverrideEditor.value = this._previousOverrideFieldValue;
+      this._quotaOverrideEditor.focus();
+    } else if (this._target) {
+      this._quotaOverrideControlRow.classList.add('hidden');
+      this._quotaOverrideCheckbox.checkboxElement.checked = false;
+      const securityOriginManager = /** @type {!SDK.SecurityOriginManager.SecurityOriginManager} */ (
+          this._target.model(SDK.SecurityOriginManager.SecurityOriginManager));
+      const origin = securityOriginManager.mainSecurityOrigin();
+      await this._overrideQuotaForOrigin(this._target, origin, 0);
+      this._quotaOverridden = false;
+      this._quotaOverrideErrorMessage.textContent = '';
+    }
   }
 
   _clear() {
@@ -245,15 +375,25 @@ export class ClearStorageView extends UI.ThrottledWidget.ThrottledWidget {
 
     const securityOrigin = /** @type {string} */ (this._securityOrigin);
     const response = await this._target.storageAgent().invoke_getUsageAndQuota({origin: securityOrigin});
+    this._quotaRow.textContent = '';
     if (response.getError()) {
-      this._quotaRow.textContent = '';
       this._populatePieChart(0, []);
       return;
     }
-    this._quotaRow.textContent = Common.UIString.UIString(
-        '%s used out of %s storage quota.\xA0', Platform.NumberUtilities.bytesToString(response.usage),
-        Platform.NumberUtilities.bytesToString(response.quota));
-    if (response.quota < 125829120) {  // 120 MB
+    this._quotaOverridden = response.overrideActive;
+    const quotaAsString = Platform.NumberUtilities.bytesToString(response.quota);
+    const usageAsString = Platform.NumberUtilities.bytesToString(response.usage);
+    const formattedQuotaAsString = i18nString(UIStrings.storageWithCustomMarker, {PH1: quotaAsString});
+    const quota =
+        this._quotaOverridden ? UI.Fragment.Fragment.build`<b>${formattedQuotaAsString}</b>`.element() : quotaAsString;
+    const element =
+        i18n.i18n.getFormatLocalizedString(str_, UIStrings.storageQuotaUsed, {PH1: usageAsString, PH2: quota});
+    this._quotaRow.appendChild(element);
+    this._quotaRow.title = i18nString(
+        UIStrings.storageQuotaUsedWithBytes,
+        {PH1: response.usage.toLocaleString(), PH2: response.quota.toLocaleString()});
+
+    if (!response.overrideActive && response.quota < 125829120) {  // 120 MB
       this._quotaRow.title = ls`Storage quota is limited in Incognito mode`;
       this._quotaRow.appendChild(UI.Icon.Icon.create('smallicon-info'));
     }
