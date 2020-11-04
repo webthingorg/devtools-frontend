21afcdd9fadc0135511790a27f042c006e71dd49
Johan Bay <jobay@google.com>
2020-11-04 20:41:11 +0100

    Enable flaky network datagrid tests
    Change-Id: Ic6fb9f1e4abe474083d670736d6602ddc091fa6b

--- test/e2e/network/network-datagrid_test.ts
+++ test/e2e/network/network-datagrid_test.ts
@@ -4,7 +4,7 @@
 
 import {assert} from 'chai';
 
-import {click, getBrowserAndPages, step, waitFor, waitForAria} from '../../shared/helper.js';
+import {click, getBrowserAndPages, step, waitFor, waitForAria, waitForFunction} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {navigateToNetworkTab, waitForSomeRequestsToAppear} from '../helpers/network-helpers.js';
 
@@ -22,12 +22,13 @@ describe('The Network Tab', async () => {
   });
 
   // Flaky test
-  it.skip('[crbug.com/1066813] shows Last-Modified', async () => {
-    const {frontend, target} = getBrowserAndPages();
+  /* eslint-disable-next-line rulesdir/no_repeated_tests */
+  it.repeat(100, '[crbug.com/1066813] shows Last-Modified', async () => {
+    const {target, frontend} = getBrowserAndPages();
     await navigateToNetworkTab('last-modified.html');
 
     // Reload to populate network request table
-    await target.reload();
+    await target.reload({waitUntil: 'networkidle0'});
 
     await step('Wait for the column to show up and populate its values', async () => {
       await waitForSomeRequestsToAppear(2);
@@ -47,23 +48,30 @@ describe('The Network Tab', async () => {
       await click(lastModified);
     });
 
-    const lastModifiedColumnValues = await frontend.evaluate(() => {
-      return Array.from(document.querySelectorAll('.last-modified-column')).map(message => message.textContent);
+    await step('Wait for the "Last-Modified" column to have the expected values', async () => {
+      let lastModifiedColumnValues: string[] = [];
+      const expectedValues = ['Last-Modified', '', 'Sun, 26 Sep 2010 22:04:35 GMT'];
+      await waitForFunction(async () => {
+        lastModifiedColumnValues = await frontend.$$eval(
+            'pierce/.last-modified-column',
+            cells => cells.map(element => element.textContent as string),
+        );
+        return JSON.stringify(lastModifiedColumnValues) === JSON.stringify(expectedValues);
+      });
     });
-
-    assert.deepEqual(lastModifiedColumnValues, [
-      'Last-Modified',
-      '',
-      'Sun, 26 Sep 2010 22:04:35 GMT',
-    ]);
   });
 
   // Flaky test
-  it.skip('[crbug.com/1066813] shows the HTML response including cyrillic characters with utf-8 encoding', async () => {
+  /* eslint-disable-next-line rulesdir/no_repeated_tests */
+  it.repeat(100, '[crbug.com/1066813] shows the HTML response including cyrillic characters with utf-8 encoding', async () => {
+    const {target} = getBrowserAndPages();
     await navigateToNetworkTab('utf-8.rawresponse');
 
+    // Reload to populate network request table
+    await target.reload();
+
     // Wait for the column to show up and populate its values
-    await waitForSomeRequestsToAppear(2);
+    await waitForSomeRequestsToAppear(1);
 
     // Open the HTML file that was loaded
     await click('td.name-column');
@@ -83,17 +91,18 @@ describe('The Network Tab', async () => {
   });
 
   // Flaky test
-  it.skip('[crbug.com/1066813] shows the correct MIME type when resources came from HTTP cache', async () => {
+  /* eslint-disable-next-line rulesdir/no_repeated_tests */
+  it.repeat(100, '[crbug.com/1066813] shows the correct MIME type when resources came from HTTP cache', async () => {
     const {target, frontend} = getBrowserAndPages();
 
     await navigateToNetworkTab('resources-from-cache.html');
 
-    // Wait for the column to show up and populate its values
-    await waitForSomeRequestsToAppear(3);
-
     // Reload the page without a cache, to force a fresh load of the network resources
     await click('[aria-label="Disable cache"]');
-    await target.reload({waitUntil: 'networkidle2'});
+    await target.reload({waitUntil: 'networkidle0'});
+
+    // Wait for the column to show up and populate its values
+    await waitForSomeRequestsToAppear(2);
 
     // Get the size of the first two network request responses (excluding header and favicon.ico).
     const getNetworkRequestSize = () => frontend.evaluate(() => {
@@ -117,7 +126,9 @@ describe('The Network Tab', async () => {
 
     // Allow resources from the cache again and reload the page to load from cache
     await click('[aria-label="Disable cache"]');
-    await target.reload({waitUntil: 'networkidle2'});
+    await target.reload({waitUntil: 'networkidle0'});
+    // Wait for the column to show up and populate its values
+    await waitForSomeRequestsToAppear(2);
 
     assert.deepEqual(await getNetworkRequestSize(), [
       `${formatByteSize(338)}${formatByteSize(219)}`,
