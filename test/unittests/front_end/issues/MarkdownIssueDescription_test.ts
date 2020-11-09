6d37b34bc660817cc8d82c089e58367f31449ac6
Simon Zünd <szuend@chromium.org>
2020-11-09 09:16:41 +0100

    [issues] Introduce substitutions in markdown issue descriptions
    This CL enables dynamic replacements in markdown issue descriptions.
    This can be used to insert information relating to a specific issue,
    e.g. a violating URL or a offending CSP directive.
    
    The code in this CL is very likely to be replaced with a l10n
    placeholder mechanism, once a translation pipline for markdown
    issue descriptions is in place.
    
    R=sigurds@chromium.org
    
    Bug: chromium:1125465
    Change-Id: Id71369a1fe2b485fdeea21c8b39bb0d8de3295b7

--- test/unittests/front_end/issues/MarkdownIssueDescription_test.ts
+++ test/unittests/front_end/issues/MarkdownIssueDescription_test.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {createIssueDescriptionFromRawMarkdown} from '../../../../front_end/issues/MarkdownIssueDescription.js';
+import {createIssueDescriptionFromRawMarkdown, substitutePlaceholders} from '../../../../front_end/issues/MarkdownIssueDescription.js';
 import {IssueKind} from '../../../../front_end/sdk/Issue.js';
 
 const {assert} = chai;
@@ -10,6 +10,7 @@ const {assert} = chai;
 describe('createIssueDescriptionFromMarkdown', () => {
   const emptyMarkdownDescription = {
     file: '<unused>',
+    substitutions: undefined,
     issueKind: IssueKind.BreakingChange,
     links: [],
   };
@@ -25,5 +26,33 @@ describe('createIssueDescriptionFromMarkdown', () => {
     const invalidIssueDescription = 'Just some text, but the heading is missing!';
 
     assert.throws(() => createIssueDescriptionFromRawMarkdown(invalidIssueDescription, emptyMarkdownDescription));
+  });
+});
+
+describe('substitutePlaceholders', () => {
+  it('returns the input as-is, with no placeholders present in the input', () => {
+    const str = 'Example string with no placeholders';
+
+    assert.strictEqual(substitutePlaceholders(str), str);
+  });
+
+  it('subsitutes a single placeholder', () => {
+    const str = 'Example string with a single {placeholder}';
+
+    const actual = substitutePlaceholders(str, new Map<string, string>([['placeholder', 'fooholder']]));
+    assert.strictEqual(actual, 'Example string with a single fooholder');
+  });
+
+  it('substitutes multiple placeholders', () => {
+    const str = 'Example string with two placeholders, \'{ph1}\' and \'{ph2}\'.';
+
+    const actual = substitutePlaceholders(str, new Map<string, string>([['ph1', 'foo'], ['ph2', 'bar']]));
+    assert.strictEqual(actual, 'Example string with two placeholders, \'foo\' and \'bar\'.');
+  });
+
+  it('ignores placeholder syntax when the map doesn\'t contain a replacement', () => {
+    const str = 'Example string where {placeholder} syntax is part of the text.';
+
+    assert.strictEqual(substitutePlaceholders(str), str);
   });
 });
