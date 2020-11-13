6f88796ac4b693a9e63460b7837a2dbb2100dce2
Kim-Anh Tran <kimanh@chromium.org>
2020-11-13 07:18:15 +0100

    [LMI] Add settings view to linear memory inspector
    This adds the ValueInterpreterSettings which allows the user to
    select the value types to show.
    
    Screenshot: https://imgur.com/a/CFovWk5
    
    Bug: chromium:1110202
    Change-Id: Id7e7a781281efbc8ae09fdfc544724f4bf104a41

--- front_end/linear_memory_inspector/LinearMemoryValueInterpreter.ts
+++ front_end/linear_memory_inspector/LinearMemoryValueInterpreter.ts
@@ -3,18 +3,20 @@
 // found in the LICENSE file.
 
 import './ValueInterpreterDisplay.js';
+import './ValueInterpreterSettings.js';
 
 import * as Elements from '../elements/elements.js';
 import * as LitHtml from '../third_party/lit-html/lit-html.js';
 
 import type {ValueDisplayData} from './ValueInterpreterDisplay.js';
 import {Endianness, ValueType, ValueTypeMode} from './ValueInterpreterDisplayUtils.js';
+import type {TypeToggleEvent, ValueInterpreterSettingsData} from './ValueInterpreterSettings.js';
 
 const {render, html} = LitHtml;
 
 export interface LinearMemoryValueInterpreterData {
   value: ArrayBuffer;
-  valueTypes: ValueType[];
+  valueTypes: Set<ValueType>;
   endianness: Endianness;
   valueTypeModes?: Map<ValueType, ValueTypeMode>;
 }
@@ -23,8 +25,9 @@ export class LinearMemoryValueInterpreter extends HTMLElement {
   private readonly shadow = this.attachShadow({mode: 'open'});
   private endianness = Endianness.Little;
   private buffer = new ArrayBuffer(0);
-  private valueTypes: ValueType[] = [];
+  private valueTypes: Set<ValueType> = new Set();
   private valueTypeModeConfig: Map<ValueType, ValueTypeMode> = new Map();
+  private showSettings = false;
 
   set data(data: LinearMemoryValueInterpreterData) {
     this.endianness = data.endianness;
@@ -49,6 +52,7 @@ export class LinearMemoryValueInterpreter extends HTMLElement {
           background-color: var(--toolbar-bg-color, #f3f3f3);
           overflow: hidden;
           --text-highlight-color: #80868b;
+          width: 400px;
         }
 
         .settings-toolbar {
@@ -79,26 +83,53 @@ export class LinearMemoryValueInterpreter extends HTMLElement {
               .data=${{iconName: 'dropdown_7x6_icon', color: 'rgb(110 110 110)', width: '7px'} as Elements.Icon.IconWithName}>
             </devtools-icon>
           </div>
-          <div class="settings-item">
+          <button data-settings="true" class="settings-item" @click=${this.onSettingsToggled}>
             <devtools-icon
               .data=${{iconName: 'settings_14x14_icon', color: 'rgb(110 110 110)', width: '14px'} as Elements.Icon.IconWithName}>
             </devtools-icon>
-          </div>
+          </button>
         </div>
         <span class="divider"></span>
-        <devtools-linear-memory-inspector-interpreter-display
-          .data=${{
-            buffer: this.buffer,
-            valueTypes: this.valueTypes,
-            endianness: this.endianness,
-            valueTypeModes: this.valueTypeModeConfig,
-          } as ValueDisplayData}>
-        </devtools-linear-memory-inspector-interpreter-display>
+        <div>
+          ${this.showSettings ?
+            html`
+              <devtools-linear-memory-inspector-interpreter-settings
+                .data=${{valueTypes: this.valueTypes} as ValueInterpreterSettingsData}
+                @type-toggle=${this.onTypeToggle}>
+              </devtools-linear-memory-inspector-interpreter-settings>` :
+            html`
+              <devtools-linear-memory-inspector-interpreter-display
+                .data=${{
+                  buffer: this.buffer,
+                  valueTypes: this.valueTypes,
+                  endianness: this.endianness,
+                  valueTypeModes: this.valueTypeModeConfig,
+                } as ValueDisplayData}>
+              </devtools-linear-memory-inspector-interpreter-display>`}
+        </div>
       </div>
     `,
         this.shadow, {eventContext: this},
     );
     // clang-format on
+  }
+
+  private onSettingsToggled() {
+    this.showSettings = !this.showSettings;
+    this.render();
+  }
+
+  private onTypeToggle(e: TypeToggleEvent) {
+    this.dispatchEvent(new ValueTypeToggleEvent(e.data.type, e.data.checked));
+  }
+}
+
+export class ValueTypeToggleEvent extends Event {
+  data: {type: ValueType, checked: boolean};
+
+  constructor(type: ValueType, checked: boolean) {
+    super('value-type-toggle');
+    this.data = {type, checked};
   }
 }
 
