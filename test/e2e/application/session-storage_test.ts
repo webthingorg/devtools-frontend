591a18a1241266114c40f08c561c7ff11ff22ed8
Jan Scheffler <janscheffler@chromium.org>
2020-11-13 16:23:25 +0100

    [e2etest] Move hosted mode server to https
    This allows testing things that require secure connections
    like thrid party cookies.
    
    Change-Id: Id7f6dc672f8a2e701f2baad22fa035826a8d7b2a

--- test/e2e/application/session-storage_test.ts
+++ test/e2e/application/session-storage_test.ts
@@ -12,7 +12,8 @@ let DOMAIN_SELECTOR: string;
 
 describe('The Application Tab', async () => {
   before(async () => {
-    DOMAIN_SELECTOR = `${SESSION_STORAGE_SELECTOR} + ol > [aria-label="http://localhost:${getHostedModeServerPort()}"]`;
+    DOMAIN_SELECTOR =
+        `${SESSION_STORAGE_SELECTOR} + ol > [aria-label="https://localhost:${getHostedModeServerPort()}"]`;
   });
 
   it('shows Session Storage keys and values', async () => {
@@ -37,10 +38,6 @@ describe('The Application Tab', async () => {
         {
           key: 'secondKey',
           value: '{"field":"complexValue","primitive":2}',
-        },
-        {
-          key: '',
-          value: '',
         },
       ]);
     });
