86a05150e44c8bbbff543522092b494406ad4dc9
Michael Liao <michael.liao@microsoft.com>
2020-11-13 19:17:59 -0800

    [Font Editor Prototype]: Implements Experimental Typography CSS Editor in Styles Pane
    Reference Document: https://docs.google.com/document/d/1zvmKDfdgVgL-QX2W4nKfgoeG_69YDF0AsLerIzL3rPE/edit?usp=sharing
    
    Explainer: https://docs.google.com/document/d/1TmvbZuJR4fHuViE0X23y_1kUT43qt98mdj5GZxWf_W0/edit
    
    Bug: 1093229
    Change-Id: I0a8d049bd564a5ba547466067a12185565af7820

--- front_end/ui/UIUtils.js
+++ front_end/ui/UIUtils.js
@@ -1291,6 +1291,36 @@ export function createInput(className, type) {
 }
 
 /**
+ * @param {string} name
+ * @param {!Array<!Map<string, !Array<string>>> | !Array<string> | !Set} options
+ * @return {!HTMLSelectElement}
+ */
+export function createSelect(name, options) {
+  const select = document.createElementWithClass('select', 'chrome-select');
+  ARIAUtils.setAccessibleName(select, name);
+  for (const option of options) {
+    if (option instanceof Map) {
+      for (const [key, value] of option) {
+        const optGroup = select.createChild('optgroup');
+        optGroup.label = key;
+        for (const child of value) {
+          if (typeof child === 'string') {
+            optGroup.appendChild(new Option(child, child));
+          } else if (child.text && child.value) {
+            optGroup.appendChild(new Option(child.text, child.value));
+          }
+        }
+      }
+    } else if (typeof option === 'string') {
+      select.add(new Option(option, option));
+    } else if (option.text && option.value) {
+      select.add(new Option(option.text, option.value));
+    }
+  }
+  return select;
+}
+
+/**
  * @param {string} title
  * @param {string=} className
  * @param {!Element=} associatedControl
@@ -1336,10 +1366,10 @@ export function createIconLabel(title, iconClass) {
 }
 
 /**
- * @return {!Element}
  * @param {number} min
  * @param {number} max
  * @param {number} tabIndex
+ * @return {!Element}
  */
 export function createSlider(min, max, tabIndex) {
   const element = createElement('span', 'dt-slider');
@@ -1482,7 +1512,7 @@ function radioClickHandler(event) {
 
 registerCustomElement('span', 'dt-icon-label', DevToolsIconLabel);
 
-class DevToolsSlider extends HTMLSpanElement {
+export class DevToolsSlider extends HTMLSpanElement {
   constructor() {
     super();
     const root = createShadowRootWithCoreStyles(
