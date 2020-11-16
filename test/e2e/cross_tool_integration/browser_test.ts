231374b0f1f9f9aadcbd46dd233f26fd79ca627e
Kim-Anh Tran <kimanh@chromium.org>
2020-11-16 12:59:58 +0100

    [e2e] Temporarily skip flaky browser tests
    TBR=joselea@microsoft.com
    
    Bug: chromium:1149334
    Change-Id: I23324e1ab09c2c3430bff1fd3d5ab413888e6bf9

--- test/e2e/cross_tool_integration/browser_test.ts
+++ test/e2e/cross_tool_integration/browser_test.ts
@@ -9,7 +9,7 @@ import {describe, it} from '../../shared/mocha-extensions.js';
 import {getCurrentConsoleMessages} from '../helpers/console-helpers.js';
 import {openPanelViaMoreTools} from '../helpers/settings-helpers.js';
 
-describe('Browser', async () => {
+describe.skip('[crbug.com/1149334]: Browser', async () => {
   it('can reload a website after all closeable tools are closed', async () => {
     // Navigate to website
     const {target} = getBrowserAndPages();
