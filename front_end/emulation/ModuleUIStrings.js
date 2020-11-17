83e1cdc4b080fd3ffb6d2e68de77127c7168df43
Kalon Hinds <kahinds@microsoft.com>
2020-11-17 01:17:34 -0800

    [Loc] Migrate emulation to Localization V2
    Bug: 1136655
    Change-Id: I935a118ad134ebce061395baf1e6721cef85d7c5

---
+++ front_end/emulation/ModuleUIStrings.js
@@ -0,0 +1,168 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as i18n from '../i18n/i18n.js';
+export const UIStrings = {
+  /**
+  *@description Text for the mobile platform, as opposed to desktop
+  */
+  mobile: 'Mobile',
+  /**
+  *@description Title of an action in the emulation tool to toggle device mode
+  */
+  toggleDeviceToolbar: 'Toggle device toolbar',
+  /**
+  *@description Text for one or a group of screenshots
+  */
+  screenshot: 'Screenshot',
+  /**
+  *@description Title of an action in the emulation tool to capture screenshot
+  */
+  captureScreenshot: 'Capture screenshot',
+  /**
+  *@description Title of an action in the emulation tool to capture full height screenshot
+  */
+  captureFullSizeScreenshot: 'Capture full size screenshot',
+  /**
+  *@description Title of an action in the emulation tool to capture node screenshot
+  */
+  captureNodeScreenshot: 'Capture node screenshot',
+  /**
+  *@description Text in Device Mode Toolbar of the Device Toolbar
+  */
+  showMediaQueries: 'Show media queries',
+  /**
+  *@description Text in Device Mode Toolbar of the Device Toolbar
+  */
+  hideMediaQueries: 'Hide media queries',
+  /**
+  *@description A tag of Mobile related settings that can be searched in the command menu
+  */
+  device: 'device',
+  /**
+  *@description Text to show the measuring rulers on the target
+  */
+  showRulers: 'Show rulers',
+  /**
+  *@description Text in Device Mode Toolbar of the Device Toolbar
+  */
+  hideRulers: 'Hide rulers',
+  /**
+  *@description Text in Device Mode Toolbar of the Device Toolbar
+  */
+  showDeviceFrame: 'Show device frame',
+  /**
+  *@description Text in Device Mode Toolbar of the Device Toolbar
+  */
+  hideDeviceFrame: 'Hide device frame',
+  /**
+  *@description Title of the Devices tab
+  */
+  devices: 'Devices',
+  /**
+  *@description Title of an action in the emulation tool to show sensors
+  */
+  sensors: 'Sensors',
+  /**
+  *@description A tag of Sensors tool that can be searched in the command menu
+  */
+  geolocation: 'geolocation',
+  /**
+  *@description A tag of Sensors tool that can be searched in the command menu
+  */
+  timezones: 'timezones',
+  /**
+  *@description Text in Sensors View of the Device Toolbar
+  */
+  locale: 'locale',
+  /**
+  *@description A tag of Sensors tool that can be searched in the command menu
+  */
+  locales: 'locales',
+  /**
+  *@description A tag of Sensors tool that can be searched in the command menu
+  */
+  accelerometer: 'accelerometer',
+  /**
+  *@description A tag of Sensors tool that can be searched in the command menu
+  */
+  deviceOrientation: 'device orientation',
+  /**
+  *@description City name in Locations Settings
+  */
+  berlin: 'Berlin',
+  /**
+  *@description City name in Locations Settings
+  */
+  london: 'London',
+  /**
+  *@description City name in Locations Settings
+  */
+  moscow: 'Moscow',
+  /**
+  *@description City name in Locations Settings
+  */
+  mountainView: 'Mountain View',
+  /**
+  *@description City name in Locations Settings
+  */
+  mumbai: 'Mumbai',
+  /**
+  *@description City name in Locations Settings
+  */
+  sanFrancisco: 'San Francisco',
+  /**
+  *@description City name in Locations Settings
+  */
+  shanghai: 'Shanghai',
+  /**
+  *@description City name in Locations Settings
+  */
+  soPaulo: 'São Paulo',
+  /**
+  *@description City name in Locations Settings
+  */
+  tokyo: 'Tokyo',
+  /**
+  *@description Title of Locations settings
+  */
+  locations: 'Locations',
+  /**
+  *@description Text for the touch type to simulate on a device
+  */
+  touch: 'Touch',
+  /**
+  *@description Text in Sensors View of the Device Toolbar
+  */
+  devicebased: 'Device-based',
+  /**
+  *@description Text in Sensors View of the Device Toolbar
+  */
+  forceEnabled: 'Force enabled',
+  /**
+  *@description Title of a section option in Sensors tab for idle emulation
+  */
+  emulateIdleDetectorState: 'Emulate Idle Detector state',
+  /**
+  *@description Title of an option in Sensors tab idle emulation select
+  */
+  noIdleEmulation: 'No idle emulation',
+  /**
+  *@description Title of an option in Sensors tab idle emulation select
+  */
+  userActiveScreenUnlocked: 'User active, screen unlocked',
+  /**
+  *@description Title of an option in Sensors tab idle emulation select
+  */
+  userActiveScreenLocked: 'User active, screen locked',
+  /**
+  *@description Title of an option in Sensors tab idle emulation select
+  */
+  userIdleScreenUnlocked: 'User idle, screen unlocked',
+  /**
+  *@description Title of an option in Sensors tab idle emulation select
+  */
+  userIdleScreenLocked: 'User idle, screen locked',
+};
+i18n.i18n.registerUIStrings('emulation/ModuleUIStrings.js', UIStrings);
