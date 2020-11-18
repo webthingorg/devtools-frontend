ace5b24ad2323624db0e7a375a93d76ec5934e46
Brandon Goddard <brgoddar@microsoft.com>
2020-11-18 09:44:06 -0800

    WIP migrating devtools tests to frontend
    Change-Id: Ib086dd203087a175bf3051ab18be5331b73dd0d0

--- test/webtests/http/tests/devtools/sources/debugger-breakpoints/resources/worker.js
+++ test/webtests/http/tests/devtools/sources/debugger-breakpoints/resources/worker.js
@@ -1,9 +1,5 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-onmessage = data => {
-  const a = 1;
-  const b = 2;
+onmessage = (data) => {
+  var a = 1;
+  var b = 2;
   return a + b;
 };
