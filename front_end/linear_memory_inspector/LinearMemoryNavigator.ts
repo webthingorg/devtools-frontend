25ae3da1c135cc2f339aa9f0c126a50c3818a368
Kim-Anh Tran <kimanh@chromium.org>
2020-11-24 13:05:19 +0100

    [LMI] Dark theme support, alignment and UX fixes.
    This adds support for the dark theme, and fixes a few issues in the
    UX of the LinearMemoryViewer and the LinearMemoryNavigator.
    
    Bug: chromium:1110202
    Change-Id: I43488447eec55c6c37762b5bd197196f79a6016c

--- front_end/linear_memory_inspector/LinearMemoryNavigator.ts
+++ front_end/linear_memory_inspector/LinearMemoryNavigator.ts
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as ComponentHelpers from '../component_helpers/component_helpers.js';
 import * as Elements from '../elements/elements.js';
 import * as LitHtml from '../third_party/lit-html/lit-html.js';
 
 import {toHexString} from './LinearMemoryInspectorUtils.js';
 
 const {render, html} = LitHtml;
+const getStyleSheets = ComponentHelpers.GetStylesheet.getStyleSheets;
 
 export const enum Navigation {
   Backward = 'Backward',
@@ -46,6 +48,14 @@ export class LinearMemoryNavigator extends HTMLElement {
   private readonly shadow = this.attachShadow({mode: 'open'});
   private address: number = 0;
 
+  constructor() {
+    super();
+    this.shadow.adoptedStyleSheets = [
+      ...getStyleSheets('ui/toolbar.css', {patchThemeSupport: true}),
+    ];
+  }
+
+
   set data(data: LinearMemoryNavigatorData) {
     if (data.address < 0) {
       throw new Error('Address should be greater or equal to zero.');
@@ -60,30 +70,57 @@ export class LinearMemoryNavigator extends HTMLElement {
     render(html`
       <style>
         .navigator {
-          min-height: 24px;
+          height: 24px;
           display: flex;
           flex-wrap: nowrap;
           justify-content: space-between;
           overflow: hidden;
           align-items: center;
+          --navigator-border-color: #9aa0a6
         }
 
         .navigator-item {
           white-space: nowrap;
           overflow: hidden;
+          display: flex;
+          align-items: center;
         }
 
-        input {
+        .navigator-input {
           text-align: center;
+          height: 22px;
+          padding: 0px;
+          margin: 0px 5px;
+          border: solid 1px;
+          border-color: var(--navigator-border-color);
         }
 
         .navigator-button {
-          background: transparent;
-          overflow: hidden;
-          vertical-align: middle;
+          padding: 0;
+          background: none;
+          outline: none;
           border: none;
-          padding: 0px;
+          width: 16px;
+          height: 16px;
         }
+
+        .navigator-button:focus,
+        .navigator-button:hover {
+          background-color: white;
+          border-color:white;
+        }
+
+        .navigator-button-page {
+          border: solid 1px;
+          border-color: var(--navigator-border-color);
+          width: 24px;
+          height: 24px;
+        }
+
+        .navigator-button + .navigator-button {
+          margin-left: 5px;
+        }
+
       </style>
       <div class="navigator">
         <div class="navigator-item">
@@ -91,9 +128,9 @@ export class LinearMemoryNavigator extends HTMLElement {
           ${this.createButton('ic_redo_16x16_icon', new HistoryNavigationEvent(Navigation.Forward))}
         </div>
         <div class="navigator-item">
-          ${this.createButton('ic_page_prev_16x16_icon', new PageNavigationEvent(Navigation.Backward))}
-          <input data-input="true" contenteditable="true" value="0x${toHexString(this.address, 8)}"/>
-          ${this.createButton('ic_page_next_16x16_icon', new PageNavigationEvent(Navigation.Forward))}
+          ${this.createButton('ic_page_prev_16x16_icon', new PageNavigationEvent(Navigation.Backward), 'navigator-button-page')}
+          <input class="navigator-input" data-input="true" contenteditable="true" value="0x${toHexString(this.address, 8)}"/>
+          ${this.createButton('ic_page_next_16x16_icon', new PageNavigationEvent(Navigation.Forward), 'navigator-button-page')}
         </div>
         ${this.createButton('refresh_12x12_icon', new RefreshEvent())}
       </div>
@@ -101,12 +138,13 @@ export class LinearMemoryNavigator extends HTMLElement {
     // clang-format on
   }
 
-  private createButton(name: string, event: Event) {
+  private createButton(name: string, event: Event, buttonClass?: string) {
     return html`
-      <button class="navigator-button" data-button=${event.type} @click=${this.dispatchEvent.bind(this, event)}>
-        <devtools-icon .data=${
-        {iconName: name, color: 'rgb(110 110 110)', width: '16px'} as Elements.Icon.IconWithName}>
-        </devtools-icon>
+      <button class="${buttonClass} navigator-button" data-button=${event.type} @click=${
+        this.dispatchEvent.bind(this, event)}>
+      <devtools-icon .data=${
+        {iconName: name, color: 'rgb(110 110 110)', width: '16px', height: '16px'} as Elements.Icon.IconWithName}>
+      </devtools-icon>
       </button>`;
   }
 }
