545e26cc5d04abf860edfdb7aa300d0fff0f576f
Jack Franklin <jacktfranklin@chromium.org>
2020-11-26 16:05:24 +0000

    [ESLint] rule for .data=${{} as X}
    This rule enforces that within a LitHtml template when we bind to the
    `.data=` property we provide an explicit type cast that must be a type
    reference.
    
    This is one of the checks that the component bridges used to make, but
    now we need to port them to ESLint rules.
    
    Bug: 1130536
    Change-Id: I401661289a829fd181bf91943fffe162b87de8b8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 const path = require('path');
-const rulesDirPlugin = require('eslint-plugin-rulesdir')
+const rulesDirPlugin = require('eslint-plugin-rulesdir');
 rulesDirPlugin.RULES_DIR = path.join(__dirname, 'scripts', 'eslint_rules', 'lib');
 
 module.exports = {
@@ -153,6 +157,7 @@ module.exports = {
       'no-unused-vars': 0,
       '@typescript-eslint/no-unused-vars': [2, {'argsIgnorePattern': '^_'}],
       'rulesdir/kebab_case_events': 2,
+      'rulesdir/lit_html_data_as_type': 2,
     }
   }]
 };
