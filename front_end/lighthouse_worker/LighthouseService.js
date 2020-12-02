b35ac64abcde4ed3fbb61c2234a8138c03e7e46d
Connor Clark <cjamcl@chromium.org>
2020-12-01 18:02:04 -0600

    Fix Lighthouse locale fetching.
    Bug: 1131516
    
    Change-Id: I0e4ec95618ba33fff59f03970ff1df297f98d2ad

--- front_end/lighthouse_worker/LighthouseService.js
+++ front_end/lighthouse_worker/LighthouseService.js
@@ -81,6 +81,9 @@ class LighthouseService {  // eslint-disable-line
   }
 
   /**
+   * Finds a locale supported by Lighthouse from the user's system locales.
+   * If no matching locale is found, or if fetching locale data fails, this function returns nothing
+   * and Lighthouse will use `en-US` by default.
    * @param {string[]} locales
    * @return {!Promise<(string|undefined)>}
    */
@@ -90,23 +93,34 @@ class LighthouseService {  // eslint-disable-line
 
     // If the locale is en-US, no need to fetch locale data.
     if (locale === 'en-US' || locale === 'en') {
-      return undefined;
+      return;
     }
 
     // Try to load the locale data.
-    const localeResource = `../third_party/lighthouse/locales/${locale}.json`;
+    const remoteBase = Root.Runtime.getRemoteBase();
+    if (!remoteBase || !remoteBase.base) {
+      return;
+    }
+    const localeUrl = `${remoteBase.base}third_party/lighthouse/locales/${locale}.json`;
+
     try {
-      const module = Root.Runtime.Runtime.instance().module('lighthouse_worker');
-      const localeDataText = await module.fetchResource(localeResource);
+      const timeoutPromise =
+          new Promise((resolve, reject) => setTimeout(() => reject(new Error('timed out fetching locale')), 5000));
+      const localeDataTextPromise = Root.Runtime.loadResourcePromise(localeUrl).catch(() => {
+        // Attempt to load a locale file from the local file system. CQ will always hit this code path.
+        console.error(`Could not fetch locale at ${localeUrl}. Trying relative path.`);
+        return Root.Runtime.loadResourcePromise(`../third_party/lighthouse/locales/${locale}.json`);
+      });
+      const localeDataText = await Promise.race([timeoutPromise, localeDataTextPromise]);
       const localeData = JSON.parse(localeDataText);
       // @ts-ignore https://github.com/GoogleChrome/lighthouse/issues/11628
       self.registerLocaleData(locale, localeData);
       return locale;
-    } catch (_) {
+    } catch (err) {
+      console.error(err);
     }
 
-    // If no locale was found, or fetching locale data fails, Lighthouse will use `en-US` by default.
-    return undefined;
+    return;
   }
 
   async stop() {
