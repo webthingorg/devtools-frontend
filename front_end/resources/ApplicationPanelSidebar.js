4fd534c19e93f8f272071e8e53f3e09dc78f78bd
Wolfgang Beyer <wolfi@chromium.org>
2020-12-04 11:57:47 +0100

    Show service workers in the Application Panel's frame tree
    This CL adds service workers to the frame tree by listening to new
    targets being added.
    
    Screenshot: https://i.imgur.com/EAbI3d8.png
    Bug: 1122507
    
    Change-Id: I138430ea4db741ace4ff2e74f80e67ccb89640a6

--- front_end/resources/ApplicationPanelSidebar.js
+++ front_end/resources/ApplicationPanelSidebar.js
@@ -2185,7 +2185,7 @@ export class ResourcesSection {
    * @param {!SDK.SDKModel.Target} target
    */
   targetAdded(target) {
-    if (target.type() === 'worker') {
+    if ((target.type() === SDK.SDKModel.Type.Worker) || (target.type() === SDK.SDKModel.Type.ServiceWorker)) {
       this._workerAdded(target);
     }
   }
@@ -2415,7 +2415,7 @@ export class FrameTreeElement extends BaseStorageTreeElement {
   /**
    * @param {!SDK.ResourceTreeModel.ResourceTreeFrame} frame
    */
-  frameNavigated(frame) {
+  async frameNavigated(frame) {
     const icon = UI.Icon.Icon.create(this.getIconTypeForFrame(frame));
     if (frame.unreachableUrl()) {
       icon.classList.add('red-icon');
@@ -2436,12 +2436,27 @@ export class FrameTreeElement extends BaseStorageTreeElement {
     }
     this._categoryElements.clear();
     this._treeElementForResource.clear();
+    this._treeElementForWorker.clear();
 
     if (this.selected) {
       this._view = new FrameDetailsView(this._frame);
       this.showView(this._view);
     } else {
       this._view = null;
+    }
+
+    // Service Workers' parent is always the top frame. We need to reconstruct
+    // the service worker tree elements after those navigations which allow
+    // the service workers to stay alive.
+    if (frame.isTopFrame()) {
+      const targets = SDK.SDKModel.TargetManager.instance().targets();
+      for (const target of targets) {
+        if (target.type() === SDK.SDKModel.Type.ServiceWorker) {
+          const agent = frame.resourceTreeModel().target().targetAgent();
+          const targetInfo = (await agent.invoke_getTargetInfo({targetId: target.id()})).targetInfo;
+          this.workerCreated(targetInfo);
+        }
+      }
     }
   }
 
@@ -2538,13 +2553,14 @@ export class FrameTreeElement extends BaseStorageTreeElement {
    * @param {!Protocol.Target.TargetInfo} targetInfo
    */
   workerCreated(targetInfo) {
-    const categoryKey = 'Workers';
+    const categoryKey = targetInfo.type === 'service_worker' ? 'Service Workers' : 'Web Workers';
     let categoryElement = this._categoryElements.get(categoryKey);
     if (!categoryElement) {
-      categoryElement = new StorageCategoryTreeElement(this._section._panel, ls`Workers`, categoryKey);
+      categoryElement = new StorageCategoryTreeElement(this._section._panel, ls`${categoryKey}`, categoryKey);
       this._categoryElements.set(categoryKey, categoryElement);
       this.appendChild(categoryElement, FrameTreeElement._presentationOrderCompare);
     }
+
     if (!this._treeElementForWorker.get(targetInfo.targetId)) {
       const workerTreeElement = new WorkerTreeElement(this._section._panel, targetInfo);
       categoryElement.appendChild(workerTreeElement);
