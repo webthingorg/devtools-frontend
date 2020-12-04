007be8b03715ef861db853a0b1e3a629e5755256
Kim-Anh Tran <kimanh@chromium.org>
2020-12-04 01:27:16 +0100

    [LMI] Implement refresh functionality
    On clicking the refresh button, the linear memory inspector requests
    new data to refresh the view. This also contains some clean up
    related to the logic of requesting memory, which is now moved to
    the LinearMemoryInspectorController.
    
    Bug: chromium:1110202
    Change-Id: I0cbd53d95cf5cbf51cfb55566a345bccee341943

--- front_end/linear_memory_inspector/LinearMemoryInspectorPane.js
+++ front_end/linear_memory_inspector/LinearMemoryInspectorPane.js
@@ -8,19 +8,10 @@ import * as UI from '../ui/ui.js';
 const {ls} = Common;
 
 import {LinearMemoryInspector} from './LinearMemoryInspector.js';
+import {LazyUint8Array, LinearMemoryInspectorController} from './LinearMemoryInspectorController.js';  // eslint-disable-line no-unused-vars
 
 /** @type {!LinearMemoryInspectorPaneImpl} */
 let inspectorInstance;
-
-export class ViewClosedEvent extends Event {
-  /**
-   * @param {string} tabId
-   */
-  constructor(tabId) {
-    super('view-closed');
-    this.data = tabId;
-  }
-}
 
 export class Wrapper extends UI.Widget.VBox {
   constructor() {
@@ -100,29 +91,6 @@ export class LinearMemoryInspectorPaneImpl extends UI.Widget.VBox {
   }
 }
 
-/**
- * @interface
- */
-export class LazyUint8Array {
-  /**
-   * @param {number} start
-   * @param {number} end
-   * @return {!Promise<!Uint8Array>}
-   */
-  getRange(start, end) {
-    return Promise.resolve(new Uint8Array([]));
-  }
-
-  /**
-   * @return {number}
-   */
-  length() {
-    return 0;
-  }
-}
-
-const MEMORY_TRANSFER_MIN_CHUNK_SIZE = 1000;
-
 class LinearMemoryInspectorView extends UI.Widget.VBox {
   /**
    *
@@ -143,16 +111,11 @@ class LinearMemoryInspectorView extends UI.Widget.VBox {
     });
     this.contentElement.appendChild(this._inspector);
 
-    // Provide a chunk of memory that covers the address to show and some before and after
-    // as 1. the address shown is not necessarily at the beginning of a page and
-    // 2. to allow for fewer memory requests.
-    const memoryChunkStart = Math.max(0, address - MEMORY_TRANSFER_MIN_CHUNK_SIZE / 2);
-    const memoryChunkEnd = memoryChunkStart + MEMORY_TRANSFER_MIN_CHUNK_SIZE;
-    this.memoryWrapper.getRange(memoryChunkStart, memoryChunkEnd).then(memory => {
+    LinearMemoryInspectorController.getMemoryForAddress(this.memoryWrapper, address).then(({memory, offset}) => {
       this._inspector.data = {
         memory: memory,
         address: address,
-        memoryOffset: memoryChunkStart,
+        memoryOffset: offset,
         outerMemoryLength: this.memoryWrapper.length(),
       };
     });
@@ -167,15 +130,7 @@ class LinearMemoryInspectorView extends UI.Widget.VBox {
       throw new Error('Requested address is out of bounds.');
     }
 
-    // Check that the requested start is within bounds.
-    // If the requested end is larger than the actual
-    // memory, it will be automatically capped when
-    // requesting the range.
-    if (start < 0 || start > end || start >= this.memoryWrapper.length()) {
-      throw new Error('Requested range is out of bounds.');
-    }
-    const chunkEnd = Math.max(end, start + MEMORY_TRANSFER_MIN_CHUNK_SIZE);
-    this.memoryWrapper.getRange(start, chunkEnd).then(memory => {
+    LinearMemoryInspectorController.getMemoryRange(this.memoryWrapper, start, end).then(memory => {
       this._inspector.data = {
         memory: memory,
         address: address,
