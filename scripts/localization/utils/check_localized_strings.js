d4c2e1fd709591eeef9a368a66169bffef896912
Christy Chen <chrche@microsoft.com>
2020-12-07 14:21:08 -0800

    [Loc] Migrate help to localization V2
    migrate help directory
    
    Bug: 1136655
    Change-Id: Ib361747d53faa0a5cc860d331238e08d327031cc

--- scripts/localization/utils/check_localized_strings.js
+++ scripts/localization/utils/check_localized_strings.js
@@ -105,24 +105,9 @@ let devtoolsFrontendDirs;
 // e.g. const migratedDirsSet = new Set(['settings', 'console']);
 // TODO(crbug.com/941561): Remove once localization V1 is no longer used.
 const migratedDirsSet = new Set([
-  'animation',
-  'changes',
-  'coverage',
-  'developer_resources',
-  'devices',
-  'layers',
-  'layer_viewer',
-  'lighthouse',
-  'media',
-  'performance_monitor',
-  'protocol_monitor',
-  'search',
-  'security',
-  'settings',
-  'snippets',
-  'text_editor',
-  'web_audio',
-  'webauthn',
+  'animation',    'changes',    'coverage',    'developer_resources', 'devices',          'help',   'layers',
+  'layer_viewer', 'lighthouse', 'media',       'performance_monitor', 'protocol_monitor', 'search', 'security',
+  'settings',     'snippets',   'text_editor', 'web_audio',           'webauthn',
 ]);
 const locV1CallsInMigratedFiles = new Set();
 
