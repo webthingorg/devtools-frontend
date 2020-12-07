352d26d3a764f0de7a2096ae929c7920813acac8
Andres Olivares <andoli@chromium.org>
2020-12-07 17:31:20 +0000

    [module.json extensions] Migrate network views
    Bug: 1134103
    Change-Id: I3bcc8c238bf0ec2734236777ed696067846aa7a3

--- front_end/ui/ViewRegistration.ts
+++ front_end/ui/ViewRegistration.ts
@@ -19,6 +19,9 @@ export const enum ViewLocationValues {
   SETTINGS_VIEW = 'settings-view',
   ELEMENTS_SIDEBAR = 'elements-sidebar',
   SOURCES_SIDEBAR_BOTTOM = 'sources.sidebar-bottom',
+  NAVIGATOR_VIEW = 'navigator-view',
+  DRAWER_VIEW = 'drawer-view',
+  NETWORK_SIDEBAR = 'network-sidebar',
 }
 
 export interface ViewRegistration {
