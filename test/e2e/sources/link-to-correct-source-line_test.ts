66e3d586d2ca9a5dea496a7c82582b90c8124174
Eric Leese <leese@chromium.org>
2020-12-09 14:34:37 +0100

    Re-enable tests and fix flaky Issues tab test
    Fixes: 1073406
    Change-Id: I3182bd5ac2f3bcdb903632ee68db7cc462622c3c

--- test/e2e/sources/link-to-correct-source-line_test.ts
+++ test/e2e/sources/link-to-correct-source-line_test.ts
@@ -5,17 +5,18 @@
 import {goToResource} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {expandIssue, navigateToIssuesTab, revealViolatingSourcesLines} from '../helpers/issues-helpers.js';
-import {waitForhighlightedLineWhichIncludesText} from '../helpers/sources-helpers.js';
+import {waitForHighlightedLineWhichIncludesText} from '../helpers/sources-helpers.js';
 
 describe('The Issues tab', async () => {
   // Flaky test
-  it.skip('[crbug.com/1073406]: should highlight a line in the Sources panel when the link is clicked', async () => {
+  it('should highlight a line in the Sources panel when the link is clicked', async () => {
+    ((this as unknown) as {timeout: (arg0: number) => void})?.timeout(30000);
     await goToResource('elements/element-reveal-inline-issue.html');
 
     await navigateToIssuesTab();
     await expandIssue();
     await revealViolatingSourcesLines();
 
-    await waitForhighlightedLineWhichIncludesText('<script class="violating-script">');
+    await waitForHighlightedLineWhichIncludesText('<script class="violating-script">');
   });
-});
+}).slow();
