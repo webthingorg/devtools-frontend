e1a27964b0456d8fd3918a6485e7b913f08e7071
chait pinnamaneni <cpinnamaneni@microsoft.com>
2020-12-09 16:54:29 -0800

    adding link support for markdownview
    Bug:1154783
    Change-Id: Id87ae9da3ce1a705397af00ffd52a21175795286

--- test/unittests/front_end/issues/MarkdownView_test.ts
+++ test/unittests/front_end/issues/MarkdownView_test.ts
@@ -62,6 +62,16 @@ describeWithEnvironment('MarkdownView', async () => {
     it('throws an error for invalid or unsupported token types', () => {
       assert.throws(() => Issues.MarkdownView.renderToken({type: 'no_way_this_is_a_valid_markdown_token'}));
     });
+
+    it('renders link with valid key', () => {
+      const renderResult = Issues.MarkdownView.renderToken({type: 'link', text: 'learn more', href: 'exampleLink'});
+      assert.deepStrictEqual(renderResult.strings.raw, ['<a>', '</a>']);
+      assert.deepStrictEqual(renderResult.values, ['https://web.dev/']);
+    });
+
+    it('throws an error if invalid link key is provided', () => {
+      assert.throws(() => Issues.MarkdownView.renderToken({type: 'link', text: 'learn more', href: 'testErrorLink'}));
+    });
   });
 
   const paragraphText =
