7712b7f848ed4a6b52dd2f0cd16883d9fff69bc9
Alfonso Castaño <alcastano@google.com>
2020-12-11 11:06:18 +0000

    Make Issue Message in Source Tab link to Issues tab
    Design-doc: https://docs.google.com/document/d/1TgMAUsruc_xAPAseG96H0-NHRT_PiXciedD58hgq1e4/
    Bug: chromium:1150883
    Change-Id: Ibe320de74ff726f5d649007e7c7b298a50892301

--- front_end/sources/UISourceCodeFrame.js
+++ front_end/sources/UISourceCodeFrame.js
@@ -690,6 +690,11 @@ export class RowMessage {
     this._icon.data = getIconClassPerLevel(message.level());
     this._icon.classList.add('text-editor-row-message-icon');
 
+    const clickHandler = message.clickHandler();
+    if (clickHandler) {
+      this._icon.addEventListener('click', clickHandler);
+    }
+
     this.element.append(this._icon);
     /** @type {!UI.UIUtils.DevToolsSmallBubble} */
     this._repeatCountElement =
@@ -709,6 +714,13 @@ export class RowMessage {
    */
   message() {
     return this._message;
+  }
+
+  callClickHandler() {
+    const handler = this._message.clickHandler();
+    if (handler) {
+      handler();
+    }
   }
 
   /**
@@ -761,6 +773,7 @@ export class RowMessageBucket {
     this._issueIcon = new WebComponents.Icon.Icon();
     this._issueIcon.data = getIconClassPerLevel(Workspace.UISourceCode.Message.Level.Issue);
     this._issueIcon.classList.add('text-editor-line-decoration-icon-issue', 'hidden');
+    this._issueIcon.addEventListener('click', () => this._issueClickHandler());
 
     const iconsElement = this._wave.createChild('span');
     iconsElement.append(this._errorIcon);
@@ -835,6 +848,13 @@ export class RowMessageBucket {
    */
   uniqueMessagesCount() {
     return this._messages.length;
+  }
+
+  _issueClickHandler() {
+    const firstIssue = this._messages.find(m => m.message().level() === Workspace.UISourceCode.Message.Level.Issue);
+    if (firstIssue) {
+      firstIssue.callClickHandler();
+    }
   }
 
   /**
