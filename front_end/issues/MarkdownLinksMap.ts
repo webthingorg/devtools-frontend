b4f5f8483b7d566535e7b3cab891852427e65dc2
chait pinnamaneni <cpinnamaneni@microsoft.com>
2020-12-11 11:19:37 -0800

    adding link support for markdownview
    Bug:1154783
    Change-Id: Id87ae9da3ce1a705397af00ffd52a21175795286

---
+++ front_end/issues/MarkdownLinksMap.ts
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+  To use links in markdown, add key here with the link and
+  use the added key in markdown.
+  @example markdown
+  Find more information about web development at [Learn more](exampleLink)
+*/
+export const markdownLinks = new Map<string, string>([]);
+
+export const getMarkdownLink = (key: string) => {
+  const link = markdownLinks.get(key);
+  if (!link) {
+    throw new Error(`Markdown link with key '${key}' is not available, please check MarkdownLinksMap.ts`);
+  }
+  return link;
+};
