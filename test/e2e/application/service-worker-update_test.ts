530603af5929bac319725f81f1eaaf7c9d66c526
Songtao Xia <soxia@microsoft.com>
2021-01-05 15:54:55 -0800

    Service Worker Update Cycle Timeline
    This CL adds SW update cycle timeline to the Service Workers
    view, Application pane. See the explainers below for
    requirements.
    
    A summary of features implemented:
    - For the service worker associated with the current target, for
    every version still alive (including redundant ones), show a
    timeline of the install, wait, or activate events.
    - The timeline is a waterfall style, different than in the explainer
    but similar to that in network timing.
    - Such a timeline is based on the observation in the frontend and
    is intended to show for example, if an event takes extra long
    time in waiting. It is not meant to be a measurement of execution
    time of the event handler at the backend.
    - Each event's timeline may be expanded to reveal details of
    the event handler, such as the starting and ending time of
    the event handling.
    - Other details in the explainer are not included.
    
    
    crbug.com/1066604
    
    Explainer:
    https://docs.google.com/document/d/1M2xKrlFHrojGHIJ9AVpQdGrmUUU0G8YVToa3kZz3RZA/edit?usp=sharing
    
    Change-Id: I0b3725b342eeb32470ef759bab8a7da5a350dba9

---
+++ test/e2e/application/service-worker-update_test.ts
@@ -0,0 +1,23 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {getBrowserAndPages, step, waitFor} from '../../shared/helper.js';
+import {beforeEach, describe, it} from '../../shared/mocha-extensions.js';
+import {navigateToApplicationTab} from '../helpers/application-helpers.js';
+
+const TEST_HTML_FILE = 'service-worker-network';
+const SERVICE_WORKER_UPDATE_TIMELINE_SELECTOR = 'resource-service-worker-update-view';
+
+describe('The Application Tab', async () => {
+  beforeEach(async () => {
+    const {target} = getBrowserAndPages();
+    await navigateToApplicationTab(target, TEST_HTML_FILE);
+  });
+
+  it('Navigate to a page with service worker we should find service worker update timeline info', async () => {
+    await step('wait and locate service worker update time line', async () => {
+      waitFor(SERVICE_WORKER_UPDATE_TIMELINE_SELECTOR);
+    });
+  });
+});
