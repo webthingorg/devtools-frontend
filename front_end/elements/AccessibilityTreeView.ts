e1af4baf533db3c43569fdf20d281fc362313de6
Annabel Zhou <annabelzhou@google.com>
2021-01-07 18:09:45 +1100

    Added AccessibilityTree web component and toggle switch between accessibility and DOM trees.
    This change is hidden behind experiment flag "fullAccessibilityTree".
    Design doc: https://goo.gle/devtools-a11y-tree
    
    Bug: 887173
    Change-Id: Ib3eb0cd22a45198c5718f2f00bbdad419d3de06f

---
+++ front_end/elements/AccessibilityTreeView.ts
@@ -0,0 +1,51 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as SDK from '../sdk/sdk.js';
+import * as UI from '../ui/ui.js';
+
+import {AccessibilityTree} from './AccessibilityTree.js';
+
+export class AccessibilityTreeView extends UI.Widget.VBox {
+  private readonly accessibilityTreeComponent = new AccessibilityTree();
+  private readonly toggleButton: HTMLButtonElement;
+  private node: SDK.DOMModel.DOMNode|null = null;
+  private rootNode: SDK.AccessibilityModel.AccessibilityNode|null;
+
+  constructor(toggleButton: HTMLButtonElement) {
+    super();
+    // toggleButton is bound to a click handler on ElementsPanel to switch between the DOM tree
+    // and accessibility tree views.
+    this.toggleButton = toggleButton;
+    this.contentElement.appendChild(this.toggleButton);
+    this.contentElement.appendChild(this.accessibilityTreeComponent);
+    this.rootNode = null;
+  }
+
+  setNode(node: SDK.DOMModel.DOMNode): void {
+    this.node = node;
+    this.refreshAccessibilityTree();
+  }
+
+  private async refreshAccessibilityTree(): Promise<void> {
+    if (!this.node) {
+      return;
+    }
+
+    const accessibilityModel = this.node.domModel().target().model(SDK.AccessibilityModel.AccessibilityModel);
+    if (!accessibilityModel) {
+      return;
+    }
+
+    const result = await accessibilityModel.requestFullAXTree();
+    if (!result) {
+      return;
+    }
+    this.rootNode = result;
+
+    this.accessibilityTreeComponent.data = {
+      rootNode: this.rootNode,
+    };
+  }
+}
