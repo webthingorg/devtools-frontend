75a0ebe9839949033a693d2199b2b9e3b1bdae44
Annabel Zhou <annabelzhou@google.com>
2021-01-08 15:06:11 +1100

    Added AccessibilityTree web component and toggle switch between accessibility and DOM trees.
    This change is hidden behind experiment flag "fullAccessibilityTree".
    Design doc: https://goo.gle/devtools-a11y-tree
    
    Bug: 887173
    Change-Id: Ib3eb0cd22a45198c5718f2f00bbdad419d3de06f

---
+++ front_end/elements/AccessibilityTree.ts
@@ -0,0 +1,61 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as SDK from '../sdk/sdk.js';
+import * as LitHtml from '../third_party/lit-html/lit-html.js';
+
+export interface AccessibilityTreeData {
+  node: SDK.DOMModel.DOMNode|null;
+}
+
+export class AccessibilityTree extends HTMLElement {
+  private readonly shadow = this.attachShadow({mode: 'open'});
+  private node: SDK.DOMModel.DOMNode|null = null;
+  private rootNode: SDK.AccessibilityModel.AccessibilityNode|null = null;
+
+  set data(data: AccessibilityTreeData) {
+    this.node = data.node;
+  }
+
+  async refreshAccessibilityTree(): Promise<void> {
+    if (!this.node) {
+      return;
+    }
+
+    const accessibilityModel = this.node.domModel().target().model(SDK.AccessibilityModel.AccessibilityModel);
+    if (!accessibilityModel) {
+      return;
+    }
+
+    const result = await accessibilityModel.requestFullAXTree();
+    if (!result) {
+      return;
+    }
+    this.rootNode = result;
+    this.render();
+  }
+
+  private render(): void {
+    if (!this.rootNode) {
+      return;
+    }
+
+    // clang-format off
+    const output = LitHtml.html`
+    <div>TODO(annabelzhou): Accessibility Tree goes here</div>`;
+    // clang-format on
+    LitHtml.render(output, this.shadow);
+  }
+}
+
+if (!customElements.get('devtools-accessibility-tree')) {
+  customElements.define('devtools-accessibility-tree', AccessibilityTree);
+}
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'devtools-accessibility-tree': AccessibilityTree;
+  }
+}
