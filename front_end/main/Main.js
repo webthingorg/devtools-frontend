60231611d3784835b4b0b3e7052e9deb24048f6b
Alexander Timin <altimin@chromium.org>
2021-01-08 10:20:11 +0000

    [devtools] Remove timeline flow event experiment.
    This experiment is relying on the deprecated format of flow events and
    there are no plans to ship in the immediate future.
    
    Remove it to unblock removing the old flow event macroses. This
    experiment should be reimplemented on top of the new trace event flow
    format instead.
    
    R=yangguao@chromium.org,caseq@chromium.org
    BUG=1164298
    
    Change-Id: Ibf29a2842750c0fab8b6099e992aed27f4886b12

--- front_end/main/Main.js
+++ front_end/main/Main.js
@@ -140,7 +140,6 @@ Main.Main = class {
 
     // Timeline
     Root.Runtime.experiments.register('timelineEventInitiators', 'Timeline: event initiators');
-    Root.Runtime.experiments.register('timelineFlowEvents', 'Timeline: flow events', true);
     Root.Runtime.experiments.register('timelineInvalidationTracking', 'Timeline: invalidation tracking', true);
     Root.Runtime.experiments.register('timelineShowAllEvents', 'Timeline: show all events', true);
     Root.Runtime.experiments.register(
