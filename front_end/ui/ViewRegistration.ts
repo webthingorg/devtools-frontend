30f3f9dd459d5ba813a5504662f575bd55dd67f8
Andres Olivares <andoli@chromium.org>
2021-01-12 16:11:36 +0000

    [module.json extensions] Add support for ViewLocationResolver
    Modules can now register extensions of type 'ViewLocationResolver' in
    their meta files.
    
    Bug: 1134103
    Change-Id: I07eb5034634128c941d15507f72dcc53016ead5e

--- front_end/ui/ViewRegistration.ts
+++ front_end/ui/ViewRegistration.ts
@@ -4,6 +4,7 @@
 import * as Platform from '../platform/platform.js';
 import * as Root from '../root/root.js';
 
+import {ViewLocationResolver} from './View.js';  // eslint-disable-line no-unused-vars
 import {PreRegisteredView} from './ViewManager.js';
 
 import type {Widget} from './Widget.js';
@@ -48,4 +49,37 @@ export function registerViewExtension(registration: ViewRegistration): void {
 export function getRegisteredViewExtensions(): Array<PreRegisteredView> {
   return registeredViewExtensions.filter(
       view => Root.Runtime.Runtime.isDescriptorEnabled({experiment: view.experiment(), condition: view.condition()}));
+}
+
+const registeredLocationResolvers: Array<LocationResolverRegistration> = [];
+
+const viewLocationNameSet = new Set<ViewLocationValues>();
+
+export function registerLocationResolver(registration: LocationResolverRegistration): void {
+  const locationName = registration.name;
+  if (viewLocationNameSet.has(locationName)) {
+    throw new Error(`Duplicate view location name registration '${locationName}'`);
+  }
+  viewLocationNameSet.add(locationName);
+  registeredLocationResolvers.push(registration);
+}
+
+export function getRegisteredLocationResolvers(): Array<LocationResolverRegistration> {
+  return registeredLocationResolvers;
+}
+
+export const ViewLocationCategoryValues = {
+  ELEMENTS: ls`Elements`,
+  DRAWER: ls`Drawer`,
+  DRAWER_SIDEBAR: ls`Drawer sidebar`,
+  PANEL: ls`Panel`,
+  NETWORK: ls`Network`,
+  SETTINGS: ls`Settings`,
+  SOURCES: ls`Sources`,
+};
+
+type ViewLocationCategory = typeof ViewLocationCategoryValues[keyof typeof ViewLocationCategoryValues];
+
+export interface LocationResolverRegistration {
+  name: ViewLocationValues, category: ViewLocationCategory, loadResolver: () => Promise<ViewLocationResolver>
 }
