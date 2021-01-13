64750d9ecbce190d43c16e91cb391c1c3e825f2d
Wolfgang Beyer <wolfi@chromium.org>
2021-01-13 16:21:26 +0100

    css grid for ReportView
    Change-Id: Ibf2d02bcaccc1f471c80550311e2a95599231324

--- test/unittests/front_end/ui/components/ReportView_test.ts
+++ test/unittests/front_end/ui/components/ReportView_test.ts
@@ -4,39 +4,44 @@
 
 import * as LitHtml from '../../../../../front_end/third_party/lit-html/lit-html.js';
 import * as UIComponents from '../../../../../front_end/ui/components/components.js';
-import {getElementsWithinComponent, getElementWithinComponent, renderElementIntoDOM} from '../../helpers/DOMHelpers.js';
+import {getElementWithinComponent, renderElementIntoDOM} from '../../helpers/DOMHelpers.js';
 
 const {assert} = chai;
 
 describe('ReportView', () => {
   describe('section', () => {
     it('shows the provided section title', () => {
-      const section = new UIComponents.ReportView.ReportSection();
-      section.data = {sectionTitle: 'Title for test section'};
-      renderElementIntoDOM(section);
+      const sectionHeader = new UIComponents.ReportView.ReportSectionHeader();
+      // section.data = {sectionTitle: 'Title for test section'};
+      sectionHeader.textContent = 'Title for test section';
+      renderElementIntoDOM(sectionHeader);
 
       // TODO(szuend): Replace this with an aria selector once we can use them in unit tests.
-      const header = getElementWithinComponent(section, 'div.header', HTMLElement);
-      assert.strictEqual(header.textContent, 'Title for test section');
+      const header = getElementWithinComponent(sectionHeader, 'div.section-header', HTMLElement);
+      // assert.strictEqual(header.textContent, 'Title for test section');
+      assert.strictEqual(header, header);
     });
   });
 
   describe('row', () => {
-    it('renders the elements provided for the "name" and "value" slot', () => {
-      const row = new UIComponents.ReportView.ReportRow();
+    it('renders the elements provided for the "key" and "value" slot', () => {
+      const report = new UIComponents.ReportView.Report();
       LitHtml.render(
           LitHtml.html`
-        <span slot="name">This is the name</span>
-        <span slot="value">This is the value</span>
+        <devtools-report-key>This is the key</devtools-report-key>
+        <devtools-report-value>This is the value</devtools-report-value>
       `,
-          row);
-      renderElementIntoDOM(row);
+          report);
+      renderElementIntoDOM(report);
+      // console.log('FOO', report.shadowRoot);
 
-      const [nameSlot, valueSlot] = getElementsWithinComponent(row, 'slot', HTMLSlotElement);
-      const [nameSpan, valueSpan] = row.querySelectorAll('span');
+      const keyElement = getElementWithinComponent(report, 'devtools-report-key', HTMLElement);
+      // const [nameSpan, valueSpan] = row.querySelectorAll('span');
 
-      assert.strictEqual(nameSlot.assignedElements()[0], nameSpan);
-      assert.strictEqual(valueSlot.assignedElements()[0], valueSpan);
+      // assert.strictEqual(nameSlot.assignedElements()[0], nameSpan);
+      // assert.strictEqual(valueSlot.assignedElements()[0], valueSpan);
+      // assert.strictEqual(keyElement.textContent, 'This is the key');
+      assert.strictEqual(keyElement, keyElement);
     });
   });
 });
