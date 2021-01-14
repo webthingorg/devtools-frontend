08a6d204b6315f9014860edbe9a7085e5a9bf07f
Jan Scheffler <janscheffler@chromium.org>
2021-01-14 19:40:28 +0100

    [JSDOC2TS]: Migrate devices
    Bug: chromium:1158760
    Change-Id: Iac3f59bf9f17a372579f58a1426195a1f809749f

--- front_end/devices/DevicesView.ts
+++ front_end/devices/DevicesView.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* eslint-disable rulesdir/no_underscored_properties */
+
 import * as i18n from '../i18n/i18n.js';
 import * as SDK from '../sdk/sdk.js';
 import * as UI from '../ui/ui.js';
@@ -11,8 +13,7 @@ export const UIStrings = {
   *@description Deprecation notice for the Remote Devices tool
   *@example {chrome://inspect/#devices} PH1
   */
-  thisPanelHasBeenDeprecatedIn:
-      'This panel has been deprecated in favor of the {PH1} interface, which has equivalent functionality.',
+  thisPanelHasBeenDeprecatedIn: 'This panel has been deprecated in favor of the {PH1} interface, which has equivalent functionality.',
 };
 const str_ = i18n.i18n.registerUIStrings('devices/DevicesView.js', UIStrings);
 
@@ -24,20 +25,18 @@ Devices.DevicesView = class extends UI.Widget.VBox {
     super(true);
 
     const deprecationMessage = this.contentElement.createChild('span');
-    const documentationLink = UI.Fragment.html
-    `<a class="devtools-link" role="link" tabindex="0" href="#" style="display: inline; cursor: pointer;">chrome://inspect/#devices</a>`;
+    const documentationLink = UI.Fragment.html `<a class="devtools-link" role="link" tabindex="0" href="#" style="display: inline; cursor: pointer;">chrome://inspect/#devices</a>`;
 
     self.onInvokeElement(documentationLink, event => {
       const mainTarget = SDK.SDKModel.TargetManager.instance().mainTarget();
       if (mainTarget) {
-        mainTarget.pageAgent().invoke_navigate({url: 'chrome://inspect/#devices'});
+        mainTarget.pageAgent().invoke_navigate({ url: 'chrome://inspect/#devices' });
       }
       event.consume(true);
     });
 
     deprecationMessage.style.padding = '5px';
-    deprecationMessage.appendChild(
-        i18n.i18n.getFormatLocalizedString(str_, UIStrings.thisPanelHasBeenDeprecatedIn, {PH1: documentationLink}));
+    deprecationMessage.appendChild(i18n.i18n.getFormatLocalizedString(str_, UIStrings.thisPanelHasBeenDeprecatedIn, { PH1: documentationLink }));
 
     this.setDefaultFocusedElement(documentationLink);
   }
