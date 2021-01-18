9fde1bf229bdc1def9b7407773f2ffd3fd0d8299
Annabel Zhou <annabelzhou@google.com>
2021-01-18 16:46:16 +1100

    Fetch unloaded children into the accessibility model and append to tree
    This change is hidden behind experiment flag "fullAccessibilityTree".
    
    Design doc: https://goo.gle/devtools-a11y-tree
    Follow-up CL: tying it to the DOM inspector.
    
    Bug: 887173
    Change-Id: I41c4ed01cc1fcb7326ffe9f39b09ce2cf83182b1

--- front_end/sdk/AccessibilityModel.js
+++ front_end/sdk/AccessibilityModel.js
@@ -55,6 +55,13 @@ export class AccessibilityNode {
   }
 
   /**
+   * @return {string}
+   */
+  id() {
+    return this._id;
+  }
+
+  /**
    * @return {!AccessibilityModel}
    */
   accessibilityModel() {
@@ -259,6 +266,7 @@ export class AccessibilityModel extends SDKModel {
    * @return ?{!Promise<AccessibilityNode>}
    */
   async requestRootNode(depth = 2) {
+    await this._agent.invoke_enable();
     const {nodes} = await this._agent.invoke_getFullAXTree({max_depth: depth});
     if (!nodes) {
       return;
@@ -272,6 +280,29 @@ export class AccessibilityModel extends SDKModel {
       }
     }
     return axNodes[0];
+  }
+
+  /**
+   * @param {!string} nodeId
+   * @return ?{!Promise<AccessibilityNode[]>}
+   */
+  async requestAXChildren(nodeId) {
+    const {nodes} = await this._agent.invoke_getChildAXNodes({id: nodeId});
+    if (!nodes) {
+      return;
+    }
+
+    const axNodes = [];
+    for (const payload of nodes) {
+      axNodes.push(new AccessibilityNode(this, payload));
+    }
+
+    for (const axNode of this._axIdToAXNode.values()) {
+      for (const axChild of axNode.children()) {
+        axChild._setParentNode(axNode);
+      }
+    }
+    return axNodes;
   }
 
   /**
