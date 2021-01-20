83de0ecd6dd67e2e305c916aff3da85b5efe7995
Michael Liao <michael.liao@microsoft.com>
2021-01-20 12:57:01 -0800

    [Font Editor]: Fix bug where deleting global value will remove selector
    Issue:
    - Font selector input does not repopulate after deleting a global value
    
    Changes:
    - Add boolean to check if a second selector exists before deleting selector
    
    Before: https://imgur.com/swXjStT
    
    After: https://imgur.com/e863WUt
    
    Bug: 1168630
    Change-Id: Ia6d534c0c79eca764b96ba5ce2cb20b6b88c1eb5

--- front_end/inline_editor/FontEditor.js
+++ front_end/inline_editor/FontEditor.js
@@ -287,9 +287,12 @@ export class FontEditor extends UI.Widget.VBox {
       index = 1;
     }
     if (fontSelectorObject.input.parentNode) {
-      this._fontSelectorSection.removeChild(fontSelectorObject.input.parentNode);
-      this._fontSelectors.splice(index, 1);
-      this._updateFontSelectorList();
+      const hasSecondarySelector = this._fontSelectors.length > 1;
+      if (!isPrimary || hasSecondarySelector) {
+        this._fontSelectorSection.removeChild(fontSelectorObject.input.parentNode);
+        this._fontSelectors.splice(index, 1);
+        this._updateFontSelectorList();
+      }
       UI.ARIAUtils.alert(i18nString(UIStrings.fontSelectorDeletedAtIndexS, {PH1: index}), this.contentElement);
     }
     this._onFontSelectorChanged();
