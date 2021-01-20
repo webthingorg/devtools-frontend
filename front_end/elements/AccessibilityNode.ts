f34a867a7e264906c95bb6106256d1c1b2248cbe
Annabel Zhou <annabelzhou@google.com>
2021-01-20 15:56:00 +1100

    Fetch unloaded children into the accessibility model and append to tree
    This change is hidden behind experiment flag "fullAccessibilityTree".
    
    Design doc: https://goo.gle/devtools-a11y-tree
    Follow-up CL: tying it to the DOM inspector.
    
    Bug: 887173
    Change-Id: I41c4ed01cc1fcb7326ffe9f39b09ce2cf83182b1

--- front_end/elements/AccessibilityNode.ts
+++ front_end/elements/AccessibilityNode.ts
@@ -44,9 +44,25 @@ export class AccessibilityNode extends HTMLElement {
 
     this.expanded = !this.expanded;
     this.classList.toggle('expanded', this.expanded);
+
+    if (this.axNode && this.axNode.hasOnlyUnloadedChildren()) {
+      this.getChildAXNodes();
+    }
   }
 
-  // TODO(annabelzhou): Track whether the children should be expanded and change arrow accordingly
+  private async getChildAXNodes(): Promise<void> {
+    if (!this.axNode) {
+      return;
+    }
+
+    const unloadedChildren = await this.axNode.accessibilityModel().requestAXChildren(this.axNode.id());
+    if (!unloadedChildren) {
+      return;
+    }
+
+    this.render();
+  }
+
   private renderChildren(node: SDK.AccessibilityModel.AccessibilityNode): LitHtml.TemplateResult {
     if (!node) {
       return LitHtml.html``;
@@ -112,7 +128,10 @@ export class AccessibilityNode extends HTMLElement {
     } else {
       const nodeContent = this.renderNodeContent();
 
-      if (this.axNode.numChildren()) {
+      if (this.axNode.hasOnlyUnloadedChildren()) {
+        this.shadow.host.classList.add('parent');
+        this.expanded = false;
+      } else if (this.axNode.numChildren()) {
         this.shadow.host.classList.add('parent', 'expanded');
       } else {
         this.shadow.host.classList.add('no-children');
