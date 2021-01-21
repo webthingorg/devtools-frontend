85e341c125895b4506e0bc976683d6039770c7aa
Andres Olivares <andoli@chromium.org>
2021-01-21 17:13:33 +0000

    [module.json extensions] timeline views
    Bug: 1134103
    Change-Id: I168d621048b93f00b16b6f7d4fd11d2e4bb4f031

--- front_end/devtools_app.js
+++ front_end/devtools_app.js
@@ -17,6 +17,7 @@ import './issues/issues-meta.js';
 import './help/help-meta.js';
 import './layers/layers-meta.js';
 import './lighthouse/lighthouse-meta.js';
+import './timeline/timeline-meta.js';
 import * as Startup from './startup/startup.js';
 
 Startup.RuntimeInstantiator.startApplication('devtools_app');
