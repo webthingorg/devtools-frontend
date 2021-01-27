8f3086c4338039127635e8e70e2e00fe7dcab5d7
Paul Lewis <aerotwist@chromium.org>
2021-01-27 09:36:07 +0000

    Adds component render coordination support
    Currently our components don't orchestrate their DOM updates, which
    leads to interleaved reading and writing layout-centric values, e.g.
    clientHeight, scrollTop etc.
    
    This CL introduces a helper to ensure that we do reads, writes, and
    scrolls at the correct point in the frame lifecycle. Future work would
    be to lint the use of the various layout-centric DOM APIs and to ensure
    that they are wrapped in the correct callbacks.
    
    See: https://gist.github.com/paulirish/5d52fb081b3570c81e3a
    Change-Id: Ie70df412ebb2db006bcbc50608d8855e093e7a07

---
+++ front_end/render_coordinator/RenderCoordinator.ts
@@ -0,0 +1,133 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+interface CoordinatorCallback {
+  (): unknown;
+}
+
+interface CoordinatorFrame {
+  readers: CoordinatorCallback[];
+  writers: CoordinatorCallback[];
+}
+
+const enum ACTION {
+  READ = 'read',
+  WRITE = 'write'
+}
+
+export class RenderCoordinatorQueueEmptyEvent extends Event {
+  constructor() {
+    super('queueempty');
+  }
+}
+
+let renderCoordinatorInstance: RenderCoordinator;
+
+export class RenderCoordinator extends EventTarget {
+  static instance({forceNew = false} = {}): RenderCoordinator {
+    if (!renderCoordinatorInstance || forceNew) {
+      renderCoordinatorInstance = new RenderCoordinator();
+    }
+
+    return renderCoordinatorInstance;
+  }
+
+  private readonly pendingWorkFrames: CoordinatorFrame[] = [];
+  private readonly resolvers = new WeakMap<CoordinatorCallback, (value: unknown) => void>();
+  private scheduledWorkId = 0;
+
+  private constructor() {
+    super();
+  }
+
+  async read<T extends unknown>(callback: CoordinatorCallback): Promise<T> {
+    return this.enqueueHandler<T>(callback, ACTION.READ);
+  }
+
+  async write<T extends unknown>(callback: CoordinatorCallback): Promise<T> {
+    return this.enqueueHandler<T>(callback, ACTION.WRITE);
+  }
+
+  async scroll<T extends unknown>(callback: CoordinatorCallback): Promise<T> {
+    return this.enqueueHandler<T>(callback, ACTION.WRITE);
+  }
+
+  private enqueueHandler<T = unknown>(callback: CoordinatorCallback, action: ACTION): Promise<T> {
+    if (this.pendingWorkFrames.length === 0) {
+      this.pendingWorkFrames.push({
+        readers: [],
+        writers: [],
+      });
+    }
+
+    const frame = this.pendingWorkFrames[0];
+    if (!frame) {
+      throw new Error('No frame available');
+    }
+
+    switch (action) {
+      case ACTION.READ:
+        frame.readers.push(callback);
+        break;
+
+      case ACTION.WRITE:
+        frame.writers.push(callback);
+        break;
+
+      default:
+        throw new Error('unknown action');
+    }
+
+    const resolverPromise = new Promise(resolve => {
+      this.resolvers.set(callback, resolve);
+    });
+
+    this.scheduleWork();
+    return resolverPromise as Promise<T>;
+  }
+
+  private handleWork(handler: CoordinatorCallback): void {
+    const data = handler.call(undefined);
+    const resolver = this.resolvers.get(handler);
+    if (!resolver) {
+      throw new Error('Unable to locate resolver');
+    }
+
+    resolver.call(undefined, data);
+    this.resolvers.delete(handler);
+  }
+
+  private scheduleWork(): void {
+    const hasPendingFrames = this.pendingWorkFrames.length > 0;
+    const hasScheduledWork = this.scheduledWorkId !== 0;
+    if (!hasPendingFrames || hasScheduledWork) {
+      // No pending frames means totally finished.
+      if (!hasPendingFrames) {
+        this.dispatchEvent(new RenderCoordinatorQueueEmptyEvent());
+      }
+      return;
+    }
+
+    this.scheduledWorkId = requestAnimationFrame(() => {
+      const frame = this.pendingWorkFrames.shift();
+      if (!frame) {
+        return;
+      }
+
+      for (const reader of frame.readers) {
+        this.handleWork(reader);
+      }
+
+      for (const writer of frame.writers) {
+        this.handleWork(writer);
+      }
+
+      // Since there may have been more work requested in
+      // the callback of a reader / writer, we attempt to schedule
+      // it at this point.
+      this.scheduledWorkId = 0;
+      this.scheduleWork();
+    });
+  }
+}
