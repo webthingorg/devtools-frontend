28437428e767d98f37e78ec0575625b72e461db9
Paul Lewis <aerotwist@chromium.org>
2021-01-27 09:36:07 +0000

    Adds component render coordination support
    Currently our components don't orchestrate their DOM updates, which
    leads to interleaved reading and writing layout-centric values, e.g.
    clientHeight, scrollTop etc.
    
    This CL introduces a helper to ensure that we do reads, writes, and
    scrolls at the correct point in the frame lifecycle. Future work would
    be to lint the use of the various layout-centric DOM APIs and to ensure
    that they are wrapped in the correct callbacks.
    
    Bug: 1171202
    See: https://gist.github.com/paulirish/5d52fb081b3570c81e3a
    Change-Id: Ie70df412ebb2db006bcbc50608d8855e093e7a07

---
+++ test/unittests/front_end/render_coordinator/render_coordinator_test.ts
@@ -0,0 +1,69 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+import * as RenderCoordinator from '../../../../front_end/render_coordinator/render_coordinator.js';
+
+describe('Render Coordinator', () => {
+  it('groups interleaved reads and writes', done => {
+    const expected = [
+      'Read 1',
+      'Read 2',
+      'Read 3',
+      'Write 1',
+      'Write 2',
+      'Write (inside Read 2)',
+    ];
+    const coordinator = RenderCoordinator.RenderCoordinator.RenderCoordinator.instance();
+    const actual: string[] = [];
+
+    coordinator.write(() => actual.push('Write 1'));
+    coordinator.read(() => actual.push('Read 1'));
+    coordinator.read(() => actual.push('Read 2'));
+    coordinator.write(() => actual.push('Write 2'));
+    coordinator.read(() => actual.push('Read 3'));
+
+    coordinator.addEventListener('queueempty', () => {
+      assert.deepEqual(actual, expected, 'render coordinator messages are out of order');
+      done();
+    });
+  });
+
+  it('handles nested reads and writes', done => {
+    const expected = [
+      'Read 1',
+      'Read 2',
+      'Write 1',
+      'Write 2',
+    ];
+    const coordinator = RenderCoordinator.RenderCoordinator.RenderCoordinator.instance();
+    const actual: string[] = [];
+
+    coordinator.read(() => {
+      actual.push('Read 1');
+      coordinator.write(() => actual.push('Write 1'));
+    });
+
+    coordinator.read(() => {
+      actual.push('Read 2');
+      coordinator.write(() => actual.push('Write 2'));
+    });
+
+    coordinator.addEventListener('queueempty', () => {
+      assert.deepEqual(actual, expected, 'render coordinator messages are out of order');
+      done();
+    });
+  });
+
+  it('returns values', async () => {
+    const element = document.createElement('div');
+    element.style.height = '800px';
+    document.body.appendChild(element);
+
+    const coordinator = RenderCoordinator.RenderCoordinator.RenderCoordinator.instance();
+    const height = await coordinator.read(() => element.clientHeight);
+
+    assert.strictEqual(height, 800);
+  });
+});
