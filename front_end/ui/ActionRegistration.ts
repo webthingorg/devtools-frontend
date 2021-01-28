ae0ab0ddd23847e486f5a012eda590a4c7a295c3
Andres Olivares <andoli@chromium.org>
2021-01-28 22:21:44 +0000

    [module.json extensions] Add `order` field to ActionRegistration
    A few actions define an order which sorts them when they are retrieved
    from UI.ActionRegistration. Several e2e tests rely on this order for
    their workflow.
    
    Bug: 1134103
    Change-Id: I365dca3b60a8eef35adc056a8ae75a7296a351de

--- front_end/ui/ActionRegistration.ts
+++ front_end/ui/ActionRegistration.ts
@@ -277,6 +277,10 @@ export class PreRegisteredAction extends Common.ObjectWrapper.ObjectWrapper impl
   condition(): string|undefined {
     return this.actionRegistration.condition;
   }
+
+  order(): number|undefined {
+    return this.actionRegistration.order;
+  }
 }
 
 const registeredActionExtensions: Array<PreRegisteredAction> = [];
@@ -293,9 +297,15 @@ export function registerActionExtension(registration: ActionRegistration): void 
 }
 
 export function getRegisteredActionExtensions(): Array<PreRegisteredAction> {
-  return registeredActionExtensions.filter(
-      action =>
-          Root.Runtime.Runtime.isDescriptorEnabled({experiment: action.experiment(), condition: action.condition()}));
+  return registeredActionExtensions
+      .filter(
+          action => Root.Runtime.Runtime.isDescriptorEnabled(
+              {experiment: action.experiment(), condition: action.condition()}))
+      .sort((firstAction, secondAction) => {
+        const order1 = firstAction.order() || 0;
+        const order2 = secondAction.order() || 0;
+        return order1 - order2;
+      });
 }
 
 export const enum Platforms {
@@ -324,6 +334,8 @@ export const ActionCategory = {
   INPUTS: ls`Inputs`,
   LAYERS: ls`Layers`,
   NAVIGATION: ls`Navigation`,
+  DRAWER: ls`Drawer`,
+  GLOBAL: ls`Global`,
 };
 
 type ActionCategory = typeof ActionCategory[keyof typeof ActionCategory];
@@ -485,4 +497,8 @@ export interface ActionRegistration {
    * property and in that case the behaviour of the action's availability will be inverted.
    */
   condition?: Root.Runtime.ConditionName;
+  /**
+   * Used to sort actions when all registered actions are queried.
+   */
+  order?: number;
 }
