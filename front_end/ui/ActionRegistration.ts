9cbdd96e8202c69605c173f420e51c15acd4bb78
Andres Olivares <andoli@chromium.org>
2021-01-28 16:10:05 +0000

    Reland "[module.json extensions] input actions"
    This reverts commit 6d0c7a8f26d4e80df88ef660a051c32edf78f694.
    
    Reason for revert: The original CL was failing as a side effect of an issue solved in https://chromium.googlesource.com/devtools/devtools-frontend/+/a1ee70e85e999910e91c5d8f79825a038546d9ae .
    
    Original change's description:
    > Revert "[module.json extensions] input actions"
    >
    > This reverts commit 5d81b459ad3fb84c44da8ffc80494f50e3b08975.
    >
    > Reason for revert: suspect for bot failure (https://ci.chromium.org/ui/p/devtools-frontend/builders/ci/DevTools%20Linux/4109/overview)
    >
    > Original change's description:
    > > [module.json extensions] input actions
    > >
    > > Bug: 1134103
    > > Change-Id: I61171ea3ac69b2123f0f604527f6ad5df8c3911c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2650515
    > > Commit-Queue: Andres Olivares <andoli@chromium.org>
    > > Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    >
    > TBR=tvanderlippe@chromium.org,andoli@chromium.org
    >
    > Change-Id: Ib4e25bf1a27bd4a079eaedc6b75631b9b4bc3993
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1134103
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2653091
    > Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    > Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    
    TBR=tvanderlippe@chromium.org,wolfi@chromium.org,andoli@chromium.org
    
    Bug: 1134103
    Change-Id: Id557985dd9a298549deff3746e944794066fdcf2

--- front_end/ui/ActionRegistration.ts
+++ front_end/ui/ActionRegistration.ts
@@ -320,6 +320,8 @@ export const ActionCategory = {
   PERFORMANCE: ls`Performance`,
   MOBILE: ls`Mobile`,
   SENSORS: ls`Sensors`,
+  HELP: ls`Help`,
+  INPUTS: ls`Inputs`,
 };
 
 type ActionCategory = typeof ActionCategory[keyof typeof ActionCategory];
@@ -332,6 +334,9 @@ export const enum IconClass {
   LARGEICON_CLEAR = 'largeicon-clear',
   LARGEICON_VISIBILITY = 'largeicon-visibility',
   LARGEICON_PHONE = 'largeicon-phone',
+  LARGEICON_PLAY = 'largeicon-play',
+  LARGEICON_PAUSE = 'largeicon-pause',
+  LARGEICON_RESUME = 'largeicon-resume',
 }
 
 export const enum KeybindSet {
