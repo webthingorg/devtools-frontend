f344775dc78c0f95467737e403599df02f47743a
Sigurd Schneider <sigurds@chromium.org>
2021-01-28 14:36:17 +0100

    Add test for heavy ad issues
    Drive-by: Fix bug in aggregation logic preventing HeavyAd
    issues from being aggregated.
    
    Bug: chromium:1072335
    Change-Id: I92d9df2111fb9301fc1c83337fd5abc3f1617101

--- front_end/sdk/HeavyAdIssue.js
+++ front_end/sdk/HeavyAdIssue.js
@@ -13,8 +13,7 @@ export class HeavyAdIssue extends Issue {
    * @param {!IssuesModel} issuesModel
    */
   constructor(issueDetails, issuesModel) {
-    const issue_code = [Protocol.Audits.InspectorIssueCode.HeavyAdIssue, issueDetails.reason].join('::');
-    super(issue_code, issuesModel);
+    super(Protocol.Audits.InspectorIssueCode.HeavyAdIssue, issuesModel);
     this._issueDetails = issueDetails;
   }
 
