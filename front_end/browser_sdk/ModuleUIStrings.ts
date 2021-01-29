e5ab4647e50f75c84a4d8ed83834d3ccb9506590
Jan Scheffler <janscheffler@chromium.org>
2021-01-29 17:36:20 +0100

    [JSDOC2TS]: Migrate browser_sdk
    Bug: chromium:1158760
    Change-Id: I4bc8b48fc46153f949039d43508311e5a48dd160

--- front_end/browser_sdk/ModuleUIStrings.ts
+++ front_end/browser_sdk/ModuleUIStrings.ts
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* eslint-disable rulesdir/no_underscored_properties */
+
 import * as i18n from '../i18n/i18n.js';
-export const UIStrings = {
-};
-i18n.i18n.registerUIStrings('browser_sdk/ModuleUIStrings.js', UIStrings);
+export const UIStrings = {};
+i18n.i18n.registerUIStrings('browser_sdk/ModuleUIStrings.ts', UIStrings);
