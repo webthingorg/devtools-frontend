a5cae277c70024af07aaf1e632636633e1751245
Peter Marshall <petermarshall@chromium.org>
2021-01-29 11:52:07 +0100

    [l10n] Prevent strings being passed as placeholder objects for i18n
    Previously we had callsites that were accidentally passing strings
    instead of objects. This prevents strings.
    
    Also-by: alexrudenko@chromium.org
    Bug: 1136655
    Change-Id: Ie98d33323c0c975c8225161901790b40685845ce

--- front_end/layer_viewer/LayerDetailsView.ts
+++ front_end/layer_viewer/LayerDetailsView.ts
@@ -422,7 +422,7 @@ export class LayerDetailsView extends UI.Widget.Widget implements LayerView {
       element.classList.add('active');
     }
     element.textContent = i18nString(UIStrings.scrollRectangleDimensions, {
-      PH1: slowScrollRectNames.get(scrollRect.type),
+      PH1: slowScrollRectNames.get(scrollRect.type) || null,
       PH2: scrollRect.rect.width,
       PH3: scrollRect.rect.height,
       PH4: scrollRect.rect.x,
