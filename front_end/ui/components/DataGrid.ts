40b1b6fd2d0a57308526d71b909da82195b87cf9
Jack Franklin <jacktfranklin@chromium.org>
2021-02-02 10:07:27 +0000

    [DataGrid] Don't attempt to re-focus column headers
    We were being a bit keen when trying to re-focus a data-grid row if it
    had lost focus due to being removed from the DOM and then added back in.
    We don't need to apply that logic if the focused cell is a header,
    because they are never removed from the DOM as the user scrolls.
    
    Fixed: chromium:1173437
    Change-Id: If27cbe70a4d3f7053581d0941869c30b827f2d57

--- front_end/ui/components/DataGrid.ts
+++ front_end/ui/components/DataGrid.ts
@@ -893,10 +893,13 @@ export class DataGrid extends HTMLElement {
       });
       // clang-format on
 
-      if (this.userHasFocused) {
-        // This ensures if the user has a cell focused, but then scrolls so that
-        // the focused cell is now not rendered, that when it then gets scrolled
-        // back in, that it becomes rendered.
+
+      // This ensures if the user has a cell focused, but then scrolls so that
+      // the focused cell is now not rendered, that when it then gets scrolled
+      // back in, that it becomes rendered.
+      // However, if the cell is a column header, we don't do this, as that
+      // can never be not-rendered.
+      if (this.userHasFocused && this.focusableCell[1] > 0) {
         this.focusCell(this.focusableCell);
       }
       this.scrollToBottomIfRequired();
