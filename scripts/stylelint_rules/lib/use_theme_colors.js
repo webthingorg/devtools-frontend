15c7c2be8b6d7aeae4ac12f67515797792ff79d2
Jack Franklin <jacktfranklin@chromium.org>
2021-02-02 12:30:32 +0000

    [DarkMode]: Plugin to lint color variable usage
    This introduces the `use_theme_colors` Stylelint plugin which checks
    that all colors used in our codebase use the CSS variables defined in
    either themeColors.css or inspectorStyle.css. Long term we will remove
    inspectorStyle.css but for now it's not going anywhere.
    
    It does this by looking at certain rules and seeing if there are any
    colors defined in them. If there are, it errors unless those use
    variables that are defined.
    
    It also can be run in fix mode, where it disables the rule for that
    given line. This is important as we have 100s of violations of this rule
    at the moment, and it's going to take time to get us to a place where we
    do not.
    
    It also will allow violations in a
    :host-context(.-theme-with-dark-background) block as those will normally
    be using specific colors to override (although this is discouraged).
    
    Bug: chromium:1152736
    Change-Id: I79281ba931ad94f76216ee1f4c8fd0406cf0c304

---
+++ scripts/stylelint_rules/lib/use_theme_colors.js
@@ -0,0 +1,127 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Abbreviated example
+const stylelint = require('stylelint');
+const postcss = require('postcss');
+const path = require('path');
+const fs = require('fs');
+
+const ruleName = 'plugin/use_theme_colors';
+
+const rulesThatUseColors = new Set([
+  'color', 'box-shadow', 'text-shadow', 'outline-color', 'background-image', 'background-color', 'border-left-color',
+  'border-right-color', 'border-top-color', 'border-bottom-color', '-webkit-border-image', 'fill', 'stroke'
+]);
+
+function getRootVariableDeclarationsFromCSSFile(filePath) {
+  const fileContents = fs.readFileSync(filePath, {encoding: 'utf-8'});
+  const definedVariableNames = new Set();
+  const parsed = postcss.parse(fileContents);
+  // Only parse the colors in the :root declaration.
+  parsed.walkRules(':root', rule => {
+    for (const node of (rule.nodes || [])) {
+      if (node.prop && node.prop.startsWith('--')) {
+        definedVariableNames.add(node.prop);
+      }
+    }
+  });
+  return definedVariableNames;
+}
+const themeColorsPath = path.join(__dirname, '..', '..', '..', 'front_end', 'ui', 'themeColors.css');
+const DEFINED_THEME_COLOR_VARIABLES = getRootVariableDeclarationsFromCSSFile(themeColorsPath);
+const inspectorStylesPath = path.join(__dirname, '..', '..', '..', 'front_end', 'ui', 'inspectorStyle.css');
+const DEFINED_INSPECTOR_STYLE_VARIABLES = getRootVariableDeclarationsFromCSSFile(inspectorStylesPath);
+/**
+ *
+ * @param {postcss.Rule} rule
+ * @returns {Array<postcss.Node>}
+ */
+function gatherColorDeclarations(rule) {
+  if (!rule.nodes) {
+    return [];
+  }
+  /**
+   * @type {Array<postcss.Node>}
+   */
+  const declarationsWithColors = [];
+
+  for (const node of rule.nodes) {
+    if (rulesThatUseColors.has(node.prop)) {
+      declarationsWithColors.push(node);
+    }
+  }
+  return declarationsWithColors;
+}
+
+const thingsThatIndicateColor = new Set([
+  // We don't have to check for named colors ("blue") as we lint to ban those separately.
+  /^#[a-zA-Z0-9]{3,6}/,
+  /^hsla?/,
+  /^rgba?/,
+]);
+
+
+module.exports = stylelint.createPlugin(ruleName, function(primary, secondary, context) {
+  return function(postcssRoot, postcssResult) {
+    function reportError(declaration) {
+      stylelint.utils.report({
+        message: 'All CSS color declarations should use a variable defined in ui/themeColors.css',
+        ruleName,
+        node: declaration,
+        result: postcssResult,
+      });
+    }
+
+    /**
+     * @param {postcss.Node} declaration
+     */
+    function dealWithError(declaration) {
+      if (context.fix) {
+        declaration.after(' /* stylelint-disable-line plugin/use_theme_colors */');
+
+      } else {
+        reportError(declaration);
+      }
+    }
+    const sourceFile = postcssResult.opts.from;
+    // For now, only apply this linting to CSS within our components.
+    if (sourceFile && path.extname(sourceFile) !== '.ts') {
+      return;
+    }
+    postcssRoot.walkRules(rule => {
+      // If you are providing a selector specifically for dark mode, you can use
+      // any colors you want, as it means you are purposefully deviating. This
+      // is not encouraged but we do need to allow it.
+      if (rule.selector.startsWith(':host-context(.-theme-with-dark-background)') ||
+          rule.selector.startsWith('.-theme-with-dark-background')) {
+        return;
+      }
+
+      const declarationsToCheck = gatherColorDeclarations(rule);
+
+      for (const declaration of declarationsToCheck) {
+        for (const indicator of thingsThatIndicateColor) {
+          if (indicator.test(declaration.value)) {
+            dealWithError(declaration);
+          }
+        }
+
+        if (declaration.value.startsWith('var')) {
+          const [match, variableName] = /var\((--[\w-]+)/.exec(declaration.value);
+          if (!match) {
+            throw new Error(`Could not parse CSS variable usage: ${declaration.value}`);
+          }
+          const variableIsValid =
+              DEFINED_INSPECTOR_STYLE_VARIABLES.has(variableName) || DEFINED_THEME_COLOR_VARIABLES.has(variableName);
+          if (!variableIsValid) {
+            dealWithError(declaration);
+          }
+        }
+      }
+    });
+  };
+});
+
+module.exports.ruleName = ruleName;
