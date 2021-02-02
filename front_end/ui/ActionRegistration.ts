b4804c96cc738dcd1faa89cf83721466b98b87b1
Andres Olivares <andoli@chromium.org>
2021-02-02 00:22:28 +0000

    [module.json extensions] sources actions & settings pt. 1
    Bug: 1134103
    Change-Id: I762128af2a63d2742a026f89fa2fff64416f1bab

--- front_end/ui/ActionRegistration.ts
+++ front_end/ui/ActionRegistration.ts
@@ -336,6 +336,9 @@ export const ActionCategory = {
   NAVIGATION: ls`Navigation`,
   DRAWER: ls`Drawer`,
   GLOBAL: ls`Global`,
+  DEBUGGER: ls`Debugger`,
+  RECORDER: ls`Recorder`,
+  SOURCES: ls`Sources`,
 };
 
 type ActionCategory = typeof ActionCategory[keyof typeof ActionCategory];
@@ -352,6 +355,11 @@ export const enum IconClass {
   LARGEICON_PAUSE = 'largeicon-pause',
   LARGEICON_RESUME = 'largeicon-resume',
   LARGEICON_TRASH_BIN = 'largeicon-trash-bin',
+  LARGEICON_STEP_OVER = 'largeicon-step-over',
+  LARGE_ICON_STEP_INTO = 'largeicon-step-into',
+  LARGE_ICON_STEP = 'largeicon-step',
+  LARGE_ICON_STEP_OUT = 'largeicon-step-out',
+  LARGE_ICON_DEACTIVATE_BREAKPOINTS = 'largeicon-deactivate-breakpoints',
 }
 
 export const enum KeybindSet {
