023172387061d55fe3a44eee21a49b933ab7fba7
Alex Rudenko <alexrudenko@chromium.org>
2021-02-02 09:11:08 +0000

    Report low contrast issues
    This CL adds the logic to trigger contrast checks on DevTools
    startup or on page load events. The check is only triggered if
    the corresponding experiment (contrastIssues) is enabled.
    Deduplication of the reported issues happens in the IssueManager
    on the frontend since the same issue might be reported twice
    if there are multiple documents on the target page.
    
    Bug: chromium:1155460
    Change-Id: Ib0d5115c527ceb9da9d2ed2dfa48cfdca6f82f60

---
+++ front_end/sdk/LowTextContrastIssue.ts
@@ -0,0 +1,46 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {ls} from '../common/common.js';  // eslint-disable-line rulesdir/es_modules_import
+
+import {AffectedElement, Issue, IssueCategory, IssueKind, MarkdownIssueDescription} from './Issue.js';  // eslint-disable-line no-unused-vars
+
+export class LowTextContrastIssue extends Issue {
+  private _details: Protocol.Audits.LowTextContrastIssueDetails;
+
+  constructor(issueDetails: Protocol.Audits.LowTextContrastIssueDetails) {
+    super('LowTextContrastIssue');
+    this._details = issueDetails;
+  }
+
+  primaryKey(): string {
+    return `${this.code()}-(${this._details.violatingNodeId})`;
+  }
+
+  getCategory(): IssueCategory {
+    return IssueCategory.LowTextContrast;
+  }
+
+  elements(): Iterable<AffectedElement> {
+    return [{
+      backendNodeId: this._details.violatingNodeId,
+      nodeName: this._details.violatingNodeSelector,
+    }];
+  }
+
+  details(): Protocol.Audits.LowTextContrastIssueDetails {
+    return this._details;
+  }
+
+  getDescription(): MarkdownIssueDescription {
+    return {
+      file: 'issues/descriptions/LowTextContrast.md',
+      substitutions: undefined,
+      issueKind: IssueKind.BreakingChange,
+      links: [
+        {link: 'https://web.dev/color-and-contrast-accessibility/', linkTitle: ls`Color and contrast accessibility`},
+      ],
+    };
+  }
+}
