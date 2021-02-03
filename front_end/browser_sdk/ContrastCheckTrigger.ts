fdefaadee43a18a112722e085ea2fce0933a7838
Alex Rudenko <alexrudenko@chromium.org>
2021-02-03 09:26:08 +0000

    Report low contrast issues
    This CL adds the logic to trigger contrast checks on DevTools
    startup or on page load events. The check is only triggered if
    the corresponding experiment (contrastIssues) is enabled.
    Deduplication of the reported issues happens in the IssueManager
    on the frontend since the same issue might be reported twice
    if there are multiple documents on the target page.
    
    Bug: chromium:1155460
    Change-Id: Ib0d5115c527ceb9da9d2ed2dfa48cfdca6f82f60

---
+++ front_end/browser_sdk/ContrastCheckTrigger.ts
@@ -0,0 +1,58 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Common from '../common/common.js';
+import * as Root from '../root/root.js';
+import * as SDK from '../sdk/sdk.js';
+
+let contrastCheckTriggerInstance: ContrastCheckTrigger|null = null;
+
+export class ContrastCheckTrigger extends Common.ObjectWrapper.ObjectWrapper {
+  private eventListeners: WeakMap<SDK.ResourceTreeModel.ResourceTreeModel, Common.EventTarget.EventDescriptor> =
+      new WeakMap();
+
+  constructor() {
+    super();
+    SDK.SDKModel.TargetManager.instance().observeModels(SDK.ResourceTreeModel.ResourceTreeModel, this);
+  }
+
+  static instance({forceNew}: {forceNew: boolean} = {forceNew: false}): ContrastCheckTrigger {
+    if (!contrastCheckTriggerInstance || forceNew) {
+      contrastCheckTriggerInstance = new ContrastCheckTrigger();
+    }
+
+    return contrastCheckTriggerInstance;
+  }
+
+  async modelAdded(resourceTreeModel: SDK.ResourceTreeModel.ResourceTreeModel): Promise<void> {
+    const listener = resourceTreeModel.addEventListener(SDK.ResourceTreeModel.Events.Load, this.pageLoaded, this);
+    this.eventListeners.set(resourceTreeModel, listener);
+    // If the target document finished loading, check the contrast immediately.
+    // Otherwise, it should be triggered when the page load event fires.
+    const response = await resourceTreeModel.target().runtimeAgent().invoke_evaluate(
+        {expression: 'document.readyState', returnByValue: true});
+    if (response.result && response.result.value === 'complete') {
+      this.checkContrast(resourceTreeModel);
+    }
+  }
+
+  modelRemoved(resourceTreeModel: SDK.ResourceTreeModel.ResourceTreeModel): void {
+    const listener = this.eventListeners.get(resourceTreeModel);
+    if (listener) {
+      Common.EventTarget.EventTarget.removeEventListeners([listener]);
+    }
+  }
+
+  private checkContrast(resourceTreeModel: SDK.ResourceTreeModel.ResourceTreeModel): void {
+    if (!Root.Runtime.experiments.isEnabled('contrastIssues')) {
+      return;
+    }
+    resourceTreeModel.target().auditsAgent().invoke_checkContrast();
+  }
+
+  private pageLoaded(event: Common.EventTarget.EventTargetEvent): void {
+    const {resourceTreeModel} = event.data as {resourceTreeModel: SDK.ResourceTreeModel.ResourceTreeModel};
+    this.checkContrast(resourceTreeModel);
+  }
+}
