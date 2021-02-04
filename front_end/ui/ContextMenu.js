3ffdef6bf5435bf416a1d49423c8c406b21b05c6
Andres Olivares <andoli@chromium.org>
2021-02-04 01:55:43 +0000

    [module.json extensions] @UI.ViewLocationResolver
    Bug: 1134103
    Change-Id: Ief50d175afafc627a40da5f61c9cfe3abde46bda

--- front_end/ui/ContextMenu.js
+++ front_end/ui/ContextMenu.js
@@ -628,7 +628,7 @@ export class ContextMenu extends SubMenu {
     // promises. Since the `_pendingTargets` array requires each of its indices
     // to have a matching array of providers, we need to duplicate the target
     // in that array as well.
-    this._pendingPromises.push(loadRegisteredProviders());
+    this._pendingPromises.push(loadApplicableRegisteredProviders(target));
     this._pendingTargets.push(target);
   }
 }
@@ -662,13 +662,35 @@ export function registerProvider(registration) {
   registeredProviders.push(registration);
 }
 
-async function loadRegisteredProviders() {
-  return Promise.all(registeredProviders.map(registration => registration.loadProvider()));
+/**
+ * @param {!Object} target
+ * @return {!Promise<Array<Provider>>} target
+ */
+async function loadApplicableRegisteredProviders(target) {
+  return Promise.all(
+      registeredProviders.filter(isProviderApplicableToContextTypes).map(registration => registration.loadProvider()));
+
+  /**
+   * @param {!ProviderRegistration} providerRegistration
+   * @return {boolean}
+   */
+  function isProviderApplicableToContextTypes(providerRegistration) {
+    if (!providerRegistration.contextTypes) {
+      return true;
+    }
+    for (const contextType of providerRegistration.contextTypes()) {
+      if (target instanceof contextType) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 /**
  * @typedef {{
- *   loadProvider: function(): !Promise<!Provider>
+ *  contextTypes: function(): !Array<?>
+ *  loadProvider: function(): !Promise<!Provider>
  * }} */
 // @ts-ignore typedef
 export let ProviderRegistration;
