1fcf857076a20aeab9a2415b50f54e09ba15669e
Andres Olivares <andoli@chromium.org>
2021-02-09 22:52:23 +0000

    [module.json extensions] @Common.Revealer Part 3
    Bug: 1134103
    Change-Id: I2ba1a57478e556ba4f77767560dbe7e0e548f0b6

--- front_end/common/Revealer.js
+++ front_end/common/Revealer.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Root from '../root/root.js';
+import {ls} from '../platform/platform.js';
 
 /**
  * @interface
@@ -23,14 +23,14 @@ export class Revealer {
  * @param {boolean=} omitFocus
  * @return {!Promise.<void>}
  */
-export let reveal = function(revealable, omitFocus) {
+export let reveal = async function(revealable, omitFocus) {
   if (!revealable) {
     return Promise.reject(new Error('Can\'t reveal ' + revealable));
   }
-  return Root.Runtime.Runtime.instance()
-      .allInstances(Revealer, revealable)
-      .then(revealers => reveal(/** @type {!Array<!Revealer>} */ (revealers)));
+  const revealers =
+      await Promise.all(getApplicableRegisteredRevealers(revealable).map(registration => registration.loadRevealer()));
 
+  return reveal(revealers);
   /**
    * @param {!Array.<!Revealer>} revealers
    * @return {!Promise.<void>}
@@ -56,9 +56,69 @@ export function setRevealForTest(newReveal) {
  * @return {?string}
  */
 export const revealDestination = function(revealable) {
-  const extension = Root.Runtime.Runtime.instance().extension(Revealer, revealable);
+  if (!revealable) {
+    if (!registeredRevealers[0]) {
+      return null;
+    }
+    return registeredRevealers[0].destination || null;
+  }
+  const extension = revealable ? getApplicableRegisteredRevealers(revealable)[0] : registeredRevealers[0];
   if (!extension) {
     return null;
   }
-  return extension.descriptor()['destination'];
+  return extension.destination || null;
+};
+
+/** @type {!Array<!RevealerRegistration>} */
+const registeredRevealers = [];
+
+/**
+ * @param {!RevealerRegistration} registration
+ */
+export function registerRevealer(registration) {
+  registeredRevealers.push(registration);
+}
+
+/**
+ * @param {!Object} revealable
+ * @return {!Array<RevealerRegistration>}
+ */
+function getApplicableRegisteredRevealers(revealable) {
+  return registeredRevealers.filter(isRevealerApplicableToContextTypes);
+
+  /**
+   * @param {!RevealerRegistration} revealerRegistration
+   * @return {boolean}
+   */
+  function isRevealerApplicableToContextTypes(revealerRegistration) {
+    if (!revealerRegistration.contextTypes) {
+      return true;
+    }
+    for (const contextType of revealerRegistration.contextTypes()) {
+      if (revealable instanceof contextType) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+/**
+ * @typedef {{
+  *  contextTypes: function(): !Array<?>,
+  *  loadRevealer: function(): !Promise<!Revealer>,
+  *  destination: (undefined|RevealerDestination)
+  * }} */
+// @ts-ignore typedef
+export let RevealerRegistration;
+
+/** @enum {string} */
+export const RevealerDestination = {
+  ELEMENTS_PANEL: ls`Elements panel`,
+  STYLES_SIDEBAR: ls`styles sidebar`,
+  CHANGES_DRAWER: ls`Changes drawer`,
+  ISSUES_VIEW: ls`Issues view`,
+  NETWORK_PANEL: ls`Network panel`,
+  APPLICATION_PANEL: ls`Application panel`,
+  SOURCES_PANEL: ls`Sources panel`,
 };
