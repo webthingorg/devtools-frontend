5bf57c388bd1f47bd3bebafb9fc18d01f83eb8a4
Yisi <yiorsi@gmail.com>
2021-02-09 17:22:17 +0800

    New: undefined CSS var() is set to gray
    The title is no longer displayed as Jump to definition. I don’t think it makes much sense. The underline usually means that it can be clicked.
    There is no need to display these words in this critical position. It is more practical to display the actual value of the custom properties.
    
    Screenshot: https://i.imgur.com/BjVx3So.png
    
    Bug: 1124707
    Change-Id: I33d8b37de2dc6911580f979e31ee61ebcd518e8f

--- front_end/inline_editor/CSSVarSwatch.ts
+++ front_end/inline_editor/CSSVarSwatch.ts
@@ -7,10 +7,6 @@ import * as LitHtml from '../third_party/lit-html/lit-html.js';
 
 export const UIStrings = {
   /**
-  *@description Text in CSS Variable Switch
-  */
-  jumpToDefinition: 'Jump to definition',
-  /**
   *@description Text displayed in a tooltip shown when hovering over a var() CSS function in the Styles pane when the custom property in this function does not exist. The parameter is the name of the property.
   *@example {--my-custom-property-name} PH1
   */
@@ -82,8 +78,7 @@ export class CSSVarSwatch extends HTMLElement {
       'css-var-link': true,
       'undefined': !isDefined,
     });
-    const title =
-        isDefined ? i18nString(UIStrings.jumpToDefinition) : i18nString(UIStrings.sIsNotDefined, {PH1: variableName});
+    const title = isDefined ? this.computedValue : i18nString(UIStrings.sIsNotDefined, {PH1: variableName});
     // The this.variableName's space must be removed, otherwise it cannot be triggered when clicked.
     const onClick = isDefined ? this.onLinkClick.bind(this, this.variableName.trim()) : null;
 
@@ -106,6 +101,7 @@ export class CSSVarSwatch extends HTMLElement {
       html`<style>
       .css-var-link:not(.undefined) {
         cursor: pointer;
+<<<<<<< HEAD
         text-underline-position: under;
         color: var(--link-color);
       }
@@ -116,6 +112,14 @@ export class CSSVarSwatch extends HTMLElement {
 
       .css-var-link:focus:not(:focus-visible) {
         outline: none;
+=======
+        text-decoration: underline;
+        text-underline-offset: 2px;
+      }
+
+      .css-var-link.undefined {
+        color: hsl(0deg 0% 46%);
+>>>>>>> a3cc2a573... New: Undefined CSS var() is set to gray
       }
       </style><span title="${this.computedValue || ''}">${functionParts.pre}${link}${functionParts.post}</span>`,
       this.shadow, { eventContext: this });
