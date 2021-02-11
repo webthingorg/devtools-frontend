0960c1a47333e452fd5b65d27e142957ba2518ba
Andres Olivares <andoli@chromium.org>
2021-02-11 22:06:29 +0000

    [module.json extensions] @Elements.MarkerDecorator
    Bug: 1134103
    Change-Id: I22853ec55c30125951a02a87983c58036a48af15

--- front_end/elements/MarkerDecorator.js
+++ front_end/elements/MarkerDecorator.js
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Common from '../common/common.js';
-import * as Root from '../root/root.js';  // eslint-disable-line no-unused-vars
-import * as SDK from '../sdk/sdk.js';     // eslint-disable-line no-unused-vars
+import * as Platform from '../platform/platform.js';  // eslint-disable-line no-unused-vars
+import {ls} from '../platform/platform.js';
+import * as SDK from '../sdk/sdk.js';  // eslint-disable-line no-unused-vars
+
+import {PseudoStateMarkerDecorator} from './ElementsPanel.js';  // eslint-disable-line no-unused-vars
 
 /**
  * @interface
@@ -24,11 +26,14 @@ export class MarkerDecorator {
  */
 export class GenericDecorator {
   /**
-   * @param {!Root.Runtime.Extension} extension
+   * @param {{marker: string, title: string, color: string}} extension
    */
   constructor(extension) {
-    this._title = Common.UIString.UIString(extension.title());
-    this._color = /** @type {string} */ (extension.descriptor()['color']);
+    if (!extension.title || !extension.color) {
+      throw new Error(`Generic decorator requires a color and a title: ${extension.marker}`);
+    }
+    this._title = extension.title;
+    this._color = /** @type {string} */ (extension.color);
   }
 
   /**
@@ -40,3 +45,40 @@ export class GenericDecorator {
     return {title: this._title, color: this._color};
   }
 }
+
+/**
+ * @return {!Array<!MarkerDecoratorRegistration>}
+ */
+export function getRegisteredDecorators() {
+  return [
+    new (function() {
+      this.marker = 'breakpoint-marker';
+      this.title = ls`DOM Breakpoint`;
+      this.color = 'rgb(105, 140, 254)';
+      this.decorator = new GenericDecorator(this);
+    })(),
+    new (function() {
+      this.marker = 'hidden-marker';
+      this.title = ls`Element is hidden`;
+      this.color = '#555';
+      this.decorator = new GenericDecorator(this);
+    })(),
+    {
+      decorator: PseudoStateMarkerDecorator.instance(),
+      marker: 'pseudo-state-marker',
+      title: undefined,
+      color: undefined,
+    },
+  ];
+}
+
+/**
+  * @typedef {{
+  *   decorator: MarkerDecorator,
+  *   marker: string,
+  *   color: string|undefined,
+  *   title: Platform.UIString.LocalizedString|undefined
+  * }}
+  */
+// @ts-ignore typedef
+export let MarkerDecoratorRegistration;
