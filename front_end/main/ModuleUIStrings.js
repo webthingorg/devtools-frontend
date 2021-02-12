fa37200cb0f86b1291783b9da3f4f8f7036b5b88
Christy Chen <chrche@microsoft.com>
2021-02-12 13:36:33 -0800

    [Loc] Migrate Main module to localization V2
    Migrate main module
    
    Bug: 1136655
    Change-Id: I32c522f8ec747c103bcb813425cf0f357c18d8f3

---
+++ front_end/main/ModuleUIStrings.js
@@ -0,0 +1,12 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as i18n from '../i18n/i18n.js';
+export const UIStrings = {
+  /**
+  *@description A title of the 'Extensions' UI.SettingUI category
+  */
+  extensions: 'Extensions',
+};
+i18n.i18n.registerUIStrings('main/ModuleUIStrings.js', UIStrings);
