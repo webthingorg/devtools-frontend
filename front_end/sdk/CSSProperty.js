32704ac70ce9dea44b98a4ecf8a3fb83883f4d7a
Andres Olivares <andoli@chromium.org>
2021-02-16 23:25:58 +0000

    [module.json extensions] @TextUtils.TokenizerFactory
    Bug: 1134103
    Change-Id: I83ff2d18fead54ff9b5cda224fc116865829a77c

--- front_end/sdk/CSSProperty.js
+++ front_end/sdk/CSSProperty.js
@@ -5,8 +5,8 @@
 import * as Common from '../common/common.js';
 import * as HostModule from '../host/host.js';
 import * as Platform from '../platform/platform.js';
-import * as Root from '../root/root.js';
 import * as TextUtils from '../text_utils/text_utils.js';
+import * as UI from '../ui/ui.js';
 
 import {cssMetadata, GridAreaRowRegex} from './CSSMetadata.js';
 import {Edit} from './CSSModel.js';                            // eslint-disable-line no-unused-vars
@@ -192,11 +192,7 @@ export class CSSProperty {
     const endIndentation = this.ownerStyle.cssText ? indentation.substring(0, this.ownerStyle.range.endColumn) : '';
     const text = new TextUtils.Text.Text(this.ownerStyle.cssText || '');
     const newStyleText = text.replaceRange(range, Platform.StringUtilities.sprintf(';%s;', propertyText));
-    const tokenizerFactory =
-        /** @type {!TextUtils.TextUtils.TokenizerFactory} */ (
-            await /** @type {!Root.Runtime.Extension} */ (
-                Root.Runtime.Runtime.instance().extension(TextUtils.TextUtils.TokenizerFactory))
-                .instance());
+    const tokenizerFactory = UI.SyntaxHighlighter.SyntaxHighlighter.codeMirrorTokenizerFactoryInstance();
     const styleText = CSSProperty._formatStyle(newStyleText, indentation, endIndentation, tokenizerFactory);
     return this.ownerStyle.setText(styleText, majorChange);
   }
