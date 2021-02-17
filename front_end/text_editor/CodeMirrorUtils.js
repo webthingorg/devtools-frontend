bfbd8390b245cd8eee1252f8eb167034f01485cb
Andres Olivares <andoli@chromium.org>
2021-02-17 17:14:39 +0000

    Move ui/SyntaxHighlighter.js to text_editor/
    Bug: none
    Change-Id: I9c7aa79a4b2b106669276f990161d273a7de9134

--- front_end/text_editor/CodeMirrorUtils.js
+++ front_end/text_editor/CodeMirrorUtils.js
@@ -29,6 +29,7 @@
  */
 
 import * as TextUtils from '../text_utils/text_utils.js';
+import * as UI from '../ui/ui.js';
 
 /**
  * @param {!TextUtils.TextRange.TextRange} range
@@ -91,10 +92,99 @@ export function pullLines(codeMirror, linesCount) {
   }
 }
 
+export class SyntaxHighlighter {
+  /**
+   * @param {string} mimeType
+   * @param {boolean} stripExtraWhitespace
+   */
+  constructor(mimeType, stripExtraWhitespace) {
+    this._mimeType = mimeType;
+    this._stripExtraWhitespace = stripExtraWhitespace;
+  }
+
+  /**
+   * @param {string} content
+   * @param {string} className
+   * @return {!Element}
+   */
+  createSpan(content, className) {
+    const span = document.createElement('span');
+    span.className = className.replace(/\S+/g, 'cm-$&');
+    if (this._stripExtraWhitespace && className !== 'whitespace') {
+      content = content.replace(/^[\n\r]*/, '').replace(/\s*$/, '');
+    }
+    UI.UIUtils.createTextChild(span, content);
+    return span;
+  }
+
+  /**
+   * @param {!Element} node
+   * @return {!Promise.<void>}
+   */
+  syntaxHighlightNode(node) {
+    const lines = node.textContent ? node.textContent.split('\n') : [];
+    /** @type {number} */
+    let plainTextStart;
+    /** @type {string} */
+    let line;
+
+    node.removeChildren();
+    const tokenize = TokenizerFactory.instance().createTokenizer(this._mimeType);
+    for (let i = 0; i < lines.length; ++i) {
+      line = lines[i];
+      plainTextStart = 0;
+      tokenize(line, processToken.bind(this));
+      if (plainTextStart < line.length) {
+        const plainText = line.substring(plainTextStart, line.length);
+        UI.UIUtils.createTextChild(node, plainText);
+      }
+      if (i < lines.length - 1) {
+        UI.UIUtils.createTextChild(node, '\n');
+      }
+    }
+    return Promise.resolve();
+
+    /**
+     * @param {string} token
+     * @param {?string} tokenType
+     * @param {number} column
+     * @param {number} newColumn
+     * @this {SyntaxHighlighter}
+     */
+    function processToken(token, tokenType, column, newColumn) {
+      if (!tokenType) {
+        return;
+      }
+
+      if (column > plainTextStart) {
+        const plainText = line.substring(plainTextStart, column);
+        UI.UIUtils.createTextChild(node, plainText);
+      }
+      node.appendChild(this.createSpan(token, tokenType));
+      plainTextStart = newColumn;
+    }
+  }
+}
+
+
+/** @type {!TokenizerFactory} */
+let tokenizerFactoryInstance;
+
 /**
  * @implements {TextUtils.TextUtils.TokenizerFactory}
  */
 export class TokenizerFactory {
+  /**
+   * @param {{forceNew: ?boolean}} opts
+   */
+  static instance(opts = {forceNew: null}) {
+    const {forceNew} = opts;
+    if (!tokenizerFactoryInstance || forceNew) {
+      tokenizerFactoryInstance = new TokenizerFactory();
+    }
+
+    return tokenizerFactoryInstance;
+  }
   /**
    * @override
    * @param {string} mimeType
