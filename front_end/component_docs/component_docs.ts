049d4142df90628e7cfe45fdc9b41fa4e7fb02a5
Wolfgang Beyer <wolfi@chromium.org>
2021-02-18 13:52:18 +0100

    add basic example
    Change-Id: I25bb80c604bc11cd4fd1de86688496cfbbebb10b

--- front_end/component_docs/component_docs.ts
+++ front_end/component_docs/component_docs.ts
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as i18n from '../i18n/i18n.js';
+
 import * as CreateBreadcrumbs from './create_breadcrumbs.js';
 import * as ToggleDarkMode from './toggle_dark_mode.js';
+
+i18n.i18n.registerLocale('en-US');
 
 ToggleDarkMode.init();
 CreateBreadcrumbs.init();
