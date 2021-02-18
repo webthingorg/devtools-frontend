674fd7c6f2e3fe58da0f989209663c3f8b7c1255
Peter Marshall <petermarshall@chromium.org>
2021-02-18 17:06:56 +0100

    [l10n] Lock some UI string terms and update descriptions.
    Bug: 1163928
    Change-Id: Ied271629613b32f70e4dbb839a82707b228b1efb

--- front_end/layer_viewer/LayerDetailsView.ts
+++ front_end/layer_viewer/LayerDetailsView.ts
@@ -80,7 +80,8 @@ export const UIStrings = {
   */
   containingBlocRectangleDimensions: 'Containing Block {PH1} × {PH2} (at {PH3}, {PH4})',
   /**
-  *@description Text in Layer Details View of the Layers panel
+  * @description Layers panel text meaning a Sticky Box which causes a layer to shift. In this case
+  * box means a 2D rectangular area of the website. Sticky means this area itself does not move.
   */
   nearestLayerShiftingStickyBox: 'Nearest Layer Shifting Sticky Box',
   /**
@@ -126,7 +127,7 @@ export const UIStrings = {
   /**
   *@description Compositing reason description displayed in the Layer Details View of the Layers panel
   */
-  hasADTransform: 'Has a 3d transform.',
+  hasADTransform: 'Has a 3D transform.',
   /**
   *@description Compositing reason description displayed in the Layer Details View of the Layers panel
   */
@@ -256,12 +257,12 @@ export const UIStrings = {
   *@description Compositing reason description displayed in the Layer Details View of the Layers panel
   */
   hasAPerspectiveTransformThat:
-      'Has a perspective transform that needs to be known by compositor because of 3d descendants.',
+      'Has a perspective transform that needs to be known by compositor because of 3D descendants.',
   /**
   *@description Compositing reason description displayed in the Layer Details View of the Layers panel
   */
   hasAPreservesdPropertyThatNeeds:
-      'Has a preserves-3d property that needs to be known by compositor because of 3d descendants.',
+      'Has a `preserves-3D` property that needs to be known by compositor because of 3D descendants.',
   /**
   *@description Compositing reason description displayed in the Layer Details View of the Layers panel
   */
