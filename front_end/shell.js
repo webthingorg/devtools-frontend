6da70ef9a1a4c36974e2dd0eeba881fd8e22b33a
Tim van der Lippe <tvanderlippe@chromium.org>
2021-02-18 16:28:44 +0000

    Remove browser_sdk/module.json
    The resources that were previously included in `browser_sdk`
    are already included in `issues/module.json`.
    
    R=andoli@chromium.org
    
    Bug: 1127902
    Change-Id: Ic9a8b6fd1961749bdc7bbf40a01f382ff26ca008

--- front_end/shell.js
+++ front_end/shell.js
@@ -12,7 +12,6 @@ import './workspace/workspace-legacy.js';
 import './bindings/bindings-legacy.js';
 import './components/components-legacy.js';
 import './persistence/persistence-legacy.js';
-import './browser_sdk/browser_sdk.js';
 import './extensions/extensions-legacy.js';
 import './console_counters/console_counters-legacy.js';
 import './sources/sources-meta.js';
