952f14f416b6cb0d31cf4113a94b9192570976cb
Andres Olivares <andoli@chromium.org>
2021-02-22 12:38:12 +0000

    Add missing meta files imports in worker_app.js
    Several meta files of modules that belong to the worker_app were not
    imported in worker_app.js when we started migrating extensions out from
    their corresponding module.json files. Thus, the functionalities of the
    migrated extensions were removed from the worker_app.
    
    This CL adds the corresponding imports to worker_app.js so that the
    functionalites are restored.
    
    Fixed: 1180613
    Change-Id: I8779a010fa4e37c9593aaaa7e0cf6893b13c6328

--- front_end/worker_app.js
+++ front_end/worker_app.js
@@ -2,7 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import './shell.js';
+import './browser_debugger/browser_debugger-meta.js';
+import './developer_resources/developer_resources-meta.js';
+import './elements/elements-meta.js';
+import './help/help-meta.js';
+import './issues/issues-meta.js';
 import './layer_viewer/layer_viewer-meta.js';
+import './network/network-meta.js';
+import './resources/resources-meta.js';
+import './timeline/timeline-meta.js';
 import * as Startup from './startup/startup.js';
 
 Startup.RuntimeInstantiator.startApplication('worker_app');
