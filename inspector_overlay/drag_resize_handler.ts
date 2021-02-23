7037a76637e761e6f80c9ac407b95abbf91542d6
Alex Rudenko <alexrudenko@chromium.org>
2021-02-23 08:59:51 +0000

    Implement interactive resizing of grid gaps
    Backend CL: crrev.com/c/2712870
    
    Bug: chromium:1181209
    Change-Id: Ib080a17d856baa60bfd2c8416bf23a1099abdf22

---
+++ inspector_overlay/drag_resize_handler.ts
@@ -0,0 +1,136 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export interface Draggable {
+  draggableCoordinate: 'clientX'|'clientY';
+  initialValue: number;
+  update(newValue: number): void;
+}
+
+export interface Delegate {
+  getDraggable(x: number, y: number): Draggable|undefined;
+}
+
+export class DragResizeHandler {
+  private document: Document;
+  private delegate: Delegate;
+  private origin = {
+    coord: null as number | null,
+    value: 0,
+  };
+  private temp = {
+    coord: null as number | null,
+    value: 0,
+  };
+  private settings = {
+    distanceThreshold: 10,
+    modifiers: {
+      alt: .1,
+      shift: 10,
+    },
+  };
+
+  constructor(document: Document, delegate: Delegate) {
+    this.document = document;
+    this.delegate = delegate;
+    this.onMouseHover = this.onMouseHover.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+  }
+
+  install() {
+    document.body.addEventListener('mousemove', this.onMouseHover);
+    document.body.addEventListener('mousedown', this.onMouseDown);
+  }
+
+  uninstall() {
+    document.body.removeEventListener('mousemove', this.onMouseHover);
+    document.body.removeEventListener('mousedown', this.onMouseDown);
+  }
+
+  /**
+   * Starts dragging
+   */
+  private onMouseDown(event: MouseEvent) {
+    const match = this.delegate.getDraggable(event.clientX, event.clientY);
+
+    if (!match) {
+      return;
+    }
+
+    this.origin.value = match.initialValue;
+    this.temp.value = match.initialValue;
+
+    const boundMouthMove = this.onDrag.bind(this, match);
+
+    event.stopPropagation();
+    event.preventDefault();
+    this.origin.coord = Math.round(event[match.draggableCoordinate]);
+    this.temp.coord = this.origin.coord;
+
+    document.body.removeEventListener('mousemove', this.onMouseHover);
+    document.body.style.cursor = match.draggableCoordinate === 'clientX' ? 'ew-resize' : 'ns-resize';
+
+    const endDrag = (event: Event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      this.origin.coord = null;
+      this.temp.coord = null;
+
+      document.body.style.cursor = 'default';
+      document.body.removeEventListener('mousemove', boundMouthMove);
+      document.body.addEventListener('mousemove', this.onMouseHover);
+      document.body.removeEventListener('mouseup', endDrag);
+      window.removeEventListener('mouseout', endDrag);
+    };
+
+    document.body.addEventListener('mouseup', endDrag, {once: true});
+    window.addEventListener('mouseout', endDrag, {once: true});
+
+    document.body.addEventListener('mousemove', boundMouthMove);
+  }
+
+  /**
+   * Computes the new value while the cursor is being dragged and calls InspectorOverlayHost with the new value.
+   */
+  private onDrag(match: Draggable, e: MouseEvent) {
+    if (this.origin.coord === null) {
+      return;
+    }
+
+    const newTargetCoord = Math.round(parseInt(String(e[match.draggableCoordinate]), 10));
+    let delta = Math.round(((this.temp.coord || 0) - newTargetCoord) / this.settings.distanceThreshold);
+
+    let amount = 1;
+
+    if (e.shiftKey) {
+      amount = this.settings.modifiers.shift;
+    }
+    if (e.altKey) {
+      amount = this.settings.modifiers.alt;
+    }
+
+    delta *= amount * -1;
+
+    let newValue = this.temp.value + delta;
+
+    if (!Number.isInteger(newValue)) {
+      newValue = parseFloat(newValue.toFixed(1));
+    }
+
+    if (newValue !== this.temp.value) {
+      this.temp.coord = newTargetCoord;
+      this.temp.value = newValue;
+      match.update(newValue);
+    }
+  }
+
+  /**
+   * Updates the cursor style of the mouse is hovered over a resizeable area.
+   */
+  private onMouseHover(event: MouseEvent) {
+    const match = this.delegate.getDraggable(event.clientX, event.clientY);
+    this.document.body.style.cursor =
+        match ? (match.draggableCoordinate === 'clientX' ? 'ew-resize' : 'ns-resize') : 'default';
+  }
+}
--- inspector_overlay/tool_persistent.ts
+++ inspector_overlay/tool_persistent.ts
@@ -29,18 +29,57 @@
 //  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import {Overlay, ResetData} from './common.js';
+import {Delegate, DragResizeHandler} from './drag_resize_handler.js';
 import {drawLayoutFlexContainerHighlight, FlexContainerHighlight} from './highlight_flex_common.js';
 import {drawLayoutGridHighlight, GridHighlight} from './highlight_grid_common.js';
+
+interface GapMetadata {
+  rowGapPath: Path2D|null;
+  columnGapPath: Path2D|null;
+  // Stores the index of the grid highlight config so that it can be found later.
+  highlightIdx: number;
+  rowGapValue: number;
+  columnGapValue: number;
+}
+
+function makeDraggableDelegate(overlay: PersistentOverlay): Delegate {
+  return {
+    getDraggable: (x, y) => {
+      const result = overlay.isPointInGapPath(x, y);
+      if (!result) {
+        return;
+      }
+      return {
+        draggableCoordinate: result.matchType === 'column' ? 'clientX' : 'clientY',
+        initialValue: result.matchType === 'column' ? result.match.columnGapValue : result.match.rowGapValue,
+        id: result.match.highlightIdx,
+        update: newValue => {
+          window.InspectorOverlayHost.send(JSON.stringify({
+            matchType: result.matchType,
+            highlightIdx: result.match.highlightIdx,
+            value: newValue + 'px',
+          }));
+        },
+      };
+    },
+  };
+}
 
 export class PersistentOverlay extends Overlay {
   private gridLabelState = {gridLayerCounter: 0};
 
   private gridLabels!: HTMLElement;
+  private gaps: Array<GapMetadata> = [];
+  // Keeps track drawGridHighlight requests and assigns an index to each one.
+  private highlightIdx = 0;
+  private dragHandler = new DragResizeHandler(document, makeDraggableDelegate(this));
 
   reset(data: ResetData) {
     super.reset(data);
     this.gridLabelState.gridLayerCounter = 0;
     this.gridLabels.innerHTML = '';
+    this.gaps = [];
+    this.highlightIdx = 0;
   }
 
   renderGridMarkup() {
@@ -63,20 +102,33 @@ export class PersistentOverlay extends Overlay {
     this.setCanvas(canvas);
 
     super.install();
+
+    this.dragHandler.install();
   }
 
   uninstall() {
     this.document.body.classList.remove('fill');
     this.document.body.innerHTML = '';
+    this.gaps = [];
+    this.highlightIdx = 0;
     super.uninstall();
+    this.dragHandler.uninstall();
   }
 
   drawGridHighlight(highlight: GridHighlight) {
     this.context.save();
-    drawLayoutGridHighlight(
+    const result = drawLayoutGridHighlight(
         highlight, this.context, this.deviceScaleFactor, this.canvasWidth, this.canvasHeight, this.emulationScaleFactor,
         this.gridLabelState);
     this.context.restore();
+    this.gaps.push({
+      rowGapPath: result.rowGapPath,
+      columnGapPath: result.columnGapPath,
+      highlightIdx: this.highlightIdx,
+      rowGapValue: parseInt(highlight.rowGap, 10),
+      columnGapValue: parseInt(highlight.columnGap, 10),
+    });
+    this.highlightIdx++;
   }
 
   drawFlexContainerHighlight(highlight: FlexContainerHighlight) {
@@ -85,5 +137,24 @@ export class PersistentOverlay extends Overlay {
         highlight, this.context, this.deviceScaleFactor, this.canvasWidth, this.canvasHeight,
         this.emulationScaleFactor);
     this.context.restore();
+  }
+
+  isPointInGapPath(x: number, y: number): {match: GapMetadata, matchType: 'column'|'row'}|null {
+    const context = this.context;
+    let matchType: 'column'|'row' = 'row';
+    let match: GapMetadata|null = null;
+    for (const gap of this.gaps) {
+      if (gap.columnGapPath && context.isPointInPath(gap.columnGapPath, x, y)) {
+        matchType = 'column';
+        match = gap;
+        break;
+      }
+      if (gap.rowGapPath && context.isPointInPath(gap.rowGapPath, x, y)) {
+        matchType = 'row';
+        match = gap;
+        break;
+      }
+    }
+    return match ? {matchType, match} : null;
   }
 }
