09afda33582eb6f50c005ff70f01a822ff9567a1
Andres Olivares <andoli@chromium.org>
2021-02-23 21:14:55 +0000

    Add unit test for CSSProperty._formatStyle
    This replaces the layout test in
    http/tests/devtools/elements/styles/styles-format-style.js as a unit
    test.
    
    Bug: none
    Change-Id: I5d86356a51d5f655f395714ce3c526a3b7581ce5

--- front_end/text_editor/CodeMirrorUtils.js
+++ front_end/text_editor/CodeMirrorUtils.js
@@ -111,13 +111,20 @@ export class TokenizerFactory {
   }
 
   /**
+   * @param {string} mimeType
+   */
+  getMode(mimeType) {
+    return CodeMirror.getMode({indentUnit: 2}, mimeType);
+  }
+  /**
    * @override
    * @param {string} mimeType
+   * @param {!CodeMirror.Mode<*>=} mode
    * @return {function(string, function(string, ?string, number, number))}
    */
-  createTokenizer(mimeType) {
-    const mode = CodeMirror.getMode({indentUnit: 2}, mimeType);
-    const state = CodeMirror.startState(mode);
+  createTokenizer(mimeType, mode) {
+    const cmMode = mode || CodeMirror.getMode({indentUnit: 2}, mimeType);
+    const state = CodeMirror.startState(cmMode);
     /**
      * @param {string} line
      * @param {function(string, (string|null), number, number):void} callback
@@ -125,8 +132,7 @@ export class TokenizerFactory {
     function tokenize(line, callback) {
       const stream = new CodeMirror.StringStream(line);
       while (!stream.eol()) {
-        // @ts-expect-error CodeMirror types specify token as optional.
-        const style = mode.token(stream, state);
+        const style = cmMode.token(stream, state);
         const value = stream.current();
         callback(value, style, stream.start, stream.start + value.length);
         stream.start = stream.pos;
