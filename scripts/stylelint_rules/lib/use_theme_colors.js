cddcdfbc47eb2634bb5b0aff6f11be95bbc06a41
Jack Franklin <jacktfranklin@chromium.org>
2021-02-24 14:48:52 +0000

    merge inspectorStyle.css and inspectorCommon.css
    It seems there is no requirement for these files to be split; they don't
    seem to have a logical split and they are both injected into the `body`
    element when DevTools runs. If we kept both of these files around, we'd
    have to inject them both into the component docs helpers, and deal with
    both of them when it comes to figuring out where legacy CSS variables
    are defined.
    
    To make it a bit simpler I've merged `inspectorStyle` into
    `inspectorCommon`. I went this way because:
    
    * `inspectorCommon.css` is (I think!) a better name than `inspectorStyle.css`
    * `inspectorCommon.css` was bigger.
    
    I also drive-by disabled the stylelint `comment-empty-line-before`,
    which was forbidding empty lines before any CSS comments; which made the
    entire file feel very squashed!
    
    Bug: none
    Change-Id: Ifaa834c7bf56291561e2e5cc124c4efd85c3cc56

--- scripts/stylelint_rules/lib/use_theme_colors.js
+++ scripts/stylelint_rules/lib/use_theme_colors.js
@@ -23,7 +23,7 @@ const COLOR_INDICATOR_REGEXES = new Set([
 ]);
 
 const themeColorsPath = path.join(__dirname, '..', '..', '..', 'front_end', 'ui', 'themeColors.css');
-const inspectorStylesPath = path.join(__dirname, '..', '..', '..', 'front_end', 'ui', 'inspectorStyle.css');
+const inspectorCommonPath = path.join(__dirname, '..', '..', '..', 'front_end', 'ui', 'inspectorCommon.css');
 
 function getRootVariableDeclarationsFromCSSFile(filePath) {
   const fileContents = fs.readFileSync(filePath, {encoding: 'utf-8'});
@@ -41,7 +41,7 @@ function getRootVariableDeclarationsFromCSSFile(filePath) {
 }
 
 const DEFINED_THEME_COLOR_VARIABLES = getRootVariableDeclarationsFromCSSFile(themeColorsPath);
-const DEFINED_INSPECTOR_STYLE_VARIABLES = getRootVariableDeclarationsFromCSSFile(inspectorStylesPath);
+const DEFINED_INSPECTOR_STYLE_VARIABLES = getRootVariableDeclarationsFromCSSFile(inspectorCommonPath);
 
 module.exports = stylelint.createPlugin(RULE_NAME, function(primary, secondary, context) {
   return function(postcssRoot, postcssResult) {
