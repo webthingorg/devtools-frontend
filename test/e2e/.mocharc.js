02ae1559519239c9b1b850ae63b6046c77abf6a1
Philip Pfaffe <pfaffe@chromium.org>
2021-03-01 11:37:37 +0100

    Enable globbing patterns for running e2e tests
    By supporting glob patterns for specifying tests to run (either through
    --test-file or the TEST_FILE env var), it's possible to run tests for a
    specific panel only, for example. For instance, tests can be run for
    'sources/*' with this change.
    
    Bug: none
    Change-Id: I3c5d319b58a937758533ae679a628e19726512d2

--- test/e2e/.mocharc.js
+++ test/e2e/.mocharc.js
@@ -11,38 +11,41 @@ const fs = require('fs');
 // aren't incorrectly included, we glob for the TypeScript files instead and use that
 // to instruct Mocha to run the output JavaScript file.
 const ROOT_DIRECTORY = path.join(__dirname, '..', '..', '..', '..', '..', 'test', 'e2e');
-let testFiles = glob.sync(path.join(ROOT_DIRECTORY, '**/*_test.ts')).map(fileName => {
+
+const hasCustomPattern = 'TEST_FILE' in process.env;
+const testPattern = (() => {
+  if (!hasCustomPattern) {
+    return '**/*_test.ts';
+  }
+  const spec = process.env['TEST_FILE'];
+  if (spec.endsWith('_test.ts')) {
+    return spec
+  } else if (spec.endsWith('_test')) {
+    return `${spec}.ts`;
+  } else if (spec.endsWith('_test.js')) {
+    return `${spec.slice(-2)}js`;
+  }
+  return `${spec}_test.ts`;
+})();
+
+
+
+const testFiles = glob.sync(path.join(ROOT_DIRECTORY, testPattern)).map(fileName => {
   const renamedFile = fileName.replace(/\.ts$/, '.js');
   const generatedFile = path.join(__dirname, path.relative(ROOT_DIRECTORY, renamedFile));
 
   if (!fs.existsSync(generatedFile)) {
-    throw new Error(`Test file missing in "ts_library": ${generatedFile}`);
+    if (hasCustomPattern) {
+      throw new Error(
+          `\nNo test found matching --test-file=${process.env['TEST_FILE']}.` +
+          ' Use a relative path from test/e2e/.');
+    } else {
+      throw new Error(`Test file missing in "ts_library": ${generatedFile}`);
+    }
   }
 
   return generatedFile;
 });
-
-// Respect the test file if defined.
-// This way you can test one single file instead of running all e2e tests every time.
-if (process.env['TEST_FILE']) {
-  let testFile = process.env['TEST_FILE'];
-  // Accept .ts, .js and no extension.
-  testFile = testFile.replace(/\.ts$/, '.js');
-  if (!testFile.endsWith('.js')) {
-    testFile += '.js';
-  }
-  const absoluteTestFile = path.join(__dirname, testFile);
-
-  // We also need to filter the test against the .ts list above because there can be
-  // leftover .js files from previous builds.
-  if (!testFiles.includes(absoluteTestFile)) {
-    // The leading \n makes the failure easier to find in the output.
-    throw new Error(
-        `\nNo test found matching --test-file=${process.env['TEST_FILE']}.` +
-        ' Use a relative path from test/e2e/.');
-  }
-  testFiles = absoluteTestFile;
-}
 
 // When we are debugging, we don't want to timeout any test. This allows to inspect the state
 // of the application at the moment of the timeout. Here, 0 denotes "indefinite timeout".
