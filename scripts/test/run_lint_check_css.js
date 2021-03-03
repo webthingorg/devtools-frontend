56cd185f128f2516b476e56a2ac06990264255be
Jack Franklin <jacktfranklin@chromium.org>
2021-03-03 14:17:24 +0000

    Parameterise run_lint_check_css script
    It now takes a flag to customise the CWD that it runs, and also
    correctly passes all stylelint arguments through. Before any stylelint
    argument that had a dash in would be passed through twice, and that
    would cause stylelint to error.
    
    Bug: 1184228
    Change-Id: I21c0d9561dcd055968cf4bf9b4514b8184040b88

--- scripts/test/run_lint_check_css.js
+++ scripts/test/run_lint_check_css.js
@@ -13,6 +13,15 @@ const {
 const yargsObject = require('yargs')
                         .option('files', {type: 'array', desc: 'One or more files to lint.'})
                         .option('glob', {default: '**/*.css', desc: 'A glob to choose which files to lint.'})
+                        .option('cwd', {default: devtoolsRootPath(), desc: 'Working directory to glob from'})
+                        .parserConfiguration({
+                          // So that if we pass --foo-bar, Yargs only populates
+                          // argv with '--foo-bar', not '--foo-bar' and
+                          // 'fooBar'. This is important because if we have both
+                          // versions and pass them to stylelint, it errors
+                          // saying that we've passed the same argument twice.
+                          'camel-case-expansion': false
+                        })
                         .argv;
 
 // Note: stylelint requires POSIX-formatted paths/globs, even on Windows.
@@ -33,7 +42,7 @@ function getCSSFilesOrGlobList() {
 
 function run() {
   // eslint-disable-next-line no-unused-vars
-  const {glob, files, _, $0, ...flagsForStylelint} = yargsObject;
+  const {glob, files, cwd, _, $0, ...flagsForStylelint} = yargsObject;
 
   /*
    * This reduce is a flatten, because our Node version
@@ -45,9 +54,7 @@ function run() {
   const args = [
     stylelintExecutablePath(), ...getCSSFilesOrGlobList(), '--fix', '--allow-empty-input', ...extraFlagsForStylelint
   ];
-
-  const result =
-      childProcess.spawnSync(nodePath(), args, {encoding: 'utf-8', cwd: devtoolsRootPath(), stdio: 'inherit'});
+  const result = childProcess.spawnSync(nodePath(), args, {encoding: 'utf-8', cwd, stdio: 'inherit'});
   process.exit(result.status);
 }
 
