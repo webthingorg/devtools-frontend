b42d6ea14db325d6331825660011f1afb866c0e1
Wolfgang Beyer <wolfi@chromium.org>
2021-03-04 15:03:15 +0100

    remove double license headers
    Change-Id: I368280caee2b4b77fa70d3dc062beef0e3bdc8d4

--- front_end/resources/CookieItemsView.ts
+++ front_end/resources/CookieItemsView.ts
@@ -1,7 +1,3 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 /*
  * Copyright (C) 2009 Apple Inc.  All rights reserved.
  * Copyright (C) 2009 Joseph Pecoraro
