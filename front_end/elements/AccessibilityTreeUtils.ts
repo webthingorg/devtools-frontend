36e73b8c35966ee2a25c40b6733a981a152b67bf
Meredith Lane <meredithl@chromium.org>
2021-03-10 15:10:49 +1100

    Refactor new AccessibilityTree to use TreeOutline component.
    Removes the custom components AccessibilityTree, AccessibilityNode and
    tests in favour of the new TreeOutline component.
    
    There are some requirements in order to bring the refactor to parity
    with the existing implementation:
    
    1) Attach click/mouse handlers to TreeNodes. We will need this to
        activate the DOM node overlay, as well as allow the a11y tree to
        set the inspected DOM node.
    2) Set focus on the first node in the TreeOutline, as per WAI-ARIA
        guidelines: https://www.w3.org/TR/wai-aria-practices/#keyboard-interaction-22
    
    Change-Id: I61c30e7f510dde316aaaa45a7f5ab382603f180e
    Bug: 887173

--- front_end/elements/AccessibilityTreeUtils.ts
+++ front_end/elements/AccessibilityTreeUtils.ts
@@ -2,57 +2,94 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as i18n from '../i18n/i18n.js';
+import * as Platform from '../platform/platform.js';
 import * as SDK from '../sdk/sdk.js';
-import {AccessibilityTree} from './AccessibilityTree.js';
+import * as LitHtml from '../third_party/lit-html/lit-html.js';
+import * as UIComponents from '../ui/components/components.js';
 
-export interface AXNode {
-  id: string;
-  role: string|null;
-  name: string|null;
-  ignored: boolean;
-  parent: AXNode|null;
-  hasChildren: () => boolean;
-  children: () => Promise<AXNode[]>;
-  axTree: AccessibilityTree|null;
-  highlightNode: () => void;
-  clearHighlight: () => void;
-}
+type AXTreeNode = UIComponents.TreeOutlineUtils.TreeNode<SDK.AccessibilityModel.AccessibilityNode>;
 
-export function sdkNodeToAXNode(
-    parent: AXNode|null, sdkNode: SDK.AccessibilityModel.AccessibilityNode, tree: AccessibilityTree): AXNode {
-  let axChildren: AXNode[] = [];
+const UIStrings = {
+  /**
+  *@description Ignored node element text content in Accessibility tree outline of the Elements panel
+  */
+  ignored: 'Ignored',
+};
+const str_ = i18n.i18n.registerUIStrings('elements/AccessibilityTreeUtils.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 
-  const axNode = {
-    id: sdkNode.id(),
-    role: sdkNode.role()?.value,
-    name: sdkNode.name()?.value,
-    ignored: sdkNode.ignored(),
-    parent: parent,
-    axTree: tree,
-    hasChildren: (): boolean => Boolean(sdkNode.numChildren()),
-    // TODO: Remove next line once crbug.com/1177242 is solved.
-    // eslint-disable-next-line @typescript-eslint/space-before-function-paren
-    children: async(): Promise<AXNode[]> => {
-      // sdkNode.numChildren() returns the true number of children that exist in the
-      // backend whereas axChildren contains only nodes that have been fetched by the frontend.
-      // If the number of local children is different from the number of children known
-      // to the backend, we load the children and expect the numbers to match.
-      if (sdkNode.numChildren() !== axChildren.length) {
-        const children = await sdkNode.accessibilityModel().requestAXChildren(sdkNode.id());
-        axChildren = (children || []).map(child => sdkNodeToAXNode(axNode, child, tree));
-        if (axChildren.length !== sdkNode.numChildren()) {
-          throw new Error('Unexpected: actual and expected number of child nodes is different');
-        }
-      }
-      return axChildren;
-    },
-    highlightNode: (): void => sdkNode.highlightDOMNode(),
-    clearHighlight: (): void => SDK.OverlayModel.OverlayModel.hideDOMNodeHighlight(),
-  };
 
-  for (const child of sdkNode.children()) {
-    axChildren.push(sdkNodeToAXNode(axNode, child, tree));
+export function sdkNodeToAXTreeNode(node: SDK.AccessibilityModel.AccessibilityNode): AXTreeNode {
+  if (!node.numChildren()) {
+    return {
+      treeNodeData: node,
+    };
   }
 
-  return axNode;
+  return {
+    treeNodeData: node,
+    children: async(): Promise<AXTreeNode[]> => {
+      let children: SDK.AccessibilityModel.AccessibilityNode[] = node.children() || [];
+      if (node.numChildren() !== children.length) {
+        children = await node.accessibilityModel().requestAXChildren(node.id());
+      }
+      const treeNodeChildren = (children || []).map(child => sdkNodeToAXTreeNode(child));
+      return Promise.resolve(treeNodeChildren);
+    },
+  };
+}
+
+// This function is a variant of setTextContentTruncatedIfNeeded found in DOMExtension.
+function truncateTextIfNeeded(text: string): string {
+  const maxTextContentLength = 10000;
+
+  if (text.length > maxTextContentLength) {
+    return Platform.StringUtilities.trimMiddle(text, maxTextContentLength);
+  }
+  return text;
+}
+
+export function accessibilityNodeRenderer(
+    node: UIComponents.TreeOutlineUtils.TreeNode<SDK.AccessibilityModel.AccessibilityNode>): LitHtml.TemplateResult {
+  const templateResult: LitHtml.TemplateResult[] = [];
+  const axNode = node.treeNodeData;
+
+  // TODO(meredithl): Ignored nodes (and their potential children) to be handled in the future.
+  if (axNode.ignored()) {
+    templateResult.push(LitHtml.html`<span class='monospace ignored-node'>${i18nString(UIStrings.ignored)}</span>`);
+  } else {
+    const role = axNode.role();
+    if (!role) {
+      return LitHtml.html``;
+    }
+
+    const roleElement = LitHtml.html`<span class='monospace'>${truncateTextIfNeeded(role.value || '')}</span>`;
+    templateResult.push(LitHtml.html`${roleElement}`);
+
+    const name = axNode.name();
+    if (name) {
+      templateResult.push(LitHtml.html`<span class='separator'>\xA0</span>`);
+      templateResult.push(LitHtml.html`<span class='ax-readable-string'>"${name.value}"</span>`);
+    }
+  }
+
+  return LitHtml.html`
+      <style>
+      .ignored-node {
+        font-style: italic;
+        opacity: 70%;
+      }
+
+      .ax-readable-string {
+        font-style: italic;
+      }
+
+      .monospace {
+        font-family: var(--monospace-font-family);
+        font-size: var(--monospace-font-size);
+      }
+      </style>
+      ${templateResult}
+      `;
 }
