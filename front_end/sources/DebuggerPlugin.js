5479cf80df8266dd5610fb3f430b204b6f990062
Andres Olivares <andoli@chromium.org>
2021-03-15 17:15:51 +0000

    Explicitly focus BreakpointEditDialog editor after the dialog is shown
    This dialog is shown when a breakpoint is requested to be edited in the
    sources panel. For this, it utilizes an instance of
    CodeMirrorTextEditorFactory which was previously instanced
    asynchronously via legacy runtime extensions. This async instancing
    apparently introduced a race condition which allowed the editor inside
    the dialog to be focused via a call inside the constructor of
    BreakpointEditDialog. However, when migrating @UI.TextEditorFactory
    extensions we moved to synchronously instancing
    CodeMirrorTextEditorFactory, which affected the race condition and
    resulted in the editor not being focused automatically when the dialog
    is shown.
    
    To make sure we focus the editor we add a call to do so after the
    dialog is shown.
    
    Also, some type annotations are refactored to be more explicit as a
    allowed by the migration.
    
    Fixed: 1187845
    Change-Id: Ibe3331c537cd470c48be7fc308639fc77bd39319

--- front_end/sources/DebuggerPlugin.js
+++ front_end/sources/DebuggerPlugin.js
@@ -939,6 +939,7 @@ export class DebuggerPlugin extends Plugin {
     this._textEditor.addDecoration(decorationElement, editorLineNumber);
     dialog.markAsExternallyManaged();
     dialog.show(decorationElement);
+    dialog.focusEditor();
   }
 
   /**
