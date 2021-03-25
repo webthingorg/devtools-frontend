7b99d3c1f16f9fa59c7b0348dee2a104d343d8f9
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-25 15:09:14 +0000

    Load lighthouse_test_runner with loadTestModule
    R=aerotwist@chromium.org
    
    Bug: 1187573
    Change-Id: Ib1c1f50649926dfce1bf401b6dc9f3ee235bf26c

--- test/webtests/http/tests/devtools/a11y-axe-core/lighthouse-start-view-a11y-test.js
+++ test/webtests/http/tests/devtools/a11y-axe-core/lighthouse-start-view-a11y-test.js
@@ -5,7 +5,7 @@
 (async function() {
   TestRunner.addResult('Tests accessibility in the lighthouse start view using the axe-core linter.\n');
   await TestRunner.loadModule('axe_core_test_runner');
-  await TestRunner.loadModule('lighthouse_test_runner');
+  await TestRunner.loadTestModule('lighthouse_test_runner');
   await TestRunner.showPanel('lighthouse');
   await AxeCoreTestRunner.runValidation(LighthouseTestRunner.getContainerElement());
   TestRunner.completeTest();
