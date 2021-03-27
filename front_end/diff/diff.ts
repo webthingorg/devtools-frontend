340589240acbdd4f8af52dfb1fbc66dd5bbb4663
Jan Scheffler <janscheffler@chromium.org>
2021-03-27 11:46:33 +0100

    [JSDOC2TS]: Migrate diff
    Bug: chromium:1158760
    Change-Id: Ie564c35149ccd9ffdb10d9b1e89232b8c6d1f7c8

--- front_end/diff/diff.ts
+++ front_end/diff/diff.ts
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './DiffWrapper.js';
-import './diff_match_patch.js';
+import './DiffWrapper.ts';
+import './diff_match_patch.ts';
 
-import * as Diff from './DiffWrapper.js';
+import * as Diff from './DiffWrapper';
 
 export {
   Diff,
