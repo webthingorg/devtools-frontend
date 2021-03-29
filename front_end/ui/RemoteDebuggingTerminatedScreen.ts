34627a3b39111fdb5a3321eab5947e20d4c7571e
Jan Scheffler <janscheffler@chromium.org>
2021-03-29 17:15:55 +0200

    [JSDOC2TS]: Migrate RemoteDebuggingTerminatedScreen in ui
    Bug: chromium:1158760
    Change-Id: I2f0c2ff55138d603e059afa1a5e5989806b7066f

--- front_end/ui/RemoteDebuggingTerminatedScreen.ts
+++ front_end/ui/RemoteDebuggingTerminatedScreen.ts
@@ -1,12 +1,14 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+/* eslint-disable rulesdir/no_underscored_properties */
+
 import * as i18n from '../i18n/i18n.js';
 
-import {Dialog} from './Dialog.js';
-import {SizeBehavior} from './GlassPane.js';
-import {createTextButton, formatLocalized} from './UIUtils.js';
-import {VBox} from './Widget.js';
+import { Dialog } from './Dialog.js';
+import { SizeBehavior } from './GlassPane.js';
+import { createTextButton, formatLocalized } from './UIUtils.js';
+import { VBox } from './Widget.js';
 
 const UIStrings = {
   /**
@@ -18,15 +20,12 @@ const UIStrings = {
   */
   reconnectDevtools: 'Reconnect DevTools'
 };
-const str_ = i18n.i18n.registerUIStrings('ui/RemoteDebuggingTerminatedScreen.js', UIStrings);
+const str_ = i18n.i18n.registerUIStrings('ui/RemoteDebuggingTerminatedScreen.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 export class RemoteDebuggingTerminatedScreen extends VBox {
-  /**
-   * @param {string} reason
-   */
-  constructor(reason) {
+  constructor(reason: string) {
     super(true);
-    this.registerRequiredCSS('ui/remoteDebuggingTerminatedScreen.css', {enableLegacyPatching: true});
+    this.registerRequiredCSS('ui/remoteDebuggingTerminatedScreen.css', { enableLegacyPatching: true });
     const message = this.contentElement.createChild('div', 'message');
     const reasonElement = message.createChild('span', 'reason');
     reasonElement.textContent = reason;
@@ -36,10 +35,7 @@ export class RemoteDebuggingTerminatedScreen extends VBox {
     this.contentElement.createChild('div', 'button').appendChild(button);
   }
 
-  /**
-   * @param {string} reason
-   */
-  static show(reason) {
+  static show(reason: string): void {
     const dialog = new Dialog();
     dialog.setSizeBehavior(SizeBehavior.MeasureContent);
     dialog.addCloseButton();
