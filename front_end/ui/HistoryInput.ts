8118d1e4a43336ed55291957ab6f4869d8fe31f6
Jan Scheffler <janscheffler@chromium.org>
2021-03-29 10:50:44 +0200

    [JSDOC2TS]: Migrate HistoryInput in ui
    Bug: chromium:1158760
    Change-Id: I5a73855a4731affdf5cc0cdc9fb15170bc78da22

--- front_end/ui/HistoryInput.ts
+++ front_end/ui/HistoryInput.ts
@@ -2,13 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {Keys} from './KeyboardShortcut.js';
-import {registerCustomElement} from './utils/register-custom-element.js';
+/* eslint-disable rulesdir/no_underscored_properties */
 
-/** @type {?function():!Element} */
-let _constructor = null;
+import { Keys } from './KeyboardShortcut.js';
+import { registerCustomElement } from './utils/register-custom-element.js';
+
+let _constructor: (() => Element) | null = null;
 
 export class HistoryInput extends HTMLInputElement {
+  _history: string[];
+  _historyPosition: number;
+  value?: string;
   constructor() {
     super();
     this._history = [''];
@@ -16,47 +20,40 @@ export class HistoryInput extends HTMLInputElement {
     this.addEventListener('keydown', this._onKeyDown.bind(this), false);
     this.addEventListener('input', this._onInput.bind(this), false);
   }
-  /**
-   * @return {!HistoryInput}
-   */
-  static create() {
+  static create(): HistoryInput {
     if (!_constructor) {
       _constructor = registerCustomElement('input', 'history-input', HistoryInput);
     }
 
-    return /** @type {!HistoryInput} */ (_constructor());
+    return /** @type {!HistoryInput} */ _constructor() as HistoryInput;
   }
 
-  /**
-   * @param {!Event} event
-   */
-  _onInput(event) {
+  _onInput(event: Event): void {
     if (this._history.length === this._historyPosition + 1) {
       this._history[this._history.length - 1] = this.value;
     }
   }
 
-  /**
-   * @param {!Event} ev
-   */
-  _onKeyDown(ev) {
-    const event = /** @type {!KeyboardEvent} */ (ev);
+  _onKeyDown(ev: Event): void {
+    const event = (ev as KeyboardEvent);
     if (event.keyCode === Keys.Up.code) {
       this._historyPosition = Math.max(this._historyPosition - 1, 0);
       this.value = this._history[this._historyPosition];
-      this.dispatchEvent(new Event('input', {'bubbles': true, 'cancelable': true}));
+      this.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
       event.consume(true);
-    } else if (event.keyCode === Keys.Down.code) {
+    }
+    else if (event.keyCode === Keys.Down.code) {
       this._historyPosition = Math.min(this._historyPosition + 1, this._history.length - 1);
       this.value = this._history[this._historyPosition];
-      this.dispatchEvent(new Event('input', {'bubbles': true, 'cancelable': true}));
+      this.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
       event.consume(true);
-    } else if (event.keyCode === Keys.Enter.code) {
+    }
+    else if (event.keyCode === Keys.Enter.code) {
       this._saveToHistory();
     }
   }
 
-  _saveToHistory() {
+  _saveToHistory(): void {
     if (this._history.length > 1 && this._history[this._history.length - 2] === this.value) {
       return;
     }
