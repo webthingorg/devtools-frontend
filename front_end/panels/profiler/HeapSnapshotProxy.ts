f15595caa2ca3b15e24826bc3a6adfe0571fdf11
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-31 11:40:49 +0100

    Move profiler to panels/profiler
    R=aerotwist@chromium.org
    
    Bug: 1187573
    Change-Id: Ifc969f6d37a90cd90eccb0d8cd07131575a0a300

--- front_end/panels/profiler/HeapSnapshotProxy.ts
+++ front_end/panels/profiler/HeapSnapshotProxy.ts
@@ -30,9 +30,9 @@
 
 /* eslint-disable rulesdir/no_underscored_properties */
 
-import * as Common from '../common/common.js';
-import type * as HeapSnapshotModel from '../heap_snapshot_model/heap_snapshot_model.js';
-import * as i18n from '../i18n/i18n.js';
+import * as Common from '../../common/common.js';
+import type * as HeapSnapshotModel from '../../heap_snapshot_model/heap_snapshot_model.js';
+import * as i18n from '../../i18n/i18n.js';
 import type {ChildrenProvider} from './ChildrenProvider.js';
 
 const UIStrings = {
@@ -42,7 +42,7 @@ const UIStrings = {
   */
   anErrorOccurredWhenACallToMethod: 'An error occurred when a call to method \'{PH1}\' was requested',
 };
-const str_ = i18n.i18n.registerUIStrings('profiler/HeapSnapshotProxy.ts', UIStrings);
+const str_ = i18n.i18n.registerUIStrings('panels/profiler/HeapSnapshotProxy.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 export class HeapSnapshotWorkerProxy extends Common.ObjectWrapper.ObjectWrapper {
   // TODO(crbug.com/1172300) Ignored during the jsdoc to ts migration)
