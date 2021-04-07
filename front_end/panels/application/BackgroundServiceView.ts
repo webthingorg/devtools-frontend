52e5428b8e2d37fe135669a4240be1b8d1a6c716
Tim van der Lippe <tvanderlippe@chromium.org>
2021-04-07 15:02:08 +0100

    Move ui to ui/legacy
    R=jacktfranklin@chromium.org
    
    Bug: 1187573
    Change-Id: Ifc536ba79b0a324aa38e76aa294c73cd699c6536

--- front_end/panels/application/BackgroundServiceView.ts
+++ front_end/panels/application/BackgroundServiceView.ts
@@ -10,7 +10,7 @@ import * as Platform from '../../core/platform/platform.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import * as DataGrid from '../../data_grid/data_grid.js';
 import * as Bindings from '../../models/bindings/bindings.js';
-import * as UI from '../../ui/ui.js';
+import * as UI from '../../ui/legacy/legacy.js';
 
 import {BackgroundServiceModel, Events} from './BackgroundServiceModel.js';  // eslint-disable-line no-unused-vars
 
