a4a4215cbb7b64790d028e7fbd582607736d9a19
Alex Rudenko <alexrudenko@chromium.org>
2021-04-20 09:19:23 +0000

    Use in-page recording
    Backend CL: https://crrev.com/c/2835786
    V8 CL: https://chromium-review.googlesource.com/c/v8/v8/+/2835712
    
    Bug: none
    Change-Id: Icef824fe08034f90f3cb667944102595040cb946

--- front_end/core/sdk/RuntimeModel.ts
+++ front_end/core/sdk/RuntimeModel.ts
@@ -298,6 +298,10 @@ export class RuntimeModel extends SDKModel {
     object.release();
   }
 
+  _bindingCalled(event: Protocol.Runtime.BindingCalledEvent): void {
+    this.dispatchEventToListeners(Events.BindingCalled, event);
+  }
+
   _copyRequested(object: RemoteObject): void {
     if (!object.objectId) {
       Host.InspectorFrontendHost.InspectorFrontendHostInstance.copyText(
@@ -443,6 +447,7 @@ const _sideEffectTestExpression: string = '(async function(){ await 1; })()';
 // TODO(crbug.com/1167717): Make this a const enum again
 // eslint-disable-next-line rulesdir/const_enum
 export enum Events {
+  BindingCalled = 'BindingCalled',
   ExecutionContextCreated = 'ExecutionContextCreated',
   ExecutionContextDestroyed = 'ExecutionContextDestroyed',
   ExecutionContextChanged = 'ExecutionContextChanged',
@@ -489,7 +494,8 @@ class RuntimeDispatcher implements ProtocolProxyApi.RuntimeDispatcher {
     this._runtimeModel._inspectRequested(object, hints);
   }
 
-  bindingCalled(_event: Protocol.Runtime.BindingCalledEvent): void {
+  bindingCalled(event: Protocol.Runtime.BindingCalledEvent): void {
+    this._runtimeModel._bindingCalled(event);
   }
 }
 
@@ -705,6 +711,7 @@ export interface EvaluationOptions {
   disableBreaks?: boolean;
   replMode?: boolean;
   allowUnsafeEvalBlockedByCSP?: boolean;
+  contextId?: number;
 }
 
 export type QueryObjectResult = {
