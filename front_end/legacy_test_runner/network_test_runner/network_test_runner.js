2a8f686e091324d4b0e6a10b07322edaf043d1d5
Sigurd Schneider <sigurds@chromium.org>
2021-04-20 13:17:49 +0200

    Export test helpers to loosen coupling with web tests
    Bug: chromium:1199668
    Change-Id: I249fed97ce6de0ccc118d56be6a90a5dc0e07251

--- front_end/legacy_test_runner/network_test_runner/network_test_runner.js
+++ front_end/legacy_test_runner/network_test_runner/network_test_runner.js
@@ -200,6 +200,9 @@ NetworkTestRunner.HARPropertyFormatters = {
 NetworkTestRunner.HARPropertyFormattersWithSize = JSON.parse(JSON.stringify(NetworkTestRunner.HARPropertyFormatters));
 NetworkTestRunner.HARPropertyFormattersWithSize.size = 'formatAsTypeName';
 
+NetworkTestRunner.buildHARLog = SDK.HARLog.build;
+NetworkTestRunner.buildHARLogEntry = SDK.HARLog.Entry.build;
+
 TestRunner.deprecatedInitAsync(`
   let lastXHRIndex = 0;
 
