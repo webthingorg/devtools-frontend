17bcba9d7eadc1b7acf83a20edb22f6a86a8b412
Tim van der Lippe <tvanderlippe@chromium.org>
2021-04-21 12:26:48 +0100

    Move components to ui/legacy/components/utils
    R=aerotwist@chromium.org
    
    Bug: 1187573
    Change-Id: Ib55515e9a436e21c4b1e7a014a95220b46021136

--- front_end/panels/timeline/TimelineUIUtils.ts
+++ front_end/panels/timeline/TimelineUIUtils.ts
@@ -35,7 +35,6 @@
 
 /* eslint-disable rulesdir/no_underscored_properties */
 
-import * as Components from '../../components/components.js';
 import * as Common from '../../core/common/common.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as Platform from '../../core/platform/platform.js';
@@ -43,6 +42,7 @@ import * as SDK from '../../core/sdk/sdk.js';
 import * as Bindings from '../../models/bindings/bindings.js';
 import * as TimelineModel from '../../models/timeline_model/timeline_model.js';
 import * as PerfUI from '../../ui/legacy/components/perf_ui/perf_ui.js';
+import * as Components from '../../ui/legacy/components/utils/utils.js';
 import * as UI from '../../ui/legacy/legacy.js';
 
 import {CLSRect} from './CLSLinkifier.js';
