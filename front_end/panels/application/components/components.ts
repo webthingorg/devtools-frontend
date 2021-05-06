a48239c83362d8dd0a23fef22613746485ebc30c
Tim van der Lippe <tvanderlippe@chromium.org>
2021-05-06 11:02:15 +0100

    Move custom elements from application into application/components
    R=jacktfranklin@chromium.org
    
    Bug: 1206211
    Change-Id: I7d6f077adfd84eba80b96c2babc1c80a8a6dddec

---
+++ front_end/panels/application/components/components.ts
@@ -0,0 +1,13 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ApplicationComponents from './FrameDetailsView.js';
+import * as StackTrace from './StackTrace.js';
+import * as TrustTokensView from './TrustTokensView.js';
+
+export {
+  ApplicationComponents as FrameDetailsView,
+  StackTrace,
+  TrustTokensView,
+};
