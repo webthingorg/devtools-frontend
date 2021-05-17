87b07946b97da69c102cb1ce519bc2c49938986c
Sigurd Schneider <sigurds@chromium.org>
2021-05-17 14:29:55 +0200

    Try to re-enable flaky tests
    Bug: chromium:1184162
    Change-Id: I970cee8ea563a47f067f669faabcef3e41aa43ac

--- test/e2e/sources/icon-row-bucket_test.ts
+++ test/e2e/sources/icon-row-bucket_test.ts
@@ -125,8 +125,7 @@ describe('The row\'s icon bucket', async function() {
     assert.strictEqual(icons.length, 1);
   });
 
-  // Flaky test.
-  it.skipOnPlatforms(['mac'], '[crbug.com/1184162]: should reveal Issues tab when the icon is clicked', async () => {
+  it('should reveal Issues tab when the icon is clicked', async () => {
     await openFileInSourceTab('trusted-type-policy-violation-report-only.rawresponse');
 
     const HIDE_DEBUGGER_SELECTOR = '[aria-label="Hide debugger"]';
@@ -143,29 +142,27 @@ describe('The row\'s icon bucket', async function() {
     assert.isTrue(expandedIssues.has('Trusted Type policy creation blocked by Content Security Policy'));
   });
 
-  // Flaky test.
-  it.skipOnPlatforms(
-      ['mac'], '[crbug.com/1184162]: should reveal the Issues tab if the icon in the popover is clicked', async () => {
-        await navigateToIssuesTab();
-        await openFileInSourceTab('trusted-type-violations-report-only.rawresponse');
+  it('should reveal the Issues tab if the icon in the popover is clicked', async () => {
+    await navigateToIssuesTab();
+    await openFileInSourceTab('trusted-type-violations-report-only.rawresponse');
 
-        const HIDE_DEBUGGER_SELECTOR = '[aria-label="Hide debugger"]';
-        const HIDE_NAVIGATOR_SELECTOR = '[aria-label="Hide navigator"]';
-        await click(HIDE_DEBUGGER_SELECTOR);
-        await click(HIDE_NAVIGATOR_SELECTOR);
+    const HIDE_DEBUGGER_SELECTOR = '[aria-label="Hide debugger"]';
+    const HIDE_NAVIGATOR_SELECTOR = '[aria-label="Hide navigator"]';
+    await click(HIDE_DEBUGGER_SELECTOR);
+    await click(HIDE_NAVIGATOR_SELECTOR);
 
-        const bucketIssueIconComponents = await getIconComponents('text-editor-line-decoration-icon-issue');
-        assert.strictEqual(bucketIssueIconComponents.length, 1);
-        const issueIconComponent = bucketIssueIconComponents[0];
-        await issueIconComponent.hover();
+    const bucketIssueIconComponents = await getIconComponents('text-editor-line-decoration-icon-issue');
+    assert.strictEqual(bucketIssueIconComponents.length, 1);
+    const issueIconComponent = bucketIssueIconComponents[0];
+    await issueIconComponent.hover();
 
-        const vbox = await waitFor('div.vbox.flex-auto.no-pointer-events');
-        const rowMessage = await waitFor('.text-editor-row-message', vbox);
-        const issueTitle = await rowMessage.evaluate(x => (x instanceof HTMLElement) ? x.innerText : '');
-        const issueIcon = await waitFor('.text-editor-row-message-icon', rowMessage);
-        await issueIcon.click();
+    const vbox = await waitFor('div.vbox.flex-auto.no-pointer-events');
+    const rowMessage = await waitFor('.text-editor-row-message', vbox);
+    const issueTitle = await rowMessage.evaluate(x => (x instanceof HTMLElement) ? x.innerText : '');
+    const issueIcon = await waitFor('.text-editor-row-message-icon', rowMessage);
+    await issueIcon.click();
 
-        const expandedIssues = await waitForExpandedIssueTitle(issueIconComponent);
-        assert.isTrue(expandedIssues.has(issueTitle));
-      });
+    const expandedIssues = await waitForExpandedIssueTitle(issueIconComponent);
+    assert.isTrue(expandedIssues.has(issueTitle));
+  });
 });
