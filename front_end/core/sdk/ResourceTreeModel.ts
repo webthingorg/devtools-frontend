c6c650e1d40f40b5ede89fcc4b87298ac04f9a35
Charlie Hu <chenleihu@google.com>
2021-05-20 18:10:36 -0400

    [OriginTrial] Implement Devtools Front-End
    This CL adds the Front-End UI for origin trial devtools support to
    FrameDetailsView.
    
    Design Doc:
    https://docs.google.com/document/d/1Evd32hgpYQuPsoPw6UPmVvz1PrWrh2PmBlHGmfItmNo/edit?usp=sharing
    
    Bug: 607555
    Change-Id: Idcded6bb1b9885538376a6a07b547a13cccb65af

--- front_end/core/sdk/ResourceTreeModel.ts
+++ front_end/core/sdk/ResourceTreeModel.ts
@@ -574,6 +574,7 @@ export class ResourceTreeFrame {
   _secureContextType: Protocol.Page.SecureContextType|null;
   _crossOriginIsolatedContextType: Protocol.Page.CrossOriginIsolatedContextType|null;
   _gatedAPIFeatures: Protocol.Page.GatedAPIFeatures[]|null;
+  _originTrials: Protocol.Page.OriginTrial[]|null;
   private creationStackTrace: Protocol.Runtime.StackTrace|null;
   private creationStackTraceTarget: Target|null;
   _childFrames: Set<ResourceTreeFrame>;
@@ -599,6 +600,7 @@ export class ResourceTreeFrame {
     this._secureContextType = payload && payload.secureContextType;
     this._crossOriginIsolatedContextType = payload && payload.crossOriginIsolatedContextType;
     this._gatedAPIFeatures = payload && payload.gatedAPIFeatures;
+    this._originTrials = payload && (payload.originTrials || null);
 
     this.creationStackTrace = creationStackTrace;
     this.creationStackTraceTarget = null;
@@ -632,6 +634,10 @@ export class ResourceTreeFrame {
     return this._gatedAPIFeatures;
   }
 
+  getOriginTrials(): Protocol.Page.OriginTrial[]|null {
+    return this._originTrials;
+  }
+
   getCreationStackTraceData():
       {creationStackTrace: Protocol.Runtime.StackTrace|null, creationStackTraceTarget: Target} {
     return {
@@ -652,6 +658,7 @@ export class ResourceTreeFrame {
     this._secureContextType = framePayload.secureContextType;
     this._crossOriginIsolatedContextType = framePayload.crossOriginIsolatedContextType;
     this._gatedAPIFeatures = framePayload.gatedAPIFeatures;
+    this._originTrials = framePayload.originTrials || null;
 
     const mainResource = this._resourcesMap.get(this._url);
     this._resourcesMap.clear();
