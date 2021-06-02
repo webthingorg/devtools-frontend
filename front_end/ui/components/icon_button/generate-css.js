9d7419899993a7b84dd548ad2a5e9e6afb2d97c6
Kriti Sapra <kritisapra@google.com>
2021-06-02 13:33:30 +0000

    Updated formatting
    made node_actions for generating CSSStyleSheets from css files
    
    Change-Id: I789f7dcb5ac3c1143bfc43a6bef161efbcba31e0

---
+++ front_end/ui/components/icon_button/generate-css.js
@@ -0,0 +1,19 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const fs = require('fs');
+const path = require('path');
+const [, , srcDir, targetGenDir, fileName] = process.argv;
+
+const output = fs.readFileSync(path.join(srcDir, fileName), {encoding: 'utf8', flag: 'r'});
+
+fs.writeFileSync(
+    path.join(targetGenDir, fileName + '.js'),
+    `// Copyright ${new Date().getFullYear()} The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const styles = new CSSStyleSheet(); 
+styles.replaceSync(
+\`${output}\`);`);
