9c0161e7e8eac4382f87cba277557a5e3bcf2326
Kriti Sapra <kritisapra@google.com>
2021-06-08 08:23:20 +0000

    Prototyping using adoptedStyleSheets to append styles to DOM and using the ninja build system to import the stylesheets.
    Created a CSS stylesheet for IconButton.ts to import instead of
    using inline styles in web components.
    
    Bug: 1106746
    Change-Id: I789f7dcb5ac3c1143bfc43a6bef161efbcba31e0

---
+++ front_end/ui/components/icon_button/generate-css.js
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const fs = require('fs');
+const path = require('path');
+const [, , srcDir, targetGenDir, fileName] = process.argv;
+
+const output = fs.readFileSync(path.join(srcDir, fileName), {encoding: 'utf8', flag: 'r'});
+
+fs.writeFileSync(
+    path.join(targetGenDir, fileName + '.d.ts'),
+    `// Copyright ${new Date().getFullYear()} The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const styles: CSSStyleSheet;
+export default styles;
+`);
+
+fs.writeFileSync(path.join(targetGenDir, 'generate_css-tsconfig.json'), `{
+    "compilerOptions": {
+        "composite": true,
+        "outDir": "."
+    },
+    "files": [
+        "${fileName + '.js'}",
+        "${fileName + '.d.ts'}"
+    ]
+}
+`);
+
+fs.writeFileSync(
+    path.join(targetGenDir, fileName + '.js'),
+    `// Copyright ${new Date().getFullYear()} The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const styles = new CSSStyleSheet();
+styles.replaceSync(
+\`${output}
+/*# sourceURL=${fileName} */
+\`);
+export default styles;
+`);
