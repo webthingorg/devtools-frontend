f5d59bcd69835d1470b85f915fdd55246d000a4e
Danil Somsikov <dsv@chromium.org>
2021-06-11 12:50:19 +0200

    Implement minimal web bundle preview
    https://imgur.com/a/Mu6edRS
    
    Bug: 1182537
    Change-Id: I25e791c7b5d1284581e57a82ead066dd5f739b2b

---
+++ front_end/panels/network/components/WebBundleInfoView.ts
@@ -0,0 +1,113 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/* eslint-disable rulesdir/no_underscored_properties */
+
+import * as i18n from '../../core/i18n/i18n.js';
+import type * as SDK from '../../core/sdk/sdk.js'; // eslint-disable-line no-unused-vars
+import * as UI from '../../ui/legacy/legacy.js';
+import * as LitHtml from '../../ui/lit-html/lit-html.js';
+import * as ComponentHelpers from '../../ui/components/helpers/helpers.js';
+import * as IconButton from '../../ui/components/icon_button/icon_button.js';
+import * as DataGrid from '../../ui/components/data_grid/data_grid.js';
+
+const {render, html} = LitHtml;
+
+const UIStrings = {
+  /**
+  *@description Header for the column that contains URL of the resource in a web bundle.
+  */
+  bundledResource: 'Bundled resource',
+};
+const str_ = i18n.i18n.registerUIStrings('panels/network/components/WebBundleInfoView.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+export class WebBundleInfoView extends UI.Widget.VBox {
+  constructor(request: SDK.NetworkRequest.NetworkRequest) {
+    super();
+    const webBundleInfo = request.webBundleInfo();
+    if (!webBundleInfo) {
+      throw new Error('Trying to render a Web Bundle info without providing data');
+    }
+
+    const webBundleInfoElement = new WebBundleInfoElement(webBundleInfo, request.parsedURL.lastPathComponent);
+    this.contentElement.appendChild(webBundleInfoElement);
+    webBundleInfoElement.render();
+  }
+}
+
+export class WebBundleInfoElement extends HTMLElement {
+  private readonly shadow = this.attachShadow({mode: 'open'});
+  private webBundleInfo: Readonly<SDK.NetworkRequest.WebBundleInfo>;
+  private webBundleName: Readonly<string>;
+  constructor(webBundleInfo: SDK.NetworkRequest.WebBundleInfo, webBundleName: string) {
+    super();
+    this.webBundleInfo = webBundleInfo;
+    this.webBundleName = webBundleName;
+  }
+
+  render(): void {
+    const rows = this.webBundleInfo.resourceUrls?.map(url => ({
+                                                        cells: [
+                                                          {
+                                                            columnId: 'url',
+                                                            value: url,
+                                                          },
+                                                        ],
+                                                      }));
+    render(
+        html`
+      <style>
+        .header {
+          font-weight: bold;
+          padding: 8px 4px;
+          display: flex;
+        }
+
+        .icon {
+          margin: 0 4px;
+        }
+
+      </style>
+      <div class="header">
+        <${IconButton.Icon.Icon.litTagName} class="icon"
+          .data=${{color: '', iconName: 'resourceWebBundle', width: '16px'} as IconButton.Icon.IconData}>
+        </${IconButton.Icon.Icon.litTagName}>
+        <span>${this.webBundleName}</span>
+        <a href="https://github.com/WICG/webpackage/blob/main/explainers/subresource-loading.md" target = "_blank">
+          <${IconButton.Icon.Icon.litTagName} class="icon"
+            .data=${
+            {color: 'var(--color-text-secondary)', iconName: 'help_outline', width: '16px'} as
+            IconButton.Icon.IconData}>
+          </${IconButton.Icon.Icon.litTagName}>
+        </a>
+      </div>
+      <div>
+        <${DataGrid.DataGrid.DataGrid.litTagName}
+          .data=${{
+          columns: [
+            {
+              id: 'url',
+              title: i18nString(UIStrings.bundledResource),
+              widthWeighting: 1,
+              visible: true,
+              hideable: false,
+            },
+          ],
+          rows: rows,
+          activeSort: null,
+        } as DataGrid.DataGrid.DataGridData}>
+        </${DataGrid.DataGrid.DataGrid.litTagName}>
+      </div>`,
+        this.shadow);
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-web-bundle-info', WebBundleInfoElement);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'devtools-web-bundle-info': WebBundleInfoElement;
+  }
+}
