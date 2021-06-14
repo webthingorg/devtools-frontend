bf6578ed8abeebc1e51e0f696b378375c4f6d3d1
Kim-Anh Tran <kimanh@chromium.org>
2021-06-14 12:02:03 +0200

    [settings] Add setting to disable auto-focus on the sources panel
    This adds a setting to DevTools to disable auto-focusing on the
    sources panel if a Debugger.pause event is received.
    The context here is that extensions may be attached that do not
    want that behavior (e.g. if source debugging is happening in
    another panel that is provided by the extension).
    
    Bug: b/182581017
    Change-Id: I7a50c0c6eb7d5eb7abe45bcfafc40e394a4b8e17

---
+++ test/e2e/sources/can-disable-auto-sources-focus_test.ts
@@ -0,0 +1,56 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {getBrowserAndPages, getPendingEvents, installEventListener, step, waitFor, waitForFunction, waitForNone} from '../../shared/helper.js';
+import {navigateToElementsTab} from '../helpers/elements-helpers.js';
+import {togglePreferenceInSettingsTab} from '../helpers/settings-helpers.js';
+import {addBreakpointForLine, DEBUGGER_PAUSED_EVENT, openSourceCodeEditorForFile} from '../helpers/sources-helpers.js';
+
+async function breakAndCheckFocusedPanel(expectedPanel: string) {
+  const {frontend, target} = getBrowserAndPages();
+
+  await step('navigate to a page and open the Sources tab', async () => {
+    await openSourceCodeEditorForFile('click-breakpoint.js', 'click-breakpoint.html');
+  });
+
+  await step('add a breakpoint to line No.4', async () => {
+    await addBreakpointForLine(frontend, 4);
+  });
+
+  await step('navigate to the elements tab', async () => {
+    await navigateToElementsTab();
+  });
+
+  await step('trigger a Debugger.pause event', async () => {
+    target.evaluate('f2();');
+  });
+
+  await step('wait for debugger paused event', async () => {
+    await waitForFunction(() => getPendingEvents(frontend, DEBUGGER_PAUSED_EVENT));
+  });
+
+  await step(`check that we are in the ${expectedPanel} tab`, async () => {
+    await waitFor(`.panel[aria-label="${expectedPanel}"]`);
+  });
+}
+
+describe('Sources Panel', async () => {
+  beforeEach(async () => {
+    const {frontend} = getBrowserAndPages();
+    installEventListener(frontend, DEBUGGER_PAUSED_EVENT);
+  });
+
+  it('is not openend on Debugger.pause if autoFocusOnDebuggerPauseEnabled is false', async () => {
+    await step('toggle preference in settings tab', async () => {
+      await togglePreferenceInSettingsTab('Focus Sources panel when triggering a breakpoint');
+    });
+
+    await breakAndCheckFocusedPanel('elements');
+    await waitForNone('.panel[aria-label="sources"]');
+  });
+
+  it('is opened on Debugger.pause if autoFocusOnDebuggerPauseEnabled is true (default)', async () => {
+    await breakAndCheckFocusedPanel('sources');
+  });
+});
