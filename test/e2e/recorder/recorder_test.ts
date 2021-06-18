836dcf848561b1896e78ebf0f53faa2bd301a251
Alex Rudenko <alexrudenko@chromium.org>
2021-06-18 09:27:06 +0000

    Enable flaky recorder test
    There seems to be some inherent flakiness related to the sources UI.
    This CL fixes the flakiness by retrying to stop the recording multiple
    times. It's acceptable for now to keep the coverage because soon
    recorder will have a dedicated UI.
    
    Bug: 1219505
    Change-Id: Ib4a5a8f1329d466b78859bd7052dfb15b9d188bb

--- test/e2e/recorder/recorder_test.ts
+++ test/e2e/recorder/recorder_test.ts
@@ -7,7 +7,7 @@ import {assert} from 'chai';
 import type {UserFlow, Selector} from '../../../front_end/models/recorder/Steps.js';
 
 import {enableExperiment, getBrowserAndPages, getTestServerPort, goToResource} from '../../shared/helper.js';
-import {describe, it} from '../../shared/mocha-extensions.js';
+import {describe, it, takeScreenshots} from '../../shared/mocha-extensions.js';
 import {createNewRecording, openRecorderSubPane, openSourcesPanel} from '../helpers/sources-helpers.js';
 
 function retrieveCodeMirrorEditorContent() {
@@ -66,11 +66,18 @@ async function startRecording(path: string, networkCondition: string = '', untru
   await frontend.waitForSelector('aria/Stop');
 }
 
-async function stopRecording() {
-  const {frontend} = getBrowserAndPages();
-  await frontend.bringToFront();
-  await frontend.waitForSelector('aria/Stop', {timeout: 0});
-  await frontend.click('aria/Stop');
+async function stopRecording(attempt = 0, error = new Error()) {
+  if (attempt >= 3) {
+    throw error;
+  }
+  try {
+    const {frontend} = getBrowserAndPages();
+    await frontend.bringToFront();
+    const el = await frontend.waitForSelector('aria/Stop', {timeout: 0});
+    await frontend.click('aria/Stop');
+  } catch (err) {
+    stopRecording(attempt + 1, err);
+  }
 }
 
 async function assertOutput(expected: UserFlow) {
@@ -556,8 +563,7 @@ describe('Recorder', function() {
     });
   });
 
-  // Flakes occasionally on Mac.
-  it.skipOnPlatforms(['mac'], '[crbug.com/1219505] should record scroll events', async () => {
+  it.only('should record scroll events', async () => {
     const untrustedEvents = true;
     const networkCondition = '';
     await startRecording('recorder/scroll.html', networkCondition, untrustedEvents);
