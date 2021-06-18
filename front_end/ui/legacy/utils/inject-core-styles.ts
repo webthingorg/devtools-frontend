288e4e64a8c1b3ef60c1aba5126f75debcebb692
Kriti Sapra <kritisapra@google.com>
2021-06-18 09:41:25 +0000

    Exported themeColors.css to be imported using css.js file
    Bug: 1106746
    Change-Id: I7582edfa88acc9d4afc6d9e9de15f2580298ead3

--- front_end/ui/legacy/utils/inject-core-styles.ts
+++ front_end/ui/legacy/utils/inject-core-styles.ts
@@ -2,15 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Platform from '../../../core/platform/platform.js';
+import InspectorCommonStyles from '../inspectorCommon.css.js';
+import InspectorScrollbarsStyles from '../inspectorScrollbars.css.js';
+import TextButtonStyles from '../textButton.css.js';
 import * as ThemeSupport from '../theme_support/theme_support.js';
-
-import {appendStyle} from './append-style.js';
+import ThemeColors from '../themeColors.css.js';
 
 export function injectCoreStyles(root: Element|ShadowRoot): void {
-  appendStyle(root, 'ui/legacy/inspectorCommon.css', {enableLegacyPatching: false});
-  appendStyle(root, 'ui/legacy/inspectorScrollbars.css', {enableLegacyPatching: false});
-  appendStyle(root, 'ui/legacy/themeColors.css', {enableLegacyPatching: false});
-  appendStyle(root, 'ui/legacy/textButton.css', {enableLegacyPatching: false});
+  const potentialRoot = root.getRootNode();
+  Platform.DCHECK(
+      () => potentialRoot instanceof Document || potentialRoot instanceof ShadowRoot,
+      `Expected root of widget to be a document or shadowRoot, but was "${potentialRoot.nodeName}"`);
+
+  const finalRoot = potentialRoot as ShadowRoot | Document;
+  finalRoot.adoptedStyleSheets = finalRoot.adoptedStyleSheets.concat(
+      [InspectorCommonStyles, InspectorScrollbarsStyles, ThemeColors, TextButtonStyles]);
+
   ThemeSupport.ThemeSupport.instance().injectHighlightStyleSheets(root);
   ThemeSupport.ThemeSupport.instance().injectCustomStyleSheets(root);
 }
