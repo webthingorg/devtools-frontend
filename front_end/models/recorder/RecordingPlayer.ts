e5dd74287fd70e1dbb3e7eceafb10aa5723bdc9d
Alex Rudenko <alexrudenko@chromium.org>
2021-06-21 12:32:50 +0000

    [Recorder] Recorder and replay click event offsets
    This CL adds offsetX/offsetY to click events allow to be more
    precises about where an element is clicked. This solves a problem
    of clicking on complex inputs such as date inputs.
    
    As a drive-by I fixed the wrong viewport measurements being captured.
    Happy to extract to a separate CL if the clicking part will be
    controversial.
    
    Bug: chromium:1221043
    Change-Id: I9e18a3e77ddb99f93712f1065da1a0fe3a1cbfa7

--- front_end/models/recorder/RecordingPlayer.ts
+++ front_end/models/recorder/RecordingPlayer.ts
@@ -110,7 +110,15 @@ export class RecordingPlayer extends Common.ObjectWrapper.ObjectWrapper {
         if (!element) {
           throw new Error('Could not find element: ' + step.selector);
         }
-        await element.click();
+        const {offsetLeft, offsetTop} = await element.evaluate(el => {
+          const borderTop = parseFloat(getComputedStyle(el).getPropertyValue('border-top-width'));
+          const borderLeft = parseFloat(getComputedStyle(el).getPropertyValue('border-left-width'));
+          return {
+            offsetTop: (el as HTMLElement).offsetTop + borderTop,
+            offsetLeft: (el as HTMLElement).offsetLeft + borderLeft,
+          };
+        });
+        await page.mouse.click(offsetLeft + step.offsetX, offsetTop + step.offsetY);
       } break;
       case 'emulateNetworkConditions': {
         await page.emulateNetworkConditions(step.conditions);
