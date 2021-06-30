390d2b8abf34815450ccb930ffd8c43b9cf70238
Jesus David Garcia Gomez <jegarc@microsoft.com>
2021-06-30 13:24:16 -0700

    Fix accessible mensage in Error/Issues counters
    This CL adds an aria label to the button element inside an IconButton
    component to fix an a11y issue where the screen reader doesn't read
    the label propertly.
    
    Bug:1223208
    Change-Id: Ibcc8fd7ca3e2313d90fb68139089aeb665353e7b

--- front_end/ui/components/issue_counter/IssueCounter.ts
+++ front_end/ui/components/issue_counter/IssueCounter.ts
@@ -200,6 +200,7 @@ export class IssueCounter extends HTMLElement {
       ],
       clickHandler: this.clickHandler,
       leadingText: this.leadingText,
+      accessibleName: this.accessibleName,
     };
     LitHtml.render(
         // eslint-disable-next-line rulesdir/ban_style_tags_in_lit_html
@@ -210,8 +211,7 @@ export class IssueCounter extends HTMLElement {
               display: inline-block;
             }
         </style>
-        <icon-button .data=${data as IconButton.IconButton.IconButtonData}
-          aria-label="${LitHtml.Directives.ifDefined(this.accessibleName)}"></icon-button>
+        <icon-button .data=${data as IconButton.IconButton.IconButtonData}></icon-button>
         `,
         this.shadow);
     this.tooltipCallback?.();
