52d758f8474ce499c41608a4afd6d87be64db8f9
Kriti Sapra <kritisapra@google.com>
2021-07-23 13:59:21 +0000

    Added ESLint rule to migrate legacy calls to createShadowRootWithStyles
    Bug: 1106746
    Change-Id: Id39b79e3baea93411630f6526348f30081844e82

---
+++ scripts/eslint_rules/tests/migrate_create_shadow_root_with_styles_test.js
@@ -0,0 +1,163 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+const rule = require('../lib/migrate_create_shadow_root_with_styles.js');
+const ruleTester = new (require('eslint').RuleTester)({
+  parserOptions: {ecmaVersion: 9, sourceType: 'module'},
+  parser: require.resolve('@typescript-eslint/parser'),
+});
+
+const MIGRATION_ERROR_MESSAGE = 'Import CSS file instead of passing a string into createShadowRootWithStyles';
+const MANUALLY_MIGRATE_ERROR_ESSAGE = 'Please manually migrate this file manually.';
+
+ruleTester.run('check_migrate_RegisterRequiredCSS', rule, {
+  valid: [
+    {
+      code: `
+      import smallBubbleStyles from './smallBubbleStyles.css.js';
+      export class Component extends UI.Widget.Widget {
+        constructor(){
+          const shadow = createShadowRootWithCoreStyles(this, 
+            {cssFile: smallBubbleStyles, delegatesFocus: undefined});
+        }
+      }
+      `,
+      filename: 'front_end/components/test.ts',
+    },
+    {
+      code: `
+      import smallBubbleStyles from './smallBubbleStyles.css.js';
+      export class Component extends UI.Widget.Widget {
+        constructor(){
+          const shadow = UI.Utils.createShadowRootWithCoreStyles(this, 
+            {cssFile: smallBubbleStyles, delegatesFocus: undefined});
+        }
+      }
+      `,
+      filename: 'front_end/components/test.ts',
+    },
+    {
+      code: `export class Component extends UI.Widget.Widget {
+constructor(){
+  const shadow = createShadowRootWithCoreStyles(this, 
+    {delegatesFocus: undefined});
+  }
+}`,
+      filename: 'front_end/components/test.ts',
+    },
+    {
+      code: `export class Component extends UI.Widget.Widget {
+constructor(){
+  const shadow = UI.Utils.createShadowRootWithCoreStyles(this, 
+    {delegatesFocus: undefined});
+  }
+}`,
+      filename: 'front_end/components/test.ts',
+    },
+
+  ],
+  invalid: [
+    {
+      code: `export class Component extends UI.Widget.Widget {
+  constructor(){
+    const shadow = createShadowRootWithCoreStyles(this, 
+      {cssFile: 'ui/legacy/smallBubble.css', delegatesFocus: undefined});
+  }
+}`,
+      filename: 'front_end/components/test.ts',
+      errors: [{message: MIGRATION_ERROR_MESSAGE}],
+      output: `import smallBubbleStyles from './smallBubble.css.js';
+export class Component extends UI.Widget.Widget {
+  constructor(){
+    const shadow = createShadowRootWithCoreStyles(this, 
+      {cssFile: smallBubbleStyles, delegatesFocus: undefined});
+  }
+}`
+    },
+    {
+      code: `export class Component extends UI.Widget.Widget {
+  constructor(){
+    const shadow = UI.Utils.createShadowRootWithCoreStyles(this, 
+      {cssFile: 'ui/legacy/smallBubble.css', delegatesFocus: undefined});
+  }
+}`,
+      filename: 'front_end/components/test.ts',
+      errors: [{message: MIGRATION_ERROR_MESSAGE}],
+      output: `import smallBubbleStyles from './smallBubble.css.js';
+export class Component extends UI.Widget.Widget {
+  constructor(){
+    const shadow = UI.Utils.createShadowRootWithCoreStyles(this, 
+      {cssFile: smallBubbleStyles, delegatesFocus: undefined});
+  }
+}`
+    },
+    {
+      code: `export class Component extends UI.Widget.Widget {
+constructor(){
+  const shadow = createShadowRootWithCoreStyles(this, 
+    {cssFile: 'ui/legacy/smallBubble.css', delegatesFocus: undefined});
+  const root = createShadowRootWithCoreStyles(this, 
+    {cssFile: 'ui/legacy/smallBubbles.css', delegatesFocus: undefined});
+}
+}`,
+      filename: 'front_end/components/test.ts',
+      errors: [{message: MIGRATION_ERROR_MESSAGE}, {message: MIGRATION_ERROR_MESSAGE}],
+      output: `import smallBubblesStyles from './smallBubbles.css.js';
+import smallBubbleStyles from './smallBubble.css.js';
+export class Component extends UI.Widget.Widget {
+constructor(){
+  const shadow = createShadowRootWithCoreStyles(this, 
+    {cssFile: smallBubbleStyles, delegatesFocus: undefined});
+  const shadow = createShadowRootWithCoreStyles(this, 
+    {cssFile: smallBubblesStyles, delegatesFocus: undefined});
+}
+}`
+    },
+    {
+      code: `export class Component extends UI.Widget.Widget {
+constructor(){
+  const shadow = UI.Utils.createShadowRootWithCoreStyles(this, 
+    {cssFile: 'ui/legacy/smallBubble.css', delegatesFocus: undefined});
+  const root = UI.Utils.createShadowRootWithCoreStyles(this, 
+    {cssFile: 'ui/legacy/smallBubbles.css', delegatesFocus: undefined});
+}
+}`,
+      filename: 'front_end/components/test.ts',
+      errors: [{message: MIGRATION_ERROR_MESSAGE}, {message: MIGRATION_ERROR_MESSAGE}],
+      output: `import smallBubblesStyles from './smallBubbles.css.js';
+import smallBubbleStyles from './smallBubble.css.js';
+export class Component extends UI.Widget.Widget {
+constructor(){
+  const shadow = UI.Utils.createShadowRootWithCoreStyles(this, 
+    {cssFile: smallBubbleStyles, delegatesFocus: undefined});
+  const shadow = UI.Utils.createShadowRootWithCoreStyles(this, 
+    {cssFile: smallBubblesStyles, delegatesFocus: undefined});
+}
+}`
+    },
+    {
+      code: `
+      export class Component extends UI.Widget.Widget {
+        constructor(){
+          const shadow = createShadowRootWithCoreStyles(this);
+        }
+      }
+      `,
+      filename: 'front_end/components/test.ts',
+      errors: [{message: MANUALLY_MIGRATE_ERROR_ESSAGE}],
+    },
+    {
+      code: `
+      export class Component extends UI.Widget.Widget {
+        constructor(){
+          const shadow = UI.Utils.createShadowRootWithCoreStyles(this);
+        }
+      }
+      `,
+      filename: 'front_end/components/test.ts',
+      errors: [{message: MANUALLY_MIGRATE_ERROR_ESSAGE}],
+    },
+  ]
+});
