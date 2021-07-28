81ddd75b9b309bc0ab667aa62b253d745b78ea61
Jack Franklin <jacktfranklin@chromium.org>
2021-07-28 13:11:29 +0000

    [DataGrid] migrate events
    Experimented here with an Events.ts file which I think works quite
    well. Wouldn't do it for all components but when there's a fair few
    events I think it's a nice pattern to follow.
    
    Bug: chromium:1229172
    Change-Id: Icab8fd80f71047b4310eb97a0138933add236032

--- test/unittests/front_end/ui/components/DataGrid_test.ts
+++ test/unittests/front_end/ui/components/DataGrid_test.ts
@@ -505,7 +505,7 @@ describe('DataGrid', () => {
     await coordinator.done();
 
     const columnHeaderClickEvent =
-        getEventPromise<DataGrid.DataGrid.ColumnHeaderClickEvent>(component, 'columnheaderclick');
+        getEventPromise<DataGrid.DataGridEvents.ColumnHeaderClickEvent>(component, 'columnheaderclick');
     const cityColumn = getHeaderCellForColumnId(component.shadowRoot, 'city');
     dispatchClickEvent(cityColumn);
 
@@ -520,7 +520,7 @@ describe('DataGrid', () => {
     await coordinator.done();
 
     const columnHeaderClickEvent =
-        getEventPromise<DataGrid.DataGrid.ColumnHeaderClickEvent>(component, 'columnheaderclick');
+        getEventPromise<DataGrid.DataGridEvents.ColumnHeaderClickEvent>(component, 'columnheaderclick');
     const focusableCell = getFocusableCell(component.shadowRoot);
     // Check that the focusable cell is the header cell as it's a table with
     // sortable columns.
@@ -542,7 +542,8 @@ describe('DataGrid', () => {
     assertShadowRoot(component.shadowRoot);
     await coordinator.done();
 
-    const bodyCellFocusedEvent = getEventPromise<DataGrid.DataGrid.BodyCellFocusedEvent>(component, 'cellfocused');
+    const bodyCellFocusedEvent =
+        getEventPromise<DataGrid.DataGridEvents.BodyCellFocusedEvent>(component, 'cellfocused');
     const focusableCell = getFocusableCell(component.shadowRoot);
     focusableCell.focus();
     const cellFocusedEvent = await bodyCellFocusedEvent;
