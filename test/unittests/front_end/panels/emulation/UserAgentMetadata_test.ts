f1d47347aaf4484c029a3bad90e2f024df615b15
Kriti Sapra <kritisapra@google.com>
2021-08-03 14:54:15 +0000

    Moving DevicesSettingsTab from emulation to settings/emulation
    Bug: 1234798
    Change-Id: I714a66d35edf68e7824c1dad9421311d74451f7b

--- test/unittests/front_end/panels/emulation/UserAgentMetadata_test.ts
+++ test/unittests/front_end/panels/emulation/UserAgentMetadata_test.ts
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 const {assert} = chai;
-import * as EmulationUtils from '../../../../../front_end/panels/emulation/utils/utils.js';
+import * as EmulationUtils from '../../../../../front_end/panels/settings/emulation/utils/utils.js';
 import type * as Protocol from '../../../../../front_end/generated/protocol.js';
 const UserAgentMetadata = EmulationUtils.UserAgentMetadata;
 
