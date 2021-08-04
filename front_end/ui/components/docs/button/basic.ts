6da431053cc43724dd4d868cf4164a95b8e889a6
Alex Rudenko <alexrudenko@chromium.org>
2021-08-04 14:00:27 +0000

    Create Button component
    This component wraps around a <button> element and implements
    common DevTools styles for primary and secondary buttons as
    well as a button with an icon and text. Hover/focus/pressed
    states are still in progress in the designs and the component
    will be updated in the follow-ups to include those styles.
    
    Screenshot: https://i.imgur.com/5EHND1v.png
    Bug: none
    Change-Id: Id0d11e49e678e1de007025b548fa1357f86ba8f2

---
+++ front_end/ui/components/docs/button/basic.ts
@@ -0,0 +1,35 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as FrontendHelpers from '../../../../../test/unittests/front_end/helpers/EnvironmentHelpers.js';
+import * as Buttons from '../../buttons/buttons.js';
+import * as ComponentHelpers from '../../helpers/helpers.js';
+
+await ComponentHelpers.ComponentServerSetup.setup();
+await FrontendHelpers.initializeGlobalVars();
+
+function appenButton(button: Buttons.Button.Button): void {
+  document.querySelector('#container')?.appendChild(button);
+}
+
+// Primary
+const primaryButton = new Buttons.Button.Button();
+primaryButton.innerText = 'Click me';
+primaryButton.onclick = () => alert('clicked');
+appenButton(primaryButton);
+
+// Secondary
+const secondaryButton = new Buttons.Button.Button();
+secondaryButton.innerText = 'Click me';
+secondaryButton.onclick = () => alert('clicked');
+secondaryButton.primary = false;
+appenButton(secondaryButton);
+
+// Icon
+const iconButton = new Buttons.Button.Button();
+iconButton.innerText = 'Click me';
+iconButton.iconUrl =
+    'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTEyIDZIMTFWMTEuNUg1LjVWMTIuNUgxMVYxOEgxMlYxMi41SDE3LjVWMTEuNUgxMlY2WiIgZmlsbD0iYmxhY2siLz4KPC9zdmc+Cg==';
+iconButton.onclick = () => alert('clicked');
+appenButton(iconButton);
