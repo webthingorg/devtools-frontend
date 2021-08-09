7ab5322830e7f3bfdeff7d9e51f2c54c2630d325
Rahat Muneeb <irahatmuneeb@gmail.com>
2021-08-10 00:51:52 +0100

    Lit-Components for Joint Filter Model - Hide Issues Feature
    This cl contains Lit-Components needed for the new joint filter model for hidingissues. It also contains changes to the issue manager needed for the lit-components.
    
    (1/3) CLs
    
    Bug:1175722
    Change-Id: Icaafa2c1c15d1088850d762a09081de87f4d6381

--- front_end/panels/issues/components/components.ts
+++ front_end/panels/issues/components/components.ts
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as HideIssueLabel from './HideIssueLabel.js';
 import * as HideIssuesMenu from './HideIssuesMenu.js';
+import * as IssueFilterSection from './IssueFilterSection.js';
 
 export {
+  HideIssueLabel,
   HideIssuesMenu,
+  IssueFilterSection,
 };
