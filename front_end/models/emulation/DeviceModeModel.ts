d2f48496cfb0f7fec0a49e3155558074cc294a78
Kateryna Prokopenko <kprokopenko@chromium.org>
2021-08-13 14:28:29 +0000

    Use type-safe events for DeviceModeModel
    Bug: 1228674
    Change-Id: I13b75b2297baca401936659366dc31124cc57edb

--- front_end/models/emulation/DeviceModeModel.ts
+++ front_end/models/emulation/DeviceModeModel.ts
@@ -855,6 +855,10 @@ export const enum Events {
   Updated = 'Updated',
 }
 
+export type EventTypes = {
+  [Events.Updated]: void,
+};
+
 // TODO(crbug.com/1167717): Make this a const enum again
 // eslint-disable-next-line rulesdir/const_enum
 export enum Type {
