928e2320c0f10b86eb8c1c2bbd02823dee012cb1
Rahat Muneeb <irahatmuneeb@gmail.com>
2021-08-18 01:31:46 +0100

    Group Issues in the issues panel by IssueKind
    This cl includes logic which allows users to toggle grouping issues by issue kinds.
    
    Bug:1175722
    Change-Id: Ic6e0bc664f548b3f594df5fa79a5ec0f1455fd79

---
+++ front_end/panels/issues/IssueKindView.ts
@@ -0,0 +1,122 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Common from '../../core/common/common.js';
+import * as i18n from '../../core/i18n/i18n.js';
+import * as IssuesManager from '../../models/issues_manager/issues_manager.js';
+import * as Adorners from '../../ui/components/adorners/adorners.js';
+import * as IconButton from '../../ui/components/icon_button/icon_button.js';
+import * as IssueCounter from '../../ui/components/issue_counter/issue_counter.js';
+import * as UI from '../../ui/legacy/legacy.js';
+
+const UIStrings = {
+  /**
+   * @description Kind title for a group of improvements
+   */
+  improvements: 'Improvements',
+  /**
+    * @description Kind title for a group of page errors
+    */
+  pageErrors: 'Page Errors',
+  /**
+    * @description Kind title for a group of breaking changes
+    */
+  breakingChanges: 'Breaking Changes',
+};
+
+const str_ = i18n.i18n.registerUIStrings('panels/issues/IssueKindView.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+export function getGroupIssuesByKindSetting(): Common.Settings.Setting<boolean> {
+  return Common.Settings.Settings.instance().createSetting('groupIssuesByKind', false);
+}
+
+export function issueKindViewSortPriority(a: IssueKindView, b: IssueKindView): number {
+  if (a.getKind() === IssuesManager.Issue.IssueKind.PageError) {
+    return -1;
+  }
+  if (a.getKind() === IssuesManager.Issue.IssueKind.BreakingChange &&
+      b.getKind() === IssuesManager.Issue.IssueKind.Improvement) {
+    return -1;
+  }
+  return 1;
+}
+
+export function getClassNameFromKind(kind: IssuesManager.Issue.IssueKind): string {
+  switch (kind) {
+    case IssuesManager.Issue.IssueKind.BreakingChange:
+      return 'breaking-changes';
+    case IssuesManager.Issue.IssueKind.Improvement:
+      return 'improvements';
+    case IssuesManager.Issue.IssueKind.PageError:
+      return 'page-errors';
+  }
+}
+
+export class IssueKindView extends UI.TreeOutline.TreeElement {
+  private kind: IssuesManager.Issue.IssueKind;
+  private issueCount: HTMLElement;
+
+  constructor(kind: IssuesManager.Issue.IssueKind) {
+    super(undefined, true);
+    this.kind = kind;
+    this.issueCount = document.createElement('span');
+
+    this.toggleOnClick = true;
+    this.listItemElement.classList.add('issue-kind');
+    this.listItemElement.classList.add(getClassNameFromKind(kind));
+    this.childrenListElement.classList.add('issue-kind-body');
+  }
+
+  getKindName(): string {
+    switch (this.kind) {
+      case IssuesManager.Issue.IssueKind.BreakingChange:
+        return i18nString(UIStrings.breakingChanges);
+      case IssuesManager.Issue.IssueKind.Improvement:
+        return i18nString(UIStrings.improvements);
+      case IssuesManager.Issue.IssueKind.PageError:
+        return i18nString(UIStrings.pageErrors);
+    }
+  }
+
+  getKind(): IssuesManager.Issue.IssueKind {
+    return this.kind;
+  }
+
+  private appendHeader(): void {
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const issueKindIcon = new IconButton.Icon.Icon();
+    issueKindIcon.data = IssueCounter.IssueCounter.getIssueKindIconData(this.kind);
+    issueKindIcon.classList.add('leading-issue-icon');
+
+    const countAdorner = new Adorners.Adorner.Adorner();
+    countAdorner.data = {
+      name: 'countWrapper',
+      content: this.issueCount,
+    };
+    countAdorner.classList.add('aggregated-issues-count');
+    this.issueCount.textContent = '0';
+
+    const title = document.createElement('div');
+    title.classList.add('title');
+    title.textContent = this.getKindName();
+
+    header.appendChild(issueKindIcon);
+    header.appendChild(countAdorner);
+    header.appendChild(title);
+
+    this.listItemElement.appendChild(header);
+  }
+
+  onattach(): void {
+    this.appendHeader();
+    this.expand();
+  }
+
+  update(count: number): void {
+    this.issueCount.textContent = `${count}`;
+  }
+}
--- front_end/panels/issues/IssuesPane.ts
+++ front_end/panels/issues/IssuesPane.ts
@@ -12,6 +12,7 @@ import {HiddenIssuesRow} from './HiddenIssuesRow.js';
 import type {AggregatedIssue} from './IssueAggregator.js';
 import {Events as IssueAggregatorEvents, IssueAggregator} from './IssueAggregator.js';
 import {IssueView} from './IssueView.js';
+import {IssueKindView, getGroupIssuesByKindSetting, issueKindViewSortPriority} from './IssueKindView.js';
 
 const UIStrings = {
   /**
@@ -61,6 +62,14 @@ const UIStrings = {
    * @description Label for a checkbox which toggles grouping by category in the issues tab
    */
   groupByCategory: 'Group by category',
+  /**
+  * @description Title for a checkbox which toggles grouping by kind in the issues tab
+    */
+  groupDisplayedIssuesUnderKind: 'Group displayed issues as Page errors, Breaking changes and Improvements',
+  /**
+  * @description Label for a checkbox which toggles grouping by kind in the issues tab
+    */
+  groupByKind: 'Group by kind',
   /**
    * @description Title for a checkbox. Whether the issues tab should include third-party issues or not.
    */
@@ -158,6 +167,7 @@ let issuesPaneInstance: IssuesPane;
 
 export class IssuesPane extends UI.Widget.VBox {
   private categoryViews: Map<IssuesManager.Issue.IssueCategory, IssueCategoryView>;
+  private kindViews: Map<IssuesManager.Issue.IssueKind, IssueKindView>;
   private issueViews: Map<string, IssueView>;
   private showThirdPartyCheckbox: UI.Toolbar.ToolbarSettingCheckbox|null;
   private issuesTree: UI.TreeOutline.TreeOutlineInShadow;
@@ -173,6 +183,7 @@ export class IssuesPane extends UI.Widget.VBox {
     this.contentElement.classList.add('issues-pane');
 
     this.categoryViews = new Map();
+    this.kindViews = new Map();
     this.issueViews = new Map();
     this.showThirdPartyCheckbox = null;
 
@@ -225,6 +236,14 @@ export class IssuesPane extends UI.Widget.VBox {
     groupByCategoryCheckbox.setVisible(false);
     rightToolbar.appendToolbarItem(groupByCategoryCheckbox);
     groupByCategorySetting.addChangeListener(() => {
+      this.fullUpdate(true);
+    });
+
+    const groupByKindSetting = getGroupIssuesByKindSetting();
+    const groupByKindSettingCheckbox = new UI.Toolbar.ToolbarSettingCheckbox(
+        groupByKindSetting, i18nString(UIStrings.groupDisplayedIssuesUnderKind), i18nString(UIStrings.groupByKind));
+    rightToolbar.appendToolbarItem(groupByKindSettingCheckbox);
+    groupByKindSetting.addChangeListener(() => {
       this.fullUpdate(true);
     });
 
@@ -305,28 +324,44 @@ export class IssuesPane extends UI.Widget.VBox {
   }
 
   private getIssueViewParent(issue: AggregatedIssue): UI.TreeOutline.TreeOutline|UI.TreeOutline.TreeElement {
-    if (!getGroupIssuesByCategorySetting().get()) {
-      if (issue.isHidden()) {
-        return this.hiddenIssuesRow;
-      }
-      return this.issuesTree;
+    if (issue.isHidden()) {
+      return this.hiddenIssuesRow;
     }
-
-    const category = issue.getCategory();
-    const view = this.categoryViews.get(category);
-    if (view) {
-      return view;
-    }
-
-    const newView = new IssueCategoryView(category);
-    this.issuesTree.appendChild(newView, (a, b) => {
-      if (a instanceof IssueCategoryView && b instanceof IssueCategoryView) {
-        return a.getCategoryName().localeCompare(b.getCategoryName());
+    if (getGroupIssuesByCategorySetting().get()) {
+      const category = issue.getCategory();
+      const view = this.categoryViews.get(category);
+      if (view) {
+        return view;
       }
-      return 0;
-    });
-    this.categoryViews.set(category, newView);
-    return newView;
+
+      const newView = new IssueCategoryView(category);
+      this.issuesTree.appendChild(newView, (a, b) => {
+        if (a instanceof IssueCategoryView && b instanceof IssueCategoryView) {
+          return a.getCategoryName().localeCompare(b.getCategoryName());
+        }
+        return 0;
+      });
+      this.categoryViews.set(category, newView);
+      return newView;
+    }
+    if (getGroupIssuesByKindSetting().get()) {
+      const kind = issue.getKind();
+      const view = this.kindViews.get(kind);
+      if (view) {
+        return view;
+      }
+
+      const newView = new IssueKindView(kind);
+      this.issuesTree.appendChild(newView, (a, b) => {
+        if (a instanceof IssueKindView && b instanceof IssueKindView) {
+          return issueKindViewSortPriority(a, b);
+        }
+        return 0;
+      });
+      this.kindViews.set(kind, newView);
+      return newView;
+    }
+    return this.issuesTree;
   }
 
   private clearViews<T>(views: Map<T, UI.TreeOutline.TreeElement>, preservedSet?: Set<T>): void {
@@ -345,6 +380,7 @@ export class IssuesPane extends UI.Widget.VBox {
 
   private fullUpdate(force: boolean): void {
     this.clearViews(this.categoryViews, force ? undefined : this.aggregator.aggregatedIssueCategories());
+    this.clearViews(this.kindViews, force ? undefined : this.aggregator.aggregatedIssueKinds());
     this.clearViews(this.issueViews, force ? undefined : this.aggregator.aggregatedIssueCodes());
     if (this.aggregator) {
       for (const issue of this.aggregator.aggregatedIssues()) {
@@ -354,9 +390,20 @@ export class IssuesPane extends UI.Widget.VBox {
     this.updateCounts();
   }
 
+  private updateIssueKindViewsCount(): void {
+    for (const view of this.kindViews.values()) {
+      const count =
+          this.issuesManager.numberOfIssues(view.getKind()) - this.issuesManager.numberOfHiddenIssues(view.getKind());
+      view.update(count);
+    }
+  }
+
   private updateCounts(): void {
     this.showIssuesTreeOrNoIssuesDetectedMessage(
         this.issuesManager.numberOfIssues(), this.issuesManager.numberOfHiddenIssues());
+    if (getGroupIssuesByKindSetting().get()) {
+      this.updateIssueKindViewsCount();
+    }
   }
 
   private showIssuesTreeOrNoIssuesDetectedMessage(issuesCount: number, hiddenIssueCount: number): void {
@@ -390,6 +437,11 @@ export class IssuesPane extends UI.Widget.VBox {
       if (issueView.isForHiddenIssue()) {
         this.hiddenIssuesRow.expand();
         this.hiddenIssuesRow.reveal();
+      }
+      if (getGroupIssuesByKindSetting().get() && !issueView.isForHiddenIssue()) {
+        const kindView = this.kindViews.get(issueView.getIssueKind());
+        kindView?.expand();
+        kindView?.reveal();
       }
       issueView.expand();
       issueView.reveal();
