43809f31d8cb1044b046110d19c1506713e15780
Sigurd Schneider <sigurds@chromium.org>
2021-08-19 13:01:58 +0200

    Type annotate Runtime.RemoteObjectId
    DISABLE_THIRD_PARTY_CHECK=disable
    
    Bug: chromium:1226471
    Change-Id: If9c171471a6b7320fbc2c09764b44faa5502d74c

--- front_end/generated/protocol.d.ts
+++ front_end/generated/protocol.d.ts
@@ -14853,7 +14853,7 @@ declare namespace Protocol {
     /**
      * Heap snapshot object id.
      */
-    export type HeapSnapshotObjectId = string;
+    export type HeapSnapshotObjectId = OpaqueIdentifier<string, 'Protocol.HeapProfiler.HeapSnapshotObjectId'>;
 
     /**
      * Sampling Heap Profile node. Holds callsite information, allocation statistics and child nodes.
@@ -15371,7 +15371,7 @@ declare namespace Protocol {
     /**
      * Unique object identifier.
      */
-    export type RemoteObjectId = string;
+    export type RemoteObjectId = OpaqueIdentifier<string, 'Protocol.Runtime.RemoteObjectId'>;
 
     /**
      * Primitive value which cannot be JSON-stringified. Includes values `-0`, `NaN`, `Infinity`,
