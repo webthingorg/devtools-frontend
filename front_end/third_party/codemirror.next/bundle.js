db281e4d41ae0e1106b79e573bd446a87ef42506
Tim van der Lippe <tvanderlippe@chromium.org>
2021-08-20 14:27:59 +0100

    Add CodeMirror.next as a third party dependency
    Change-Id: I7c9ec572377c04bed8f7610527252778ea463df9

---
+++ front_end/third_party/codemirror.next/bundle.js
@@ -0,0 +1,12 @@
+// Base script used with Rollup to bundle the necessary CodeMirror
+// components.
+//
+// Note that this file is also used as a TypeScript source to bundle
+// the .d.ts files.
+
+import {EditorView} from "@codemirror/view"
+
+export {
+  EditorView,
+}
+
---
+++ front_end/third_party/codemirror.next/rollup.config.js
@@ -0,0 +1,25 @@
+import {nodeResolve} from "@rollup/plugin-node-resolve"
+import dts from "rollup-plugin-dts"
+import {terser} from "rollup-plugin-terser"
+
+export default [{
+  input: "./bundle.js",
+  output: {
+    format: "es",
+    file: "./codemirror.next.js",
+    externalLiveBindings: false
+  },
+  plugins: [
+    nodeResolve(),
+    terser()
+  ]
+}, {
+  input: "./bundle.d.ts",
+  output: {
+    file: "./codemirror.next.d.ts",
+    format: "es"
+  },
+  plugins: [
+    dts({respectExternal: true})
+  ]
+}]
