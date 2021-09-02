268f7fe08006068f0c8dba7231ef829c55045112
Andrey Kosyakov <caseq@chromium.org>
2021-09-02 16:18:59 -0700

    Make (shared|service) worker reload work with browser-level connection
    - Assume target type of Worker for shared workers (this used to
        be Browser due do a bug before)
    - Add Inspector capability to Worker targets;
    - Make TargetDetachedDialog a singleton (because it's a modal anyway);
    - Dismiss TargetDetachedDialog when a new top-level target is created.
    
    This is a pre-requisite of crrev.com/c/3114919 and a follow-up to
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3115448
    
    Bug: 1213714
    Change-Id: If788e206e13a47aabe34a3bbdd9f6d02b23580b7

--- front_end/core/sdk/Target.ts
+++ front_end/core/sdk/Target.ts
@@ -56,8 +56,8 @@ export class Target extends ProtocolClient.InspectorBackend.TargetBase {
         }
         break;
       case Type.Worker:
-        this.capabilitiesMask =
-            Capability.JS | Capability.Log | Capability.Network | Capability.Target | Capability.IO | Capability.Media;
+        this.capabilitiesMask = Capability.JS | Capability.Log | Capability.Network | Capability.Target |
+            Capability.IO | Capability.Media | Capability.Inspector;
         break;
       case Type.Node:
         this.capabilitiesMask = Capability.JS;
