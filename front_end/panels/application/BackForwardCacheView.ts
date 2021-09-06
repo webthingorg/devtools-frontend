3b1ade36eb29410aa614b289fc28bc8850f2e8ab
Yuzu Saijo <yuzus@chromium.org>
2021-09-06 22:07:38 +0900

    [devtools] Reorganize not-restored reasons in DevTools
    This CL reorganizes the not-restored reasons shown in DevTools so that
    they can read better in a user friendly manner.
    
    More context here:
    https://docs.google.com/document/d/1jRSmS3geJ3dYbjjustc2SRXPUqq-8mgQgxIlmPD6lwc/edit?usp=sharing
    
    Bug:1110752
    Change-Id: Ia1aca299063c19200581f654c03cd58242304b5e

--- front_end/panels/application/BackForwardCacheView.ts
+++ front_end/panels/application/BackForwardCacheView.ts
@@ -8,7 +8,7 @@ import * as SDK from '../../core/sdk/sdk.js';
 import * as LitHtml from '../../ui/lit-html/lit-html.js';
 import * as ReportView from '../../ui/components/report_view/report_view.js';
 import * as UI from '../../ui/legacy/legacy.js';
-import type * as Protocol from '../../generated/protocol.js';
+import * as Protocol from '../../generated/protocol.js';
 
 const UIStrings = {
   /**
@@ -40,10 +40,6 @@ const UIStrings = {
    */
   unknown: 'unknown',
   /**
-   * @description Text for the row with explanations for the back-forward cache status
-   */
-  explanations: 'Explanations',
-  /**
    * @description Status text for the status of the back-forward cache status indicating that
    * the back-forward cache was not used and a normal navigation occured instead.
    */
@@ -53,6 +49,21 @@ const UIStrings = {
    * the back-forward cache was used to restore the page instead of reloading it.
    */
   restoredFromBFCache: 'Restored from back-forward cache',
+  /**
+   * @description Category text for the reasons which need to be cleaned up on the websites in
+   * order to make the page eligible for the back-forward cache.
+   */
+  pageSupportNeeded: 'Features preventing back-forward cache:',
+  /**
+   * @description Category text for the reasons which need to be addressed on the chrome's side
+   * in order to make the page eligible for the back-forward cache.
+   */
+  chromeSupportNeeded: 'The last navigation was not cached because:',
+  /**
+   * @description Explanation text for the reasons which will be supported in a future version
+   * of Chrome.
+   */
+  willBeSupported: '(Supported in a future version of Chrome)',
 };
 const str_ = i18n.i18n.registerUIStrings('panels/application/BackForwardCacheView.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
@@ -129,10 +140,47 @@ export class BackForwardCacheView extends UI.ThrottledWidget.ThrottledWidget {
     if (explanations.length === 0) {
       return LitHtml.nothing;
     }
-    return LitHtml.html`<${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.explanations)}</${
-        ReportView.ReportView.ReportKey.litTagName}>
-    <${ReportView.ReportView.ReportValue.litTagName}>${LitHtml.html`${explanations.map(explanation => {
-      return LitHtml.html`<div>${explanation.reason} (${explanation.type})</div>`;
-    })}`}</${ReportView.ReportView.ReportValue.litTagName}>`;
+
+    const pageSupportNeeded = explanations.filter(
+        explanation => explanation.type === Protocol.Page.BackForwardCacheNotRestoredReasonType.PageSupportNeeded);
+    const supportPending = explanations.filter(
+        explanation => explanation.type === Protocol.Page.BackForwardCacheNotRestoredReasonType.SupportPending);
+    const circumstantial = explanations.filter(
+        explanation => explanation.type === Protocol.Page.BackForwardCacheNotRestoredReasonType.Circumstantial);
+
+    return LitHtml.html`
+    <${ReportView.ReportView.ReportKey.litTagName}>
+    ${
+        pageSupportNeeded.length + supportPending.length > 0 ?
+            LitHtml.html`${i18nString(UIStrings.pageSupportNeeded)}` :
+            LitHtml.nothing}
+    </${ReportView.ReportView.ReportKey.litTagName}>
+    <${ReportView.ReportView.ReportValue.litTagName}>
+${this.renderReasonsHelper(pageSupportNeeded)}
+${this.renderReasonsHelper(supportPending)}
+    </${ReportView.ReportView.ReportValue.litTagName}>
+    <${ReportView.ReportView.ReportKey.litTagName}>
+        ${circumstantial.length > 0 ? LitHtml.html`${i18nString(UIStrings.chromeSupportNeeded)}` : LitHtml.nothing}
+    </${ReportView.ReportView.ReportKey.litTagName}>
+    <${ReportView.ReportView.ReportValue.litTagName}>
+${this.renderReasonsHelper(circumstantial)}
+    </${ReportView.ReportView.ReportValue.litTagName}>`;
+  }
+
+  private renderReasonsHelper(explanations: Protocol.Page.BackForwardCacheNotRestoredExplanation[]):
+      LitHtml.TemplateResult|{} {
+    return LitHtml.html`${explanations.map(explanation => this.renderExplanation(explanation))}`;
+  }
+
+  private renderExplanation(explanation: Protocol.Page.BackForwardCacheNotRestoredExplanation): LitHtml.TemplateResult {
+    return LitHtml.html`
+      <div>
+        ${explanation.reason}
+        ${
+        explanation.type === Protocol.Page.BackForwardCacheNotRestoredReasonType.SupportPending ?
+            i18nString(UIStrings.willBeSupported) :
+            ''}
+      </div>
+    `;
   }
 }
