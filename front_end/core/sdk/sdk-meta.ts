8c731280c93aa2983a44aed4e16fc4ea88169067
Johan Bay <jobay@chromium.org>
2021-09-08 10:48:42 +0000

    Add auto dark mode emulation in Renderer tab
    This change adds a drop down select box for toggling auto dark mode.
    Screenshot: https://imgur.com/a/tnpZFpO
    Backend cl: https://crrev.com/c/3137939
    
    Doc: https://goo.gle/devtools-forced-dark-mode
    Bug: 1243309
    Change-Id: Ie11b539d3ef0238fb62ea39ab1be53d8a31ba681

--- front_end/core/sdk/sdk-meta.ts
+++ front_end/core/sdk/sdk-meta.ts
@@ -167,6 +167,14 @@ const UIStrings = {
   */
   doNotEmulateAFocusedPage: 'Do not emulate a focused page',
   /**
+  *@description Title of a Rendering setting that can be invoked through the Command Menu
+  */
+  emulateForcedDarkMode: 'Emulate forced dark mode',
+  /**
+  *@description Title of a Rendering setting that can be invoked through the Command Menu
+  */
+  doNotEmulateForcedDarkMode: 'Do not emulate forced dark mode',
+  /**
   *@description Title of a setting under the Rendering category that can be invoked through the Command Menu
   */
   doNotEmulateCssMediaType: 'Do not emulate CSS media type',
@@ -313,6 +321,19 @@ const UIStrings = {
    * @description Title of a setting under the Network category that can be invoked through the Command Menu
    */
   disableCache: 'Disable cache (while DevTools is open)',
+  /**
+  * @description The name of a checkbox setting in the Rendering tool. This setting
+  * emulates that the webpage is in auto dark mode.
+  */
+  emulateAutoDarkMode: 'Emulate auto dark mode',
+  /**
+  *@description Title of a setting for emulating enabled auto dark mode.
+  */
+  enabledDarkMode: 'Enable',
+  /**
+   * @description Title of a setting for emulating disabled auto dark mode.
+   */
+  disabledDarkMode: 'Disable',
 };
 const str_ = i18n.i18n.registerUIStrings('core/sdk/sdk-meta.ts', UIStrings);
 const i18nLazyString = i18n.i18n.getLazilyComputedLocalizedString.bind(undefined, str_);
@@ -669,6 +690,25 @@ Common.Settings.registerSettingExtension({
 
 Common.Settings.registerSettingExtension({
   category: Common.Settings.SettingCategory.RENDERING,
+  title: i18nLazyString(UIStrings.emulateAFocusedPage),
+  settingName: 'emulateForcedDarkMode',
+  settingType: Common.Settings.SettingType.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: false,
+  options: [
+    {
+      value: true,
+      title: i18nLazyString(UIStrings.emulateForcedDarkMode),
+    },
+    {
+      value: false,
+      title: i18nLazyString(UIStrings.doNotEmulateForcedDarkMode),
+    },
+  ],
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategory.RENDERING,
   settingName: 'emulatedCSSMedia',
   settingType: Common.Settings.SettingType.ENUM,
   storageType: Common.Settings.SettingStorageType.Session,
@@ -991,6 +1031,32 @@ Common.Settings.registerSettingExtension({
     {
       value: false,
       title: i18nLazyString(UIStrings.enableCache),
+    },
+  ],
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategory.RENDERING,
+  title: i18nLazyString(UIStrings.emulateAutoDarkMode),
+  settingName: 'emulateAutoDarkMode',
+  settingType: Common.Settings.SettingType.ENUM,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: 'default',
+  options: [
+    {
+      title: i18nLazyString(UIStrings.emulateAutoDarkMode),
+      text: i18nLazyString(UIStrings.noEmulation),
+      value: 'default',
+    },
+    {
+      title: i18nLazyString(UIStrings.emulateAutoDarkMode),
+      text: i18nLazyString(UIStrings.enabledDarkMode),
+      value: 'enabled',
+    },
+    {
+      title: i18nLazyString(UIStrings.emulateAutoDarkMode),
+      text: i18nLazyString(UIStrings.disabledDarkMode),
+      value: 'disabled',
     },
   ],
 });
