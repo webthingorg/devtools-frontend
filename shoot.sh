51ac691645908d579476787468d03d9635bee47f
Liviu Rau <liviurau@chromium.org>
2021-09-15 14:53:01 +0200

    Isolate and test
    Change-Id: I538e2ad91268df3c62c032f82ec14149dd7a2a41

--- shoot.sh
+++ shoot.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
+isolated_folder="$HOME/tmp$1"
+
+rm -rf $isolated_folder/* $isolated_folder/.?*
+rm unittests.*
 tools/mb/mb.py gen -m bar -b 'foo' --config-file=infra/mb/mb_config.pyl --isolate-map-file=infra/mb/gn_isolate_map.pyl --swarming-targets-file=targets //out/Release
+echo '__________________________'
+
+ninja -C out/Release -j80 test/e2e:e2e test:test
+echo '__________________________'
+
+
+isolate remap -i out/Release/unittests.isolate -outdir=$isolated_folder
+#isolate batcharchive --dump-json out.json -cas-instance projects/chromium-swarm/instances/default_instance -quiet out/Release/unittests.isolated.gen.json 
+
+#cashash=`jq -r .unittests out.json`
+#echo $cashash
+#cas download -cas-instance projects/chromium-swarm/instances/default_instance -dir $isolated_folder -digest $cashash
+
+
+echo '__________________________'
+
+cd $isolated_folder
+python scripts/test/run_unittests.py --target=Release --expanded-reporting
+cd -
