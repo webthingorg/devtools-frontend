e6cba5d98946bb60a6c656425e4ef0bbf2efe732
Wolfgang Beyer <wolfi@chromium.org>
2021-09-21 14:44:51 +0200

    update test
    move note from tooltip to report
    
    Change-Id: I41b30942273f3cad73e3bd843cba9d1e56b0b8b2

--- test/e2e/application/manifest_test.ts
+++ test/e2e/application/manifest_test.ts
@@ -43,6 +43,11 @@ describe.skip('The Manifest Page', async () => {
     const fieldValues = await getTrimmedTextContent(FIELD_VALUES_SELECTOR);
     assert.strictEqual(fieldNames[3], 'App Id');
     assert.strictEqual(
-        fieldValues[3], `https://localhost:${getTestServerPort()}/test/e2e/resources/application/some_start_url`);
+        fieldValues[3],
+        `https://localhost:${getTestServerPort()}/test/e2e/resources/application/some_start_url` +
+            'Note: id is not specified in the manifest, start_url is used instead. To specify an ' +
+            'App Id that matches the current identity, set the id field to /some_start_url .',
+    );
+    await waitFor('icon-button[title="Copy to clipboard"]');
   });
 });
