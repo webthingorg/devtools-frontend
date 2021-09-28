0428605ab96cf162f0a803d51989f13743af5025
Wolfgang Beyer <wolfi@chromium.org>
2021-09-28 11:55:33 +0200

    Display reports in the ReportingApiView
    Upon entering the ReportingApiView DevTools starts listening to
    ReportingApiReportAdded and ReportingApiReportUpdated events. The
    payload for these events contains info about Reporting API reports,
    which DevTools displays in a dedicated panel of the application panel.
    
    Screenshot: https://i.imgur.com/hDMffTP.png
    
    Bug: 1205856
    Change-Id: Idc58cce317f0a3943ae2151aea148a022d4aea07

---
+++ test/unittests/front_end/panels/application/components/EndpointsGrid_test.ts
@@ -0,0 +1,24 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ApplicationComponents from '../../../../../../front_end/panels/application/components/components.js';
+import * as Coordinator from '../../../../../../front_end/ui/components/render_coordinator/render_coordinator.js';
+
+import {assertShadowRoot, renderElementIntoDOM} from '../../../helpers/DOMHelpers.js';
+
+const {assert} = chai;
+
+const coordinator = Coordinator.RenderCoordinator.RenderCoordinator.instance();
+
+describe('EndpointsGrid', async () => {
+  it('displays placeholder text if no data', async () => {
+    const component = new ApplicationComponents.EndpointsGrid.EndpointsGrid();
+    renderElementIntoDOM(component);
+    assertShadowRoot(component.shadowRoot);
+    await coordinator.done();
+
+    const footer = component.shadowRoot.querySelector('.reporting-footer div');
+    assert.strictEqual(footer?.textContent, 'No endpoints to display');
+  });
+});
