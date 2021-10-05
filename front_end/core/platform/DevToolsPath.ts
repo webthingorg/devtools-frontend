ce1e6a3c690c1612281bb52bec91813fe590f045
Kateryna Prokopenko <kprokopenko@chromium.org>
2021-10-05 07:29:07 +0000

    Introduce branded types for urls and paths in DevTools
    Bug: 1253323
    Change-Id: Ia3e805e2df1b4e6e46627009fc1d290bbd786557

---
+++ front_end/core/platform/DevToolsPath.ts
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class UrlStringTag {
+  private urlTag: (string|undefined);
+}
+export type UrlString = string&UrlStringTag;
+
+class RawPathStringTag {
+  private rawPathTag: (string|undefined);
+}
+export type RawPathString = string&RawPathStringTag;
+
+class EncodedPathStringTag {
+  private encodedPathTag: (string|undefined);
+}
+export type EncodedPathString = string&EncodedPathStringTag;
