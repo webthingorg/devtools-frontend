c87475e96f65ea37184a16103c92d988e1be28e7
Riho Isawa <rhisawa@google.com>
2021-10-20 00:53:34 +0000

    [devtools] Add the "Run Test" button about BFcache
    This CL adds the "Run Test" Button. After the user clicks the button, it
    will be shown the status whether BFcache is available in the page.
    
    
    Merge branch 'main' of https://chromium.googlesource.com/devtools/devtools-frontend into createButton
    
    Change-Id: I24cfe7292151b6b4f7d6e73c026df9ddc5266f60

--- front_end/panels/application/BackForwardCacheView.ts
+++ front_end/panels/application/BackForwardCacheView.ts
@@ -7,6 +7,8 @@ import * as i18n from '../../core/i18n/i18n.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import * as LitHtml from '../../ui/lit-html/lit-html.js';
 import * as ReportView from '../../ui/components/report_view/report_view.js';
+// import * as Button from '../../ui/components/buttons/buttons.js';
+// import * as IconButton from '../../ui/components/icon_button/icon_button.js';
 import * as UI from '../../ui/legacy/legacy.js';
 import * as Protocol from '../../generated/protocol.js';
 
@@ -68,18 +70,32 @@ const UIStrings = {
    * of the back-forward cache anymore.
    */
   supportPending: 'Pending Support',
+  /**
+   * @description Button name for showing whether BFcahce is available in the pages.
+   */
+  runTest: 'Run Test',
 };
 const str_ = i18n.i18n.registerUIStrings('panels/application/BackForwardCacheView.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 
+export interface FrameDetailsReportViewData {
+  data: SDK.ResourceTreeModel.ResourceTreeModel;
+}
+
 export class BackForwardCacheView extends UI.ThrottledWidget.ThrottledWidget {
+  // private frame?: SDK.ResourceTreeModel.ResourceTreeModel
+
   constructor() {
     super(true, 1000);
+    // this.target = target;
+    // this.model = target.model(SDK.ResourceTreeModel.ResourceTreeModel);
     this.getMainResourceTreeModel()?.addEventListener(
         SDK.ResourceTreeModel.Events.MainFrameNavigated, this.onBackForwardCacheUpdate, this);
     this.getMainResourceTreeModel()?.addEventListener(
         SDK.ResourceTreeModel.Events.BackForwardCacheDetailsUpdated, this.onBackForwardCacheUpdate, this);
     this.update();
+    // this.Buttonrender*()
+    this.renderButton();
   }
 
   wasShown(): void {
@@ -110,6 +126,50 @@ export class BackForwardCacheView extends UI.ThrottledWidget.ThrottledWidget {
     return this.getMainResourceTreeModel()?.mainFrame || null;
   }
 
+  private startButton!: HTMLButtonElement;
+
+  private renderButton(): void {
+    // this.populateRuntimeSettingAsToolbarCheckbox('lighthouse.clear_storage', this.settingsToolbarInternal);
+    // this.populateRuntimeSettingAsToolbarCheckbox('lighthouse.throttling', this.settingsToolbarInternal);
+    this.startButton = UI.UIUtils.createTextButton(
+        i18nString(UIStrings.runTest), () => this.showStatus(), 'runTest-button',
+        /* primary */ true);
+    this.setDefaultFocusedElement(this.startButton);
+    this.contentElement.style.overflow = 'auto';
+  }
+
+  private async historyEntryBack(): Promise<void> {
+    const mainTarget = SDK.TargetManager.TargetManager.instance().mainTarget();
+    if (mainTarget) {
+      const resourceTreeModel = mainTarget.model(SDK.ResourceTreeModel.ResourceTreeModel);
+      if (resourceTreeModel) {
+        const Historyresults = await resourceTreeModel.navigationHistory();
+        if (Historyresults) {
+          resourceTreeModel.navigateToHistoryEntry(Historyresults.entries[Historyresults.currentIndex - 1]);
+        }
+      }
+    }
+    SDK.TargetManager.TargetManager.instance().removeModelListener(
+        SDK.ResourceTreeModel.ResourceTreeModel, SDK.ResourceTreeModel.Events.FrameNavigated, this.HistoryEntryBack,
+        this);
+  }
+
+  private async showStatus(): Promise<void> {
+    const mainTarget = SDK.TargetManager.TargetManager.instance().mainTarget();
+    if (!mainTarget) {
+      return;
+    }
+    const resourceTreeModel = mainTarget.model(SDK.ResourceTreeModel.ResourceTreeModel);
+
+    SDK.TargetManager.TargetManager.instance().addModelListener(
+        SDK.ResourceTreeModel.ResourceTreeModel, SDK.ResourceTreeModel.Events.FrameNavigated, this.historyEntryBack,
+        this);
+
+    if (resourceTreeModel) {
+      resourceTreeModel.navigate('https:////blog.youtube/');
+    }
+  }
+
   private renderMainFrameInformation(mainFrame: SDK.ResourceTreeModel.ResourceTreeFrame|null): LitHtml.TemplateResult {
     if (!mainFrame) {
       return LitHtml.html`<${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.mainFrame)}</${
@@ -121,13 +181,14 @@ export class BackForwardCacheView extends UI.ThrottledWidget.ThrottledWidget {
     return LitHtml.html`
       <${ReportView.ReportView.ReportSectionHeader.litTagName}>${i18nString(UIStrings.lastMainFrameNavigation)}</${
         ReportView.ReportView.ReportSectionHeader.litTagName}>
+${this.startButton}
       <${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.url)}</${
         ReportView.ReportView.ReportKey.litTagName}>
       <${ReportView.ReportView.ReportValue.litTagName}>${mainFrame.url}</${
         ReportView.ReportView.ReportValue.litTagName}>
       <${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.bfcacheStatus)}</${
         ReportView.ReportView.ReportKey.litTagName}>
-      <${ReportView.ReportView.ReportValue.litTagName}>${
+        <${ReportView.ReportView.ReportValue.litTagName}>${
         this.renderBackForwardCacheStatus(
             mainFrame.backForwardCacheDetails.restoredFromCache)}</${ReportView.ReportView.ReportValue.litTagName}>
        ${this.maybeRenderExplanations(mainFrame.backForwardCacheDetails.explanations)}
