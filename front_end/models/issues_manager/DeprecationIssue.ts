d0c743ca41be820a3f51b6c982a62091b127c83e
Ari Chivukula <arichiv@chromium.org>
2021-11-02 16:11:45 +0000

    [Deprecation Console->Issue] (8) Implement Deprecation Issue (DevTools)
    This implements the deprecation protocol code per:
    https://docs.google.com/document/d/1gU2caqBEek2x8AeKop9SrSxqlO51ipealPMV4aisQNE/edit
    https://docs.google.com/document/d/1Jot9P30LFW9TQtc6eaQP9wRBwjLpUn-hIaxRJRUBFvI/edit
    
    This is a part of a set of CLs:
    (1) Remove legacy CSS Deprecation path
    (2) Consolidate DeprecationInfo
    (3) Streamline redundant Deprecation messages
    (4) Add features for console/issue deprecation
    (5) Fix feature status detail link
    (6) Implement Deprecation Issue (Chromium)
    (7) Move DeprecationIssue.ts code
    (8) Implement Deprecation Issue (DevTools)
    (9) Disable Console Deprecation
    (10) Remove Console Deprecation
    
    Bug: 1248484
    Change-Id: I59b2ce66dfaea82bf38bf36dc905f9ab13172a21

---
+++ front_end/models/issues_manager/DeprecationIssue.ts
@@ -0,0 +1,69 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as SDK from '../../core/sdk/sdk.js';
+import type * as Protocol from '../../generated/protocol.js';
+
+import {Issue, IssueCategory, IssueKind} from './Issue.js';
+import type {MarkdownIssueDescription} from './MarkdownIssueDescription.js';
+
+// TODO(crbug.com/1167717): Make this a const enum again
+// eslint-disable-next-line rulesdir/const_enum
+export enum IssueCode {
+  DeprecationIssue = 'DeprecationIssue',
+}
+
+export class DeprecationIssue extends Issue<IssueCode> {
+  private issueDetails: Protocol.Audits.DeprecationIssueDetails;
+
+  constructor(issueDetails: Protocol.Audits.DeprecationIssueDetails, issuesModel: SDK.IssuesModel.IssuesModel) {
+    super(IssueCode.DeprecationIssue, issuesModel);
+    this.issueDetails = issueDetails;
+  }
+
+  getCategory(): IssueCategory {
+    return IssueCategory.Other;
+  }
+
+  details(): Protocol.Audits.DeprecationIssueDetails {
+    return this.issueDetails;
+  }
+
+  getDescription(): MarkdownIssueDescription|null {
+    return {
+      file: 'deprecationNavigatorUserAgent.md',
+      substitutions: new Map([
+        // TODO(crbug.com/1264960): Re-work format to add i18n support per:
+        // https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/public/devtools_protocol/README.md
+        ['PLACEHOLDER_message', this.issueDetails.message],
+      ]),
+      links: [],
+    };
+  }
+
+  sources(): Iterable<Protocol.Audits.SourceCodeLocation> {
+    if (this.issueDetails.sourceCodeLocation) {
+      return [this.issueDetails.sourceCodeLocation];
+    }
+    return [];
+  }
+
+  primaryKey(): string {
+    return JSON.stringify(this.issueDetails);
+  }
+
+  getKind(): IssueKind {
+    return IssueKind.Improvement;
+  }
+
+  static fromInspectorIssue(issuesModel: SDK.IssuesModel.IssuesModel, inspectorIssue: Protocol.Audits.InspectorIssue):
+      DeprecationIssueDetails[] {
+    const details = inspectorIssue.details.deprecationIssueDetails;
+    if (!details) {
+      console.warn('Deprecation issue without details received.');
+      return [];
+    }
+    return [new DeprecationIssueDetails(details, issuesModel)];
+  }
+}
