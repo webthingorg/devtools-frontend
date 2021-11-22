4652ad2e73c84bb221aa4c22d95b43607e08de38
Johan Bay <jobay@chromium.org>
2021-11-22 07:37:58 +0000

    Stop accessibiliy sidebar from clearing model
    Change-Id: I83635c2c224e4780e6e8f2132164ba88b8a44c01
    Bug: 1226486

--- front_end/panels/accessibility/AccessibilitySidebarView.ts
+++ front_end/panels/accessibility/AccessibilitySidebarView.ts
@@ -107,7 +107,6 @@ export class AccessibilitySidebarView extends UI.ThrottledWidget.ThrottledWidget
     if (!accessibilityModel) {
       return;
     }
-    accessibilityModel.clear();
     await accessibilityModel.requestPartialAXTree(node);
     this.accessibilityNodeCallback(accessibilityModel.axNodeForDOMNode(node));
   }
