cbef278d7cc9586231eadf838dd5336535f8ef4c
Ari Chivukula <arichiv@chromium.org>
2021-12-16 09:54:33 +0000

    [Client Hint Issue] (3) Implement devtools issue {DEVTOOLS}
    Now that third-party delegation via markup for client hints is in M99,
    we need to start alerting developers where they use unsupported
    features. These features will be expanded and extended as more client
    hint features launch.
    https://docs.google.com/document/d/1lDEvj8tMeuvUs1HTTqL-44YiI-7ljeQkusM_WhUfIeE/edit
    
    This CL is a part of a series:
    (1) Implement devtools issue {CHROMIUM}
    (2) Update devtools protocol {DEVTOOLS}
    (3) Implement devtools issue {DEVTOOLS}
    
    Bug: 1219359
    Change-Id: Ia8aaf05b98894fd32f5aad8f973fcdf774e1634c

---
+++ test/e2e/issues/client-hint-issues_test.ts
@@ -0,0 +1,37 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertNotNullOrUndefined, goToResource} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {ensureResourceSectionIsExpanded, expandIssue, getIssueByTitle, getResourcesElement, navigateToIssuesTab, waitForTableFromResourceSectionContents} from '../helpers/issues-helpers.js';
+
+describe('Client Hint issues test', async () => {
+  it('should display issue when Client Hints are used with invalid origin', async () => {
+    await goToResource('issues/client-hint-issue-MetaTagAllowListInvalidOrigin.html');
+    await navigateToIssuesTab();
+    await expandIssue();
+    const issueElement = await getIssueByTitle('Client Hint meta tag contained invalid origin');
+    assertNotNullOrUndefined(issueElement);
+    const section = await getResourcesElement('1 source', issueElement, '.affected-resource-label');
+    await ensureResourceSectionIsExpanded(section);
+    const expectedTableRows = [
+      ['client-hint-issue-MetaTagAllowListInvalidOrigin.html:0'],
+    ];
+    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+  });
+
+  it('should display issue when Client Hints are modified by javascript', async () => {
+    await goToResource('issues/client-hint-issue-MetaTagModifiedHTML.html');
+    await navigateToIssuesTab();
+    await expandIssue();
+    const issueElement = await getIssueByTitle('Client Hint meta tag modified by javascript');
+    assertNotNullOrUndefined(issueElement);
+    const section = await getResourcesElement('1 source', issueElement, '.affected-resource-label');
+    await ensureResourceSectionIsExpanded(section);
+    const expectedTableRows = [
+      ['client-hint-issue-MetaTagModifiedHTML.html:6'],
+    ];
+    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+  });
+});
