ad5b924107e6fab80f99c9c036c131b68a837f87
Johan Bay <jobay@chromium.org>
2021-12-27 13:40:45 +0000

    Make auto dark setting enable `prefers-color-scheme: dark`
    The auto dark mode is intended to always be used together with
    `prefers-color-scheme: dark`. This change makes the setting in the
    Renderer tab both enable auto dark and sets the prefered color scheme to
    dark. To make it clear that these two settings are intertwined, we
    disable the select box for prefers-color-scheme when auto dark mode is
    enabled.
    This change also makes the auto dark setting to a checkbox to
    simplify the UX.
    
    https: //imgur.com/a/79wNwc0
    
    Change-Id: I3917356865203b89196402a8a7d4dbfa6cbeab18
    Bug: none

--- front_end/core/sdk/EmulationModel.ts
+++ front_end/core/sdk/EmulationModel.ts
@@ -126,9 +126,21 @@ export class EmulationModel extends SDKModel<void> {
     this.updateCssMedia();
 
     const autoDarkModeSetting = Common.Settings.Settings.instance().moduleSetting('emulateAutoDarkMode');
-    autoDarkModeSetting.addChangeListener(() => this.emulateAutoDarkMode(autoDarkModeSetting.get()));
+    autoDarkModeSetting.addChangeListener(() => {
+      const enabled = autoDarkModeSetting.get();
+      mediaFeaturePrefersColorSchemeSetting.setDisabled(enabled);
+      if (enabled) {
+        mediaFeaturePrefersColorSchemeSetting.set('dark');
+        this.emulateAutoDarkMode(true);
+      } else {
+        mediaFeaturePrefersColorSchemeSetting.set('');
+        this.emulateAutoDarkMode();
+      }
+    });
     if (autoDarkModeSetting.get()) {
-      this.emulateAutoDarkMode(autoDarkModeSetting.get());
+      mediaFeaturePrefersColorSchemeSetting.setDisabled(true);
+      mediaFeaturePrefersColorSchemeSetting.set('dark');
+      this.emulateAutoDarkMode(true);
     }
 
     const visionDeficiencySetting = Common.Settings.Settings.instance().moduleSetting('emulatedVisionDeficiency');
@@ -277,21 +289,11 @@ export class EmulationModel extends SDKModel<void> {
     }
   }
 
-  private static parseAutoDarkModeSetting(setting: string): boolean|undefined {
-    switch (setting) {
-      case 'default':
-        return undefined;
-      case 'enabled':
-        return true;
-      case 'disabled':
-        return false;
-      default:
-        throw Error('unrecognized auto dark mode setting');
+  private async emulateAutoDarkMode(enabled?: boolean): Promise<void> {
+    if (enabled) {
+      this.#mediaConfiguration.set('prefers-color-scheme', 'dark');
+      await this.updateCssMedia();
     }
-  }
-
-  private async emulateAutoDarkMode(setting: string): Promise<void> {
-    const enabled = EmulationModel.parseAutoDarkModeSetting(setting);
     await this.#emulationAgent.invoke_setAutoDarkModeOverride({enabled});
   }
 
@@ -360,7 +362,7 @@ export class EmulationModel extends SDKModel<void> {
         {enabled: configuration.enabled, configuration: configuration.configuration});
   }
 
-  private updateCssMedia(): void {
+  private async updateCssMedia(): Promise<void> {
     // See the note above, where this.#mediaConfiguration is defined.
     const type = this.#mediaConfiguration.get('type') ?? '';
     const features = [
@@ -389,7 +391,7 @@ export class EmulationModel extends SDKModel<void> {
         value: this.#mediaConfiguration.get('prefers-reduced-motion') ?? '',
       },
     ];
-    this.emulateCSSMedia(type, features);
+    return this.emulateCSSMedia(type, features);
   }
 }
 
