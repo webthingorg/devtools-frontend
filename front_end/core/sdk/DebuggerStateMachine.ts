ce7bc0b32c7f26f0f58bf7545773e51e17c163e7
Kim-Anh Tran <kimanh@chromium.org>
2022-01-19 13:55:38 +0100

    Introducing a DebuggerStateMachine
    This is 1 out of 3 CLs to make sure to synchronize the
    BreakpointManager and the DebuggerModel. The DebuggerStateMachine
    will reflect in what state the debuggerModel is at the moment,
    and which actions can happen.
    
    It has the following states:
    Disabled,
    StartingUp,   -- when Debugger.enable is called
    Enabled,      -- when the response of enable returns
    ReadyToPause, -- when all cached breakpoints are set in v8
    ShuttingDown  -- when Debugger.disable is called
    Disabled      -- when the response of disable returns
    
    Bug: chromium:1229541, chromium:1133307
    Change-Id: I630a16f6e99337674a1a0acf10b7f9928212cf92

---
+++ front_end/core/sdk/DebuggerStateMachine.ts
@@ -0,0 +1,42 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Platform from '../platform/platform.js';
+
+export const enum DebuggerState {
+  ShuttingDown = 0,
+  Disabled = 1,
+  StartingUp = 2,
+  Enabled = 3,
+  ReadyToPause = 4,
+  Length = 5,
+}
+
+export class DebuggerStateMachine {
+  #state: DebuggerState;
+
+  constructor() {
+    this.#state = DebuggerState.Disabled;
+  }
+
+  get state(): DebuggerState {
+    return this.#state;
+  }
+
+  transition(newState: DebuggerState): boolean {
+    if (this.#validTransitions(this.#state, newState)) {
+      this.#state = newState;
+      return true;
+    }
+    return false;
+  }
+
+  #validTransitions(oldState: DebuggerState, newState: DebuggerState): boolean {
+    Platform.DCHECK(() => newState !== DebuggerState.Length);
+    const isTransitioningToNext = newState === (oldState + 1) % DebuggerState.Length;
+    const isShuttingDown = newState === DebuggerState.ShuttingDown &&
+        (oldState === DebuggerState.Enabled || oldState === DebuggerState.ReadyToPause);
+    return isTransitioningToNext || isShuttingDown;
+  }
+}
