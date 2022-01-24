cb1893411745ad76b1c44bea377b0bb624f3c51b
Takuto Ikuta <tikuta@chromium.org>
2022-01-24 12:34:09 +0900

    WIP: introduce devtools_fast_bundle build config
    This CL introduce build flag switching bundler from rollup.js to
    esbuild by
    * adding esbuild to npm without downloading binary packages
    * making devtools_plugin for rollup.js re-usable to esbuild
    
    On 24C/48T Z840 Linux machine, this shows following performance
    difference by using
    ```
    devtools_skip_typecheck = true
    is_debug = false
    ```
    as base build config.
    
    esbuild (devtools_fast_bundle = true)
    $ time ninja -C out/Default/
    ...
    real    0m21.174s
    user    2m47.513s
    sys     0m38.549s
    
    rollup.js (devtools_fast_bundle = false)
    $ time ninja -C out/Default/
    ...
    real    1m28.286s
    user    30m19.220s
    sys     5m36.392s
    
    
    So esbuild is 3.2x faster and use only 9.6% of machine resouce
    (user + sys) compared to rollup.js.
    
    refs:
    * https://esbuild.github.io/plugins/#on-resolve
    * https://rollupjs.org/guide/en/#resolveid
    
    Bug: 1278663
    DISABLE_THIRD_PARTY_CHECK=need to update esbuild
    Change-Id: If6b2e774f48091b0fe9c959e7ed1ed9bc2b0847c

---
+++ scripts/build/esbuild.js
@@ -0,0 +1,60 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @ts-check
+
+const path = require('path');
+
+const devtools_paths = require('../devtools_paths.js');
+const devtools_plugin = require('./devtools_plugin.js');
+
+// esbuild module uses binary in this path.
+process.env.ESBUILD_BINARY_PATH = path.join(devtools_paths.thirdPartyPath(), 'esbuild', 'esbuild').replaceAll('\\', '/');
+console.log('ESBUILD_BINARY_PATH', process.env.ESBUILD_BINARY_PATH);
+
+const entryPoints = [process.argv[2]];
+const outfile = process.argv[3];
+
+const outdir = path.dirname(outfile);
+
+const plugin = {
+  name: 'devtools-plugin',
+  setup(build) {
+    // https://esbuild.github.io/plugins/#on-resolve
+    build.onResolve({filter: /.*/}, args => {
+      const res = devtools_plugin.devtoolsPlugin(args.path, args.importer);
+      if (!res) {
+        return null;
+      }
+
+      if (res.external && res.id) {
+        return {
+          external: res.external,
+          path: './' + path.relative(outdir, res.id),
+        };
+      }
+
+      if (res.external) {
+        return {
+          external: true,
+        };
+      }
+
+      return {
+        path: res.id,
+      };
+    });
+  },
+};
+
+require('esbuild')
+    .build({
+      entryPoints,
+      outfile,
+      bundle: true,
+      format: 'esm',
+      platform: 'browser',
+      plugins: [plugin],
+    })
+    .catch(() => process.exit(1));
--- third_party/node/node.py
+++ third_party/node/node.py
@@ -44,4 +44,4 @@ if __name__ == '__main__':
         args = sys.argv[2:]
     else:
         output = subprocess.PIPE
-    RunNode(args, output)
+    RunNode(args, None)
