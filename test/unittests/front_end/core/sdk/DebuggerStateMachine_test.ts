b8219a7eb5951b3de2258cd01d0e93f242d5f70a
Kim-Anh Tran <kimanh@chromium.org>
2022-01-24 14:00:41 +0100

    Introducing a DebuggerStateMachine
    This is 1 out of 3 CLs to make sure to synchronize the
    BreakpointManager and the DebuggerModel. The DebuggerStateMachine
    will reflect in what state the debuggerModel is at the moment,
    and which actions can happen.
    
    It has the following states:
    Disabled,
    StartingUp,   -- when Debugger.enable is called
    Enabled,      -- when the response of enable returns
    ReadyToPause, -- when all cached breakpoints are set in v8
    ShuttingDown  -- when Debugger.disable is called
    Disabled      -- when the response of disable returns
    
    Bug: chromium:1229541, chromium:1133307
    Change-Id: I630a16f6e99337674a1a0acf10b7f9928212cf92

---
+++ test/unittests/front_end/core/sdk/DebuggerStateMachine_test.ts
@@ -0,0 +1,85 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
+
+const {assert} = chai;
+
+function checkInvalidTransition(
+    debuggerStateMachine: SDK.DebuggerStateMachine.DebuggerStateMachine,
+    currentState: SDK.DebuggerStateMachine.DebuggerState, toState: SDK.DebuggerStateMachine.DebuggerState): void {
+  // Invalid transition of invalid state DebuggerState.Length.
+  if (toState === SDK.DebuggerStateMachine.DebuggerState.Length) {
+    assert.throws(() => {
+      debuggerStateMachine.transition(toState);
+    });
+  } else {
+    // Invalid transition of valid state.
+    assert.isFalse(debuggerStateMachine.transition(toState));
+  }
+}
+
+describe('DebuggerStateMachine', () => {
+  const transitionCycle = [
+    SDK.DebuggerStateMachine.DebuggerState.StartingUp,
+    SDK.DebuggerStateMachine.DebuggerState.Enabled,
+    SDK.DebuggerStateMachine.DebuggerState.ShuttingDown,
+    SDK.DebuggerStateMachine.DebuggerState.Disabled,
+  ];
+
+  it('transitions states as expected if transition is valid', () => {
+    const debuggerStateMachine = new SDK.DebuggerStateMachine.DebuggerStateMachine();
+    assert.deepEqual(debuggerStateMachine.state, SDK.DebuggerStateMachine.DebuggerState.Disabled);
+    for (const newState of transitionCycle) {
+      assert.isTrue(debuggerStateMachine.transition(newState));
+      assert.deepEqual(debuggerStateMachine.state, newState);
+    }
+  });
+
+  it('disallows invalid transtions', () => {
+    const allStates = [
+      SDK.DebuggerStateMachine.DebuggerState.Enabled,
+      SDK.DebuggerStateMachine.DebuggerState.Disabled,
+      SDK.DebuggerStateMachine.DebuggerState.ShuttingDown,
+      SDK.DebuggerStateMachine.DebuggerState.StartingUp,
+      SDK.DebuggerStateMachine.DebuggerState.Length,
+    ];
+    const debuggerStateMachine = new SDK.DebuggerStateMachine.DebuggerStateMachine();
+    const validTransitions = new Map<SDK.DebuggerStateMachine.DebuggerState, SDK.DebuggerStateMachine.DebuggerState[]>([
+      [
+        SDK.DebuggerStateMachine.DebuggerState.Disabled,
+        [SDK.DebuggerStateMachine.DebuggerState.Disabled, SDK.DebuggerStateMachine.DebuggerState.StartingUp],
+      ],
+      [
+        SDK.DebuggerStateMachine.DebuggerState.StartingUp,
+        [SDK.DebuggerStateMachine.DebuggerState.StartingUp, SDK.DebuggerStateMachine.DebuggerState.Enabled],
+      ],
+      [
+        SDK.DebuggerStateMachine.DebuggerState.Enabled,
+        [
+          SDK.DebuggerStateMachine.DebuggerState.Enabled,
+          SDK.DebuggerStateMachine.DebuggerState.ShuttingDown,
+        ],
+      ],
+      [
+        SDK.DebuggerStateMachine.DebuggerState.ShuttingDown,
+        [SDK.DebuggerStateMachine.DebuggerState.ShuttingDown, SDK.DebuggerStateMachine.DebuggerState.Disabled],
+      ],
+    ]);
+    for (const validNextState of transitionCycle) {
+      const currentState = debuggerStateMachine.state;
+      for (const toState of allStates) {
+        const transitions = validTransitions.get(debuggerStateMachine.state);
+        if (!transitions || transitions.indexOf(toState) === -1) {
+          checkInvalidTransition(debuggerStateMachine, currentState, toState);
+        }
+      }
+      // Double check that we have not switched states so far.
+      assert.deepEqual(debuggerStateMachine.state, currentState);
+
+      assert.isTrue(debuggerStateMachine.transition(validNextState));
+      assert.deepEqual(debuggerStateMachine.state, validNextState);
+    }
+  });
+});
