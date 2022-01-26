abdcc553882725233e671080f77246bd5947de39
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-01-26 14:44:57 +0000

    Fix path encodings
    Bug: 1253323
    Change-Id: Iefa0517bb39d2f000f5abf1f184887325702c52a

--- front_end/models/workspace/WorkspaceImpl.ts
+++ front_end/models/workspace/WorkspaceImpl.ts
@@ -29,6 +29,7 @@
  */
 
 import * as Common from '../../core/common/common.js';
+import type * as Platform from '../../core/platform/platform.js';
 import type * as TextUtils from '../text_utils/text_utils.js';
 
 import type {UISourceCodeMetadata} from './UISourceCode.js';
@@ -182,7 +183,10 @@ export class ProjectStore {
 
   renameUISourceCode(uiSourceCode: UISourceCode, newName: string): void {
     const oldPath = uiSourceCode.url();
-    const newPath = uiSourceCode.parentURL() ? uiSourceCode.parentURL() + '/' + newName : newName;
+    const newPath = uiSourceCode.parentURL() ?
+        Common.ParsedURL.ParsedURL.urlFromParentUrlAndName(
+            uiSourceCode.parentURL() as Platform.DevToolsPath.UrlString, newName) :
+        encodeURIComponent(newName);
     const value = this.uiSourceCodesMap.get(oldPath) as {
       uiSourceCode: UISourceCode,
       index: number,
