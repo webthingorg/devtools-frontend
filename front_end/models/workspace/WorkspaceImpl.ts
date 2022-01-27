9a993d9f5bd06a579ebd33cf1e3b0081ff50ce7b
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-01-27 16:15:39 +0000

    Remove cast to UrlString from WorkspaceImpl
    Bug: 1253323
    Change-Id: Iba59eb081e5fa87e4aec315fc62dbcc995a7df91

--- front_end/models/workspace/WorkspaceImpl.ts
+++ front_end/models/workspace/WorkspaceImpl.ts
@@ -29,7 +29,6 @@
  */
 
 import * as Common from '../../core/common/common.js';
-import type * as Platform from '../../core/platform/platform.js';
 import type * as TextUtils from '../text_utils/text_utils.js';
 
 import type {UISourceCodeMetadata} from './UISourceCode.js';
@@ -184,8 +183,7 @@ export class ProjectStore {
   renameUISourceCode(uiSourceCode: UISourceCode, newName: string): void {
     const oldPath = uiSourceCode.url();
     const newPath = uiSourceCode.parentURL() ?
-        Common.ParsedURL.ParsedURL.urlFromParentUrlAndName(
-            uiSourceCode.parentURL() as Platform.DevToolsPath.UrlString, newName) :
+        Common.ParsedURL.ParsedURL.urlFromParentUrlAndName(uiSourceCode.parentURL(), newName) :
         encodeURIComponent(newName);
     const value = this.uiSourceCodesMap.get(oldPath) as {
       uiSourceCode: UISourceCode,
