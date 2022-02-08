17ddf83a7d5eec35e72da6b8c1c4b35f9f540635
Wolfgang Beyer <wolfi@chromium.org>
2022-02-08 10:51:32 +0100

    Handle response headers overrides defined in `.headers` files
    This CL enables DevTools to override a request's response headers.
    Header overrides are defined in local '.headers' files.
    
    DevTools informs the Chromium backend about which requests to intercept
    via CDP's `Fetch.enable`. The header overrides defined in '.headers'
    files are taken into account when generating these interception
    patterns.
    
    When a request is being intercepted and DevTools is notified via
    `Fetch.requestPaused`, DevTools checks if there are header overrides
    which match this request. If so, it assembles the overridden response
    headers by merging the response headers as they were received with the
    local overrides defined by the user. The merged response headers are
    then sent to the Chromium backend via `Fetch.fulfillRequest`.
    
    Note:
    - There is no proper editing experience with DevTools for '.header'
      files yet.
    - When response headers are overridden, the headers displayed in the
      Network panel still show the non-overridden response headers. This is
      being addressed in https://crrev.com/c/3443342.
    
    Design Doc:
    https://docs.google.com/document/d/1H45xqNnxx6B1oDbf3_4W-dJEAX-vDkDs0u8tE0tQ_H8/edit
    
    Bug: 1288023
    Change-Id: I07ada375b0ede7915dac8cbb39ee31ee63b3c328

---
+++ test/unittests/front_end/models/persistence/NetworkPersistenceManager_test.ts
@@ -0,0 +1,365 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+
+import * as Bindings from '../../../../../front_end/models/bindings/bindings.js';
+import * as Persistence from '../../../../../front_end/models/persistence/persistence.js';
+import * as Root from '../../../../../front_end/core/root/root.js';
+import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
+import * as Workspace from '../../../../../front_end/models/workspace/workspace.js';
+import {describeWithMockConnection} from '../../helpers/MockConnection.js';
+import {initializeGlobalVars, deinitializeGlobalVars, createTarget} from '../../helpers/EnvironmentHelpers.js';
+
+async function setUpEnvironment() {
+  const workspace = Workspace.Workspace.WorkspaceImpl.instance();
+  const forceNew = true;
+  const targetManager = SDK.TargetManager.TargetManager.instance();
+  const debuggerWorkspaceBinding =
+      Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding.instance({forceNew, targetManager, workspace});
+  const breakpointManager = Bindings.BreakpointManager.BreakpointManager.instance(
+      {forceNew: true, targetManager, workspace, debuggerWorkspaceBinding});
+  Persistence.Persistence.PersistenceImpl.instance({forceNew: true, workspace, breakpointManager});
+  const networkPersistenceManager =
+      Persistence.NetworkPersistenceManager.NetworkPersistenceManager.instance({forceNew: true, workspace});
+  return {networkPersistenceManager};
+}
+
+async function setUpHeaderOverrides() {
+  createTarget();
+  const {networkPersistenceManager} = await setUpEnvironment();
+  const uiSourceCodeMap = new Map<string, Workspace.UISourceCode.UISourceCode>();
+  const fileSystem = {
+    fileSystemPath: () => 'file:///path/to/overrides',
+    fileSystemBaseURL: 'file:///path/to/overrides/',
+    uiSourceCodeForURL: (url: string): Workspace.UISourceCode.UISourceCode | null => uiSourceCodeMap.get(url) || null,
+  } as unknown as Persistence.FileSystemWorkspaceBinding.FileSystem;
+  const globalHeaders = `
+  [
+    {
+      "applyTo": "*",
+      "headers": {
+        "age":"overridden"
+      }
+    }
+  ]
+  `;
+  const exampleHeaders = `
+  [
+    {
+      "applyTo": "index.html",
+      "headers": {
+        "index-only":"only added to index.html"
+      }
+    },
+    {
+      "applyTo": "*.css",
+      "headers": {
+        "css-only":"only added to css files"
+      }
+    },
+    {
+      "applyTo": "path/to/*.js",
+      "headers": {
+        "another-header":"only added to specific path"
+      }
+    }
+  ]
+  `;
+  const exampleSourceCode = {
+    requestContent: () => {
+      return Promise.resolve({content: exampleHeaders});
+    },
+    url: () => 'file:///path/to/overrides/www.example.com/.headers',
+    project: () => fileSystem,
+    name: () => '.headers',
+  } as unknown as Workspace.UISourceCode.UISourceCode;
+  const globalSourceCode = {
+    requestContent: () => {
+      return Promise.resolve({content: globalHeaders});
+    },
+    url: () => 'file:///path/to/overrides/.headers',
+    project: () => fileSystem,
+    name: () => '.headers',
+  } as unknown as Workspace.UISourceCode.UISourceCode;
+  const uiSourceCodeArray = [exampleSourceCode, globalSourceCode];
+  uiSourceCodeMap.set(exampleSourceCode.url(), exampleSourceCode);
+  uiSourceCodeMap.set(globalSourceCode.url(), globalSourceCode);
+  const mockProject = {
+    uiSourceCodes: () => uiSourceCodeArray,
+    id: () => 'file:///path/to/overrides',
+  } as unknown as Workspace.Workspace.Project;
+  await networkPersistenceManager.setProject(mockProject);
+  SDK.NetworkManager.MultitargetNetworkManager.instance().setInterceptionHandlerForPatterns = async () => {};
+  await networkPersistenceManager.updateInterceptionPatternsForTests();
+  return {networkPersistenceManager};
+}
+
+describeWithMockConnection('NetworkPersistenceManager', () => {
+  beforeEach(() => {
+    Root.Runtime.experiments.register(Root.Runtime.ExperimentName.HEADER_OVERRIDES, '');
+    Root.Runtime.experiments.enableForTest(Root.Runtime.ExperimentName.HEADER_OVERRIDES);
+  });
+
+  it('splits URLs into segments', async () => {
+    createTarget();
+    const {networkPersistenceManager} = await setUpEnvironment();
+    const mockProject = {
+      uiSourceCodes: () => [],
+      id: () => 'file:///path/to/overrides',
+    } as unknown as Workspace.Workspace.Project;
+    await networkPersistenceManager.setProject(mockProject);
+    assert.deepEqual(
+        networkPersistenceManager.splitUrlIntoSegments('https://www.example.com/index.html'),
+        ['', 'www.example.com', 'index.html']);
+    assert.deepEqual(
+        networkPersistenceManager.splitUrlIntoSegments('http://www.example.com/this/is/a/path/index.html'),
+        ['', 'www.example.com', 'this', 'is', 'a', 'path', 'index.html']);
+    assert.deepEqual(
+        networkPersistenceManager.splitUrlIntoSegments('http://www.example.com/this/is/a/path/'),
+        ['', 'www.example.com', 'this', 'is', 'a', 'path', 'index.html']);
+    assert.deepEqual(
+        networkPersistenceManager.splitUrlIntoSegments('http://www.example.com/this/is/a/path'),
+        ['', 'www.example.com', 'this', 'is', 'a', 'path']);
+    assert.deepEqual(
+        networkPersistenceManager.splitUrlIntoSegments('http://www.example.com/this/is/a/path/index.html?foo=bar'),
+        ['', 'www.example.com', 'this', 'is', 'a', 'path', 'index.html%3ffoo=bar']);
+    assert.deepEqual(
+        networkPersistenceManager.splitUrlIntoSegments('http://www.example.com/this/is/a/path/?foo=bar'),
+        ['', 'www.example.com', 'this', 'is', 'a', 'path', '%3ffoo=bar']);
+  });
+
+  it('merges request headers with override without overlap', async () => {
+    const {networkPersistenceManager} = await setUpHeaderOverrides();
+    const interceptedRequest = {
+      request: {
+        url: 'https://www.example.com/',
+      },
+      responseHeaders: [
+        {name: 'server', value: 'DevTools mock server'},
+      ],
+    } as SDK.NetworkManager.InterceptedRequest;
+    const expected = [
+      {name: 'server', value: 'DevTools mock server'},
+      {name: 'age', value: 'overridden'},
+      {name: 'index-only', value: 'only added to index.html'},
+    ];
+    assert.deepEqual(await networkPersistenceManager.handleHeaderInterception(interceptedRequest), expected);
+  });
+
+  it('merges request headers with override with overlap', async () => {
+    const {networkPersistenceManager} = await setUpHeaderOverrides();
+    const interceptedRequest = {
+      request: {
+        url: 'https://www.example.com/index.html',
+      },
+      responseHeaders: [
+        {name: 'server', value: 'DevTools mock server'},
+        {name: 'age', value: '1'},
+      ],
+    } as SDK.NetworkManager.InterceptedRequest;
+    const expected = [
+      {name: 'server', value: 'DevTools mock server'},
+      {name: 'age', value: 'overridden'},
+      {name: 'index-only', value: 'only added to index.html'},
+    ];
+    assert.deepEqual(await networkPersistenceManager.handleHeaderInterception(interceptedRequest), expected);
+  });
+
+  it('merges request headers with override with file type wildcard', async () => {
+    const {networkPersistenceManager} = await setUpHeaderOverrides();
+    const interceptedRequest = {
+      request: {
+        url: 'https://www.example.com/styles.css',
+      },
+      responseHeaders: [
+        {name: 'server', value: 'DevTools mock server'},
+        {name: 'age', value: '1'},
+      ],
+    } as SDK.NetworkManager.InterceptedRequest;
+    const expected = [
+      {name: 'server', value: 'DevTools mock server'},
+      {name: 'age', value: 'overridden'},
+      {name: 'css-only', value: 'only added to css files'},
+    ];
+    assert.deepEqual(await networkPersistenceManager.handleHeaderInterception(interceptedRequest), expected);
+  });
+
+  it('merges request headers with override with specific path', async () => {
+    const {networkPersistenceManager} = await setUpHeaderOverrides();
+    const interceptedRequest = {
+      request: {
+        url: 'https://www.example.com/path/to/script.js',
+      },
+      responseHeaders: [
+        {name: 'server', value: 'DevTools mock server'},
+        {name: 'age', value: '1'},
+      ],
+    } as SDK.NetworkManager.InterceptedRequest;
+    const expected = [
+      {name: 'server', value: 'DevTools mock server'},
+      {name: 'age', value: 'overridden'},
+      {name: 'another-header', value: 'only added to specific path'},
+    ];
+    assert.deepEqual(await networkPersistenceManager.handleHeaderInterception(interceptedRequest), expected);
+  });
+
+  it('merges request headers only when domain matches', async () => {
+    const {networkPersistenceManager} = await setUpHeaderOverrides();
+    const interceptedRequest = {
+      request: {
+        url: 'https://www.web.dev/index.html',
+      },
+      responseHeaders: [
+        {name: 'server', value: 'DevTools mock server'},
+      ],
+    } as SDK.NetworkManager.InterceptedRequest;
+    const expected = [
+      {name: 'server', value: 'DevTools mock server'},
+      {name: 'age', value: 'overridden'},
+    ];
+    assert.deepEqual(await networkPersistenceManager.handleHeaderInterception(interceptedRequest), expected);
+  });
+});
+
+describe('NetworkPersistenceManager', () => {
+  before(async () => {
+    await initializeGlobalVars();
+  });
+  after(async () => {
+    await deinitializeGlobalVars();
+  });
+
+  it('determines whether a URL matches a pattern', async () => {
+    const {networkPersistenceManager} = await setUpEnvironment();
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern('www.example.com/', 'www.example.com/*'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern('www.example.com/index.html', 'www.example.com/*'));
+    assert.isTrue(
+        networkPersistenceManager.doesUrlMatchPattern('www.example.com/index.html', 'www.example.com/index.html'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern('www.example.com/file', 'www.example.com/*'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern('www.example.com/file', 'www.example.com/file/*'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern('www.example.com/path/', 'www.example.com/*'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern('www.example.com/path/', 'www.example.com/path/*'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern('www.example.com/styles.css', 'www.example.com/*.css'));
+    assert.isTrue(
+        networkPersistenceManager.doesUrlMatchPattern('www.example.com/path/styles.css', 'www.example.com/*.css'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern(
+        'www.example.com/some/long/path/index.html', 'www.example.com/*/index.html'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern(
+        'www.example.com/file([{with-special$_^chars}])', 'www.example.com/file([{with-special$_^chars}])'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern(
+        'www.example.com/page.html?foo=bar', 'www.example.com/*?foo=bar'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern(
+        'www.example.com/page.html?foo=bar', 'www.example.com/page.html?foo=bar'));
+    assert.isTrue(networkPersistenceManager.doesUrlMatchPattern('www.example.com/', 'www.example.com/*.html'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern('www.example.com', 'www.example.com/*.html'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern('www.example.com', 'www.example.com/*'));
+    assert.isTrue(
+        networkPersistenceManager.doesUrlMatchPattern('www.example.com/?source=test', 'www.example.com/*.html*'));
+    assert.isFalse(
+        networkPersistenceManager.doesUrlMatchPattern('www.example.com?source=test', 'www.example.com/*.html*'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern('www.example.com/index.html', 'www.example.com/'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern('www.example.com/styles.css', 'www.example.com/*.js'));
+    assert.isFalse(
+        networkPersistenceManager.doesUrlMatchPattern('www.example.com/path.html', 'www.example.com/path/*'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern(
+        'www.example.com/index/page.html', 'www.example.com/*/index.html'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern(
+        'www.example.com/file([{with-special$_^chars}])', 'www.example.com/file([{with-special_^chars}])'));
+    assert.isFalse(networkPersistenceManager.doesUrlMatchPattern(
+        'www.example.com/page.html?foo=bar', 'www.example.com/page.html'));
+    assert.isFalse(
+        networkPersistenceManager.doesUrlMatchPattern('www.example.com/page.html?foo=bar', 'www.example.com/*.html'));
+    assert.isTrue(
+        networkPersistenceManager.doesUrlMatchPattern('www.example.com/page.html?foo=bar', 'www.example.com/*.html*'));
+  });
+
+  it('merges headers which do not overlap', async () => {
+    const {networkPersistenceManager} = await setUpEnvironment();
+    const baseHeaders = [{
+      name: 'age',
+      value: '0',
+    }];
+    const overrideHeaders = {
+      'accept-ranges': 'bytes',
+    };
+    const merged = [
+      {name: 'age', value: '0'},
+      {name: 'accept-ranges', value: 'bytes'},
+    ];
+    assert.deepEqual(networkPersistenceManager.mergeHeaders(baseHeaders, overrideHeaders), merged);
+  });
+
+  it('merges headers which overlap', async () => {
+    const {networkPersistenceManager} = await setUpEnvironment();
+    const baseHeaders = [{
+      name: 'age',
+      value: '0',
+    }];
+    const overrideHeaders = {
+      'accept-ranges': 'bytes',
+      'age': '1',
+    };
+    const merged = [
+      {name: 'age', value: '1'},
+      {name: 'accept-ranges', value: 'bytes'},
+    ];
+    assert.deepEqual(networkPersistenceManager.mergeHeaders(baseHeaders, overrideHeaders), merged);
+  });
+
+  it('generates header patterns', async () => {
+    const {networkPersistenceManager} = await setUpEnvironment();
+    const headers = `
+    [
+      {
+        "applyTo": "*",
+        "headers": {
+          "age":"0"
+        }
+      },
+      {
+        "applyTo": "page.html",
+        "headers": {
+          "age":"1"
+        }
+      },
+      {
+        "applyTo": "index.html",
+        "headers": {
+          "age":"2"
+        }
+      },
+      {
+        "applyTo": "nested/path/*.js",
+        "headers": {
+          "age":"3"
+        }
+      },
+      {
+        "applyTo": "*/path/*.js",
+        "headers": {
+          "age":"4"
+        }
+      }
+    ]
+    `;
+    const uiSourceCode = {
+      requestContent: () => {
+        return Promise.resolve({content: headers});
+      },
+      url: () => 'file:///path/to/overrides/www.example.com/.headers',
+    } as unknown as Workspace.UISourceCode.UISourceCode;
+    const expectedPatterns = [
+      'http?://www.example.com/*',
+      'http?://www.example.com/page.html',
+      'http?://www.example.com/index.html',
+      'http?://www.example.com/',
+      'http?://www.example.com/nested/path/*.js',
+      'http?://www.example.com/*/path/*.js',
+    ];
+    const returnedPatterns =
+        await networkPersistenceManager.generateHeaderPatterns(uiSourceCode, 'http?://www.example.com/.headers');
+    assert.deepEqual(Array.from(returnedPatterns).sort(), expectedPatterns.sort());
+  });
+});
