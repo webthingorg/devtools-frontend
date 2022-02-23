190cde50995db58c70ecf353431647f9d945093c
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-02-23 09:31:58 +0000

    Remove casts to EncodedPathString from ScriptSnippetFileSystem
    Also replace deprecated substr with substring in ParsedURL
    
    Bug: 1299786
    Change-Id: I8b874211d44bc203391819a4653447a67b8c2fe1

--- test/unittests/front_end/panels/sources/SourcesView_test.ts
+++ test/unittests/front_end/panels/sources/SourcesView_test.ts
@@ -7,6 +7,7 @@ const {assert} = chai;
 import * as Bindings from '../../../../../front_end/models/bindings/bindings.js';
 import * as Common from '../../../../../front_end/core/common/common.js';
 import * as Persistence from '../../../../../front_end/models/persistence/persistence.js';
+import type * as Platform from '../../../../../front_end/core/platform/platform.js';
 import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
 import * as SourceFrame from '../../../../../front_end/ui/legacy/components/source_frame/source_frame.js';
 import * as Sources from '../../../../../front_end/panels/sources/sources.js';
@@ -60,15 +61,15 @@ describe('SourcesView', () => {
     assert.isTrue(sourcesView.getSourceView(uiSourceCode) instanceof Sources.UISourceCodeFrame.UISourceCodeFrame);
 
     // Rename, but contentType stays the same
-    await uiSourceCode.rename('newName.html');
+    await uiSourceCode.rename('newName.html' as Platform.DevToolsPath.RawPathString);
     assert.isTrue(sourcesView.getSourceView(uiSourceCode) instanceof Sources.UISourceCodeFrame.UISourceCodeFrame);
 
     // Rename which changes contentType
-    await uiSourceCode.rename('image.jpg');
+    await uiSourceCode.rename('image.jpg' as Platform.DevToolsPath.RawPathString);
     assert.isTrue(sourcesView.getSourceView(uiSourceCode) instanceof SourceFrame.ImageView.ImageView);
 
     // Rename which changes contentType
-    await uiSourceCode.rename('font.woff');
+    await uiSourceCode.rename('font.woff' as Platform.DevToolsPath.RawPathString);
     assert.isTrue(sourcesView.getSourceView(uiSourceCode) instanceof SourceFrame.FontView.FontView);
   });
 });
