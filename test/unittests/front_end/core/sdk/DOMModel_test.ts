32821d9646f7331e93bce5542f5662f0ff395bc4
Kim-Anh Tran <kimanh@chromium.org>
2022-02-23 15:05:07 +0100

    [DOMModel] Request document if it hasn't happened yet
    The documentUpdated() function is called upon a DOM.documentUpdated
    CDP event. Before, we would *always* set the document to null, but
    only request the document if *we previously had a document* or
    we *already requested* a document. This changes the logic to always
    set it to null and make sure to request an update if there is not
    already an ongoing update.
    
    Bug: none
    Change-Id: I97604afbe7904970a08a54c5b6ef969e12c64963

---
+++ test/unittests/front_end/core/sdk/DOMModel_test.ts
@@ -0,0 +1,40 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertNotNullOrUndefined} from '../../../../../front_end/core/platform/platform.js';
+import type * as SDKModule from '../../../../../front_end/core/sdk/sdk.js';
+import type * as Protocol from '../../../../../front_end/generated/protocol.js';
+import {createTarget} from '../../helpers/EnvironmentHelpers.js';
+import {describeWithMockConnection} from '../../helpers/MockConnection.js';
+
+const {assert} = chai;
+
+describeWithMockConnection('DOMModel', () => {
+  let SDK: typeof SDKModule;
+  before(async () => {
+    SDK = await import('../../../../../front_end/core/sdk/sdk.js');
+  });
+  it('updates the document on an documentUpdate event', () => {
+    const parentTarget = createTarget();
+    const target = createTarget({parentTarget});
+    const domModel = target.model(SDK.DOMModel.DOMModel);
+    assertNotNullOrUndefined(domModel);
+    assertNotNullOrUndefined(domModel.agent);
+
+    // Create a fake call for the method to invoke getDocument on the agent.
+    const getDocumentStub = sinon.stub(domModel.agent, 'invoke_getDocument');
+    const root =
+        {nodeId: 1, backendNodeId: 1, nodeType: 0, nodeName: '', localName: 'test.html', nodeValue: '<html></html>'} as
+        Protocol.DOM.Node;
+    const getError = () => undefined;
+    const response = {root, getError} as Protocol.DOM.GetDocumentResponse;
+    getDocumentStub.callsFake(() => Promise.resolve(response));
+
+    assert.isTrue(getDocumentStub.notCalled);
+    assert.isNull(domModel.existingDocument());
+
+    domModel.documentUpdated();
+    assert.isTrue(getDocumentStub.calledOnce);
+  });
+});
