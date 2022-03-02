0500e8ad1291b4aa7f61caca744dac0a00c0134f
Tim van der Lippe <tvanderlippe@chromium.org>
2022-03-02 15:18:38 +0000

    Add back console autocompletion for map.get
    With the upgrade to CodeMirror 6, we didn't include the autocompletion
    for maps yet. This CL adds back for detection of `map.get(` which will
    list all available keys on the map.
    
    If the object is not a map, then the `evaluateExpression` will contain a
    `result.exceptionDetails`, which means we gracefully ignore the result.
    However, if it is map, we traverse the array and obtain all keys (which
    are strings). The autocompletion will then also include the closing
    paraenthesis, so that the result would become:
    
    ```js
    const map = new Map([["somekey", 5], ["some other key"]]);
    map.get(<auto-complete><press tab>
    map.get("somekey")
    ```
    
    The keys of the map are ordered based on the original appearance in the
    keys array to ensure a deterministic autocompletion value.
    
    Corresponding unit and e2e tests are written that verify the user story.
    
    R=bmeurer@chromium.org
    
    Bug: 1293807
    Change-Id: I53dd9559e4a7e13fe4e36e934b66a693f606ec86

--- test/e2e/console/console-autocomplete_test.ts
+++ test/e2e/console/console-autocomplete_test.ts
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {click, getBrowserAndPages, typeText, waitFor, waitForNone} from '../../shared/helper.js';
+import {click, getBrowserAndPages, tabForward, typeText, waitFor, waitForNone} from '../../shared/helper.js';
 import {beforeEach, describe, it} from '../../shared/mocha-extensions.js';
-import {CONSOLE_TAB_SELECTOR, CONSOLE_TOOLTIP_SELECTOR, focusConsolePrompt} from '../helpers/console-helpers.js';
+import {CONSOLE_TAB_SELECTOR, CONSOLE_TOOLTIP_SELECTOR, focusConsolePrompt, waitForLastConsoleMessageToHaveContent} from '../helpers/console-helpers.js';
 import {openSourcesPanel} from '../helpers/sources-helpers.js';
 
 describe('The Console Tab', async () => {
@@ -14,7 +14,7 @@ describe('The Console Tab', async () => {
     await click(CONSOLE_TAB_SELECTOR);
     await focusConsolePrompt();
 
-    await typeText('let object = {a:1, b:2};');
+    await typeText('let object = {a:1, b:2}; let map = new Map([["somekey", 5], ["some other key", 42]])');
     await frontend.keyboard.press('Enter');
 
     // Wait for the console to be usable again.
@@ -61,5 +61,69 @@ describe('The Console Tab', async () => {
 
   it('triggers autocompletion for `object[`', async () => {
     await objectAutocompleteTest('[');
+  });
+
+  describe('triggers autocompletion for maps', () => {
+    async function typeMapGetter() {
+      const {frontend} = getBrowserAndPages();
+
+      const appearPromise = waitFor(CONSOLE_TOOLTIP_SELECTOR);
+      await typeText('map.get');
+      await appearPromise;
+
+      const disappearPromise = waitForNone(CONSOLE_TOOLTIP_SELECTOR);
+      await frontend.keyboard.press('Escape');
+      await disappearPromise;
+
+      const appearPromise2 = waitFor(CONSOLE_TOOLTIP_SELECTOR);
+      await typeText('(');
+      await appearPromise2;
+
+      // The first auto-suggest result is evaluated and generates a preview, which
+      // we wait for so that we don't end the test/navigate with an open
+      // Runtime.evaluate CDP request, which causes an error. crbug.com/1134579.
+      await waitFor('.console-eager-inner-preview > span');
+    }
+
+    it('can select the first key result', async () => {
+      const {frontend} = getBrowserAndPages();
+      await typeMapGetter();
+
+      const completionPromise = waitForNone(CONSOLE_TOOLTIP_SELECTOR);
+      await tabForward();
+      await completionPromise;
+
+      await frontend.keyboard.press('Enter');
+      await frontend.waitForFunction(() => {
+        return document.querySelectorAll('.console-user-command-result').length === 2;
+      });
+
+      await waitForLastConsoleMessageToHaveContent('5');
+    });
+
+    it('can select the second key result', async () => {
+      const {frontend} = getBrowserAndPages();
+      await typeMapGetter();
+
+      // Select the second key by pressing the arrow down.
+      // Keys should be ordered by appearance in the original `Map.keys()` array
+      await frontend.keyboard.press('ArrowDown');
+
+      await frontend.waitForFunction(() => {
+        return document.querySelectorAll('.console-eager-inner-preview').length === 1 &&
+            document.querySelectorAll('.console-eager-inner-preview')[0].textContent === '42';
+      });
+
+      const completionPromise = waitForNone(CONSOLE_TOOLTIP_SELECTOR);
+      await tabForward();
+      await completionPromise;
+
+      await frontend.keyboard.press('Enter');
+      await frontend.waitForFunction(() => {
+        return document.querySelectorAll('.console-user-command-result').length === 2;
+      });
+
+      await waitForLastConsoleMessageToHaveContent('42');
+    });
   });
 });
