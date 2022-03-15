01f1a58092a7252ea593e02b99d8a32cf954ce2a
Danil Somsikov <dsv@chromium.org>
2022-03-15 13:28:07 +0100

    Demo CL showing strange build behaviour
    Bug: none
    Change-Id: Ibf96d45d1b0312ee3de587d6567ce95c74a8d0a6

---
+++ test/unittests/front_end/helpers/RealConnection.ts
@@ -0,0 +1,5 @@
+/**
+ * @fileoverview A description of this module.  What would someone
+ * new to your team want to know about the code in this file?
+ * (DO NOT SUBMIT as is; replace this comment.)
+ */
