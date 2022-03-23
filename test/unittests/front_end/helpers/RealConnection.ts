16766660fc38f1ddbb11b4931f3896c56e7cfa63
Danil Somsikov <dsv@chromium.org>
2022-03-23 12:43:07 +0100

    An option to run Karma unit tests with a fullt bootstrapped frontend and real CDP connection.
    This is needed to instantiate some classes e.g. CPUProfileView.
    
    Bug: 1306830
    Change-Id: Ib59ebc4235bfd08ef847e49f478351f2a830081a

---
+++ test/unittests/front_end/helpers/RealConnection.ts
@@ -0,0 +1,62 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Root from '../../../../front_end/core/root/root.js';
+import * as Main from '../../../../front_end/entrypoints/main/main.js';
+
+import {deinitializeGlobalVars} from './EnvironmentHelpers.js';
+
+const REMOTE_DEBUGGING_PORT = 7722;
+let hasOnly = false;
+let initialized = false;
+export let markStaticTestsLoaded: () => void;
+const staticTestsLoaded = new Promise<void>(resolve => {
+  markStaticTestsLoaded = resolve;
+});
+
+function describeBody(title: string, fn: (this: Mocha.Suite) => void) {
+  before(async function() {
+    if (initialized) {
+      return;
+    }
+    await deinitializeGlobalVars();
+    await import('../../../../front_end/entrypoints/shell/shell.js');
+    await import('../../../../front_end/panels/sensors/sensors-meta.js');
+    await import('../../../../front_end/entrypoints/inspector_main/inspector_main-meta.js');
+    const response = await fetch('/json/new');
+    const target = await response.json();
+    Root.Runtime.Runtime.setQueryParamForTesting('ws', `localhost:${REMOTE_DEBUGGING_PORT}/devtools/page/${target.id}`);
+    const main = new Main.MainImpl.MainImpl();
+    await main.readyForTest();
+    initialized = true;
+  });
+
+  describe(title, fn);
+}
+
+export function describeWithRealConnection(title: string, fn: (this: Mocha.Suite) => void) {
+  if (fn.toString().match(/(^|\s)it.only\('[^]+',.*\)/)?.length) {
+    describeWithRealConnection.only(title, fn);
+    return;
+  }
+  staticTestsLoaded
+      .then(() => {
+        if (hasOnly) {
+          return;
+        }
+        describe(`real-${title}`, () => {
+          describeBody(title, fn);
+        });
+      })
+      .catch(e => {
+        throw e;
+      });
+}
+
+describeWithRealConnection.only = function(title: string, fn: (this: Mocha.Suite) => void) {
+  hasOnly = true;
+  describe.only(`real-${title}`, () => {
+    describeBody(title, fn);
+  });
+};
