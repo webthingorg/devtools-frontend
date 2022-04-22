3a18830a4f2cf31bed861edca7753da553772c34
Wolfgang Beyer <wolfi@chromium.org>
2022-04-22 15:01:40 +0200

    add console logs
    Change-Id: I78c4f04c9304137c7a2707ca8e30483a3d5d1e40

--- test/e2e/console/issues-toolbar_test.ts
+++ test/e2e/console/issues-toolbar_test.ts
@@ -7,45 +7,51 @@ import {describe, it} from '../../shared/mocha-extensions.js';
 import {navigateToConsoleTab, waitForIssueButtonLabel} from '../helpers/console-helpers.js';
 
 describe('The Console Tab', async () => {
-  it('shows the toolbar button for no issue correctly', async () => {
-    // Navigate to page which causes no issues.
-    await goToResource('empty.html');
-    await navigateToConsoleTab();
+  // it('shows the toolbar button for no issue correctly', async () => {
+  //   // Navigate to page which causes no issues.
+  //   await goToResource('empty.html');
+  //   await navigateToConsoleTab();
 
-    await waitForIssueButtonLabel('No Issues');
-  });
+  //   await waitForIssueButtonLabel('No Issues');
+  // });
 
   // Flaky test on mac.
-  it.skipOnPlatforms(['mac'], '[crbug.com/1317582]: shows the toolbar button for one issue correctly', async () => {
+  it('shows the toolbar button for one issue correctly', async () => {
     // Navigate to page which causes a CookieIssue.
     await goToResource('console/cookie-issue.html');
     await navigateToConsoleTab();
-
-    await waitForIssueButtonLabel('1 Issue:');
-  });
-
-  // Flaky test on mac.
-  it.skipOnPlatforms(['mac'], '[crbug.com/1317582]:shows the toolbar button for two issues correctly', async () => {
-    // Navigate to page which causes two CookieIssue.
-    await goToResource('console/two-cookie-issues.html');
-    await navigateToConsoleTab();
-
-    await waitForIssueButtonLabel('2 Issues:');
-  });
-
-  it('updates the toolbar button correctly', async () => {
-    // Navigate to page which causes no issues.
-    await goToResource('empty.html');
-    await navigateToConsoleTab();
-
-    await waitForIssueButtonLabel('No Issues');
-
     const {target} = getBrowserAndPages();
     await target.evaluate(() => {
-      // Trigger a CookieIssue.
-      document.cookie = 'foo=bar;samesite=None';
+      /* eslint-disable no-console */
+      console.log('New message');
+      console.log(document.URL);
     });
 
     await waitForIssueButtonLabel('1 Issue:');
   });
+
+  // Flaky test on mac.
+  // it.skipOnPlatforms(['mac'], '[crbug.com/1317582]:shows the toolbar button for two issues correctly', async () => {
+  //   // Navigate to page which causes two CookieIssue.
+  //   await goToResource('console/two-cookie-issues.html');
+  //   await navigateToConsoleTab();
+
+  //   await waitForIssueButtonLabel('2 Issues:');
+  // });
+
+  // it('updates the toolbar button correctly', async () => {
+  //   // Navigate to page which causes no issues.
+  //   await goToResource('empty.html');
+  //   await navigateToConsoleTab();
+
+  //   await waitForIssueButtonLabel('No Issues');
+
+  //   const {target} = getBrowserAndPages();
+  //   await target.evaluate(() => {
+  //     // Trigger a CookieIssue.
+  //     document.cookie = 'foo=bar;samesite=None';
+  //   });
+
+  //   await waitForIssueButtonLabel('1 Issue:');
+  // });
 });
