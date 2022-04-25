1b414a12a063dfab322b5976a63c2ad222a89131
Jacky Hu <flameddd@gmail.com>
2022-04-25 15:41:49 +0800

    Display non-Color CSSVariable value in style autocompletions
    Previously, ONLY "Color CSSVariable value" will display in style
    autocompletions.
    
    This CL will also display "non-Color CSSVariable" value in
    autocompletions, without change list order.
    
    
    How to test:
    1. open empty page
    2. open devtools
    3. add new style to check style autocompletions work.
    
    Before v.s After screenshots:
    - https://imgur.com/a/RSxrvnh
    
    After, screen video record:
    - https://youtu.be/4seqbcXPz4I
    
    
    Note:
      If "non-Color CSSVariable value" without "max-width" limitation, will
    take too much space. The UX are bad in some case.
    
    screenshot: https://imgur.com/a/EbVgVbb
    
    
    Bug: 1285091
    Change-Id: I01aa6bea8e7802a03c6922e9b2506e44d916aa1d

--- front_end/panels/elements/StylesSidebarPane.ts
+++ front_end/panels/elements/StylesSidebarPane.ts
@@ -1265,6 +1265,10 @@ export interface StylesUpdateCompletedEvent {
   hasMatchedStyles: boolean;
 }
 
+interface CompletionResult extends UI.SuggestBox.Suggestion {
+  isCSSVariableColor?: boolean;
+}
+
 export type EventTypes = {
   [Events.InitialUpdateCompleted]: void,
   [Events.StylesUpdateCompleted]: StylesUpdateCompletedEvent,
@@ -1556,8 +1560,8 @@ export class CSSPropertyPrompt extends UI.TextPrompt.TextPrompt {
       return Promise.resolve([]);
     }
 
-    const prefixResults: UI.SuggestBox.Suggestions = [];
-    const anywhereResults: UI.SuggestBox.Suggestions = [];
+    const prefixResults: Array<CompletionResult> = [];
+    const anywhereResults: Array<CompletionResult> = [];
     if (!editingVariable) {
       this.cssCompletions.forEach(completion => filterCompletions.call(this, completion, false /* variable */));
     }
@@ -1652,10 +1656,10 @@ export class CSSPropertyPrompt extends UI.TextPrompt.TextPrompt {
 
     if (this.isColorAware && !this.isEditingName) {
       results.sort((a, b) => {
-        if (Boolean(a.subtitleRenderer) === Boolean(b.subtitleRenderer)) {
+        if (a.isCSSVariableColor && b.isCSSVariableColor) {
           return 0;
         }
-        return a.subtitleRenderer ? -1 : 1;
+        return a.isCSSVariableColor ? -1 : 1;
       });
     }
     return Promise.resolve(results);
@@ -1663,7 +1667,7 @@ export class CSSPropertyPrompt extends UI.TextPrompt.TextPrompt {
     function filterCompletions(
         this: CSSPropertyPrompt, completion: string, variable: boolean, nameValue?: boolean): void {
       const index = completion.toLowerCase().indexOf(lowerQuery);
-      const result: UI.SuggestBox.Suggestion = {
+      const result: CompletionResult = {
         text: completion,
         title: undefined,
         subtitle: undefined,
@@ -1674,6 +1678,7 @@ export class CSSPropertyPrompt extends UI.TextPrompt.TextPrompt {
         selectionRange: undefined,
         hideGhostText: undefined,
         iconElement: undefined,
+        isCSSVariableColor: false,
       };
       if (variable) {
         const computedValue =
@@ -1682,6 +1687,9 @@ export class CSSPropertyPrompt extends UI.TextPrompt.TextPrompt {
           const color = Common.Color.Color.parse(computedValue);
           if (color) {
             result.subtitleRenderer = swatchRenderer.bind(null, color);
+            result.isCSSVariableColor = true;
+          } else {
+            result.subtitleRenderer = computedValueSubtitleRenderer.bind(null, computedValue);
           }
         }
       }
@@ -1701,6 +1709,14 @@ export class CSSPropertyPrompt extends UI.TextPrompt.TextPrompt {
       swatch.renderColor(color);
       swatch.style.pointerEvents = 'none';
       return swatch;
+    }
+    function computedValueSubtitleRenderer(computedValue: string): Element {
+      const subtitleElement = document.createElement('span');
+      subtitleElement.className = 'suggestion-subtitle';
+      subtitleElement.textContent = `${computedValue}`;
+      subtitleElement.style.maxWidth = '100px';
+      subtitleElement.title = `${computedValue}`;
+      return subtitleElement;
     }
   }
 }
