36bd912939bd0f41ab3ab8b4826eb47c36352828
Kim-Anh Tran <kimanh@chromium.org>
2022-04-29 11:47:14 +0200

    [timeline] Always use top frame for text location
    Previously we were using the uiLocation's linkText, if existent, in
    order to encode the location. Because of a backend bug
    (https://crbug.com/1318635), we never were able to resolve the location
    since at least ~April 2021. The code for using the linkText would,
    however, also only work if we already had a uiLocation (which depends
    on timing), and would be racy (if the bug hadn't been introduced).
    
    This change makes the location text deterministic: use the top frame
    as a link text. Note, that this is not a link, but just a text.
    
    Bug: 1318635
    Change-Id: I5078d18d92d8b60d8c8d4253efe07f7527bb3c19

--- test/unittests/front_end/panels/timeline/TimelineUIUtils_test.ts
+++ test/unittests/front_end/panels/timeline/TimelineUIUtils_test.ts
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
+import * as TimelineModel from '../../../../../front_end/models/timeline_model/timeline_model.js';
 import * as Timeline from '../../../../../front_end/panels/timeline/timeline.js';
-import type * as SDK from '../../../../../front_end/core/sdk/sdk.js';
+import {FakeStorage} from '../../helpers/TimelineHelpers.js';
 
 const {assert} = chai;
 
@@ -100,5 +102,53 @@ describe('groupLayoutShiftsIntoClusters', () => {
 
     assert.strictEqual(shiftThree.args.data._current_cluster_id, 2);
     assert.strictEqual(shiftThree.args.data._current_cluster_score, 0.05);
+  });
+});
+
+describe('TimelineUIUtils', () => {
+  let tracingModel: SDK.TracingModel.TracingModel;
+  let process: SDK.TracingModel.Process;
+  let thread: SDK.TracingModel.Thread;
+  const SCRIPT_ID = 'SCRIPT_ID';
+
+  function createEvent(eventName: string) {
+    const event =
+        new SDK.TracingModel.Event('devtools.timeline', eventName, SDK.TracingModel.Phase.Complete, 10, thread);
+
+    event.addArgs({
+      data: {
+        stackTrace: [
+          {
+            functionName: 'test',
+            url: 'test.js',
+            scriptId: SCRIPT_ID,
+            lineNumber: 0,
+            columnNumber: 0,
+          },
+        ],
+      },
+    });
+    const data = TimelineModel.TimelineModel.TimelineData.forEvent(event);
+    data.stackTrace = event.args.data.stackTrace;
+    return event;
+  }
+
+  before(() => {
+    tracingModel = new SDK.TracingModel.TracingModel(new FakeStorage());
+    process = new SDK.TracingModel.Process(tracingModel, 1);
+    thread = new SDK.TracingModel.Thread(process, 1);
+  });
+
+  it('creates top frame location text for function calls', async () => {
+    const event = createEvent('FunctionCall');
+    assert.strictEqual(
+        'test.js:1:1', await Timeline.TimelineUIUtils.TimelineUIUtils.buildDetailsTextForTraceEvent(event));
+  });
+
+  it('creates top frame location text as a fallback', async () => {
+    // 'CPUProfile' is chosen such that we run into the 'default' case.
+    const event = createEvent('CPUProfile');
+    assert.strictEqual(
+        'test.js:1:1', await Timeline.TimelineUIUtils.TimelineUIUtils.buildDetailsTextForTraceEvent(event));
   });
 });
