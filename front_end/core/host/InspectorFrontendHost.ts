a3bfbef0eb7b468f3794e9c2444c82cbcc5bdfa0
Wolfgang Beyer <wolfi@chromium.org>
2022-05-02 17:46:08 +0200

    e2e-test for header override
    Bug: 1313436
    Change-Id: I6eb3e2ff9de336fa62648647fe48042009743d18

--- front_end/core/host/InspectorFrontendHost.ts
+++ front_end/core/host/InspectorFrontendHost.ts
@@ -55,6 +55,7 @@ const MAX_RECORDED_HISTOGRAMS_SIZE = 100;
 export class InspectorFrontendHostStub implements InspectorFrontendHostAPI {
   readonly #urlsBeingSaved: Map<Platform.DevToolsPath.RawPathString|Platform.DevToolsPath.UrlString, string[]>;
   events!: Common.EventTarget.EventTarget<EventTypes>;
+  #fileSystem: FileSystem|null = null;
 
   recordedEnumeratedHistograms: {actionName: EnumeratedHistogram, actionCode: number}[] = [];
   recordedPerformanceHistograms: {histogramName: string, duration: number}[] = [];
@@ -206,17 +207,28 @@ export class InspectorFrontendHostStub implements InspectorFrontendHostAPI {
   }
 
   requestFileSystems(): void {
+    const onFs = (fileSystem: FileSystem): void => {
+      this.#fileSystem = fileSystem;
+    };
+    window.webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024, onFs);
     this.events.dispatchEventToListeners(Events.FileSystemsLoaded, []);
   }
 
   addFileSystem(type?: string): void {
+    const fileSystem = {
+      fileSystemName: 'fSName',
+      fileSystemPath: '/overrides' as Platform.DevToolsPath.RawPathString,
+      rootURL: 'filesystem:devtools://devtools/isolated/',
+      type: 'overrides',
+    };
+    this.events.dispatchEventToListeners(Events.FileSystemAdded, {fileSystem});
   }
 
   removeFileSystem(fileSystemPath: Platform.DevToolsPath.RawPathString): void {
   }
 
   isolatedFileSystem(fileSystemId: string, registeredName: string): FileSystem|null {
-    return null;
+    return this.#fileSystem;
   }
 
   loadNetworkResource(
---
+++ test/e2e/sources/header-overrides_test.ts
@@ -0,0 +1,50 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {$$, click, enableExperiment, goToResource, pressKey, typeText, waitFor} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {navigateToNetworkTab, selectRequestByName, waitForSomeRequestsToAppear} from '../helpers/network-helpers.js';
+import {clickOnContextMenu, openSourcesPanel} from '../helpers/sources-helpers.js';
+
+const MORE_TABS_SELECTOR = '[aria-label="More tabs"]';
+const OVERRIDES_TAB_SELECTOR = '[aria-label="Overrides"]';
+const ENABLE_OVERRIDES_SELECTOR = '[aria-label="Select folder for overrides"]';
+
+describe('The Overrides Panel', async () => {
+  it('can create header overrides', async () => {
+    await enableExperiment('headerOverrides');
+    await goToResource('empty.html');
+    await openSourcesPanel();
+
+    await click(MORE_TABS_SELECTOR);
+    await click(OVERRIDES_TAB_SELECTOR);
+    await click(ENABLE_OVERRIDES_SELECTOR);
+    await clickOnContextMenu('[aria-label="overrides, fs"]', 'New file');
+    await waitFor('.being-edited');
+    await typeText('.headers\n');
+    await click('.add-block');
+    await waitFor('.editable.apply-to');
+    await typeText('*.html\n');
+    await typeText('aaa\n');
+    await typeText('bbb');
+    await pressKey('s', {control: true});
+
+    await navigateToNetworkTab('empty.html');
+    await waitForSomeRequestsToAppear(1);
+    await selectRequestByName('empty.html');
+    const networkView = await waitFor('.network-item-view');
+    const headersTabHeader = await waitFor('[aria-label=Headers][role="tab"]', networkView);
+    await click(headersTabHeader);
+    await waitFor('[aria-label=Headers][role=tab][aria-selected=true]', networkView);
+    const headersView = await waitFor('.request-headers-view');
+    const headersOutline = await $$('[role=treeitem]:not(.hidden)', headersView);
+
+    const foo = await headersOutline[5].evaluate(el => el.textContent || '');
+    assert.isTrue(foo.startsWith('Response Headers'));
+    const bar = await headersOutline[6].evaluate(el => el.textContent || '');
+    assert.strictEqual(bar, 'aaa: bbb');
+  });
+});
