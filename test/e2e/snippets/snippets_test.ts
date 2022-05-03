6329e280bdfffea8b18c2ba86ac9803e3505ac4c
Andres Olivares <andoli@chromium.org>
2022-05-03 11:29:38 -0500

    Deflake snippets e2e tests
    The 'adds an expression to watches' was reenabled recently in a deflake attempt at https://crrev.com/c/3596912. However, the test remained flaky on the mac_relase bot. This is another attempt to deflake it. I ran 100 successful consecutive execs of the test with this CL's fix applied in the mac_release bot at https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket/8815244297494067409/+/u/E2E_tests/stdout, so it is with greater confidence that it's deflaked this time
    
    Bug: 1198160
    Change-Id: Ib2d64de93a242cfa3caddae2b749ef50a35d2497

--- test/e2e/snippets/snippets_test.ts
+++ test/e2e/snippets/snippets_test.ts
@@ -4,7 +4,7 @@
 
 import {assert} from 'chai';
 
-import {getBrowserAndPages, typeText, waitFor} from '../../shared/helper.js';
+import {getBrowserAndPages, typeText, waitFor, waitForFunction} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {getCurrentConsoleMessages} from '../helpers/console-helpers.js';
 import {getAvailableSnippets, openCommandMenu, showSnippetsAutocompletion} from '../helpers/quick_open-helpers.js';
@@ -82,9 +82,15 @@ describe('Expression evaluation', () => {
     ]);
   });
 
-  it('adds an expression to watches', async () => {
-    await addSelectedTextToWatches();
-    const watchExpressions = await getWatchExpressionsValues();
+  it.repeat(200, 'adds an expression to watches', async () => {  // eslint-disable-line rulesdir/no_repeated_tests
+    const watchExpressions = await waitForFunction(async () => {
+      await addSelectedTextToWatches();
+      return await getWatchExpressionsValues();
+    });
+
+    if (!watchExpressions) {
+      assert.fail('No watch expressions found');
+    }
     const cleanWatchExpressions = watchExpressions.map(expression => expression.replace(/["]+/g, '\''));
     assert.deepEqual(cleanWatchExpressions, [
       message,
