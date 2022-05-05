a7167d08940358402ed2bcb5317887fae824daeb
Dave Tapuska <dtapuska@chromium.org>
2022-05-05 12:43:41 -0400

    Add handling of isOutermostMainFrame.
    https://chromium-review.googlesource.com/c/chromium/src/+/3625434
    added support for isOutermostMainFrame to discern the
    outermost main frame from embedded main frames.
    
    BUG=chromium:1314858
    
    Change-Id: I4aa181b2a863050d2153d29108b5d7b826d9053b

--- front_end/core/sdk/TracingModel.ts
+++ front_end/core/sdk/TracingModel.ts
@@ -203,17 +203,17 @@ export class TracingModel {
       this.#minimumRecordTimeInternal = timestamp;
     }
 
-    // Track only main thread navigation start items. This is done by tracking isLoadingMainFrame,
-    // and whether documentLoaderURL is set.
+    // Track only main thread navigation start items. This is done by tracking
+    // isOutermostMainFrame, and whether documentLoaderURL is set.
     if (payload.name === 'navigationStart') {
       const data = (payload.args.data as {
-        isLoadingMainFrame: boolean,
+        isOutermostMainFrame: boolean,
         documentLoaderURL: string,
         navigationId: string,
       } | null);
       if (data) {
-        const {isLoadingMainFrame, documentLoaderURL, navigationId} = data;
-        if (isLoadingMainFrame && documentLoaderURL !== '') {
+        const {isOutermostMainFrame, documentLoaderURL, navigationId} = data;
+        if (isOutermostMainFrame && documentLoaderURL !== '') {
           const thread = process.threadById(payload.tid);
           const navStartEvent = Event.fromPayload(payload, thread);
           this.#mainFrameNavStartTimes.set(navigationId, navStartEvent);
