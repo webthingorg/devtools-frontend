28759a34fb147446f5091d134fd43008de12087b
Wolfgang Beyer <wolfi@chromium.org>
2022-05-06 16:20:02 +0200

    e2e-test for header override
    Bug: 1313436
    Change-Id: I6eb3e2ff9de336fa62648647fe48042009743d18

---
+++ test/e2e/sources/header-overrides_test.ts
@@ -0,0 +1,88 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {
+  $,
+  $$,
+  click,
+  enableExperiment,
+  goToResource,
+  pressKey,
+  typeText,
+  waitFor,
+  waitForFunction,
+} from '../../shared/helper.js';
+// import {click, enableExperiment, goToResource, typeText, waitFor} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {navigateToNetworkTab, selectRequestByName, waitForSomeRequestsToAppear} from '../helpers/network-helpers.js';
+import {clickOnContextMenu, openSourcesPanel} from '../helpers/sources-helpers.js';
+
+const MORE_TABS_SELECTOR = '[aria-label="More tabs"]';
+const OVERRIDES_TAB_SELECTOR = '[aria-label="Overrides"]';
+const ENABLE_OVERRIDES_SELECTOR = '[aria-label="Select folder for overrides"]';
+
+describe('The Overrides Panel', async function() {
+  this.timeout(10000);
+
+  afterEach(async () => {
+    await openSourcesPanel();
+    await click('[aria-label="Clear configuration"]');
+    await waitFor(ENABLE_OVERRIDES_SELECTOR);
+  });
+
+  it('can create header overrides', async () => {
+    await enableExperiment('headerOverrides');
+    await goToResource('empty.html');
+    await openSourcesPanel();
+
+    await click(MORE_TABS_SELECTOR);
+    await click(OVERRIDES_TAB_SELECTOR);
+    await click(ENABLE_OVERRIDES_SELECTOR);
+    await waitFor('[aria-label="Clear configuration"]');
+    // await new Promise(() => {});
+
+    await clickOnContextMenu('[aria-label="overrides, fs"]', 'New file');
+    await waitFor('.being-edited');
+    await typeText('.headers\n');
+    await click('.add-block');
+    await waitFor('.editable.apply-to');
+    await typeText('*.html\n');
+    await typeText('aaa\n');
+    await typeText('bbb');
+    await waitFor('[aria-label="NewFile, file"] .tree-element-title');
+    const title = await $('[aria-label="NewFile, file"] .tree-element-title');
+    // assert.isNotNull(title);
+    let foo = await title?.evaluate(el => el.textContent);
+    assert.strictEqual(foo, '*.headers');
+
+    await pressKey('s', {control: true});
+    await waitForFunction(async () => {
+      foo = await title?.evaluate(el => el.textContent);
+      return foo === '.headers';
+    });
+
+    await navigateToNetworkTab('hello.html');
+    await waitForSomeRequestsToAppear(1);
+    await selectRequestByName('hello.html');
+    const networkView = await waitFor('.network-item-view');
+    const headersTabHeader = await waitFor('[aria-label=Headers][role="tab"]', networkView);
+    await click(headersTabHeader);
+    await waitFor('[aria-label=Headers][role=tab][aria-selected=true]', networkView);
+    const headersView = await waitFor('.request-headers-view');
+    const headersOutline = await $$('[role=treeitem]:not(.hidden)', headersView);
+    const textContents =
+        await Promise.all(headersOutline.map(line => line.evaluate(message => message.textContent || '')));
+    // const found = array1.find(element => element > 10);
+
+    // const foo = await headersOutline[5].evaluate(el => el.textContent || '');
+    // assert.isTrue(foo.startsWith('Response Headers'), `response headers heading not found: ${foo}`);
+    // const bar = await headersOutline[6].evaluate(el => el.textContent || '');
+    // assert.strictEqual(bar, 'aaa: bbb', `aaa header not found: ${bar}`);
+    assert.isTrue(textContents.includes('aaa: bbb'), `cannot find overridden header: ${textContents}`);
+
+    // await new Promise(() => {});
+  });
+});
