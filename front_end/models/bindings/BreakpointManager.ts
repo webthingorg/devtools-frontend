2b2feede1c7b4951000d3983baa8b637537f8a1a
Kim-Anh Tran <kimanh@chromium.org>
2022-05-10 10:01:47 +0200

    [BreakpointManager] Use 0 as columnNumber for breakpoints
    Use 0 as columnNumber if not defined. Reasoning:
    
    * DebuggerModel will default it to zero, let's make it explicit
    (if that columnNumber is existent in the script)
    * If defaulted to 0, we would distinguish between
    bp1: (line:0, column:undefined), and bp2: (line: 0, column:0) and
    create two different breakpoints for that, even though the
    DebuggerModel doesn't distinguish them
    * This can lead to bp1 and bp2 existing at the same time, which will
    confuse v8 (e.g., if we try to re-set a breakpoint with the same
    information, it will return a ServerError), and thus DevTools
    
    Bug: 1280621
    Change-Id: I62c467ffc1efd92a7f276003dc206a1d59224635

--- front_end/models/bindings/BreakpointManager.ts
+++ front_end/models/bindings/BreakpointManager.ts
@@ -225,7 +225,7 @@ export class BreakpointManager extends Common.ObjectWrapper.ObjectWrapper<EventT
   }
 
   private innerSetBreakpoint(
-      uiSourceCode: Workspace.UISourceCode.UISourceCode, lineNumber: number, columnNumber: number|undefined,
+      uiSourceCode: Workspace.UISourceCode.UISourceCode, lineNumber: number, columnNumber: number|undefined = 0,
       condition: string, enabled: boolean): Breakpoint {
     const itemId = BreakpointManager.breakpointStorageId(uiSourceCode.url(), lineNumber, columnNumber);
     let breakpoint = this.#breakpointByStorageId.get(itemId);
@@ -380,7 +380,7 @@ export class Breakpoint implements SDK.TargetManager.SDKModelObserver<SDK.Debugg
   readonly breakpointManager: BreakpointManager;
   urlInternal: Platform.DevToolsPath.UrlString;
   readonly #lineNumberInternal: number;
-  readonly #columnNumberInternal: number|undefined;
+  readonly #columnNumberInternal: number;
   readonly #uiLocations: Set<Workspace.UISourceCode.UILocation>;
   uiSourceCodes: Set<Workspace.UISourceCode.UISourceCode>;
   #conditionInternal!: string;
@@ -391,7 +391,7 @@ export class Breakpoint implements SDK.TargetManager.SDKModelObserver<SDK.Debugg
 
   constructor(
       breakpointManager: BreakpointManager, primaryUISourceCode: Workspace.UISourceCode.UISourceCode,
-      url: Platform.DevToolsPath.UrlString, lineNumber: number, columnNumber: number|undefined, condition: string,
+      url: Platform.DevToolsPath.UrlString, lineNumber: number, columnNumber: number, condition: string,
       enabled: boolean) {
     this.breakpointManager = breakpointManager;
     this.urlInternal = url;
@@ -565,12 +565,10 @@ export class Breakpoint implements SDK.TargetManager.SDKModelObserver<SDK.Debugg
     const removeFromStorage = !keepInStorage;
 
     // Await removing for all targets.
-    const updatePromises = [];
-    for (const modelBreakpoint of this.#modelBreakpoints.values()) {
+    await Promise.all(Array.from(this.#modelBreakpoints.values()).map((modelBreakpoint: ModelBreakpoint) => {
       modelBreakpoint.removeEventListeners();
-      updatePromises.push(modelBreakpoint);
-    }
-    await Promise.all(updatePromises);
+      return modelBreakpoint.scheduleUpdateInDebugger();
+    }));
 
     this.breakpointManager.removeBreakpoint(this, removeFromStorage);
     this.breakpointManager.targetManager.unobserveModels(SDK.DebuggerModel.DebuggerModel, this);
