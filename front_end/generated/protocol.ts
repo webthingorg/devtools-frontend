66b2ad67b340f00c6bc9836b3d70f25a94a918ab
Philip Pfaffe <pfaffe@chromium.org>
2022-05-13 10:39:17 +0000

    Update browser_protocol for setHardwareConcurrencyOverride command
    Bug: chromium:1297439
    Change-Id: Ibb856ba75756bb970e4d12f08d0fbc09dea7b429

--- front_end/generated/protocol.ts
+++ front_end/generated/protocol.ts
@@ -5464,6 +5464,13 @@ export namespace Emulation {
     imageTypes: DisabledImageType[];
   }
 
+  export interface SetHardwareConcurrencyOverrideRequest {
+    /**
+     * Hardware concurrency to report
+     */
+    hardwareConcurrency: integer;
+  }
+
   export interface SetUserAgentOverrideRequest {
     /**
      * User agent to use.
