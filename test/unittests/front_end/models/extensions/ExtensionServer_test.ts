f44f2d38159594fa41cb9a82c280751da9659ab1
Alex Rudenko <alexrudenko@chromium.org>
2022-06-01 09:29:13 +0200

    Add mimeType and stringifyStep to Recorder extension API
    Bug: 1325751
    Change-Id: I2a8c7c55cf5c8ea299a5e133725ce59e6ea69dad

--- test/unittests/front_end/models/extensions/ExtensionServer_test.ts
+++ test/unittests/front_end/models/extensions/ExtensionServer_test.ts
@@ -32,8 +32,11 @@ describe('Extensions', () => {
       async stringify(recording: object) {
         return JSON.stringify(recording);
       }
+      async stringifyStep(step: object) {
+        return JSON.stringify(step);
+      }
     }
-    await chrome.devtools?.recorder.registerRecorderExtensionPlugin(new RecorderPlugin(), 'Test');
+    await chrome.devtools?.recorder.registerRecorderExtensionPlugin(new RecorderPlugin(), 'Test', 'text/javascript');
 
     const manager = Extensions.RecorderPluginManager.RecorderPluginManager.instance();
     assert.strictEqual(manager.plugins().length, 1);
@@ -44,7 +47,12 @@ describe('Extensions', () => {
       steps: [],
     });
 
+    const stepResult = await plugin.stringify({
+      type: 'scroll',
+    });
+
     assert.strictEqual(manager.plugins().length, 1);
     assert.deepStrictEqual(result, '{"name":"test","steps":[]}');
+    assert.deepStrictEqual(stepResult, '{"type":"scroll"}');
   });
 });
