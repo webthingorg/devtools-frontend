2244e3280e0b29e21275e262c6f4931125c7dd58
Paul Irish <paulirish@chromium.org>
2022-06-02 11:38:10 -0700

    Migrate Lighthouse data-warning test from webtests to e2e
    Change-Id: Ic43ae8597fbc7bd0a7e610a78a2feeb6fa886361
    Bug: 1331155

--- test/e2e/helpers/lighthouse-helpers.ts
+++ test/e2e/helpers/lighthouse-helpers.ts
@@ -2,16 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {click, goToResource, waitFor} from '../../shared/helper.js';
+import {click, getBrowserAndPages, goToResource, waitFor} from '../../shared/helper.js';
 
-export async function navigateToLighthouseTab(path: string) {
-  await goToResource(path);
+import type {BrowserAndPages} from '../../conductor/puppeteer-state.js';
+
+export async function lighthousePanelContentIsLoaded() {
+  await waitFor('.view-container[aria-label="Lighthouse panel"]');
+}
+
+export async function navigateToLighthouseTab(path?: string): Promise<BrowserAndPages> {
   await click('#tab-lighthouse');
-  // Make sure the lighthouse start view is shown
-  await waitFor('.lighthouse-start-view');
+  await lighthousePanelContentIsLoaded();
+  if (path) {
+    await goToResource(path);
+  }
+
+  return getBrowserAndPages();
 }
 
 export async function isGenerateReportButtonDisabled() {
-  const button = await waitFor('.lighthouse-start-view .primary-button');
+  const button = await waitFor('.lighthouse-start-view-fr .primary-button');
   return button.evaluate(element => (element as HTMLButtonElement).disabled);
 }
