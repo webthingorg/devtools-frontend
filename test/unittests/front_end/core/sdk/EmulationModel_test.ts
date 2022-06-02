23033cc1d74cc02a349675b02feb7377b230d2ed
Ergün Erdoğmuş <ergunsh@chromium.org>
2022-06-02 10:23:48 +0000

    Add EmulationModel method to disable enabling touch emulation
    Bug: 1330870
    Change-Id: I2f2a94c0036ca2a49b4ad9622ca3a06415bfb764

---
+++ test/unittests/front_end/core/sdk/EmulationModel_test.ts
@@ -0,0 +1,48 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertNotNullOrUndefined} from '../../../../../front_end/core/platform/platform.js';
+import type * as SDKModule from '../../../../../front_end/core/sdk/sdk.js';
+import {createTarget} from '../../helpers/EnvironmentHelpers.js';
+import {describeWithMockConnection} from '../../helpers/MockConnection.js';
+
+const {assert} = chai;
+
+describeWithMockConnection('EmulationModel', () => {
+  let SDK: typeof SDKModule;
+  before(async () => {
+    SDK = await import('../../../../../front_end/core/sdk/sdk.js');
+  });
+
+  it('should `emulateTouch` enable touch emulation', async () => {
+    const parentTarget = createTarget();
+    const target = createTarget({parentTarget});
+    const emulationModel = target.model(SDK.EmulationModel.EmulationModel);
+    const emulationAgent = target.emulationAgent();
+    assertNotNullOrUndefined(emulationModel);
+    const spy = sinon.spy(emulationAgent, 'invoke_setTouchEmulationEnabled');
+    assert.isTrue(spy.notCalled);
+
+    await emulationModel.emulateTouch(true, true);
+
+    assert.isTrue(spy.calledOnce);
+    assert.isTrue(spy.calledWith({enabled: true, maxTouchPoints: 1}));
+  });
+
+  it('should `emulateTouch` not enable touch emulation when `setCanEmulateTouchEmulation` is called with false',
+     async () => {
+       const parentTarget = createTarget();
+       const target = createTarget({parentTarget});
+       const emulationModel = target.model(SDK.EmulationModel.EmulationModel);
+       const emulationAgent = target.emulationAgent();
+       assertNotNullOrUndefined(emulationModel);
+       const spy = sinon.spy(emulationAgent, 'invoke_setTouchEmulationEnabled');
+       assert.isTrue(spy.notCalled);
+
+       emulationModel.setCanEnableTouchEmulation(false);
+       await emulationModel.emulateTouch(true, true);
+
+       assert.isFalse(spy.called);
+     });
+});
