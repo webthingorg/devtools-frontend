350aeed51e9bb14759db35370213518511c363d2
Rouslan Solomakhin <rouslan@chromium.org>
2022-06-10 18:53:54 +0000

    Add "canmakepayment" field deprecation message
    Bug: 1317941, 1290492
    Change-Id: Iea2b7737c3bfcbe3bf44032a742d4ed82d851608

---
+++ test/e2e/resources/issues/canmakepayment.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (evt) => {
+  const canMakePaymentEvent = new CanMakePaymentEvent('canmakepayment', {
+    topOrigin: 'https://test1.example',
+    paymentRequestOrigin: 'https://test2.example',
+    methodData: [],
+    modifiers: [],
+  });
+  // Access the deprecated identity fields:
+  console.log(canMakePaymentEvent.topOrigin);
+  console.log(canMakePaymentEvent.paymentRequestOrigin);
+  console.log(canMakePaymentEvent.methodData);
+  console.log(canMakePaymentEvent.modifiers);
+});
