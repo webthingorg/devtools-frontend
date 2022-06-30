448e29d5443be48a3165d1e82cba78f92b9f6acd
Saba Khukhunashvili <khukhunashvili@google.com>
2022-06-30 22:08:28 +0000

    Added styling to Authoring Hints popup
    Bug: 1178508
    Change-Id: I447248e67b9706c92c8850b4aa4daf56dac44c15

--- front_end/panels/elements/CSSRuleValidator.ts
+++ front_end/panels/elements/CSSRuleValidator.ts
@@ -6,17 +6,96 @@ import * as i18n from '../../core/i18n/i18n.js';
 
 const UIStrings = {
   /**
-    *@description Hint for Align-content rule where element also has flex-wrap nowrap rule.
+    *@description Hint prefix for deprecated properties.
     */
-  alignContentRuleOnNoWrapFlex: 'This element has flex-wrap: nowrap rule, therefore \'align-content\' has no effect.',
+  deprecatedPropertyHintPrefix: 'Deprecated Property',
+  /**
+    *@description Hint prefix for rule validation.
+    */
+  ruleValidationHintPrefix: 'Inactive rule',
+  /**
+    *@description Hint for Align-content rule where element also has flex-wrap nowrap rule.
+    *@example {<code class="unbreakable-text">} RULE_OPENING
+    *@example {</code>} RULE_CLOSING
+    *@example {<span class="property">} PROPERTY_NAME_OPENING
+    *@example {</span>} PROPERTY_NAME_CLOSING
+    */
+  alignContentRuleOnNoWrapFlexReason: 'This element has {RULE_OPENING}{PROPERTY_NAME_OPENING}flex-wrap{PROPERTY_NAME_CLOSING}: nowrap{RULE_CLOSING} rule, therefore {RULE_OPENING}{PROPERTY_NAME_OPENING}align-content{PROPERTY_NAME_CLOSING}{RULE_CLOSING} has no effect.',
+  /**
+    *@description Possible fix for Align-content rule where element also has flex-wrap nowrap rule.
+    *@example {<code class="unbreakable-text">} RULE_OPENING
+    *@example {</code>} RULE_CLOSING
+    *@example {<span class="property">} PROPERTY_NAME_OPENING
+    *@example {</span>} PROPERTY_NAME_CLOSING
+    */
+  alignContentRuleOnNoWrapFlexPossibleFix: 'For this property to work, please remove or change the value of {RULE_OPENING}{PROPERTY_NAME_OPENING}flex-wrap{PROPERTY_NAME_CLOSING}{RULE_CLOSING} rule.',
   /**
     *@description Hint for element that does not have effect if parent container is not flex.
     *@example {flex} PH1
     */
-  notFlexItemHint: 'Parent of this element is not flex container, therefore {PH1} property has no effect.',
+  notFlexItemReason: 'Parent of this element is not flex container, therefore {PH1} property has no effect.',
+  /**
+    *@description Possible fix for element that does not have effect if parent container is not flex.
+    *@example {<code class="unbreakable-text">} RULE_OPENING
+    *@example {</code>} RULE_CLOSING
+    *@example {<span class="property">} PROPERTY_NAME_OPENING
+    *@example {</span>} PROPERTY_NAME_CLOSING
+    *@example {<span id="parent-element" class="clickable">} PARENT_ELEMENT_OPENING
+    *@example {</span>} PARENT_ELEMENT_CLOSING
+    */
+  notFlexItemPossibleFix: 'For this property to work, please add {RULE_OPENING}{PROPERTY_NAME_OPENING}display{PROPERTY_NAME_CLOSING}: flex{RULE_CLOSING} to {PARENT_ELEMENT_OPENING}parent element{PARENT_ELEMENT_CLOSING}',
 };
 const str_ = i18n.i18n.registerUIStrings('panels/elements/CSSRuleValidator.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+const DEFAULT_HINT_ARGUMENTS = {
+  'RULE_OPENING': '<code class="unbreakable-text">',
+  'RULE_CLOSING': '</code>',
+  'PROPERTY_NAME_OPENING': '<span class="property">',
+  'PROPERTY_NAME_CLOSING': '</span>',
+  'PARENT_ELEMENT_OPENING': '<span id="parent-element" class="clickable">',
+  'PARENT_ELEMENT_CLOSING': '</span>',
+};
+
+export const enum AuthoringHintType {
+  RULE_VALIDATION = 'ruleValidation',
+  DEPRECATED_PROPERTY = 'deprecatedProperty',
+}
+
+export class AuthoringHint {
+  readonly #hintType: AuthoringHintType;
+  readonly #hintMessage: string;
+  readonly #possibleFixMessage: string|null;
+  readonly #learnMore: string|null;
+
+  constructor(property: string, hintType: AuthoringHintType, hintMessage: string, possibleFixMessage: string|null, showLearnMore: boolean) {
+    this.#hintType = hintType;
+    this.#hintMessage = hintMessage;
+    this.#possibleFixMessage = possibleFixMessage;
+    this.#learnMore = showLearnMore ? property : null; // TODO: Add Goo.gle short link base url
+  }
+
+  getHintPrefix(): string {
+    switch (this.#hintType) {
+      case AuthoringHintType.RULE_VALIDATION:
+        return i18nString(UIStrings.ruleValidationHintPrefix);
+      case AuthoringHintType.DEPRECATED_PROPERTY:
+        return i18nString(UIStrings.deprecatedPropertyHintPrefix);
+    }
+  }
+
+  getHintMessage(): string {
+    return this.#hintMessage;
+  }
+
+  getPossibleFixMessage(): string|null {
+    return this.#possibleFixMessage;
+  }
+
+  getLearnMoreLink(): string|null {
+    return this.#learnMore;
+  }
+}
 
 export abstract class CSSRuleValidator {
   readonly #affectedProperties: string[];
@@ -32,7 +111,7 @@ export abstract class CSSRuleValidator {
     return this.#affectedProperties;
   }
 
-  abstract getHintMessage(propertyName: string): string;
+  abstract getAuthoringHint(propertyName: string): AuthoringHint;
 }
 
 export class AlignContentValidator extends CSSRuleValidator {
@@ -51,8 +130,14 @@ export class AlignContentValidator extends CSSRuleValidator {
     return computedStyles.get('flex-wrap') !== 'nowrap';
   }
 
-  getHintMessage(): string {
-    return i18nString(UIStrings.alignContentRuleOnNoWrapFlex);
+  getAuthoringHint(): AuthoringHint {
+    return new AuthoringHint(
+      'align-content',
+      AuthoringHintType.RULE_VALIDATION,
+      i18nString(UIStrings.alignContentRuleOnNoWrapFlexReason, DEFAULT_HINT_ARGUMENTS),
+      i18nString(UIStrings.alignContentRuleOnNoWrapFlexPossibleFix, DEFAULT_HINT_ARGUMENTS),
+      true,
+    );
   }
 }
 
@@ -70,10 +155,14 @@ export class FlexItemValidator extends CSSRuleValidator {
     return parentDisplay === 'flex' || parentDisplay === 'inline-flex';
   }
 
-  getHintMessage(property: string): string {
-    return i18nString(UIStrings.notFlexItemHint, {
-      'PH1': property,
-    });
+  getAuthoringHint(property: string): AuthoringHint {
+    return new AuthoringHint(
+      property,
+      AuthoringHintType.RULE_VALIDATION,
+      i18nString(UIStrings.notFlexItemReason, {'PH1': property, ...DEFAULT_HINT_ARGUMENTS}),
+      i18nString(UIStrings.notFlexItemPossibleFix, DEFAULT_HINT_ARGUMENTS),
+      true,
+    );
   }
 }
 
---
+++ front_end/panels/elements/components/AuthoringHintPopup.ts
@@ -0,0 +1,71 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ComponentHelpers from '../../../ui/components/helpers/helpers.js';
+import * as LitHtml from '../../../ui/lit-html/lit-html.js';
+import * as i18n from '../../../core/i18n/i18n.js';
+
+import authoringHintPopupStyles from './authoringHintPopup.css.js';
+
+const UIStrings = {
+  /**
+    *@description Hint prefix for deprecated properties.
+    */
+    learnMore: 'Learn More',
+};
+const str_ = i18n.i18n.registerUIStrings('panels/elements/components/AuthoringHintPopup.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+const {render, html, Directives} = LitHtml;
+
+interface Hint {
+    getHintPrefix(): string;
+    getHintMessage(): string;
+    getPossibleFixMessage(): string|null;
+    getLearnMoreLink(): string|null;
+}
+
+export class AuthoringHintPopup extends HTMLElement {
+    static readonly litTagName = LitHtml.literal`devtools-authoring-hints-popup`;
+    readonly #shadow = this.attachShadow({mode: 'open'});
+    readonly #authoringHint: Hint;
+
+    constructor(authoringHint: Hint) {
+        super();
+        this.#authoringHint = authoringHint;
+        this.#shadow.adoptedStyleSheets = [authoringHintPopupStyles];
+        this.#render();
+    }
+
+    #render(): void {
+        render(html`
+            <div class="hint-popup-wrapper">
+                <div class="hint-popup-reason">
+                    <strong>${this.#authoringHint.getHintPrefix()}:</strong> ${Directives.unsafeHTML(this.#authoringHint.getHintMessage())}
+                </div>
+                ${this.#authoringHint.getPossibleFixMessage() ? html`
+                    <div class="hint-popup-possible-fix">
+                        ${Directives.unsafeHTML(this.#authoringHint.getPossibleFixMessage())}
+                        ${this.#authoringHint.getLearnMoreLink() ? html`
+                            <x-link id='learn-more' href='${this.#authoringHint.getLearnMoreLink()}' class='clickable underlined unbreakable-text'}>
+                                ${i18nString(UIStrings.learnMore)}
+                            </x-link>
+                        `: ''}
+                    </div>
+                ` : ''}
+            </div>
+        `, this.#shadow, {
+            host: this,
+          });
+    }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-authoring-hints-popup', AuthoringHintPopup);
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface HTMLElementTagNameMap {
+      'devtools-authoring-hints-popup': AuthoringHintPopup;
+    }
+}
