437a03a9d896da10d2d4ba2c72dabc090cf1704d
Ted Meyer <tmathmeyer@chromium.org>
2022-07-01 12:32:24 -0700

    Better error display for the media tab
    We'd previously just been rendering errors as a string, but there is
    more data associated with an error than just it's message, including
    a stack trace and potentially other unstructured data. This change
    renders all of that information.
    
    R=victorporof
    
    Bug: 1043289
    Change-Id: Ib0f98a351b9c30d491900069369028a4bb9c5dca

--- test/e2e/helpers/media-helpers.ts
+++ test/e2e/helpers/media-helpers.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {getBrowserAndPages, goToResource, waitFor, waitForFunction, waitForMany} from '../../shared/helper.js';
+import {click, getBrowserAndPages, goToResource, waitFor, waitForFunction, waitForMany} from '../../shared/helper.js';
 
 export async function playMediaFile(media: string) {
   const {target} = getBrowserAndPages();
@@ -28,6 +28,30 @@ export async function playMediaFile(media: string) {
 
 export async function getPlayerButton() {
   return await waitFor('.player-entry-player-title');
+}
+
+export async function getPlayerErrors(count: number) {
+<<<<<<< HEAD
+<<<<<<< HEAD
+  await click(await waitFor('.player-entry-player-title'));
+  await click(await waitFor('#tab-messages'));
+  return await waitForMany('.media-message-error', count);
+=======
+  await waitFor('.player-entry-player-title');
+
+  if (count > 0) {
+    throw 'we were able to click on the messages!';
+  } else {
+    await click(await waitFor('#tab-messages'));
+    await waitFor('.media-messages-body');
+    return await waitForMany('.media-message-error', count);
+  }
+>>>>>>> 632bdc690 (errors)
+=======
+  await click(await waitFor('.player-entry-player-title'));
+  await click(await waitFor('#tab-messages'));
+  return await waitForMany('.media-message-error', count);
+>>>>>>> 8dc1299c5 (its alive!)
 }
 
 export async function getPlayerButtonText() {
--- test/e2e/media/media-tab_test.ts
+++ test/e2e/media/media-tab_test.ts
@@ -3,10 +3,23 @@
 // found in the LICENSE file.
 
 import {assert} from 'chai';
-import {goToResource} from '../../shared/helper.js';
-import {describe, it} from '../../shared/mocha-extensions.js';
 
-import {getPlayerButtonText, playMediaFile, waitForPlayerButtonTexts} from '../helpers/media-helpers.js';
+<<<<<<< HEAD
+<<<<<<< HEAD
+import {goToResource, getBrowserAndPages} from '../../shared/helper.js';
+=======
+import {goToResource} from '../../shared/helper.js';
+>>>>>>> 632bdc690 (errors)
+=======
+import {getBrowserAndPages, goToResource} from '../../shared/helper.js';
+>>>>>>> 8dc1299c5 (its alive!)
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {
+  getPlayerButtonText,
+  getPlayerErrors,
+  playMediaFile,
+  waitForPlayerButtonTexts,
+} from '../helpers/media-helpers.js';
 import {openPanelViaMoreTools} from '../helpers/settings-helpers.js';
 
 describe('Media Tab', () => {
@@ -22,4 +35,40 @@ describe('Media Tab', () => {
     await goToResource('media/codec_worker.html');
     await waitForPlayerButtonTexts(4);
   });
+
+  it('ensures that errors are rendered nicely', async () => {
+    await openPanelViaMoreTools('Media');
+<<<<<<< HEAD
+<<<<<<< HEAD
+    await goToResource(`media/corrupt.webm`);
+
+=======
+    await goToResource(`media/corrupt.webm`);
+
+
+>>>>>>> 8dc1299c5 (its alive!)
+    const {target} = getBrowserAndPages();
+    await target.evaluate(() => {
+      const videoElement = document.getElementsByName('media')[0] as HTMLVideoElement;
+      void videoElement.play();
+    });
+
+<<<<<<< HEAD
+    const errors = await getPlayerErrors(2);
+    const errorContent = await errors[1].evaluate(el => el.textContent);
+    assert.include(errorContent, 'PipelineStatus');
+  });
+
+=======
+    await playMediaFile('corrupt.webm');
+    const errorNodes = await getPlayerErrors(3);
+    assert.include(errorNodes[2].toString(), 'PipelineStatus');
+  });
+>>>>>>> 632bdc690 (errors)
+=======
+    const errors = await getPlayerErrors(3);
+    assert.include(errors[2].toString(), 'PipelineStatus');
+  });
+
+>>>>>>> 8dc1299c5 (its alive!)
 });
