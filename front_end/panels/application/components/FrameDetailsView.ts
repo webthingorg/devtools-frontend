e45fa6d65648416df2dbb26a5c263985f727193c
Robert Lin <robertlin@chromium.org>
2022-07-05 05:04:27 +0000

    prerender: Add descriptive prerender cancellation reasons to DevTools
    Bug: 1332377
    Change-Id: I9bcde38b8c5afb1272b49701b1d57528afb86a81

--- front_end/panels/application/components/FrameDetailsView.ts
+++ front_end/panels/application/components/FrameDetailsView.ts
@@ -252,6 +252,133 @@ const UIStrings = {
   *@description Label for a link to an ad script, which created the current iframe.
   */
   creatorAdScript: 'Creator Ad Script',
+  /**
+  *@description  Description text for Prerender2 cancellation status Destroyed.
+  */
+  Destroyed: 'A prerendered page was abandoned for unknown reasons.',
+  /**
+  *@description  Description text for Prerender2 cancellation status LowEndDevice.
+  */
+  LowEndDevice: 'Prerendering is not supported for low-memory devices.',
+  /**
+  *@description  Description text for Prerender2 cancellation status CrossOriginRedirect.
+  */
+  CrossOriginRedirect:
+      'Attempted to prerender a URL which redirected to a cross-origin URL. Currently prerendering cross-origin pages is disallowed.',
+  /**
+  *@description  Description text for Prerender2 cancellation status CrossOriginNavigation.
+  */
+  CrossOriginNavigation:
+      'The prerendered page navigated to a cross-origin URL after loading. Currently prerendering cross-origin pages is disallowed.',
+  /**
+  *@description  Description text for Prerender2 cancellation status InvalidSchemeRedirect.
+  */
+  InvalidSchemeRedirect:
+      'Attempted to prerender a URL that redirected to a non-HTTP(S) URL. Only HTTP(S) pages can be prerendered.',
+  /**
+  *@description  Description text for Prerender2 cancellation status InvalidSchemeNavigation.
+  */
+  InvalidSchemeNavigation: 'Only HTTP(S) navigation allowed for Prerender.',
+  /**
+  *@description  Description text for Prerender2 cancellation status NavigationRequestBlockedByCsp.
+  */
+  NavigationRequestBlockedByCsp: 'Navigation request is blocked by CSP.',
+  /**
+  *@description  Description text for Prerender2 cancellation status MainFrameNavigation.
+  */
+  MainFrameNavigation: 'Navigations after the initial prerendering navigation are disallowed',
+  /**
+  *@description  Description text for Prerender2 cancellation status MojoBinderPolicy.
+  */
+  MojoBinderPolicy: 'A disallowed API was used by the prerendered page',
+  /**
+  *@description  Description text for Prerender2 cancellation status RendererProcessCrashed.
+  */
+  RendererProcessCrashed: 'The prerendered page crashed.',
+  /**
+  *@description  Description text for Prerender2 cancellation status RendererProcessKilled.
+  */
+  RendererProcessKilled: 'The renderer process for the prerendering page was killed.',
+  /**
+  *@description  Description text for Prerender2 cancellation status Download.
+  */
+  Download: 'Download is disallowed in Prerender.',
+  /**
+  *@description  Description text for Prerender2 cancellation status TriggerDestroyed.
+  */
+  TriggerDestroyed: 'Prerender is not activated and destroyed with the trigger.',
+  /**
+  *@description  Description text for Prerender2 cancellation status NavigationNotCommitted.
+  */
+  NavigationNotCommitted: 'The prerendering page is not committed in the end.',
+  /**
+  *@description  Description text for Prerender2 cancellation status NavigationBadHttpStatus.
+  */
+  NavigationBadHttpStatus:
+      'The initial prerendering navigation was not successful due to the server returning a non-200/204/205 status code.',
+  /**
+  *@description  Description text for Prerender2 cancellation status ClientCertRequested.
+  */
+  ClientCertRequested: 'The page is requesting client cert, which is not suitable for a hidden page like prerendering.',
+  /**
+  *@description  Description text for Prerender2 cancellation status NavigationRequestNetworkError.
+  */
+  NavigationRequestNetworkError: 'Encountered a network error during prerendering.',
+  /**
+  *@description  Description text for Prerender2 cancellation status MaxNumOfRunningPrerendersExceeded.
+  */
+  MaxNumOfRunningPrerendersExceeded: 'Max number of prerendering exceeded.',
+  /**
+  *@description  Description text for Prerender2 cancellation status DidFailLoad.
+  */
+  DidFailLoad: 'DidFailLoadWithError happened during prerendering.',
+  /**
+  *@description  Description text for Prerender2 cancellation status Stop.
+  */
+  Stop: 'The tab is stopped.',
+  /**
+  *@description  Description text for Prerender2 cancellation status SslCertificateError.
+  */
+  SslCertificateError: 'Ssl certificate error.',
+  /**
+  *@description  Description text for Prerender2 cancellation status LoginAuthRequested.
+  */
+  LoginAuthRequested: 'Prerender does not support auth requests from UI.',
+  /**
+  *@description  Description text for Prerender2 cancellation status UaChangeRequiresReload.
+  */
+  UaChangeRequiresReload: 'Reload is needed after UserAgentOverride.',
+  /**
+  *@description  Description text for Prerender2 cancellation status BlockedByClient.
+  */
+  BlockedByClient: 'Resource load is blocked by the client.',
+  /**
+  *@description  Description text for Prerender2 cancellation status AudioOutputDeviceRequested.
+  */
+  AudioOutputDeviceRequested: 'Prerendering has not supported the AudioContext API yet.',
+  /**
+  *@description  Description text for Prerender2 cancellation status MixedContent.
+  */
+  MixedContent: 'Prerendering is canceled by a mixed content frame.',
+  /**
+  *@description  Description text for Prerender2 cancellation status TriggerBackgrounded.
+  */
+  TriggerBackgrounded: 'The tab is in the background',
+  /**
+  *@description  Description text for Prerender2 cancellation status EmbedderTriggeredAndSameOriginRedirected.
+  */
+  EmbedderTriggeredAndSameOriginRedirected:
+      'Prerendering triggered by Chrome internal (e.g., Omnibox prerendering) is canceled because the navigation is redirected to another same-origin page.',
+  /**
+  *@description  Description text for Prerender2 cancellation status EmbedderTriggeredAndCrossOriginRedirected.
+  */
+  EmbedderTriggeredAndCrossOriginRedirected:
+      'Prerendering triggered by Chrome internal (e.g., Omnibox prerendering) is is canceled because the navigation is redirected to another cross-origin page.',
+  /**
+  *@description  Description text for Prerender2 cancellation status EmbedderTriggeredAndDestroyed.
+  */
+  EmbedderTriggeredAndDestroyed:
+      'Prerendering triggered by Chrome internal (e.g., Omnibox prerendering) is is destroyed in the destructor.',
 };
 const str_ = i18n.i18n.registerUIStrings('panels/application/components/FrameDetailsView.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
