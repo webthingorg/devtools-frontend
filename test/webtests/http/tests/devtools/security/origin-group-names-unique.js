032980cdc420d41df7c7d3eb1da106995325176f
David Benjamin <davidben@google.com>
2022-07-13 17:26:20 -0400

    Import http/tests/devtools/security tests from Chromium
    Ideally these would be converted to one of the lighter test frameworks,
    as they're just unit testing the UI logic against expected HTML, but
    start with a simple import for now, so we can at least avoid the need
    for three-sided CLs.
    
    Modified slightly from the original versions to satisfy DevTools
    presubmits.
    
    Bug: none
    Change-Id: I4c0e0917a9c9e3f4015c3ad50d31fa8e49f8bfad

---
+++ test/webtests/http/tests/devtools/security/origin-group-names-unique.js
@@ -0,0 +1,23 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult('Tests that origin group names in the Security panel are distinct.\n');
+  await TestRunner.loadTestModule('security_test_runner');
+  await TestRunner.showPanel('security');
+
+  const originGroupNameSize = Object.keys(Security.SecurityPanelSidebarTree.OriginGroup).length;
+
+  const deduplicatedNames = new Set();
+  for (const key in Security.SecurityPanelSidebarTree.OriginGroup) {
+    const name = Security.SecurityPanelSidebarTree.OriginGroup[key];
+    deduplicatedNames.add(name);
+  }
+
+  TestRunner.addResult(
+      'Number of names (' + originGroupNameSize + ') == number of unique names (' + deduplicatedNames.size +
+      '): ' + (originGroupNameSize == deduplicatedNames.size) + ' (expected: true)');
+
+  TestRunner.completeTest();
+})();
