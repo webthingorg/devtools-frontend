00dfa0eca1a3c686e40e1f8a9fe81f42f830f92b
Philip Pfaffe <pfaffe@chromium.org>
2022-08-03 10:44:20 +0000

    stress test
    Bug: 1349290
    Change-Id: I1b75d20ae99dea11d1929e11157da0253f4736cc

--- test/e2e/sources/debug-raw-wasm_test.ts
+++ test/e2e/sources/debug-raw-wasm_test.ts
@@ -271,7 +271,7 @@ describe('Sources Tab', async function() {
   });
 
   // TODO(crbug.com/1349297) Disabled because of time out flakes.
-  it.skip('[crbug.com/1349297] is able to step with state in multi-threaded code in main thread', async () => {
+  it.only('[crbug.com/1349297] is able to step with state in multi-threaded code in main thread', async () => {
     const {target, frontend} = getBrowserAndPages();
     const fileName = 'stepping-with-state.wasm';
     await step('navigate to a page and open the Sources tab', async () => {
