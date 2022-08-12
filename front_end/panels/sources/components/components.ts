b2cc54cd2caeafd53253dd90a42e248052ce4045
Kim-Anh Tran <kimanh@chromium.org>
2022-08-12 10:42:17 +0200

    [component] Add a BreakpointView component
    This adds a BreakpointView component that renders breakpoints grouped
    by their file name.
    
    This component is _not_ visible yet within DevTools, as this is only
    adding the component itself.
    
    Screenshot: https://imgur.com/a/08qaZl4
    Doc: https://bit.ly/3pa4y92
    Bug: 1324904, 1346231
    Change-Id: Ieec31594d0c26e08adf6f114dd7cc80eddf5dc27

--- front_end/panels/sources/components/components.ts
+++ front_end/panels/sources/components/components.ts
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as BreakpointView from './BreakpointView.js';
 import * as HeadersView from './HeadersView.js';
 
 export {
+  BreakpointView,
   HeadersView,
 };
