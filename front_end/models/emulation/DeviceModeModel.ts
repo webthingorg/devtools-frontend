3e4b976c5956a815680fcb6f5b5ef04621ac300a
Arkadia Kowalczyk <arkadiak@google.com>
2022-08-12 14:25:16 +0000

    test changed comment
    test added space
    
    Change-Id: I4c2fd346d7c946c54e0201abe859355635abfc8a

--- front_end/models/emulation/DeviceModeModel.ts
+++ front_end/models/emulation/DeviceModeModel.ts
@@ -1,6 +1,6 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file. dupa
 
 import * as Common from '../../core/common/common.js';
 import * as Host from '../../core/host/host.js';
