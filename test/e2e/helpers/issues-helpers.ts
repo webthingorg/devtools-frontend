d04aa3bffd9831895d745dad28b55236b15a1bce
Philip Pfaffe <pfaffe@chromium.org>
2022-08-17 18:51:56 +0000

    DON'T SUBMIT [deps] Roll pptr to 16.0.0
    pptr 16 comes with more accurate typings, so this CL also updates type
    usages.
    
    Bug: none
    Change-Id: Ia3719c742069710a0179652ffedc2943a7b2f5dc

--- test/e2e/helpers/issues-helpers.ts
+++ test/e2e/helpers/issues-helpers.ts
@@ -80,7 +80,7 @@ async function getIssueByTitleElement(issueMessageElement: puppeteer.ElementHand
   if (header) {
     const headerClassList = await header.evaluate(el => el.classList.toString());
     assert.include(headerClassList, 'header');
-    const issue = await header.evaluateHandle(el => el.parentElement.nextSibling);
+    const issue = await header.evaluateHandle(el => el.parentElement?.nextSibling);
     if (issue) {
       return issue as puppeteer.ElementHandle<HTMLElement>;
     }
