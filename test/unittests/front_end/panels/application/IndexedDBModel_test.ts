92a4bf11c5e464d1bf0ca20a966e729e072eb078
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-08-31 11:47:35 +0000

    [IndexedDB] Prepare frontend to support storage key
    Bug: 1296584
    Change-Id: I32ad88dc94627c68bc6ec865a1eff2419e74d551

--- test/unittests/front_end/panels/application/IndexedDBModel_test.ts
+++ test/unittests/front_end/panels/application/IndexedDBModel_test.ts
@@ -9,6 +9,8 @@ import * as Resources from '../../../../../front_end/panels/application/applicat
 import {createTarget} from '../../helpers/EnvironmentHelpers.js';
 import {
   describeWithMockConnection,
+  dispatchEvent,
+  setMockConnectionResponseHandler,
 } from '../../helpers/MockConnection.js';
 import type * as ProtocolProxyApi from '../../../../../front_end/generated/protocol-proxy-api.js';
 
@@ -16,6 +18,7 @@ describeWithMockConnection('IndexedDBModel', () => {
   let indexedDBModel: Resources.IndexedDBModel.IndexedDBModel;
   let target: SDK.Target.Target;
   let indexedDBAgent: ProtocolProxyApi.IndexedDBApi;
+  let manager: SDK.StorageKeyManager.StorageKeyManager|null;
   const testKey = 'test-storage-key';
   const testDBId = new Resources.IndexedDBModel.DatabaseId(undefined, testKey, 'test-database');
 
@@ -23,6 +26,98 @@ describeWithMockConnection('IndexedDBModel', () => {
     target = createTarget();
     indexedDBModel = new Resources.IndexedDBModel.IndexedDBModel(target);
     indexedDBAgent = target.indexedDBAgent();
+    manager = target.model(SDK.StorageKeyManager.StorageKeyManager);
+    setMockConnectionResponseHandler('IndexedDB.requestDatabase', () => {
+      return {
+        databaseWithObjectStores: {
+          name: 'test-database',
+          version: 1,
+          objectStores: [
+            {
+              name: 'test-store',
+              keyPath: {
+                type: 'null',
+              },
+              autoIncrement: false,
+              indexes: [],
+            },
+          ],
+        },
+      };
+    });
+  });
+
+  it('handles StorageKeyAdded events when enabled up to requestIndexedDBNames', async () => {
+    const storageKeyAddedSpy =
+        sinon.spy(indexedDBModel, 'storageKeyAdded' as keyof Resources.IndexedDBModel.IndexedDBModel);
+    const trackIndexedDBSpy = sinon.spy(target.storageAgent(), 'invoke_trackIndexedDBForStorageKey' as never);
+    const requestIndexedDBNamesSpy = sinon.spy(indexedDBAgent, 'invoke_requestDatabaseNames');
+    const untrackIndexedDBSpy = sinon.spy(target.storageAgent(), 'invoke_untrackIndexedDBForStorageKey');
+
+    manager?.dispatchEventToListeners(SDK.StorageKeyManager.Events.StorageKeyAdded, testKey);
+    assert.isTrue(storageKeyAddedSpy.notCalled);
+
+    indexedDBModel.enable();
+    manager?.dispatchEventToListeners(SDK.StorageKeyManager.Events.StorageKeyAdded, testKey);
+    assert.isTrue(storageKeyAddedSpy.calledOnce);
+    assert.isTrue(trackIndexedDBSpy.calledOnceWith({storageKey: testKey}));
+    assert.isTrue(requestIndexedDBNamesSpy.calledOnceWith({storageKey: testKey}));
+
+    manager?.dispatchEventToListeners(SDK.StorageKeyManager.Events.StorageKeyRemoved, testKey);
+    assert.isTrue(untrackIndexedDBSpy.calledOnceWith({storageKey: testKey}));
+  });
+
+  it('handles an indexedDBListUpdated event', async () => {
+    const idbListUpdatedSpy = sinon.spy(indexedDBModel, 'indexedDBListUpdated');
+
+    indexedDBModel.enable();
+    dispatchEvent(target, 'Storage.indexedDBListUpdated', {origin: '', storageKey: testKey});
+    assert.isTrue(idbListUpdatedSpy.calledOnce);
+  });
+
+  it('handles an indexedDBContentUpdated event', async () => {
+    const eventDispatcherSpy = sinon.spy(indexedDBModel, 'dispatchEventToListeners');
+
+    indexedDBModel.enable();
+    dispatchEvent(
+        target, 'Storage.indexedDBContentUpdated',
+        {origin: '', storageKey: testKey, databaseName: 'test-database', objectStoreName: 'test-store'});
+    assert.isTrue(eventDispatcherSpy.calledOnceWith(
+        Resources.IndexedDBModel.Events.IndexedDBContentUpdated as unknown as sinon.SinonMatcher,
+        {databaseId: testDBId, objectStoreName: 'test-store', model: indexedDBModel}));
+  });
+
+  it('calls protocol methods on refreshDatabase', () => {
+    const requestDatabaseSpy = sinon.spy(indexedDBAgent, 'invoke_requestDatabase');
+
+    indexedDBModel.enable();
+    indexedDBModel.refreshDatabase(testDBId);
+    assert.isTrue(requestDatabaseSpy.calledOnceWith({storageKey: testKey, databaseName: 'test-database'}));
+  });
+
+  it('calls protocol method on loadObjectStoreData', () => {
+    const requestDataSpy = sinon.spy(indexedDBAgent, 'invoke_requestData');
+
+    indexedDBModel.enable();
+    indexedDBModel.loadObjectStoreData(testDBId, 'test-store', null, 0, 50, () => {});
+    assert.isTrue(requestDataSpy.calledOnceWith({
+      storageKey: testKey,
+      databaseName: 'test-database',
+      objectStoreName: 'test-store',
+      indexName: '',
+      skipCount: 0,
+      pageSize: 50,
+      keyRange: undefined,
+    }));
+  });
+
+  it('calls protocol method on getMetadata', () => {
+    const getMetadataSpy = sinon.spy(indexedDBAgent, 'invoke_getMetadata');
+
+    indexedDBModel.enable();
+    void indexedDBModel.getMetadata(testDBId, new Resources.IndexedDBModel.ObjectStore('test-store', null, false));
+    assert.isTrue(getMetadataSpy.calledOnceWith(
+        {storageKey: testKey, databaseName: 'test-database', objectStoreName: 'test-store'}));
   });
 
   it('calls protocol method on clearObjectStore', async () => {
@@ -42,5 +137,13 @@ describeWithMockConnection('IndexedDBModel', () => {
     void indexedDBModel.deleteEntries(testDBId, 'test-store', testKeyRange);
     assert.isTrue(deleteEntriesSpy.calledOnceWithExactly(
         {storageKey: testKey, databaseName: 'test-database', objectStoreName: 'test-store', keyRange: testKeyRange}));
+  });
+
+  it('calls protocol method on deleteDatabase', async () => {
+    const deleteDatabaseSpy = sinon.spy(indexedDBAgent, 'invoke_deleteDatabase');
+
+    indexedDBModel.enable();
+    void indexedDBModel.deleteDatabase(testDBId);
+    assert.isTrue(deleteDatabaseSpy.calledOnceWith({storageKey: testKey, databaseName: 'test-database'}));
   });
 });
