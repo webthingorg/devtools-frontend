f3e7d163c5c54e94004c1b3c1f7778500b78b2d8
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-09-12 14:44:44 +0000

    [IndexedDB] Support storage key in the views
    Bug: 1296584
    Change-Id: I32ad88dc94627c68bc6ec865a1eff2419e74d551

--- front_end/panels/application/IndexedDBModel.ts
+++ front_end/panels/application/IndexedDBModel.ts
@@ -259,6 +259,10 @@ export class IndexedDBModel extends SDK.SDKModel.SDKModel<EventTypes> implements
     this.removeOrigin(event.data);
   }
 
+  private storageKeyRemoved(event: Common.EventTarget.EventTargetEvent<string>): void {
+    this.removeStorageKey(event.data);
+  }
+
   private addOrigin(securityOrigin: string): void {
     console.assert(!this.databaseNamesBySecurityOrigin.has(securityOrigin));
     this.databaseNamesBySecurityOrigin.set(securityOrigin, new Set());
@@ -293,10 +297,6 @@ export class IndexedDBModel extends SDK.SDKModel.SDKModel<EventTypes> implements
     }
     this.databaseNamesByStorageKey.delete(storageKey);
     void this.storageAgent.invoke_untrackIndexedDBForStorageKey({storageKey});
-  }
-
-  private storageKeyRemoved(event: Common.EventTarget.EventTargetEvent<string>): void {
-    this.removeStorageKey(event.data);
   }
 
   private isValidSecurityOrigin(securityOrigin: string): boolean {
@@ -643,6 +643,10 @@ export class DatabaseId {
     this.securityOrigin = securityOrigin;
     this.storageKey = securityOrigin ? undefined : storageKey;
     this.name = name;
+  }
+
+  getOriginOrStorageKey(): string|null {
+    return this.securityOrigin ? this.securityOrigin : (this.storageKey ? this.storageKey : null);
   }
 
   equals(databaseId: DatabaseId): boolean {
