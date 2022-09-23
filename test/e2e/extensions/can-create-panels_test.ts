94a3d6cfcc5da028374868868f6038ac46d5741c
Philip Pfaffe <pfaffe@chromium.org>
2022-09-23 11:49:49 +0000

    [extensions] Properly put together resource paths
    Blindly concatenating resource URLs for e.g. panel pages or icons can
    lead to double slashes in the URL. This change introduced proper URL
    concatenation instead and will report an error if extensions try to
    use an absolute URL.
    
    Fixed: 1364293
    Change-Id: I43d2b214dd90e2c542ec3c5d42b6961e719ee7bf

---
+++ test/e2e/extensions/can-create-panels_test.ts
@@ -0,0 +1,70 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {expectError} from '../../conductor/events.js';
+import {
+  getBrowserAndPages,
+  waitFor,
+  waitForAria,
+  waitForAriaNone,
+  waitForFunction,
+} from '../../shared/helper.js';
+import {loadExtension} from '../helpers/extension-helpers.js';
+
+describe('The Extension API', async () => {
+  it('can create panels with callbacks', async () => {
+    const extension = await loadExtension('TestExtension');
+
+    const callbackArgs = await extension.evaluate(
+        () => new Promise<string>(
+            r => window.chrome.devtools.panels.create('title', '', '', (...args) => r(JSON.stringify(args)))));
+
+    assert.deepEqual(callbackArgs, '[{"onShown":{},"onHidden":{},"onSearch":{}}]');
+  });
+
+  it('handles resource paths correctly', async () => {
+    const extension = await loadExtension('TestExtension');
+
+    const error = expectError(
+        'Extension server error: Invalid argument page: Resources paths cannot point to non-extension resources');
+    // absolute URLs should report an error and not create a panel view
+    await extension.evaluate(async () => {
+      await new Promise(r => window.chrome.devtools.panels.create('extension-tab-title', '', 'http://example.com', r));
+    });
+    await waitForFunction(async () => error.caught);
+    await waitForAriaNone('extension-tab-title');
+
+    const {frontend} = getBrowserAndPages();
+    // We don't have a real extension host, so the extension origin will match the frontend
+    const origin = await frontend.evaluate(() => document.location.origin);
+
+    {
+      // absolute URL paths work
+      await extension.evaluate(async () => {
+        await new Promise(r => window.chrome.devtools.panels.create('extension-tab-title', '', '/blank.html', r));
+      });
+      const header = await waitForAria('extension-tab-title');
+      await header.click();
+      const panel = await waitForAria('extension-tab-title panel');
+      const page = await waitFor('iframe', panel);
+      const target = await page.evaluate(e => (e as HTMLIFrameElement).src);
+      assert.strictEqual(target, `${origin}/blank.html`);
+    }
+
+    {
+      // relative URL paths work
+      await extension.evaluate(async () => {
+        await new Promise(r => window.chrome.devtools.panels.create('extension-tab-title', '', '/blank.html', r));
+      });
+      const header = await waitForAria('extension-tab-title');
+      await header.click();
+      const panel = await waitForAria('extension-tab-title panel');
+      const page = await waitFor('iframe', panel);
+      const target = await page.evaluate(e => (e as HTMLIFrameElement).src);
+      assert.strictEqual(target, `${origin}/blank.html`);
+    }
+  });
+});
