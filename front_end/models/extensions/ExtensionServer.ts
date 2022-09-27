4dbf7240ff5680a9f0da3d4617364e96199ef8b6
Danil Somsikov <dsv@chromium.org>
2022-09-27 16:47:48 +0200

    Skip file URLs in chrome.devtools.inspectedWindow.getResources if the extensions doesn't have file access.
    Chromium counterpart at crrev.com/c/3921760
    
    Bug: 1349146
    Change-Id: I44fa9d13f7a8ebf2c3cde89e8e92a23686c49566

--- front_end/models/extensions/ExtensionServer.ts
+++ front_end/models/extensions/ExtensionServer.ts
@@ -701,7 +701,10 @@ export class ExtensionServer extends Common.ObjectWrapper.ObjectWrapper<EventTyp
     return {url: contentProvider.contentURL(), type: contentProvider.contentType().name()};
   }
 
-  private onGetPageResources(): {url: string, type: string}[] {
+  private onGetPageResources(message: PrivateAPI.ExtensionServerRequestMessage): {url: string, type: string}[] {
+    if (message.command !== PrivateAPI.Commands.GetPageResources) {
+      return this.status.E_BADARG('command', `expected ${PrivateAPI.Commands.GetPageResources}`);
+    }
     const resources = new Map<unknown, {
       url: string,
       type: string,
@@ -709,6 +712,9 @@ export class ExtensionServer extends Common.ObjectWrapper.ObjectWrapper<EventTyp
 
     function pushResourceData(
         this: ExtensionServer, contentProvider: TextUtils.ContentProvider.ContentProvider): boolean {
+      if (!message.allowFileAccess && contentProvider.contentURL().startsWith('file://')) {
+        return false;
+      }
       if (!resources.has(contentProvider.contentURL())) {
         resources.set(contentProvider.contentURL(), this.makeResource(contentProvider));
       }
