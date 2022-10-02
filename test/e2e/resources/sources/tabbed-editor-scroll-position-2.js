381d4ab10086027ecca3f0cc5958b9d900184bb4
Yoichiro Hibara <hibarayoichiro871@gmail.com>
2022-10-02 13:30:53 +0900

    Show why Chrome uses a specific transport protocol for HTTP semantics in the DevTools.
    Note that this CL is transferred from https://crrev.com/c/3889955.
    
    In some cases, Chrome fails to use HTTP/3 even when the server
    supports HTTP/3, so it'll be useful to show the reason in the DevTools.
    
    Design Docs is here:
    https://docs.google.com/document/d/1SfMy7MMTbSn2C1RkPxnlnQdj9AnZ5-fQdEl0XO1s2ZQ/edit?usp=sharing
    
    DISABLE_THIRD_PARTY_CHECK=tentative
    
    Bug:1345536
    Change-Id: I5d1e03a12762812dce8b281b60d6697f9a215015

--- test/e2e/resources/sources/tabbed-editor-scroll-position-2.js
+++ test/e2e/resources/sources/tabbed-editor-scroll-position-2.js
@@ -1,5 +1,5 @@
 function thereAreMultipleLinesToScrollTo() {
-  let a = 'first line';
-  let b = 'second line';
-  let c = 'third line';
+    let a = 'first line';
+    let b = 'second line';
+    let c = 'third line';
 }
