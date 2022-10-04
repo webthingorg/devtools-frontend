fb7a9ccdc95c8b6bb3fdf994e62b9823d1a1103c
Wolfgang Beyer <wolfi@chromium.org>
2022-10-04 14:10:41 +0000

    Introduce `HeaderOverridesManager`
    This moves the logic for creating the initial state of the header
    overrides editor in the network panel to a separate file.
    
    Bug: 1297533
    Change-Id: Ifb8f5df5990164627a8b6920ea3e02808411740e

--- test/unittests/front_end/panels/network/components/HeaderSectionRow_test.ts
+++ test/unittests/front_end/panels/network/components/HeaderSectionRow_test.ts
@@ -22,7 +22,7 @@ const coordinator = Coordinator.RenderCoordinator.RenderCoordinator.instance();
 
 const {assert} = chai;
 
-async function renderHeaderSectionRow(header: NetworkComponents.HeaderSectionRow.HeaderDescriptor):
+async function renderHeaderSectionRow(header: NetworkComponents.HeaderOverridesManager.HeaderDescriptor):
     Promise<NetworkComponents.HeaderSectionRow.HeaderSectionRow> {
   const component = new NetworkComponents.HeaderSectionRow.HeaderSectionRow();
   renderElementIntoDOM(component);
@@ -33,7 +33,7 @@ async function renderHeaderSectionRow(header: NetworkComponents.HeaderSectionRow
 
 describeWithEnvironment('HeaderSectionRow', () => {
   it('emits UMA event when a header value is being copied', async () => {
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('some-header-name'),
       value: 'someHeaderValue',
     };
@@ -50,7 +50,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
   });
 
   it('renders detailed reason for blocked requests', async () => {
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('cross-origin-resource-policy'),
       value: null,
       headerNotSet: true,
@@ -93,7 +93,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
   });
 
   it('displays decoded "x-client-data"-header', async () => {
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('x-client-data'),
       value: 'CJa2yQEIpLbJAQiTocsB',
     };
@@ -114,7 +114,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
   });
 
   it('displays info about blocked "Set-Cookie"-headers', async () => {
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('set-cookie'),
       value: 'secure=only; Secure',
       setCookieBlockedReasons:
@@ -143,7 +143,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
   });
 
   it('can be highlighted', async () => {
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('some-header-name'),
       value: 'someHeaderValue',
       highlight: true,
@@ -157,7 +157,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
   it('allows editing header name and header value', async () => {
     const headerName = Platform.StringUtilities.toLowerCaseString('some-header-name');
     const headerValue = 'someHeaderValue';
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: headerName,
       value: headerValue,
       nameEditable: true,
@@ -201,7 +201,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
 
   it('does not allow setting an emtpy header name', async () => {
     const headerName = Platform.StringUtilities.toLowerCaseString('some-header-name');
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: headerName,
       value: 'someHeaderValue',
       nameEditable: true,
@@ -228,7 +228,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
 
   it('resets edited value on escape key', async () => {
     const originalHeaderValue = 'someHeaderValue';
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('some-header-name'),
       value: originalHeaderValue,
       valueEditable: true,
@@ -253,7 +253,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
   });
 
   it('confirms edited value and exits editing mode on "Enter"-key', async () => {
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('some-header-name'),
       value: 'someHeaderValue',
       valueEditable: true,
@@ -281,7 +281,7 @@ describeWithEnvironment('HeaderSectionRow', () => {
   });
 
   it('removes formatting for pasted content', async () => {
-    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+    const headerData: NetworkComponents.HeaderOverridesManager.HeaderDescriptor = {
       name: Platform.StringUtilities.toLowerCaseString('some-header-name'),
       value: 'someHeaderValue',
       valueEditable: true,
