171d2bddd795dadb1bbab91a806893713307c3f1
cammie <cammie@chromium.org>
2022-10-25 15:01:50 -0400

    (Practice run:) Update protocol
    Change-Id: I0ce68a3078f1d25f8d7c0e4df54a8aad00a23a8f

--- front_end/generated/protocol-mapping.d.ts
+++ front_end/generated/protocol-mapping.d.ts
@@ -3536,6 +3536,20 @@ export namespace ProtocolMapping {
       returnType: void;
     };
     /**
+     * Gets metadata for an origin's shared storage.
+     */
+    'Storage.getSharedStorageMetadata': {
+      paramsType: [Protocol.Storage.GetSharedStorageMetadataRequest];
+      returnType: Protocol.Storage.GetSharedStorageMetadataResponse;
+    };
+    /**
+     * Gets the entries in an given origin's shared storage.
+     */
+    'Storage.getSharedStorageEntries': {
+      paramsType: [Protocol.Storage.GetSharedStorageEntriesRequest];
+      returnType: Protocol.Storage.GetSharedStorageEntriesResponse;
+    };
+    /**
      * Returns information about the system.
      */
     'SystemInfo.getInfo': {
