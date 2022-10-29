5688ab9d7e3eb2a82eec9dff5c0a5d65bed2eaf9
Yisi <yiorsi@gmail.com>
2022-10-29 21:51:08 +0800

    Show popover when CSS variable hover
    Bug: None
    Change-Id: Ib6985d25fad415028ff3905201a44a3dda4e32c2

--- front_end/ui/legacy/components/inline_editor/CSSVarSwatch.ts
+++ front_end/ui/legacy/components/inline_editor/CSSVarSwatch.ts
@@ -4,6 +4,7 @@
 
 import * as i18n from '../../../../core/i18n/i18n.js';
 import * as Platform from '../../../../core/platform/platform.js';
+// import * as UI from '../../../../ui/legacy/legacy.js';
 import * as ComponentHelpers from '../../../components/helpers/helpers.js';
 import * as LitHtml from '../../../lit-html/lit-html.js';
 
@@ -43,6 +44,7 @@ export class CSSVarSwatch extends HTMLElement {
   private computedValue: string|null = null;
   private fromFallback: boolean = false;
   private onLinkActivate: (varialeName: string, event: MouseEvent|KeyboardEvent) => void = () => undefined;
+  // private varPopoverHelper!: UI.PopoverHelper.PopoverHelper;
 
   constructor() {
     super();
@@ -114,7 +116,7 @@ export class CSSVarSwatch extends HTMLElement {
     return '';
   }
 
-  private renderLink(variableName: string): LitHtml.TemplateResult {
+  private renderLink(variableName: string): {html: LitHtml.TemplateResult, title: string} {
     const isDefined = this.computedValue && !this.fromFallback;
 
     const classes = Directives.classMap({
@@ -125,8 +127,13 @@ export class CSSVarSwatch extends HTMLElement {
     // The this.variableName's space must be removed, otherwise it cannot be triggered when clicked.
     const onActivate = isDefined ? this.onLinkActivate.bind(this, this.variableName.trim()) : null;
 
-    return html`<span class=${classes} title=${title} @mousedown=${onActivate} @keydown=${
-        onActivate} role="link" tabindex="-1">${variableName}</span>`;
+    return {
+      // We added var popover, so don't need the title attribute anymore and
+      // only provide the data-title for the popover to get the data.
+      html: html`<span class=${classes} data-title=${title} @mousedown=${onActivate} @keydown=${
+          onActivate} role="link" tabindex="-1">${variableName}</span>`,
+      title: title || '',
+    };
   }
 
   private render(): void {
@@ -135,16 +142,38 @@ export class CSSVarSwatch extends HTMLElement {
       render('', this.shadow, {host: this});
       return;
     }
-
-    const variableNameLink = this.renderLink(functionParts.variableName);
+    const variableName = functionParts.variableName;
+    const variableNameLink = this.renderLink(variableName);
     const fallbackIncludeComma = functionParts.fallbackIncludeComma ? functionParts.fallbackIncludeComma : '';
 
     // Disabled until https://crbug.com/1079231 is fixed.
     // clang-format off
     render(
-      html`<span title=${this.computedValue || ''}>${functionParts.pre}${variableNameLink}${fallbackIncludeComma}${functionParts.post}</span>`,
+      html`<span data-title=${this.computedValue || ''}>${functionParts.pre}${variableNameLink.html}${fallbackIncludeComma}${functionParts.post}</span>`,
       this.shadow, { host: this });
     // clang-format on
+
+    // Render Popover
+    // const cssVarLink = this.shadow.querySelector('.css-var-link') as HTMLElement;
+    // this.varPopoverHelper = new UI.PopoverHelper.PopoverHelper(cssVarLink, event => {
+    //   const anchor = (event.target as HTMLElement);
+    //   if (!anchor) {
+    //     return null;
+    //   }
+
+    //   return {
+    //     box: anchor.boxInWindow(),
+    //     show: async(popover: UI.GlassPane.GlassPane): Promise<boolean> => {
+    //       const popupElement = document.createElement('div');
+    //       popupElement.innerText = variableNameLink.title;
+    //       popupElement.style.padding = '.2em .3em';
+    //       popover.contentElement.insertAdjacentElement('beforeend', popupElement);
+    //       return true;
+    //     },
+    //   };
+    // });
+    // this.varPopoverHelper.setTimeout(200, 110);
+    // this.varPopoverHelper.setHasPadding(true);
   }
 }
 
