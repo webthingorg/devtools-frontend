743efdd400cc759cb7334b7859c954b2014747cb
cammie <cammie@chromium.org>
2022-10-31 18:16:31 -0400

    Shared Storage: DevTools Frontend Prototype.
    Bug:1372007,1218540
    Change-Id: Id1bdfa8a7db437045743334713dfcc3a17a7139d

---
+++ front_end/panels/application/SharedStorageItemsView.ts
@@ -0,0 +1,271 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+ * Copyright (C) 2008 Nokia Inc.  All rights reserved.
+ * Copyright (C) 2013 Samsung Electronics. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import * as Common from '../../core/common/common.js';
+import * as i18n from '../../core/i18n/i18n.js';
+import type * as Protocol from '../../generated/protocol.js';
+import * as TextUtils from '../../models/text_utils/text_utils.js';
+import * as DataGrid from '../../ui/legacy/components/data_grid/data_grid.js';
+import type * as Platform from '../../core/platform/platform.js';
+import * as SourceFrame from '../../ui/legacy/components/source_frame/source_frame.js';
+import * as UI from '../../ui/legacy/legacy.js';
+
+import {SharedStorage} from './components/SharedStorageModel.js';
+import {StorageItemsView} from './StorageItemsView.js';
+import {SharedStorageMetadataView} from './components/SharedStorageMetadataView.js';
+
+const UIStrings = {
+  /**
+  *@description Text in SharedStorage Items View of the Application panel
+  */
+  sharedStorage: 'Shared Storage',
+  /**
+  *@description Text in SharedStorage Items View of the Application panel
+  */
+  key: 'Key',
+  /**
+  *@description Text for the value of something
+  */
+  value: 'Value',
+  /**
+  *@description Name for the "Shared Storage Items" table that shows the content of the Shared Storage.
+  */
+  sharedStorageItems: 'Shared Storage Items',
+  /**
+   *@description Text for announcing that the "Shared Storage Items" table was cleared, that is, all
+   * entries were deleted.
+   */
+  sharedStorageItemsCleared: 'Shared Storage items cleared',
+  /**
+  *@description Text in SharedStorage Items View of the Application panel
+  */
+  selectAValueToPreview: 'Select a value to preview',
+  /**
+   *@description Text for announcing a Shared Storage key/value item has been deleted
+   */
+  sharedStorageItemDeleted: 'The storage item was deleted.',
+  /**
+   *@description Text for announcing number of entries after filtering
+   *@example {5} PH1
+   */
+  sharedStorageNumberEntries: 'Number of entries shown in table: {PH1}',
+};
+const str_ = i18n.i18n.registerUIStrings('panels/application/SharedStorageItemsView.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+export class SharedStorageItemsView extends StorageItemsView {
+  private sharedStorage: SharedStorage;
+  private metadataView: SharedStorageMetadataView;
+  private dataGrid: DataGrid.DataGrid.DataGridImpl<unknown>;
+  private readonly outerSplitWidget: UI.SplitWidget.SplitWidget;
+  private readonly previewPanel: UI.Widget.VBox;
+  private preview: UI.Widget.Widget|null;
+  private previewValue: string|null;
+  private eventListeners: Common.EventTarget.EventDescriptor[];
+
+  constructor(sharedStorage: SharedStorage) {
+    super(i18nString(UIStrings.sharedStorage), 'sharedStoragePanel');
+
+    this.sharedStorage = sharedStorage;
+
+    this.element.classList.add('storage-view', 'table');
+
+    const columns = ([
+      {id: 'key', title: i18nString(UIStrings.key), sortable: false, editable: false, longText: true, weight: 50},
+      {id: 'value', title: i18nString(UIStrings.value), sortable: false, editable: false, longText: true, weight: 50},
+    ] as DataGrid.DataGrid.ColumnDescriptor[]);
+    this.dataGrid = new DataGrid.DataGrid.DataGridImpl({
+      displayName: i18nString(UIStrings.sharedStorageItems),
+      columns,
+      editCallback: undefined,
+      deleteCallback: this.deleteCallback.bind(this),
+      refreshCallback: this.refreshItems.bind(this),
+    });
+    this.dataGrid.addEventListener(DataGrid.DataGrid.Events.SelectedNode, event => {
+      void this.previewEntry(event.data);
+    });
+    this.dataGrid.addEventListener(DataGrid.DataGrid.Events.DeselectedNode, () => {
+      void this.previewEntry(null);
+    });
+    this.dataGrid.setStriped(true);
+    this.dataGrid.setName('SharedStorageItemsView');
+
+    this.outerSplitWidget = new UI.SplitWidget.SplitWidget(
+        /* isVertical: */ false, /* secondIsSidebar: */ true, 'sharedStorageSplitViewState');
+    this.outerSplitWidget.show(this.element);
+
+    this.previewPanel = new UI.Widget.VBox();
+    this.previewPanel.setMinimumSize(0, 50);
+    const resizer = this.previewPanel.element.createChild('div', 'preview-panel-resizer');
+
+    const dataGridWidget = this.dataGrid.asWidget();
+    dataGridWidget.setMinimumSize(0, 50);
+
+    this.metadataView = new SharedStorageMetadataView(sharedStorage);
+
+    const topPanel = new UI.SplitWidget.SplitWidget(
+        /* isVertical: */ false, /* secondIsSidebar: */ false, 'sharedStorageSplitViewState');
+    topPanel.setSidebarWidget(this.metadataView);
+    topPanel.setMainWidget(dataGridWidget);
+
+    this.outerSplitWidget.setMainWidget(topPanel);
+    this.outerSplitWidget.setSidebarWidget(this.previewPanel);
+    this.outerSplitWidget.installResizer(resizer);
+
+    this.preview = null;
+    this.previewValue = null;
+
+    this.showPreview(null, null);
+
+    this.eventListeners = [];
+    this.setStorage(sharedStorage);
+  }
+
+  setStorage(sharedStorage: SharedStorage): void {
+    Common.EventTarget.removeEventListeners(this.eventListeners);
+    this.sharedStorage = sharedStorage;
+    this.eventListeners = [
+      this.sharedStorage.addEventListener(SharedStorage.Events.SharedStorageChanged, this.sharedStorageChanged, this),
+    ];
+    this.refreshItems();
+  }
+
+  private sharedStorageItemsCleared(): void {
+    if (!this.isShowing() || !this.dataGrid) {
+      return;
+    }
+
+    this.dataGrid.rootNode().removeChildren();
+    this.dataGrid.addCreationNode(false);
+    UI.ARIAUtils.alert(i18nString(UIStrings.sharedStorageItemsCleared));
+    this.setCanDeleteSelected(false);
+  }
+
+  private sharedStorageChanged(event: Common.EventTarget.EventTargetEvent<SharedStorage.SharedStorageChangedEvent>):
+      void {
+    if (!this.isShowing() || !this.dataGrid) {
+      return;
+    }
+    void event;
+    this.refreshItems();
+  }
+
+  private showSharedStorageItems(items: Protocol.Storage.SharedStorageEntry[]): void {
+    const rootNode = this.dataGrid.rootNode();
+    let selectedKey: null = null;
+    for (const node of rootNode.children) {
+      if (!node.selected) {
+        continue;
+      }
+      selectedKey = node.data.key;
+      break;
+    }
+    rootNode.removeChildren();
+    let selectedNode: DataGrid.DataGrid.DataGridNode<unknown>|null = null;
+    const filteredItems = (item: Protocol.Storage.SharedStorageEntry): string => `${item.key} ${item.value}`;
+    const filteredList = this.filter(items, filteredItems);
+    for (const item of filteredList) {
+      const node = new DataGrid.DataGrid.DataGridNode({key: item.key, value: item.value}, false);
+      node.selectable = true;
+      rootNode.appendChild(node);
+      if (!selectedNode || item.key === selectedKey) {
+        selectedNode = node;
+      }
+    }
+    if (selectedNode) {
+      selectedNode.selected = true;
+    }
+    this.dataGrid.addCreationNode(false);
+    this.setCanDeleteSelected(Boolean(selectedNode));
+    UI.ARIAUtils.alert(i18nString(UIStrings.sharedStorageNumberEntries, {PH1: filteredList.length}));
+  }
+
+  deleteSelectedItem(): void {
+    if (!this.dataGrid || !this.dataGrid.selectedNode) {
+      return;
+    }
+
+    this.deleteCallback(this.dataGrid.selectedNode);
+  }
+
+  refreshItems(): void {
+    void this.sharedStorage.getEntries().then(entries => entries && this.showSharedStorageItems(entries));
+  }
+
+  deleteAllItems(): void {
+    this.sharedStorage.clear();
+    this.sharedStorageItemsCleared();
+  }
+
+  private deleteCallback(node: DataGrid.DataGrid.DataGridNode<unknown>): void {
+    if (!node || node.isCreationNode) {
+      return;
+    }
+
+    if (this.sharedStorage) {
+      this.sharedStorage.deleteEntry(node.data.key);
+    }
+
+    UI.ARIAUtils.alert(i18nString(UIStrings.sharedStorageItemDeleted));
+  }
+
+  private showPreview(preview: UI.Widget.Widget|null, value: string|null): void {
+    if (this.preview && this.previewValue === value) {
+      return;
+    }
+    if (this.preview) {
+      this.preview.detach();
+    }
+    if (!preview) {
+      preview = new UI.EmptyWidget.EmptyWidget(i18nString(UIStrings.selectAValueToPreview));
+    }
+    this.previewValue = value;
+    this.preview = preview;
+    preview.show(this.previewPanel.contentElement);
+  }
+
+  private async previewEntry(entry: DataGrid.DataGrid.DataGridNode<unknown>|null): Promise<void> {
+    const key = entry && entry.data && entry.data.key;
+    const value = entry && entry.data && entry.data.value;
+    const wrappedEntry = key && value && {key: key as string, value: value as string};
+    const wrappedEntryString: string|null = wrappedEntry ? JSON.stringify(wrappedEntry) : null;
+    if (entry && entry.data && entry.data.key && entry.data.value) {
+      const url = `shared-storage://${entry.key}` as Platform.DevToolsPath.UrlString;
+      const provider = TextUtils.StaticContentProvider.StaticContentProvider.fromString(
+          url, Common.ResourceType.resourceTypes.XHR, (wrappedEntryString as string));
+      const preview = await SourceFrame.PreviewFactory.PreviewFactory.createPreview(provider, 'text/plain');
+      // Selection could've changed while the preview was loaded
+      if (entry.selected) {
+        this.showPreview(preview, wrappedEntryString);
+      }
+    } else {
+      this.showPreview(null, wrappedEntryString);
+    }
+  }
+}
---
+++ front_end/panels/application/SharedStorageListTreeElement.ts
@@ -0,0 +1,64 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Common from '../../core/common/common.js';
+import * as i18n from '../../core/i18n/i18n.js';
+
+import type * as Platform from '../../core/platform/platform.js';
+
+import {ApplicationPanelTreeElement} from './ApplicationPanelTreeElement.js';
+import {type ResourcesPanel} from './ResourcesPanel.js';
+import {SharedStorageEventsView} from './SharedStorageEventsView.js';
+
+const UIStrings = {
+  /**
+  *@description Text in SharedStorage Category View of the Application panel
+  */
+  sharedStorage: 'Shared Storage',
+};
+const str_ = i18n.i18n.registerUIStrings('panels/application/SharedStorageListTreeElement.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+export class SharedStorageListTreeElement extends ApplicationPanelTreeElement {
+  private readonly expandedSetting: Common.Settings.Setting<boolean>;
+  private categoryLink: Platform.DevToolsPath.UrlString|null;
+  private view: SharedStorageEventsView;
+
+  constructor(resourcesPanel: ResourcesPanel, settingsDefault = false) {
+    super(resourcesPanel, i18nString(UIStrings.sharedStorage), false);
+    this.expandedSetting =
+        Common.Settings.Settings.instance().createSetting('resources Shared Storage Expanded', settingsDefault);
+    this.categoryLink = null;
+    this.view = new SharedStorageEventsView();
+  }
+
+  get itemURL(): Platform.DevToolsPath.UrlString {
+    return 'category://' + i18nString(UIStrings.sharedStorage) as Platform.DevToolsPath.UrlString;
+  }
+
+  setLink(link: Platform.DevToolsPath.UrlString): void {
+    this.categoryLink = link;
+  }
+
+  onselect(selectedByUser: boolean|undefined): boolean {
+    super.onselect(selectedByUser);
+    this.resourcesPanel.showView(this.view);
+    return false;
+  }
+
+  onattach(): void {
+    super.onattach();
+    if (this.expandedSetting.get()) {
+      this.expand();
+    }
+  }
+
+  onexpand(): void {
+    this.expandedSetting.set(true);
+  }
+
+  oncollapse(): void {
+    this.expandedSetting.set(false);
+  }
+}
