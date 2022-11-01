11db1da6b7bce030f7444db3e441937bce936b1f
cammie <cammie@chromium.org>
2022-11-01 12:18:50 -0400

    Shared Storage: DevTools Frontend Prototype.
    Bug:1372007,1218540
    Change-Id: Id1bdfa8a7db437045743334713dfcc3a17a7139d

---
+++ front_end/panels/application/components/SharedStorageMetadataView.ts
@@ -0,0 +1,161 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as i18n from '../../../core/i18n/i18n.js';
+import type * as Protocol from '../../../generated/protocol.js';
+import * as ComponentHelpers from '../../../ui/components/helpers/helpers.js';
+import * as Coordinator from '../../../ui/components/render_coordinator/render_coordinator.js';
+import * as ReportView from '../../../ui/components/report_view/report_view.js';
+import * as UI from '../../../ui/legacy/legacy.js';
+import * as LitHtml from '../../../ui/lit-html/lit-html.js';
+
+import {SharedStorage} from './SharedStorageModel.js';
+import sharedStorageMetadataViewStyles from './sharedStorageMetadataView.css.js';
+
+const UIStrings = {
+  /**
+  *@description Text in SharedStorage Metadata View of the Application panel
+  */
+  sharedStorage: 'Shared Storage',
+  /**
+  *@description Section header for Metadata
+  */
+  metadata: 'Metadata',
+  /**
+  *@description The origin of a URL (https://web.dev/same-site-same-origin/#origin)
+  *(for a lot of languages this does not need to be translated, please translate only where necessary)
+  */
+  origin: 'Origin',
+  /**
+  *@description The time when the origin most recently created its shared storage database
+  */
+  creationTime: 'Creation Time',
+  /**
+  *@description The number of entries currently in the origin's database
+  */
+  length: 'Length',
+  /**
+  *@description The number of bits remaining in the origin's shared storage privacy budget
+  */
+  remainingBudget: 'Remaining Privacy Budget',
+};
+const str_ = i18n.i18n.registerUIStrings('panels/application/components/SharedStorageMetadataView.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+export class SharedStorageMetadataView extends UI.ThrottledWidget.ThrottledWidget {
+  readonly #reportView = new SharedStorageMetadataReportView();
+  #sharedStorage: SharedStorage;
+
+  constructor(sharedStorage: SharedStorage) {
+    super();
+    this.#sharedStorage = sharedStorage;
+    this.contentElement.classList.add('overflow-auto');
+    this.contentElement.appendChild(this.#reportView);
+    this.#reportView.origin = this.#sharedStorage.securityOrigin;
+    this.update();
+    this.#sharedStorage.addEventListener(SharedStorage.Events.SharedStorageChanged, this.update, this);
+  }
+
+  async doUpdate(): Promise<void> {
+    const metadata = await this.#sharedStorage.getMetadata();
+    const timeOrNull: Protocol.Network.TimeSinceEpoch|null = metadata ? metadata.creationTime : null;
+    const len: number = metadata ? metadata.length : -1;
+    const budget: number = metadata ? metadata.remainingBudget : 0;
+    this.#reportView.data = {creationTime: timeOrNull, length: len, remainingBudget: budget};
+  }
+}
+
+export interface SharedStorageMetadataData {
+  creationTime: Protocol.Network.TimeSinceEpoch|null;
+  length: number;
+  remainingBudget: number;
+}
+
+const coordinator = Coordinator.RenderCoordinator.RenderCoordinator.instance();
+
+export class SharedStorageMetadataReportView extends HTMLElement {
+  static readonly litTagName = LitHtml.literal`devtools-shared-storage-metadata-view`;
+  readonly #shadow = this.attachShadow({mode: 'open'});
+  origin: string = '';
+  #creationTime: Protocol.Network.TimeSinceEpoch|null = null;
+  #length: number = -1;
+  #remainingBudget: number = 0;
+
+  connectedCallback(): void {
+    this.#shadow.adoptedStyleSheets = [sharedStorageMetadataViewStyles];
+  }
+
+  set data(data: SharedStorageMetadataData) {
+    this.#creationTime = data.creationTime;
+    this.#length = data.length;
+    this.#remainingBudget = data.remainingBudget;
+    void this.#render();
+  }
+
+  async #render(): Promise<void> {
+    await coordinator.write('SharedStorageMetadataView render', () => {
+      if (!this.#creationTime) {
+        return;
+      }
+
+      const titleForReport = {reportTitle: i18nString(UIStrings.sharedStorage)} as ReportView.ReportView.ReportData;
+
+      // Disabled until https://crbug.com/1079231 is fixed.
+      // clang-format off
+      LitHtml.render(LitHtml.html`
+        <${ReportView.ReportView.Report.litTagName} .data=${titleForReport as ReportView.ReportView.ReportData}>
+          ${this.#renderMetadataSection()}
+        </${ReportView.ReportView.Report.litTagName}>
+      `, this.#shadow, {host: this});
+      // clang-format on
+    });
+  }
+
+  #renderMetadataSection(): LitHtml.LitTemplate {
+    if (!this.#creationTime) {
+      return LitHtml.nothing;
+    }
+
+    return LitHtml.html`
+      <${ReportView.ReportView.ReportSectionHeader.litTagName}>${i18nString(UIStrings.metadata)}</${
+        ReportView.ReportView.ReportSectionHeader.litTagName}>
+      <${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.origin)}</${
+        ReportView.ReportView.ReportKey.litTagName}>
+      <${ReportView.ReportView.ReportValue.litTagName}>
+          <div class="text-ellipsis" title=${this.origin}>${this.origin}</div>
+      </${ReportView.ReportView.ReportValue.litTagName}>
+     <${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.creationTime)}</${
+        ReportView.ReportView.ReportKey.litTagName}>
+     <${ReportView.ReportView.ReportValue.litTagName}>
+       ${this.#renderDateForCreationTime()}</${ReportView.ReportView.ReportValue.litTagName}>
+     <${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.length)}
+     </${ReportView.ReportView.ReportKey.litTagName}>
+     <${ReportView.ReportView.ReportValue.litTagName}>${this.#length}</${ReportView.ReportView.ReportValue.litTagName}>
+     <${ReportView.ReportView.ReportKey.litTagName}>${i18nString(UIStrings.remainingBudget)}
+     </${ReportView.ReportView.ReportKey.litTagName}>
+     <${ReportView.ReportView.ReportValue.litTagName}>${this.#remainingBudget}</${
+        ReportView.ReportView.ReportValue.litTagName}>
+      <${ReportView.ReportView.ReportSectionDivider.litTagName}></${
+        ReportView.ReportView.ReportSectionDivider.litTagName}>
+    `;
+  }
+
+  #renderDateForCreationTime(): LitHtml.LitTemplate {
+    if (!this.#creationTime) {
+      return LitHtml.nothing;
+    }
+    const date = new Date(1e3 * (this.#creationTime as number));
+    return LitHtml.html`${date.toLocaleString()}`;
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent(
+    'devtools-shared-storage-metadata-view', SharedStorageMetadataReportView);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'devtools-shared-storage-metadata-view': SharedStorageMetadataReportView;
+  }
+}
