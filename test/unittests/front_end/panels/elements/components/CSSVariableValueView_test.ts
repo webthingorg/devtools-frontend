77ad71c5f24336418e85d3918ec420fcc82082c3
han <suyanhanx@gmail.com>
2022-11-02 23:11:17 +0800

    Show popover when hover on CSS variable
    Currently, there will show the CSS variable value on hover as a title. But, the time that waiting for the title is a bit too long, and many people even don't know that they could get the value by hover.
    Also, although you can jump to the definition to get the value by clicking on it, it is still very inconvenient.
    
    This commit use popover instead of title to show the CSS variable value on hover. On top of that, we can extend more functions later.
    
    Previously when the cursor was located outside the variables of an expression, the value of the entire expression would display.
    The following commit highlight the part of the value in effect， so the tooltip on whole expression has been removed.
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3983512
    
    Video: https://imgur.com/a/aSGAvNJ
    
    Bug: 1380779
    Change-Id: I16faf4d2139486deef390f75b1cd08009f5b4fe0

---
+++ test/unittests/front_end/panels/elements/components/CSSVariableValueView_test.ts
@@ -0,0 +1,29 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ElementsComponents from '../../../../../../front_end/panels/elements/components/components.js';
+import {assertShadowRoot, renderElementIntoDOM} from '../../../helpers/DOMHelpers.js';
+import {describeWithEnvironment} from '../../../helpers/EnvironmentHelpers.js';
+
+const {assert} = chai;
+
+describeWithEnvironment('CSSVariableValueView', async () => {
+  it('renders right tooltip', () => {
+    const link = document.createElement('span');
+    link.setAttribute('data-title', 'pink');
+
+    const popupComponent = new ElementsComponents.CSSVariableValueView.CSSVariableValueView(link);
+    renderElementIntoDOM(popupComponent);
+
+    assertShadowRoot(popupComponent.shadowRoot);
+
+    const shadowRoot = popupComponent.shadowRoot;
+
+    const popupContentRendered = shadowRoot.querySelector('.variable-value-popup-wrapper') !== null;
+    assert.isTrue(popupContentRendered);
+
+    const popupContent = shadowRoot.querySelector('.variable-value-popup-wrapper')?.textContent;
+    assert.strictEqual(popupContent, 'pink');
+  });
+});
