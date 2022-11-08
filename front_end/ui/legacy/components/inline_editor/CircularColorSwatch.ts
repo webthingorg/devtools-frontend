db5ce5b2a366a2d69e982e00812eb0f73be5835c
Ergün Erdoğmuş <ergunsh@chromium.org>
2022-11-08 15:39:08 +0000

    [Color] Add CircularColorSwatch and use it in styles and computed panes
    This CL:
    * Adds a new component: CircularColorSwatch
    * Updates `Color` regex to also match colors with new syntax
    * Updates StylePropertyTreeElement processColor to render circular color
    swatch if the given color is represented in one of the new color
    syntaxes[1]. This shows the circular color swatch in Styles pane
    * Updates autocompletions for variables to show this color swatch for the
    colors written in new color syntax
    * Updates ComputedStyleWidget to render CircularColorSwatch for colors
    in computed style pane.
    
    The reason I chose for implementing a new component instead of updating
    `Color` class to parse wide gamut colors is: it will require more work
    and this will be tackled while implementing conversions.
    
    So, as the next steps, what will happen is:
    * We'll add conversions to different color spaces and parse colors with
    new syntax to be a `Color` object.
    * Remove `CircularColorSwatch` and handle rendering/interactions in
    `ColorSwatch` itself.
    * Implement Color Picker for the colors defined in new syntax and
    remove special handling of such colors (with removing `canBeWideGamut`)
    
    I'll tackle showing color swatch for shadow editor in a later CL.
    
    See the screenshots for the component:
    * Opaque: https://bugs.chromium.org/p/chromium/issues/attachment?aid=576179&signed_aid=PiYe3o8328N5hGz4PEsWlw==&inline=1
    * Transparent: https://bugs.chromium.org/p/chromium/issues/attachment?aid=576180&signed_aid=0FkQWZ4t9410IMGDVkUjRA==&inline=1
    
    
    [1]: lch, oklch, lab, oklab, color
    
    Fixed: 1382409
    Change-Id: I6adfd4a85d6552937f89708039a19c089db2811e

---
+++ front_end/ui/legacy/components/inline_editor/CircularColorSwatch.ts
@@ -0,0 +1,54 @@
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ComponentHelpers from '../../../components/helpers/helpers.js';
+import * as LitHtml from '../../../lit-html/lit-html.js';
+
+import circularColorSwatchStyles from './circularColorSwatch.css.js';
+
+function consumeEvent(event: Event) {
+  event.stopPropagation();
+}
+
+/**
+ * For colors that we don't have support in color picker (wide gamut colors)
+ * we're going to show a circular color swatch.
+ *
+ * After we update the color picker for those colors
+ * we'll remove this and show the regular color swatch that opens
+ * the color picker.
+ */
+export class CircularColorSwatch extends HTMLElement {
+  static readonly litTagName = LitHtml.literal`devtools-circular-color-swatch`;
+  private readonly shadow = this.attachShadow({mode: 'open'});
+  private text: string|null = null;
+
+  constructor() {
+    super();
+    this.shadow.adoptedStyleSheets = [
+      circularColorSwatchStyles,
+    ];
+  }
+
+  renderColor(text: string) {
+    this.text = text;
+
+    this.render();
+  }
+
+  private render(): void {
+    // Disabled until https://crbug.com/1079231 is fixed.
+    // clang-format off
+    LitHtml.render(
+      LitHtml.html`<span class="circular-color-swatch"><span class="circular-color-swatch-inner"
+        style="background-color: ${this.text};"
+        @click=${consumeEvent}
+        @mousedown=${consumeEvent}
+        @dblclick=${consumeEvent}></span></span><slot><span>${this.text}</span></slot>`,
+      this.shadow, {host: this});
+    // clang-format on
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-circular-color-swatch', CircularColorSwatch);
