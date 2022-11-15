1a703caabeda57d4e714436f263cdf508987130e
han <suyanhanx@gmail.com>
2022-11-15 19:17:02 +0800

    Make elements tree ellipsis clickable to expand
    The ellipsis in the elements tree panel isn't clickable now.
    Giving a clear button for expanding is more in line with human intuition. And with the button in the middle, it's easier to click than the left triangle.
    
    Screen Recording: https://i.imgur.com/XejrhXW.mp4
    
    Bug: None
    Change-Id: I5fbd78dff38490af08fdb01c5cd8a1ed063f3974

---
+++ front_end/panels/elements/components/ElementsTreeExpandButton.ts
@@ -0,0 +1,54 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ComponentHelpers from '../../../ui/components/helpers/helpers.js';
+import * as LitHtml from '../../../ui/lit-html/lit-html.js';
+
+import elementsTreeExpandButtonStyles from './elementsTreeExpandButton.css.js';
+
+export interface ElementsTreeExpandButtonData {
+  onButtonClick: (event?: Event) => void;
+}
+export class ElementsTreeExpandButton extends HTMLElement {
+  static readonly litTagName = LitHtml.literal`devtools-elements-tree-expand-button`;
+  readonly #shadow = this.attachShadow({mode: 'open'});
+
+  #onButtonClick: ((event?: Event) => void) = () => {};
+
+  set data(data: ElementsTreeExpandButtonData) {
+    this.#onButtonClick = data.onButtonClick;
+    this.#update();
+  }
+
+  #update(): void {
+    this.#render();
+  }
+
+  connectedCallback(): void {
+    this.#shadow.adoptedStyleSheets = [elementsTreeExpandButtonStyles];
+  }
+
+  #render(): void {
+    // clang-format off
+    LitHtml.render(LitHtml.html`
+      <span
+        class="button-content"
+        @click=${this.#onButtonClick}>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </span>
+      `, this.#shadow, {host: this});
+    // clang-format on
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-elements-tree-expand-button', ElementsTreeExpandButton);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'devtools-elements-tree-expand-button': ElementsTreeExpandButton;
+  }
+}
