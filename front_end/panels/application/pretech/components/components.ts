45d8a7b40c9af2931794895036ec843b7331702b
keno <kenoss@chromium.org>
2022-11-16 09:13:35 +0000

    PrerenderPanel: Add PretechDetailsReportView
    Bug: 1384419
    Change-Id: I267c4ad101f0d2f19c4c964c81726a3880227376

--- front_end/panels/application/pretech/components/components.ts
+++ front_end/panels/application/pretech/components/components.ts
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as PretechDetailsReportView from './PretechDetailsReportView.js';
 import * as PretechGrid from './PretechGrid.js';
 
 export {
+  PretechDetailsReportView,
   PretechGrid,
 };
