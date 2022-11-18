27dd8b820642912b91541a2bda64ecd493b95385
keno <kenoss@chromium.org>
2022-11-18 08:09:32 +0000

    PreloadingPanel: Add PrerenderingRegistry
    Bug: 1384419
    Change-Id: I23dd6e192ff4bf0f09c4188c28538158fb27a2c9

---
+++ front_end/core/sdk/PrerenderingModel.ts
@@ -0,0 +1,209 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Protocol from '../../generated/protocol.js';
+import type * as Platform from '../platform/platform.js';
+
+// PrerenderingRegistry is aimed to be used in PrerenderingModel.
+// TODO(https://crbug.com/1384419): Add PrerenderingModel.
+
+// Id for preloading events and prerendering attempt.
+export type PreloadingId = string;
+
+export type PrerenderingAttemptId = string;
+
+export interface PrerenderingAttempt {
+  kind: 'PrerenderingAttempt';
+  prerenderingAttemptId: PrerenderingAttemptId;
+  startedAt: number;
+  trigger: PrerenderingTrigger;
+  url: Platform.DevToolsPath.UrlString;
+  status: PrerenderingStatus;
+  discardedReason?: Protocol.Page.PrerenderFinalStatus|null|'Unknown';
+}
+
+type PrerenderingTrigger =
+    PrerenderingTriggerSpecRules|PrerenderingTriggerDUI|PrerenderingTriggerDSE|PrerenderingTriggerOpaque;
+
+interface PrerenderingTriggerSpecRules {
+  kind: 'PrerenderingTriggerSpecRules';
+  rule: object;
+}
+
+interface PrerenderingTriggerDUI {
+  kind: 'PrerenderingTriggerDUI';
+}
+
+interface PrerenderingTriggerDSE {
+  kind: 'PrerenderingTriggerDSE';
+}
+
+interface PrerenderingTriggerOpaque {
+  kind: 'PrerenderingTriggerOpaque';
+}
+
+export type PrerenderingStatus = 'prerendering'|'activated'|'cancelled'|'discarded';
+
+export type PrerenderingAttemptEvent = PrerenderingAttemptEventAdd|PrerenderingAttemptEventUpdate;
+
+export interface PrerenderingAttemptEventAdd {
+  kind: 'PrerenderingAttemptEventAdd';
+  attempt: PrerenderingAttempt;
+}
+
+export interface PrerenderingAttemptEventUpdate {
+  kind: 'PrerenderingAttemptEventUpdate';
+  update: PrerenderingAttempt;
+}
+
+// export only for testing.
+export class PrerenderingRegistry {
+  private entities: Map<PreloadingId, PrerenderingAttempt> = new Map<PreloadingId, PrerenderingAttempt>();
+  // Hack:
+  // Curretly, PrerenderAttemptCompleted event doesn't have information
+  // to identify corresponding attempt. To mitigate this, we utilize the
+  // fact that attempts are activated/cancelled if navigated out. So,
+  // in many cases, we can identify an ongoing attempt by URL.
+  // TODO(https://crbug.com/1384419): Consider to add a relevant CDP
+  // event for prerendering.
+  private opaqueUrlToPreId: Map<Platform.DevToolsPath.UrlString, PreloadingId> =
+      new Map<Platform.DevToolsPath.UrlString, PreloadingId>();
+
+  // Returns reference. Don't save returned values.
+  getById(id: PreloadingId): PrerenderingAttempt|null {
+    return this.entities.get(id) || null;
+  }
+
+  // Returns array of pairs of id and reference. Don't save returned references.
+  getAll(): [PreloadingId, PrerenderingAttempt][] {
+    return Array.from(this.entities.entries());
+  }
+
+  private preId(x: PrerenderingAttempt): PreloadingId {
+    if (x.trigger.kind === 'PrerenderingTriggerOpaque') {
+      return `PrerenderingAttempt-opaque:${x.prerenderingAttemptId}` as PreloadingId;
+    }
+    return `${x.kind}:${x.prerenderingAttemptId}` as PreloadingId;
+  }
+
+  private makePreIdOfPrerendering(frameId: Protocol.Page.FrameId): PreloadingId {
+    return `PrerenderingAttempt-opaque:${frameId}` as PreloadingId;
+  }
+
+  // TODO(https://crbug.com/1384419): Make this private.
+  applyEvent(event: PrerenderingAttemptEvent): void {
+    switch (event.kind) {
+      case 'PrerenderingAttemptEventAdd': {
+        this.entities.set(this.preId(event.attempt), event.attempt);
+        break;
+      }
+      case 'PrerenderingAttemptEventUpdate': {
+        this.entities.set(this.preId(event.update), event.update);
+
+        const x = event.update;
+        if (x.status !== 'prerendering') {
+          if (this.opaqueUrlToPreId.get(x.url)) {
+            this.opaqueUrlToPreId.delete(x.url);
+          }
+        }
+
+        break;
+      }
+    }
+  }
+
+  // Clear not ongoing prerendering attempts.
+  clearNotOngoing(): void {
+    for (const [id, x] of this.entities.entries()) {
+      if (x.status !== 'prerendering') {
+        this.entities.delete(id);
+      }
+    }
+  }
+
+  // Initial support of detecting prerendering start
+  // TODO: Make CDP changes correctly.
+  maybeAddOpaquePrerendering(frameId: Protocol.Page.FrameId, url: Platform.DevToolsPath.UrlString): void {
+    // Ad-hoc filtering
+    //
+    // If a page has SpeculationRules and browser navigated out to a not
+    // related page, current Chrome throws PrerenderAttemptCompleted
+    // event and then TargetInfoChanged event. This filtering prevents
+    // adding a new prerendering attempt by the latter TargetInfoChanged.
+    if (this.entities.get(this.makePreIdOfPrerendering(frameId)) !== undefined) {
+      return;
+    }
+
+    const prerenderingAttemptId: PrerenderingAttemptId = frameId as PrerenderingAttemptId;
+    const event: PrerenderingAttemptEventAdd = {
+      kind: 'PrerenderingAttemptEventAdd',
+      attempt: {
+        kind: 'PrerenderingAttempt',
+        prerenderingAttemptId: prerenderingAttemptId,
+        startedAt: Date.now(),
+        trigger: {
+          kind: 'PrerenderingTriggerOpaque',
+        },
+        url: url,
+        status: 'prerendering',
+      },
+    };
+    this.applyEvent(event);
+
+    const id = this.makePreIdOfPrerendering(frameId);
+    this.opaqueUrlToPreId.set(url, id);
+  }
+
+  updateOpaquePrerenderingAttempt(event: Protocol.Page.PrerenderAttemptCompletedEvent): void {
+    const id = this.opaqueUrlToPreId.get(event.prerenderingUrl as Platform.DevToolsPath.UrlString);
+
+    if (id === undefined) {
+      return;
+    }
+
+    const origAttempt = this.entities.get(id);
+
+    if (origAttempt === undefined) {
+      return;
+    }
+
+    if (origAttempt.kind !== 'PrerenderingAttempt') {
+      throw new Error('unreachable');
+    }
+
+    const [status, discardedReason] = ((): [PrerenderingStatus, Protocol.Page.PrerenderFinalStatus|null] => {
+      const getDiscardedReason =
+          (event: Protocol.Page.PrerenderAttemptCompletedEvent): Protocol.Page.PrerenderFinalStatus|null => {
+            switch (event.finalStatus) {
+              case Protocol.Page.PrerenderFinalStatus.Activated:
+                throw new Error('unreachable');
+              case Protocol.Page.PrerenderFinalStatus.Destroyed:
+                return null;
+              default:
+                return event.finalStatus;
+            }
+          };
+
+      switch (event.finalStatus) {
+        case 'Activated':
+          return ['activated', null];
+        default:
+          return ['discarded', getDiscardedReason(event)];
+      }
+    })();
+    const eventInternal: PrerenderingAttemptEventUpdate = {
+      kind: 'PrerenderingAttemptEventUpdate',
+      update: {
+        kind: 'PrerenderingAttempt',
+        prerenderingAttemptId: origAttempt.prerenderingAttemptId,
+        startedAt: origAttempt.startedAt,
+        trigger: origAttempt.trigger,
+        url: origAttempt.url,
+        status: status,
+        discardedReason: discardedReason,
+      },
+    };
+    this.applyEvent(eventInternal);
+  }
+}
