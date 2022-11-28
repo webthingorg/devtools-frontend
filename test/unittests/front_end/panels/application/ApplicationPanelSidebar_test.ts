3cb83b10f4d1f5239ae7a3e67492b91a6370ae1f
cammie <cammie@chromium.org>
2022-11-28 00:20:06 -0500

    Shared Storage: Add DevTools tree elements & hook up to app panel.
    For the full initial Shared Storage DevTools UI prototype, see
    https://crrev.com/c/devtools/devtools-frontend/+/3983292.
    
    Bug:1372007,1218540
    Change-Id: If765c317124578e32bd77832bf8f314d6f3ad3ac

--- test/unittests/front_end/panels/application/ApplicationPanelSidebar_test.ts
+++ test/unittests/front_end/panels/application/ApplicationPanelSidebar_test.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type * as Common from '../../../../../front_end/core/common/common.js';
+import type * as Protocol from '../../../../../front_end/generated/protocol.js';
 import {assertNotNullOrUndefined} from '../../../../../front_end/core/platform/platform.js';
 import * as Root from '../../../../../front_end/core/root/root.js';
 import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
@@ -12,9 +14,47 @@ import {describeWithMockConnection} from '../../helpers/MockConnection.js';
 
 const {assert} = chai;
 
+class SharedStorageTreeElementListener {
+  #sidebar: Application.ApplicationPanelSidebar.ApplicationPanelSidebar;
+  #originsAdded: Array<String> = new Array<String>();
+
+  constructor(sidebar: Application.ApplicationPanelSidebar.ApplicationPanelSidebar) {
+    this.#sidebar = sidebar;
+
+    this.#sidebar.sharedStorageTreeElementDispatcher.addEventListener(
+        Application.ApplicationPanelSidebar.SharedStorageTreeElementDispatcher.Events.TreeElementAdded,
+        this.#treeElementAdded, this);
+  }
+
+  dispose(): void {
+    this.#sidebar.sharedStorageTreeElementDispatcher.removeEventListener(
+        Application.ApplicationPanelSidebar.SharedStorageTreeElementDispatcher.Events.TreeElementAdded,
+        this.#treeElementAdded, this);
+  }
+
+  #treeElementAdded(event: Common.EventTarget.EventTargetEvent<
+                    Application.ApplicationPanelSidebar.SharedStorageTreeElementDispatcher.TreeElementAddedEvent>):
+      void {
+    this.#originsAdded.push(event.data.origin);
+  }
+
+  async waitForElementsAdded(expectedCount: number): Promise<void> {
+    while (this.#originsAdded.length < expectedCount) {
+      await this.#sidebar.sharedStorageTreeElementDispatcher.once(
+          Application.ApplicationPanelSidebar.SharedStorageTreeElementDispatcher.Events.TreeElementAdded);
+    }
+  }
+}
+
 describeWithMockConnection('ApplicationPanelSidebar', () => {
   const tests = (targetFactory: () => SDK.Target.Target) => {
     let target: SDK.Target.Target;
+
+    const METADATA = {
+      creationTime: 100 as Protocol.Network.TimeSinceEpoch,
+      length: 4,
+      remainingBudget: 2.5,
+    } as unknown as Protocol.Storage.SharedStorageMetadata;
 
     beforeEach(() => {
       stubNoopSettings();
@@ -52,6 +92,72 @@ describeWithMockConnection('ApplicationPanelSidebar', () => {
       assert.deepStrictEqual(
           sidebar.cookieListTreeElement.children().map(e => e.title),
           ['http://www.example.com', 'http://www.example.org']);
+    });
+
+    it('shows shared storage for origins using shared storage', async () => {
+      sinon.stub(UI.ViewManager.ViewManager.instance(), 'showView').resolves();  // Silence console error
+      const resourceTreeModel = target.model(SDK.ResourceTreeModel.ResourceTreeModel);
+      assertNotNullOrUndefined(resourceTreeModel);
+      sinon.stub(resourceTreeModel, 'frames').returns([
+        {
+          url: 'http://www.example.com/',
+          unreachableUrl: () => null,
+          resourceTreeModel: () => resourceTreeModel,
+        } as unknown as SDK.ResourceTreeModel.ResourceTreeFrame,
+        {
+          url: 'http://www.example.com/admin/',
+          unreachableUrl: () => null,
+          resourceTreeModel: () => resourceTreeModel,
+        } as unknown as SDK.ResourceTreeModel.ResourceTreeFrame,
+        {
+          url: 'http://www.example.org/',
+          unreachableUrl: () => null,
+          resourceTreeModel: () => resourceTreeModel,
+        } as unknown as SDK.ResourceTreeModel.ResourceTreeFrame,
+        {
+          url: 'http://www.example.net/',
+          unreachableUrl: () => null,
+          resourceTreeModel: () => resourceTreeModel,
+        } as unknown as SDK.ResourceTreeModel.ResourceTreeFrame,
+      ]);
+
+      const securityOriginManager = target.model(SDK.SecurityOriginManager.SecurityOriginManager);
+      assertNotNullOrUndefined(securityOriginManager);
+      sinon.stub(securityOriginManager, 'securityOrigins').returns([
+        'http://www.example.com/',
+        'http://www.example.org/',
+        'http://www.example.net/',
+      ]);
+
+      const sharedStorageModel = target.model(Application.SharedStorageModel.SharedStorageModel);
+      assertNotNullOrUndefined(sharedStorageModel);
+      const setTrackingSpy = sinon.stub(sharedStorageModel.storageAgent, 'invoke_setSharedStorageTracking').resolves({
+        getError: () => undefined,
+      });
+      const getMetadataSpy = sinon.stub(sharedStorageModel.storageAgent, 'invoke_getSharedStorageMetadata').resolves({
+        metadata: METADATA,
+        getError: () => undefined,
+      });
+      getMetadataSpy.withArgs({ownerOrigin: 'http://www.example.org/'}).resolves({
+        metadata: METADATA,
+        getError: () => 'origin does not exist in database',
+      });
+
+      Application.ResourcesPanel.ResourcesPanel.instance({forceNew: true});
+      const sidebar = await Application.ResourcesPanel.ResourcesPanel.showAndGetSidebar();
+
+      const listener = new SharedStorageTreeElementListener(sidebar);
+      const addedPromise = listener.waitForElementsAdded(2);
+      resourceTreeModel.dispatchEventToListeners(SDK.ResourceTreeModel.Events.CachedResourcesLoaded, resourceTreeModel);
+      await addedPromise;
+
+      assert.isTrue(setTrackingSpy.calledOnceWithExactly({enable: true}));
+      assert.strictEqual(getMetadataSpy.callCount, 5);
+
+      assert.strictEqual(sidebar.sharedStorageListTreeElement.childCount(), 2);
+      assert.deepStrictEqual(
+          sidebar.sharedStorageListTreeElement.children().map(e => e.title),
+          ['http://www.example.com/', 'http://www.example.net/']);
     });
   };
   describe('without tab target', () => tests(() => createTarget()));
