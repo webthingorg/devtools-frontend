f4db12434b5c09f5bb86f43c41333e010a0c34a8
cammie <cammie@chromium.org>
2022-11-29 01:59:26 -0500

    Shared Storage: Hook up to application panel.
    For the full initial Shared Storage DevTools UI prototype, see
    https://crrev.com/c/devtools/devtools-frontend/+/3983292.
    
    Bug:1372007,1218540
    Change-Id: Id33b949d0dd50c886564dd3d7aa97e23ad48c3ad

---
+++ test/e2e/application/shared-storage_test.ts
@@ -0,0 +1,56 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {
+  getBrowserAndPages,
+  getTestServerPort,
+  step,
+} from '../../shared/helper.js';
+import {
+  describe,
+  it,
+} from '../../shared/mocha-extensions.js';
+import {
+  doubleClickSourceTreeItem,
+  getStorageItemsData,
+  navigateToApplicationTab,
+} from '../helpers/application-helpers.js';
+
+const SHARED_STORAGE_SELECTOR = '[aria-label="Shared Storage"].parent';
+let DOMAIN_SELECTOR: string;
+
+describe('The Application Tab', async () => {
+  before(async () => {
+    DOMAIN_SELECTOR = `${SHARED_STORAGE_SELECTOR} + ol > [aria-label="https://localhost:${getTestServerPort()}"]`;
+  });
+
+  it('shows Shared Storage keys and values', async () => {
+    const {target} = getBrowserAndPages();
+
+    await step('navigate to shared-storage resource and open Application tab', async () => {
+      await navigateToApplicationTab(target, 'shared-storage');
+    });
+
+    await step('open the domain storage', async () => {
+      await doubleClickSourceTreeItem(SHARED_STORAGE_SELECTOR);
+      await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
+    });
+
+    await step('check that storage data values are correct', async () => {
+      const dataGridRowValues = await getStorageItemsData(['key', 'value']);
+      assert.deepEqual(dataGridRowValues, [
+        {
+          key: 'firstKey',
+          value: 'firstValue',
+        },
+        {
+          key: 'secondKey',
+          value: '{"field":"complexValue","primitive":2}',
+        },
+      ]);
+    });
+  });
+});
