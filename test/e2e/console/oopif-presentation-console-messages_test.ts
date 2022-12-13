57f98b754b4d77e830abc315ecd606196e122dda
Al Muthanna Athamina <almuthanna@chromium.org>
2022-12-13 15:32:39 +0100

    [E2E] Convert oopif/oopif-presentation-console-messages.js Layout test to Mocha test
    Bug: chromium:1377770
    Change-Id: Ib5c443d3367d056b735d1c9a8c20f9c39ae61175

---
+++ test/e2e/console/oopif-presentation-console-messages_test.ts
@@ -0,0 +1,9 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {describe, it} from '../../shared/mocha-extensions.js';
+
+describe('The Console Tab', async () => {
+  it('leads to the right file when clicking on the source link from an iframe', async () => {});
+});
