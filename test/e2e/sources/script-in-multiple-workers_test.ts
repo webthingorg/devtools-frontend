5cf4028a64a80709f002d3339ab5a08c79eaa9a5
Kim-Anh Tran <kimanh@chromium.org>
2022-12-15 16:35:42 +0100

    Add uiSourceCodeForScript to DebuggerWorkspaceBinding
    The new method is used to get a uiSourceCode for a given script. This
    will be used for instrumentation pauses, when we await a uiSourceCode
    for setting a breakpoint.
    
    This CL also adds instrumentation breakpoint tests for inline
    scripts (with and without the html uiSourceCode being available),
    and for regular scripts. These tests all make use of the
    uiSourceCodeForScript method.
    
    Bug: 1396298, 1395337
    Change-Id: Ic699a39cc414bd831d2d34afdd848317d32eae90

--- test/e2e/sources/script-in-multiple-workers_test.ts
+++ test/e2e/sources/script-in-multiple-workers_test.ts
@@ -7,6 +7,7 @@ import {assert} from 'chai';
 import {
   $$,
   click,
+  enableExperiment,
   getBrowserAndPages,
   goToResource,
   step,
@@ -42,6 +43,10 @@ describe('Multi-Workers', async function() {
   if (this.timeout() !== 0) {
     this.timeout(10000);
   }
+
+  before(async () => {
+    await enableExperiment('instrumentationBreakpoints');
+  });
 
   [false, true].forEach(sourceMaps => {
     const withOrWithout = sourceMaps ? 'with source maps' : 'without source maps';
@@ -238,8 +243,7 @@ describe('Multi-Workers', async function() {
         await validateSourceTabs();
       });
 
-      // Flaky on mac
-      it.skipOnPlatforms(['mac'], '[crbug.com/1368493] for newly created workers', async () => {
+      it('for newly created workers', async () => {
         const {target} = getBrowserAndPages();
         // Launch new worker to hit breakpoint
         await target.evaluate(`new Worker('${scriptFile}').postMessage({});`);
