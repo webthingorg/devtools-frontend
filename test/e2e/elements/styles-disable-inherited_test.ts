130ad16312b227bf804c02ea0be31e3513212939
Al Muthanna Athamina <almuthanna@chromium.org>
2022-12-30 18:49:50 +0100

    [E2E] Convert elements/styles-3/styles-disable-inherited.js Layout test to Mocha test
    Bug: chromium:1377770
    Change-Id: I4ec13a57e3f8d7624c8c967b7b9d20bf15a030ea

---
+++ test/e2e/elements/styles-disable-inherited_test.ts
@@ -0,0 +1,28 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {goToResource, waitForElementWithTextContent} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {
+  checkStyleAttributes,
+  expandSelectedNodeRecursively,
+  uncheckStylesPaneCheckbox,
+  waitForElementsStyleSection,
+} from '../helpers/elements-helpers.js';
+
+describe('The Elements tab', async function() {
+  it('does not break further style inspection if inherited style property was disabled', async () => {
+    await goToResource('elements/styles-disable-inherited.html');
+    await expandSelectedNodeRecursively();
+    const nested = await waitForElementWithTextContent('nested');
+    await nested.click();
+    await waitForElementsStyleSection();
+    await checkStyleAttributes(['display: block;', 'font-weight: bold;']);
+    const container = await waitForElementWithTextContent('container');
+    await container.click();
+    await uncheckStylesPaneCheckbox('font-weight bold');
+    await nested.click();
+    await checkStyleAttributes(['display: block;']);
+  });
+});
