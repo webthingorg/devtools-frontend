2f505905c14279cca035df7d97e552bbacc81445
Jack Franklin <jacktfranklin@chromium.org>
2023-01-12 14:23:25 +0000

    Move TraceEngine parsing into a worker
    This CL moves the new trace engine into a worker.
    
    The `Model` is kept on the main thread, but the processor that does the
    work of running trace events through the handler now is run in a worker.
    The model hides this implementation detail and communicates to/from the
    worker using messages.
    
    This CL also updates the panel to call into the new engine. It does this
    without `awaiting`, so we won't materially slow down the user
    experience, and it does not yet use the data in any form. This is just
    the first step towards beginning to use the data to power the panel.
    
    Fixed: 1399473
    Change-Id: I50aae988d88d7fc916a9400ae81a8808c02f098e

---
+++ front_end/models/trace/worker/worker_entrypoint.ts
@@ -0,0 +1,52 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import * as Processor from './Processor.js';
+import type * as Types from './Types.js';
+import * as Platform from '../../../core/platform/platform.js';
+
+self.postMessage('workerReady');
+
+const processor = Processor.TraceProcessor.create();
+
+// Send a message back from the worker. Exists purely to provide some type
+// safety around the messages we send.
+function sendMessage(event: Types.MessageFromWorker): void {
+  self.postMessage(event);
+}
+
+// TODO: this event should likely be renamed to TraceParseProgressEvent as it
+// does not represent the 'done' event now. The ModelImpl does that. We can
+// keep the ModelImpl TraceParseEvent alone as that can still be emitted from
+// the model, but we should change the Processor event to make it clear that
+// it's just the update event.
+processor.addEventListener(Processor.TraceParseEvent.eventName, (event: Event) => {
+  const updateEvent = event as Processor.TraceParseEvent;
+  sendMessage({message: 'PARSE_UPDATE', ...updateEvent.data});
+});
+
+self.onmessage = async function(event: MessageEvent): Promise<void> {
+  if (!event.data.action) {
+    return;
+  }
+
+  const message = event.data as Types.MessageToWorker;
+  switch (message.action) {
+    case 'PARSE': {
+      try {
+        await processor.parse(event.data.events, event.data.freshRecording);
+        sendMessage({message: 'PARSE_COMPLETE', data: processor.data});
+      } catch (error) {
+        sendMessage({message: 'PARSE_ERROR', error});
+      } finally {
+        break;
+      }
+    }
+    case 'RESET': {
+      processor.reset();
+      break;
+    }
+    default:
+      Platform.assertNever(message, `Unknown trace worker message ${JSON.stringify(message)}`);
+  }
+};
