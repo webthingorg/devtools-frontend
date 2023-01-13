53bc7e9761a4bddafab36604a846e9891cbbd425
Ergün Erdoğmuş <ergunsh@chromium.org>
2023-01-13 13:48:58 +0000

    Handle ColorMix properly by separating parsing and rendering
    * Updated `colorMixHandler` to run before `varHandler`
    so that colorMixHandler is run even though there is a variable reference
    inside of the syntax.
    
    * Used CodeMirror parser to get values out of `color-mix` syntax.
    
    * Handle resolving variables properly and rendering them
    
    Bug: none for now
    Change-Id: Ifc0d2c4b418531936b6730b5bb1d0e48da76c715

--- front_end/ui/legacy/components/inline_editor/ColorMixSwatch.ts
+++ front_end/ui/legacy/components/inline_editor/ColorMixSwatch.ts
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Common from '../../../../core/common/common.js';
 import * as ComponentHelpers from '../../../components/helpers/helpers.js';
 import * as LitHtml from '../../../lit-html/lit-html.js';
 
@@ -11,7 +10,7 @@ import colorMixSwatchStyles from './colorMixSwatch.css.js';
 export class ColorMixSwatch extends HTMLElement {
   static readonly litTagName = LitHtml.literal`devtools-color-mix-swatch`;
   private readonly shadow = this.attachShadow({mode: 'open'});
-  private text: string = 'color-mix(in srgb, hotpink, white)';
+  private colorMixText: string = 'color-mix(in srgb, hotpink, white)';
   private firstColorText: string = 'hotpink';
   private secondColorText: string = 'white';
 
@@ -22,38 +21,21 @@ export class ColorMixSwatch extends HTMLElement {
     ];
   }
 
-  setText(text: string): void {
-    this.text = text;
-
-    const parametersMatch = text.match(/color-mix\(in\s+(?:\w+)\s*,(?<firstColor>.+),(?<secondColor>.+)\)/);
-    if (!parametersMatch || !parametersMatch.groups || parametersMatch.length !== 3) {
-      this.#renderTextOnly();
-      return;
-    }
-
-    const firstColorAndPercentage = parametersMatch.groups['firstColor'];
-    const secondColorAndPercentage = parametersMatch.groups['secondColor'];
-
-    const firstColorMatch = firstColorAndPercentage.match(Common.Color.Regex);
-    if (!firstColorMatch) {
-      this.#renderTextOnly();
-      return;
-    }
-
-    const secondColorMatch = secondColorAndPercentage.match(Common.Color.Regex);
-    if (!secondColorMatch) {
-      this.#renderTextOnly();
-      return;
-    }
-
-    this.firstColorText = firstColorMatch[0];
-    this.secondColorText = secondColorMatch[0];
-
+  setFirstColor(text: string): void {
+    this.colorMixText = this.colorMixText.replace(this.firstColorText, text);
+    this.firstColorText = text;
     this.#render();
   }
 
-  #renderTextOnly(): void {
-    LitHtml.render(this.text, this.shadow, {host: this});
+  setSecondColor(text: string): void {
+    this.colorMixText = this.colorMixText.replace(this.secondColorText, text);
+    this.secondColorText = text;
+    this.#render();
+  }
+
+  setColorMixText(text: string): void {
+    this.colorMixText = text;
+    this.#render();
   }
 
   #render(): void {
@@ -68,8 +50,8 @@ export class ColorMixSwatch extends HTMLElement {
       LitHtml.html`<div class="swatch-icon">
         <span class="swatch swatch-left" id="swatch-1" style="--color: ${this.firstColorText}"></span>
         <span class="swatch swatch-right" id="swatch-2" style="--color: ${this.secondColorText}"></span>
-        <span class="swatch swatch-mix" id="mix-result" style="--color: ${this.text}"></span>
-      </div><slot>${this.text}</slot>`,
+        <span class="swatch swatch-mix" id="mix-result" style="--color: ${this.colorMixText}"></span>
+      </div><slot>${this.colorMixText}</slot>`,
       this.shadow, {host: this});
     // clang-format on
   }
