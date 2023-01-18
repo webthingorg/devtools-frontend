84196d1305f8628ae9ce105fd59d31acbc175040
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-01-18 11:42:50 +0000

    [DevToolsIcons] Introduce IconElement component
    This CL introduces a new devtools-icon-element component which should
    replace all other icon implementations going forward
    
    Bug: 1405244
    Change-Id: If2f9642813b15fdc5aeafb69d9beae69fa0f0035

---
+++ front_end/ui/components/icon_button/NewIcon.ts
@@ -0,0 +1,51 @@
+// Copyright (c) 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as LitHtml from '../../lit-html/lit-html.js';
+import * as ComponentHelpers from '../helpers/helpers.js';
+
+export class NewIcon extends LitHtml.LitElement {
+  // static readonly litTagName = LitHtml.literal`devtools-icon-element`;
+  @LitHtml
+      .Decorators.property({
+        type: String,
+      }) name = '';
+  static styles = LitHtml.css`
+    :host {
+      display: inline-block;
+      width: 18px;
+      height: 18px;
+    }
+
+    span {
+      display: block;
+      width: 100%;
+      height: 100%;
+      white-space: nowrap;
+      background-color: currentcolor;
+      -webkit-mask-position: center;
+      -webkit-mask-repeat: no-repeat;
+      -webkit-mask-size: 99%;
+    }
+  `;
+
+  render(): LitHtml.LitTemplate {
+    const path = this.pathFromName(this.name);
+    const styles = {webkitMaskImage: `url(${path})`};
+    return LitHtml.html`<span style=${LitHtml.Directives.styleMap(styles)}></span>`;
+  }
+
+  pathFromName(name: string|null): string {
+    return new URL(`../../../Images/${name}.svg`, import.meta.url).toString();
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('new-devtools-icon', NewIcon);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'new-devtools-icon': NewIcon;
+  }
+}
