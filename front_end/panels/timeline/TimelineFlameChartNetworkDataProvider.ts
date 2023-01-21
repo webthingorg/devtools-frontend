470dcfd383e42e4419f97a3d89b535b7861c148b
Paul Irish <paulirish@chromium.org>
2023-01-20 16:53:19 -0800

    [DONT LAND] Performance: flamechart debug mode.
    Shows trace event detail in the floating tooltip.
    Screenshots: https://imgur.com/a/B6JMcNi
    
    
    Also, when selecting an event, copies the original trace event's ts to clipboard.
    
    Bug: none
    Change-Id: Iebb4367cf184795d5bf8925f76a330d639164c03

--- front_end/panels/timeline/TimelineFlameChartNetworkDataProvider.ts
+++ front_end/panels/timeline/TimelineFlameChartNetworkDataProvider.ts
@@ -313,6 +313,10 @@ export class TimelineFlameChartNetworkDataProvider implements PerfUI.FlameChart.
       div.style.color = this.colorForPriority(request.priority) || 'black';
     }
     contents.createChild('span').textContent = Platform.StringUtilities.trimMiddle(request.url, maxURLChars);
+
+    const requestClone = {...request};
+    // requestClone.children = requestClone.children.map(c => )
+    contents.createChild('span').textContent = '\n' + JSON.stringify(requestClone, null, 2).slice(0, 2000);
     return element;
   }
 
