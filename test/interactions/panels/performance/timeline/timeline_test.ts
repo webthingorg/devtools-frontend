7f791aa6e593e1d6194d3992fa5f63bb80c8eb96
Andrés Olivares <andoli@chromium.org>
2023-01-31 13:17:17 +0000

    Do not check in
    Bug: none
    Change-Id: I9a6dd89ee2d799a7307f6b84ba0431fe740a41db

--- test/interactions/panels/performance/timeline/timeline_test.ts
+++ test/interactions/panels/performance/timeline/timeline_test.ts
@@ -8,8 +8,7 @@ import {assertElementScreenshotUnchanged, itScreenshot} from '../../../../shared
 import {loadComponentDocExample} from '../../../helpers/shared.js';
 
 describe('Performance panel', () => {
-  // Disabled until screenshot tests infrastructure is available in DevTools bots.
-  itScreenshot.skip('[crbug.com/1407638] renders the timeline correctly', async () => {
+  itScreenshot('renders the timeline correctly', async () => {
     await loadComponentDocExample('performance_panel/basic.html?trace=animation');
     await waitFor('#timeline-overview-panel');
     const panel = await waitFor('body');
