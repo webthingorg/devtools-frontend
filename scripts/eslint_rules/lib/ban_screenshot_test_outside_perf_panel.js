ff5463c82667e37a556bbc47417938fe5ad107e8
Andrés Olivares <andoli@chromium.org>
2023-01-31 12:31:48 +0000

    Lint against screenshot tests outside perf panel interaction test base
    A new rule is added that checks that calls to itScreenshot are only made
    within the newly added directory for the Performance Panel interactions
    tests. This plus the rule that ensures screenshots are asserted in an
    itScreenshot block, restrict using the screenshot test API outside of
    the mentioned directory.
    
    Bug: 1407638
    Change-Id: I6cff308e9c150bfa231bd65b6190aa293273910a

---
+++ scripts/eslint_rules/lib/ban_screenshot_test_outside_perf_panel.js
@@ -0,0 +1,42 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+const path = require('path');
+const PERFOAMCE_PANEL_INTERACTION_TESTS_PATH = 'test/interactions/panels/performance/';
+module.exports = {
+  meta : {
+    type : 'problem',
+
+    docs : {
+      description :  'Bans writting screenshot tests outside the directory for the Performance Panel interaction tests.',
+      category : 'Possible Errors',
+    },
+    fixable : 'code',
+    messages : {
+      invalidScreenshotTest : 'It is banned to write screenshot tests outside the directory of the Performance Panel interaction tests.',
+    },
+    schema : []
+  },
+  create : function(context) {
+    const fileName = path.resolve(context.getFilename());
+    function reportPathIfInvalid(node) {
+      if(!fileName.includes(PERFOAMCE_PANEL_INTERACTION_TESTS_PATH)) {
+        context.report({
+          node,
+          messageId: 'invalidScreenshotTest'
+        });
+      }
+    }
+
+    return {
+      'CallExpression[callee.type="Identifier"][callee.name="itScreenshot"], MemberExpression'(node) {
+        if ((node.property?.name === 'skip' && node.object?.name === 'itScreenshot' )|| node.type === 'CallExpression') {
+          reportPathIfInvalid(node);
+          return;
+        }
+      },
+    };
+  }
+};
