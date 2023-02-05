748fe93224544d5ac403c11d9a998dddbce597bc
Alexey Rodionov <fluorescent.hallucinogen@gmail.com>
2023-02-05 14:51:05 +0300

    Display MDN docs by hovering over CSS properties in DevTools
    Bug: 1401107
    Change-Id: I5afd79288806c4220caa3b2b1876fbe4e01110f7

---
+++ front_end/panels/elements/components/CSSPropertyDocsView.ts
@@ -0,0 +1,90 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as i18n from '../../../core/i18n/i18n.js';
+import * as ComponentHelpers from '../../../ui/components/helpers/helpers.js';
+import * as LitHtml from '../../../ui/lit-html/lit-html.js';
+
+import CSSPropertyDocsViewStyles from './cssPropertyDocsView.css.js';
+
+const UIStrings = {
+  /**
+   *@description Text for button that redirects to CSS property documentation.
+   */
+  learnMore: 'Learn more',
+  /**
+   *@description Prefix for the syntax section of CSS property documentation.
+   */
+  syntax: 'Syntax',
+};
+const str_ = i18n.i18n.registerUIStrings('panels/elements/components/CSSPropertyDocsView.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+const {render, html} = LitHtml;
+
+interface Reference {
+  name: string;
+  url: string;
+}
+
+interface CSSProperty {
+  description?: string;
+  syntax?: string;
+  references?: Reference[];
+}
+
+export class CSSPropertyDocsView extends HTMLElement {
+  static readonly litTagName = LitHtml.literal`devtools-css-property-docs-view`;
+  readonly #shadow = this.attachShadow({mode: 'open'});
+  readonly #cssProperty: CSSProperty;
+
+  constructor(cssProperty: CSSProperty) {
+    super();
+    this.#cssProperty = cssProperty;
+    this.#shadow.adoptedStyleSheets = [CSSPropertyDocsViewStyles];
+    this.#render();
+  }
+
+  #render(): void {
+    const description = this.#cssProperty.description;
+    const syntax = this.#cssProperty.syntax;
+    const link = this.#cssProperty.references?.[0].url;
+
+    // Disabled until https://crbug.com/1079231 is fixed.
+    // clang-format off
+    render(html`
+      <div class="docs-popup-wrapper">
+        ${description ? html`<div>${description}</div>` : ''}
+        ${syntax ? html`
+          <div class="docs-popup-section">
+            ${i18nString(UIStrings.syntax)}: <code>${syntax}</code>
+          </div>
+        ` : ''}
+        ${link ? html`
+          <div class="docs-popup-section">
+            <x-link
+              id="learn-more"
+              href=${link}
+              class="clickable underlined unbreakable-text"
+            >
+              ${i18nString(UIStrings.learnMore)}
+            </x-link>
+          </div>
+        ` : ''}
+      </div>
+    `, this.#shadow, {
+        host: this,
+      });
+    // clang-format on
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-css-property-docs-view', CSSPropertyDocsView);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'devtools-css-property-docs-view': CSSPropertyDocsView;
+  }
+}
