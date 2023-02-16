f9c2dd01749935515bbc363b597740c1a80554b9
Al Muthanna Athamina <almuthanna@chromium.org>
2023-02-16 13:41:08 +0100

    Deflake memory test
    Bug: 1239550
    Change-Id: I5ec052005a1f0a0d72cccdc0ac125f1ad5cb5fd7

--- test/e2e/memory/memory_test.ts
+++ test/e2e/memory/memory_test.ts
@@ -84,9 +84,10 @@ describe('The Memory Panel', async function() {
     ]);
   });
 
-  // Flaky test
-  it.skip(
-      '[crbug.com/1134602] Correctly retains the path for event listeners', async () => {
+  // eslint-disable-next-line rulesdir/no_only
+  it.only(
+      'Correctly retains the path for event listeners', async () => {
+        this.timeout(30000);
         await goToResource('memory/event-listeners.html');
         await step('taking a heap snapshot', async () => {
           await navigateToMemoryTab();
