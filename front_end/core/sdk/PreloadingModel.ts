7d5933140df82cb875584fc49985e94481ece990
keno <kenoss@chromium.org>
2023-02-21 14:45:24 +0000

    PreloadingDevTools: Add PreloadingModel
    This CL adds PreloadingModel, which handles CDP events `Preload.*` and
    manages preloading related information:
    
    - SpeculationRule rule sets
    - (TODO) Preloading attempts
    - (TODO) Relationship between rule sets and preloading attempts
    
    In this CL, the model handles only rule sets. Other capabilities will
    be added when relevant CDP changes landed.
    
    Bug: 1384419
    Change-Id: I1947dd748dc5e2531ea1a4bd6d385e9dafa2a1ff

---
+++ front_end/core/sdk/PreloadingModel.ts
@@ -0,0 +1,182 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as Common from '../common/common.js';
+import type * as Protocol from '../../generated/protocol.js';
+import type * as ProtocolProxyApi from '../../generated/protocol-proxy-api.js';
+
+import * as SDKModel from './SDKModel.js';
+import * as Target from './Target.js';
+import * as TargetManager from './TargetManager.js';
+import * as ResourceTreeModel from './ResourceTreeModel.js';
+
+export interface WithId<I, V> {
+  id: I;
+  value: V;
+}
+
+// Holds preloading related information.
+//
+// - SpeculationRule rule sets
+// - (TODO) Preloading attempts
+// - (TODO) Relationship between rule sets and preloading attempts
+export class PreloadingModel extends SDKModel.SDKModel<EventTypes> {
+  private agent: ProtocolProxyApi.PreloadApi;
+  private resourceTreeModelObserver: ResourceTreeModelObserver;
+  private ruleSets: RuleSetRegistry = new RuleSetRegistry();
+
+  constructor(target: Target.Target) {
+    super(target);
+
+    target.registerPreloadDispatcher(new PreloadDispatcher(this));
+
+    this.agent = target.preloadAgent();
+    void this.agent.invoke_enable();
+
+    this.resourceTreeModelObserver = new ResourceTreeModelObserver(this);
+  }
+
+  dispose(): void {
+    super.dispose();
+
+    this.resourceTreeModelObserver.dispose();
+    void this.agent.invoke_disable();
+  }
+
+  // Returns reference. Don't save returned values.
+  // Returned value may or may not be updated as the time grows.
+  getRuleSetById(id: Protocol.Preload.RuleSetId): Protocol.Preload.RuleSet|null {
+    return this.ruleSets.getById(id);
+  }
+
+  // Returns array of pairs of id and reference. Don't save returned references.
+  // Returned values may or may not be updated as the time grows.
+  getAllRuleSets(): WithId<Protocol.Preload.RuleSetId, Protocol.Preload.RuleSet>[] {
+    return this.ruleSets.getAll();
+  }
+
+  onMainFrameNavigated(event: Common.EventTarget.EventTargetEvent<ResourceTreeModel.ResourceTreeFrame>): void {
+    const frame = event.data;
+
+    if (frame.loaderId === '') {
+      return;
+    }
+
+    // Note that ResourceTreeFrame.loaderId is
+    // Protocol.Network.LoaderId if it is constructed from Page.Frame.
+    const loaderId = frame.loaderId as Protocol.Network.LoaderId;
+    this.ruleSets.clearOnMainFrameNavigation(loaderId);
+    this.dispatchEventToListeners(Events.RuleSetsModified);
+  }
+
+  onRuleSetUpdated(event: Protocol.Preload.RuleSetUpdatedEvent): void {
+    const ruleSet = event.ruleSet;
+
+    if (this.ruleSets.getById(ruleSet.id)) {
+      // Currently, modification of <script> has no effect and doesn't
+      // emit Preload.ruleSetAdded.
+      // For more details, see https://github.com/whatwg/html/issues/7986.
+      throw new Error('unreachable');
+    } else {
+      this.ruleSets.insert(ruleSet);
+      this.dispatchEventToListeners(Events.RuleSetsModified);
+    }
+  }
+
+  onRuleSetRemoved(event: Protocol.Preload.RuleSetRemovedEvent): void {
+    const id = event.id;
+
+    this.ruleSets.delete(id);
+    this.dispatchEventToListeners(Events.RuleSetsModified);
+  }
+}
+
+SDKModel.SDKModel.register(PreloadingModel, {capabilities: Target.Capability.Target, autostart: false});
+
+class ResourceTreeModelObserver implements TargetManager.SDKModelObserver<ResourceTreeModel.ResourceTreeModel> {
+  model: PreloadingModel;
+
+  constructor(model: PreloadingModel) {
+    this.model = model;
+
+    TargetManager.TargetManager.instance().observeModels(ResourceTreeModel.ResourceTreeModel, this);
+  }
+
+  dispose(): void {
+    TargetManager.TargetManager.instance().unobserveModels(ResourceTreeModel.ResourceTreeModel, this);
+  }
+
+  modelAdded(model: ResourceTreeModel.ResourceTreeModel): void {
+    model.addEventListener(ResourceTreeModel.Events.MainFrameNavigated, this.model.onMainFrameNavigated, this.model);
+  }
+
+  // implements TargetManager.SDKModelObserver<ResourceTreeModel.ResourceTreeModel>
+  modelRemoved(model: ResourceTreeModel.ResourceTreeModel): void {
+    model.removeEventListener(ResourceTreeModel.Events.MainFrameNavigated, this.model.onMainFrameNavigated, this.model);
+  }
+}
+
+// TODO(crbug.com/1167717): Make this a const enum again
+// eslint-disable-next-line rulesdir/const_enum
+export enum Events {
+  RuleSetsModified = 'RuleSetsModified',
+}
+
+export type EventTypes = {
+  [Events.RuleSetsModified]: void,
+};
+
+class PreloadDispatcher implements ProtocolProxyApi.PreloadDispatcher {
+  private model: PreloadingModel;
+
+  constructor(model: PreloadingModel) {
+    this.model = model;
+  }
+
+  ruleSetUpdated(event: Protocol.Preload.RuleSetUpdatedEvent): void {
+    this.model.onRuleSetUpdated(event);
+  }
+
+  ruleSetRemoved(event: Protocol.Preload.RuleSetRemovedEvent): void {
+    this.model.onRuleSetRemoved(event);
+  }
+}
+
+class RuleSetRegistry {
+  private map: Map<Protocol.Preload.RuleSetId, Protocol.Preload.RuleSet> =
+      new Map<Protocol.Preload.RuleSetId, Protocol.Preload.RuleSet>();
+
+  // Returns reference. Don't save returned values.
+  // Returned values may or may not be updated as the time grows.
+  getById(id: Protocol.Preload.RuleSetId): Protocol.Preload.RuleSet|null {
+    return this.map.get(id) || null;
+  }
+
+  // Returns reference. Don't save returned values.
+  // Returned values may or may not be updated as the time grows.
+  getAll(): WithId<Protocol.Preload.RuleSetId, Protocol.Preload.RuleSet>[] {
+    return Array.from(this.map.entries()).map(([id, value]) => ({id, value}));
+  }
+
+  insert(ruleSet: Protocol.Preload.RuleSet): void {
+    if (this.map.get(ruleSet.id)) {
+      throw new Error(`cannot insert, already exists: id = ${ruleSet.id}`);
+    }
+
+    this.map.set(ruleSet.id, ruleSet);
+  }
+
+  delete(id: Protocol.Preload.RuleSetId): void {
+    this.map.delete(id);
+  }
+
+  // Clear all except for rule sets with given loader id (for race).
+  clearOnMainFrameNavigation(loaderId: Protocol.Network.LoaderId): void {
+    for (const ruleSet of this.map.values()) {
+      if (ruleSet.loaderId !== loaderId) {
+        this.map.delete(ruleSet.id);
+      }
+    }
+  }
+}
