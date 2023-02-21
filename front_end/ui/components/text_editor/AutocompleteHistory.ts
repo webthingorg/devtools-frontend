d53ddb197e38668925bbee3a6a2721223af7c739
Simon Zünd <szuend@chromium.org>
2023-02-21 10:13:29 +0100

    Move 'ConsoleHistoryManager' into 'text_editor' component
    We plan to share the implementation between console and breakpoint
    edit dialog so as a first step we move the class into the
    'text_editor' component and rename it to 'AutocompleteHistory'.
    
    The implementation is identical modulo 2 refactorings:
    
      - `private` keyword is replaced with private fields/methods
      - syncing the history with the setting is now also handled by
        'AutocompleteHistory'
    
    The CL also adds unit tests that check the non-trivial behaviors.
    
    R=kimanh@chromium.org
    
    Bug: 1027458
    Change-Id: I8e95c47d5ac84a401f1be32422e965c0f16f7249

---
+++ front_end/ui/components/text_editor/AutocompleteHistory.ts
@@ -0,0 +1,94 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as Common from '../../../core/common/common.js';
+
+export class AutocompleteHistory {
+  static #historySize = 300;
+
+  #setting: Common.Settings.Setting<string[]>;
+
+  /**
+   * The data mirrors the setting. We have the mirror for 2 reasons:
+   *   1) The setting is size limited
+   *   2) We track the user's current input, even though it's not committed yet.
+   */
+  #data: string[] = [];
+
+  /** 1-based entry in the history stack. */
+  #historyOffset = 1;
+  #uncommittedIsTop: boolean = false;
+
+  /**
+   * Creates a new settings-backed history. The class assumes it has sole
+   * ownership of the setting.
+   */
+  constructor(setting: Common.Settings.Setting<string[]>) {
+    this.#setting = setting;
+    this.#data = this.#setting.get();
+  }
+
+  clear(): void {
+    this.#data = [];
+    this.#setting.set([]);
+  }
+
+  get data(): string[] {
+    return this.#data;
+  }
+
+  /**
+   * Pushes a committed text into the history.
+   */
+  pushHistoryItem(text: string): void {
+    if (this.#uncommittedIsTop) {
+      this.#data.pop();
+      this.#uncommittedIsTop = false;
+    }
+
+    this.#historyOffset = 1;
+    if (text !== this.#currentHistoryItem()) {
+      this.#data.push(text);
+    }
+    this.#store();
+  }
+
+  previous(currentText: string): string|undefined {
+    if (this.#historyOffset > this.#data.length) {
+      return undefined;
+    }
+    if (this.#historyOffset === 1) {
+      this.#pushCurrentText(currentText);
+    }
+    ++this.#historyOffset;
+    return this.#currentHistoryItem();
+  }
+
+  next(): string|undefined {
+    if (this.#historyOffset === 1) {
+      return undefined;
+    }
+    --this.#historyOffset;
+    return this.#currentHistoryItem();
+  }
+
+  /**
+   * Pushes the current (uncommitted) text into the history.
+   */
+  #pushCurrentText(currentText: string): void {
+    if (this.#uncommittedIsTop) {
+      this.#data.pop();
+    }  // Throw away obsolete uncommitted text.
+    this.#uncommittedIsTop = true;
+    this.#data.push(currentText);
+  }
+
+  #currentHistoryItem(): string|undefined {
+    return this.#data[this.#data.length - this.#historyOffset];
+  }
+
+  #store(): void {
+    this.#setting.set(this.#data.slice(-AutocompleteHistory.#historySize));
+  }
+}
