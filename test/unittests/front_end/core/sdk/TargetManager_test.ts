6d000946062a68a7e5bce7a62d3d3de8cef894bf
Danil Somsikov <dsv@chromium.org>
2023-02-22 20:55:47 +0100

    Option to scope certain target manager operation to a subtree.
    Bug: 1418045
    Change-Id: Id9167c54a6dc494ac96213f39fe2bf7678f604b4

--- test/unittests/front_end/core/sdk/TargetManager_test.ts
+++ test/unittests/front_end/core/sdk/TargetManager_test.ts
@@ -76,4 +76,70 @@ describeWithMockConnection('TargetManager', () => {
         ?.dispatchEventToListeners(SDK.ResourceTreeModel.Events.WillReloadPage);
     assert.isTrue(listener.calledTwice);
   });
+
+  it('allows observing targets in scope', () => {
+    const observer = sinon.spy(new SDK.TargetManager.Observer());
+    const target1 = createTarget();
+    const target2 = createTarget();
+    targetManager.setScopeTarget(target1);
+    targetManager.observeTargets(observer, {scoped: true});
+    assert.isTrue(observer.targetAdded.calledOnceWith(target1));
+
+    createTarget({parentTarget: target2});
+    assert.isTrue(observer.targetAdded.calledOnceWith(target1));
+
+    const subtarget1 = createTarget({parentTarget: target1});
+    assert.isTrue(observer.targetAdded.calledTwice);
+    assert.isTrue(observer.targetAdded.calledWith(subtarget1));
+  });
+
+  it('allows observing models in scope', () => {
+    const observer = sinon.spy(new SDK.TargetManager.SDKModelObserver<SDK.ResourceTreeModel.ResourceTreeModel>());
+    const target1 = createTarget();
+    const model1 = target1.model(SDK.ResourceTreeModel.ResourceTreeModel) as SDK.ResourceTreeModel.ResourceTreeModel;
+    const target2 = createTarget();
+    targetManager.setScopeTarget(target1);
+    targetManager.observeModels(SDK.ResourceTreeModel.ResourceTreeModel, observer, {scoped: true});
+    assert.isTrue(observer.modelAdded.calledOnceWith(model1));
+
+    createTarget({parentTarget: target2});
+    assert.isTrue(observer.modelAdded.calledOnceWith(model1));
+
+    const subtarget1 = createTarget({parentTarget: target1});
+    assert.isTrue(observer.modelAdded.calledTwice);
+    const submodel1 =
+        subtarget1.model(SDK.ResourceTreeModel.ResourceTreeModel) as SDK.ResourceTreeModel.ResourceTreeModel;
+    assert.isTrue(observer.modelAdded.calledWith(submodel1));
+  });
+
+  it('allows listening to models in scope', () => {
+    const thisObject = {};
+    const listener = sinon.spy();
+    const target1 = createTarget();
+    targetManager.setScopeTarget(target1);
+    targetManager.addModelListener(
+        SDK.ResourceTreeModel.ResourceTreeModel, SDK.ResourceTreeModel.Events.WillReloadPage, listener, thisObject,
+        {scoped: true});
+    target1.model(SDK.ResourceTreeModel.ResourceTreeModel)
+        ?.dispatchEventToListeners(SDK.ResourceTreeModel.Events.WillReloadPage);
+    assert.isTrue(listener.calledOnce);
+    assert.isTrue(listener.calledOn(thisObject));
+    const target2 = createTarget();
+    target2.model(SDK.ResourceTreeModel.ResourceTreeModel)
+        ?.dispatchEventToListeners(SDK.ResourceTreeModel.Events.WillReloadPage);
+    assert.isTrue(listener.calledOnce);
+
+    const subtarget1 = createTarget({parentTarget: target1});
+    subtarget1.model(SDK.ResourceTreeModel.ResourceTreeModel)
+        ?.dispatchEventToListeners(SDK.ResourceTreeModel.Events.WillReloadPage);
+    assert.isTrue(listener.calledTwice);
+
+    targetManager.setScopeTarget(target2);
+    target1.model(SDK.ResourceTreeModel.ResourceTreeModel)
+        ?.dispatchEventToListeners(SDK.ResourceTreeModel.Events.WillReloadPage);
+    assert.isTrue(listener.calledTwice);
+    target2.model(SDK.ResourceTreeModel.ResourceTreeModel)
+        ?.dispatchEventToListeners(SDK.ResourceTreeModel.Events.WillReloadPage);
+    assert.isTrue(listener.calledThrice);
+  });
 });
