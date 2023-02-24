80621b13bc9235936332b0b499277c80ac1c729c
Andrés Olivares <andoli@chromium.org>
2023-02-24 13:51:03 +0000

    Bump slow LH test
    Bug: 1418624
    Change-Id: I38b8aad82b353eb8f1d9eedb92a340def8dd8449

--- test/e2e/lighthouse/start-view_test.ts
+++ test/e2e/lighthouse/start-view_test.ts
@@ -68,8 +68,11 @@ describe('The Lighthouse start view', async () => {
     assert.isTrue(disabled, 'The Generate Report button should be disabled');
   });
 
-  // Flaky on mac
-  it.skipOnPlatforms(['mac'], '[crbug.com/1418624] displays warning if important data may affect performance', async () => {
+  it('displays warning if important data may affect performance', async function() {
+    // Slow test, especially in parallel
+    if (this.timeout() !== 0) {
+      this.timeout(120_000);
+    }
     // e2e tests in application/ create websql and indexeddb items and don't clean up after themselves
     await clearSiteData();
 
