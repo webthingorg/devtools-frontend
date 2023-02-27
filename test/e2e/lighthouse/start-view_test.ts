f79ad81659d4ec0c2069728b3281476c44a12fcf
Al Muthanna Athamina <almuthanna@chromium.org>
2023-02-27 16:18:47 +0100

    Deflake LH test
    Bug: 1418624, 1418288
    Change-Id: Ie75b35203597d6531deec1fe0d1699a9f1c86d01

--- test/e2e/lighthouse/start-view_test.ts
+++ test/e2e/lighthouse/start-view_test.ts
@@ -68,8 +68,8 @@ describe('The Lighthouse start view', async () => {
     assert.isTrue(disabled, 'The Generate Report button should be disabled');
   });
 
-  // Flaky on mac
-  it.skipOnPlatforms(['mac'], '[crbug.com/1418624] displays warning if important data may affect performance', async () => {
+  // eslint-disable-next-line rulesdir/no_only
+  it.only('displays warning if important data may affect performance', async () => {
     // e2e tests in application/ create websql and indexeddb items and don't clean up after themselves
     await clearSiteData();
 
