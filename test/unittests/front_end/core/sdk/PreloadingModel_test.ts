22dd86582f2e14e588180bda5729f4ef5444e10f
keno <kenoss@chromium.org>
2023-02-27 06:18:50 +0000

    PreloadingDevTools: Mitigate wrong timing of Page.frameNavigated for prerender
    Page.frameNavigated event is emitted in wrong timing for prerendered
    pages [1]. This CL mitigates it by simulating frameNavigated with
    Page.prerenderAttemptCompleted.
    
    [1] https://crbug.com/1317959
    
    Bug: 1384419, 1317959
    Change-Id: I3cd7c37b26efaeda587ea435a87903536dabe2a3

--- test/unittests/front_end/core/sdk/PreloadingModel_test.ts
+++ test/unittests/front_end/core/sdk/PreloadingModel_test.ts
@@ -195,10 +195,10 @@ describeWithMockConnection('PreloadingModel', async () => {
         loaderId: 'loaderId:2' as Protocol.Network.LoaderId,
         sourceText: `
 {
-  "prerender":[
+  "prefetch":[
     {
       "source": "list",
-      "urls": ["/page.html"]
+      "urls": ["/style.css"]
     }
   ]
 }
@@ -212,7 +212,7 @@ describeWithMockConnection('PreloadingModel', async () => {
       frame: {
         id: 'frameId:2',
         loaderId: 'loaderId:2' as Protocol.Network.LoaderId,
-        url: 'https://example.com/index2.html',
+        url: 'https://example.com/',
         domainAndRegistry: 'example.com',
         securityOrigin: 'https://example.com/',
         mimeType: 'text/html',
@@ -230,10 +230,117 @@ describeWithMockConnection('PreloadingModel', async () => {
           loaderId: 'loaderId:2' as Protocol.Network.LoaderId,
           sourceText: `
 {
+  "prefetch":[
+    {
+      "source": "list",
+      "urls": ["/style.css"]
+    }
+  ]
+}
+`,
+        },
+      },
+    ]);
+  });
+
+  it('clears SpeculationRules for previous pages when prerendered page activated', async () => {
+    const target = createTarget();
+    const model = target.model(SDK.PreloadingModel.PreloadingModel);
+    assertNotNullOrUndefined(model);
+
+    assert.deepEqual(model.getAllRuleSets(), []);
+
+    dispatchEvent(target, 'Page.frameNavigated', {
+      frame: {
+        id: 'frameId:1',
+        loaderId: 'loaderId:1' as Protocol.Network.LoaderId,
+        url: 'https://example.com/',
+        domainAndRegistry: 'example.com',
+        securityOrigin: 'https://example.com/',
+        mimeType: 'text/html',
+        secureContextType: Protocol.Page.SecureContextType.Secure,
+        crossOriginIsolatedContextType: Protocol.Page.CrossOriginIsolatedContextType.Isolated,
+        gatedAPIFeatures: [],
+      },
+    });
+
+    // Assume that the first page has a speculationrules for prerendering.
+    dispatchEvent(target, 'Preload.ruleSetUpdated', {
+      ruleSet: {
+        id: 'ruleSetId:1',
+        loaderId: 'loaderId:1' as Protocol.Network.LoaderId,
+        sourceText: `
+{
   "prerender":[
     {
       "source": "list",
-      "urls": ["/page.html"]
+      "urls": ["/prerendered.html"]
+    }
+  ]
+}
+`,
+      },
+    });
+
+    assert.strictEqual(model.getAllRuleSets().length, 1);
+
+    // Commit of prerendering page with loaderId 'loaderId:2'.
+    dispatchEvent(target, 'Page.frameNavigated', {
+      frame: {
+        id: 'frameId:2',
+        loaderId: 'loaderId:2' as Protocol.Network.LoaderId,
+        url: 'https://example.com/prerendered.html',
+        domainAndRegistry: 'example.com',
+        securityOrigin: 'https://example.com/',
+        mimeType: 'text/html',
+        secureContextType: Protocol.Page.SecureContextType.Secure,
+        crossOriginIsolatedContextType: Protocol.Page.CrossOriginIsolatedContextType.Isolated,
+        gatedAPIFeatures: [],
+      },
+    });
+
+    // Assume that the prerendered page has a speculationrules for prerendering.
+    //
+    // This is not current behavior. This event is actually not emitted.
+    // We put this event to check the model and race.
+    //
+    // TODO(https://crbug.com/1418020): Remove/update this comment.
+    dispatchEvent(target, 'Preload.ruleSetUpdated', {
+      ruleSet: {
+        id: 'ruleSetId:2',
+        loaderId: 'loaderId:2' as Protocol.Network.LoaderId,
+        sourceText: `
+{
+  "prefetch":[
+    {
+      "source": "list",
+      "urls": ["/subresource.js"]
+    }
+  ]
+}
+`,
+      },
+    });
+
+    // Activation with loaderId 'loaderId:2'.
+    dispatchEvent(target, 'Page.prerenderAttemptCompleted', {
+      'initiatingFrameId': 'frameId:1',
+      'prerenderingUrl': 'https://example.com/prerendered.html',
+      'finalStatus': Protocol.Page.PrerenderFinalStatus.Activated,
+    });
+
+    assert.deepEqual(model.getAllRuleSets(), [
+      {
+        id: 'ruleSetId:2',
+        value: {
+          id: 'ruleSetId:2' as Protocol.Preload.RuleSetId,
+          loaderId: 'loaderId:2' as Protocol.Network.LoaderId,
+          sourceText: `
+{
+  "prefetch":[
+    {
+      "source": "list",
+      "urls": ["/subresource.js"]
     }
   ]
 }
