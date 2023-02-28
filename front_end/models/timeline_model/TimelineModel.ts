0cbd27aef37c60c109694a3a116c9525c898cad4
Jack Franklin <jacktfranklin@chromium.org>
2023-02-28 11:36:35 +0000

    Migrate Phase.COMPLETE to new engine
    Bug: 1417587
    Change-Id: I0bac17eb3900a67e66deb5663571631bafd25ea4

--- front_end/models/timeline_model/TimelineModel.ts
+++ front_end/models/timeline_model/TimelineModel.ts
@@ -37,6 +37,7 @@ import * as Platform from '../../core/platform/platform.js';
 import * as Root from '../../core/root/root.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import type * as Protocol from '../../generated/protocol.js';
+import * as TraceEngine from '../trace/trace.js';
 
 import {TimelineJSProfileProcessor} from './TimelineJSProfile.js';
 
@@ -1926,7 +1927,7 @@ export class Track {
         break;
       }
       const syncEvent = new SDK.TracingModel.ConstructedEvent(
-          event.categoriesString, event.name, SDK.TracingModel.Phase.Complete, startTime, event.thread);
+          event.categoriesString, event.name, TraceEngine.Types.TraceEvents.Phase.COMPLETE, startTime, event.thread);
       syncEvent.setEndTime(endTime);
       syncEvent.addArgs(event.args);
       this.syncEventsInternal.push(syncEvent);
