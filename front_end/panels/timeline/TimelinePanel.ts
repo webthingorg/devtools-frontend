0d7bce50546b39d681e4cc14c0febe0576bd7e49
Andrés Olivares <andoli@chromium.org>
2023-03-06 07:50:48 +0000

    [RPP] Use the new engine in the Timings track
    NOTE: This has to be landed *after* the new engine has been reenabled
    
    The first track appender is introduced to add the Timings track into the flame chart data (TimingsTrackAppender).
    
    This appender consists of the bits previously in the TimelineFlameChartDataProvider that were in charge of appending the data of the track along with the bits that provided extra feature for events in the track (like stylings and display names).
    
    All these parts are moved to a common place to handle the details of the track together in the same module, but separate from other tracks. The source of data used by the track appender is the output of the new engine
    
    In the future further test coverage will be added using dedicated screenshot tests.
    
    Bug: 1409044
    Change-Id: I007d5ffd93e310cba26546fa09829e02e3cc581c

--- front_end/panels/timeline/TimelinePanel.ts
+++ front_end/panels/timeline/TimelinePanel.ts
@@ -1266,6 +1266,7 @@ export class TimelinePanel extends UI.Panel.Panel implements Client, TimelineMod
       this.historyManager.addRecording(this.performanceModel, traceParsedData);
     } catch (error) {
       this.recordingFailed(error.message);
+      console.error(error);
     }
   }
 
