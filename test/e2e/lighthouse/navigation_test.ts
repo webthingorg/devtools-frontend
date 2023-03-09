f70013df756ff8a1724443a561ac9bbba0928caa
Al Muthanna Athamina <almuthanna@chromium.org>
2023-03-09 14:55:33 +0100

    Add sequential it and invert flag
    With itSequential we can reverse grep for it in the run using mocha-fgrep and
    invert flags. This will allow us to choose which tests we want to exclude from
    parallel runs.
    
    Bug: 1419802
    Change-Id: I2cd479f476b0c3ef2b0c204d49bdd9da535397a9

--- test/e2e/lighthouse/navigation_test.ts
+++ test/e2e/lighthouse/navigation_test.ts
@@ -34,11 +34,9 @@ import {
 // This test will fail (by default) in headful mode, as the target page never gets painted.
 // To resolve this when debugging, just make sure the target page is visible during the lighthouse run.
 
-describe('Navigation', async function() {
-  // The tests in this suite are particularly slow especially in parallel
-  if (this.timeout() !== 0) {
-    this.timeout(120_000);
-  }
+describe.skipOnParallel('Navigation', async function() {
+  // The tests in this suite are particularly slow
+  this.timeout(60_000);
 
   let consoleLog: string[] = [];
   const consoleListener = (e: puppeteer.ConsoleMessage) => {
