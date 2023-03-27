e6d30666aedfb867987572f8ed4a186ac07faf18
Bruno Braga <brunobraga@google.com>
2023-03-27 13:59:39 +0000

    [Autofill in Devtools] Introduce wavy line highlighting violating node id.
    Bug:1399414
    Change-Id: I212799483784cab81d5a0e34c2277678b78b4898

---
+++ test/e2e/elements/violating-element-and-attributes_test.ts
@@ -0,0 +1,33 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {goToResource, waitForMany} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {
+  expandSelectedNodeRecursively,
+} from '../helpers/elements-helpers.js';
+
+/**
+ * Skipping this test for now as it only works on non-headless chrome.
+ */
+describe.skip('Element has violating properties', async function() {
+  beforeEach(async function() {
+    await goToResource('elements/form-with-issues.html');
+    await expandSelectedNodeRecursively();
+  });
+
+  it('Tag is highlighted on input without name nor id', async () => {
+    const elements = await waitForMany('.violating-element', 2);
+    const violatingElementOrAttr = await elements[0].evaluate(node => node.textContent);
+    assert.strictEqual(violatingElementOrAttr, 'input');
+  });
+
+  it('Autocomplete attribute is highlighted when empty.', async () => {
+    const elements = await waitForMany('.violating-element', 2);
+    const violatingElementOrAttr = await elements[1].evaluate(node => node.textContent);
+    assert.strictEqual(violatingElementOrAttr, 'autocomplete');
+  });
+});
