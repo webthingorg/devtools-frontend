a2c4bf3763210016d65e4df96d4f4f11403e72a7
Philip Pfaffe <pfaffe@chromium.org>
2023-03-30 17:33:09 +0000

    Autostep over unmapped wasm locations
    Upon step over starting from a mapped wasm location, devtools will automatically continue stepping if the step causes us to pause on an unmapped wasm location. Autostepping also ends when it lands outside of the function we started in, i.e., after we returned from the function. The behavior is controlled by a new setting which is on by default.
    
    Fixed: 1418938
    Change-Id: I45010bc6536b01c2d00dba40348c585c08672a4e

--- front_end/models/bindings/DebuggerWorkspaceBinding.ts
+++ front_end/models/bindings/DebuggerWorkspaceBinding.ts
@@ -2,12 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type * as Common from '../../core/common/common.js';
+import * as Common from '../../core/common/common.js';
 import * as Platform from '../../core/platform/platform.js';
 import * as Root from '../../core/root/root.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import type * as TextUtils from '../text_utils/text_utils.js';
 import * as Workspace from '../workspace/workspace.js';
+import * as Protocol from '../../generated/protocol.js';
 
 import {CompilerScriptMapping} from './CompilerScriptMapping.js';
 import {DebuggerLanguagePluginManager} from './DebuggerLanguagePlugins.js';
@@ -159,6 +160,7 @@ export class DebuggerWorkspaceBinding implements SDK.TargetManager.SDKModelObser
   }
 
   modelAdded(debuggerModel: SDK.DebuggerModel.DebuggerModel): void {
+    debuggerModel.setBeforePausedCallback(this.beforePaused.bind(this));
     this.#debuggerModelToData.set(debuggerModel, new ModelData(debuggerModel, this));
     debuggerModel.setComputeAutoStepRangesCallback(this.computeAutoStepRanges.bind(this));
   }
@@ -488,6 +490,27 @@ export class DebuggerWorkspaceBinding implements SDK.TargetManager.SDKModelObser
   private debuggerResumed(event: Common.EventTarget.EventTargetEvent<SDK.DebuggerModel.DebuggerModel>): void {
     this.reset(event.data);
   }
+
+  private async beforePaused(
+      debuggerPausedDetails: SDK.DebuggerModel.DebuggerPausedDetails,
+      autoStep: SDK.DebuggerModel.Location|null): Promise<boolean> {
+    const {callFrames: [frame]} = debuggerPausedDetails;
+    if (!frame) {
+      return false;
+    }
+    const functionLocation = frame.functionLocation();
+    if (!autoStep || debuggerPausedDetails.reason !== Protocol.Debugger.PausedEventReason.Step || !functionLocation ||
+        !this.pluginManager || !frame.script.isWasm() || !Common.Settings.moduleSetting('wasmAutoStepping').get()) {
+      return true;
+    }
+    const uiLocation = await this.pluginManager.rawLocationToUILocation(frame.location());
+    if (uiLocation) {
+      return true;
+    }
+
+    return autoStep.script() !== functionLocation.script() || autoStep.columnNumber !== functionLocation.columnNumber ||
+        autoStep.lineNumber !== functionLocation.lineNumber;
+  }
 }
 
 class ModelData {
@@ -513,8 +536,6 @@ class ModelData {
     this.compilerMapping = new CompilerScriptMapping(debuggerModel, workspace, debuggerWorkspaceBinding);
 
     this.#locations = new Platform.MapUtilities.Multimap();
-
-    debuggerModel.setBeforePausedCallback(this.beforePaused.bind(this));
   }
 
   async createLiveLocation(
@@ -589,10 +610,6 @@ class ModelData {
     // and there's currently no way to inform the UI to update.
     // mappedLines = mappedLines ?? this.#resourceMapping.getMappedLines(uiSourceCode);
     return mappedLines;
-  }
-
-  private beforePaused(debuggerPausedDetails: SDK.DebuggerModel.DebuggerPausedDetails): boolean {
-    return Boolean(debuggerPausedDetails.callFrames[0]);
   }
 
   dispose(): void {
