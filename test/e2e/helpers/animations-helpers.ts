b91cab6543d1a4eec13960434c51bdae2729b870
Al Muthanna <almuthanna@chromium.org>
2023-04-01 20:59:12 +0200

    experiment with mutation wait
    Bug: none
    Change-Id: Iafd928862df1b058e04b73bcb32a70127d33aee3

--- test/e2e/helpers/animations-helpers.ts
+++ test/e2e/helpers/animations-helpers.ts
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {click, goToResource, waitFor} from '../../shared/helper.js';
+import {click, goToResource, waitFor, waitForFunction} from '../../shared/helper.js';
+
 import {openPanelViaMoreTools} from './settings-helpers.js';
 
 export async function waitForAnimationsPanelToLoad() {
@@ -17,9 +18,30 @@ export async function navigateToSiteWithAnimation() {
   await goToResource('animations/default.html');
 }
 
+export const waitForMutation = async () => {
+  const element = await waitFor('body');
+  let elementContent = await element.evaluate(e => e.outerHTML);
+  let stoppedMutating = 0;
+  await waitForFunction(async () => {
+    const newElementContent = await element.evaluate(e => e.outerHTML);
+    // find faster way to compare
+    if (elementContent === newElementContent) {
+      stoppedMutating++;
+    } else {
+      stoppedMutating = 0;
+    }
+    if (stoppedMutating > 10) {
+      return true;
+    }
+    elementContent = newElementContent;
+    return false;
+  });
+};
+
 export async function waitForAnimationContent() {
   const firstAnimationPreviewSelector = '.animation-buffer-preview[aria-label="Animation Preview 1"]';
   await waitFor(firstAnimationPreviewSelector);
+  await waitForMutation();
   await click(firstAnimationPreviewSelector);
   await waitFor('.animation-node-row');
   await waitFor('svg.animation-ui');
