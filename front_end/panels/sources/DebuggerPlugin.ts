dcf074976a027919ac73c447f3cf6e13315a11a0
Kim-Anh Tran <kimanh@chromium.org>
2023-04-05 10:52:10 +0200

    [uma] Merged: Adding uma data to the BreakpointEditDialog
    Changes:
    1. Adding another source from where we open the edit dialog (Linkifier)
    2. Log if a condition was edited after opening from the edit button
       in the breakpoint sidebar pane.
    
    Bug: 1430027
    Change-Id: I93fb9729eeda1e5e6cb29fe32b243a85cd53fca0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4394794
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    (cherry picked from commit 0bd17e7a3dfda613358142edb614ddcab78479fc)

--- front_end/panels/sources/DebuggerPlugin.ts
+++ front_end/panels/sources/DebuggerPlugin.ts
@@ -50,7 +50,11 @@ import {BreakpointEditDialog, type BreakpointEditDialogResult} from './Breakpoin
 import {Plugin} from './Plugin.js';
 import {ScriptFormatterEditorAction} from './ScriptFormatterEditorAction.js';
 import {SourcesPanel} from './SourcesPanel.js';
+<<<<<<< HEAD   (e4ccf4 [uma] Merged: Add UMA data to track removing/enabling/disabl)
 import {getRegisteredEditorActions} from './SourcesView.js';
+=======
+import {BreakpointsSidebarController} from './BreakpointsSidebarPane.js';
+>>>>>>> CHANGE (0bd17e [uma] Adding uma data to the BreakpointEditDialog)
 
 const {EMPTY_BREAKPOINT_CONDITION, NEVER_PAUSE_HERE_CONDITION} = Bindings.BreakpointManager;
 
@@ -825,9 +829,10 @@ export class DebuggerPlugin extends Plugin {
       dialog.detach();
       editor.dispatch({effects: compartment.reconfigure([])});
       if (!result.committed) {
+        BreakpointsSidebarController.instance().breakpointEditFinished(breakpoint, false);
         return;
       }
-
+      BreakpointsSidebarController.instance().breakpointEditFinished(breakpoint, oldCondition !== result.condition);
       recordBreakpointWithConditionAdded(result);
       if (breakpoint) {
         breakpoint.setCondition(result.condition, result.isLogpoint);
@@ -856,6 +861,7 @@ export class DebuggerPlugin extends Plugin {
     dialog.focusEditor();
     this.activeBreakpointDialog = dialog;
 
+    // This counts new conditional breakpoints or logpoints that are added.
     function recordBreakpointWithConditionAdded(result: BreakpointEditDialogResult): void {
       const {condition: newCondition, isLogpoint} = result;
       const isConditionalBreakpoint = newCondition.length !== 0 && !isLogpoint;
@@ -1631,6 +1637,8 @@ export class BreakpointLocationRevealer implements Common.Revealer.Revealer {
     const debuggerPlugin = debuggerPluginForUISourceCode.get(uiLocation.uiSourceCode);
     if (debuggerPlugin) {
       debuggerPlugin.editBreakpointLocation(breakpointLocation);
+    } else {
+      BreakpointsSidebarController.instance().breakpointEditFinished(breakpointLocation.breakpoint, false);
     }
   }
 }
--- front_end/panels/sources/components/BreakpointsView.ts
+++ front_end/panels/sources/components/BreakpointsView.ts
@@ -182,11 +182,11 @@ export class BreakpointSelectedEvent extends Event {
 
 export class BreakpointEditedEvent extends Event {
   static readonly eventName = 'breakpointedited';
-  data: {breakpointItem: BreakpointItem};
+  data: {breakpointItem: BreakpointItem, editButtonClicked: boolean};
 
-  constructor(breakpointItem: BreakpointItem) {
+  constructor(breakpointItem: BreakpointItem, editButtonClicked: boolean) {
     super(BreakpointEditedEvent.eventName);
-    this.data = {breakpointItem};
+    this.data = {breakpointItem, editButtonClicked};
   }
 }
 
@@ -379,7 +379,7 @@ export class BreakpointsView extends HTMLElement {
     const clickHandler = (event: Event): void => {
       Host.userMetrics.breakpointEditDialogRevealedFrom(
           Host.UserMetrics.BreakpointEditDialogRevealedFrom.BreakpointSidebarEditButton);
-      this.dispatchEvent(new BreakpointEditedEvent(breakpointItem));
+      this.dispatchEvent(new BreakpointEditedEvent(breakpointItem, true /* editButtonClicked */));
       event.consume();
     };
     const title = breakpointItem.type === SDK.DebuggerModel.BreakpointType.LOGPOINT ?
@@ -544,6 +544,14 @@ export class BreakpointsView extends HTMLElement {
     const editBreakpointText = breakpointItem.type === SDK.DebuggerModel.BreakpointType.LOGPOINT ?
         i18nString(UIStrings.editLogpoint) :
         i18nString(UIStrings.editCondition);
+<<<<<<< HEAD   (e4ccf4 [uma] Merged: Add UMA data to track removing/enabling/disabl)
+=======
+    menu.revealSection().appendItem(editBreakpointText, () => {
+      Host.userMetrics.breakpointEditDialogRevealedFrom(
+          Host.UserMetrics.BreakpointEditDialogRevealedFrom.BreakpointSidebarContextMenu);
+      this.dispatchEvent(new BreakpointEditedEvent(breakpointItem, false /* editButtonClicked */));
+    }, !editable);
+>>>>>>> CHANGE (0bd17e [uma] Adding uma data to the BreakpointEditDialog)
 
     menu.defaultSection().appendItem(i18nString(UIStrings.removeBreakpoint), () => {
       Host.userMetrics.actionTaken(Host.UserMetrics.Action.BreakpointRemovedFromContextMenu);
