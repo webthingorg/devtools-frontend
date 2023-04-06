a231da6ec1e2de1ed44a256f05f1e601d42fcd17
Al Muthanna Athamina <almuthanna@chromium.org>
2023-04-06 12:34:07 +0200

    Deflake source panel new page test
    Bug: 1418288, 1327683
    Change-Id: Ia7561bd6718ff109bba7188e044face89086d2ef

--- test/e2e/sources/authored-deployed-grouping_test.ts
+++ test/e2e/sources/authored-deployed-grouping_test.ts
@@ -306,9 +306,7 @@ describe('Source Panel grouping', async function() {
     assert.deepEqual(await readSourcesTreeView(), groupedRedundantExpectedTree);
   });
 
-  // The localhost domain is getting renamed, which breaks this test.
-  // TODO(crbug.com/1327683): Enable this once the domain displays correctly.
-  it.skip('[crbug.com/1327683] can load new page with group by authored/deployed', async () => {
+  it('can load new page with group by authored/deployed', async () => {
     // Have the target load the non-sourcemapped page.
     await goToResource('sources/multi-workers.html');
     await openSourcesPanel();
