26381e2595904ff2793479cc20fadfd3dbd77d87
Bruno Braga <brunobraga@google.com>
2023-04-17 10:52:54 +0000

    [autofill in devtool] popup
    [autofill in devtools] Squiggly line for violating element
    
    DISABLE_THIRD_PARTY_CHECK=Approach validation.
    
    Bug:1399414
    Change-Id: I129ae4093d7ad32d991456d0d264e21bc85a4b3d

--- front_end/panels/elements/ElementsTreeElement.ts
+++ front_end/panels/elements/ElementsTreeElement.ts
@@ -37,6 +37,7 @@ import * as Host from '../../core/host/host.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as Platform from '../../core/platform/platform.js';
 import * as SDK from '../../core/sdk/sdk.js';
+import type * as IssuesManager from '../../models/issues_manager/issues_manager.js';
 import * as TextUtils from '../../models/text_utils/text_utils.js';
 import * as CodeMirror from '../../third_party/codemirror.next/codemirror.next.js';
 import * as Adorners from '../../ui/components/adorners/adorners.js';
@@ -46,7 +47,8 @@ import * as TextEditor from '../../ui/components/text_editor/text_editor.js';
 import * as Components from '../../ui/legacy/components/utils/utils.js';
 import * as UI from '../../ui/legacy/legacy.js';
 import * as Emulation from '../emulation/emulation.js';
-import type * as IssuesManager from '../../models/issues_manager/issues_manager.js';
+import {IssuesPane} from '../issues/IssuesPane.js';
+
 import * as ElementsComponents from './components/components.js';
 import {canGetJSPath, cssPath, jsPath, xPath} from './DOMPath.js';
 import {ElementsPanel} from './ElementsPanel.js';
@@ -427,6 +429,33 @@ export class ElementsTreeElement extends UI.TreeOutline.TreeElement {
     }
   }
 
+  #issueCodeToTooltipTitle(issue_code: string): string {
+    switch (issue_code) {
+      case 'GenericIssue::FormLabelForNameError':
+        return 'Incorrect use of <label for=FORM_ELEMENT>';
+      case 'GenericIssue::FormDuplicateIdForInputError':
+        return 'Duplicate form field id in the same form';
+      case 'GenericIssue::FormInputWithNoLabelError':
+        return 'Form field without valid aria-labelledby attribute or associated label';
+      case 'GenericIssue::FormAutocompleteAttributeEmptyError':
+        return 'Incorrect use of autocomplete attribute';
+      case 'GenericIssue::FormEmptyIdAndNameAttributesForInputError':
+        return 'A form field element should have an id or name attribute';
+      case 'GenericIssue::FormAriaLabelledByToNonExistingId':
+        return 'An aria-labelledby attribute doesn\'t match any element id';
+      case 'GenericIssue::FormInputAssignedAutocompleteValueToIdOrNameAttributeError':
+        return 'An element doesn\'t have an autocomplete attribute';
+      case 'GenericIssue::FormLabelHasNeitherForNorNestedInput':
+        return 'No label associated with a form field';
+      case 'GenericIssue::FormLabelForMatchesNonExistingIdError':
+        return 'Incorrect use of <label for=FORM_ELEMENT>';
+      case 'GenericIssue::FormInputHasWrongButWellIntendedAutocompleteValueError':
+        return 'Non-standard autocomplete attribute value';
+      default:
+        return '';
+    }
+  }
+
   addIssue(newIssue: IssuesManager.GenericIssue.GenericIssue): void {
     if (this.#elementIssues.has(newIssue.primaryKey())) {
       return;
@@ -440,22 +469,47 @@ export class ElementsTreeElement extends UI.TreeOutline.TreeElement {
     const issueDetails = issue.details();
 
     if (issueDetails.violatingNodeAttribute) {
-      this.#highlightViolatingAttr(issueDetails.violatingNodeAttribute);
+      this.#highlightViolatingAttr(issueDetails.violatingNodeAttribute, issue);
     } else {
-      this.#highlightTagAsViolating();
+      this.#highlightTagAsViolating(issue);
     }
   }
 
-  #highlightTagAsViolating(): void {
-    this.listItemElement.getElementsByClassName('webkit-html-tag-name')[0].classList.add('violating-element');
+  #highlightTagAsViolating(issue: IssuesManager.Issue.Issue): void {
+    const tooltipTitle = this.#issueCodeToTooltipTitle(issue.code());
+    if (tooltipTitle.length === 0) {
+      return;
+    }
+
+    const tagElement = this.listItemElement.getElementsByClassName('webkit-html-tag-name')[0];
+    tagElement.classList.add('violating-element');
+    const violatingElementTooltip = tagElement.createChild('span', 'violating-element-tooltip');
+    violatingElementTooltip.textContent = tooltipTitle;
+    violatingElementTooltip.addEventListener('click', () => {
+      void UI.ViewManager.ViewManager.instance().showView('issues-pane');
+      IssuesPane.instance().reveal(issue);
+    });
   }
 
-  #highlightViolatingAttr(name: string): void {
+  #highlightViolatingAttr(name: string, issue: IssuesManager.Issue.Issue): void {
+    const tooltipTitle = this.#issueCodeToTooltipTitle(issue.code());
+    if (tooltipTitle.length === 0) {
+      return;
+    }
+
     const tag = this.listItemElement.getElementsByClassName('webkit-html-tag')[0];
     const attributes = tag.getElementsByClassName('webkit-html-attribute');
-    for (const attribute of attributes) {
-      if (attribute.getElementsByClassName('webkit-html-attribute-name')[0].textContent === name) {
-        attribute.getElementsByClassName('webkit-html-attribute-name')[0].classList.add('violating-element');
+    for (let i = 0; i < attributes.length; i++) {
+      if (attributes[i].getElementsByClassName('webkit-html-attribute-name')[0].textContent === name) {
+        const attributeElement = attributes[i].getElementsByClassName('webkit-html-attribute-name')[0];
+        attributeElement.classList.add('violating-element');
+
+        const violatingElementTooltip = attributeElement.createChild('span', 'violating-element-tooltip');
+        violatingElementTooltip.textContent = tooltipTitle;
+        violatingElementTooltip.addEventListener('click', () => {
+          void UI.ViewManager.ViewManager.instance().showView('issues-pane');
+          IssuesPane.instance().reveal(issue);
+        });
       }
     }
   }
