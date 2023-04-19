47458a82bab1cfc1c3b5900c5c4710ce6c907508
Randolf Jung <jrandolf@chromium.org>
2023-04-19 00:32:25 +0000

    Add `noImplicitOverride`
    `noImplicitOverride` guarantees type safety when overriding a class method. This also improves refactoring behavior.
    
    Change-Id: Ia095c38a6d1fc160f9b08c350d6f7248daab38a4

--- front_end/panels/application/StorageView.ts
+++ front_end/panels/application/StorageView.ts
@@ -475,7 +475,7 @@ export class StorageView extends UI.ThrottledWidget.ThrottledWidget {
     }
   }
 
-  async doUpdate(): Promise<void> {
+  override async doUpdate(): Promise<void> {
     if (!this.securityOrigin || !this.target) {
       this.quotaRow.textContent = '';
       this.populatePieChart(0, []);
@@ -556,7 +556,7 @@ export class StorageView extends UI.ThrottledWidget.ThrottledWidget {
         return i18nString(UIStrings.other);
     }
   }
-  wasShown(): void {
+  override wasShown(): void {
     super.wasShown();
     this.reportView.registerCSSFiles([storageViewStyles]);
     this.registerCSSFiles([storageViewStyles]);
