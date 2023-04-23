73ae151770b525c07ef81ea1242c02690c8528e8
Bruno Braga <brunobraga@google.com>
2023-04-23 20:11:44 +0000

    [autofill in devtool] Introduce popup that links to the issues panel.
    https://screencast.googleplex.com/cast/NTc3NjA3MjMwMjkxOTY4MHw0NzhjOGI0ZS1hMQ
    
    Bug:1399414
    Change-Id: I129ae4093d7ad32d991456d0d264e21bc85a4b3d

--- test/e2e/elements/violating-element-and-attributes_test.ts
+++ test/e2e/elements/violating-element-and-attributes_test.ts
@@ -4,16 +4,18 @@
 
 import {assert} from 'chai';
 
-import {enableExperiment, goToResource, waitForMany} from '../../shared/helper.js';
+import {click, enableExperiment, waitFor, waitForMany} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
+import {expandSelectedNodeRecursively, goToResourceAndWaitForStyleSection} from '../helpers/elements-helpers.js';
 
 /**
  * Skipping this test for now as it only works on non-headless chrome.
  */
-describe.skip('[crbug.com/1399414]: Element has violating properties', async function() {
+describe('[crbug.com/1399414]: Element has violating properties', async function() {
   beforeEach(async function() {
     await enableExperiment('highlightErrorsElementsPanel');
-    await goToResource('elements/form-with-issues.html');
+    await goToResourceAndWaitForStyleSection('elements/form-with-issues.html');
+    await expandSelectedNodeRecursively();
   });
 
   it('tag is highlighted on input without name nor id', async () => {
@@ -21,9 +23,35 @@ describe.skip('[crbug.com/1399414]: Element has violating properties', async fun
     const violatingElementOrAttr = await elements[0].evaluate(node => node.textContent);
     assert.strictEqual(violatingElementOrAttr, 'input');
   });
+
   it('autocomplete attribute is highlighted when empty.', async () => {
     const elements = await waitForMany('.violating-element', 2);
     const violatingElementOrAttr = await elements[1].evaluate(node => node.textContent);
     assert.strictEqual(violatingElementOrAttr, 'autocomplete');
+  });
+
+  it('navigate to issues panel on hover', async () => {
+    const elements = await waitForMany('.violating-element', 2);
+    const violatingElementOrAttr = elements[0];
+    await violatingElementOrAttr.hover();
+    const popupParent = await waitFor('div.vbox.flex-auto.no-pointer-events');
+    const popupText = await popupParent.evaluate(async (node: Element) => {
+      if (!node.shadowRoot) {
+        throw new Error('Node shadow root not found.');
+      }
+      const popup = node.shadowRoot.querySelector('div.widget.has-padding');
+      if (!popup) {
+        throw new Error('Popup not found.');
+      }
+      return popup.textContent;
+    });
+
+    const expectedTitle = 'A form field element should have an id or name attribute';
+    assert.strictEqual(popupText, expectedTitle);
+    // Open the issue panel and look for the title.
+    await click('div.widget.has-padding');
+    const highlitedIssue = await waitFor('.issue .header .title');
+    const issueTitle = await highlitedIssue.evaluate(async (node: Element) => node.textContent);
+    assert.strictEqual(issueTitle, expectedTitle);
   });
 });
