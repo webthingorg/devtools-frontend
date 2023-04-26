100b0d51e4807a118a88ec856e132d6b60b374b4
Robert Lin <robertlin@chromium.org>
2023-04-26 14:52:18 +0900

    PreloadingPanel: Show warnings if preloadingState is disabled
    This CL shows a warnings if preloadingState is disabled utilizing
    getPreloadPagesState.
    
    Demo: https://drive.google.com/file/d/1CJz4QfDDWI2XTyadQDUPz_24ScUmDJRQ/view?usp=share_link
    
    Bug: 1384419
    Change-Id: I3b83f272d4a1d53e0c7bcd22227c861a996e5448

--- front_end/panels/application/preloading/PreloadingView.ts
+++ front_end/panels/application/preloading/PreloadingView.ts
@@ -4,6 +4,7 @@
 
 import type * as DataGrid from '../../../ui/components/data_grid/data_grid.js';
 
+import * as ChromeLink from '../../../ui/components/chrome_link/chrome_link.js';
 import * as i18n from '../../../core/i18n/i18n.js';
 import * as UI from '../../../ui/legacy/legacy.js';
 import * as Protocol from '../../../generated/protocol.js';
@@ -70,6 +71,25 @@ const UIStrings = {
    */
   warningDetailPrerenderingDisabledByFeatureFlag:
       'Prerendering is forced-enabled because DevTools is open. When DevTools is closed, prerendering will be disabled because this browser session is part of a holdback group used for performance comparisons.',
+  /**
+   *@description Title of preloading state disabled warning in infobar
+   */
+  warningTitlePreloadingStateDisabled: 'Preloading is disabled',
+  /**
+   *@description Detail of preloading sate disabled warning in infobar
+   *@example {chrome://settings/preloading} PH1
+   *@example {chrome://extensions} PH2
+   */
+  warningDetailPreloadingStateDisabled:
+      'Preloading is disabled because of an extension. Go to {PH1} to learn more, or go to {PH2} to disable the extension.',
+  /**
+   *@description Text of Preload pages settings
+   */
+  preloadingPageSettings: 'Preload pages settings',
+  /**
+   *@description Text of Extension settings
+   */
+  extensionSettings: 'Extensions settings',
 };
 const str_ = i18n.i18n.registerUIStrings('panels/application/preloading/PreloadingView.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
@@ -335,6 +355,18 @@ export class PreloadingView extends UI.Widget.VBox {
         preloadingAttempt,
         ruleSets,
       };
+    }
+    if (this.modelProxy.model.getPreloadEnabledState() === 'DisabledByPreference') {
+      const preloadingSettingLink = new ChromeLink.ChromeLink.ChromeLink();
+      preloadingSettingLink.href = 'chrome://settings/preloading';
+      preloadingSettingLink.textContent = i18nString(UIStrings.preloadingPageSettings);
+      const extensionSettingLink = new ChromeLink.ChromeLink.ChromeLink();
+      extensionSettingLink.href = 'chrome://extensions';
+      extensionSettingLink.textContent = i18nString(UIStrings.extensionSettings);
+      const detailsMessage = i18n.i18n.getFormatLocalizedString(
+          str_, UIStrings.warningDetailPreloadingStateDisabled,
+          {PH1: preloadingSettingLink, PH2: extensionSettingLink});
+      this.showInfobar(i18nString(UIStrings.warningTitlePreloadingStateDisabled), detailsMessage);
     }
   }
 
