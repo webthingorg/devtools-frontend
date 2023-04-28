b9bfc0b64f657267a294ee690226cb2bb6bbd5bd
Nancy Li <nancyly@chromium.org>
2023-04-28 18:48:04 +0000

    [RPP] Add tests for loading cpu profile
    - Also combine the cpuprofile.html logic to basic.html since they have a few duplicate code.
    
    Bug: 1429602
    Change-Id: If0ed05904a1d5289d493d0a50957236497ee28ff

--- test/interactions/panels/performance/timeline/timeline_test.ts
+++ test/interactions/panels/performance/timeline/timeline_test.ts
@@ -16,4 +16,18 @@ describe('Performance panel', () => {
     const panel = await waitFor('body');
     await assertElementScreenshotUnchanged(panel, 'performance/timeline.png', 3);
   });
+
+  itScreenshot('renders the CPU profile correctly in non-node mode', async () => {
+    await loadComponentDocExample('performance_panel/basic.html?cpuprofile=node-fibonacci-website');
+    await waitFor('.timeline-flamechart');
+    const panel = await waitFor('body');
+    await assertElementScreenshotUnchanged(panel, 'performance/cpu-profile.png', 3);
+  });
+
+  itScreenshot('renders the CPU profile correctly in node mode', async () => {
+    await loadComponentDocExample('performance_panel/basic.html?cpuprofile=node-fibonacci-website&isNode=true');
+    await waitFor('.timeline-flamechart');
+    const panel = await waitFor('body');
+    await assertElementScreenshotUnchanged(panel, 'performance/cpu-profile-node.png', 3);
+  });
 });
