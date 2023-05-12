e4269371a0bba7913b1dbd9b4efb327cc5f162b1
Nancy Li <nancyly@chromium.org>
2023-05-12 09:24:25 +0000

    [RPP] Hide timings track when there is no event
    Also changed some parameters' name to make them more clear
    
    Bug: none
    Change-Id: If01360aedab31d8013d471d4b0a9b8c2aa42633a

--- front_end/panels/timeline/InteractionsTrackAppender.ts
+++ front_end/panels/timeline/InteractionsTrackAppender.ts
@@ -53,19 +53,19 @@ export class InteractionsTrackAppender implements TrackAppender {
   /**
    * Appends into the flame chart data the data corresponding to the
    * interactions track.
-   * @param level the horizontal level of the flame chart events where
+   * @param trackStartLevel the horizontal level of the flame chart events where
    * the track's events will start being appended.
    * @param expanded wether the track should be rendered expanded.
    * @returns the first available level to append more data after having
    * appended the track's events.
    */
-  appendTrackAtLevel(currentLevel: number, expanded?: boolean): number {
+  appendTrackAtLevel(trackStartLevel: number, expanded?: boolean): number {
     if (this.#traceParsedData.UserInteractions.interactionEvents.length === 0) {
-      return currentLevel;
+      return trackStartLevel;
     }
-    this.#appendTrackHeaderAtLevel(currentLevel, expanded);
+    this.#appendTrackHeaderAtLevel(trackStartLevel, expanded);
     const interactions = this.#traceParsedData.UserInteractions.interactionEventsWithNoNesting;
-    return this.#compatibilityBuilder.appendEventsAtLevel(interactions, currentLevel, this);
+    return this.#compatibilityBuilder.appendEventsAtLevel(interactions, trackStartLevel, this);
   }
 
   /**
