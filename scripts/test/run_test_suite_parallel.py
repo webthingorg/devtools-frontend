cd6750d83292647884d2eec19243531dbe6c40cc
Al Muthanna Athamina <almuthanna@chromium.org>
2023-05-16 10:06:06 +0200

    [experiment] Multiprocessing with python
    Bug: none
    Change-Id: I8204eeaa73d3d53e1c1b2ab6e28b5ff55d7e9d8a

---
+++ scripts/test/run_test_suite_parallel.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env vpython3
+#
+# Copyright 2023 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+import time
+
+test_files = []
+for root, dirs, files in os.walk(os.getcwd() + '/test/e2e'):
+    for file in files:
+        if file.endswith('_test.ts'):
+            test_files.append(root.split("/")[-1] + '/' + file)
+
+chunks = 7
+divided_test_files = []
+
+for i in range(chunks):
+    divided_test_files.append(test_files[i:i + (len(test_files) // chunks)])
+
+processes = []
+for l in divided_test_files:
+    pattern = ','.join(l)
+    processes.append(
+        subprocess.Popen(
+            'vpython3 third_party/node/node.py --output scripts/test/run_test_suite.js --config=test/e2e/test-runner-config.json --test-file-pattern='
+            + pattern,
+            shell=True))
+
+start_time = time.time()
+for proc in processes:
+    proc.communicate()
+
+print('Run Time: ' + str(round(time.time() - start_time, 2)) + ' seconds')
