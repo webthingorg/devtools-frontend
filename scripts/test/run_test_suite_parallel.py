3a404a704b1309fce5bf8ff0f5d8bf98b1fe371b
Al Muthanna Athamina <almuthanna@chromium.org>
2023-05-16 16:01:27 +0200

    [experiment] Multiprocessing with python
    Bug: none
    Change-Id: I8204eeaa73d3d53e1c1b2ab6e28b5ff55d7e9d8a

---
+++ scripts/test/run_test_suite_parallel.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env vpython3
+#
+# Copyright 2023 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+import sys
+import time
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import devtools_paths
+
+test_files = []
+for root, dirs, files in os.walk(os.path.join(os.getcwd(), 'test', 'e2e')):
+    for file in files:
+        if file.endswith('_test.ts'):
+            test_files.append(os.path.join(os.path.split(root)[-1], file))
+
+chunks = 4
+divided_test_files = []
+
+for i in range(chunks):
+    divided_test_files.append(test_files[i:i + (len(test_files) // chunks)])
+
+processes = []
+for l in divided_test_files:
+    command = [
+        devtools_paths.node_path(),
+        os.path.join(devtools_paths.devtools_root_path(), 'scripts', 'test',
+                     'run_test_suite.js'),
+        '--config=test/e2e/test-runner-config.json',
+        '--test-file-pattern=' + ','.join(l)
+    ]
+    processes.append(subprocess.Popen(' '.join(command), shell=True))
+
+start_time = time.time()
+for proc in processes:
+    proc.communicate()
+
+print('Run Time: ' + str(round(time.time() - start_time, 2)) + ' seconds')
