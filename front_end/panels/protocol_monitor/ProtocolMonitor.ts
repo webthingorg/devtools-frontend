5688755a66c481131fc02191d05b417890c930dc
Hadrien Jaubert <hadrienjaubert@google.com>
2023-06-07 14:49:28 +0000

    Update the code of the CDP command editor to Lit Element
    Bug: 1452134
    Change-Id: I4b5c94b5f597b735a659b799ea4c4c45aed0de99

--- front_end/panels/protocol_monitor/ProtocolMonitor.ts
+++ front_end/panels/protocol_monitor/ProtocolMonitor.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './components/EditorWidget.js';
+
 import * as Common from '../../core/common/common.js';
 import * as Host from '../../core/host/host.js';
 import * as i18n from '../../core/i18n/i18n.js';
@@ -16,7 +18,7 @@ import * as SourceFrame from '../../ui/legacy/components/source_frame/source_fra
 import * as UI from '../../ui/legacy/legacy.js';
 import * as LitHtml from '../../ui/lit-html/lit-html.js';
 
-import {JSONPromptEditor} from './JSONPromptEditor.js';
+import {type EditorElement} from './components/EditorWidget.js';
 import protocolMonitorStyles from './protocolMonitor.css.js';
 
 const UIStrings = {
@@ -208,7 +210,7 @@ export class ProtocolMonitorImpl extends UI.Widget.VBox {
     splitTextAreaEditor.show(this.contentElement);
     this.infoWidget = new InfoWidget();
     this.#editorWidget.addEventListener(Events.CommandSent, event => {
-      this.#onCommandSend(JSON.stringify(event.data));
+      this.onCommandSend(JSON.stringify(event.data));
     });
 
     const dataGridInitialData: DataGrid.DataGridController.DataGridControllerData = {
@@ -374,7 +376,7 @@ export class ProtocolMonitorImpl extends UI.Widget.VBox {
     const input = new UI.Toolbar.ToolbarInput(
         placeholder, accessiblePlaceholder, growFactor, shrinkFactor, tooltip,
         this.#commandAutocompleteSuggestionProvider.buildTextPromptCompletions, false);
-    input.addEventListener(UI.Toolbar.ToolbarInput.Event.EnterPressed, () => this.#onCommandSend(input.value()));
+    input.addEventListener(UI.Toolbar.ToolbarInput.Event.EnterPressed, () => this.onCommandSend(input.value()));
     input.addEventListener(UI.Toolbar.ToolbarInput.Event.TextChanged, () => this.#onCommandChange(input));
 
     return input;
@@ -397,7 +399,7 @@ export class ProtocolMonitorImpl extends UI.Widget.VBox {
     return selector;
   }
 
-  #onCommandSend(input: string): void {
+  onCommandSend(input: string): void {
     const {command, parameters} = parseCommandInput(input);
     const test = ProtocolClient.InspectorBackend.test;
     const targetManager = SDK.TargetManager.TargetManager.instance();
@@ -618,11 +620,18 @@ export class CommandAutocompleteSuggestionProvider {
     if (!prefix && !force && expression) {
       return [];
     }
+
     const newestToOldest = [...this.#commandHistory].reverse();
     newestToOldest.push(...this.#protocolMethods);
     return newestToOldest.filter(cmd => cmd.startsWith(prefix)).map(text => ({
                                                                       text,
                                                                     }));
+  };
+
+  buildNewTextPromptCompletions = (): string[] => {
+    const newestToOldest = [...this.#commandHistory].reverse();
+    newestToOldest.push(...this.#protocolMethods);
+    return newestToOldest;
   };
 
   buildProtocolCommands(domains: Iterable<ProtocolDomain>): Set<string> {
@@ -699,63 +708,40 @@ export interface Command {
   command: string;
   parameters: object;
 }
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'devtools-editor-element': EditorElement;
+  }
+}
+
 export class EditorWidget extends Common.ObjectWrapper.eventMixin<EventTypes, typeof UI.Widget.VBox>(UI.Widget.VBox) {
-  private readonly promptContainer: HTMLElement;
-  private readonly promptElement: HTMLElement;
-  readonly promptList: HTMLElement;
-  private readonly promptInner: HTMLElement;
-  #commandAutocompleteSuggestionProvider: CommandAutocompleteSuggestionProvider;
-  private jsonPromptEditors: JSONPromptEditor[] = [];
-  private commandPromptEditor: JSONPromptEditor;
+  readonly promptContainer: EditorElement;
   constructor(commandAutocompleteSuggestionProvider: CommandAutocompleteSuggestionProvider) {
     super();
-    // TODO: fix ad hoc section property in a separate CL to be safe
-    this.promptContainer = this.element.createChild('div', 'cdp-command-prompt-container');
-    this.promptElement = this.promptContainer.createChild('div');
-    this.promptList = this.promptElement.createChild('ul');
-    this.promptList.style.paddingLeft = '0px';
-    this.promptInner = this.promptList.createChild('div');
+    this.promptContainer = document.createElement('devtools-editor-element');
+    this.promptContainer.commandAutocompleteSuggestionProvider = commandAutocompleteSuggestionProvider;
+
+    this.element.append(this.promptContainer);
     this.promptContainer.addEventListener('keydown', (event: Event) => {
-      if ((event as KeyboardEvent).key === 'Enter') {
+      if ((event as KeyboardEvent).key === 'Enter' && (event as KeyboardEvent).ctrlKey) {
         this.dispatchEventToListeners(Events.CommandSent, this.getCommand());
       }
     });
-
-    this.#commandAutocompleteSuggestionProvider = commandAutocompleteSuggestionProvider;
-    this.commandPromptEditor = new JSONPromptEditor('command', '', this.#commandAutocompleteSuggestionProvider);
-
-    const output = this.commandPromptEditor.render();
-    LitHtml.render(output, this.promptInner, {host: this});
   }
 
   getCommand(): Command {
     return {
-      command: this.commandPromptEditor.getText(),
-      parameters: this.jsonPromptEditors.reduce<{[key: string]: string}>(
-          (parameters, editor) => {
-            parameters[editor.getKey()] = editor.getText();
-            return parameters;
-          },
-          {}),
+      command: this.promptContainer.getCommand(),
+      parameters: this.promptContainer.getParameters(),
     };
   }
 
   setCommand(command: string, parameters: {
     [x: string]: unknown,
   }): void {
-    this.commandPromptEditor.setText(command);
-    this.jsonPromptEditors = [];
-    if (parameters) {
-      for (const key of Object.keys(parameters)) {
-        const value = JSON.stringify(parameters[key]);
-        const jsonPromptEditor = new JSONPromptEditor(key, value, this.#commandAutocompleteSuggestionProvider);
-        this.jsonPromptEditors.push(jsonPromptEditor);
-      }
-
-      const output = this.jsonPromptEditors.map(editor => editor.render());
-
-      LitHtml.render(output, this.promptList, {host: this});
-    }
+    this.promptContainer.parameters = parameters;
+    this.promptContainer.command = command;
   }
 }
 
