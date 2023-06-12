99fc40faa4fe0b26bb8b884a6170f03400e2e283
Al Muthanna Athamina <almuthanna@chromium.org>
2023-06-12 11:59:28 +0200

    Add scripts to split the e2e tests command and a local parallel runner
    This adds two scripts: one that divides the E2E tests command into
    multiple ones that can be run separately, and another script that runs
    these commands in parallel Python processes. This will allow us to run
    the E2E tests locally but it is also necessary for running tests in
    parallel on the builders.
    
    Bug: 1419802
    Change-Id: I7e4c2a6f1847c3a63cab1cc2365ddaa69b77f2fb

---
+++ scripts/test/run_test_suite_parallel.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env vpython3
+#
+# Copyright 2023 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import json
+import os
+import subprocess
+import sys
+import tempfile
+import time
+from e2e_divider import divide_run
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import devtools_paths
+
+
+def parse_options(cli_args):
+    parser = argparse.ArgumentParser(description='Run tests')
+    parser.add_argument(
+        '--test-suite-source-dir',
+        dest='test_suite_source_dir',
+        default='test/e2e',
+        help=
+        'Path to the source folder containing the tests, relative to the current working directory.'
+    )
+    parser.add_argument(
+        '--jobs',
+        dest='jobs',
+        default=4,
+        help='Number of parallel runners to use (if supported). Defaults to 1.'
+    )
+    parser.add_argument(
+        '--test-file-pattern',
+        dest='test_file_pattern',
+        default=None,
+        help=
+        'A comma separated glob (or just a file path) to select specific test files to execute.'
+    )
+    parser.add_argument('--iterations',
+                        dest='iterations',
+                        default=1,
+                        help='Number of test iterations.')
+    parser.add_argument(
+        '--no-failure-screenshots-file',
+        dest='no_failure_screenshots_file',
+        action='store_true',
+        help='Does not save screenshots to failure_screenshots.html.')
+    return parser.parse_args(cli_args)
+
+
+class ColoredPrint:
+    def __init__(self):
+        self.color_codes = {
+            'no_color': '0',
+            'red': '0;31',
+            'green': '0;32',
+            'yellow': '1;33',
+            'blue': '0;34',
+            'purple': '0;35',
+            'gray': '0;37',
+            'white': '1;37',
+        }
+
+        if os.popen('tput colors').read().strip() == '256':
+            self.supports_color = True
+        else:
+            self.supports_color = False
+
+    def cprint(self, text, color='no_color'):
+        if self.supports_color:
+            print(f'\033[{self.color_codes[color]}m {text} \033[0m')
+        else:
+            print(text)
+
+
+if __name__ == '__main__':
+    args = parse_options(sys.argv[1:])
+    commands = divide_run(chunks=int(args.jobs),
+                          test_suite_source_dir=args.test_suite_source_dir,
+                          pattern=args.test_file_pattern,
+                          iterations=int(args.iterations))
+
+    screenshots = ''
+    env = os.environ.copy()
+    if not args.no_failure_screenshots_file:
+        env["HTML_OUTPUT_FILE"] = f'{devtools_paths.devtools_root_path()}/out/failure_screenshots.html'
+
+    results_log_files = []
+    processes = []
+    for i in range(len(commands)):
+        temp = tempfile.NamedTemporaryFile(delete=False)
+        open(temp.name, 'a')
+        results_log_files.append(temp)
+        processes.append(
+            subprocess.Popen(
+                f'{" ".join(commands[i])} --mocha-reporter json-stream',
+                env=env,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                shell=True))
+
+    start_time = time.time()
+
+    while None in [p.poll() for p in processes]:
+        for i in range(len(processes)):
+            line = processes[i].stdout.readline()
+            if line:
+                results_log_files[i].write(line)
+                print(line.decode("utf-8"), end='')
+
+    for f in results_log_files:
+        f.close()
+
+    failed_tests = []
+    tests, passes, pending, failures = 0, 0, 0, 0
+    for i in range(len(commands)):
+        with open(results_log_files[i].name, "r") as f:
+            for l in f.readlines():
+                try:
+                    json_line = json.loads(l)
+                    if 'fail' in json_line:
+                        failed_tests.append(json.loads(l)[1])
+                    if 'end' in json_line:
+                        result = json.loads(l)[1]
+                        tests += result['tests']
+                        passes += result['passes']
+                        pending += result['pending']
+                        failures += result['failures']
+                except:
+                    pass
+
+    for tf in results_log_files:
+        tf.close()
+
+    colored_print = ColoredPrint()
+
+    print('\n\nFailed tests:')
+    for f in failed_tests:
+        print(json.dumps(f, indent=2))
+        print()
+
+    print()
+    colored_print.cprint(f'Total tests: {tests}', 'yellow')
+    colored_print.cprint(f'Passed: {passes}', 'green')
+    colored_print.cprint(f'Pending: {pending}', 'blue')
+    colored_print.cprint(f'Failures: {failures}', 'red')
+    print()
+
+    if not args.no_failure_screenshots_file and failed_tests:
+        colored_print.cprint(
+            f'Failure screenshots: {devtools_paths.devtools_root_path()}/out/failure_screenshots.html',
+            'white')
+        print()
+
+    colored_print.cprint(
+        'Run Time: ' + str(round(
+            (time.time() - start_time) / 60, 2)) + ' minutes', 'white')
