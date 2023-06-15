eab8f9792a4cf1ea3930eda79afb282148548c4c
Hadrien Jaubert <hadrienjaubert@google.com>
2023-06-15 16:51:24 +0000

    [Protocol Monitor] Change CDP command editor so that it displays the parameters corresponding to a specific command.
    Description:
    - A user can now enter a command and corresponding parameters will appear below it (first the required ones, then the optinal ones)
    - It has support only at the moment for parameters of type string, boolean and number
    
    Screenshot : https://bugs.chromium.org/p/chromium/issues/detail?id=1454742
    
    Bug: 1454742
    Change-Id: I0a4424a8ef060b6f1eedf06d99da3aa609c204de

--- front_end/panels/protocol_monitor/components/JSONEditor.ts
+++ front_end/panels/protocol_monitor/components/JSONEditor.ts
@@ -8,34 +8,115 @@ import * as RecorderComponents from '../../recorder/components/components.js';
 
 import editorWidgetStyles from './JSONEditor.css.js';
 
-const {html, Decorators, LitElement, nothing} = LitHtml;
+const {html, Decorators, LitElement, Directives, nothing} = LitHtml;
 const {customElement, property, state} = Decorators;
+const {live} = Directives;
 declare global {
   interface HTMLElementTagNameMap {
     'devtools-json-editor': JSONEditor;
   }
 }
 
+export interface Parameter {
+  type: string;
+  optional: boolean;
+  value: string|undefined;
+  name: string;
+}
+
+interface Command {
+  command: string;
+  parameters: object;
+}
+
+/**
+ * Parents should listen for this event and register the listeners provided by
+ * this event"
+ */
+export class SubmitEditorEvent extends Event {
+  static readonly eventName = 'submiteditor';
+  readonly data: Command;
+
+  constructor(data: Command) {
+    super(SubmitEditorEvent.eventName);
+    this.data = data;
+  }
+}
+
 @customElement('devtools-json-editor')
 export class JSONEditor extends LitElement {
   static override styles = [editorWidgetStyles];
-  @property() declare jsonPromptEditors: RecorderComponents.RecorderInput.RecorderInput[];
-  @property() declare parameters: {[x: string]: unknown};
-  @property() declare protocolMethods: string[];
+  @property() declare protocolMethodWithParametersMap: Map<string, Parameter[]>;
+  @state() declare parameters: Record<string, Parameter>;
   @state() command: string = '';
 
-  getCommand(): string {
-    return this.command;
+  constructor() {
+    super();
+    this.parameters = {};
+    this.addEventListener('keydown', (event: Event) => {
+      if ((event as KeyboardEvent).key === 'Enter' &&
+          ((event as KeyboardEvent).metaKey || (event as KeyboardEvent).ctrlKey)) {
+        this.dispatchEvent(new SubmitEditorEvent({
+          command: this.command,
+          parameters: this.getParameters(),
+        }));
+      }
+    });
   }
 
   getParameters(): {[x: string]: unknown} {
-    return this.parameters;
+    const formattedParameters: {[x: string]: unknown} = {};
+    for (const [key, param] of Object.entries(this.parameters)) {
+      if (param.type === 'number') {
+        formattedParameters[key] = Number(param.value);
+      } else if (param.type === 'boolean') {
+        formattedParameters[key] = Boolean(param.value);
+      } else {
+        formattedParameters[key] = param.value;
+      }
+    }
+    return formattedParameters;
   }
 
+  #populateParametersForCommand(command: string): void {
+    const parameters = this.protocolMethodWithParametersMap.get(command);
+    if (parameters && parameters.length !== 0) {
+      const parametersPerCommand = this.protocolMethodWithParametersMap.get(this.command);
+      if (parametersPerCommand) {
+        for (const parameter of parametersPerCommand) {
+          this.parameters[parameter.name] = {
+            optional: parameter.optional,
+            type: parameter.type,
+            value: parameter.value || undefined,
+            name: parameter.name,
+          };
+        }
+        this.requestUpdate();
+      }
+    }
+  }
+
+  #sanitizeValue(value: string|undefined): string|void {
+    if (value) {
+      return value.replace(/['"]+/g, '');
+    }
+  }
+
+  #handleParameterInputBlur = async(event: Event, parameterName: string): Promise<void> => {
+    if (event.target instanceof RecorderComponents.RecorderInput.RecorderInput) {
+      const value = event.target.value;
+      this.#sanitizeValue(value);
+      this.parameters[parameterName].value = value;
+    }
+  };
+
   #handleTypeInputBlur = async(event: Event): Promise<void> => {
+    this.parameters = {};
     if (event.target instanceof RecorderComponents.RecorderInput.RecorderInput) {
       this.command = event.target.value;
     }
+    this.#populateParametersForCommand(this.command);
+    this.requestUpdate();
   };
 
   #renderCommandRow(): LitHtml.TemplateResult|undefined {
@@ -44,7 +125,7 @@ export class JSONEditor extends LitElement {
       <div>command<span class="separator">:</span></div>
       <devtools-recorder-input
         .disabled=${false}
-        .options=${this.protocolMethods}
+        .options=${[...this.protocolMethodWithParametersMap.keys()]}
         .value=${this.command}
         .placeholder=${'Enter your command...'}
         @blur=${this.#handleTypeInputBlur}
@@ -69,22 +150,29 @@ export class JSONEditor extends LitElement {
    * Renders the parameters list corresponding to a specific CDP command.
    */
   #renderParameters(parameters: {
-    [x: string]: unknown,
+    [x: string]: Parameter,
   }): LitHtml.TemplateResult|undefined {
+    parameters = Object.fromEntries(
+        Object.entries(parameters)
+            .sort(([, a]: [string, Parameter], [, b]: [string, Parameter]) => Number(a.optional) - Number(b.optional)),
+    );
     // clang-format off
     return html`
       <ul>
-        ${Object.keys(parameters).map(key => {
-        const value = JSON.stringify(parameters[key]);
-        return html`
-              <div class="row attribute padded double" data-attribute="type">
-                <div>${key}<span class="separator">:</span></div>
-                <devtools-recorder-input
-                  .disabled=${false}
-                  .value=${value}
-                  .placeholder=${'Enter your parameter...'}
-                ></devtools-recorder-input>
-              </div>
+      ${Object.keys(parameters).map(key => {
+      const value = JSON.stringify(this.#sanitizeValue(parameters[key].value));
+      return html`
+            <div class="row attribute padded double" data-attribute="type">
+              <div class=${parameters[key].optional && 'color-blue'}>${key}<span class="separator">:</span></div>
+              <devtools-recorder-input
+                .disabled=${false}
+                .value=${live(value !== undefined ? this.#sanitizeValue(value) : '')}
+                .placeholder=${'Enter your parameter...'}
+                @blur=${async (event: Event) : Promise<void> => {
+                  await this.#handleParameterInputBlur(event, key);}
+                }
+              ></devtools-recorder-input>
+            </div>
             `;
         })}
       </ul>`;
