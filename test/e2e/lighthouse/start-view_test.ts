874e4a0191b67b6683439b0e92357d7f81f47f03
Alex Rudenko <alexrudenko@chromium.org>
2023-06-15 15:40:48 +0200

    Reland "Update Puppeteer to v20.7.1 (front_end)"
    This is a reland of commit 85d5950706e67782dbfb61f08d388706be28e591
    
    Original change's description:
    > Update Puppeteer to v20.7.1 (front_end)
    >
    > Bug: none
    > Change-Id: Icbb5e86277224dbf6f78fb23d9f1f196be6dfb13
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4608422
    > Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    > Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    
    Bug: none
    Change-Id: I5427c6afe750d2feb1b00acc1b5a4e4bc1dff926

--- test/e2e/lighthouse/start-view_test.ts
+++ test/e2e/lighthouse/start-view_test.ts
@@ -15,7 +15,7 @@ import {
   waitForStorageUsage,
 } from '../helpers/lighthouse-helpers.js';
 
-describe('The Lighthouse start view', async () => {
+describe.only('The Lighthouse start view', async () => {
   it('shows a button to generate a new report', async () => {
     await navigateToLighthouseTab('empty.html');
 
