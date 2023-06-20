6eca14e4bd3bf2e6411a7768cbb03a0f78c6fc9d
Alex Rudenko <alexrudenko@chromium.org>
2023-06-20 08:59:36 +0200

    Roll puppeteer + replay
    Bug: none
    Change-Id: I8994f0f82f1a760e27ae9fbd462d44aa46cde7f6

--- front_end/third_party/puppeteer/package/lib/cjs/third_party/mitt/index.js
+++ front_end/third_party/puppeteer/package/lib/cjs/third_party/mitt/index.js
@@ -1,5 +1,29 @@
 'use strict';
 
+function getDefaultExportFromCjs$2 (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
+function getDefaultExportFromCjs$1 (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
+function getDefaultExportFromCjs (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
 function mitt(n){return {all:n=n||new Map,on:function(t,e){var i=n.get(t);i?i.push(e):n.set(t,[e]);},off:function(t,e){var i=n.get(t);i&&(e?i.splice(i.indexOf(e)>>>0,1):n.set(t,[]));},emit:function(t,e){var i=n.get(t);i&&i.slice().map(function(n){n(e);}),(i=n.get("*"))&&i.slice().map(function(n){n(t,e);});}}}
 
-module.exports = mitt;
+var mitt_1 = mitt;
+
+var index = /*@__PURE__*/getDefaultExportFromCjs(mitt_1);
+
+var mitt_2 = index;
+
+var index$1 = /*@__PURE__*/getDefaultExportFromCjs$1(mitt_2);
+
+var mitt_3 = index$1;
+
+var index$2 = /*@__PURE__*/getDefaultExportFromCjs$2(mitt_3);
+
+module.exports = index$2;
