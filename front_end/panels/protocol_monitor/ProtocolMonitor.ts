348379c4afbcf6542858357db1f15c8b07b06e21
Hadrien Jaubert <hadrienjaubert@google.com>
2023-06-23 13:01:08 +0000

    [Protocol Monitor] Enable the addition of parameters when their type is array. Moreover, the modification of the scripts/build/code_generator_frontend.py generated a new front_end/generated/InspectorBackendCommands.js file
    Screenshot : https://bugs.chromium.org/p/chromium/issues/detail?id=1457512
    
    Bug: 1457512
    Change-Id: I368c0146975d95fa024d59f4fb3e7d4ab590722d

--- front_end/panels/protocol_monitor/ProtocolMonitor.ts
+++ front_end/panels/protocol_monitor/ProtocolMonitor.ts
@@ -724,6 +724,7 @@ export class InfoWidget extends UI.Widget.VBox {
 // eslint-disable-next-line rulesdir/const_enum
 export enum Events {
   CommandSent = 'CommandSent',
+  TargetChose = 'TargetChose',
 }
 
 export type EventTypes = {
@@ -750,7 +751,7 @@ export class EditorWidget extends Common.ObjectWrapper.eventMixin<EventTypes, ty
   }
 }
 
-export function parseCommandInput(input: string): {command: string, parameters: {[x: string]: unknown}} {
+export function parseCommandInput(input: string): {command: string, parameters: object} {
   // If input cannot be parsed as json, we assume it's the command name
   // for a command without parameters. Otherwise, we expect an object
   // with "command"/"method"/"cmd" and "parameters"/"params"/"args"/"arguments" attributes.
