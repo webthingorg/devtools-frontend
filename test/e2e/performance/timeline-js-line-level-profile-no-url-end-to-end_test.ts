2b37133b12a2a36047f96eed4ab60f7ad9d15f31
Randolf Jung <jrandolf@chromium.org>
2023-06-26 15:41:48 +0200

    Update puppeteer and @puppeteer/replay
    - puppeteer replaced with puppeteer-core (20.7.3). DevTools does not actually need puppeteer as it downloads the browsers on its own. PUPPETEER_SKIP_CHROMIUM_DOWNLOAD is not needed too.
    - @puppeteer/replay upgraded to 2.11.2
    
    DISABLE_THIRD_PARTY_CHECK=requires code updates for puppeteer-core
    Bug: none
    Change-Id: I173c228d766681a1a8a9ada3fcf17525cd191726

--- test/e2e/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
+++ test/e2e/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import {getBrowserAndPages} from '../../conductor/puppeteer-state.js';
-import {click, waitForMany} from '../../shared/helper.js';
+import {waitForMany} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {navigateToPerformanceTab, startRecording, stopRecording} from '../helpers/performance-helpers.js';
 import {openSourcesPanel} from '../helpers/sources-helpers.js';
@@ -13,7 +13,7 @@ describe('The Performance panel', () => {
   it.skipOnPlatforms(
       ['mac', 'win32'], '[crbug.com/1428866] can collect a line-level CPU profile and show it in the text editor',
       async () => {
-        const {target} = getBrowserAndPages();
+        const {frontend, target} = getBrowserAndPages();
         await navigateToPerformanceTab();
         await startRecording();
         await target.evaluate(() => {
@@ -28,8 +28,11 @@ describe('The Performance panel', () => {
         });
         await stopRecording();
         await openSourcesPanel();
-        await click('[aria-label="__puppeteer_evaluation_script__, domain"]', {clickOptions: {clickCount: 2}});
-        await click('[aria-label=", file"]', {clickOptions: {clickCount: 2}});
+        await frontend.waitForSelector('>>> .navigator-file-tree-item');
+        const elements = await frontend.$$('>>> .navigator-file-tree-item');
+        for (const element of elements) {
+          await element.click({count: 2});
+        }
         await waitForMany('.cm-performanceGutter .cm-gutterElement', 2);
       });
 });
