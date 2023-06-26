c23c1423939f84d9eda4cc3a33efdfcfe3dedecb
Nancy Li <nancyly@chromium.org>
2023-06-26 17:55:37 +0000

    [RPP] Add missing parts to NetworkHandlerHandler
    - Add |requestMethod| to the |TraceEventSyntheticNetworkRequest|, which will be used in RPP Details view.
    - Store |TraceEventResourceMarkAsCached| type, so we can know if a request is cached from memory
    - Update some |timing| fields to the correct numbers
    
    Bug: 1457485
    Change-Id: I488356a3afa44286ebcfa5b98ff021a626f2e158

--- test/unittests/front_end/models/trace/handlers/NetworkRequestsHandler_test.ts
+++ test/unittests/front_end/models/trace/handlers/NetworkRequestsHandler_test.ts
@@ -227,14 +227,16 @@ describe('NetworkRequestsHandler', function() {
             {
               url: 'http://localhost:3000/foo',
               priority: 'VeryHigh',
-              ts: 1311223447642,
-              dur: 7845,
+              requestMethod: 'GET',
+              ts: TraceModel.Types.Timing.MicroSeconds(1311223447642),
+              dur: TraceModel.Types.Timing.MicroSeconds(7845),
             },
             {
               url: 'http://localhost:3000/bar',
               priority: 'VeryHigh',
-              ts: 1311223455487,
-              dur: 3771,
+              requestMethod: 'GET',
+              ts: TraceModel.Types.Timing.MicroSeconds(1311223455487),
+              dur: TraceModel.Types.Timing.MicroSeconds(3771),
             },
           ],
           'Incorrect number of redirects (request 1)');
