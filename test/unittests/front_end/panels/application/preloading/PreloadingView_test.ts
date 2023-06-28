b0b08d341f2ea05f262dd76bfa828f650ae904ba
keno <kenoss@chromium.org>
2023-06-28 16:39:20 +0000

    PreloadingDevTools: Use RequestLinkIcon to reveal in Network
    Bug: 1410709
    Change-Id: Id4fe67091113cf1f2b5e8ca55bd056571f1efe42

--- test/unittests/front_end/panels/application/preloading/PreloadingView_test.ts
+++ test/unittests/front_end/panels/application/preloading/PreloadingView_test.ts
@@ -907,7 +907,7 @@ describeWithMockConnection('PreloadingResultView', async () => {
     const usedPreloadingComponent = view.getUsedPreloadingForTest();
     assertShadowRoot(usedPreloadingComponent.shadowRoot);
 
-    assert.include(usedPreloadingComponent.shadowRoot.textContent, 'This page was prerendered');
+    assert.include(usedPreloadingComponent.shadowRoot.textContent, 'This page was successfully prerendered.');
   });
 });
 
