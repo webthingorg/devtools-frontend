ae8974b514d9772b65c49fcdcf7da0ecbb4a9da2
Nidhi Jaju <nidhijaju@chromium.org>
2023-06-29 08:09:01 +0000

    [WIP] Devtools Frontend: Show Zstd Content-Encoding support
    Bug: 1246971
    Change-Id: I8e4cb0d6e850df44bc85ae04f642583f0240106c

--- front_end/panels/network/NetworkConfigView.ts
+++ front_end/panels/network/NetworkConfigView.ts
@@ -300,7 +300,7 @@ export class NetworkConfigView extends UI.Widget.VBox {
     const customAcceptedEncodingSetting = Common.Settings.Settings.instance().createSetting(
         'customAcceptedEncodings',
         `${Protocol.Network.ContentEncoding.Gzip},${Protocol.Network.ContentEncoding.Br},${
-            Protocol.Network.ContentEncoding.Deflate}`);
+            Protocol.Network.ContentEncoding.Deflate},${Protocol.Network.ContentEncoding.Zstd}`);
 
     function onSettingChange(): void {
       if (!useCustomAcceptedEncodingSetting.get()) {
