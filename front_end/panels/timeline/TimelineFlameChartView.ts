00d6151d4d0f24446f6657de16c7da84d5d486f1
Nancy Li <nancyly@chromium.org>
2023-06-29 15:44:13 +0000

    [OPP] Add uniitest to TimelineDetailsView for network requests
    Bug: 1453622
    Change-Id: Iced5a6b614c2da95c99715cf1dd4911453c9cba5

--- front_end/panels/timeline/TimelineFlameChartView.ts
+++ front_end/panels/timeline/TimelineFlameChartView.ts
@@ -224,7 +224,8 @@ export class TimelineFlameChartView extends UI.Widget.VBox implements PerfUI.Fla
 
   private updateTrack(): void {
     this.countersView.setModel(this.model, this.#selectedEvents);
-    this.detailsView.setModel(this.model, this.#traceEngineData, this.#filmStripModel, this.#selectedEvents);
+    // TODO(crbug.com/1459265):  Change to await after migration work.
+    void this.detailsView.setModel(this.model, this.#traceEngineData, this.#filmStripModel, this.#selectedEvents);
   }
 
   private refresh(): void {
@@ -317,7 +318,8 @@ export class TimelineFlameChartView extends UI.Widget.VBox implements PerfUI.Fla
     index = this.networkDataProvider.entryIndexForSelection(selection);
     this.networkFlameChart.setSelectedEntry(index);
     if (this.detailsView) {
-      this.detailsView.setSelection(selection);
+      // TODO(crbug.com/1459265):  Change to await after migration work.
+      void this.detailsView.setSelection(selection);
     }
   }
 
