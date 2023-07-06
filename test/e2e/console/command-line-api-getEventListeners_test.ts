96fad5f165b83b7d49860bf63f604f72ae2e2961
Al Muthanna Athamina <almuthanna@chromium.org>
2023-07-06 10:50:10 +0200

    Deflake command-line-api-getEventListeners test
    Split the test "returns the correct values when using the
    getEventListeners method" test so it would not timeout.
    
    Bug: 1418288
    Change-Id: I4adf6836d6f34e917df80451c0ce0e32b781a800

--- test/e2e/console/command-line-api-getEventListeners_test.ts
+++ test/e2e/console/command-line-api-getEventListeners_test.ts
@@ -14,68 +14,70 @@ import {
 const checkCommandResult = checkCommandResultFunction(0);
 
 describe('The Console Tab', async function() {
-  it('returns the correct values when using the getEventListeners method', async () => {
-    await goToResource('../resources/console/command-line-api-getEventListeners.html');
-    await navigateToConsoleTab();
+  describe('returns the correct values when using the getEventListeners method', async () => {
+    const testCases = [
+      {
+        command: 'innerListeners();',
+        expectedResult: '{keydown: Array(2), wheel: Array(1)}',
+        failureMessage: 'inner listeners are not displayed correctly',
+      },
+      {
+        command: 'removeInnerListeners(); getEventListeners(innerElement());',
+        expectedResult: '{keydown: Array(1)}',
+        failureMessage: 'inner listeners after removal are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners(document.getElementById("outer"));',
+        expectedResult: '{mousemove: Array(1), mousedown: Array(1), keydown: Array(1), keyup: Array(1)}',
+        failureMessage: 'outer listeners are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners(document.getElementById("button"));',
+        expectedResult: '{click: Array(1), mouseover: Array(1)}',
+        failureMessage: 'attribute event listeners are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners(window);',
+        expectedResult: '{popstate: Array(1)}',
+        failureMessage: 'window listeners are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners(document.getElementById("empty"));',
+        expectedResult: '{}',
+        failureMessage: 'empty listeners are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners(document.getElementById("invalid"));',
+        expectedResult: '{}',
+        failureMessage: 'invalid listeners are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners({});',
+        expectedResult: '{}',
+        failureMessage: 'object listeners are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners(null);',
+        expectedResult: '{}',
+        failureMessage: 'null listeners are not displayed correctly',
+      },
+      {
+        command: 'getEventListeners(undefined);',
+        expectedResult: '{}',
+        failureMessage: 'undefined listeners are not displayed correctly',
+      },
+    ];
 
-    await checkCommandResult(
-        'innerListeners();',
-        '{keydown: Array(2), wheel: Array(1)}',
-        'inner listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'removeInnerListeners(); getEventListeners(innerElement());',
-        '{keydown: Array(1)}',
-        'inner listeners after removal are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners(document.getElementById("outer"));',
-        '{mousemove: Array(1), mousedown: Array(1), keydown: Array(1), keyup: Array(1)}',
-        'outer listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners(document.getElementById("button"));',
-        '{click: Array(1), mouseover: Array(1)}',
-        'attribute event listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners(window);',
-        '{popstate: Array(1)}',
-        'window listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners(document.getElementById("empty"));',
-        '{}',
-        'empty listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners(document.getElementById("invalid"));',
-        '{}',
-        'invalid listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners({});',
-        '{}',
-        'object listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners(null);',
-        '{}',
-        'null listeners are not displayed correctly',
-    );
-
-    await checkCommandResult(
-        'getEventListeners(undefined);',
-        '{}',
-        'undefined listeners are not displayed correctly',
-    );
+    testCases.forEach(testCase => {
+      it(`for the command \"${testCase['command']}\"`, async () => {
+        await goToResource('../resources/console/command-line-api-getEventListeners.html');
+        await navigateToConsoleTab();
+        await checkCommandResult(
+            testCase['command'],
+            testCase['expectedResult'],
+            testCase['failureMessage'],
+        );
+      });
+    });
   });
 });
