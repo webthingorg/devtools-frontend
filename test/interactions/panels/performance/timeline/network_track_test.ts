de56b843427087d3e3b178f7db219fc977fd775d
Nancy Li <nancyly@chromium.org>
2023-07-06 13:40:26 +0000

    Update mac screenshots
    Bug: none
    Change-Id: I5382403e2691c44e6cae1385918b1db377981573

--- test/interactions/panels/performance/timeline/network_track_test.ts
+++ test/interactions/panels/performance/timeline/network_track_test.ts
@@ -12,25 +12,27 @@ describe('Network track', () => {
 
   const urlForTest = 'performance_panel/track_example.html?track=Network&fileName=cls-cluster-max-timeout';
 
-  itScreenshot('renders the expanded Network track correctly', async () => {
-    await loadComponentDocExample(`${urlForTest}&expanded=true`);
-    const flameChart = await waitFor('.flame-chart-main-pane');
-    await assertElementScreenshotUnchanged(flameChart, 'performance/network_track_expanded.png', 4);
-  });
+  itScreenshot.skipOnPlatforms(
+      ['mac'], '[crbug.com/1462593] renders the expanded Network track correctly', async () => {
+        await loadComponentDocExample(`${urlForTest}&expanded=true`);
+        const flameChart = await waitFor('.flame-chart-main-pane');
+        await assertElementScreenshotUnchanged(flameChart, 'performance/network_track_expanded.png', 4);
+      });
 
-  itScreenshot('renders the collapsed Network track correctly', async () => {
-    await loadComponentDocExample(`${urlForTest}&expanded=false`);
-    const flameChart = await waitFor('.flame-chart-main-pane');
-    await assertElementScreenshotUnchanged(flameChart, 'performance/network_track_collapsed.png', 4);
-  });
+  itScreenshot.skipOnPlatforms(
+      ['mac'], '[crbug.com/1462593] renders the collapsed Network track correctly', async () => {
+        await loadComponentDocExample(`${urlForTest}&expanded=false`);
+        const flameChart = await waitFor('.flame-chart-main-pane');
+        await assertElementScreenshotUnchanged(flameChart, 'performance/network_track_collapsed.png', 4);
+      });
 
-  itScreenshot('renders the track (dark mode and expanded)', async () => {
+  itScreenshot.skipOnPlatforms(['mac'], '[crbug.com/1462593] renders the track (dark mode and expanded)', async () => {
     await loadComponentDocExample(`${urlForTest}&expanded=true&darkMode=true`);
     const flameChart = await waitFor('.flame-chart-main-pane');
     await assertElementScreenshotUnchanged(flameChart, 'performance/network_track_expanded_dark_mode.png', 4);
   });
 
-  itScreenshot('renders the track (dark mode and collapsed)', async () => {
+  itScreenshot.skipOnPlatforms(['mac'], '[crbug.com/1462593] renders the track (dark mode and collapsed)', async () => {
     await loadComponentDocExample(`${urlForTest}&expanded=false&darkMode=true`);
     const flameChart = await waitFor('.flame-chart-main-pane');
     await assertElementScreenshotUnchanged(flameChart, 'performance/network_track_collapsed_dark_mode.png', 4);
