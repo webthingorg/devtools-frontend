6e5d890e20af971b52b538813396d874ce60a9b0
Hadrien Jaubert <hadrienjaubert@google.com>
2023-07-08 08:35:52 +0000

    [Protocol Monitor] Change python script to access the description of commands and parameters
    Bug: 1463211
    Change-Id: I78b8b355745572af18fba9c9d676cb93468f8245

--- front_end/panels/protocol_monitor/components/JSONEditor.ts
+++ front_end/panels/protocol_monitor/components/JSONEditor.ts
@@ -78,6 +78,7 @@ export class JSONEditor extends LitElement {
   static override styles = [editorWidgetStyles];
   @property() declare protocolMethodWithParametersMap: Map<string, Parameter[]>;
   @property() declare protocolTypesMap: Map<string, Type[]>;
+  @property() declare protocolMethodWithDescriptionMap: Map<string, string>;
   @property() declare targetManager;
   @state() declare parameters: Parameter[];
   @state() command: string = '';
