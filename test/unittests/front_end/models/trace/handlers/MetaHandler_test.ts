d418d1e003b4401822bf6a86a9d9eeb158244034
Andrés Olivares <andoli@chromium.org>
2023-07-11 10:40:31 +0000

    [RPP] Cache frame by process id
    Up until now we were tracking which process ids are present in a given
    frame, but there was no straight forward way to know which frame owns
    a given process (without having to traverse the frames to processes map).
    
    For practicality, this CL adds a map that tracks process to frame. This way we will be able to match data from the renderer handler (grouped by
    process) with URL (obtained from the frame).
    
    Bug: 1428024
    Change-Id: I442c92171b3fefe369bcba0512fe0c5c99a85030
    f

--- test/unittests/front_end/models/trace/handlers/MetaHandler_test.ts
+++ test/unittests/front_end/models/trace/handlers/MetaHandler_test.ts
@@ -178,6 +178,21 @@ describe('MetaHandler', function() {
       const data = TraceModel.Handlers.ModelHandlers.Meta.data();
       assert.strictEqual(data.mainFrameId, '1D148CB660D1F96ED70D78DC6A53267B');
     });
+    it('tracks the frame for found processes', async () => {
+      const events = await TraceLoader.rawEvents(this, 'reload-and-trace-page.json.gz');
+      TraceModel.Handlers.ModelHandlers.Meta.reset();
+      TraceModel.Handlers.ModelHandlers.Meta.initialize();
+      for (const event of events) {
+        TraceModel.Handlers.ModelHandlers.Meta.handleEvent(event);
+      }
+
+      await TraceModel.Handlers.ModelHandlers.Meta.finalize();
+      const data = TraceModel.Handlers.ModelHandlers.Meta.data();
+      assert.strictEqual(data.frameByProcessId.size, 1);
+      assert.strictEqual(data.frameByProcessId.keys().next().value, 3581385);
+      assert.strictEqual(data.frameByProcessId.values().next().value.url, 'https://example.com/');
+    });
+
   });
 
   describe('finding GPU thread and main frame', function() {
