98879d37e62e9aac94ef53a9b90b10db080acbbf
Andrew Paseltiner <apaseltiner@chromium.org>
2023-07-12 10:59:33 -0400

    Report DevTools issue for navigation attributionsrc without user gesture
    Bug: 1459311
    Change-Id: I9daefaffaba94c7adffc99c4d37af7f3f05d61dc

--- front_end/panels/issues/AttributionReportingIssueDetailsView.ts
+++ front_end/panels/issues/AttributionReportingIssueDetailsView.ts
@@ -80,6 +80,9 @@ export class AttributionReportingIssueDetailsView extends AffectedResourcesView 
       case IssuesManager.AttributionReportingIssue.IssueCode.WebAndOsHeaders:
         this.appendColumnTitle(header, i18nString(UIStrings.request));
         break;
+      case IssuesManager.AttributionReportingIssue.IssueCode.NavigationRegistrationWithoutTransientUserActivation:
+        this.appendColumnTitle(header, i18nString(UIStrings.element));
+        break;
     }
 
     this.affectedResources.appendChild(header);
@@ -119,6 +122,9 @@ export class AttributionReportingIssueDetailsView extends AffectedResourcesView 
         break;
       case IssuesManager.AttributionReportingIssue.IssueCode.SourceAndTriggerHeaders:
         this.#appendRequestOrEmptyCell(element, details.request);
+        break;
+      case IssuesManager.AttributionReportingIssue.IssueCode.NavigationRegistrationWithoutTransientUserActivation:
+        await this.#appendElementOrEmptyCell(element, issue);
         break;
     }
 
