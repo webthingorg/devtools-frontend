4c3cfd4a526a885c961f04f68914391ec32d8f74
Hadrien Jaubert <hadrienjaubert@google.com>
2023-07-13 08:39:42 +0000

    [Protocol Monitor] Enable access to the description in JSONEditor
    DISABLE_THIRD_PARTY_CHECK=needed-to-build
    
    Bug: 1463950
    Change-Id: Ica4c870a347c1f40762f4ea897170db0f95a0fac

--- front_end/core/protocol_client/InspectorBackend.ts
+++ front_end/core/protocol_client/InspectorBackend.ts
@@ -141,9 +141,10 @@ export class InspectorBackend {
     return prototype;
   }
 
-  registerCommand(method: QualifiedName, parameters: CommandParameter[], replyArgs: string[]): void {
+  registerCommand(method: QualifiedName, parameters: CommandParameter[], replyArgs: string[], description: string):
+      void {
     const [domain, command] = splitQualifiedName(method);
-    this.agentPrototype(domain as ProtocolDomainName).registerCommand(command, parameters, replyArgs);
+    this.agentPrototype(domain as ProtocolDomainName).registerCommand(command, parameters, replyArgs, description);
     this.#initialized = true;
   }
 
@@ -924,6 +925,7 @@ class _AgentPrototype {
   replyArgs: {
     [x: string]: string[],
   };
+  description = '';
   commandParameters: {
     [x: string]: CommandParameter[],
   };
@@ -936,13 +938,13 @@ class _AgentPrototype {
     this.commandParameters = {};
   }
 
-  registerCommand(methodName: UnqualifiedName, parameters: CommandParameter[], replyArgs: string[]): void {
+  registerCommand(
+      methodName: UnqualifiedName, parameters: CommandParameter[], replyArgs: string[], description: string): void {
     const domainAndMethod = qualifyName(this.domain, methodName);
 
     function sendMessagePromise(this: _AgentPrototype, ...args: unknown[]): Promise<unknown> {
       return _AgentPrototype.prototype.sendMessageToBackendPromise.call(this, domainAndMethod, parameters, args);
     }
-
     // @ts-ignore Method code generation
     this[methodName] = sendMessagePromise;
     this.commandParameters[domainAndMethod] = parameters;
@@ -953,7 +955,7 @@ class _AgentPrototype {
 
     // @ts-ignore Method code generation
     this['invoke_' + methodName] = invoke;
-
+    this.description = description;
     this.replyArgs[domainAndMethod] = replyArgs;
   }
 
