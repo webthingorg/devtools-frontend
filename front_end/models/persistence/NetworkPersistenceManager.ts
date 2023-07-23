47a44eecca74ef2cd3f3b583918dd81697001d97
Jecelyn Yeen <jec@google.com>
2023-07-24 00:16:26 +0200

    [Override] Improve local override UX in the Network panel
    - Rename "Save for overrides" to "Override content"
    - Group both overrides menu under same section
    - Always show "Override content" in all scenarios
       - before user setup overrides (we will prompt for setup)
       - when user disable overrides (we will enable it automatically)
       - after user overrides the file (we will open the overridden file)
    
    Prior to this change, we only show "Save for overrides" if:
    - user has setup overrides before in the Sources panel && never override the selected network resource before
    
    Screenshot: https://crbug.com/1465785#c2
    
    Bug:1465785
    Change-Id: I1182a23598484112626fbd0fae9d8fe71edf7a28

--- front_end/models/persistence/NetworkPersistenceManager.ts
+++ front_end/models/persistence/NetworkPersistenceManager.ts
@@ -376,6 +376,19 @@ export class NetworkPersistenceManager extends Common.ObjectWrapper.ObjectWrappe
     this.updateInterceptionPatterns();
   }
 
+  isUISourceCodeOverridable(uiSourceCode: Workspace.UISourceCode.UISourceCode): boolean {
+    return uiSourceCode.project().type() === Workspace.Workspace.projectTypes.Network;
+  }
+
+  isUISourceCodeAlreadyOverrided(uiSourceCode: Workspace.UISourceCode.UISourceCode): boolean {
+    return this.bindings.has(uiSourceCode) || this.savingForOverrides.has(uiSourceCode);
+  }
+
+  shouldPromptSaveForOverridesDialog(uiSourceCode: Workspace.UISourceCode.UISourceCode): boolean {
+    return this.isUISourceCodeOverridable(uiSourceCode) && !this.isUISourceCodeAlreadyOverrided(uiSourceCode) &&
+        !this.activeInternal && !this.projectInternal;
+  }
+
   canSaveUISourceCodeForOverrides(uiSourceCode: Workspace.UISourceCode.UISourceCode): boolean {
     return this.activeInternal && uiSourceCode.project().type() === Workspace.Workspace.projectTypes.Network &&
         !this.bindings.has(uiSourceCode) && !this.savingForOverrides.has(uiSourceCode);
