a35f18c9c02b175632d60894abebc221d23f49ea
Alina Varkki <alinavarkki@google.com>
2023-07-28 15:53:38 +0000

    Array of synthetic events that pairs animation begin and end events
    AnimationHandler now gathers all begin and end events and pairs them up in TraceEventSyntheticNestableAsyncEvent array.
    
    Bug: 1467826
    Change-Id: Ib828bbdd465c53708214a65df4acf9d0125eb01c

--- test/unittests/front_end/models/trace/handlers/AnimationHandler_test.ts
+++ test/unittests/front_end/models/trace/handlers/AnimationHandler_test.ts
@@ -14,7 +14,22 @@ describe('AnimationHandler', function() {
       TraceModel.Handlers.ModelHandlers.Animation.handleEvent(event);
     }
 
+    await TraceModel.Handlers.ModelHandlers.Animation.finalize();
+
     const animationEvents = TraceModel.Handlers.ModelHandlers.Animation.data().animations;
     assert.lengthOf(animationEvents, 5);
+  });
+
+  it('checks event length', async function() {
+    const events = await TraceLoader.rawEvents(this, 'animation.json.gz');
+
+    for (const event of events) {
+      TraceModel.Handlers.ModelHandlers.Animation.handleEvent(event);
+    }
+
+    await TraceModel.Handlers.ModelHandlers.Animation.finalize();
+
+    const eventDuration = TraceModel.Handlers.ModelHandlers.Animation.data().animationsSynteticEvents[0].dur;
+    assert.strictEqual(eventDuration, 2006450);
   });
 });
