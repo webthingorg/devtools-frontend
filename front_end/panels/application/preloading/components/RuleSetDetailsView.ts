cec0ee33a5c5d16f50851d59629a41721df53798
Robert Lin <robertlin@chromium.org>
2023-08-07 11:29:49 +0900

    PreloadingDevTools: Revise RuleSetDetailsReportView
    This CL revises RuleSetDetailsReportView.ts, and this includes
    removing 'Validity' and 'Source' rows, and adding TextEditor view-only
    for JsonSource. By removing ReportView, this CL also renames
    RuleSetDetailsReportView to RuleSetDetailsView.
    
    Demo:
    Successful case https://drive.google.com/file/d/1Vp3C5MTvQqUq-vJ16ECRzP7HDmU5IqU_/view?usp=drive_link
    Failure case https://drive.google.com/file/d/1rRTuYlueXxID3bAQxzhj_ifwV6rV4kS7/view?usp=sharing
    
    Bug: 1467874
    Change-Id: Ibcae1954b6abccf826e72c5d0239fc6c272ef2bb

---
+++ front_end/panels/application/preloading/components/RuleSetDetailsView.ts
@@ -0,0 +1,147 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as Protocol from '../../../../generated/protocol.js';
+import * as CodeMirror from '../../../../third_party/codemirror.next/codemirror.next.js';
+import * as CodeHighlighter from '../../../../ui/components/code_highlighter/code_highlighter.js';
+import * as ComponentHelpers from '../../../../ui/components/helpers/helpers.js';
+import * as LegacyWrapper from '../../../../ui/components/legacy_wrapper/legacy_wrapper.js';
+import * as Coordinator from '../../../../ui/components/render_coordinator/render_coordinator.js';
+import * as TextEditor from '../../../../ui/components/text_editor/text_editor.js';
+import * as LitHtml from '../../../../ui/lit-html/lit-html.js';
+import * as IconButton from '../../../../ui/components/icon_button/icon_button.js';
+import {assertNotNullOrUndefined} from '../../../../core/platform/platform.js';
+
+import type * as UI from '../../../../ui/legacy/legacy.js';
+
+import preloadingDetailsReportViewStyles from './preloadingDetailsReportView.css.js';
+
+type RuleSet = Protocol.Preload.RuleSet;
+
+const coordinator = Coordinator.RenderCoordinator.RenderCoordinator.instance();
+const codeMirrorJsonType = await CodeHighlighter.CodeHighlighter.languageFromMIME('application/json');
+
+export type RuleSetDetailsViewData = RuleSet|null;
+
+export class RuleSetDetailsView extends LegacyWrapper.LegacyWrapper.WrappableComponent<UI.Widget.VBox> {
+  readonly #shadow = this.attachShadow({mode: 'open'});
+  #data: RuleSetDetailsViewData = null;
+  #prerenderedUrl: String|null = null;
+
+  connectedCallback(): void {
+    this.#shadow.adoptedStyleSheets = [preloadingDetailsReportViewStyles];
+  }
+
+  set data(data: RuleSetDetailsViewData) {
+    this.#data = data;
+    void this.#render();
+  }
+
+  set prerenderedUrl(prerenderedUrl: String|null) {
+    this.#prerenderedUrl = prerenderedUrl;
+  }
+
+  async #render(): Promise<void> {
+    await coordinator.write('RuleSetDetailsView render', () => {
+      if (this.#data === null) {
+        LitHtml.render(LitHtml.nothing, this.#shadow, {host: this});
+        return;
+      }
+
+      // Disabled until https://crbug.com/1079231 is fixed.
+      // clang-format off
+      LitHtml.render(LitHtml.html`
+        <div class="content">
+          <div class="gray-panel" id="prerendered-url">${this.#prerenderedUrl}</div>
+          ${this.#maybeError()}
+          <slot></slot>
+        </div>
+        <div class="text-ellipsis" title="">
+        ${this.#source(this.#data.sourceText)}
+        </div>
+      `, this.#shadow, {host: this});
+      // clang-format on
+    });
+  }
+
+  // TODO(https://crbug.com/1425354): Support i18n.
+  #maybeError(): LitHtml.LitTemplate {
+    assertNotNullOrUndefined(this.#data);
+
+    if (this.#data.errorMessage === undefined) {
+      return LitHtml.nothing;
+    }
+
+    // Disabled until https://crbug.com/1079231 is fixed.
+    // clang-format off
+    const icon = new IconButton.Icon.Icon();
+    icon.data = {iconName: 'cross-circle-filled', color: 'var(--icon-error)', width: '14px', height: '14px'};
+    return LitHtml.html`
+      <div class="gray-panel">
+      ${icon}
+      <span id="error-message-text">${this.#data.errorMessage}</span>
+      </div>
+    `;
+    // clang-format on
+  }
+
+  #source(sourceText: string): LitHtml.LitTemplate {
+    let sourceJson;
+    try {
+      sourceJson = JSON.parse(sourceText);
+    } catch (_) {
+    }
+
+    if (sourceJson === undefined) {
+      // Disabled until https://crbug.com/1079231 is fixed.
+      // clang-format off
+      const textEditorInternal = new TextEditor.TextEditor.TextEditor(CodeMirror.EditorState.create({
+        doc: sourceText,
+        extensions: [
+          CodeMirror.EditorState.readOnly.of(true),
+          CodeMirror.lineNumbers(),
+          CodeMirror.highlightSpecialChars(),
+          CodeMirror.highlightSelectionMatches(),
+          CodeMirror.indentOnInput(),
+          CodeMirror.syntaxHighlighting(CodeHighlighter.CodeHighlighter.highlightStyle),
+          TextEditor.Config.theme(),
+        ],
+      }));
+      return LitHtml.html`
+            ${textEditorInternal}
+      `;
+      // clang-format on
+    }
+
+    const textEditorInternal = new TextEditor.TextEditor.TextEditor(CodeMirror.EditorState.create({
+      doc: sourceText,
+      extensions: [
+        CodeMirror.EditorState.readOnly.of(true),
+        CodeMirror.lineNumbers(),
+        codeMirrorJsonType as CodeMirror.Extension,
+        CodeMirror.highlightSpecialChars(),
+        CodeMirror.highlightSelectionMatches(),
+        CodeMirror.indentOnInput(),
+        CodeMirror.syntaxHighlighting(CodeHighlighter.CodeHighlighter.highlightStyle),
+        TextEditor.Config.theme(),
+      ],
+    }));
+    textEditorInternal.style.flexGrow = '1';
+    // Disabled until https://crbug.com/1079231 is fixed.
+    // clang-format off
+      return LitHtml.html`
+          ${textEditorInternal}
+      `;
+    // clang-format on
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-resources-rulesets-details-view', RuleSetDetailsView);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'devtools-resources-rulesets-details-view': RuleSetDetailsView;
+  }
+}
