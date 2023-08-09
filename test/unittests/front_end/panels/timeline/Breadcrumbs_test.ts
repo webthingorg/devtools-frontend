6d0bdb4ba415769b8e995ea759b83ce86e4f9269
Alina Varkki <alinavarkki@google.com>
2023-08-09 15:27:00 +0000

    RPP: Minimap Breadcrumbs data structure
    We are going to add breadcrumbs to the Performance Panel to aid users as they zoom in and out of the timeline.
    This CL adds a data structure to store breadcrumbs. Because breadcrumbs are nested within each other, we represent them as a linked list.
    
    Bug: 1471463
    Change-Id: I7d66a1e7f10ba1cc59d74bc037e09b4e38512701

---
+++ test/unittests/front_end/panels/timeline/Breadcrumbs_test.ts
@@ -0,0 +1,102 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as Trace from '../../../../../front_end/models/trace/trace.js';
+import * as Types from '../../../../../front_end/models/trace/types/types.js';
+import * as Timeline from '../../../../../front_end/panels/timeline/timeline.js';
+
+describe('Timeline breadcrumbs', () => {
+  it('can create breadcrumbs', () => {
+    const initialTraceWindow: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(1),
+      max: Types.Timing.MicroSeconds(10),
+      range: Types.Timing.MicroSeconds(9),
+    };
+    const crumbs = new Timeline.Breadcrumbs.Breadcrumbs(initialTraceWindow);
+
+    const breadcrumb1: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(3),
+      max: Types.Timing.MicroSeconds(9),
+      range: Types.Timing.MicroSeconds(6),
+    };
+
+    const breadcrumb2: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(4),
+      max: Types.Timing.MicroSeconds(6),
+      range: Types.Timing.MicroSeconds(2),
+    };
+
+    crumbs.addNewBreadcrumb(breadcrumb1);
+    crumbs.addNewBreadcrumb(breadcrumb2);
+
+    assert.deepEqual(crumbs.getBreacrumbsTraceWindows(), [initialTraceWindow, breadcrumb1, breadcrumb2]);
+  });
+
+  it('can remove breadcrumbs', () => {
+    const initialTraceWindow: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(1),
+      max: Types.Timing.MicroSeconds(10),
+      range: Types.Timing.MicroSeconds(9),
+    };
+    const crumbs = new Timeline.Breadcrumbs.Breadcrumbs(initialTraceWindow);
+
+    const breadcrumb1: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(3),
+      max: Types.Timing.MicroSeconds(9),
+      range: Types.Timing.MicroSeconds(6),
+    };
+
+    const breadcrumb2: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(4),
+      max: Types.Timing.MicroSeconds(6),
+      range: Types.Timing.MicroSeconds(2),
+    };
+
+    crumbs.addNewBreadcrumb(breadcrumb1);
+    crumbs.addNewBreadcrumb(breadcrumb2);
+
+    assert.deepEqual(crumbs.getBreacrumbsTraceWindows(), [initialTraceWindow, breadcrumb1, breadcrumb2]);
+
+    crumbs.removeBreadcrumbChildren(breadcrumb1);
+    assert.deepEqual(crumbs.getBreacrumbsTraceWindows(), [initialTraceWindow, breadcrumb1]);
+  });
+
+  it('does not create a child breadcrumb with equal to the parent TraceWindow', () => {
+    const initialTraceWindow: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(1),
+      max: Types.Timing.MicroSeconds(10),
+      range: Types.Timing.MicroSeconds(9),
+    };
+    const crumbs = new Timeline.Breadcrumbs.Breadcrumbs(initialTraceWindow);
+
+    const breadcrumb1: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(1),
+      max: Types.Timing.MicroSeconds(10),
+      range: Types.Timing.MicroSeconds(9),
+    };
+
+    crumbs.addNewBreadcrumb(breadcrumb1);
+
+    assert.deepEqual(crumbs.getBreacrumbsTraceWindows(), [initialTraceWindow]);
+  });
+
+  it('does not create a child breadcrumb with TraceWindow outside of parent TraceWindow bounds', () => {
+    const initialTraceWindow: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(1),
+      max: Types.Timing.MicroSeconds(10),
+      range: Types.Timing.MicroSeconds(9),
+    };
+    const crumbs = new Timeline.Breadcrumbs.Breadcrumbs(initialTraceWindow);
+
+    const breadcrumb1: Trace.Types.Timing.TraceWindow = {
+      min: Types.Timing.MicroSeconds(0),
+      max: Types.Timing.MicroSeconds(10),
+      range: Types.Timing.MicroSeconds(10),
+    };
+
+    crumbs.addNewBreadcrumb(breadcrumb1);
+
+    assert.deepEqual(crumbs.getBreacrumbsTraceWindows(), [initialTraceWindow]);
+  });
+});
