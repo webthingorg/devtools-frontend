c3f4fafef260870a922b64a2e18b32f267f4c452
Alina Varkki <alinavarkki@google.com>
2023-08-09 13:51:09 +0000

    RPP: Minimap Breadcrumbs data structure
    We are going to add breadcrumbs to the Performance Panel to aid users as they zoom in and out of the timeline.
    This CL adds a data structure to store breadcrumbs. Because breadcrumbs are nested within each other, we represent them as a linked list.
    
    Bug: 1471463
    Change-Id: I7d66a1e7f10ba1cc59d74bc037e09b4e38512701

---
+++ front_end/panels/timeline/Breadcrumbs.ts
@@ -0,0 +1,68 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as Trace from '../../models/trace/trace.js';
+
+export interface Breadcrumb {
+  window: Trace.Types.Timing.TraceWindow;
+  child: Breadcrumb|null;
+}
+
+export class Breadcrumbs {
+  readonly #initialBreadcrumb: Breadcrumb;
+  #lastBreadcrumb: Breadcrumb;
+
+  constructor(initialTraceWindow: Trace.Types.Timing.TraceWindow) {
+    this.#initialBreadcrumb = {
+      window: initialTraceWindow,
+      child: null,
+    };
+    this.#lastBreadcrumb = this.#initialBreadcrumb;
+  }
+
+  addNewBreadcrumb(newBreadcrumbTraceWindow: Trace.Types.Timing.TraceWindow): void {
+    if (this.isTraceWindowWithinTraceWindow(newBreadcrumbTraceWindow, this.#lastBreadcrumb.window)) {
+      const newBreadcrumb = {
+        window: newBreadcrumbTraceWindow,
+        child: null,
+      };
+      this.#lastBreadcrumb.child = newBreadcrumb;
+      this.#lastBreadcrumb = newBreadcrumb;
+    }
+  }
+
+  isTraceWindowWithinTraceWindow(child: Trace.Types.Timing.TraceWindow, parent: Trace.Types.Timing.TraceWindow):
+      boolean {
+    return child.min > parent.min && child.max < parent.max;
+  }
+
+  removeBreadcrumbChildren(newLastBreadcrumb: Trace.Types.Timing.TraceWindow): void {
+    let breadcrumbsIter: Breadcrumb = this.#initialBreadcrumb;
+
+    while (breadcrumbsIter.window !== newLastBreadcrumb) {
+      const iterChild = breadcrumbsIter.child;
+      if (iterChild !== null) {
+        breadcrumbsIter = iterChild;
+      }
+    }
+
+    breadcrumbsIter.child = null;
+    this.#lastBreadcrumb = breadcrumbsIter;
+  }
+
+  getBreacrumbsTraceWindows(): Trace.Types.Timing.TraceWindow[] {
+    const allBreadcrumbs: Trace.Types.Timing.TraceWindow[] = [this.#initialBreadcrumb.window];
+    let breadcrumbsIter: Breadcrumb = this.#initialBreadcrumb;
+
+    while (breadcrumbsIter.child !== null) {
+      const iterChild = breadcrumbsIter.child;
+      if (iterChild !== null) {
+        allBreadcrumbs.push(iterChild.window);
+        breadcrumbsIter = iterChild;
+      }
+    }
+
+    return allBreadcrumbs;
+  }
+}
