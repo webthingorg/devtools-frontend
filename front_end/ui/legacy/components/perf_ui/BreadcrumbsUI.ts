87e1bdd0237426cdb114ef0dfbb353c72e30492f
Alina Varkki <alinavarkki@google.com>
2023-08-14 12:23:49 +0000

    added a component to component server
    Change-Id: I549f49ce554800396ca2d1bfe3d3ec1209db6f09

---
+++ front_end/ui/legacy/components/perf_ui/BreadcrumbsUI.ts
@@ -0,0 +1,51 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Timeline from '../../../../panels/timeline/timeline.js';
+import * as Types from '../../../../../front_end/models/trace/types/types.js';
+import * as ComponentHelpers from '../../../components/helpers/helpers.js';
+import * as IconButton from '../../../components/icon_button/icon_button.js';
+import * as LitHtml from '../../../lit-html/lit-html.js';
+
+const {render, html} = LitHtml;
+
+export interface BreadcrumbsUIData {
+  breadcrumb: Timeline.Breadcrumbs.Breadcrumb;
+}
+
+export class BreadcrumbsUI extends HTMLElement {
+  static readonly litTagName = LitHtml.literal`devtools-breadcrumbs-ui`;
+  private readonly shadow = this.attachShadow({mode: 'open'});
+  private sliceLengthMs: Types.Timing.MicroSeconds = Types.Timing.MicroSeconds(0);
+
+  set data(data: BreadcrumbsUIData) {
+
+    
+    console.log(JSON.stringify(data));    
+    // this.sliceLengthMs = data.breadcrumb.window.range;
+    this.sliceLengthMs = Types.Timing.MicroSeconds(10);
+    this.render();
+  }
+
+  private render(): void {
+    const output = html`
+        <div style="align-items: center; display: flex">
+            <span>${this.sliceLengthMs} ms</span>
+            <${IconButton.Icon.Icon.litTagName} .data=${
+        {iconName: 'chevron-right', color: 'var(--icon-default)', width: '20px', height: '20px'} as
+        IconButton.Icon.IconWithName}>
+        </div>
+        `;
+    render(output, this.shadow, {host: this});
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-breadcrumbs-ui', BreadcrumbsUI);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLElementTagNameMap {
+    'devtools-breadcrumbs-ui': BreadcrumbsUI;
+  }
+}
