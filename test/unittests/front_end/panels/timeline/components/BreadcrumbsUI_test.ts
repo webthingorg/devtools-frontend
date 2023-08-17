58beb0097df5bbaf5e573e32b5ebecacc10625b2
Alina Varkki <alinavarkki@google.com>
2023-08-17 16:23:48 +0000

    Nested  breadcrumbs
    Change-Id: Ib3098792383c13815e7c2cf430c8a470dba326b5

---
+++ test/unittests/front_end/panels/timeline/components/BreadcrumbsUI_test.ts
@@ -0,0 +1,6 @@
+import * as TimelineComponents from '../../../../../../front_end/panels/timeline/components/components.js';
+
+describe('BreadcrumbsUI', async () => {
+    const BreadcrumbsUIComponent = TimelineComponents.BreadcrumbsUI;
+    
+});
