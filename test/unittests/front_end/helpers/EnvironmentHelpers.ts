2b1f279f7a9cdede468990973cc48d0cd77a48b7
Simon Zünd <szuend@chromium.org>
2023-08-31 10:01:27 +0200

    [test] Remove second TargetManager global
    Before this CL, the "EnvironmentHelpers.ts" file would maintain a
    separate TargetManager global, parallel to the TargetManager.instance().
    
    This is iffy in unit tests: The first time a unit test calls
    `createTarget`, we would nuke the existing TargetManager. Classes
    already set-up would have used the wrong TargetManager.
    
    An example is the DebuggerWorkspaceBinding. If the instance is created
    with the default TargetManager.instance() but we call 'createTarget()'
    later, the DebuggerWorkspaceBinding would not get notified about any
    of the models since `createTarget` nuked the global.
    
    To fix this, we rely solely on the TargetManager.instance() global.
    EnvironmentHelpers.ts will still nuke the TargetManager.instance() in
    "afterEach", so each test gets a fresh TargetManager. But otherwise
    'createTarget' will no longer interfere.
    
    R=jarin@chromium.org
    
    Bug: None
    Change-Id: Idf35f39e851122d71d4e284d101e857989d9fda7

--- test/unittests/front_end/helpers/EnvironmentHelpers.ts
+++ test/unittests/front_end/helpers/EnvironmentHelpers.ts
@@ -21,14 +21,6 @@ import type * as UIModule from '../../../../front_end/ui/legacy/legacy.js';
 // initialization phase.
 let UI: typeof UIModule;
 
-let targetManager: SDK.TargetManager.TargetManager|null;
-
-function initializeTargetManagerIfNecessary(): SDK.TargetManager.TargetManager {
-  // Create the target manager.
-  targetManager = targetManager || SDK.TargetManager.TargetManager.instance({forceNew: true});
-  return targetManager;
-}
-
 let uniqueTargetId = 0;
 
 export function createTarget(
@@ -47,7 +39,7 @@ export function createTarget(
       id = ('test' + uniqueTargetId) as Protocol.Target.TargetID;
     }
   }
-  const targetManager = initializeTargetManagerIfNecessary();
+  const targetManager = SDK.TargetManager.TargetManager.instance();
   return targetManager.createTarget(
       id, name ?? id, type, parentTarget ? parentTarget : null, /* sessionId=*/ parentTarget ? id : undefined,
       /* suspended=*/ false,
@@ -284,8 +276,6 @@ export async function initializeGlobalVars({reset = true} = {}) {
   // Initialize theme support and context menus.
   Common.Settings.Settings.instance().createSetting('uiTheme', 'systemPreferred');
   UI.UIUtils.initializeUIUtils(document);
-
-  initializeTargetManagerIfNecessary();
 }
 
 export async function deinitializeGlobalVars() {
@@ -304,7 +294,6 @@ export async function deinitializeGlobalVars() {
   await deinitializeGlobalLocaleVars();
   Logs.NetworkLog.NetworkLog.removeInstance();
   SDK.TargetManager.TargetManager.removeInstance();
-  targetManager = null;
   Root.Runtime.Runtime.removeInstance();
   Common.Settings.Settings.removeInstance();
   Common.Console.Console.removeInstance();
