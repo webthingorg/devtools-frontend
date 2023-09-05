ab0d11fa59b78e6165f46d8601392e68d9a81c16
Hadrien Jaubert <hadrienjaubert@google.com>
2023-09-05 11:44:12 +0000

    [Performance Panel] Update the colors of PieChart, FlameChart and TimelineOverView
    Bug: 1477543
    Change-Id: Ic03b8bf5a0f86f7740071debbd6c5c39a14ca82f

--- front_end/ui/legacy/components/perf_ui/FlameChart.ts
+++ front_end/ui/legacy/components/perf_ui/FlameChart.ts
@@ -1089,9 +1089,23 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
       const entryIndex = indexes[i];
       this.#drawEventRect(context, timelineData, entryIndex);
     }
-    context.fillStyle = color;
+    this.#fillCanvas(context, color);
     context.fill();
     context.restore();
+  }
+
+  /**
+   * Fill canvas depending on color value
+   */
+  #fillCanvas(context: CanvasRenderingContext2D, color: string): void {
+    // The color is either a css variable --app-color-scripting for instance or a hls color
+    // So if the first character of the color is -, it is a CSS variable.
+    const isColorCSSVariable = color[0] === '-';
+    if (isColorCSSVariable) {
+      context.fillStyle = ThemeSupport.ThemeSupport.instance().getComputedValue(color);
+    } else {
+      context.fillStyle = color;
+    }
   }
 
   /**
@@ -1667,7 +1681,7 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
         context.fill();
         context.beginPath();
         lastColor = segments[i].data;
-        context.fillStyle = lastColor;
+        this.#fillCanvas(context, lastColor);
       }
       context.rect(segment.begin, y, segment.end - segment.begin, groupBarHeight);
     }
@@ -1821,7 +1835,8 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
     this.entryColorsCache = new Array(this.rawTimelineDataLength);
     for (let i = 0; i < this.rawTimelineDataLength; ++i) {
       this.forceDecorationCache[i] = this.dataProvider.forceDecoration(i) ? 1 : 0;
-      this.entryColorsCache[i] = this.dataProvider.entryColor(i);
+      const color = this.dataProvider.entryColor(i);
+      this.entryColorsCache[i] = color;
     }
 
     const entryCounters = new Uint32Array(this.dataProvider.maxStackDepth() + 1);
