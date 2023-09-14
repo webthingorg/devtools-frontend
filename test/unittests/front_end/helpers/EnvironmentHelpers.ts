543d265b3980865613a6a49f7b66027aee30b24f
Silvia Eremia <silviaeremia@google.com>
2023-09-14 13:05:03 +0000

    Add 'More filters' dropdown instead of filter checkboxes
    Draft - still have to work on styling
    
    Change-Id: I40d74c1a52bddd8ab4b1319d5d28a8582aedfa1b
    Bug:1477617

--- test/unittests/front_end/helpers/EnvironmentHelpers.ts
+++ test/unittests/front_end/helpers/EnvironmentHelpers.ts
@@ -125,6 +125,7 @@ const REGISTERED_EXPERIMENTS = [
   'selfXssWarning',
   'evaluateExpressionsWithSourceMaps',
   'useSourceMapScopes',
+  'networkPanelFilterBarRedesign',
 ];
 
 export async function initializeGlobalVars({reset = true} = {}) {
