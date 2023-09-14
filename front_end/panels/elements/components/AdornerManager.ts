fc58d08c45e1d475e3cb82ea5158ae11ab9f1beb
joonyoo181 <joon.yoo181@berkeley.edu>
2023-09-14 00:27:20 -0700

    Adding <audio>/<video> link adorners
    In an effort to increase the accessibility of media panels, this CL
    adds a new link adorner on media elements inside the DOM Tree
    of DevTool's Elements Panel. Upon clicking on this link adorner,
    users will be directed to media panel where it displays the relevant
    information regarding the media element selected.
    
    Notice: This CL is done by a student developer Junseo (Jeremy) Yoo
    as part of Google Summer of Code 2023
    
    Bug: 1448214
    Change-Id: I7eabcc67359f883cac2e9a1406b233999cefedf7

--- front_end/panels/elements/components/AdornerManager.ts
+++ front_end/panels/elements/components/AdornerManager.ts
@@ -33,6 +33,7 @@ export enum RegisteredAdorners {
   SLOT = 'slot',
   TOP_LAYER = 'top-layer',
   REVEAL = 'reveal',
+  MEDIA = 'media',
 }
 
 // This enum-like const object serves as the authoritative registry for all the
@@ -90,6 +91,12 @@ export function getRegisteredAdorner(which: RegisteredAdorners): RegisteredAdorn
     case RegisteredAdorners.REVEAL:
       return {
         name: 'reveal',
+        category: AdornerCategories.DEFAULT,
+        enabledByDefault: true,
+      };
+    case RegisteredAdorners.MEDIA:
+      return {
+        name: 'media',
         category: AdornerCategories.DEFAULT,
         enabledByDefault: true,
       };
