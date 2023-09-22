822086e7658a4fa422e393588a076a22b931d09c
Silvia Eremia <silviaeremia@google.com>
2023-09-22 14:29:00 +0000

    Add 'More filters' dropdown in the Network Panel Filterbar
    Show only/hide requests dropdown with the functionalities of the previous checkboxes
    
    Change-Id: I40d74c1a52bddd8ab4b1319d5d28a8582aedfa1b
    Bug:1477617

--- front_end/ui/legacy/ContextMenu.ts
+++ front_end/ui/legacy/ContextMenu.ts
@@ -396,6 +396,7 @@ export class ContextMenu extends SubMenu {
     this.handlers = new Map();
     this.idInternal = 0;
     this.hostedMenuOpened = false;
+    this.eventTarget = this.event.target;
 
     const target = deepElementFromEvent(event);
     if (target) {
@@ -470,7 +471,6 @@ export class ContextMenu extends SubMenu {
 
   private innerShow(): void {
     const menuObject = this.buildMenuDescriptors();
-
     if (!this.eventTarget) {
       return;
     }
