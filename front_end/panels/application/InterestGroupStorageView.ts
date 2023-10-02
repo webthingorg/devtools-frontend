dc81e30b23f151242443f0181858f68b826bae32
Maks Orlovich <morlovich@chromium.org>
2023-10-02 13:07:21 -0400

    InterestGroupStorageView: don't try to show details of additionalBids
    They are not backed by the database, so trying to look them up there will not produce useful information.
    
    Bug:1488021
    Change-Id: I24335eadc795667516a84af83f23f6866ee47932

--- front_end/panels/application/InterestGroupStorageView.ts
+++ front_end/panels/application/InterestGroupStorageView.ts
@@ -3,12 +3,12 @@
 // found in the LICENSE file.
 
 import * as i18n from '../../core/i18n/i18n.js';
-import * as UI from '../../ui/legacy/legacy.js';
+import * as Protocol from '../../generated/protocol.js';
 import type * as DataGrid from '../../ui/components/data_grid/data_grid.js';
-import type * as Protocol from '../../generated/protocol.js';
 import * as SourceFrame from '../../ui/legacy/components/source_frame/source_frame.js';
-import * as ApplicationComponents from './components/components.js';
+import * as UI from '../../ui/legacy/legacy.js';
 
+import * as ApplicationComponents from './components/components.js';
 import interestGroupStorageViewStyles from './interestGroupStorageView.css.js';
 
 const UIStrings = {
@@ -100,11 +100,19 @@ export class InterestGroupStorageView extends UI.SplitWidget.SplitWidget {
 
     const ownerOrigin = row.cells.find(cell => cell.columnId === 'event-group-owner')?.value as string;
     const name = row.cells.find(cell => cell.columnId === 'event-group-name')?.value as string;
+    const eventType =
+        row.cells.find(cell => cell.columnId === 'event-type')?.value as Protocol.Storage.InterestGroupAccessType;
     if (!ownerOrigin || !name) {
       return;
     }
 
-    const details = await this.detailsGetter.getInterestGroupDetails(ownerOrigin, name);
+    let details = null;
+    // Details of additional bids can't be looked up like regular bids,
+    // they are ephemeral to the auction.
+    if (eventType !== Protocol.Storage.InterestGroupAccessType.AdditionalBid &&
+        eventType !== Protocol.Storage.InterestGroupAccessType.AdditionalBidWin) {
+      details = await this.detailsGetter.getInterestGroupDetails(ownerOrigin, name);
+    }
     if (details) {
       const jsonView = await SourceFrame.JSONView.JSONView.createView(JSON.stringify(details));
       jsonView?.setMinimumSize(0, 40);
