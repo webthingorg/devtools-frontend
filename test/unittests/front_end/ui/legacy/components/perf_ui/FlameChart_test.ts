03c1549f8a03657c9bf115f072fb3f5c6ad8d925
Andrés Olivares <andoli@chromium.org>
2023-10-05 10:04:14 +0000

    Do not schedule track details if an event is selected
    Debouncing the rendering of a track's details when an event in it has
    been selected means the event's details will disappear and be replaced
    with the track's details. Since we still want to debounce the rendering
    in paths called repeatedly in short times (like scrolling or zooming),
    let's skip the debouncing only when a new event is selected.
    
    Fixed: 1489260
    Change-Id: I4c21fea31376a45243eb3e2076a336e70ea4e8ab

--- test/unittests/front_end/ui/legacy/components/perf_ui/FlameChart_test.ts
+++ test/unittests/front_end/ui/legacy/components/perf_ui/FlameChart_test.ts
@@ -480,4 +480,45 @@ describeWithEnvironment('FlameChart', () => {
       });
     });
   });
+
+  describe('entryIndexToCoordinates', () => {
+    class SetSelectedEntryTestProvider extends FakeFlameChartProvider {
+      override entryColor(_entryIndex: number): string {
+        return 'red';
+      }
+
+      override timelineData(): PerfUI.FlameChart.FlameChartTimelineData|null {
+        return PerfUI.FlameChart.FlameChartTimelineData.create({
+          entryLevels: [1, 1, 1, 1],
+          entryStartTimes: [5, 60, 80, 300],
+          entryTotalTimes: [50, 10, 10, 500],
+          groups: [{
+            name: 'Test Group' as Platform.UIString.LocalizedString,
+            startLevel: 1,
+            style: defaultGroupStyle,
+          }],
+        });
+      }
+    }
+
+    it('returns the correct coordinates for a given entry', async () => {
+      const provider = new SetSelectedEntryTestProvider();
+      const delegate = new MockFlameChartDelegate();
+      chartInstance = new PerfUI.FlameChart.FlameChart(provider, delegate);
+      // Make the width narrow so that not everything fits
+      chartInstance.setSize(100, 400);
+      chartInstance.setWindowTimes(0, 100);
+      renderChart(chartInstance);
+      const {x: canvasOffsetX, y: canvasOffsetY} = chartInstance.getCanvasOffset();
+      // TODO(crbug.com/1440169): We can get all the expected values from
+      // the chart's data and avoid magic numbers
+      assert.deepEqual(chartInstance.entryIndexToCoordinates(0), {x: 36 + canvasOffsetX, y: 51 + canvasOffsetY});
+
+      // Emulate two scrolls to force a change in coordinates.
+      chartInstance.setSelectedEntry(3);
+      assert.deepEqual(chartInstance.entryIndexToCoordinates(0), {x: 36 + canvasOffsetX, y: -17 + canvasOffsetY});
+      chartInstance.setWindowTimes(250, 600);
+      assert.deepEqual(chartInstance.entryIndexToCoordinates(0), {x: -514 + canvasOffsetX, y: -17 + canvasOffsetY});
+    });
+  });
 });
