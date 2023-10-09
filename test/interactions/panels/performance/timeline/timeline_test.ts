023a39652c5e177a19fb37614554166e1b72299c
Andrés Olivares <andoli@chromium.org>
2023-10-09 14:44:29 +0000

    [RPP] Use old engine data when loading a CPU profile
    Since the track appenders don't support data coming outside tracing.
    This is a temporary workaround until we implement crbug.com/1462387
    
    Drive-by fixes: remove the frames track from node version and delete
    some unused code.
    
    Fixed: 1491066
    Change-Id: I378060a8bc1e0903f0c3694ae831c67344be49bd

--- test/interactions/panels/performance/timeline/timeline_test.ts
+++ test/interactions/panels/performance/timeline/timeline_test.ts
@@ -120,12 +120,25 @@ describe('Performance panel', () => {
     await assertElementScreenshotUnchanged(panel, 'performance/cpu-profile.png', 3);
   });
 
-  itScreenshot('loads a cpuprofile and renders it in node mode', async () => {
-    await loadComponentDocExample('performance_panel/basic.html?cpuprofile=node-fibonacci-website&isNode=true');
-    await waitFor('.timeline-flamechart');
-    const panel = await waitFor('body');
-    await assertElementScreenshotUnchanged(panel, 'performance/cpu-profile-node.png', 3);
-  });
+  itScreenshot(
+      'loads a cpuprofile and renders it in node mode with default track source set to new engine', async () => {
+        await loadComponentDocExample(
+            'performance_panel/basic.html?cpuprofile=node-fibonacci-website&isNode=true&threadTracksSource=new');
+        await waitFor('.timeline-flamechart');
+        const panel = await waitFor('body');
+        await assertElementScreenshotUnchanged(panel, 'performance/cpu-profile-node.png', 3);
+      });
+
+  itScreenshot(
+      'loads a cpuprofile and renders it in node mode with default track source set to old engine', async () => {
+        await loadComponentDocExample(
+            'performance_panel/basic.html?cpuprofile=node-fibonacci-website&isNode=true&threadTracksSource=old');
+        await waitFor('.timeline-flamechart');
+        const panel = await waitFor('body');
+        // We use the same screenshot as in the equivalent test with the new engine (above) on
+        // purpose to ensure using either engine yields the same result.
+        await assertElementScreenshotUnchanged(panel, 'performance/cpu-profile-node.png', 3);
+      });
 
   itScreenshot('candy stripes long tasks', async () => {
     await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
