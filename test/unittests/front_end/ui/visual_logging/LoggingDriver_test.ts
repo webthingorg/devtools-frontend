fb3e43285b971c8e7025177cc52da135b7bcf69a
Danil Somsikov <dsv@chromium.org>
2023-10-09 10:26:46 +0200

    Basic VE logging
    Bug: 1467464
    Change-Id: Ibe2568e0f6f9bc9c2c0f28dfa952f629146c1824

---
+++ test/unittests/front_end/ui/visual_logging/LoggingDriver_test.ts
@@ -0,0 +1,42 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Host from '../../../../../front_end/core/host/host.js';
+import * as VisualLoggingTesting from '../../../../../front_end/ui/visual_logging/visual_logging-testing.js';
+import * as VisualLogging from '../../../../../front_end/ui/visual_logging/visual_logging.js';
+import {renderElementIntoDOM} from '../../helpers/DOMHelpers.js';
+
+const {assert} = chai;
+
+describe('LoggingDriver', () => {
+  let parent: HTMLElement;
+  let element: HTMLElement;
+
+  beforeEach(() => {
+    VisualLoggingTesting.LoggingState.resetStateForTesting();
+    parent = document.createElement('div') as HTMLElement;
+    parent.setAttribute('jslog', 'TreeItem');
+    parent.style.width = '300px';
+    parent.style.height = '300px';
+    element = document.createElement('div') as HTMLElement;
+    element.setAttribute('jslog', 'TreeItem; context:42');
+    element.style.width = '300px';
+    element.style.height = '300px';
+    parent.appendChild(element);
+    renderElementIntoDOM(parent);
+  });
+
+  it('logs impressions on startLogging', () => {
+    const recordImpression = sinon.stub(
+        Host.InspectorFrontendHost.InspectorFrontendHostInstance,
+        'recordImpression',
+    );
+    VisualLogging.startLogging();
+    assert.isTrue(recordImpression.calledOnce);
+    assert.sameDeepMembers(
+        recordImpression.firstCall.firstArg.impressions, [{id: 2, type: 1, context: 42, parent: 1}, {id: 1, type: 1}]);
+  });
+
+  // TODO(dsv): Add more tests
+});
