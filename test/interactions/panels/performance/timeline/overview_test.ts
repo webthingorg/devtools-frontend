3589c25568b92e534091c270091bcb10430c8940
Hadrien Jaubert <hadrienjaubert@google.com>
2023-10-12 11:36:12 +0000

    [RPP] Add white border to separate colors inside the minimap
    Bug: 1489613
    Change-Id: I87e0b381ffa3c05223ca7a37c1a210192a0ac72b

--- test/interactions/panels/performance/timeline/overview_test.ts
+++ test/interactions/panels/performance/timeline/overview_test.ts
@@ -12,31 +12,31 @@ describe('Performance panel overview/minimap', () => {
   itScreenshot('renders the overview', async () => {
     await loadComponentDocExample('performance_panel/overview.html?trace=web-dev');
     const pane = await waitFor('.container #timeline-overview-pane');
-    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview.png', 3);
+    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview.png', 0);
   });
 
   itScreenshot('shows a red bar for a long task', async () => {
     await loadComponentDocExample('performance_panel/overview.html?trace=one-second-interaction');
     const pane = await waitFor('.container #timeline-overview-pane');
-    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-long-task-red-bar.png', 3);
+    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-long-task-red-bar.png', 0);
   });
 
   itScreenshot('shows network requests in the overview', async () => {
     await loadComponentDocExample('performance_panel/overview.html?trace=many-requests');
     const pane = await waitFor('.container #timeline-overview-pane');
-    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-busy-network.png', 3);
+    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-busy-network.png', 0);
   });
 
   itScreenshot('shows the resizers in the overview', async () => {
     await loadComponentDocExample(
         'performance_panel/overview.html?trace=one-second-interaction&windowStart=141251500&windowEnd=141253500');
     const pane = await waitFor('.container #timeline-overview-pane');
-    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-resizers.png', 3);
+    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-resizers.png', 0);
   });
 
   itScreenshot('shows the memory usage', async () => {
     await loadComponentDocExample('performance_panel/overview.html?trace=web-dev');
     const pane = await waitFor('.container-with-memory #timeline-overview-pane');
-    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-memory.png', 3);
+    await assertElementScreenshotUnchanged(pane, 'performance/timeline-overview-memory.png', 0);
   });
 });
