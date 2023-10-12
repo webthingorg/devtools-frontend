20da3d47409ebda9605ba8e7becd84ed05749b24
Simon Zünd <szuend@chromium.org>
2023-10-12 10:01:38 +0000

    Move localized breakpoint categories from sdk/ to ui/
    This CL introduces a new `Category` enum that we use on the sdk/ layer`
    instead of localized strings. Localization now happens on the ui/
    layer.
    
    Note that we preserve the existing behavior of sorting the categories
    alphabetically given a local. We only slightly improve this by using
    String.prototype.localeCompare instead of default string comparison.
    
    R=dsv@chromium.org
    
    Bug: 1445175
    Change-Id: I28282a87a504355f48dc250dbdff6ce323035946

--- front_end/core/sdk/CategorizedBreakpoint.ts
+++ front_end/core/sdk/CategorizedBreakpoint.ts
@@ -2,19 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export const enum Category {
+  Animation = 1,
+  AuctionWorklet = 2,
+  Canvas = 3,
+  Clipboard = 4,
+  Control = 5,
+  Device = 6,
+  DomMutation = 7,
+  DragDrop = 8,
+  Geolocation = 9,
+  Keyboard = 10,
+  Load = 11,
+  Media = 12,
+  Mouse = 13,
+  Notification = 14,
+  Parse = 15,
+  PictureInPicture = 16,
+  Pointer = 17,
+  Script = 18,
+  Timer = 19,
+  Touch = 20,
+  TrustedTypeViolation = 21,
+  WebAudio = 22,
+  Window = 23,
+  Worker = 24,
+  Xhr = 25,
+}
+
 export class CategorizedBreakpoint {
-  readonly #categoryInternal: string;
+  readonly #category: Category;
   titleInternal: string;
   enabledInternal: boolean;
 
-  constructor(category: string, title: string) {
-    this.#categoryInternal = category;
+  constructor(category: Category, title: string) {
+    this.#category = category;
     this.titleInternal = title;
     this.enabledInternal = false;
   }
 
-  category(): string {
-    return this.#categoryInternal;
+  category(): Category {
+    return this.#category;
   }
 
   enabled(): boolean {
