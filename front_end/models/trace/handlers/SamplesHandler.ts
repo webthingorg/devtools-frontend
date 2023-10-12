35e5be60be11946e6c31d1fef1b1a8eee8dd96e4
Andrés Olivares <andoli@chromium.org>
2023-10-12 08:12:17 +0000

    [RPP] Use new engine in cpu profiling
    Whenever we are in cpu profiling mode, we take the tree built by the samples handler and use it in the ThreadAppender
    
    The caveat is this relies on the fake CpuProfile event injected by TimelineLoader, which is made available to the trace engine via the tracing model instance it uses. Whenever we want to remove the old engine, we need to figure out another approach to inject said event (or not inject any event at all).
    
    Bug: 1462387
    Change-Id: I4702205220fae122a4e6259eab8dd8b505881d11

--- front_end/models/trace/handlers/SamplesHandler.ts
+++ front_end/models/trace/handlers/SamplesHandler.ts
@@ -121,6 +121,23 @@ export function handleEvent(event: Types.TraceEvents.TraceEventData): void {
     throw new Error('Samples Handler is not initialized');
   }
 
+  /**
+   * A fake trace event created to support CDP.Profiler.Profiles in the
+   * trace engine.
+   */
+  if (Types.TraceEvents.isSyntheticTraceEventCpuProfile(event)) {
+    // At the moment we are attaching to a single node target so we
+    // should only get a
+    const pid = event.pid;
+    const tid = event.tid;
+    // Create an arbitrary
+    const profileId = '0x1' as Types.TraceEvents.ProfileID;
+    const profileData = getOrCreatePreProcessedData(pid, profileId);
+    profileData.rawProfile = event.args.data.cpuProfile;
+    profileData.threadId = tid;
+    return;
+  }
+
   if (Types.TraceEvents.isTraceEventProfile(event)) {
     // Do not use event.args.data.startTime as it is in CLOCK_MONOTONIC domain,
     // but use profileEvent.ts which has been translated to Perfetto's clock
@@ -181,6 +198,7 @@ export async function finalize(): Promise<void> {
     throw new Error('Samples Handler is not initialized');
   }
   buildProfileCalls();
+
   handlerState = HandlerState.FINALIZED;
 }
 
