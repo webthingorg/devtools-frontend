c986f376049f6b24731f755cfc7c9559164c976f
Anton Maliev <amaliev@chromium.org>
2023-10-19 18:46:16 +0000

    Show cookie deprecation metadata (allowlist) issues in the DevTools frontend panel.
    Bug: b/305738703
    Change-Id: I76b348352e77d0012a221f192f04d2f2aafc8896

---
+++ test/e2e/issues/cookie-deprecation-metadata-issues_test.ts
@@ -0,0 +1,58 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertNotNullOrUndefined, getBrowserAndPages, goToResource} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {
+  ensureResourceSectionIsExpanded,
+  expandIssue,
+  getIssueByTitle,
+  getResourcesElement,
+  navigateToIssuesTab,
+  waitForTableFromResourceSectionContents,
+} from '../helpers/issues-helpers.js';
+
+describe('Cookie Deprecation Metadata issue', async () => {
+  beforeEach(async () => {
+    await goToResource('empty.html');
+  });
+
+  it('should display correct information', async () => {
+    await navigateToIssuesTab();
+    const {frontend} = getBrowserAndPages();
+    frontend.evaluate(() => {
+      const issue = {
+        'code': 'CookieDeprecationMetadataIssue',
+        'details': {
+          'cookieDeprecationMetadataIssueDetails': {
+            'allowedSites': ['example_1.test'],
+          },
+        },
+      };
+      // @ts-ignore
+      window.addIssueForTest(issue);
+      const issue2 = {
+        'code': 'CookieDeprecationMetadataIssue',
+        'details': {
+          'cookieDeprecationMetadataIssueDetails': {
+            'allowedSites': ['example_2.test'],
+          },
+        },
+      };
+      // @ts-ignore
+      window.addIssueForTest(issue2);
+    });
+    await expandIssue();
+    const issueElement =
+        await getIssueByTitle('Chrome may soon delete state for intermediate websites in a recent navigation chain');
+    assertNotNullOrUndefined(issueElement);
+    const section = await getResourcesElement('2 websites allowed to access cookies', issueElement);
+    await ensureResourceSectionIsExpanded(section);
+    const expectedTableRows = [
+      ['example_1.test'],
+      ['example_2.test'],
+    ];
+    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+  });
+});
