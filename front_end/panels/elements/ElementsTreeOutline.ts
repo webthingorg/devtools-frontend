7978faf709c730b1a4ea38a42311b8be3bff8f63
Danil Somsikov <dsv@chromium.org>
2023-10-24 15:13:17 +0200

    Annotate the rest of the elements panel for the visual logging
    Bug: 1467464
    Change-Id: Ice4376011d410d12566a2219cb91c9d94f24f136

--- front_end/panels/elements/ElementsTreeOutline.ts
+++ front_end/panels/elements/ElementsTreeOutline.ts
@@ -44,6 +44,7 @@ import * as CodeHighlighter from '../../ui/components/code_highlighter/code_high
 import * as IconButton from '../../ui/components/icon_button/icon_button.js';
 import * as IssueCounter from '../../ui/components/issue_counter/issue_counter.js';
 import * as UI from '../../ui/legacy/legacy.js';
+import * as VisualLogging from '../../ui/visual_logging/visual_logging.js';
 import {IssuesPane} from '../issues/IssuesPane.js';
 
 import * as ElementsComponents from './components/components.js';
@@ -51,9 +52,8 @@ import {ElementsPanel} from './ElementsPanel.js';
 import {ElementsTreeElement, InitialChildrenLimit} from './ElementsTreeElement.js';
 import elementsTreeOutlineStyles from './elementsTreeOutline.css.js';
 import {ImagePreviewPopover} from './ImagePreviewPopover.js';
-import {TopLayerContainer} from './TopLayerContainer.js';
-
 import {type MarkerDecoratorRegistration} from './MarkerDecorator.js';
+import {TopLayerContainer} from './TopLayerContainer.js';
 
 const UIStrings = {
   /**
@@ -138,6 +138,7 @@ export class ElementsTreeOutline extends
 
     this.elementInternal = this.element;
     this.elementInternal.classList.add('elements-tree-outline', 'source-code');
+    this.elementInternal.setAttribute('jslog', `${VisualLogging.elementsTreeOutline()}`);
     if (hideGutter) {
       this.elementInternal.classList.add('elements-hide-gutter');
     }
