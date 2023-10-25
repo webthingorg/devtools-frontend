4dea16959b51f8500156075501e9c8aa807e2c99
Nancy Li <nancyly@chromium.org>
2023-10-25 12:35:38 +0000

    [RPP] Reset flamechart after data in the data provider is really changed
    Now in the network flame chart, when we change the time window, it will
    filter out some network request so this is a "real" different
    |FlameChartTimelineData|, so we should reset all previously calculated
    data to re-render.
    
    Also added some sanity checks to avoid UI breakage.
    
    Long-term we should do these:
      - Add a test to cover the zooming actions
      - Thinking about something like |TreeManipulator| to filter the event
    
    Bug: 1495583
    Change-Id: I8e2d32023e4c2c65bf324170c6e3600b0cf98c03

--- front_end/panels/timeline/TimelineFlameChartView.ts
+++ front_end/panels/timeline/TimelineFlameChartView.ts
@@ -186,8 +186,10 @@ export class TimelineFlameChartView extends UI.Widget.VBox implements PerfUI.Fla
            this.#currentBreadcrumbTimeWindow.max < window.right));
     if (!this.#currentBreadcrumbTimeWindow || isWindowWithinBreadcrumb) {
       this.mainFlameChart.setWindowTimes(window.left, window.right, animate);
-      this.networkFlameChart.setWindowTimes(window.left, window.right, animate);
+
+      this.networkFlameChart.reset();
       this.networkDataProvider.setWindowTimes(window.left, window.right);
+      this.networkFlameChart.setWindowTimes(window.left, window.right, animate);
     }
 
     this.updateSearchResults(false, false);
@@ -242,8 +244,8 @@ export class TimelineFlameChartView extends UI.Widget.VBox implements PerfUI.Fla
       ];
       const window = this.model.window();
       this.mainFlameChart.setWindowTimes(window.left, window.right);
-      this.networkFlameChart.setWindowTimes(window.left, window.right);
       this.networkDataProvider.setWindowTimes(window.left, window.right);
+      this.networkFlameChart.setWindowTimes(window.left, window.right);
       this.updateSearchResults(false, false);
       this.updateColorMapper();
     }
