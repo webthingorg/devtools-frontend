3a6824e31c990dde05f5e5c76f4f67d0614be383
Alina Varkki <alinavarkki@google.com>
2023-10-27 17:29:18 +0000

    Trigger event in timelineflamechartview when entries modified
    Bug: 1469887
    Change-Id: I1cc2e7bde58f4d42dc26439ff0b64898848076a0

--- front_end/ui/legacy/components/perf_ui/FlameChart.ts
+++ front_end/ui/legacy/components/perf_ui/FlameChart.ts
@@ -2716,6 +2716,11 @@ export enum Events {
    * chosen from the flame chart context  menu
    */
   TreeModified = 'TreeModified',
+  /**
+   * Emitted when a there is a modify actioned(ex. merge, collapse recursion)
+   * chosen from the flame chart context  menu
+   */
+  EntriesModified = 'EntriesModified',
 }
 
 export type EventTypes = {
@@ -2727,6 +2732,7 @@ export type EventTypes = {
     group: Group,
     node: number,
   },
+  [Events.EntriesModified]: void,
 };
 
 export interface Group {
