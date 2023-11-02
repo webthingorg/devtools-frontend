b9c171575590ed30c673c5b84ea02d7d75c5d8bd
Miina Koyama <miinak@google.com>
2023-11-02 05:24:39 +0000

    (frontend 2) Preloading DevTools: Show header mismatch on prerender activation
    In the previous CL[1], it changes the protocol on the DevTool side so
    that DevTool can receive the information of mismatched headers.
    
    This CL enables DevTool to display mismatched headers in a table on the
    Speculative loads panel when the header mismatch occurs.
    
    [1] https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4930959
    
    Demo:
    https://drive.google.com/file/d/1WWNsAdDGbMNEhy3opHS7NOvjVGUEW93X/view?usp=sharing&resourcekey=0-WVGananT68p7h7C71eSfLg
    
    Design Document:
    https://docs.google.com/document/d/1lIwZwBMgCQXH0hRnc2Guj-zOBI2rHtkI2yinhYjsFvo/edit?usp=sharing
    
    Bug: 1456673
    Change-Id: If58fde28621b844e378395cda721db12cd9ea109

---
+++ front_end/panels/application/preloading/components/PreloadingMismatchedHeadersGrid.ts
@@ -0,0 +1,141 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as i18n from '../../../../core/i18n/i18n.js';
+import {assertNotNullOrUndefined} from '../../../../core/platform/platform.js';
+import type * as SDK from '../../../../core/sdk/sdk.js';
+import * as DataGrid from '../../../../ui/components/data_grid/data_grid.js';
+import * as ComponentHelpers from '../../../../ui/components/helpers/helpers.js';
+import * as LegacyWrapper from '../../../../ui/components/legacy_wrapper/legacy_wrapper.js';
+import type * as UI from '../../../../ui/legacy/legacy.js';
+import * as LitHtml from '../../../../ui/lit-html/lit-html.js';
+
+import preloadingGridStyles from './preloadingGrid.css.js';
+
+// const UIStrings = {
+//     /**
+//      *@description Column header
+//      */
+//     headerName: 'headerName',
+//     /**
+//      *@description Column header: Initial value
+//      */
+//     initialValue: 'initialValue',
+//     /**
+//      *@description Column header: Activation value
+//      */
+//     activationValue: 'activationValue',
+// };
+// const str_ =
+//     i18n.i18n.registerUIStrings('panels/application/preloading/components/PreloadingMismatchedHeadersGrid.ts', UIStrings);
+// export const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+const {render, html} = LitHtml;
+
+// export interface PreloadingMismatchedHeadersGridData {
+//   rows: PreloadingMismatchedHeadersGridRow[];
+// }
+
+// export interface PreloadingMismatchedHeadersGridRow {
+//   id: string;
+//   attempt: SDK.PreloadingModel.PrerenderAttempt;
+// }
+
+export class PreloadingMismatchedHeadersGrid extends LegacyWrapper.LegacyWrapper.WrappableComponent<UI.Widget.VBox> {
+  static readonly litTagName = LitHtml.literal`devtools-resources-preloading-mismatched-headers-grid`;
+
+  readonly #shadow = this.attachShadow({mode: 'open'});
+  #data: SDK.PreloadingModel.PrerenderAttempt|null = null;
+  connectedCallback(): void {
+    this.#shadow.adoptedStyleSheets = [preloadingGridStyles];
+    this.#render();
+  }
+
+  set data(data: SDK.PreloadingModel.PrerenderAttempt) {
+    if (data === null) {
+      return;
+    }
+    this.#data = data;
+    this.#render();
+  }
+
+  #render(): void {
+    if (this.#data === null) {
+      return;
+    }
+
+    const reportsGridData: DataGrid.DataGridController.DataGridControllerData = {
+      columns: [
+        {
+          id: 'headerName',
+          title: i18n.i18n.lockedString('Header name'),
+          widthWeighting: 40,
+          hideable: false,
+          visible: true,
+          sortable: true,
+        },
+        {
+          id: 'initialValue',
+          title: i18n.i18n.lockedString('Initial value'),
+          widthWeighting: 15,
+          hideable: false,
+          visible: true,
+          sortable: true,
+        },
+        {
+          id: 'activationValue',
+          title: i18n.i18n.lockedString('Activation value'),
+          widthWeighting: 20,
+          hideable: false,
+          visible: true,
+          sortable: true,
+        },
+      ],
+      rows: this.#buildReportRows(),
+      striped: true,
+    };
+
+    // Disabled until https://crbug.com/1079231 is fixed.
+    // clang-format off
+      render(html`
+        <div class="preloading-container">
+          <${DataGrid.DataGridController.DataGridController.litTagName} .data=${
+              reportsGridData as DataGrid.DataGridController.DataGridControllerData}>
+          </${DataGrid.DataGridController.DataGridController.litTagName}>
+        </div>
+      `, this.#shadow, {host: this});
+    // clang-format on
+  }
+
+  #buildReportRows(): DataGrid.DataGridUtils.Row[] {
+    assertNotNullOrUndefined(this.#data);
+    assertNotNullOrUndefined(this.#data.mismatchedHeaders);
+
+    return this.#data.mismatchedHeaders.map(mismatchedHeaders => ({
+                                              cells: [
+                                                {
+                                                  columnId: 'headerName',
+                                                  value: mismatchedHeaders.headerName,
+                                                },
+                                                {
+                                                  columnId: 'initialValue',
+                                                  value: mismatchedHeaders.initialValue || null,
+                                                },
+                                                {
+                                                  columnId: 'activationValue',
+                                                  value: mismatchedHeaders.activationValue || null,
+                                                },
+                                              ],
+                                            }));
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent(
+    'devtools-resources-preloading-mismatched-headers-grid', PreloadingMismatchedHeadersGrid);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'devtools-resources-preloading-mismatched-headers-grid': PreloadingMismatchedHeadersGrid;
+  }
+}
