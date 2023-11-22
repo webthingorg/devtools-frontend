27da272b50057bef62917a9c5862d24cee387584
Takashi Nakayama <tnak@chromium.org>
2023-11-22 07:45:10 +0000

    RoutingDevTools: Show indicator on the Network panel when matched to router rules.
    This CL adds the initial implementation to distinguish whether a request
    matched the static router or not in the size column of Network panel.
    
    Currently, the size column shows `(ServiceWorker)` when the service worker handles the request. After this CL, the column shows
    `(SW Router)` instead when a router registered on the active service
    worker version matches to the request.
    
    UI changes by this CL are illustrated in the DesignDoc https://docs.google.com/document/d/1dxfJpjw4jKaiZbNufrIRfW4Q74OVK7hEOmAnLZSgibE/edit#heading=h.or9lkqab3i3k
    
    Backend: https://crrev.com/c/5040035
    
    Bug: 1467701
    Change-Id: Ifa185c184387b9695ae07ca6015929bfcfd2bbb0

--- front_end/panels/network/NetworkDataGridNode.ts
+++ front_end/panels/network/NetworkDataGridNode.ts
@@ -214,6 +214,16 @@ const UIStrings = {
    */
   servedFromDiskCacheResourceSizeS: 'Served from disk cache, resource size: {PH1}',
   /**
+   *@description Text of a DOM element in Network Data Grid Node of the Network panel
+   */
+  serviceWorkerRouter: '(`ServiceWorker Router`)',
+  /**
+   *@description Cell title in Network Data Grid Node of the Network panel
+   *@example {1} PH1
+   *@example {4 B} PH2
+   */
+  matchedToServiceWorkerRouter: 'Matched to `ServiceWorker Router`#{PH1}, resource size: {PH2}',
+  /**
    *@description Text in Network Data Grid Node of the Network panel
    */
   pending: 'Pending',
@@ -1495,6 +1505,12 @@ export class NetworkRequestNode extends NetworkNode {
     if (this.requestInternal.cachedInMemory()) {
       UI.UIUtils.createTextChild(cell, i18nString(UIStrings.memoryCache));
       UI.Tooltip.Tooltip.install(cell, i18nString(UIStrings.servedFromMemoryCacheResource, {PH1: resourceSize}));
+      cell.classList.add('network-dim-cell');
+    } else if (this.requestInternal.serviceWorkerRouterInfo) {
+      const ruleIdMatched = this.requestInternal.serviceWorkerRouterInfo.ruleIdMatched;
+      UI.UIUtils.createTextChild(cell, i18nString(UIStrings.serviceWorkerRouter));
+      UI.Tooltip.Tooltip.install(
+          cell, i18nString(UIStrings.matchedToServiceWorkerRouter, {PH1: ruleIdMatched, PH2: resourceSize}));
       cell.classList.add('network-dim-cell');
     } else if (this.requestInternal.fetchedViaServiceWorker) {
       UI.UIUtils.createTextChild(cell, i18nString(UIStrings.serviceworker));
