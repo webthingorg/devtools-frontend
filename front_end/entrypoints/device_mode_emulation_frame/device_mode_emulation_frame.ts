2df492b37cc18d33931d4fbfb0665f85378a3f13
Ergun Erdogmus <ergunsh@chromium.org>
2023-11-23 15:35:09 +0100

    [VELogging] Add VE logging for device mode view
    Fixed: b/308381406
    Change-Id: I56849ecf960950526a9b77469cd536b3e2d42618

--- front_end/entrypoints/device_mode_emulation_frame/device_mode_emulation_frame.ts
+++ front_end/entrypoints/device_mode_emulation_frame/device_mode_emulation_frame.ts
@@ -5,8 +5,14 @@
 import '../../core/dom_extension/dom_extension.js';
 import '../../Images/Images.js';
 
+import * as Root from '../../core/root/root.js';
+import * as VisualLogging from '../../ui/visual_logging/visual_logging.js';
+
 if (window.opener) {
   // @ts-ignore TypeScript doesn't know about `Emulation` on `Window`.
   const app = window.opener.Emulation.AdvancedApp.instance();
   app.deviceModeEmulationFrameLoaded(document);
+  if (Root.Runtime.Runtime.queryParam('veLogging')) {
+    void VisualLogging.startLogging();
+  }
 }
