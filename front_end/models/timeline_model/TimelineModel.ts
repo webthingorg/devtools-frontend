cad8eae7df1231e41a9fdfd9962b07f8ab7aa0b0
Elorm Coch <elormcoch@microsoft.com>
2023-11-27 15:59:46 -0800

    [wip] Performance tooling for cross thread post messsages
    This change provides support to render post message event data on the timeline tracks and creates association lines between post message event dispatch and handler events.
    
    [Design Doc](https://docs.google.com/document/d/14v-muPKa4ylStxY4qlDi7KFWGEZkad-9j5jo9-FTAf4/edit?usp=sharing)
    
    
    [Relevant chromium.src PR](https://chromium-review.googlesource.com/c/chromium/src/+/5022285)
    
    Bug: 1500360
    Change-Id: I2b35977225e28e345d636b207a9e63b04af7a9d5

--- front_end/models/timeline_model/TimelineModel.ts
+++ front_end/models/timeline_model/TimelineModel.ts
@@ -1383,6 +1383,9 @@ export enum RecordType {
   Profile = 'Profile',
 
   AsyncTask = 'AsyncTask',
+
+  PostMessageDispatch = 'PostMessage.Dispatch',
+  PostMessageHandler = 'PostMessage.Handler',
 }
 
 export namespace TimelineModelImpl {
