0fa72fb567d53807085a9f054a257373f1ec1799
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-12-20 14:34:00 +0000

    Visual logging for Application > Cache storage
    Bug: b/308381403
    Change-Id: I4c485fa0f6344c8cc2f9ea8d67fe72d85b657a7b

--- front_end/panels/network/RequestPreviewView.ts
+++ front_end/panels/network/RequestPreviewView.ts
@@ -31,14 +31,14 @@
 import * as i18n from '../../core/i18n/i18n.js';
 import type * as SDK from '../../core/sdk/sdk.js';
 import * as TextUtils from '../../models/text_utils/text_utils.js';
+import * as LegacyWrapper from '../../ui/components/legacy_wrapper/legacy_wrapper.js';
 import * as SourceFrame from '../../ui/legacy/components/source_frame/source_frame.js';
 import * as UI from '../../ui/legacy/legacy.js';
-import * as LegacyWrapper from '../../ui/components/legacy_wrapper/legacy_wrapper.js';
 
+import {WebBundleInfoView} from './components/WebBundleInfoView.js';
 import {RequestHTMLView} from './RequestHTMLView.js';
 import {RequestResponseView} from './RequestResponseView.js';
 import {SignedExchangeInfoView} from './SignedExchangeInfoView.js';
-import {WebBundleInfoView} from './components/WebBundleInfoView.js';
 
 const UIStrings = {
   /**
