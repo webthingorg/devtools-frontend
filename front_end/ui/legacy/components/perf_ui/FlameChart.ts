82435f0b3ceda94290e554e8b47139de06d2ac7a
Alina Varkki <alinavarkki@google.com>
2023-12-27 11:50:39 +0000

    click on the edge on entry
    Bug
    
    Change-Id: I32b29c59e9e1abe5b1407d828254e982764822d9

--- front_end/ui/legacy/components/perf_ui/FlameChart.ts
+++ front_end/ui/legacy/components/perf_ui/FlameChart.ts
@@ -115,6 +115,7 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
   private readonly selectedElement: HTMLElement;
   private rulerEnabled: boolean;
   private barHeight: number;
+  private barThresholdPx: number;
   private textBaseline: number;
   private textPadding: number;
   private readonly headerLeftPadding: number;
@@ -209,6 +210,7 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
 
     this.rulerEnabled = true;
     this.barHeight = 17;
+    this.barThresholdPx = 3;
     this.textBaseline = 5;
     this.textPadding = 5;
     this.chartViewport.setWindowTimes(
@@ -740,6 +742,22 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
     this.draw();
   }
 
+  #dispatchTreeModifiedEvent(treeAction: TraceEngine.EntriesFilter.FilterAction, index: number): void {
+    const data = this.timelineData();
+    if (!data) {
+      return;
+    }
+    const group = data.groups.at(this.selectedGroupIndex);
+    if (!group || !group.expanded || !group.showStackContextMenu) {
+      return;
+    }
+    this.dispatchEventToListeners(Events.TreeModified, {
+      group: group,
+      node: index,
+      action: treeAction,
+    });
+  }
+
   #onContextMenu(_event: Event): void {
     // The context menu only applies if the user is hovering over an individual entry.
     if (this.highlightedEntryIndex === -1) {
@@ -758,8 +776,6 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
     if (!group || !group.expanded || !group.showStackContextMenu) {
       return;
     }
-    // TODO(crbug.com/1469887): implement context menu actions that allow to modify flame chart trees.
-    // TODO(crbug.com/1469887): keep selectedTrack and Y scroll poisition when context menu action is applied.
 
     // Update the selected index to match the highlighted index, which
     // represents the entry under the cursor where the user has right clicked
@@ -767,33 +783,25 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
     this.dispatchEventToListeners(Events.EntryInvoked, this.highlightedEntryIndex);
     const contextMenu = new UI.ContextMenu.ContextMenu(_event);
 
-    const dispatchTreeModifiedEvent = (treeAction: TraceEngine.EntriesFilter.FilterAction): void => {
-      this.dispatchEventToListeners(Events.TreeModified, {
-        group: group,
-        node: this.selectedEntryIndex,
-        action: treeAction,
-      });
-    };
-
     // TODO(crbug.com/1469887): Change text/ui to the final designs when they are complete.
     contextMenu.headerSection().appendItem('Merge function', () => {
-      dispatchTreeModifiedEvent(TraceEngine.EntriesFilter.FilterApplyAction.MERGE_FUNCTION);
+      this.#dispatchTreeModifiedEvent(
+          TraceEngine.EntriesFilter.FilterApplyAction.MERGE_FUNCTION, this.highlightedEntryIndex);
     });
 
     contextMenu.headerSection().appendItem('Collapse function', () => {
-      dispatchTreeModifiedEvent(TraceEngine.EntriesFilter.FilterApplyAction.COLLAPSE_FUNCTION);
+      this.#dispatchTreeModifiedEvent(
+          TraceEngine.EntriesFilter.FilterApplyAction.COLLAPSE_FUNCTION, this.highlightedEntryIndex);
     });
 
     contextMenu.headerSection().appendItem('Collapse repeating descendants', () => {
-      dispatchTreeModifiedEvent(TraceEngine.EntriesFilter.FilterApplyAction.COLLAPSE_REPEATING_DESCENDANTS);
-    });
-
-    contextMenu.headerSection().appendItem('Reset children', () => {
-      dispatchTreeModifiedEvent(TraceEngine.EntriesFilter.FilterUndoAction.RESET_CHILDREN);
+      this.#dispatchTreeModifiedEvent(
+          TraceEngine.EntriesFilter.FilterApplyAction.COLLAPSE_REPEATING_DESCENDANTS, this.highlightedEntryIndex);
     });
 
     contextMenu.headerSection().appendItem('Reset trace', () => {
-      dispatchTreeModifiedEvent(TraceEngine.EntriesFilter.FilterUndoAction.UNDO_ALL_ACTIONS);
+      this.#dispatchTreeModifiedEvent(
+          TraceEngine.EntriesFilter.FilterUndoAction.UNDO_ALL_ACTIONS, this.highlightedEntryIndex);
     });
 
     void contextMenu.show();
@@ -1106,8 +1114,7 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
       const duration = timelineData.entryTotalTimes[entryIndex];
       const startX = this.chartViewport.timeToPosition(startTime);
       const endX = this.chartViewport.timeToPosition(startTime + duration);
-      const barThresholdPx = 3;
-      return startX - barThresholdPx < x && x < endX + barThresholdPx;
+      return startX - this.barThresholdPx < x && x < endX + this.barThresholdPx;
     }
 
     let entryIndex: number = entriesOnLevel[indexOnLevel];
@@ -1119,6 +1126,22 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
       return entryIndex;
     }
     return -1;
+  }
+
+  isEntryArrowClicked(x: number, index: number): boolean {
+    const timelineData = this.timelineData();
+    if (!timelineData) {
+      return false;
+    }
+
+    const startTime = timelineData.entryStartTimes[index];
+    const duration = timelineData.entryTotalTimes[index];
+    const endX = this.chartViewport.timeToPosition(startTime + duration);
+    const arrowWidth = this.barHeight;
+    if (endX - arrowWidth - this.barThresholdPx < x && x < endX + this.barThresholdPx) {
+      return true;
+    }
+    return false;
   }
 
   /**
@@ -2488,6 +2511,9 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
   }
 
   setSelectedEntry(entryIndex: number): void {
+    if (this.isEntryArrowClicked(this.lastMouseOffsetX, entryIndex)) {
+      this.#dispatchTreeModifiedEvent(TraceEngine.EntriesFilter.FilterUndoAction.RESET_CHILDREN, entryIndex);
+    }
     if (this.selectedEntryIndex === entryIndex) {
       return;
     }
