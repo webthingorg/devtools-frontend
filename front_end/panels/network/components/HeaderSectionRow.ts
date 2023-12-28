c7e62b30a36e003d1375a86d812832cdcce049a9
Benedikt Meurer <bmeurer@chromium.org>
2023-12-28 12:04:01 +0100

    [cleanup] Remove `iconPath` and go with just `iconName` instead.
    Previously the `IconButton.Icon.Icon` component allowed to pass either
    the basename of an SVG icon in the `front_end/Images/` folder (via the
    `iconName` property) or the URL of an icon somewhere (via `iconPath`).
    
    This CL simplifies the `Icon` element to only accept names now.
    
    Bug: chromium:1421730
    Change-Id: I9f1d1e7d68631b274a63eff64bf0201066ec8c3f

--- front_end/panels/network/components/HeaderSectionRow.ts
+++ front_end/panels/network/components/HeaderSectionRow.ts
@@ -61,9 +61,6 @@ const UIStrings = {
 const str_ = i18n.i18n.registerUIStrings('panels/network/components/HeaderSectionRow.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 
-const trashIconUrl = new URL('../../../Images/bin.svg', import.meta.url).toString();
-const editIconUrl = new URL('../../../Images/edit.svg', import.meta.url).toString();
-
 export const isValidHeaderName = (headerName: string): boolean => {
   return /^[a-z0-9_\-]+$/i.test(headerName);
 };
@@ -245,7 +242,7 @@ export class HeaderSectionRow extends HTMLElement {
         <${Buttons.Button.Button.litTagName}
           title=${i18nString(UIStrings.editHeader)}
           .size=${Buttons.Button.Size.TINY}
-          .iconUrl=${editIconUrl}
+          .iconName="edit"
           .variant=${Buttons.Button.Variant.ROUND}
           .iconWidth=${'16px'}
           .iconHeight=${'16px'}
@@ -269,7 +266,7 @@ export class HeaderSectionRow extends HTMLElement {
       <${Buttons.Button.Button.litTagName}
         title=${i18nString(UIStrings.removeOverride)}
         .size=${Buttons.Button.Size.TINY}
-        .iconUrl=${trashIconUrl}
+        .iconName="bin"
         .variant=${Buttons.Button.Variant.ROUND}
         .iconWidth=${'13px'}
         .iconHeight=${'13px'}
