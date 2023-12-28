1981f3cfffcac15f020a50209729bbb6887d3d2a
Benedikt Meurer <bmeurer@chromium.org>
2023-12-28 15:05:32 +0100

    [DevToolsIcons] Streamline the `NewIcon` component.
    Avoid usage of LitElement and decorators, and just create the stupid
    `<span>` by hand in the constructor. Also improve the CSS handling a
    bit and make it possible to hide the element despite having `display`
    set in the `:host` selector.
    
    Bug: chromium:1405244, chromium:1421730
    Change-Id: I029c6d5554dc4fb1c5edbf0ad71f305f58e3da17

--- front_end/ui/components/icon_button/NewIcon.ts
+++ front_end/ui/components/icon_button/NewIcon.ts
@@ -7,31 +7,77 @@ import * as ComponentHelpers from '../helpers/helpers.js';
 
 import iconStyles from './newIcon.css.js';
 
-export class NewIcon extends LitHtml.LitElement {
-  @LitHtml
-      .Decorators.property({
-        type: String,
-      }) name = '';
+/**
+ * A simple icon component to display SVG icons from the `front_end/Images/` folder.
+ *
+ * Usage is simple:
+ *
+ * ```js
+ * // Instantiate directly:
+ * const icon = new IconButton.NewIcon.NewIcon();
+ * icon.name = 'bin';
+ * container.appendChild(icon);
+ *
+ * // Use within a template:
+ * LitHtml.html`
+ *   <${IconButton.NewIcon.NewIcon.litTagName} name="bin">
+ *   </${IconButton.NewIcon.NewIcon.litTagName}>
+ * `;
+ * ```
+ *
+ * The color for the icon defaults to `var(--icon-default)`, while the dimensions
+ * default to 20px times 20px. You can change both color and size via CSS:
+ *
+ * ```css
+ * devtools-new-icon.my-icon {
+ *   color: red;
+ *   width: 14px;
+ *   height: 14px;
+ * }
+ * ```
+ *
+ * If you don't override the `color` property, and use the icon within a `<button>`,
+ * it'll automatically change color to `var(--icon-default-hover)` upon hovering
+ * with the mouse over the surrounding `<button>` element.
+ *
+ * @attr name - The basename of the icon file (not including the `.svg` suffix).
+ * @prop {String} name - The `"name"` attribute is reflected as property.
+ */
+export class NewIcon extends HTMLElement {
+  static readonly litTagName = LitHtml.literal`devtools-new-icon`;
+  static readonly observedAttributes = ['name'];
 
-  static override get styles(): CSSStyleSheet[] {
-    return [iconStyles];
+  readonly #shadowRoot;
+  readonly #icon;
+
+  constructor() {
+    super();
+    this.#shadowRoot = this.attachShadow({mode: 'open'});
+    this.#icon = document.createElement('span');
+    this.#shadowRoot.appendChild(this.#icon);
   }
 
-  override render(): LitHtml.LitTemplate {
-    const path = this.pathFromName(this.name);
-    const styles = {webkitMaskImage: `url(${path})`};
-    return LitHtml.html`<span style=${LitHtml.Directives.styleMap(styles)}></span>`;
+  get name(): string|null {
+    return this.getAttribute('name');
   }
 
-  pathFromName(name: string|null): string {
-    return new URL(`../../../Images/${name}.svg`, import.meta.url).toString();
+  set name(name: string) {
+    this.setAttribute('name', name);
+  }
+
+  connectedCallback(): void {
+    this.#shadowRoot.adoptedStyleSheets = [iconStyles];
+  }
+
+  attributeChangedCallback(_name: string, _oldValue: string, newValue: string): void {
+    const url = new URL(`../../../Images/${newValue}.svg`, import.meta.url);
+    this.#icon.style.setProperty('--icon-url', `url(${url})`);
   }
 }
 
 ComponentHelpers.CustomElements.defineComponent('devtools-new-icon', NewIcon);
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface HTMLElementTagNameMap {
     'devtools-new-icon': NewIcon;
   }
