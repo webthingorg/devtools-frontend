1f9f1dc356ecfb36e831f06d74e83d36a4e10ea9
Andrés Olivares <andoli@chromium.org>
2024-01-02 09:34:46 +0000

    Remove legacy Track definition
    We are no longer using it. We can thus remove the bunch of code related
    to it.
    
    Bug: 1499182
    Change-Id: I94d9bc68e3d44e9be70e8eb6a3daa5f2716f5223

--- front_end/ui/components/docs/performance_panel/track_example.ts
+++ front_end/ui/components/docs/performance_panel/track_example.ts
@@ -6,7 +6,6 @@ import * as EnvHelpers from '../../../../../test/unittests/front_end/helpers/Env
 import * as FrontendHelpers from '../../../../../test/unittests/front_end/helpers/TraceHelpers.js';
 import * as SDK from '../../../../core/sdk/sdk.js';
 import * as Bindings from '../../../../models/bindings/bindings.js';
-import * as TimelineModel from '../../../../models/timeline_model/timeline_model.js';
 import * as Workspace from '../../../../models/workspace/workspace.js';
 import * as Timeline from '../../../../panels/timeline/timeline.js';
 import type * as PerfUI from '../../../legacy/components/perf_ui/perf_ui.js';
@@ -75,9 +74,6 @@ async function renderContent(expanded: boolean) {
       const trackAppenderName = track as Timeline.CompatibilityTracksAppender.TrackAppenderName;
       flameChartData = await FrontendHelpers.getMainFlameChartWithTracks(
           file, new Set([trackAppenderName]), expanded, additionalTrackFilter);
-    } else if (track in TimelineModel.TimelineModel.TrackType) {
-      flameChartData = await FrontendHelpers.getMainFlameChartWithLegacyTrackTypes(
-          file, track as TimelineModel.TimelineModel.TrackType, expanded, additionalTrackFilter);
     } else if (track === 'Network') {
       flameChartData = await FrontendHelpers.getNetworkFlameChartWithLegacyTrack(file, expanded);
     } else {
