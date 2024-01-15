4893586da7fd70343729eac324933d66bc7ec589
Lingqi Chi <lingqi@chromium.org>
2024-01-15 11:02:55 +0000

    [PreloadingPanel] dynamically set the dropdown list's width
    There is a large space in dropdown list.
    This CL turns to calculating the width dynamically instead of using
    the fixed value.
    After this change: https://screenshot.googleplex.com/4Uqm3bdizFC7VSS
    
    Note that this issue also happens to other selectors such as network
    panel's context dropdown list, outermost selector etc. We may want a
    unified solution.
    
    Bug: 1470621
    Change-Id: Ibcfabee77f63649daa488a68923118420e641477

--- front_end/panels/application/preloading/PreloadingView.ts
+++ front_end/panels/application/preloading/PreloadingView.ts
@@ -509,6 +509,9 @@ class PreloadingRuleSetSelector implements
   private readonly listModel: UI.ListModel.ListModel<Protocol.Preload.RuleSetId|typeof AllRuleSetRootId>;
   private readonly dropDown: UI.SoftDropDown.SoftDropDown<Protocol.Preload.RuleSetId|typeof AllRuleSetRootId>;
 
+  // Width set by `UI.SoftDropDown`.
+  private readonly defaultWidth = 315;
+
   constructor(onSelectionChanged: () => void) {
     const model = SDK.TargetManager.TargetManager.instance().scopeTarget()?.model(SDK.PreloadingModel.PreloadingModel);
     assertNotNullOrUndefined(model);
@@ -554,6 +557,15 @@ class PreloadingRuleSetSelector implements
     } else {
       this.dropDown.selectItem(selected);
     }
+    this.updateWidth(items);
+  }
+
+  // Updates the width for the DropDown element.
+  private updateWidth(items: (Protocol.Preload.RuleSetId|typeof AllRuleSetRootId)[]): void {
+    const urlLengths = items.map(x => this.titleFor(x).length);
+    const maxLength = Math.max(...urlLengths);
+    const width = Math.min(maxLength * 6 + 16, this.defaultWidth);
+    this.dropDown.setWidth(width);
   }
 
   // AllRuleSetRootId is used within the selector to indicate the root item. When interacting with PreloadingModel,
