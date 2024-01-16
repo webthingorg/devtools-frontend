11eba111c179309d424f36a51e3f790602283b4d
Danil Somsikov <dsv@chromium.org>
2024-01-16 11:33:10 +0000

    Use kebap case in column IDs so that VE context will also be in camelcase.
    Bug: chromium:1467464, b:320405843
    Change-Id: I21a9ffe2e138230ab1937d0ca5023e6d1bb021e6

--- test/e2e/application/cookies_test.ts
+++ test/e2e/application/cookies_test.ts
@@ -83,7 +83,7 @@ describe('The Application Tab', async () => {
     await doubleClickSourceTreeItem(COOKIES_SELECTOR);
     await doubleClickSourceTreeItem(DOMAIN_SELECTOR);
 
-    const dataGridRowValues1 = await getStorageItemsData(['partitionKey'], 4);
+    const dataGridRowValues1 = await getStorageItemsData(['partition-key'], 4);
     assert.deepEqual(dataGridRowValues1, [
       {
         partitionKey: 'https://localhost',
