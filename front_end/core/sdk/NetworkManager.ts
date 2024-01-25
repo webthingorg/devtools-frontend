d9bbfbedf6374b0f94521fd7e6a6917054d44019
Simon Zünd <szuend@chromium.org>
2024-01-25 09:03:27 +0100

    [network] Use charset as reported by protocol
    R=wolfi@chromium.org
    
    Bug: 1509336
    Change-Id: I3501d2341883cdd1386a6cd1936e6c108f6e7ae2

--- front_end/core/sdk/NetworkManager.ts
+++ front_end/core/sdk/NetworkManager.ts
@@ -471,6 +471,7 @@ export class NetworkDispatcher implements ProtocolProxyApi.NetworkDispatcher {
       networkRequest.setUrl(response.url as Platform.DevToolsPath.UrlString);
     }
     networkRequest.mimeType = response.mimeType;
+    networkRequest.setCharset(response.charset);
     if (!networkRequest.statusCode || networkRequest.wasIntercepted()) {
       networkRequest.statusCode = response.status;
     }
