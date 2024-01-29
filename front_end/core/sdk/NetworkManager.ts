e10b2416a60fb0067e4a52552b088c113a2ba4be
Danil Somsikov <dsv@chromium.org>
2024-01-29 12:54:09 +0000

    Kebabize sdk-meta settings
    Bug: chromium:1467464, b:320405843
    Change-Id: I2dd5bdffb427b29561e4cbb4054cc04e07a581dc

--- front_end/core/sdk/NetworkManager.ts
+++ front_end/core/sdk/NetworkManager.ts
@@ -135,7 +135,7 @@ export class NetworkManager extends SDKModel<EventTypes> {
     this.#networkAgent = target.networkAgent();
     target.registerNetworkDispatcher(this.dispatcher);
     target.registerFetchDispatcher(this.fetchDispatcher);
-    if (Common.Settings.Settings.instance().moduleSetting('cacheDisabled').get()) {
+    if (Common.Settings.Settings.instance().moduleSetting('cache-disabled').get()) {
       void this.#networkAgent.invoke_setCacheDisabled({cacheDisabled: true});
     }
 
@@ -149,7 +149,7 @@ export class NetworkManager extends SDKModel<EventTypes> {
     this.#bypassServiceWorkerSetting.addChangeListener(this.bypassServiceWorkerChanged, this);
 
     Common.Settings.Settings.instance()
-        .moduleSetting('cacheDisabled')
+        .moduleSetting('cache-disabled')
         .addChangeListener(this.cacheDisabledSettingChanged, this);
   }
 
@@ -297,7 +297,7 @@ export class NetworkManager extends SDKModel<EventTypes> {
 
   override dispose(): void {
     Common.Settings.Settings.instance()
-        .moduleSetting('cacheDisabled')
+        .moduleSetting('cache-disabled')
         .removeChangeListener(this.cacheDisabledSettingChanged, this);
   }
 
@@ -1219,7 +1219,7 @@ export class MultitargetNetworkManager extends Common.ObjectWrapper.ObjectWrappe
     this.#updatingInterceptionPatternsPromise = null;
 
     // TODO(allada) Remove these and merge it with request interception.
-    this.#blockingEnabledSetting = Common.Settings.Settings.instance().moduleSetting('requestBlockingEnabled');
+    this.#blockingEnabledSetting = Common.Settings.Settings.instance().moduleSetting('request-blocking-enabled');
     this.#blockedPatternsSetting = Common.Settings.Settings.instance().createSetting('networkBlockedPatterns', []);
     this.#effectiveBlockedURLs = [];
     this.updateBlockedPatterns();
@@ -1509,8 +1509,8 @@ export class MultitargetNetworkManager extends Common.ObjectWrapper.ObjectWrappe
   }
 
   private async updateInterceptionPatterns(): Promise<void> {
-    if (!Common.Settings.Settings.instance().moduleSetting('cacheDisabled').get()) {
-      Common.Settings.Settings.instance().moduleSetting('cacheDisabled').set(true);
+    if (!Common.Settings.Settings.instance().moduleSetting('cache-disabled').get()) {
+      Common.Settings.Settings.instance().moduleSetting('cache-disabled').set(true);
     }
     this.#updatingInterceptionPatternsPromise = null;
     const promises = ([] as Promise<unknown>[]);
@@ -1573,7 +1573,7 @@ export class MultitargetNetworkManager extends Common.ObjectWrapper.ObjectWrappe
       headers['User-Agent'] = currentUserAgent;
     }
 
-    if (Common.Settings.Settings.instance().moduleSetting('cacheDisabled').get()) {
+    if (Common.Settings.Settings.instance().moduleSetting('cache-disabled').get()) {
       headers['Cache-Control'] = 'no-cache';
     }
 
