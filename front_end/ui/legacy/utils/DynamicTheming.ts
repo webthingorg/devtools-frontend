491375328b5d4a58564b037a4892dbc6c8b85652
Kateryna Prokopenko <kprokopenko@chromium.org>
2024-01-29 10:50:46 +0000

    Fetch color stylesheet dynamically only in not hosted mode
    Bug: 1522781
    Change-Id: I91bb87913d37f8d3ac23506cd6681495a6033873

--- front_end/ui/legacy/utils/DynamicTheming.ts
+++ front_end/ui/legacy/utils/DynamicTheming.ts
@@ -2,25 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Host from '../../../core/host/host.js';
+
 // export class instead of function to make sinon spying possible (it cannot mock ES modules)
 export class DynamicTheming {
-  static async refetchColors(document: Document|undefined): Promise<void> {
+  static async fetchColors(document: Document|undefined): Promise<void> {
+    if (Host.InspectorFrontendHost.InspectorFrontendHostInstance.isHostedMode()) {
+      return;
+    }
     if (!document) {
       return;
     }
     const COLORS_CSS_SELECTOR = 'link[href*=\'//theme/colors.css\']';
     const colorCssNode = document.querySelector(COLORS_CSS_SELECTOR);
+    let updatedHref: string;
     if (!colorCssNode) {
-      return;
+      updatedHref = 'devtools://theme/colors.css?sets=ui,chrome';
+    } else {
+      const href = colorCssNode.getAttribute('href');
+      if (!href) {
+        return;
+      }
+      const hrefUrl = new URL(href, location.href);
+      hrefUrl.searchParams.set('version', (new Date()).getTime().toString());
+      updatedHref = hrefUrl.toString();
     }
-    const href = colorCssNode.getAttribute('href');
-    if (!href) {
-      return;
-    }
-    const hrefUrl = new URL(href, location.href);
-    hrefUrl.searchParams.set('version', (new Date()).getTime().toString());
+
     const newColorsCssLink = document.createElement('link');
-    newColorsCssLink.setAttribute('href', hrefUrl.toString());
+    newColorsCssLink.setAttribute('href', updatedHref);
     newColorsCssLink.setAttribute('rel', 'stylesheet');
     newColorsCssLink.setAttribute('type', 'text/css');
     const newColorsLoaded = new Promise<boolean>(resolve => {
@@ -28,7 +37,7 @@ export class DynamicTheming {
       newColorsCssLink.onerror = resolve.bind(this, false);
     });
     document.body.appendChild(newColorsCssLink);
-    if (await newColorsLoaded) {
+    if (colorCssNode && await newColorsLoaded) {
       colorCssNode.remove();
     }
   }
