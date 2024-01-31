6ff83b2ab5dbc15cf749cec1a0924e5c91737e12
Andrés Olivares <andoli@chromium.org>
2024-01-31 15:54:58 +0000

    RPP extensibility based on User Timings prototype
    Bug: none
    Change-Id: Idd46f58ad5693be48b467b18ea52679acea75573

---
+++ front_end/panels/timeline/ExtensionDataGatherer.ts
@@ -0,0 +1,18 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import type * as TraceEngine from '../../models/trace/trace.js';
+
+type TrackData = TraceEngine.Handlers.ModelHandlers.ExtensionTraceData.TrackData;
+export {TrackData};
+
+export class ExtensionDataGatherer {
+  #traceParseData: TraceEngine.Handlers.Types.TraceParseData;
+  constructor(traceParsedData: TraceEngine.Handlers.Types.TraceParseData) {
+    this.#traceParseData = traceParsedData;
+  }
+
+  getExtensionFlameChartData(): readonly TrackData[] {
+    return this.#traceParseData.ExtensionTraceData.extensionFlameCharts;
+  }
+}
