45d5f5b88df7dd470eef9b36f2191075c47d29d0
Kateryna Prokopenko <kprokopenko@chromium.org>
2024-02-08 13:12:39 +0000

    [DevToolsGM3Fonts] Update default and monospace fonts for Linux
    Bug: chromium:1523538
    Change-Id: If9838a3502313ce166cab9822bc7fc3ba67ba363

--- test/e2e/elements/sidebar-event-listeners-remove_test.ts
+++ test/e2e/elements/sidebar-event-listeners-remove_test.ts
@@ -18,23 +18,30 @@ describe('Removing event listeners in the elements sidebar', async () => {
     await loadEventListenersAndSelectButtonNode();
   });
 
-  it('shows "Remove" by each node for a given event', async () => {
+  it('shows "Remove" by each node for a given event', async done => {
+    setTimeout(done, 9000);
+    console.error('before all');
     await openEventListenersPaneAndWaitForListeners();
+    console.error('after open pane, wait for listeners');
     const {
       firstListenerText,
       listenerSelector,
     } = await getFirstNodeForEventListener('[aria-label="click, event listener"]');
+    console.error('after get first node for listener');
 
     // check that we have the right event for the right element
     // and that it has the "Remove" button within it
     assert.include(firstListenerText, 'button#test-button');
     assert.include(firstListenerText, 'Remove');
+    console.error('after asserts');
 
     const removeButtonSelector = `${listenerSelector} .event-listener-button`;
     await click(removeButtonSelector);
+    console.error('after click');
 
     // now we can check that the 'click' event is gone
     const eventListenerNames = await getDisplayedEventListenerNames();
+    console.error('after displayed listener names');
     assert.deepEqual(eventListenerNames, ['custom event', 'hover']);
   });
 });
