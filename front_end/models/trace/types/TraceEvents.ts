7844ec6f42720beb435c586c02274955e680550b
Paul Irish <paulirish@chromium.org>
2024-02-16 16:02:00 -0800

    RPP: Faster trace parsing by removing fancy iterator
    We also increase our chunk sizes optimizing for tasks ~120ms.
    
    Lastly, some micro-optimizations to early exit in the UserTimingsHandler
    
    Bug: chromium:40278532
    Change-Id: I43c0075ebc3fad289bf8c29e573e3f8e3199846b

--- front_end/models/trace/types/TraceEvents.ts
+++ front_end/models/trace/types/TraceEvents.ts
@@ -1716,8 +1716,10 @@ export function isTraceEventPerformanceMeasure(traceEventData: TraceEventData):
 
 export function isTraceEventPerformanceMark(traceEventData: TraceEventData):
     traceEventData is TraceEventPerformanceMark {
-  return traceEventData.cat === 'blink.user_timing' &&
-      (traceEventData.ph === Phase.MARK || traceEventData.ph === Phase.INSTANT);
+  // Resource Timing and Nav Timing numbers are dispatched on the blink.user_timing category,
+  // but the user didn't add with User Timing.  We don't want to include them. Luckily, they use Phase.MARK.
+  // As of m117 (https://crrev.com/c/3656547), _actual_ user timing marks are emitted as Phase.INSTANT
+  return traceEventData.cat === 'blink.user_timing' && traceEventData.ph === Phase.INSTANT;
 }
 
 export function isTraceEventConsoleTime(traceEventData: TraceEventData): traceEventData is TraceEventConsoleTime {
