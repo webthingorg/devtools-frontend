6d5630ee1018e7d5b7e69d96f98fc88f5ffe8ef9
Alina Varkki <alinavarkki@google.com>
2024-02-19 12:23:49 +0000

    draw all events in the array
    Change-Id: I79e861a4f81b8e587d57d47986f1456570ede332

--- front_end/panels/timeline/Initiators.ts
+++ front_end/panels/timeline/Initiators.ts
@@ -22,10 +22,11 @@ export function eventInitiatorPairsToDraw(
   const pairs: InitiatorPair[] = [];
 
   let currentEvent: TraceEngine.Types.TraceEvents.TraceEventData|null = selectedEvent;
-
+  
+  // Build event pairs up to the selected one
   while (currentEvent) {
     const currentInitiator = traceEngineData.Initiators.eventToInitiator.get(currentEvent);
-
+    
     if (currentInitiator) {
       // Store the current pair, and then set the initiator to
       // be the current event, so we work back through the
@@ -54,6 +55,64 @@ export function eventInitiatorPairsToDraw(
     // Go up to the parent, and loop again.
     currentEvent = nodeForCurrentEvent.parent?.entry || null;
   }
+
+  // Build event pairs following the selected one using breath first search
+  let currentEvent2: TraceEngine.Types.TraceEvents.TraceEventData|null = selectedEvent;
+  const eventsQueue = [currentEvent2];
+  const seen: {[key: number]: boolean} = {};
+
+  while (eventsQueue.length > 0) {
+    console.log("here");
+    const currEvent = eventsQueue.pop();
+
+    if(!currEvent) {
+      return pairs;
+    }
+    
+    const nodeForCurrentEvent = traceEngineData.Renderer.entryToNode.get(currEvent);
+    if(!nodeForCurrentEvent?.id) {
+      break;
+    }
+
+    seen[nodeForCurrentEvent?.id] = true;
+
+    const currentInitiators = traceEngineData.Initiators.initiatorToEvents.get(currEvent);
+    // console.log(traceEngineData.Initiators.initiatorToEvents);
+
+    if (currentInitiators) {
+      console.log("initiatooors");
+      // Store the current pair, and then set the initiator to
+      // be the current event, so we work back through the
+      // trace and find the initiator of the initiator, and so
+      // on...
+      currentInitiators.forEach(initiator => {
+        pairs.push({event: currEvent, initiator: initiator});
+        eventsQueue.push(initiator);
+      });
+      // currentEvent = currentInitiator;
+      continue;
+    }
+
+    // Add children to the queue
+    if (!nodeForCurrentEvent) {
+      // Should not happen - if it does something odd is going
+      // on so let's give up.
+      currentEvent = null;
+      break;
+    }
+    const currChildren = nodeForCurrentEvent.children || null;
+    currChildren.forEach(child => {
+      console.log(child.id);
+      if(!seen[child.id]) {
+        // console.log("add");
+        eventsQueue.push(child.entry);
+      } else {
+        console.log("repeating");
+      }
+
+    })
+  }
+
 
   return pairs;
 }
