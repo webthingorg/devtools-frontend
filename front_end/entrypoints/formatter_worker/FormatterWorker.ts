671937197d30dd2cf274cfebe77074256018cf40
Benedikt Meurer <bmeurer@chromium.org>
2024-03-06 11:43:59 +0100

    [cleanup] Fix CodeMirror legacy dependencies.
    Bug: b:40816293, b:323795352
    Change-Id: I0a96ee8a0b60054274e5f89628b450c2e114e858

--- front_end/entrypoints/formatter_worker/FormatterWorker.ts
+++ front_end/entrypoints/formatter_worker/FormatterWorker.ts
@@ -28,19 +28,16 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import '../../third_party/codemirror/codemirror-legacy.js';
+
 import * as Platform from '../../core/platform/platform.js';
 import * as Root from '../../core/root/root.js';
 import * as Acorn from '../../third_party/acorn/acorn.js';
 
-// This file is required to bring some types into scope, even though it
-// is not used.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type * as CodeMirrorModule from '../../third_party/codemirror/codemirror-legacy.js';
-
 import {AcornTokenizer, ECMA_VERSION} from './AcornTokenizer.js';
 import {CSSFormatter} from './CSSFormatter.js';
 import {FormattedContentBuilder} from './FormattedContentBuilder.js';
-import {FormattableMediaTypes, type FormatResult} from './FormatterActions.js';
+import {type FormatResult, FormattableMediaTypes} from './FormatterActions.js';
 import {HTMLFormatter} from './HTMLFormatter.js';
 import {IdentityFormatter} from './IdentityFormatter.js';
 import {JavaScriptFormatter} from './JavaScriptFormatter.js';
