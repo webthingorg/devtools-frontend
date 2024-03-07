43455dae9bcc0a5a382d29a25b5e0af7f8ddd63a
Eric Leese <leese@chromium.org>
2024-03-07 17:20:42 +0100

    Initial UI for console error message search
    Adds a button to console insights results to do a web search
    for the error message instead.
    
    Bug: b:323302399
    Change-Id: I87ff17d61bb58c30609a57501a07cc0de6d862b1

--- front_end/panels/explain/components/ConsoleInsight.ts
+++ front_end/panels/explain/components/ConsoleInsight.ts
@@ -133,7 +133,7 @@ export class CloseEvent extends Event {
   }
 }
 
-type PublicPromptBuilder = Pick<PromptBuilder, 'buildPrompt'>;
+type PublicPromptBuilder = Pick<PromptBuilder, 'buildPrompt'|'getSearchQuery'>;
 type PublicAidaClient = Pick<Host.AidaClient.AidaClient, 'fetch'>;
 
 function localizeType(sourceType: SourceType): string {
@@ -384,6 +384,11 @@ export class ConsoleInsight extends HTMLElement {
     this.#openFeedbackFrom();
   }
 
+  #onSearch(): void {
+    const query = this.#promptBuilder.getSearchQuery();
+    Host.InspectorFrontendHost.InspectorFrontendHostInstance.openSearchResultsInNewTab(query);
+  }
+
   async #onConsentReminderConfirmed(): Promise<void> {
     this.#transitionTo({
       type: State.LOADING,
@@ -578,6 +583,21 @@ export class ConsoleInsight extends HTMLElement {
     // clang-format on
   }
 
+  #renderSearchButton(): LitHtml.TemplateResult {
+    // clang-format off
+    return html`<${Buttons.Button.Button.litTagName}
+      @click=${this.#onSearch}
+      class="search-button"
+      .data=${
+        {
+          variant: Buttons.Button.Variant.SECONDARY,
+        } as Buttons.Button.ButtonData
+      }
+    >
+      Search this error instead
+    </${Buttons.Button.Button.litTagName}>`;
+  }
+
   #renderLearnMoreAboutInsights(): LitHtml.TemplateResult {
     // clang-format off
     return html`<x-link href=${DOGFOODINFO_URL} class="link">Learn more about Console insights</x-link>`;
@@ -616,6 +636,9 @@ export class ConsoleInsight extends HTMLElement {
             <${ConsoleInsightSourcesList.litTagName} .sources=${this.#state.sources}>
             </${ConsoleInsightSourcesList.litTagName}>
           </details>
+          <div class="buttons">
+            ${this.#renderSearchButton()}
+          </div>
         </main>`;
       case State.ERROR:
         return html`
