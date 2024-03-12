a6847fb05569e83156ff2dee1def34ff911650f2
Alina Varkki <alinavarkki@google.com>
2024-03-12 12:20:31 +0000

    add field for annotation
    Change-Id: I9cbf26549ed8b870daa9170609850526667d4a1d

---
+++ front_end/services/annotations_manager/AnnotationsManager.ts
@@ -0,0 +1,52 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Types from '../../models/trace/types/types.js';
+
+let instance: AnnotationsManager|null = null;
+
+export interface Annotations {
+  hiddenRendererEventsIndexes: number[];
+  hiddenProfileCallsSampleIndexes: number[];
+  hiddenProfileCallsDepths: number[];
+}
+export class AnnotationsManager {
+  #allTraceEntries: Types.TraceEvents.SyntheticTraceEntry[] = [];
+  #hiddenEntries: Types.TraceEvents.TraceEventData[] = [];
+
+  static instance(opts: {
+    forceNew: boolean|null,
+  } = {forceNew: null}): AnnotationsManager {
+    const forceNew = Boolean(opts.forceNew);
+    if (!instance || forceNew) {
+      instance = new AnnotationsManager();
+    }
+    return instance;
+  }
+  private constructor() {
+  }
+
+  setSyntheticEntriesUnsortedArray(entries: Types.TraceEvents.SyntheticTraceEntry[]): void {
+    this.#allTraceEntries = entries;
+  }
+
+  getAnnotations(): Annotations {
+    const indexesOfSynteticEntries = [];
+    for (const entry of this.#hiddenEntries) {
+      if (!Types.TraceEvents.isProfileCall(entry)) {
+        indexesOfSynteticEntries.push(this.#allTraceEntries.indexOf(entry));
+      }
+    }
+
+    return {
+      hiddenRendererEventsIndexes: indexesOfSynteticEntries,
+      hiddenProfileCallsSampleIndexes: [],
+      hiddenProfileCallsDepths: [],
+    };
+  }
+
+  setHiddenEntries(hiddenEntries: Types.TraceEvents.TraceEventData[]): void {
+    this.#hiddenEntries = hiddenEntries;
+  }
+}
