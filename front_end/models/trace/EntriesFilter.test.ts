5666435a57533e6a59610b0cae408b34c695c72e
Alina Varkki <alinavarkki@google.com>
2024-03-14 09:45:41 +0000

    [RPP] Make Entries Filter a Singleton
    Let's make Entries Filter a Singleton because we will only ever need 1 instance since every entry has unique Synthetic IDs and EntriesFilter is manipulating Synthetic Ids to keep track of hidden entries.
    
    We are doing this because this allows us to delete multiple function call chains and we will need to create longer ones to start using EntriesFilter in saving annotations.
    Additionally, this allows to access the EntriesFilter from TimelineUiUtils and add "event hidden" description to the event link.
    
    Bug: 1469887
    Change-Id: Iedda089de58f13237d869023676bf52bb3ac0f7d

--- front_end/models/trace/EntriesFilter.test.ts
+++ front_end/models/trace/EntriesFilter.test.ts
@@ -40,7 +40,7 @@ function findFirstEntry(
 describe('EntriesFilter', function() {
   it('parses a stack and returns an empty list of invisible entries', async function() {
     const data = await TraceLoader.traceEngine(this, 'basic-stack.json.gz');
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     assert.deepEqual([], stack.invisibleEntries());
   });
 
@@ -75,7 +75,7 @@ describe('EntriesFilter', function() {
       return TraceEngine.Types.TraceEvents.isProfileCall(entry) && entry.callFrame.functionName === 'basicTwo' &&
           entry.dur === 827;
     });
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     stack.applyFilterAction({type: TraceEngine.EntriesFilter.FilterAction.MERGE_FUNCTION, entry: entryTwo});
     assert.isTrue(stack.invisibleEntries().includes(entryTwo), 'entryTwo is invisble');
     // Only one entry - the one for the `basicTwo` function - should have been hidden.
@@ -111,7 +111,7 @@ describe('EntriesFilter', function() {
       return TraceEngine.Types.TraceEvents.isProfileCall(entry) && entry.callFrame.functionName === 'basicTwo' &&
           entry.dur === 827;
     });
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     stack.applyFilterAction({type: TraceEngine.EntriesFilter.FilterAction.MERGE_FUNCTION, entry: entryTwo});
     assert.isTrue(stack.invisibleEntries().includes(entryTwo), 'entryTwo is invisble');
 
@@ -150,7 +150,7 @@ describe('EntriesFilter', function() {
       return TraceEngine.Types.TraceEvents.isProfileCall(entry) && entry.callFrame.functionName === 'basicTwo' &&
           entry.dur === 827;
     });
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     stack.applyFilterAction({type: TraceEngine.EntriesFilter.FilterAction.COLLAPSE_FUNCTION, entry: entryTwo});
     // basicTwo is marked as modified.
     assert.isTrue(stack.isEntryModified(entryTwo));
@@ -201,8 +201,7 @@ describe('EntriesFilter', function() {
          const {endTime} = TraceEngine.Helpers.Timing.eventTimingsMicroSeconds(entry);
          return endTime <= firstFooCallEndTime;
        });
-       const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
-
+       const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
        // Collapse all foo calls after the first one.
        stack.applyFilterAction({
          type: TraceEngine.EntriesFilter.FilterAction.COLLAPSE_REPEATING_DESCENDANTS,
@@ -278,7 +277,7 @@ describe('EntriesFilter', function() {
       const basicTwoCallEndTime = TraceEngine.Helpers.Timing.eventTimingsMicroSeconds(basicTwoCallEntry).endTime;
       return endTime <= basicTwoCallEndTime;
     });
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     stack.applyFilterAction({type: TraceEngine.EntriesFilter.FilterAction.COLLAPSE_FUNCTION, entry: basicTwoCallEntry});
 
     // We collapsed at the `basicTwo` entry - so it should not be included in the invisible list itself.
@@ -338,7 +337,7 @@ describe('EntriesFilter', function() {
       return endTime <= firstFooCallEndTime;
     });
 
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     stack.applyFilterAction(
         {type: TraceEngine.EntriesFilter.FilterAction.COLLAPSE_REPEATING_DESCENDANTS, entry: firstFooCallEntry});
 
@@ -394,7 +393,7 @@ describe('EntriesFilter', function() {
      * Applying 'undo all actions' should bring the stack to the original state.
      **/
 
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     const basicTwoCallEntry = findFirstEntry(mainThread.entries, entry => {
       // Processing this trace ends up with two distinct stacks for basicTwo()
       // So we find the first one so we can focus this test on just one stack.
@@ -485,7 +484,7 @@ describe('EntriesFilter', function() {
      * This should result in all basicTwo children being removed from the invisible array and stack being in the initial state.
      **/
 
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     const basicTwoCallEntry = findFirstEntry(mainThread.entries, entry => {
       // Processing this trace ends up with two distinct stacks for basicTwo()
       // So we find the first one so we can focus this test on just one stack.
@@ -575,7 +574,7 @@ describe('EntriesFilter', function() {
       return endTime <= firstFooCallEndTime;
     });
 
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     stack.applyFilterAction(
         {type: TraceEngine.EntriesFilter.FilterAction.COLLAPSE_REPEATING_DESCENDANTS, entry: firstFooCallEntry});
 
@@ -647,8 +646,7 @@ describe('EntriesFilter', function() {
           entry.dur === 233;
     });
 
-    const stack = new TraceEngine.EntriesFilter.EntriesFilter(data.Renderer.entryToNode);
-
+    const stack = TraceEngine.EntriesFilter.EntriesFilter.instance({entryToNodeMap: data.Renderer.entryToNode});
     // Before applying any action on a node, there should be no entries hidden under it
     assert.strictEqual(stack.findHiddenDescendantsAmount(firstFooCallEntry), 0);
 
