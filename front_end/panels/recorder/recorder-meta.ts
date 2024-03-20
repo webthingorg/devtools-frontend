a9386a6df662524c11932d18277b4786fe5bd03e
Alex Rudenko <alexrudenko@chromium.org>
2024-03-20 13:27:52 +0100

    Move Recorder out of the preview status
    Fixed: 329271496
    Change-Id: I9d71019f85c00ead50a443e7b9a399f0abe0f42f

--- front_end/panels/recorder/recorder-meta.ts
+++ front_end/panels/recorder/recorder-meta.ts
@@ -69,7 +69,7 @@ function maybeRetrieveContextTypes<T = unknown>(
   return [];
 }
 
-(UI.ViewManager.defaultOptionsForTabs as {[key: string]: boolean}).chrome_recorder = true;
+(UI.ViewManager.defaultOptionsForTabs as {[key: string]: boolean})['chrome-recorder'] = true;
 
 UI.ViewManager.registerViewExtension({
   location: UI.ViewManager.ViewLocationValues.PANEL,
@@ -78,7 +78,6 @@ UI.ViewManager.registerViewExtension({
   title: i18nLazyString(UIStrings.recorder),
   order: 90,
   persistence: UI.ViewManager.ViewPersistence.CLOSEABLE,
-  isPreviewFeature: true,
   async loadView() {
     const Recorder = await loadRecorderModule();
     return Recorder.RecorderPanel.RecorderPanel.instance();
