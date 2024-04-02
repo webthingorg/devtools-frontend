4b389af849899ecfdfc66c57239ff91c9388e3da
Danil Somsikov <dsv@chromium.org>
2024-04-02 13:19:00 +0000

    Fix kebabization logic
    Bug: 331931429
    Change-Id: I43a9714aedd1d874055a6b2c032a4ef84ff9e364

--- front_end/core/platform/StringUtilities.ts
+++ front_end/core/platform/StringUtilities.ts
@@ -497,7 +497,7 @@ export const toLowerCaseString = function(input: string): LowerCaseString {
   return input.toLowerCase() as LowerCaseString;
 };
 
-const WORD = /[A-Z]{2,}(?=[A-Z0-9][a-z0-9]+|\b|_)|[A-Za-z][0-9]+[a-z]|[A-Z]?[a-z]+|[0-9][A-Za-z]+|[A-Z]|[0-9]+|[.]/g;
+const WORD = /[A-Z]{2,}(?=[A-Z0-9][a-z0-9]+|\b|_)|[A-Za-z][0-9]+[a-z]?|[A-Z]?[a-z]+|[0-9][A-Za-z]+|[A-Z]|[0-9]+|[.]/g;
 //            <---1---><------------2-----------> <--------3--------> <-----4----> <------5-----> <-----6----> <7>
 // 1: two or more consecutive uppercase letters. This is useful for identifying acronyms
 // 2: lookahead assertion that matches a word boundary
