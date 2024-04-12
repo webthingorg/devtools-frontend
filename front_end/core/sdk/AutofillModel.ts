d8bad32e3a5a468b097aa90012ea198b3d1da48a
Bruno Braga <brunobraga@google.com>
2024-04-12 13:31:14 +0000

    [Autofill in devtools] Introduce first 2 well formatted addresses
    https://screencast.googleplex.com/cast/NTk2MDQxOTE2NzQzNjgwMHw0NjU4YTA0Ny0xMQ
    
    Bug: b/40270486
    Change-Id: If97a39d00606fc5bb837f470a267f99a32e16bac

--- front_end/core/sdk/AutofillModel.ts
+++ front_end/core/sdk/AutofillModel.ts
@@ -26,6 +26,55 @@ export class AutofillModel extends SDKModel<EventTypes> implements ProtocolProxy
       return;
     }
     void this.agent.invoke_enable();
+    void (async () => {
+      void console.error(JSON.stringify(await this.agent.invoke_setAddresses({
+        addresses: [
+          {
+            fields: [
+              {name: 'ADDRESS_HOME_COUNTRY', value: 'US'},
+              {name: 'NAME_FULL', value: 'Jon Stewart Doe'},
+              {name: 'NAME_FIRST', value: 'Jon'},
+              {name: 'NAME_MIDDLE', value: 'Stewart'},
+              {name: 'NAME_LAST', value: 'Doe'},
+              {name: 'COMPANY_NAME', value: 'Google'},
+              {name: 'ADDRESS_HOME_STREET_ADDRESS', value: '1600 Amphitheatre Parkway\nApartment 1'},
+              {name: 'ADDRESS_HOME_STREET_NAME', value: 'Amphitheatre Parkway'},
+              {name: 'ADDRESS_HOME_HOUSE_NUMBER', value: '1600'},
+              {name: 'ADDRESS_HOME_SUBPREMISE', value: '1'},
+              {name: 'ADDRESS_HOME_APT_NUM', value: '1'},
+              {name: 'ADDRESS_HOME_ZIP', value: '94043'},
+              {name: 'ADDRESS_HOME_CITY', value: 'Mountain View'},
+              {name: 'ADDRESS_HOME_STATE', value: 'CA'},
+              {name: 'EMAIL_ADDRESS', value: 'test@example.us'},
+              {name: 'PHONE_HOME_WHOLE_NUMBER', value: '601-952-1325'},
+            ],
+          },
+          {
+            fields: [
+              {name: 'ADDRESS_HOME_COUNTRY', value: 'BR'},
+              {name: 'NAME_FULL', value: 'João Souza Silva'},
+              {name: 'NAME_FIRST', value: 'João'},
+              {name: 'NAME_MIDDLE', value: 'Souza'},
+              {name: 'NAME_LAST', value: 'Silva'},
+              {name: 'COMPANY_NAME', value: 'Google'},
+              {name: 'ADDRESS_HOME_STREET_ADDRESS', value: 'Av. dos Andradas, 3000, Apartamento 1\nSanta Efigênia'},
+              {name: 'ADDRESS_HOME_STREET_NAME', value: 'Av. dos Andradas, 3000'},
+              {name: 'ADDRESS_HOME_HOUSE_NUMBER', value: '3000'},
+              {name: 'ADDRESS_HOME_SUBPREMISE', value: '1'},
+              {name: 'ADDRESS_HOME_APT_NUM', value: '1'},
+              {name: 'ADDRESS_HOME_DEPENDENT_LOCALITY', value: 'Santa Efigênia'},
+              {name: 'ADDRESS_HOME_LANDMARK', value: 'Estação Santa Efigênia'},
+              {name: 'ADDRESS_HOME_OVERFLOW', value: 'Próximo à estação'},
+              {name: 'ADDRESS_HOME_ZIP', value: '30260-070'},
+              {name: 'ADDRESS_HOME_CITY', value: 'Belo Horizonte'},
+              {name: 'ADDRESS_HOME_STATE', value: 'MG'},
+              {name: 'EMAIL_ADDRESS', value: 'teste@exemplo.us'},
+              {name: 'PHONE_HOME_WHOLE_NUMBER', value: '31 2128-6800'},
+            ],
+          },
+        ],
+      })));
+    })();
     this.#enabled = true;
   }
 
