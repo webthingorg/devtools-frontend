b16382c406d504813b92dafff5e05b0266583235
Jecelyn Yeen <jec@google.com>
2024-04-18 20:39:34 +0200

    [Overrides] Auto split header name and value on paste in Network > Headers tab
    Detect the first ":" in the pasted content, and fill the override header name and value.
    
    For example, if users copy & paste "permissions-policy: unload=(https://xyz.com)" into the header name column, we will automatically split the value to:
    
    - header name: Permissions-Policy
    - header value: unload=(https://xyz.com)
    
    See it in action: https://crbug.com/330967147#comment3
    
    Bug: 330967147
    Change-Id: I6cadee956cb55be19298caf9b53efa3ff3dd2498

--- front_end/panels/network/components/HeaderSectionRow.test.ts
+++ front_end/panels/network/components/HeaderSectionRow.test.ts
@@ -460,6 +460,44 @@ describeWithEnvironment('HeaderSectionRow', () => {
     assert.isTrue(hasReloadPrompt(component.shadowRoot));
   });
 
+  it('split header name and value on pasted content', async () => {
+    const originalHeaderName = Platform.StringUtilities.toLowerCaseString('some-header-name');
+    const originalHeaderValue = 'someHeaderValue';
+    const headerData: NetworkComponents.HeaderSectionRow.HeaderDescriptor = {
+      name: originalHeaderName,
+      value: originalHeaderValue,
+      nameEditable: true,
+      valueEditable: true,
+    };
+    const editedHeaderName = 'permissions-Policy: unload=(https://xyz.com)';
+
+    const {component, nameEditable} = await renderHeaderSectionRow(headerData);
+    assert.isNotNull(component.shadowRoot);
+    assert.instanceOf(nameEditable, HTMLElement);
+
+    let headerValueFromEvent = '';
+    let headerNameFromEvent = '';
+    let headerEditedEventCount = 0;
+
+    component.addEventListener('headeredited', event => {
+      headerValueFromEvent = event.headerValue;
+      headerNameFromEvent = event.headerName;
+      headerEditedEventCount++;
+    });
+
+    const dt = new DataTransfer();
+    dt.setData('text/plain', editedHeaderName);
+
+    nameEditable.focus();
+    dispatchPasteEvent(nameEditable, {clipboardData: dt, bubbles: true, composed: true});
+    nameEditable.blur();
+
+    await coordinator.done();
+    assert.strictEqual(headerEditedEventCount, 2);
+    assert.strictEqual(headerNameFromEvent, 'permissions-policy');
+    assert.strictEqual(headerValueFromEvent, 'unload=(https://xyz.com)');
+  });
+
   it('recoginzes only alphanumeric characters, dashes, and underscores as valid in header names', () => {
     assert.strictEqual(NetworkComponents.HeaderSectionRow.isValidHeaderName('AlphaNumeric123'), true);
     assert.strictEqual(NetworkComponents.HeaderSectionRow.isValidHeaderName('Alpha Numeric'), false);
