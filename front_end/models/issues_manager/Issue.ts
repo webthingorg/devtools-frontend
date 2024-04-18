3a3daf8d93d598f072005b3d2a0d0ba269b6e32a
Shuran Huang <shuuran@chromium.org>
2024-04-18 14:07:11 +0000

    Add message to console when 3PCD issue gets created
    Bug: 328691367
    Change-Id: I53d3505a87f785c95c9a42d84e4b1eef1dc60358

--- front_end/models/issues_manager/Issue.ts
+++ front_end/models/issues_manager/Issue.ts
@@ -6,6 +6,7 @@ import * as Common from '../../core/common/common.js';
 import * as Host from '../../core/host/host.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import type * as Platform from '../../core/platform/platform.js';
+import {type ConsoleMessage} from '../../core/sdk/ConsoleModel.js';
 import type * as SDK from '../../core/sdk/sdk.js';
 import type * as Protocol from '../../generated/protocol.js';
 
@@ -207,6 +208,10 @@ export abstract class Issue<IssueCode extends string = string> {
 
   setHidden(hidden: boolean): void {
     this.#hidden = hidden;
+  }
+
+  maybeCreateConsoleMessage(): ConsoleMessage|undefined {
+    return;
   }
 }
 
