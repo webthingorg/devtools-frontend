d2a084349b69e4fb3e33a38286b5b9a0cd8ff341
Ergun Erdogmus <ergunsh@chromium.org>
2024-04-29 13:27:20 +0200

    [VisualLogging] Pass jslogContext to TabbedPane for specific tabs
    See the comment in: b/330717103#comment2
    
    Fixed: 330717103
    Change-Id: If6b4ff54015600fc6a4b9156b11c3795a8376fed

--- front_end/ui/legacy/TabbedPane.ts
+++ front_end/ui/legacy/TabbedPane.ts
@@ -228,14 +228,15 @@ export class TabbedPane extends Common.ObjectWrapper.eventMixin<EventTypes, type
 
   appendTab(
       id: string, tabTitle: string, view: Widget, tabTooltip?: string, userGesture?: boolean, isCloseable?: boolean,
-      isPreviewFeature?: boolean, index?: number): void {
+      isPreviewFeature?: boolean, index?: number, jslogContext?: string): void {
     const closeable = typeof isCloseable === 'boolean' ? isCloseable : Boolean(this.closeableTabs);
-    const tab = new TabbedPaneTab(this, id, tabTitle, closeable, Boolean(isPreviewFeature), view, tabTooltip);
+    const tab =
+        new TabbedPaneTab(this, id, tabTitle, closeable, Boolean(isPreviewFeature), view, tabTooltip, jslogContext);
     tab.setDelegate((this.delegate as TabbedPaneTabDelegate));
     console.assert(!this.tabsById.has(id), `Tabbed pane already contains a tab with id '${id}'`);
     this.tabsById.set(id, tab);
     tab.tabElement.tabIndex = -1;
-    const context = id === 'console-view' ? 'console' : id;
+    const context = jslogContext ?? (id === 'console-view' ? 'console' : id);
     tab.tabElement.setAttribute(
         'jslog', `${VisualLogging.panelTabHeader().track({click: true, drag: true}).context(context)}`);
     if (index !== undefined) {
@@ -637,10 +638,11 @@ export class TabbedPane extends Common.ObjectWrapper.eventMixin<EventTypes, type
       }
       if (this.numberOfTabsShown() === 0 && this.tabsHistory[0] === tab) {
         menu.defaultSection().appendCheckboxItem(
-            tab.title, this.dropDownMenuItemSelected.bind(this, tab), {checked: true, jslogContext: tab.id});
+            tab.title, this.dropDownMenuItemSelected.bind(this, tab),
+            {checked: true, jslogContext: tab.jslogContextOrId});
       } else {
         menu.defaultSection().appendItem(
-            tab.title, this.dropDownMenuItemSelected.bind(this, tab), {jslogContext: tab.id});
+            tab.title, this.dropDownMenuItemSelected.bind(this, tab), {jslogContext: tab.jslogContextOrId});
       }
     }
     void menu.show().then(() => ARIAUtils.setExpanded(this.dropDownButton, menu.isHostedMenuOpen()));
@@ -999,9 +1001,10 @@ export class TabbedPaneTab {
   private delegate?: TabbedPaneTabDelegate;
   private titleElement?: HTMLElement;
   private dragStartX?: number;
+  private jslogContextInternal?: string;
   constructor(
       tabbedPane: TabbedPane, id: string, title: string, closeable: boolean, previewFeature: boolean, view: Widget,
-      tooltip?: string) {
+      tooltip?: string, jslogContext?: string) {
     this.closeable = closeable;
     this.previewFeature = previewFeature;
     this.tabbedPane = tabbedPane;
@@ -1010,6 +1013,7 @@ export class TabbedPaneTab {
     this.tooltipInternal = tooltip;
     this.viewInternal = view;
     this.shown = false;
+    this.jslogContextInternal = jslogContext;
   }
 
   get id(): string {
@@ -1032,6 +1036,10 @@ export class TabbedPaneTab {
       closeIconContainer?.setAttribute('aria-label', i18nString(UIStrings.closeS, {PH1: title}));
     }
     delete this.measuredWidth;
+  }
+
+  get jslogContextOrId(): string {
+    return this.jslogContextInternal ?? this.id;
   }
 
   isCloseable(): boolean {
