3cdcdf13b5d0684b4ba8b472982f1e9008172624
Anton Maliev <amaliev@chromium.org>
2024-05-21 18:40:57 +0000

    Extend cookie deprecation metadata issue to include opt-out percentages, if present.
    Sample screenshots:
    With 1P opt-out: https://screenshot.googleplex.com/8DfCEkp2WC6gJPU
    With 3P opt-out: https://screenshot.googleplex.com/XEZnoDPNHuvk2BE
    
    Also include cookie operation to determine whether to show the Read or Set description.
    
    Bug: b/341051114, b/341794619
    Change-Id: Id19b50a22642d6dcfad413b9f1db6849cc822935

--- front_end/panels/issues/AffectedMetadataAllowedSitesView.ts
+++ front_end/panels/issues/AffectedMetadataAllowedSitesView.ts
@@ -27,14 +27,24 @@ export class AffectedMetadataAllowedSitesView extends AffectedResourcesView {
 
   override update(): void {
     this.clear();
-    const allowedSites = this.issue.getMetadataAllowedSites();
+    const issues = this.issue.getCookieDeprecationMetadataIssues();
     let count = 0;
 
-    for (const site of allowedSites) {
+    for (const issueData of issues) {
       const row = document.createElement('tr');
       row.classList.add('affected-resource-directive');
 
-      this.appendIssueDetailCell(row, site);
+      let text: string;
+      text = issueData.details().allowedSites.join(', ');
+      if (!issueData.details().isOptOutTopLevel && issueData.details().optOutPercentage > 0) {
+        text += ' (opt-out: ' + issueData.details().optOutPercentage +
+            '%  - <a href="https://github.com/amaliev/3pcd-exemption-heuristics/blob/main/explainer.md">learn more</a>)';
+      }
+
+      // appendIssueDetailCell sets the text as a Node.textContent, which doesn't parse the HTML link.
+      // Adding the text in the innerHTML field instead.
+      const cell = this.appendIssueDetailCell(row, '');
+      cell.innerHTML = text;
       this.affectedResources.appendChild(row);
       count++;
     }
