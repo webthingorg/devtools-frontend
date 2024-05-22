2b4919bbdb8c0c3f0fe736848aabbca809e352db
Christy Chen <chrche@microsoft.com>
2024-05-22 13:44:45 -0700

    Consume websocket send and receive event
    Bug: 331351979
    Change-Id: Iee60f70d36c9e1e89080db44aa91da3d50fa95ce

---
+++ front_end/models/trace/handlers/WebSocketsHandler.ts
@@ -0,0 +1,74 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Types from '../types/types.js';
+
+import {HandlerState} from './types.js';
+
+let handlerState = HandlerState.UNINITIALIZED;
+
+export interface WebSocketTraceDataForFrame {
+  frame: string;
+  webSocketIdentifier: number;
+  events: Types.TraceEvents.WebSocketEvent[];
+}
+export interface WebSocketTraceDataForWorker {
+  workerId: string;
+  webSocketIdentifier: number;
+  events: Types.TraceEvents.WebSocketEvent[];
+}
+export type WebSocketTraceData = WebSocketTraceDataForFrame|WebSocketTraceDataForWorker;
+export interface WebSocketsData {
+  traceData: WebSocketTraceData[];
+}
+
+const webSocketData: Map<number, WebSocketTraceData> = new Map();
+export function reset(): void {
+  handlerState = HandlerState.INITIALIZED;
+}
+
+export function handleEvent(event: Types.TraceEvents.TraceEventData): void {
+  if (handlerState !== HandlerState.INITIALIZED) {
+    throw new Error('WebSockets Handler is not initialized');
+  }
+
+  if (Types.TraceEvents.isTraceEventWebSocketCreate(event) || Types.TraceEvents.isTraceEventWebSocketInfo(event) ||
+      Types.TraceEvents.isTraceEventWebSocketTransfer(event)) {
+    const identifier = event.args.data.identifier;
+    if (!webSocketData.has(identifier)) {
+      if (event.args.data.frame) {
+        webSocketData.set(identifier, {
+          frame: event.args.data.frame,
+          webSocketIdentifier: identifier,
+          events: [],
+        });
+      } else if (event.args.data.workerId) {
+        webSocketData.set(identifier, {
+          workerId: event.args.data.workerId,
+          webSocketIdentifier: identifier,
+          events: [],
+        });
+      }
+    }
+
+    webSocketData.get(identifier)?.events.push(event);
+  }
+}
+
+export async function finalize(): Promise<void> {
+  if (handlerState !== HandlerState.INITIALIZED) {
+    throw new Error('WebSockets handler is not initialized');
+  }
+  handlerState = HandlerState.FINALIZED;
+}
+
+export function data(): WebSocketsData {
+  if (handlerState !== HandlerState.FINALIZED) {
+    throw new Error('WebSockets handler is not finalized');
+  }
+
+  return {
+    traceData: [...webSocketData.values()],
+  };
+}
