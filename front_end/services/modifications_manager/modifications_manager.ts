44d05b1af2f1d995a0e94fb19a2c8603e57ffbd2
Andrés Olivares <andoli@chromium.org>
2024-05-24 09:38:37 +0000

    Rename AnnotationsManager to ModificationsManager
    Bug: 339163152
    Change-Id: Ica625fba074117552a9c764580d0ee52913cd6db

--- front_end/services/modifications_manager/modifications_manager.ts
+++ front_end/services/modifications_manager/modifications_manager.ts
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as AnnotationsManager from './AnnotationsManager.js';
+import * as ModificationsManager from './ModificationsManager.js';
 
 export {
-  AnnotationsManager,
+  ModificationsManager,
 };
