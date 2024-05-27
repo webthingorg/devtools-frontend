dfa699acd9e97d38018f93b9379744bc5904fe8a
Kateryna Prokopenko <kprokopenko@chromium.org>
2024-05-27 14:48:24 +0000

    [GM3Restyling] Update adorners to look like outlined buttons
    Bug: 325441197
    Change-Id: I0f462e6b24e344719bb0c04a394582e0b5d9f8ba

--- front_end/panels/elements/ElementsTreeOutline.ts
+++ front_end/panels/elements/ElementsTreeOutline.ts
@@ -1781,7 +1781,7 @@ export class ShortcutTreeElement extends UI.TreeOutline.TreeElement {
     const name = config.name;
     const adornerContent = document.createElement('span');
     const linkIcon = new IconButton.Icon.Icon();
-    linkIcon.data = {iconName: 'select-element', color: 'var(--icon-default)', width: '14px', height: '14px'};
+    linkIcon.data = {iconName: 'select-element', color: 'var(--icon-primary)', width: '12px', height: '12px'};
     const slotText = document.createElement('span');
     slotText.textContent = name;
     adornerContent.append(linkIcon);
