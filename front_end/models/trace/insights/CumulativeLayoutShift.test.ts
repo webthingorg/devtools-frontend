aa631e33d5d9ebfe08f04c90247f46964c760bfd
Adriana Ixba <aixba@google.com>
2024-05-28 23:24:36 +0000

    [RPP] Add iframe and font root causes to CLS insight
    For iframes, this uses an iframe creation event. For fonts,
    the font requests. It considers them root causes if they fall
    within an invalidation window of 500ms before a shift.
    
    Bug:331429158
    Change-Id: I02d46e54f05ebc0868a6c7a795ac9772fc4afada

--- front_end/models/trace/insights/CumulativeLayoutShift.test.ts
+++ front_end/models/trace/insights/CumulativeLayoutShift.test.ts
@@ -3,7 +3,9 @@
 // found in the LICENSE file.
 
 import {TraceLoader} from '../../../testing/TraceLoader.js';
+import * as Helpers from '../helpers/helpers.js';
 import * as TraceModel from '../trace.js';
+import * as Types from '../types/types.js';
 
 import {InsightRunners} from './insights.js';
 
@@ -40,6 +42,78 @@ describe('CumulativeLayoutShift', function() {
       const {animationFailures} =
           TraceModel.Insights.InsightRunners.CumulativeLayoutShift.generateInsight(data, context);
       assert.isEmpty(animationFailures);
+    });
+  });
+  describe('layout shifts', function() {
+    const invalidationWindow = Helpers.Timing.secondsToMicroseconds(Types.Timing.Seconds(0.5));
+
+    it('returns correct layout shifts', async function() {
+      const data = await TraceLoader.traceEngine(this, 'cls-single-frame.json.gz');
+      const context = {
+        frameId: data.Meta.mainFrameId,
+        navigationId: data.Meta.navigationsByNavigationId.keys().next().value,
+      };
+      const {shifts} = TraceModel.Insights.InsightRunners.CumulativeLayoutShift.generateInsight(data, context);
+      assert.exists(shifts);
+      assert.strictEqual(shifts.size, 7);
+    });
+
+    describe('root causes', function() {
+      it('handles potential iframe root cause correctly', async function() {
+        // Trace has a single iframe that gets created before the first layout shift and causes a layout shift.
+        const data = await TraceLoader.traceEngine(this, 'iframe-shift.json.gz');
+        const context = {
+          frameId: data.Meta.mainFrameId,
+          navigationId: data.Meta.navigationsByNavigationId.keys().next().value,
+        };
+        const {shifts} = TraceModel.Insights.InsightRunners.CumulativeLayoutShift.generateInsight(data, context);
+        assert.exists(shifts);
+        assert.strictEqual(shifts.size, 3);
+
+        const shift1 = Array.from(shifts)[0][0];
+        const shiftIframes = shifts.get(shift1)?.iframes;
+        assert.exists(shiftIframes);
+        assert.strictEqual(shiftIframes.length, 1);
+
+        const iframe = shiftIframes[0];
+        const iframeEndTime = iframe.dur ? iframe.ts + iframe.dur : iframe.ts;
+        // Ensure the iframe happens within the invalidation window.
+        assert.isTrue(iframeEndTime < shift1.ts && iframeEndTime >= shift1.ts - invalidationWindow);
+
+        // Other shifts should not have iframe root causes.
+        const shift2 = Array.from(shifts)[1][0];
+        assert.isEmpty(shifts.get(shift2)?.iframes);
+        const shift3 = Array.from(shifts)[2][0];
+        assert.isEmpty(shifts.get(shift3)?.iframes);
+      });
+
+      it('handles potential font root cause correctly', async function() {
+        // Trace has font load before the second layout shift.
+        const data = await TraceLoader.traceEngine(this, 'iframe-shift.json.gz');
+        const context = {
+          frameId: data.Meta.mainFrameId,
+          navigationId: data.Meta.navigationsByNavigationId.keys().next().value,
+        };
+        const {shifts} = TraceModel.Insights.InsightRunners.CumulativeLayoutShift.generateInsight(data, context);
+        assert.exists(shifts);
+        assert.strictEqual(shifts.size, 3);
+
+        const shift2 = Array.from(shifts)[1][0];
+        const shiftFonts = shifts.get(shift2)?.fontRequests;
+        assert.exists(shiftFonts);
+        assert.strictEqual(shiftFonts.length, 1);
+
+        const fontRequest = shiftFonts[0];
+        const fontRequestEndTime = fontRequest.ts + fontRequest.dur;
+        // Ensure the font loads within the invalidation window.
+        assert.isTrue(fontRequestEndTime < shift2.ts && fontRequestEndTime >= shift2.ts - invalidationWindow);
+
+        // Other shifts should not have iframe root causes.
+        const shift1 = Array.from(shifts)[0][0];
+        assert.isEmpty(shifts.get(shift1)?.fontRequests);
+        const shift3 = Array.from(shifts)[2][0];
+        assert.isEmpty(shifts.get(shift3)?.fontRequests);
+      });
     });
   });
 });
