91a60c82dd9b9819f051119bec298fe3cbd6967a
Jayson Chen <jaysonchen@microsoft.com>
2024-05-30 16:24:30 -0700

    Export enhanced traces
    Design: https://docs.google.com/document/d/16AzW-gS4PgeOlLkNfDPPp6oYhFE65GCTYhTBjj7J56c/edit
    
    Add option in performance panel to export enhanced traces that includes targets, execution context and scripts information. The user can right click or ctrl-click "save performance trace" button to see the context menu and download the current trace as enhanced traces. The exported enhanced traces can be imported back safely as a normal trace before rehydrated session is implemented.
    
    Bug: 337909145
    Change-Id: I16d7948c436721d9ab16e662c3f672a6cdb24eae

---
+++ front_end/panels/timeline/EnhancedTraces.ts
@@ -0,0 +1,230 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as TraceEngine from '../../models/trace/trace.js';
+import {type TraceFile} from '../../models/trace/types/File.js';
+
+import {traceJsonGenerator} from './SaveFileFormatter.js';
+
+export class EnhancedTracesEngine {
+  traceEvents: readonly TraceEngine.Types.TraceEvents.TraceEventData[];
+  private scriptTov8Context: Map<string, string> = new Map<string, string>();
+  private v8ContextToExecutionContextId: Map<string, number> = new Map<string, number>();
+
+  constructor(traceEvents: readonly TraceEngine.Types.TraceEvents.TraceEventData[]) {
+    this.traceEvents = traceEvents;
+
+    // Construct mapping between script and v8 context with trace information.
+    this.traceEvents.forEach(traceEvent => {
+      if (traceEvent.cat === 'disabled-by-default-devtools.target-rundown') {
+        const data = traceEvent.args?.data;
+        if (!data) {
+          return;
+        }
+        if (data.isolate && data.scriptId && data.v8context) {
+          this.scriptTov8Context.set(this.getScriptIdentifier(data.isolate, data.scriptId), data.v8context);
+        }
+      }
+    });
+
+    // Construct mapping between v8 context and execution context id with trace information.
+    this.traceEvents.forEach(traceEvent => {
+      if (traceEvent.cat === 'disabled-by-default-devtools.v8-source-rundown') {
+        const data = traceEvent.args?.data;
+        if (!data) {
+          return;
+        }
+        if (data.isolate && data.scriptId && data.executionContextId) {
+          const v8Context = this.scriptTov8Context.get(this.getScriptIdentifier(data.isolate, data.scriptId));
+          if (v8Context) {
+            this.v8ContextToExecutionContextId.set(v8Context, data.executionContextId);
+          }
+        }
+      }
+    });
+  }
+
+  static generateEnhancedTraces(
+      targets: Target[], executionContexts: ExecutionContext[], scripts: Script[],
+      enhancedTraceMetaData: EnhancedTracesMetadata,
+      traceEvents: readonly TraceEngine.Types.TraceEvents.TraceEventData[],
+      metadata: TraceEngine.Types.File.MetaData|null): string {
+    const exportedTrace: EnhancedTraces = {
+      metaData: enhancedTraceMetaData,
+      targets: targets,
+      executionContexts: executionContexts,
+      scripts: scripts,
+      payload: null,
+    };
+    const exportedTraceString = JSON.stringify(exportedTrace);
+    const formattedTraceIter = traceJsonGenerator(traceEvents, metadata);
+    let traceAsString: string = Array.from(formattedTraceIter).join('');
+    // Remove "null }" and append the original performance trace
+    traceAsString = exportedTraceString.slice(0, exportedTraceString.length - 5) + traceAsString + '}';
+
+    return traceAsString;
+  }
+
+  captureTargetsFromTraces(): Target[] {
+    const targets: Target[] = [];
+    this.traceEvents.forEach(traceEvent => {
+      if (traceEvent.cat === 'disabled-by-default-devtools.target-rundown') {
+        const data = traceEvent.args?.data;
+        if (!data || !data.frame) {
+          return;
+        }
+
+        if (!targets.find(target => target.id === data.frame)) {
+          // New target found
+          targets.push({
+            id: data.frame,
+            type: data.frameType,
+            isolate: data.isolate,
+            pid: traceEvent.pid,
+            url: data.url,
+          });
+        }
+      }
+    });
+    return targets;
+  }
+
+  captureExecutionContextFromTraces(): ExecutionContext[] {
+    const executionContexts: ExecutionContext[] = [];
+    this.traceEvents.forEach(traceEvent => {
+      if (traceEvent.cat === 'disabled-by-default-devtools.target-rundown') {
+        const data = traceEvent.args?.data;
+        if (!data || !data.v8context) {
+          return;
+        }
+
+        if (!executionContexts.find(executionContext => executionContext.v8Context === data.v8context)) {
+          // New ExecutionContexts found
+          const executionContextId = this.v8ContextToExecutionContextId.get(data.v8context);
+          executionContexts.push({
+            id: executionContextId,
+            origin: data.origin,
+            v8Context: data.v8context,
+            auxData: {
+              frameId: data.frame,
+              isDefault: data.isDefault,
+              type: data.contextType,
+            },
+          });
+        }
+      }
+    });
+    return executionContexts;
+  }
+
+  captureScriptFromTraces(): Script[] {
+    const scripts: Script[] = [];
+    const scriptToSourceText: Map<string, string|undefined> = new Map<string, string>();
+    const scriptToSourceLength: Map<string, number|undefined> = new Map<string, number>();
+    this.traceEvents.forEach(traceEvent => {
+      if (traceEvent.cat === 'disabled-by-default-devtools.v8-source-rundown') {
+        const data = traceEvent.args?.data;
+        if (!data || !data.scriptId) {
+          return;
+        }
+
+        if (!scripts.find(script => script.scriptId === data.scriptId && script.isolate === data.isolate)) {
+          // New Script found
+          scripts.push({
+            scriptId: data.scriptId,
+            isolate: data.isolate,
+            executionContextId: data.executionContextId,
+            startLine: data.startLine,
+            startColumn: data.startColumn,
+            endLine: data.endLine,
+            endColumn: data.endColumn,
+            hash: data.hash,
+            isModule: data.isModule,
+            url: data.url,
+            hasSourceUrl: data.hasSourceUrl,
+            sourceMapUrl: data.sourceMapUrl,
+          });
+        }
+      } else if (traceEvent.cat === 'disabled-by-default-devtools.v8-source-rundown-sources') {
+        const data = traceEvent.args?.data;
+        if (!data || !data.scriptId || !data.isolate) {
+          return;
+        }
+        const scriptIdentifier = this.getScriptIdentifier(data.isolate, data.scriptId);
+        scriptToSourceText.set(scriptIdentifier, data.sourceText);
+        scriptToSourceLength.set(scriptIdentifier, data.length);
+      }
+    });
+
+    scripts.forEach(script => {
+      if (!script.isolate || !script.scriptId) {
+        return;
+      }
+      const scriptIdentifier = this.getScriptIdentifier(script.isolate, script.scriptId);
+      script.sourceText = scriptToSourceText.get(scriptIdentifier);
+      script.length = scriptToSourceLength.get(scriptIdentifier);
+    });
+    return scripts;
+  }
+
+  getEnhancedTracesMetaData(): EnhancedTracesMetadata {
+    return {
+      version: 'beta',
+    };
+  }
+
+  private getScriptIdentifier(isolate: string, scriptId: number): string {
+    return isolate + '@' + scriptId;
+  }
+}
+
+export interface EnhancedTracesMetadata {
+  version: string;
+}
+
+export interface Script {
+  scriptId?: number;
+  isolate?: string;
+  executionContextId?: number;
+  startLine?: number;
+  startColumn?: number;
+  endLine?: number;
+  endColumn?: number;
+  hash?: string;
+  isModule?: boolean;
+  url?: string;
+  hasSourceUrl?: boolean;
+  sourceMapUrl?: string;
+  length?: number;
+  sourceText?: string;
+}
+
+export interface ExecutionContextAuxData {
+  frameId?: string;
+  isDefault?: boolean;
+  type?: string;
+}
+
+export interface ExecutionContext {
+  id?: number;
+  origin?: string;
+  v8Context?: string;
+  auxData?: ExecutionContextAuxData;
+}
+
+export interface Target {
+  id?: string;
+  type?: string;
+  isolate?: string;
+  url?: string;
+  pid?: number;
+}
+
+export interface EnhancedTraces {
+  metaData: EnhancedTracesMetadata;
+  targets: Target[];
+  executionContexts: ExecutionContext[];
+  scripts: Script[];
+  payload: TraceFile|null;
+}
