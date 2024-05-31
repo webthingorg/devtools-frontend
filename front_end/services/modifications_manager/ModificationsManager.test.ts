7c41c70f74e442c1472590e6ad88f17020a026fe
Andrés Olivares <andoli@chromium.org>
2024-05-31 11:09:51 +0000

    [RPP] Serialize modifications using based on go/cpq:event-data-json
    Keys for synthetic events are created using the SyntheticEventsManager
    helper. For profile calls, given a key we efficiently look up and cache
    on demand to avoid storing a map containing all profile calls (since
    that can potentially grow significantly large).
    
    Modifications are now stored and loaded using this new method.
    
    Bug: 339163152
    Change-Id: I59fbaee40e282a1dfc69e634af796c983b0c2e10

--- front_end/services/modifications_manager/ModificationsManager.test.ts
+++ front_end/services/modifications_manager/ModificationsManager.test.ts
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import * as TraceEngine from '../../models/trace/trace.js';
-import * as TraceBounds from '../../services/trace_bounds/trace_bounds.js';
 import {TraceLoader} from '../../testing/TraceLoader.js';
 
 import * as ModificationsManager from './modifications_manager.js';
@@ -36,30 +35,77 @@ function findFirstEntry(
   return entry;
 }
 
-const baseTraceWindow: TraceEngine.Types.Timing.TraceWindowMicroSeconds = {
-  min: TraceEngine.Types.Timing.MicroSeconds(0),
-  max: TraceEngine.Types.Timing.MicroSeconds(10_000),
-  range: TraceEngine.Types.Timing.MicroSeconds(10_000),
-};
-
 describe('ModificationsManager', () => {
-  it('correctly generates an entry hash', async function() {
+  it('correctly implements a bidirectional key <-> event mapping', async function() {
     const data = await TraceLoader.traceEngine(null, 'basic-stack.json.gz');
-    const boundsManager =
-        TraceBounds.TraceBounds.BoundsManager.instance({forceNew: true}).resetWithNewBounds(baseTraceWindow);
-    const modificationsManager = ModificationsManager.ModificationsManager.ModificationsManager.maybeInstance(
-        {entryToNodeMap: data.Renderer.entryToNode, wholeTraceBounds: boundsManager.state()?.micro.entireTraceBounds});
+    const modificationsManager = ModificationsManager.ModificationsManager.ModificationsManager.activeManager();
     if (!modificationsManager) {
-      throw new Error('Manager does not exist.');
+      throw new Error('Modifications manager does not exist.');
     }
     const mainThread = getMainThread(data.Renderer);
     assert.exists(modificationsManager);
     // Find first 'Timer Fired' entry in the trace
-    const timerFireEntry = findFirstEntry(mainThread.entries, entry => {
+    const rawEntry = findFirstEntry(mainThread.entries, entry => {
       return entry.name === 'TimerFire';
     });
 
-    const entryHash = modificationsManager.getEntryIndex(timerFireEntry);
-    assert.strictEqual(3649, entryHash);
+    const syntheticEvent = data.NetworkRequests.byTime[0];
+    const profileCall = findFirstEntry(mainThread.entries, entry => TraceEngine.Types.TraceEvents.isProfileCall(entry));
+    const rawEntryKey = modificationsManager.keyForEvent(rawEntry);
+    const syntheticEventKey = modificationsManager.keyForEvent(syntheticEvent);
+    const profileCallKey = modificationsManager.keyForEvent(profileCall);
+
+    // Test event -> key mappings
+    assert.deepEqual(rawEntryKey, ['r', 8036]);
+    assert.deepEqual(syntheticEventKey, ['s', 2078]);
+    assert.deepEqual(profileCallKey, ['p', 55385, 259, 38, 4]);
+
+    const resolvedRawEntry =
+        modificationsManager.eventForKey(rawEntryKey as TraceEngine.Types.File.TraceEventSerializableKey);
+    const resolvedSyntheticEntry =
+        modificationsManager.eventForKey(syntheticEventKey as TraceEngine.Types.File.TraceEventSerializableKey);
+    const resolvedProfileCall =
+        modificationsManager.eventForKey(profileCallKey as TraceEngine.Types.File.TraceEventSerializableKey);
+
+    // Test key -> event mappings
+    assert.strictEqual(resolvedRawEntry, rawEntry);
+    assert.strictEqual(resolvedSyntheticEntry, syntheticEvent);
+    assert.strictEqual(resolvedProfileCall, profileCall);
+  });
+
+  it('applies modifications when present in a trace file', async function() {
+    await TraceLoader.traceEngine(null, 'web-dev-modifications.json.gz');
+    const modificationsManager = ModificationsManager.ModificationsManager.ModificationsManager.activeManager();
+    if (!modificationsManager) {
+      throw new Error('Modifications manager does not exist.');
+    }
+    modificationsManager.applyModificationsIfPresent();
+    const entriesFilter = modificationsManager.getEntriesFilter();
+    assert.strictEqual(entriesFilter.expandableEntries().length, 1);
+    assert.strictEqual(entriesFilter.invisibleEntries().length, 42);
+    assert.deepEqual(modificationsManager.getTimelineBreadcrumbs().initialBreadcrumb, {
+      'window': {'min': 967569605481, 'max': 967573120579, 'range': 3515098},
+      'child':
+          {'window': {'min': 967569967927.7909, 'max': 967571964564.4985, 'range': 1996636.7076416016}, 'child': null},
+    } as TraceEngine.Types.File.Breadcrumb);
+  });
+
+  it('generates a serializable modifications json ', async function() {
+    await TraceLoader.traceEngine(null, 'web-dev-modifications.json.gz');
+    const modificationsManager = ModificationsManager.ModificationsManager.ModificationsManager.activeManager();
+    if (!modificationsManager) {
+      throw new Error('Modifications manager does not exist.');
+    }
+    modificationsManager.applyModificationsIfPresent();
+    const entriesFilter = modificationsManager.getEntriesFilter();
+    const modifications = modificationsManager.toJSON();
+    assert.strictEqual(entriesFilter.expandableEntries().length, 1);
+    assert.strictEqual(modifications.entriesModifications.expandableEntries.length, 1);
+    assert.strictEqual(modifications.entriesModifications.hiddenEntries.length, 42);
+    assert.deepEqual(modifications.initialBreadcrumb, {
+      'window': {'min': 967569605481, 'max': 967573120579, 'range': 3515098},
+      'child':
+          {'window': {'min': 967569967927.7909, 'max': 967571964564.4985, 'range': 1996636.7076416016}, 'child': null},
+    } as TraceEngine.Types.File.Breadcrumb);
   });
 });
--- front_end/services/modifications_manager/ModificationsManager.ts
+++ front_end/services/modifications_manager/ModificationsManager.ts
@@ -2,46 +2,120 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Platform from '../../core/platform/platform.js';
 import * as TraceEngine from '../../models/trace/trace.js';
 import * as TimelineComponents from '../../panels/timeline/components/components.js';
+import * as TraceBounds from '../trace_bounds/trace_bounds.js';
 
-let instance: ModificationsManager|null = null;
-type EntryToNodeMap =
-    Map<TraceEngine.Types.TraceEvents.SyntheticTraceEntry, TraceEngine.Helpers.TreeHelpers.TraceEntryNode>;
+const modificationsManagerByTraceIndex: ModificationsManager[] = [];
+
+type ModificationsManagerData = {
+  traceParsedData: TraceEngine.Handlers.Types.TraceParseData,
+  traceBounds: TraceEngine.Types.Timing.TraceWindowMicroSeconds,
+  modifications?: TraceEngine.Types.File.Modifications,
+               rawTraceEvents: readonly TraceEngine.Types.TraceEvents.TraceEventData[],
+               syntheticEvents: TraceEngine.Types.TraceEvents.SyntheticBasedEvent[],
+};
 
 export class ModificationsManager {
   /**
-   * An Array with all trace entries.
-   * We save modifications into the trace file by saving their id in the allEntries Array.
-   **/
-  #allEntries: TraceEngine.Types.TraceEvents.SyntheticTraceEntry[];
+   * All raw trace entries from a trace.
+   */
+  #rawTraceEvents: readonly TraceEngine.Types.TraceEvents.TraceEventData[];
   #entriesFilter: TraceEngine.EntriesFilter.EntriesFilter;
   #timelineBreadcrumbs: TimelineComponents.Breadcrumbs.Breadcrumbs;
+  #modifications: TraceEngine.Types.File.Modifications|null = null;
+  #traceParsedData: TraceEngine.Handlers.Types.TraceParseData;
+  #modifiedProfileCallByKey:
+      Map<TraceEngine.Types.File.ProfileCallKey, TraceEngine.Types.TraceEvents.SyntheticProfileCall> = new Map();
+  #syntheticEvents: TraceEngine.Types.TraceEvents.SyntheticBasedEvent[];
 
   /**
-   * A new instance is create each time a trace is recorded or loaded from a file.
-   * Both entryToNodeMap and wholeTraceBounds are mandatory to support all modifications and if one of them
-   * is not present, something has gone wrong so let's load the trace without the modifications support.
-   **/
-  static maybeInstance(opts: {
-    entryToNodeMap: EntryToNodeMap|null,
-    wholeTraceBounds: TraceEngine.Types.Timing.TraceWindowMicroSeconds|null|undefined,
-  } = {entryToNodeMap: null, wholeTraceBounds: null}): ModificationsManager|null {
-    if (opts.entryToNodeMap && opts.wholeTraceBounds) {
-      instance = new ModificationsManager(opts.entryToNodeMap, opts.wholeTraceBounds);
+   * Gets the ModificationsManager instance corresponding to a trace
+   * given its index used in Model#traces. If no index is passed gets
+   * the manager instance for the last trace. If no instance is found,
+   * throws.
+   */
+  static activeManager(): ModificationsManager|null {
+    if (modificationsManagerByTraceIndex.length === 0) {
+      return null;
     }
-    return instance;
+    return ModificationsManager.getManagerForTraceIndex(modificationsManagerByTraceIndex.length - 1);
   }
 
-  static removeInstance(): void {
-    instance = null;
+  static getManagerForTraceIndex(traceIndex: number): ModificationsManager {
+    if (!modificationsManagerByTraceIndex[traceIndex]) {
+      throw new Error(`Attempted to get a Modifications Manager with an unknown index ${traceIndex}`);
+    }
+    return modificationsManagerByTraceIndex[traceIndex];
+  }
+  /**
+   * Initializes a ModificationsManager instance for a parsed trace. This needs to be called
+   * if and only if a trace has been parsed.
+   */
+  static initModificationsManagerForTrace(
+      traceModel: TraceEngine.TraceModel.Model<typeof TraceEngine.Handlers.ModelHandlers>,
+      traceIndex: number): ModificationsManager {
+    const traceParsedData = traceModel.traceParsedData(traceIndex);
+    if (!traceParsedData) {
+      throw new Error('ModificationsManager was initialized without a corresponding trace data');
+    }
+    const traceBounds = TraceBounds.TraceBounds.BoundsManager.instance().state()?.micro.entireTraceBounds ||
+        traceParsedData.Meta.traceBounds;
+    const traceEvents = traceModel.rawTraceEvents(traceIndex);
+    if (!traceEvents) {
+      throw new Error('ModificationsManager was initialized without a corresponding raw trace events array');
+    }
+    const syntheticEventsManager = traceModel.syntheticTraceEventsManager(traceIndex);
+    if (!syntheticEventsManager) {
+      throw new Error('ModificationsManager was initialized without a corresponding SyntheticEventsManager');
+    }
+    const metadata = traceModel.metadata(traceIndex);
+    const newModificationsManager = new ModificationsManager({
+      traceParsedData,
+      traceBounds,
+      rawTraceEvents: traceEvents,
+      modifications: metadata?.modifications,
+      syntheticEvents: syntheticEventsManager.allSyntheticEvents(),
+    });
+    modificationsManagerByTraceIndex[traceIndex] = newModificationsManager;
+    return newModificationsManager;
   }
 
-  private constructor(
-      entryToNodeMap: EntryToNodeMap, wholeTraceBounds: TraceEngine.Types.Timing.TraceWindowMicroSeconds) {
+  static isProfileCallKey(key: TraceEngine.Types.File.TraceEventSerializableKey):
+      key is TraceEngine.Types.File.ProfileCallKey {
+    return key[0] === 'p';
+  }
+  static isRawEventKey(key: TraceEngine.Types.File.TraceEventSerializableKey):
+      key is TraceEngine.Types.File.RawEventKey {
+    return key[0] === 'r';
+  }
+  static isSyntheticEventKey(key: TraceEngine.Types.File.TraceEventSerializableKey):
+      key is TraceEngine.Types.File.SyntheticEventKey {
+    return key[0] === 's';
+  }
+
+  static isTraceEventSerializableKey(key: (number|string)[]): key is TraceEngine.Types.File.TraceEventSerializableKey {
+    const maybeValidKey = key as TraceEngine.Types.File.TraceEventSerializableKey;
+    if (ModificationsManager.isProfileCallKey(maybeValidKey)) {
+      return key.length === 5 &&
+          key.every((entry, i) => i === 0 || typeof entry === 'number' || !isNaN(parseInt(entry, 10)));
+    }
+    if (ModificationsManager.isRawEventKey(maybeValidKey) || ModificationsManager.isSyntheticEventKey(maybeValidKey)) {
+      return key.length === 2 && (typeof key[1] === 'number' || !isNaN(parseInt(key[1], 10)));
+    }
+    return false;
+  }
+
+  private constructor({traceParsedData, traceBounds, modifications, rawTraceEvents, syntheticEvents}:
+                          ModificationsManagerData) {
+    const entryToNodeMap = new Map([...traceParsedData.Samples.entryToNode, ...traceParsedData.Renderer.entryToNode]);
     this.#entriesFilter = new TraceEngine.EntriesFilter.EntriesFilter(entryToNodeMap);
-    this.#timelineBreadcrumbs = new TimelineComponents.Breadcrumbs.Breadcrumbs(wholeTraceBounds);
-    this.#allEntries = Array.from(entryToNodeMap.keys());
+    this.#timelineBreadcrumbs = new TimelineComponents.Breadcrumbs.Breadcrumbs(traceBounds);
+    this.#rawTraceEvents = rawTraceEvents;
+    this.#modifications = modifications || null;
+    this.#traceParsedData = traceParsedData;
+    this.#syntheticEvents = syntheticEvents;
   }
 
   getEntriesFilter(): TraceEngine.EntriesFilter.EntriesFilter {
@@ -52,62 +126,113 @@ export class ModificationsManager {
     return this.#timelineBreadcrumbs;
   }
 
-  getEntryIndex(entry: TraceEngine.Types.TraceEvents.SyntheticTraceEntry): number {
-    return this.#allEntries.indexOf(entry);
+  keyForEvent(event: TraceEngine.Types.TraceEvents.TraceEventData): TraceEngine.Types.File.TraceEventSerializableKey
+      |null {
+    if (TraceEngine.Types.TraceEvents.isProfileCall(event)) {
+      return ['p', event.pid, event.tid, TraceEngine.Types.TraceEvents.SampleIndex(event.sampleIndex), event.nodeId];
+    }
+    const key: TraceEngine.Types.File.SyntheticEventKey|TraceEngine.Types.File.RawEventKey =
+        TraceEngine.Types.TraceEvents.isSyntheticBasedEvent(event) ?
+        ['s', this.#rawTraceEvents.indexOf(event.rawSourceEvent)] :
+        ['r', this.#rawTraceEvents.indexOf(event)];
+    if (key[1] < 0) {
+      return null;
+    }
+    return key;
+  }
+
+  eventForKey(key: TraceEngine.Types.File.TraceEventSerializableKey): TraceEngine.Types.TraceEvents.TraceEventData {
+    if (ModificationsManager.isProfileCallKey(key)) {
+      return this.#getModifiedProfileCallByKey(key);
+    }
+    if (ModificationsManager.isSyntheticEventKey(key)) {
+      const syntheticEvent = this.#syntheticEvents.at(key[1]);
+      if (!syntheticEvent) {
+        throw new Error(`Attempted to get a synthetic event from an unknown raw event index: ${key[1]}`);
+      }
+      return syntheticEvent;
+    }
+    if (ModificationsManager.isRawEventKey(key)) {
+      return this.#rawTraceEvents[key[1]];
+    }
+    throw new Error(`Unknown trace event serializable key: ${(key as Array<unknown>).join('-')}`);
+  }
+
+  #getModifiedProfileCallByKey(key: TraceEngine.Types.File.ProfileCallKey):
+      TraceEngine.Types.TraceEvents.SyntheticProfileCall {
+    const cacheResult = this.#modifiedProfileCallByKey.get(key);
+    if (cacheResult) {
+      return cacheResult;
+    }
+    const processId = key[1];
+    const threadId = key[2];
+    const sampleIndex = key[3];
+    const nodeId = key[4];
+    const profileCallsInThread =
+        this.#traceParsedData.Renderer.processes.get(processId)?.threads.get(threadId)?.profileCalls;
+    if (!profileCallsInThread) {
+      throw new Error(`Unknown profile call serializable key: ${(key as Array<unknown>).join('-')}`);
+    }
+
+    // Do a binary search on the complete profile call list to efficiently lookup for a
+    // match based on sample index and node id. We need both because multiple calls can share
+    // the same sample index, in which case we need to break the tie with the node id (by which
+    // calls in a sample stack are ordered, allowing us to do a single search).
+    const matchRangeStartIndex = Platform.ArrayUtilities.nearestIndexFromBeginning(
+        profileCallsInThread, e => e.sampleIndex >= sampleIndex && e.nodeId >= nodeId);
+
+    const match = matchRangeStartIndex !== null && profileCallsInThread.at(matchRangeStartIndex);
+    if (!match) {
+      throw new Error(`Unknown profile call serializable key: ${(key as Array<unknown>).join('-')}`);
+    }
+    // Cache to avoid looking up in subsequent calls.
+    this.#modifiedProfileCallByKey.set(key, match);
+    return match;
   }
 
   /**
-   * Builds all modifications and returns the object written into the 'modifications' trace file metada field.
+   * Builds all modifications into a serializable object written into
+   * the 'modifications' trace file metadata field.
    */
-  getModifications(): TraceEngine.Types.File.Modifications {
-    const indexesOfSynteticEntries: number[] = [];
-    const hiddenEntries = this.#entriesFilter.invisibleEntries();
-    if (hiddenEntries) {
-      for (const entry of hiddenEntries) {
-        indexesOfSynteticEntries.push(this.getEntryIndex(entry));
-      }
-    }
-
-    const indexesOfModifiedEntries: number[] = [];
-    const modifiedEntries = this.#entriesFilter.expandableEntries();
-    if (modifiedEntries) {
-      for (const entry of modifiedEntries) {
-        indexesOfModifiedEntries.push(this.getEntryIndex(entry));
-      }
-    }
-
-    return {
-      entriesFilterModifications: {
-        hiddenEntriesIndexes: indexesOfSynteticEntries,
-        expandableEntriesIndexes: indexesOfModifiedEntries,
+  toJSON(): TraceEngine.Types.File.Modifications {
+    const hiddenEntries =
+        this.#entriesFilter.invisibleEntries().map(this.keyForEvent.bind(this)).filter(key => key !== null) as
+        TraceEngine.Types.File.TraceEventSerializableKey[];
+    const expandableEntries =
+        this.#entriesFilter.expandableEntries().map(this.keyForEvent.bind(this)).filter(key => key !== null) as
+        TraceEngine.Types.File.TraceEventSerializableKey[];
+    this.#modifications = {
+      entriesModifications: {
+        hiddenEntries: hiddenEntries.map(key => key.join('-')),
+        expandableEntries: expandableEntries.map(key => key.join('-')),
       },
       initialBreadcrumb: this.#timelineBreadcrumbs.initialBreadcrumb,
     };
+    return this.#modifications;
   }
 
-  applyModifications(modifications: TraceEngine.Types.File.Modifications): void {
-    this.applyEntriesFilterModifications(
-        modifications.entriesFilterModifications.hiddenEntriesIndexes,
-        modifications.entriesFilterModifications.expandableEntriesIndexes);
+  applyModificationsIfPresent(): void {
+    const modifications = this.#modifications;
+    if (!modifications) {
+      return;
+    }
+    const hiddenEntries = modifications.entriesModifications.hiddenEntries.map(
+        key => key.split('-').map(item => isNaN(parseInt(item, 10)) ? item : parseInt(item, 10)));
+    const expandableEntries = modifications.entriesModifications.expandableEntries.map(
+        key => key.split('-').map(item => isNaN(parseInt(item, 10)) ? item : parseInt(item, 10)));
+    if (!hiddenEntries.every(ModificationsManager.isTraceEventSerializableKey) ||
+        !expandableEntries.every(ModificationsManager.isTraceEventSerializableKey)) {
+      throw new Error('Invalid event key found in JSON modifications');
+    }
+    this.applyEntriesFilterModifications(hiddenEntries, expandableEntries);
     this.#timelineBreadcrumbs.setInitialBreadcrumbFromLoadedModifications(modifications.initialBreadcrumb);
   }
 
-  applyEntriesFilterModifications(hiddenEntriesIndexes: number[], expandableEntriesIndexes: number[]): void {
-    // Build the hidden events array by getting the entries by their index in the allEntries array.
-    const hiddenEntries: TraceEngine.Types.TraceEvents.SyntheticTraceEntry[] = [];
-    hiddenEntriesIndexes.map(hiddenEntryHash => {
-      const hiddenEntry = this.#allEntries[hiddenEntryHash];
-      if (hiddenEntry) {
-        hiddenEntries.push(hiddenEntry);
-      }
-    });
-    const expandableEntries: TraceEngine.Types.TraceEvents.SyntheticTraceEntry[] = [];
-    expandableEntriesIndexes.map(hiddenEntryHash => {
-      const expandableEntry = this.#allEntries[hiddenEntryHash];
-      if (expandableEntry) {
-        expandableEntries.push(expandableEntry);
-      }
-    });
+  applyEntriesFilterModifications(
+      hiddenEntriesKeys: TraceEngine.Types.File.TraceEventSerializableKey[],
+      expandableEntriesKeys: TraceEngine.Types.File.TraceEventSerializableKey[]): void {
+    const hiddenEntries = hiddenEntriesKeys.map(this.eventForKey.bind(this));
+    const expandableEntries = expandableEntriesKeys.map(this.eventForKey.bind(this));
     this.#entriesFilter.setHiddenAndExpandableEntries(hiddenEntries, expandableEntries);
   }
 }
