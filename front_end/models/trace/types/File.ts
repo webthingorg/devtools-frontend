c5613be429c284acc9ce675a9018f6980369951f
Alina Varkki <alinavarkki@google.com>
2024-06-05 13:52:06 +0000

    intoduce keys as strings
    Change-Id: Icb81b8b66de0a00a38f89ec74095eda1e23622ca

--- front_end/models/trace/types/File.ts
+++ front_end/models/trace/types/File.ts
@@ -25,17 +25,37 @@ export const enum DataOrigin {
 // references to timeline events in a trace file. These keys enable
 // user modifications that can be saved. See go/cpq:event-data-json for
 // more details on the key format.
-export type RawEventKey = ['r', number];
-export type ProfileCallKey = ['p', ProcessID, ThreadID, SampleIndex, Protocol.integer];
-export type SyntheticEventKey = ['s', number];
+export type RawEventKeyValues = {
+  type: 'r',
+  number: number,
+};
+
+export type SyntheticEventKeyValues = {
+  type: 's',
+  number: number,
+};
+
+export type ProfileCallKeyValues = {
+  type: 'p',
+  processID: ProcessID,
+  shreadID: ThreadID,
+  sampleIndex: SampleIndex,
+  protocol: Protocol.integer,
+};
+
+export type TraceEventSerializableKeyValues = RawEventKeyValues|ProfileCallKeyValues|SyntheticEventKeyValues;
+
+export type RawEventKey = `r-${number}`;
+export type SyntheticEventKey = `s-${number}`;
+export type ProfileCallKey = `p-${ProcessID}-${ThreadID}-${SampleIndex}-${Protocol.integer}`;
 export type TraceEventSerializableKey = RawEventKey|ProfileCallKey|SyntheticEventKey;
 
 export interface Modifications {
   entriesModifications: {
     // Entries hidden by the user
-    hiddenEntries: string[],
+    hiddenEntries: TraceEventSerializableKey[],
     // Entries that parent a hiddenEntry
-    expandableEntries: string[],
+    expandableEntries: TraceEventSerializableKey[],
   };
   initialBreadcrumb: Breadcrumb;
 }
--- front_end/services/events_serializer/EventsSerializer.ts
+++ front_end/services/events_serializer/EventsSerializer.ts
@@ -7,85 +7,114 @@ import * as TraceEngine from '../../models/trace/trace.js';
 
 export class EventsSerializer {
   #modifiedProfileCallByKey:
-      Map<TraceEngine.Types.File.ProfileCallKey, TraceEngine.Types.TraceEvents.SyntheticProfileCall> = new Map();
+      Map<TraceEngine.Types.File.ProfileCallKeyValues, TraceEngine.Types.TraceEvents.SyntheticProfileCall> = new Map();
 
   keyForEvent(event: TraceEngine.Types.TraceEvents.TraceEventData): TraceEngine.Types.File.TraceEventSerializableKey
       |null {
     if (TraceEngine.Types.TraceEvents.isProfileCall(event)) {
-      return ['p', event.pid, event.tid, TraceEngine.Types.TraceEvents.SampleIndex(event.sampleIndex), event.nodeId];
+      return `p-${event.pid}-${event.tid}-${TraceEngine.Types.TraceEvents.SampleIndex(event.sampleIndex)}-${
+          event.nodeId}`;
     }
     const rawEvents = TraceEngine.Helpers.SyntheticEvents.SyntheticEventsManager.getActiveManager().getRawTraceEvents();
     const key: TraceEngine.Types.File.SyntheticEventKey|TraceEngine.Types.File.RawEventKey =
-        TraceEngine.Types.TraceEvents.isSyntheticBasedEvent(event) ? ['s', rawEvents.indexOf(event.rawSourceEvent)] :
-                                                                     ['r', rawEvents.indexOf(event)];
-    if (key[1] < 0) {
+        TraceEngine.Types.TraceEvents.isSyntheticBasedEvent(event) ? `s-${rawEvents.indexOf(event.rawSourceEvent)}` :
+                                                                     `r-${rawEvents.indexOf(event)}`;
+    if (key.length < 3) {
       return null;
     }
     return key;
   }
 
-  static isTraceEventSerializableKey(key: (number|string)[]): key is TraceEngine.Types.File.TraceEventSerializableKey {
-    const maybeValidKey = key as TraceEngine.Types.File.TraceEventSerializableKey;
-    if (EventsSerializer.isProfileCallKey(maybeValidKey)) {
-      return key.length === 5 &&
-          key.every((entry, i) => i === 0 || typeof entry === 'number' || !isNaN(parseInt(entry, 10)));
-    }
-    if (EventsSerializer.isRawEventKey(maybeValidKey) || EventsSerializer.isSyntheticEventKey(maybeValidKey)) {
-      return key.length === 2 && (typeof key[1] === 'number' || !isNaN(parseInt(key[1], 10)));
-    }
-    return false;
-  }
-
   eventForKey(
       key: TraceEngine.Types.File.TraceEventSerializableKey,
       traceParsedData: TraceEngine.Handlers.Types.TraceParseData): TraceEngine.Types.TraceEvents.TraceEventData {
-    if (EventsSerializer.isProfileCallKey(key)) {
-      return this.#getModifiedProfileCallByKey(key, traceParsedData);
+    const eventValues = this.traceEventKeyToValues(key);
+
+    if (EventsSerializer.isProfileCallKey(eventValues)) {
+      return this.#getModifiedProfileCallByKeyValues(eventValues, traceParsedData);
     }
-    if (EventsSerializer.isSyntheticEventKey(key)) {
+
+    if (EventsSerializer.isSyntheticEventKey(eventValues)) {
       const syntheticEvents =
           TraceEngine.Helpers.SyntheticEvents.SyntheticEventsManager.getActiveManager().getSyntheticTraceEvents();
-      const syntheticEvent = syntheticEvents.at(key[1]);
+      const syntheticEvent = syntheticEvents.at(eventValues.number);
       if (!syntheticEvent) {
-        throw new Error(`Attempted to get a synthetic event from an unknown raw event index: ${key[1]}`);
+        throw new Error(`Attempted to get a synthetic event from an unknown raw event index: ${eventValues.number}`);
       }
       return syntheticEvent;
     }
-    if (EventsSerializer.isRawEventKey(key)) {
+
+    if (EventsSerializer.isRawEventKey(eventValues)) {
       const rawEvents =
           TraceEngine.Helpers.SyntheticEvents.SyntheticEventsManager.getActiveManager().getRawTraceEvents();
-      return rawEvents[key[1]];
+      return rawEvents[eventValues.number];
     }
-    throw new Error(`Unknown trace event serializable key: ${(key as Array<unknown>).join('-')}`);
+    throw new Error(`Unknown trace event serializable key values: ${(eventValues as Array<unknown>).join('-')}`);
   }
 
-  static isProfileCallKey(key: TraceEngine.Types.File.TraceEventSerializableKey):
-      key is TraceEngine.Types.File.ProfileCallKey {
-    return key[0] === 'p';
-  }
-  static isRawEventKey(key: TraceEngine.Types.File.TraceEventSerializableKey):
-      key is TraceEngine.Types.File.RawEventKey {
-    return key[0] === 'r';
-  }
-  static isSyntheticEventKey(key: TraceEngine.Types.File.TraceEventSerializableKey):
-      key is TraceEngine.Types.File.SyntheticEventKey {
-    return key[0] === 's';
+  traceEventKeyToValues(key: TraceEngine.Types.File.TraceEventSerializableKey):
+      TraceEngine.Types.File.TraceEventSerializableKeyValues {
+    const parts = key.split('-');
+    const type = parts[0];
+
+    switch (type) {
+      case 'p':
+        if (parts.length !== 5 ||
+            !(parts.every((entry, i) => i === 0 || typeof entry === 'number' || !isNaN(parseInt(entry, 10))))) {
+          throw new Error(`Invalid ProfileCallKey: ${key}`);
+        }
+        return {
+          type: parts[0],
+          processID: parseInt(parts[1], 10),
+          shreadID: parseInt(parts[2], 10),
+          sampleIndex: parseInt(parts[3], 10),
+          protocol: parseInt(parts[4], 10),
+        } as TraceEngine.Types.File.ProfileCallKeyValues;
+      case 'r':
+        if (parts.length !== 2 || !(typeof key[1] === 'number' || !isNaN(parseInt(key[1], 10)))) {
+          throw new Error(`Invalid RawEvent Key: ${key}`);
+        }
+        return {
+          type: parts[0],
+          number: parseInt(parts[1], 10),
+        } as TraceEngine.Types.File.RawEventKeyValues;
+      case 's':
+        if (parts.length !== 2 || !(typeof key[1] === 'number' || !isNaN(parseInt(key[1], 10)))) {
+          throw new Error(`Invalid SyntheticEvent Key: ${key}`);
+        }
+        return {
+          type: parts[0],
+          number: parseInt(parts[1], 10),
+        } as TraceEngine.Types.File.SyntheticEventKeyValues;
+      default:
+        throw new Error(`Unknown trace event key: ${key}`);
+    }
   }
 
-  #getModifiedProfileCallByKey(
-      key: TraceEngine.Types.File.ProfileCallKey,
+  static isProfileCallKey(key: TraceEngine.Types.File.TraceEventSerializableKeyValues):
+      key is TraceEngine.Types.File.ProfileCallKeyValues {
+    return key.type === 'p';
+  }
+  static isRawEventKey(key: TraceEngine.Types.File.TraceEventSerializableKeyValues):
+      key is TraceEngine.Types.File.RawEventKeyValues {
+    return key.type === 'r';
+  }
+  static isSyntheticEventKey(key: TraceEngine.Types.File.TraceEventSerializableKeyValues):
+      key is TraceEngine.Types.File.SyntheticEventKeyValues {
+    return key.type === 's';
+  }
+
+  #getModifiedProfileCallByKeyValues(
+      key: TraceEngine.Types.File.ProfileCallKeyValues,
       traceParsedData: TraceEngine.Handlers.Types.TraceParseData): TraceEngine.Types.TraceEvents.SyntheticProfileCall {
     const cacheResult = this.#modifiedProfileCallByKey.get(key);
     if (cacheResult) {
       return cacheResult;
     }
-    const processId = key[1];
-    const threadId = key[2];
-    const sampleIndex = key[3];
-    const nodeId = key[4];
-    const profileCallsInThread = traceParsedData.Renderer.processes.get(processId)?.threads.get(threadId)?.profileCalls;
+    const profileCallsInThread =
+        traceParsedData.Renderer.processes.get(key.processID)?.threads.get(key.shreadID)?.profileCalls;
     if (!profileCallsInThread) {
-      throw new Error(`Unknown profile call serializable key: ${(key as Array<unknown>).join('-')}`);
+      throw new Error(`Unknown profile call serializable key: ${(key)}`);
     }
 
     // Do a binary search on the complete profile call list to efficiently lookup for a
@@ -93,11 +122,11 @@ export class EventsSerializer {
     // the same sample index, in which case we need to break the tie with the node id (by which
     // calls in a sample stack are ordered, allowing us to do a single search).
     const matchRangeStartIndex = Platform.ArrayUtilities.nearestIndexFromBeginning(
-        profileCallsInThread, e => e.sampleIndex >= sampleIndex && e.nodeId >= nodeId);
+        profileCallsInThread, e => e.sampleIndex >= key.sampleIndex && e.nodeId >= key.protocol);
 
     const match = matchRangeStartIndex !== null && profileCallsInThread.at(matchRangeStartIndex);
     if (!match) {
-      throw new Error(`Unknown profile call serializable key: ${(key as Array<unknown>).join('-')}`);
+      throw new Error(`Unknown profile call serializable key: ${(key)}`);
     }
     // Cache to avoid looking up in subsequent calls.
     this.#modifiedProfileCallByKey.set(key, match);
