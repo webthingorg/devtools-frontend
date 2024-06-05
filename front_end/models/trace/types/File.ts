ee4cd7c7a9875c8d6d6d0239ee25d65ebbec73d0
Alina Varkki <alinavarkki@google.com>
2024-06-05 13:59:52 +0000

    intoduce keys as strings
    Change-Id: Icb81b8b66de0a00a38f89ec74095eda1e23622ca

--- front_end/models/trace/types/File.ts
+++ front_end/models/trace/types/File.ts
@@ -25,17 +25,75 @@ export const enum DataOrigin {
 // references to timeline events in a trace file. These keys enable
 // user modifications that can be saved. See go/cpq:event-data-json for
 // more details on the key format.
-export type RawEventKey = ['r', number];
-export type ProfileCallKey = ['p', ProcessID, ThreadID, SampleIndex, Protocol.integer];
-export type SyntheticEventKey = ['s', number];
+export type RawEventKeyValues = {
+  type: 'r',
+  number: number,
+};
+
+export type SyntheticEventKeyValues = {
+  type: 's',
+  number: number,
+};
+
+export type ProfileCallKeyValues = {
+  type: 'p',
+  processID: ProcessID,
+  shreadID: ThreadID,
+  sampleIndex: SampleIndex,
+  protocol: Protocol.integer,
+};
+
+export type TraceEventSerializableKeyValues = RawEventKeyValues|ProfileCallKeyValues|SyntheticEventKeyValues;
+
+export function traceEventKeyToValues(key: TraceEventSerializableKey): TraceEventSerializableKeyValues {
+  const parts = key.split('-');
+  const type = parts[0];
+
+  switch (type) {
+    case 'p':
+      if (parts.length !== 5 ||
+          !(parts.every((entry, i) => i === 0 || typeof entry === 'number' || !isNaN(parseInt(entry, 10))))) {
+        throw new Error(`Invalid ProfileCallKey: ${key}`);
+      }
+      return {
+        type: parts[0],
+        processID: parseInt(parts[1], 10),
+        shreadID: parseInt(parts[2], 10),
+        sampleIndex: parseInt(parts[3], 10),
+        protocol: parseInt(parts[4], 10),
+      } as ProfileCallKeyValues;
+    case 'r':
+      if (parts.length !== 2 || !(typeof key[1] === 'number' || !isNaN(parseInt(key[1], 10)))) {
+        throw new Error(`Invalid RawEvent Key: ${key}`);
+      }
+      return {
+        type: parts[0],
+        number: parseInt(parts[1], 10),
+      } as RawEventKeyValues;
+    case 's':
+      if (parts.length !== 2 || !(typeof key[1] === 'number' || !isNaN(parseInt(key[1], 10)))) {
+        throw new Error(`Invalid SyntheticEvent Key: ${key}`);
+      }
+      return {
+        type: parts[0],
+        number: parseInt(parts[1], 10),
+      } as SyntheticEventKeyValues;
+    default:
+      throw new Error(`Unknown trace event key: ${key}`);
+  }
+}
+
+export type RawEventKey = `r-${number}`;
+export type SyntheticEventKey = `s-${number}`;
+export type ProfileCallKey = `p-${ProcessID}-${ThreadID}-${SampleIndex}-${Protocol.integer}`;
 export type TraceEventSerializableKey = RawEventKey|ProfileCallKey|SyntheticEventKey;
 
 export interface Modifications {
   entriesModifications: {
     // Entries hidden by the user
-    hiddenEntries: string[],
+    hiddenEntries: TraceEventSerializableKey[],
     // Entries that parent a hiddenEntry
-    expandableEntries: string[],
+    expandableEntries: TraceEventSerializableKey[],
   };
   initialBreadcrumb: Breadcrumb;
 }
