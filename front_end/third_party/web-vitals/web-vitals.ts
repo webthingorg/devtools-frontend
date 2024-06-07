4e570a0998658a4aebefc255259072f4a4291c15
Adam Raine <asraine@chromium.org>
2024-06-07 14:06:50 -0700

    [RPP Observations] Add interaction event log
    Bug: 313906462
    Change-Id: If1087d8f69fb82922ebaf20b10b9c39f79e15f60

--- front_end/third_party/web-vitals/web-vitals.ts
+++ front_end/third_party/web-vitals/web-vitals.ts
@@ -1,2 +1,3 @@
 export * from './package/dist/modules/index.js';
 export * as Attribution from './package/dist/modules/attribution/index.js';
+export {DEFAULT_DURATION_THRESHOLD} from './package/dist/modules/lib/interactions.js';
