a977c77794aa013c1ef7411f8b226554a3db7915
Adriana Ixba <aixba@google.com>
2024-06-07 23:26:40 +0000

    Simple LCP insight component for sidebar
    This creates a simple outline component, and as a placeholder
    creates an LCP insight. The component is shown when the sidebar is toggled open. It does not yet consume actual LCP trace data.
    
    https://screenshot.googleplex.com/7537aGtTUydqLnS
    
    Bug:344550388
    Change-Id: I26aa204aac814c205d0e8d48a2432e6e24ae0c8e

--- front_end/panels/timeline/components/Sidebar.ts
+++ front_end/panels/timeline/components/Sidebar.ts
@@ -8,6 +8,7 @@ import * as UI from '../../../ui/legacy/legacy.js';
 import * as LitHtml from '../../../ui/lit-html/lit-html.js';
 
 import sidebarStyles from './sidebar.css.js';
+import * as SidebarInsight from './SidebarInsight.js';
 
 const COLLAPSED_WIDTH = 40;
 const DEFAULT_EXPANDED_WIDTH = 240;
@@ -58,6 +59,16 @@ export class SidebarUI extends HTMLElement {
 
   render(expanded: boolean): void {
     const toggleIcon = expanded ? 'left-panel-close' : 'left-panel-open';
+    const showLCPPhases = true;
+    const lcpTitle = 'LCP by Phase';
+
+    const lcpPhaseData = [
+      {phase: 'Time to first byte', percentage: 15},
+      {phase: 'Resource load delay', percentage: 42},
+      {phase: 'Resource load duration', percentage: 25},
+      {phase: 'Resource render delay', percentage: 18},
+    ];
+
     // clang-format off
     const output = LitHtml.html`<div class=${LitHtml.Directives.classMap({
       sidebar: true,
@@ -67,6 +78,29 @@ export class SidebarUI extends HTMLElement {
       <div class="tab-bar">
         <${IconButton.Icon.Icon.litTagName} name=${toggleIcon} @click=${this.#toggleButtonClick} class="sidebar-toggle-button">
         </${IconButton.Icon.Icon.litTagName}>
+      </div>
+      <div class="insights">
+        ${expanded && showLCPPhases ? LitHtml.html`
+          <${SidebarInsight.SidebarInsight.litTagName} .data=${{
+              title: lcpTitle,
+            } as SidebarInsight.InsightDetails}>
+            <div slot="insight-description" class="insight-description">
+                Each
+                <x-link class="link" href="https://web.dev/articles/optimize-lcp#lcp-breakdown">phase has specific recommendations to improve.</x-link>
+                In an ideal load, the two delay phases should be quite short.
+            </div>
+            <div slot="insight-content" class="table-container">
+              <dl>
+                <dt style="font-weight: bold;">Phase</dt>
+                <dd style="font-weight: bold;">% of LCP</dd>
+                ${lcpPhaseData.map(phase => LitHtml.html`
+                  <dt>${phase.phase}</dt>
+                  <dd style="font-weight: bold;">${phase.percentage}%</dd>
+                `)}
+              </dl>
+            </div>
+          </${SidebarInsight.SidebarInsight}>
+        ` : null}
       </div>
     </div>`;
     // clang-format on
