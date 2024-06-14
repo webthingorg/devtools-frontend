f84b85e2d5e8c05939bfa835cceb83529fcf88c5
Nancy Li <nancyly@chromium.org>
2024-06-14 17:07:49 +0000

    [RPP] Add metrics  to sidebar
    This cl adds:
    - a dropdown menu
    - LCP metric for the first navigation
    - CLS for the whole trace
    - INP for the whole trace
    
    Bug: 313905509
    Change-Id: I213a627504072f988c3ea828ef573761b5367122

--- front_end/models/trace/handlers/UserInteractionsHandler.ts
+++ front_end/models/trace/handlers/UserInteractionsHandler.ts
@@ -6,6 +6,7 @@ import * as Helpers from '../helpers/helpers.js';
 import * as Types from '../types/types.js';
 
 import {data as metaHandlerData} from './MetaHandler.js';
+import {ScoreClassification} from './PageLoadMetricsHandler.js';
 import {HandlerState, type TraceEventHandlerName} from './types.js';
 
 // This handler serves two purposes. It generates a list of events that are
@@ -359,4 +360,23 @@ export function data(): UserInteractionsData {
 
 export function deps(): TraceEventHandlerName[] {
   return ['Meta'];
+}
+
+/**
+ * Classifications sourced from
+ * https://web.dev/articles/inp#good-score
+ */
+export function scoreClassificationForInteractionToNextPaint(timing: Types.Timing.MicroSeconds): ScoreClassification {
+  const INP_GOOD_TIMING = Types.Timing.MilliSeconds(200);
+  const INP_MEDIUM_TIMING = Types.Timing.MilliSeconds(500);
+
+  if (timing <= INP_GOOD_TIMING) {
+    return ScoreClassification.GOOD;
+  }
+
+  if (timing <= INP_MEDIUM_TIMING) {
+    return ScoreClassification.OK;
+  }
+
+  return ScoreClassification.BAD;
 }
