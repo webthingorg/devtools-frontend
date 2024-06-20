45460d95fa3b3135c8a2dbf681be4dae1e52e20b
Connor Clark <cjamcl@chromium.org>
2024-06-20 14:42:13 -0700

    [trace engine] Add Lighthouse's Lantern library
    This is the culmination of isolating Lighthouse's Lantern library
    (https://github.com/GoogleChrome/lighthouse/issues/15841). The code in
    this CL took Lantern as it now exists in Lighthouse, converted to
    TypeScript, integrated its unit tests (Jest -> Karma conversion), and
    resolved the majority of CDT's JavaScript linter errors.
    
    There is no behavior change to CDT with this CL, besides TraceModel
    exporting the Lantern module.
    
    There is some refactoring work remaining, captured by
    crbug.com/348449529
    
    Bug: 335099340
    Change-Id: Ib2bf64fc17d334859caca5c42bea1e276465d64c

---
+++ front_end/models/trace/lantern/lantern.ts
@@ -0,0 +1,54 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const NetworkRequestTypes = {
+  XHR: 'XHR',
+  Fetch: 'Fetch',
+  EventSource: 'EventSource',
+  Script: 'Script',
+  Stylesheet: 'Stylesheet',
+  Image: 'Image',
+  Media: 'Media',
+  Font: 'Font',
+  Document: 'Document',
+  TextTrack: 'TextTrack',
+  WebSocket: 'WebSocket',
+  Other: 'Other',
+  Manifest: 'Manifest',
+  SignedExchange: 'SignedExchange',
+  Ping: 'Ping',
+  Preflight: 'Preflight',
+  CSPViolationReport: 'CSPViolationReport',
+  Prefetch: 'Prefetch',
+} as const;
+
+export {BaseNode} from './BaseNode.js';
+export type {Node} from './BaseNode.js';
+export {CPUNode} from './CpuNode.js';
+export {LanternError as Error} from './LanternError.js';
+export {Metric} from './Metric.js';
+export {NetworkNode} from './NetworkNode.js';
+export {PageDependencyGraph} from './PageDependencyGraph.js';
+export * as Metrics from './metrics/metrics.js';
+export * as Simulation from './simulation/simulation.js';
+export * as TBTUtils from './TBTUtils.js';
+export type {
+  NetworkRequest,
+  ParsedURL,
+  ResourcePriority,
+  ResourceTiming,
+  ResourceType,
+  Trace,
+  TraceEvent,
+} from './types/lantern.js';
+
+export {
+  NetworkRequestTypes,
+};
