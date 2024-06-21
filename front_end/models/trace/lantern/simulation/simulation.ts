a035029eeb2e72d98eca948b8d75fdd6319a43c3
Connor Clark <cjamcl@chromium.org>
2024-06-21 11:21:46 -0700

    [trace engine] Add Lighthouse's Lantern library
    This is the culmination of isolating Lighthouse's Lantern library
    (https://github.com/GoogleChrome/lighthouse/issues/15841). The code in
    this CL took Lantern as it now exists in Lighthouse, converted to
    TypeScript, integrated its unit tests (Jest -> Karma conversion), and
    resolved the majority of CDT's JavaScript linter errors.
    
    There is no behavior change to CDT with this CL, besides TraceModel
    exporting the Lantern module.
    
    There is some refactoring work remaining, captured by
    crbug.com/348449529
    
    Bug: 335099340
    Change-Id: Ib2bf64fc17d334859caca5c42bea1e276465d64c

---
+++ front_end/models/trace/lantern/simulation/simulation.ts
@@ -0,0 +1,28 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {type AnyNetworkObject, type Simulation} from '../types/lantern.js';
+
+export {ConnectionPool} from './ConnectionPool.js';
+export {Constants} from './Constants.js';
+export {DNSCache} from './DNSCache.js';
+export {NetworkAnalyzer} from './NetworkAnalyzer.js';
+export {type CompleteNodeTiming, SimulatorTimingMap} from './SimulationTimingMap.js';
+export {Simulator} from './Simulator.js';
+export {TCPConnection} from './TCPConnection.js';
+
+export type MetricCoefficients = Simulation.MetricCoefficients;
+export type MetricComputationDataInput = Simulation.MetricComputationDataInput;
+export type NodeTiming = Simulation.NodeTiming;
+export type Options = Simulation.Options;
+export type ProcessedNavigation = Simulation.ProcessedNavigation;
+export type Result<T = AnyNetworkObject> = Simulation.Result<T>;
+export type Settings = Simulation.Settings;
+export type URL = Simulation.URL;
