07f704e367e8fe024a2e5bd6aa2a31247f869ef4
Christy Chen <chrche@microsoft.com>
2024-06-26 14:02:41 -0700

    Add WebSocket connection and activities in the Performance tool
    Adding webcoket activities to the performance tool under the Network track. A synthetic event was created and drawn as a dash line to represent the connection, and ticks are drawn as Websocket events.
    
    Screenshot: https://imgur.com/a/U0tiec3
    Design doc: https://bit.ly/3VvcBP7
    
    
    Bug: 331351979
    Change-Id: Id44b87874dd676f01007151ff5d6f97dc7b5ba04

--- front_end/models/trace/handlers/NetworkRequestsHandler.ts
+++ front_end/models/trace/handlers/NetworkRequestsHandler.ts
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import * as Platform from '../../../core/platform/platform.js';
+import * as Protocol from '../../../generated/protocol.js';
 import * as Helpers from '../helpers/helpers.js';
 import * as Types from '../types/types.js';
 
@@ -30,6 +31,22 @@ interface TraceEventsForNetworkRequest {
   resourceMarkAsCached?: Types.TraceEvents.TraceEventResourceMarkAsCached;
 }
 
+export interface WebSocketTraceDataForFrame {
+  frame: string;
+  webSocketIdentifier: number;
+  events: Types.TraceEvents.WebSocketEvent[];
+}
+export interface WebSocketTraceDataForWorker {
+  workerId: string;
+  webSocketIdentifier: number;
+  events: Types.TraceEvents.WebSocketEvent[];
+}
+export type WebSocketTraceData = WebSocketTraceDataForFrame|WebSocketTraceDataForWorker;
+export interface WebSocketsData {
+  traceData: WebSocketTraceData[];
+}
+
+const webSocketData: Map<number, WebSocketTraceData> = new Map();
 interface NetworkRequestData {
   byOrigin: Map<string, {
     renderBlocking: Types.TraceEvents.SyntheticNetworkRequest[],
@@ -37,6 +54,7 @@ interface NetworkRequestData {
     all: Types.TraceEvents.SyntheticNetworkRequest[],
   }>;
   byTime: Types.TraceEvents.SyntheticNetworkRequest[];
+  webSocket: WebSocketTraceData[];
 }
 
 const requestMap = new Map<string, TraceEventsForNetworkRequest>();
@@ -132,6 +150,28 @@ export function handleEvent(event: Types.TraceEvents.TraceEventData): void {
   if (Types.TraceEvents.isTraceEventResourceMarkAsCached(event)) {
     storeTraceEventWithRequestId(event.args.data.requestId, 'resourceMarkAsCached', event);
     return;
+  }
+
+  if (Types.TraceEvents.isTraceEventWebSocketCreate(event) || Types.TraceEvents.isTraceEventWebSocketInfo(event) ||
+      Types.TraceEvents.isTraceEventWebSocketTransfer(event)) {
+    const identifier = event.args.data.identifier;
+    if (!webSocketData.has(identifier)) {
+      if (event.args.data.frame) {
+        webSocketData.set(identifier, {
+          frame: event.args.data.frame,
+          webSocketIdentifier: identifier,
+          events: [],
+        });
+      } else if (event.args.data.workerId) {
+        webSocketData.set(identifier, {
+          workerId: event.args.data.workerId,
+          webSocketIdentifier: identifier,
+          events: [],
+        });
+      }
+    }
+
+    webSocketData.get(identifier)?.events.push(event);
   }
 }
 
@@ -437,6 +477,26 @@ export async function finalize(): Promise<void> {
     requestsByTime.push(networkEvent);
   }
 
+  webSocketData.forEach(data => {
+    let startEvent: Types.TraceEvents.WebSocketEvent|null = null;
+    let endEvent: Types.TraceEvents.TraceEventWebSocketDestroy|null = null;
+    if (data.events.length > 0 && Types.TraceEvents.isSyntheticWebSocketConnectionEvent(data.events[0])) {
+      // drop the synthetic event if it already exists. We will create a new one with updated duration
+      data.events.shift();
+    }
+    for (const event of data.events) {
+      if (Types.TraceEvents.isTraceEventWebSocketCreate(event)) {
+        startEvent = event;
+      }
+      if (Types.TraceEvents.isTraceEventWebSocketDestroy(event)) {
+        endEvent = event;
+      }
+    }
+    const syntheticWebSocketConnectionEvent =
+        createSyntheticWebSocketConnectionEvent(startEvent, endEvent, data.events[0]);
+    data.events.unshift(syntheticWebSocketConnectionEvent);
+  });
+
   handlerState = HandlerState.FINALIZED;
 }
 
@@ -448,9 +508,40 @@ export function data(): NetworkRequestData {
   return {
     byOrigin: requestsByOrigin,
     byTime: requestsByTime,
+    webSocket: [...webSocketData.values()],
   };
 }
 
 export function deps(): TraceEventHandlerName[] {
   return ['Meta'];
+}
+
+function createSyntheticWebSocketConnectionEvent(
+    startEvent: Types.TraceEvents.TraceEventWebSocketCreate|null,
+    endEvent: Types.TraceEvents.TraceEventWebSocketDestroy|null,
+    firstRecordedEvent: Types.TraceEvents.WebSocketEvent): Types.TraceEvents.SyntheticWebSocketConnectionEvent {
+  const {traceBounds} = metaHandlerData();
+  const startTs = startEvent ? startEvent.ts : traceBounds.min;
+  const endTs = endEvent ? endEvent.ts : traceBounds.max;
+  const duration = endTs - startTs;
+  const mainEvent = startEvent || endEvent || firstRecordedEvent;
+  return {
+    name: 'SyntheticWebSocketConnectionEvent',
+    cat: mainEvent.cat,
+    ph: Types.TraceEvents.Phase.COMPLETE,
+    ts: startTs,
+    dur: duration as Types.Timing.MicroSeconds,
+    pid: mainEvent.pid,
+    tid: mainEvent.tid,
+    s: mainEvent.s,
+    rawSourceEvent: mainEvent,
+    _tag: 'SyntheticEntryTag',
+    args: {
+      data: {
+        identifier: mainEvent.args.data.identifier,
+        priority: Protocol.Network.ResourcePriority.Low,
+        url: mainEvent.args.data.url || '',
+      },
+    },
+  };
 }
