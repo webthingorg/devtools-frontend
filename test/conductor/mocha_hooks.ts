3e1b1d277c5e467667b16235edab130fdc7cb82e
Philip Pfaffe <pfaffe@chromium.org>
2024-07-03 11:47:04 +0000

    [testing] Delete obsolete runners
    Fixed: 333423685
    Change-Id: I5eb0ff36ee81fad3c50ba96b3a79d1f7296df5d3

--- test/conductor/mocha_hooks.ts
+++ test/conductor/mocha_hooks.ts
@@ -20,21 +20,11 @@ import {
 } from './hooks.js';
 import {SOURCE_ROOT} from './paths.js';
 import {TestConfig} from './test_config.js';
-import {getTestRunnerConfigSetting} from './test_runner_config.js';
 import {startServer, stopServer} from './test_server.js';
 
 /* eslint-disable no-console */
 
 process.on('SIGINT', postFileTeardown);
-
-const TEST_SERVER_TYPE = getTestRunnerConfigSetting<string>('test-server-type', TestConfig.serverType);
-
-if (TEST_SERVER_TYPE !== 'hosted-mode' && TEST_SERVER_TYPE !== 'component-docs' && TEST_SERVER_TYPE !== 'none') {
-  throw new Error(`Invalid test server type: ${TEST_SERVER_TYPE}`);
-}
-
-// Required to reassign to allow for TypeScript to correctly deduce its type
-const DERIVED_SERVER_TYPE = TEST_SERVER_TYPE;
 
 // We can run Mocha in two modes: serial and parallel. In parallel mode, Mocha
 // starts multiple node processes which don't know about each other. It provides
@@ -50,14 +40,8 @@ const DERIVED_SERVER_TYPE = TEST_SERVER_TYPE;
 // https://mochajs.org/#global-setup-fixtures. These let us start one hosted
 // mode server and share it between all the parallel test runners.
 export async function mochaGlobalSetup(this: Mocha.Suite) {
-  // Start the test server in the 'main' process. In parallel mode, we
-  // share one server between all parallel runners. The parallel runners are all
-  // in different processes, so we pass the port number as an environment var.
-  if (DERIVED_SERVER_TYPE === 'none') {
-    return;
-  }
-  process.env.testServerPort = String(await startServer(DERIVED_SERVER_TYPE));
-  console.log(`Started ${DERIVED_SERVER_TYPE} server on port ${process.env.testServerPort}`);
+  process.env.testServerPort = String(await startServer(TestConfig.serverType));
+  console.log(`Started ${TestConfig.serverType} server on port ${process.env.testServerPort}`);
 }
 
 export function mochaGlobalTeardown() {
@@ -67,9 +51,7 @@ export function mochaGlobalTeardown() {
 
 const testSuiteCoverageMap = createCoverageMap();
 
-const testsRunWithCoverageEnvSet = Boolean(process.env.COVERAGE || process.env.COVERAGE_FOLDERS || TestConfig.coverage);
-
-const SHOULD_GATHER_COVERAGE_INFORMATION = testsRunWithCoverageEnvSet && DERIVED_SERVER_TYPE === 'component-docs';
+const SHOULD_GATHER_COVERAGE_INFORMATION = TestConfig.coverage && TestConfig.serverType === 'component-docs';
 const INTERACTIONS_COVERAGE_LOCATION = path.join(TestConfig.artifactsDir, 'interactions-coverage/');
 
 let didPauseAtBeginning = false;
@@ -123,7 +105,7 @@ export const mochaHooks = {
     // We need to pause after `resetPagesBetweenTests`, otherwise the DevTools
     // and target tab are not available to us to set breakpoints in.
     // We still only want to pause once, so we remember that we did pause.
-    if ((process.env['DEBUG_TEST'] || TestConfig.debug) && !didPauseAtBeginning) {
+    if (TestConfig.debug && !didPauseAtBeginning) {
       this.timeout(0);
       didPauseAtBeginning = true;
 
