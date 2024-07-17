a00ec53dca144a5a33cf09ea571f5d08028a7235
Danil Somsikov <dsv@chromium.org>
2024-07-17 07:42:17 +0000

    [ve] Match events only once in e2e test.
    Also add a helper to dump unmatched events. This is useful to write new
    tests.
    
    Bug: 348173254
    Change-Id: I1d4965347b33b8fd95d76339650ebe0da0272ca0

--- test/e2e/helpers/visual-logging-helpers.ts
+++ test/e2e/helpers/visual-logging-helpers.ts
@@ -196,6 +196,8 @@ export async function expectVeEvents(expectedEvents: TestLogEntry[]) {
   const actualEvents =
       // @ts-ignore
       await frontend.evaluate(async () => (await globalThis.getVeDebugEventsLog()) as unknown as TestLogEntry[]);
+  const unmatchedEvents: TestLogEntry[] = [];
+  let allMatched = true;
   for (let i = 0; i < expectedEvents.length; ++i) {
     let bestError: {difference: number, description?: string}|null = null;
     const expectedEvent = expectedEvents[i];
@@ -207,10 +209,12 @@ export async function expectVeEvents(expectedEvents: TestLogEntry[]) {
               'Missing VE interaction:\n' + expectedEvent.interaction :
               'Missing VE impressions:\n' + formatImpressions(expectedEvent.impressions),
         };
+        allMatched = false;
         assert.fail(bestError.description);
       }
       const error = compareVeEvents(actualEvents[i], expectedEvent);
       if (error.difference) {
+        unmatchedEvents.push(actualEvents[i]);
         actualEvents.splice(i, 1);
         if (error.difference <= (bestError?.difference || 1)) {
           bestError = error;
@@ -219,6 +223,12 @@ export async function expectVeEvents(expectedEvents: TestLogEntry[]) {
         break;
       }
     }
+  }
+  if (allMatched) {
+    await frontend.evaluate(async unmatchedEvents => {
+      // @ts-ignore
+      globalThis.veDebugEventsLog = unmatchedEvents;
+    }, unmatchedEvents);
   }
 }
 
@@ -266,4 +276,15 @@ function editDistance(a: string, b: string) {
     }
   }
   return v1[b.length];
+}
+
+export async function dumpVeEvents(label: string) {
+  const {frontend} = getBrowserAndPages();
+  await renderCoordinatorQueueEmpty();
+  const actualEvents =
+      // @ts-ignore
+      await frontend.evaluate(async () => (await globalThis.getVeDebugEventsLog()) as unknown as TestLogEntry[]);
+  // eslint-disable-next-line no-console
+  console.log(
+      label, actualEvents.map(e => 'interaction' in e ? e.interaction : formatImpressions(e.impressions)).join('\n'));
 }
