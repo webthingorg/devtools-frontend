06598092afbc748441be276fab22db4ab87305dd
Nikolay Vitkov <nvitkov@chromium.org>
2024-07-17 13:54:54 +0200

    Add callback to UI binding for AIDA client event
    We want to be able to handle error that come from the API.
    
    Back end CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/5713262
    
    Bug: 352678676
    Change-Id: If8031b3f8a97244abd5a82762752ae833438998c

--- front_end/core/host/InspectorFrontendHostAPI.ts
+++ front_end/core/host/InspectorFrontendHostAPI.ts
@@ -144,6 +144,11 @@ export interface DoAidaConversationResult {
   detail?: string;
 }
 
+export interface DoAidaClientResult extends Record<string, unknown> {
+  error?: string;
+  detail?: string;
+}
+
 export interface VisualElementImpression {
   id: number;
   type: number;
@@ -361,7 +366,7 @@ export interface InspectorFrontendHostAPI {
   initialTargetId(): Promise<string|null>;
 
   doAidaConversation: (request: string, streamId: number, cb: (result: DoAidaConversationResult) => void) => void;
-  registerAidaClientEvent: (request: string) => void;
+  registerAidaClientEvent: (request: string, cb: (result: DoAidaClientResult) => void) => void;
 
   recordImpression(event: ImpressionEvent): void;
   recordClick(event: ClickEvent): void;
