b1aab548d7cfca9f0be50bbdfd9569ffeaad518b
Victor Porof <victorporof@chromium.org>
2024-07-24 11:41:55 +0000

    Throwaway code to give you some inspiration around AI and performance
    Find `TraceHelpers.TreeHelpers.NodeForAI.promptAI` for an easy way to prompt AIDA inside the Performance panel.
    
    Here's what you need to do to try this out:
    1. If you haven't already, set up your environment follwoing go/chrome-devtools/development-setup and https://chromium.googlesource.com/devtools/devtools-frontend/+/main/docs/get_the_code.md
    2. To use AI in Chrome, you must be on an official build, signed-in (with Chrome sync enabled), and DevTools settings enabled.
       - You can self-build a branded build, but probably eaiser to just use Canary or Beta.
    3. To open Chrome with your hacked version of devtools, use the --custom-devtools-frontend flag, per the get_the_code.md guidance.
    
    Signed-off-by: Victor Porof <victorporof@chromium.org>
    Change-Id: Idb64b30c28849a50088a711ad3ccbca26d2b292c
    Bug: None

--- front_end/panels/timeline/TimelineFlameChartDataProvider.ts
+++ front_end/panels/timeline/TimelineFlameChartDataProvider.ts
@@ -34,6 +34,7 @@ import type * as Platform from '../../core/platform/platform.js';
 import * as Root from '../../core/root/root.js';
 import * as Bindings from '../../models/bindings/bindings.js';
 import type * as TimelineModel from '../../models/timeline_model/timeline_model.js';
+import type * as TraceHelpers from '../../models/trace/helpers/helpers.js';
 import * as TraceEngine from '../../models/trace/trace.js';
 import * as PerfUI from '../../ui/legacy/components/perf_ui/perf_ui.js';
 import * as UI from '../../ui/legacy/legacy.js';
@@ -155,8 +156,25 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
 
   modifyTree(node: number, action: PerfUI.FlameChart.FilterAction): void {
     const entry = this.entryData[node] as TraceEngine.Types.TraceEvents.SyntheticTraceEntry;
-
     ModificationsManager.activeManager()?.getEntriesFilter().applyFilterAction({type: action, entry});
+  }
+
+  getTraceEntryTreeForAIFromEntryIndex(node: number): TraceHelpers.TreeHelpers.NodeForAI|null {
+    const entry = this.entryData[node] as TraceEngine.Types.TraceEvents.SyntheticTraceEntry;
+    const manager = ModificationsManager.activeManager();
+    if (!manager) {
+      return null;
+    }
+    return manager.getEntriesFilter().getTraceEntryTreeForAI(entry);
+  }
+
+  getTraceEntryTreeForAIFromEntry(entry: TraceEngine.Types.TraceEvents.TraceEventData):
+      TraceHelpers.TreeHelpers.NodeForAI|null {
+    const manager = ModificationsManager.activeManager();
+    if (!manager) {
+      return null;
+    }
+    return manager.getEntriesFilter().getTraceEntryTreeForAI(entry);
   }
 
   findPossibleContextMenuActions(node: number): PerfUI.FlameChart.PossibleFilterActions|void {
