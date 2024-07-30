d873e7e75793340a95f0706a04353b010bdcce03
Wolfgang Beyer <wolfi@chromium.org>
2024-07-30 22:51:02 +0000

    Fix tabbing behavior for Chrome AI settings page
    When an accordion is collapsed, ensure that non-visible links are not
    tab-able.
    
    Bug: 351743687, 350668580
    Change-Id: Ia0e0fbc9b4afc57c914bbd02164ff665f952771a

--- front_end/panels/settings/AISettingsTab.ts
+++ front_end/panels/settings/AISettingsTab.ts
@@ -264,10 +264,10 @@ export class AISettingsTab extends LegacyWrapper.LegacyWrapper.WrappableComponen
     const tabindex = this.#isConsoleInsightsSettingExpanded ? '0' : '-1';
     const tosLink = UI.XLink.XLink.create(
         'http://policies.google.com/terms', i18nString(UIStrings.termsOfService), undefined, undefined,
-        'terms-of-service');
+        'terms-of-service', tabindex);
     const privacyNoticeLink = UI.XLink.XLink.create(
         'http://policies.google.com/privacy', i18nString(UIStrings.privacyNotice), undefined, undefined,
-        'privacy-notice');
+        'privacy-notice', tabindex);
 
     return LitHtml.html`
       <div class="icon-container centered">
@@ -351,10 +351,10 @@ export class AISettingsTab extends LegacyWrapper.LegacyWrapper.WrappableComponen
 
     const tosLink = UI.XLink.XLink.create(
         'http://policies.google.com/terms', i18nString(UIStrings.termsOfService), undefined, undefined,
-        'terms-of-service');
+        'terms-of-service', tabindex);
     const privacyNoticeLink = UI.XLink.XLink.create(
         'http://policies.google.com/privacy', i18nString(UIStrings.privacyNotice), undefined, undefined,
-        'privacy-notice');
+        'privacy-notice', tabindex);
 
     return LitHtml.html`
       <div class="icon-container centered">
