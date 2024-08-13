86873044b061238ee86611de728d245437b2d4b8
Voicila Ionut <ionutvoicila@google.com>
2024-08-13 11:47:04 +0000

    Add an expandable table with new pseudo classes specific to the selected element.
    The new supported pseudo classes are: enable, disable, valid, invalid, user-valid, user-invalid, required, optional, read-only, read-write, in-range, out-of-range, checked, indeterminate, placeholder-shown, autofill.
    Also changed the old table to a `grid` container.
    
    Bug:40280012
    Change-Id: I0802a1f7660aee28b3878f067ccfe15497b3cd0d

--- front_end/panels/elements/ElementStatePaneWidget.ts
+++ front_end/panels/elements/ElementStatePaneWidget.ts
@@ -33,13 +33,40 @@ const UIStrings = {
    * @description Explanation text for the 'Emulate a focused page' setting in the Rendering tool.
    */
   emulatesAFocusedPage: 'Keep page focused. Commonly used for debugging disappearing elements.',
+  /**
+   * @description Similar with forceElementState but allows users to force specific state of the selected element.
+   */
+  forceElementSpecificStates: 'Force specific element state',
 };
 const str_ = i18n.i18n.registerUIStrings('panels/elements/ElementStatePaneWidget.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+enum SpecificPseudoStates {
+  ENABLED = 'enabled',
+  DISABLED = 'disabled',
+  VALID = 'valid',
+  INVALID = 'invalid',
+  USER_VALID = 'user-valid',
+  USER_INVALID = 'user-invalid',
+  REQUIRED = 'required',
+  OPTIONAL = 'optional',
+  READ_ONLY = 'read-only',
+  READ_WRITE = 'read-write',
+  IN_RANGE = 'in-range',
+  OUT_OF_RANGE = 'out-of-range',
+  VISITED = 'visited',
+  CHECKED = 'checked',
+  INDETERMINATE = 'indeterminate',
+  PLACEHOLDER_SHOWN = 'placeholder-shown',
+  AUTOFILL = 'autofill',
+}  // TODO(crbug.com/332914922): Also add :link and tests for :visited when the bug is fixed.
+
 export class ElementStatePaneWidget extends UI.Widget.Widget {
   private readonly inputs: HTMLInputElement[];
   private readonly inputStates: WeakMap<HTMLInputElement, string>;
   private cssModel?: SDK.CSSModel.CSSModel|null;
+  private specificPseudoStateDivs: Map<SpecificPseudoStates, HTMLDivElement>;
+  private readonly throttler: Common.Throttler.Throttler;
+
   constructor() {
     super(true);
     this.contentElement.className = 'styles-element-state-pane';
@@ -65,17 +92,19 @@ export class ElementStatePaneWidget extends UI.Widget.Widget {
       }
       node.domModel().cssModel().forcePseudoState(node, state, event.target.checked);
     };
-    const createElementStateCheckbox = (state: string): Element => {
-      const td = document.createElement('td');
+    const createElementStateCheckbox = (state: string): HTMLDivElement => {
+      const div = document.createElement('div');
+      div.id = state;
       const label = UI.UIUtils.CheckboxLabel.create(':' + state, undefined, undefined, undefined, true);
       const input = label.checkboxElement;
       this.inputStates.set(input, state);
       input.addEventListener('click', (clickListener as EventListener), false);
       input.setAttribute('jslog', `${VisualLogging.toggle().track({change: true}).context(state)}`);
       inputs.push(input);
-      td.appendChild(label);
-      return td;
+      div.appendChild(label);
+      return div;
     };
+
     const createEmulateFocusedPageCheckbox = (): Element => {
       const div = document.createElement('div');
       div.classList.add('page-state-checkbox');
@@ -112,22 +141,78 @@ export class ElementStatePaneWidget extends UI.Widget.Widget {
     // Populate element states
     this.contentElement.appendChild(createSectionHeader(i18nString(UIStrings.forceElementState)));
 
-    const table = document.createElement('table');
-    table.classList.add('source-code');
-    UI.ARIAUtils.markAsPresentation(table);
+    const persistentContainer = document.createElement('div');
+    persistentContainer.classList.add('source-code');
+    persistentContainer.classList.add('pseudo-states-container');
+    UI.ARIAUtils.markAsPresentation(persistentContainer);
 
-    let tr = table.createChild('tr');
-    tr.appendChild(createElementStateCheckbox('active'));
-    tr.appendChild(createElementStateCheckbox('hover'));
-    tr = table.createChild('tr');
-    tr.appendChild(createElementStateCheckbox('focus'));
-    tr.appendChild(createElementStateCheckbox('visited'));
-    tr = table.createChild('tr');
-    tr.appendChild(createElementStateCheckbox('focus-within'));
-    tr.appendChild(createElementStateCheckbox('focus-visible'));
-    tr = table.createChild('tr');
-    tr.appendChild(createElementStateCheckbox('target'));
-    this.contentElement.appendChild(table);
+    persistentContainer.appendChild(createElementStateCheckbox('active'));
+    persistentContainer.appendChild(createElementStateCheckbox('hover'));
+    persistentContainer.appendChild(createElementStateCheckbox('focus'));
+    persistentContainer.appendChild(createElementStateCheckbox('focus-within'));
+    persistentContainer.appendChild(createElementStateCheckbox('focus-visible'));
+    persistentContainer.appendChild(createElementStateCheckbox('target'));
+    this.contentElement.appendChild(persistentContainer);
+
+    const elementSpecificContainer = document.createElement('div');
+    elementSpecificContainer.classList.add('source-code');
+    elementSpecificContainer.classList.add('pseudo-states-container');
+    elementSpecificContainer.classList.add('specific-pseudo-states');
+    UI.ARIAUtils.markAsPresentation(elementSpecificContainer);
+
+    this.specificPseudoStateDivs = new Map<SpecificPseudoStates, HTMLDivElement>();
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.ENABLED, createElementStateCheckbox(SpecificPseudoStates.ENABLED));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.DISABLED, createElementStateCheckbox(SpecificPseudoStates.DISABLED));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.VALID, createElementStateCheckbox(SpecificPseudoStates.VALID));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.INVALID, createElementStateCheckbox(SpecificPseudoStates.INVALID));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.USER_VALID, createElementStateCheckbox(SpecificPseudoStates.USER_VALID));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.USER_INVALID, createElementStateCheckbox(SpecificPseudoStates.USER_INVALID));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.REQUIRED, createElementStateCheckbox(SpecificPseudoStates.REQUIRED));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.OPTIONAL, createElementStateCheckbox(SpecificPseudoStates.OPTIONAL));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.READ_ONLY, createElementStateCheckbox(SpecificPseudoStates.READ_ONLY));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.READ_WRITE, createElementStateCheckbox(SpecificPseudoStates.READ_WRITE));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.IN_RANGE, createElementStateCheckbox(SpecificPseudoStates.IN_RANGE));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.OUT_OF_RANGE, createElementStateCheckbox(SpecificPseudoStates.OUT_OF_RANGE));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.VISITED, createElementStateCheckbox(SpecificPseudoStates.VISITED));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.CHECKED, createElementStateCheckbox(SpecificPseudoStates.CHECKED));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.INDETERMINATE, createElementStateCheckbox(SpecificPseudoStates.INDETERMINATE));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.PLACEHOLDER_SHOWN, createElementStateCheckbox(SpecificPseudoStates.PLACEHOLDER_SHOWN));
+    this.specificPseudoStateDivs.set(
+        SpecificPseudoStates.AUTOFILL, createElementStateCheckbox(SpecificPseudoStates.AUTOFILL));
+
+    this.specificPseudoStateDivs.forEach(div => {
+      elementSpecificContainer.appendChild(div);
+    });
+
+    const specificHeader = document.createElement('details');
+
+    const sectionHeaderContainer = document.createElement('summary');
+    sectionHeaderContainer.classList.add('force-specific-element-header');
+    sectionHeaderContainer.classList.add('section-header');
+    UI.UIUtils.createTextChild(
+        sectionHeaderContainer.createChild('span'), i18nString(UIStrings.forceElementSpecificStates));
+
+    specificHeader.appendChild(sectionHeaderContainer);
+    specificHeader.appendChild(elementSpecificContainer);
+    this.contentElement.appendChild(specificHeader);
+
+    this.throttler = new Common.Throttler.Throttler(100);
     UI.Context.Context.instance().addFlavorChangeListener(SDK.DOMModel.DOMNode, this.update, this);
   }
   private updateModel(cssModel: SDK.CSSModel.CSSModel|null): void {
@@ -166,9 +251,126 @@ export class ElementStatePaneWidget extends UI.Widget.Widget {
         input.checked = false;
       }
     }
-    ButtonProvider.instance().item().setChecked(this.inputs.some(input => input.checked));
+    void this.throttler.schedule(this.updateElementSpecificStatesTable.bind(this, node));
+    ButtonProvider.instance().item().setToggled(this.inputs.some(input => input.checked));
+  }
+
+  private hideSpecificCheckbox(pseudoClass: SpecificPseudoStates, hide: boolean): void {
+    const checkbox = this.specificPseudoStateDivs.get(pseudoClass);
+    if (checkbox) {
+      checkbox.hidden = hide;
+    }
+  }
+
+  private async updateElementSpecificStatesTable(node: SDK.DOMModel.DOMNode|null = null): Promise<void> {
+    if (!node || node.nodeType() !== Node.ELEMENT_NODE) {
+      this.specificPseudoStateDivs.forEach(div => {
+        div.hidden = true;
+      });
+      return;
+    }
+    const isElementOfTypes = (node: SDK.DOMModel.DOMNode, types: string[]): boolean => {
+      return types.includes(node.nodeName()?.toLowerCase());
+    };
+    const isInputWithTypeRadioOrCheckbox = (node: SDK.DOMModel.DOMNode): boolean => {
+      return isElementOfTypes(node, ['input']) &&
+          (node.getAttribute('type') === 'checkbox' || node.getAttribute('type') === 'radio');
+    };
+    // An autonomous custom element is called a form-associated custom element if the element is associated with a custom element definition whose form-associated field is set to true.
+    // https://html.spec.whatwg.org/multipage/custom-elements.html#form-associated-custom-element
+    const isFormAssociatedCustomElement = async(node: SDK.DOMModel.DOMNode): Promise<boolean> => {
+      function getFormAssociatedField(this: HTMLElement): boolean {
+        return ('formAssociated' in this.constructor && this.constructor.formAssociated === true);
+      }
+      const response = await node.callFunction(getFormAssociatedField);
+      return response ? response.value : false;
+    };
+
+    const isFormAssociated = await isFormAssociatedCustomElement(node);
+
+    if (isElementOfTypes(node, ['button', 'input', 'select', 'textarea', 'optgroup', 'option', 'fieldset']) ||
+        isFormAssociated) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.ENABLED, false);
+      this.hideSpecificCheckbox(SpecificPseudoStates.DISABLED, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.ENABLED, true);
+      this.hideSpecificCheckbox(SpecificPseudoStates.DISABLED, true);
+    }
+
+    if (isElementOfTypes(node, ['button', 'fieldset', 'input', 'object', 'output', 'select', 'textarea', 'img']) ||
+        isFormAssociated) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.VALID, false);
+      this.hideSpecificCheckbox(SpecificPseudoStates.INVALID, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.VALID, true);
+      this.hideSpecificCheckbox(SpecificPseudoStates.INVALID, true);
+    }
+
+    if (isElementOfTypes(node, ['input', 'select', 'textarea'])) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.USER_VALID, false);
+      this.hideSpecificCheckbox(SpecificPseudoStates.USER_INVALID, false);
+      this.hideSpecificCheckbox(SpecificPseudoStates.REQUIRED, false);
+      this.hideSpecificCheckbox(SpecificPseudoStates.OPTIONAL, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.USER_VALID, true);
+      this.hideSpecificCheckbox(SpecificPseudoStates.USER_INVALID, true);
+      this.hideSpecificCheckbox(SpecificPseudoStates.REQUIRED, true);
+      this.hideSpecificCheckbox(SpecificPseudoStates.OPTIONAL, true);
+    }
+
+    if (isElementOfTypes(node, ['input', 'textarea'])) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.READ_ONLY, true);
+      this.hideSpecificCheckbox(SpecificPseudoStates.READ_WRITE, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.READ_ONLY, false);
+      this.hideSpecificCheckbox(SpecificPseudoStates.READ_WRITE, true);
+    }
+
+    if (isElementOfTypes(node, ['input']) &&
+        (node.getAttribute('min') !== undefined || node.getAttribute('max') !== undefined)) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.IN_RANGE, false);
+      this.hideSpecificCheckbox(SpecificPseudoStates.OUT_OF_RANGE, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.IN_RANGE, true);
+      this.hideSpecificCheckbox(SpecificPseudoStates.OUT_OF_RANGE, true);
+    }
+
+    if (isElementOfTypes(node, ['a', 'area']) && node.getAttribute('href') !== undefined) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.VISITED, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.VISITED, true);
+    }
+
+    if (isInputWithTypeRadioOrCheckbox(node) || isElementOfTypes(node, ['option'])) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.CHECKED, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.CHECKED, true);
+    }
+
+    if (isInputWithTypeRadioOrCheckbox(node) || isElementOfTypes(node, ['progress'])) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.INDETERMINATE, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.INDETERMINATE, true);
+    }
+
+    if (isElementOfTypes(node, ['input', 'textarea'])) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.PLACEHOLDER_SHOWN, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.PLACEHOLDER_SHOWN, true);
+    }
+
+    if (isElementOfTypes(node, ['input'])) {
+      this.hideSpecificCheckbox(SpecificPseudoStates.AUTOFILL, false);
+    } else {
+      this.hideSpecificCheckbox(SpecificPseudoStates.AUTOFILL, true);
+    }
+    this.updateElementSpecificStatesTableForTest();
+  }
+
+  updateElementSpecificStatesTableForTest(): void {
   }
 }
+
 let buttonProviderInstance: ButtonProvider;
 export class ButtonProvider implements UI.Toolbar.Provider {
   private readonly button: UI.Toolbar.ToolbarToggle;
