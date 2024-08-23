c7023c1ba42bf81190728e9b2a216766e6cd06a3
Alex Rudenko <alexrudenko@chromium.org>
2024-08-23 10:16:23 +0200

    bdd interface
    Bug: none
    Change-Id: I03e3ef145a9bf527631ae99c03384b8ef0179c98

--- test/shared/helper.ts
+++ test/shared/helper.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './mocha-interface.js';
+
 import {assert, AssertionError} from 'chai';
 import * as os from 'os';
 import type * as puppeteer from 'puppeteer-core';
