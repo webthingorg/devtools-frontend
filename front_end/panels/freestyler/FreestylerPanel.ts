755c44012bf5e28ef3defdc250958318ac644a6f
Alex Rudenko <alexrudenko@chromium.org>
2024-08-23 11:14:03 +0200

    fixable
    Bug: none
    Change-Id: I6ee32fd0596fb4bf318ebf37a754f6876a1c2f67

--- front_end/panels/freestyler/FreestylerPanel.ts
+++ front_end/panels/freestyler/FreestylerPanel.ts
@@ -258,8 +258,6 @@ export class FreestylerPanel extends UI.Panel.Panel {
       text,
     });
     this.#viewProps.isLoading = true;
-    // TODO: We should only show "Fix this issue" button when the answer suggests fix or fixes.
-    // We shouldn't show this when the answer is complete like a confirmation without any suggestion.
     const suggestingFix = !isFixQuery;
     let systemMessage: ModelChatMessage = {
       entity: ChatMessageEntity.MODEL,
@@ -294,6 +292,9 @@ export class FreestylerPanel extends UI.Panel.Panel {
       }
       if (data.step === Step.ERROR) {
         systemMessage.suggestingFix = false;
+      }
+      if (data.step === Step.ANSWER) {
+        systemMessage.suggestingFix = data.fixable;
       }
 
       systemMessage.rpcId = data.rpcId;
