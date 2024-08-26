c82d9eaae5622905c4a0ed42953ee30941b7b1a3
devtools-ci-autoroll-builder <devtools-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com>
2024-08-26 07:16:26 -0700

    Roll browser-protocol
    This roll requires a manual review. See http://go/reviewed-rolls for guidance.
    
    In case of failures or errors, reach out to someone from config/owner/COMMON_OWNERS.
    
    Roll created at https://cr-buildbucket.appspot.com/build/8738534200576206177
    
    R=devtools-waterfall-sheriff-onduty@rotations.google.com
    
    Bug: none
    Change-Id: I49614d3862ebff95ceb2b6cef948f65cb1d6cc59

--- front_end/generated/protocol.ts
+++ front_end/generated/protocol.ts
@@ -1018,6 +1018,7 @@ export namespace Audits {
     NoRegisterTriggerHeader = 'NoRegisterTriggerHeader',
     NoRegisterOsSourceHeader = 'NoRegisterOsSourceHeader',
     NoRegisterOsTriggerHeader = 'NoRegisterOsTriggerHeader',
+    NavigationRegistrationUniqueScopeAlreadySet = 'NavigationRegistrationUniqueScopeAlreadySet',
   }
 
   export const enum SharedDictionaryError {
