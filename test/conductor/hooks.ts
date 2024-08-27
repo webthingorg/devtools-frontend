c8aa57c8eb05ec21e83147ac7875a5000e083b97
Alex Rudenko <alexrudenko@chromium.org>
2024-08-27 07:30:35 +0200

    Debug trial
    Bug: none
    Change-Id: I315466254c8a1ff01492abd9e2a38711cd300411

--- test/conductor/hooks.ts
+++ test/conductor/hooks.ts
@@ -105,7 +105,6 @@ function launchChrome() {
     'DeferRendererTasksAfterInput',           // crbug.com/361078921
     'PMProcessPriorityPolicy',                // crbug.com/361252079
     'RenderDocument',                         // crbug.com/361519377
-    'QueueNavigationsWhileWaitingForCommit',  // crbug.com/361519377
   ];
   const launchArgs = [
     '--remote-allow-origins=*',
@@ -120,7 +119,6 @@ function launchChrome() {
     '--enable-blink-features=CSSContainerQueries,HighlightInheritance',  // TODO(crbug.com/1218390) Remove globally enabled flags and conditionally enable them
     '--disable-blink-features=WebAssemblyJSPromiseIntegration',  // TODO(crbug.com/325123665) Remove once heap snapshots work again with JSPI
     `--disable-features=${disabledFeatures.join(',')}`,
-    '--disable-field-trial-config',
   ];
   const opts: puppeteer.LaunchOptions&puppeteer.BrowserLaunchArgumentOptions&puppeteer.BrowserConnectOptions = {
     headless,
