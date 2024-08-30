f46fc5a0cf1d94c493fa47edc5cf4a893311c5f2
Adriana Ixba <aixba@chromium.org>
2024-08-30 00:50:59 +0000

    [RPP] Include top culprits to CLS insight
    This shows the first 3 culprit types based on the worst clusters. This
    also includes clusters in the insight return, which will be helpful for the overlay down the line.
    
    TODO: adding a reference to the cluster event details. We don't have this event reference in the UI yet.
    
    Bug:329735547
    Change-Id: I1812e51bddd604f0ffaf590f389f7a3c1304675a

--- front_end/panels/timeline/components/insights/CLSCulprits.ts
+++ front_end/panels/timeline/components/insights/CLSCulprits.ts
@@ -39,7 +39,43 @@ export class CLSCulprits extends BaseInsight {
     return [];
   }
 
-  #render(): LitHtml.TemplateResult {
+  /**
+   * getTopCulprits gets the top 3 shift root causes based on clusters.
+   */
+  getTopCulprits(
+      clusters: TraceEngine.Types.TraceEvents.SyntheticLayoutShiftCluster[],
+      culpritsByShift:
+          Map<TraceEngine.Types.TraceEvents.TraceEventLayoutShift,
+              TraceEngine.Insights.InsightRunners.CumulativeLayoutShift.LayoutShiftRootCausesData>): string[] {
+    const MAX_TOP_CULPRITS = 3;
+
+    const causes: Array<string> = [];
+    for (const cluster of clusters) {
+      if (causes.length === MAX_TOP_CULPRITS) {
+        break;
+      }
+      const shifts = cluster.events;
+      for (const shift of shifts) {
+        if (causes.length === MAX_TOP_CULPRITS) {
+          break;
+        }
+
+        const culprits = culpritsByShift.get(shift);
+        const fontReq = culprits?.fontRequests ?? [];
+        const iframes = culprits?.iframes ?? [];
+
+        for (let i = 0; i < fontReq.length || causes.length < MAX_TOP_CULPRITS; i++) {
+          causes.push('Font request');
+        }
+        for (let i = 0; i < iframes.length || causes.length < MAX_TOP_CULPRITS; i++) {
+          causes.push('Injected iframe');
+        }
+      }
+    }
+    return causes.slice(0, MAX_TOP_CULPRITS);
+  }
+
+  #render(culprits: Array<string>): LitHtml.TemplateResult {
     // clang-format off
     return LitHtml.html`
         <div class="insights">
@@ -53,6 +89,16 @@ export class CLSCulprits extends BaseInsight {
                          Shifts are caused by nodes changing size or newly added. Investigate
                          and fix these culprits.</p>
                 </div>
+                <div slot="insight-content" style="insight-content">
+                  <p>
+                    Top layout shift culprits:
+                    ${culprits.map(culprit => {
+                      return LitHtml.html `
+                        <li>${culprit}</li>
+                      `;
+                    })}
+                  <p>
+                </div>
             </${SidebarInsight.SidebarInsight}>
         </div>`;
     // clang-format on
@@ -60,13 +106,17 @@ export class CLSCulprits extends BaseInsight {
 
   override render(): void {
     const clsInsight = getCLSInsight(this.data.insights, this.data.navigationId);
-    const hasShifts = clsInsight?.shifts && clsInsight.shifts.size > 0;
+    const culpritsByShift = clsInsight?.shifts ?? new Map();
+    const clusters = clsInsight?.clusters ?? [];
+
+    const causes = this.getTopCulprits(clusters, culpritsByShift);
+    const hasCulprits = causes.length > 0;
 
     const matchesCategory = shouldRenderForCategory({
       activeCategory: this.data.activeCategory,
       insightCategory: this.insightCategory,
     });
-    const output = hasShifts && matchesCategory ? this.#render() : LitHtml.nothing;
+    const output = hasCulprits && matchesCategory ? this.#render(causes) : LitHtml.nothing;
     LitHtml.render(output, this.shadow, {host: this});
   }
 }
