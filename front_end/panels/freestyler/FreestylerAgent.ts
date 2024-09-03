59d82b008915deaf55b931a2214a4310434d9935
Alex Rudenko <alexrudenko@chromium.org>
2024-09-03 13:10:39 +0200

    [FreestylerEval] test using UI
    Bug: 362730981
    Change-Id: I4f70eae7aec05812922ba1035eb8a14bf831abfc

--- front_end/panels/freestyler/FreestylerAgent.ts
+++ front_end/panels/freestyler/FreestylerAgent.ts
@@ -475,10 +475,6 @@ STOP`);
           debugLog(`Action result: ${JSON.stringify(result)}`);
           if (result.sideEffect) {
             const sideEffectConfirmationPromiseWithResolvers = this.#confirmSideEffect<boolean>();
-            if (isDebugMode()) {
-              window.dispatchEvent(new CustomEvent(
-                  'freestylersideeffect', {detail: {confirm: sideEffectConfirmationPromiseWithResolvers.resolve}}));
-            }
 
             yield {
               type: ResponseType.SIDE_EFFECT,
