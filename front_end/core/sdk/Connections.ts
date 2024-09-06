95a36a1ce940ca91e78fb881d88469ea2eff96a8
Jayson Chen <jaysonchen@microsoft.com>
2024-09-06 10:57:36 -0700

    Native mode
    Change-Id: Iabaff01007241e3ad05aa464d3de5b572814f734

--- front_end/core/sdk/Connections.ts
+++ front_end/core/sdk/Connections.ts
@@ -8,6 +8,8 @@ import type * as Platform from '../platform/platform.js';
 import * as ProtocolClient from '../protocol_client/protocol_client.js';
 import * as Root from '../root/root.js';
 
+import {RehydratingConnection} from './RehydratingConnection.js';
+
 export class MainConnection implements ProtocolClient.InspectorBackend.Connection {
   onMessage: ((arg0: (Object|string)) => void)|null;
   #onDisconnect: ((arg0: string) => void)|null;
@@ -271,6 +273,85 @@ export async function initMainConnection(
 }
 
 function createMainConnection(websocketConnectionLost: () => void): ProtocolClient.InspectorBackend.Connection {
+  if (Root.Runtime.getPathName().includes('rehydrated_devtools_app')) {
+    const logDrivenConnection = RehydratingConnection.getRehydratingConnection();
+
+    function onMessage(event: MessageEvent): void {
+      if (event.data && event.data.type === 'INIT_LOG_CONNECTION') {
+        const file = event.data.log as File;
+        const reader = new FileReader();
+        reader.onload = async(): Promise<void> => {
+          await logDrivenConnection.initialize(reader.result as string);
+        };
+        reader.onerror = (): void => {
+          console.error('error loading log');
+        };
+        reader.readAsText(file);
+      }
+      window.removeEventListener('message', onMessage);
+    }
+
+    window.addEventListener('message', onMessage);
+    if (!window.opener) {
+      console.error('Loading failure, cannot find window opener.');
+    }
+    // DEBUG USE! REMOVE AFTERWARDS
+    setTimeout(() => {
+      window.opener.postMessage({type: 'INIT_READY'});
+    }, 5000);
+    // In hosted mode, we need communication between the opener window and rehydrated session window to retrieve file object
+    // if (Host.InspectorFrontendHost.InspectorFrontendHostInstance.isHostedMode()) {
+    //   function onMessage(event: MessageEvent): void {
+    //     if (event.data && event.data.type === 'INIT_LOG_CONNECTION') {
+    //       const file = event.data.log as File;
+    //       const reader = new FileReader();
+    //       reader.onload = async(): Promise<void> => {
+    //         await logDrivenConnection.initialize(reader.result as string);
+    //       };
+    //       reader.onerror = (): void => {
+    //         console.error('error loading log');
+    //       };
+    //       reader.readAsText(file);
+    //     }
+    //     window.removeEventListener('message', onMessage);
+    //   }
+    //   window.addEventListener('message', onMessage);
+    //   if (!window.opener) {
+    //     console.error('Loading failure');
+    //   }
+    //   // DEBUG USE! REMOVE AFTERWARDS
+    //   setTimeout(() => {
+    //     window.opener.postMessage({type: 'INIT_READY'});
+    //   }, 5000);
+    // } else {
+    //   // Native mode, can retrieve the file from native component
+    //   // console.error('Rehydrated Devtools not yet supported in natvie mode');
+    //   function onMessage(event: MessageEvent): void {
+    //     if (event.data && event.data.type === 'INIT_LOG_CONNECTION') {
+    //       const file = event.data.log as File;
+    //       const reader = new FileReader();
+    //       reader.onload = async(): Promise<void> => {
+    //         await logDrivenConnection.initialize(reader.result as string);
+    //       };
+    //       reader.onerror = (): void => {
+    //         console.error('error loading log');
+    //       };
+    //       console.log("HI");
+    //       reader.readAsText(file);
+    //     }
+    //     window.removeEventListener('message', onMessage);
+    //   }
+    //   window.addEventListener('message', onMessage);
+    //   if (!window.opener) {
+    //     console.error('Loading failure');
+    //   }
+    //   // DEBUG USE! REMOVE AFTERWARDS
+    //   setTimeout(() => {
+    //     window.opener.postMessage({type: 'INIT_READY'});
+    //   }, 5000);
+    // }
+    return logDrivenConnection;
+  }
   const wsParam = Root.Runtime.Runtime.queryParam('ws');
   const wssParam = Root.Runtime.Runtime.queryParam('wss');
   if (wsParam || wssParam) {
