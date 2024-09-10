7f60807890a434e1d2572a7bf50f499572420689
Nancy Li <nancyly@chromium.org>
2024-09-10 13:55:35 +0000

    [RPP] Fix the annotation colors in dark mode
    Dark:
      Side bar: https://screenshot.googleplex.com/mMks74rRqMrGA5t
      Connector: https://screenshot.googleplex.com/CnaEBPDWUaN4tWL
      Label: https://screenshot.googleplex.com/BG6AgXnpBxWVXq4
    Light:
      Connector: https://screenshot.googleplex.com/qY8eUJVbisuKoVu
      Label: https://screenshot.googleplex.com/5Ys5zLbj4jKmHf9
    
    Bug: 364610354
    Change-Id: I03f1acf7019edee26b5671a9ad54d3122ac7dd1c

--- front_end/panels/timeline/overlays/components/EntriesLinkOverlay.ts
+++ front_end/panels/timeline/overlays/components/EntriesLinkOverlay.ts
@@ -3,6 +3,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import * as ComponentHelpers from '../../../../ui/components/helpers/helpers.js';
+import * as ThemeSupport from '../../../../ui/legacy/theme_support/theme_support.js';
 import * as LitHtml from '../../../../ui/lit-html/lit-html.js';
 
 import styles from './entriesLinkOverlay.css.js';
@@ -140,7 +141,8 @@ export class EntriesLinkOverlay extends HTMLElement {
       this.#connector.setAttribute('y2', this.#coordinateTo.y.toString());
     }
 
-    this.#connector.setAttribute('stroke', 'black');
+    const arrowColor = ThemeSupport.ThemeSupport.instance().getComputedValue('--color-text-primary');
+    this.#connector.setAttribute('stroke', arrowColor);
     this.#connector.setAttribute('stroke-width', '2');
 
     void ComponentHelpers.ScheduledRender.scheduleRender(this, this.#boundRender);
@@ -157,6 +159,7 @@ export class EntriesLinkOverlay extends HTMLElement {
                  ➘ |_____entry______|
   */
   #render(): void {
+    const arrowColor = ThemeSupport.ThemeSupport.instance().getComputedValue('--color-text-primary');
     // clang-format off
     LitHtml.render(
         LitHtml.html`
@@ -169,14 +172,12 @@ export class EntriesLinkOverlay extends HTMLElement {
                 markerHeight='4'
                 refX='4'
                 refY='2'>
-                <path d='M0,0 V4 L4,2 Z' fill="black" />
+                <path d='M0,0 V4 L4,2 Z' fill=${arrowColor} />
               </marker>
             </defs>
             <line marker-end='url(#arrow)'/>
-            <rect
-              class="entryFromWrapper" fill="none" stroke="black" stroke-dasharray=${this.#fromEntryIsSource ? 'none' : DASHED_STROKE_AMOUNT} />
-            <rect
-              class="entryToWrapper" fill="none" stroke="black" stroke-dasharray=${this.#toEntryIsSource ? 'none' : DASHED_STROKE_AMOUNT} />
+            <rect class="entryFromWrapper" fill="none" stroke=${arrowColor} stroke-dasharray=${this.#fromEntryIsSource ? 'none' : DASHED_STROKE_AMOUNT} />
+            <rect class="entryToWrapper" fill="none" stroke=${arrowColor} stroke-dasharray=${this.#toEntryIsSource ? 'none' : DASHED_STROKE_AMOUNT} />
           </svg>
         `,
         this.#shadow, {host: this});
