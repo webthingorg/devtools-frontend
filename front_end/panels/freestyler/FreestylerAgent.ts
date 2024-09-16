89151b7c13f9aabe99d7fd73e8771f6c9f3b24cc
Nikolay Vitkov <nvitkov@chromium.org>
2024-09-16 17:03:36 +0200

    [Freestyler] Extract title into a separate response
    It's possible we get only a title and action without a thought,
    so we want to send it separably as well.
    
    Bug: none
    Change-Id: I370a982d815a02f7ee8c5c86d22f53de13d7a37c

--- front_end/panels/freestyler/FreestylerAgent.ts
+++ front_end/panels/freestyler/FreestylerAgent.ts
@@ -110,6 +110,7 @@ FIXABLE: true
 export const FIX_THIS_ISSUE_PROMPT = 'Fix this issue using JavaScript code execution';
 
 export enum ResponseType {
+  TITLE = 'title',
   THOUGHT = 'thought',
   ACTION = 'action',
   SIDE_EFFECT = 'side-effect',
@@ -136,10 +137,15 @@ export interface ErrorResponse {
   rpcId?: number;
 }
 
+export interface TitleResponse {
+  type: ResponseType.TITLE;
+  title: string;
+  rpcId?: number;
+}
+
 export interface ThoughtResponse {
   type: ResponseType.THOUGHT;
   thought: string;
-  title?: string;
   rpcId?: number;
 }
 
@@ -162,7 +168,8 @@ export interface QueryResponse {
   type: ResponseType.QUERYING;
 }
 
-export type ResponseData = AnswerResponse|ErrorResponse|ActionResponse|SideEffectResponse|ThoughtResponse|QueryResponse;
+export type ResponseData =
+    AnswerResponse|ErrorResponse|ActionResponse|SideEffectResponse|ThoughtResponse|TitleResponse|QueryResponse;
 
 // TODO: this should use the current execution context pased on the
 // node.
@@ -613,6 +620,14 @@ export class FreestylerAgent {
       // since the answer is not based on the observation resulted from
       // the action.
       if (action) {
+        if (title) {
+          yield {
+            type: ResponseType.TITLE,
+            title,
+            rpcId,
+          };
+        }
+
         if (thought) {
           addToHistory(`THOUGHT: ${thought}
 TITLE: ${title}
@@ -622,7 +637,6 @@ STOP`);
           yield {
             type: ResponseType.THOUGHT,
             thought,
-            title,
             rpcId,
           };
         } else {
