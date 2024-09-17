f1e105ecc624e209837c45ce7858c2d3a4a91da3
Oliver Dunk <oliverdunk@chromium.org>
2024-09-17 13:18:07 +0100

    DevTools: Add ExtensionStorageModel and initial UI for storage viewer
    Adds an ExtensionStorageModel which creates new ExtensionStorage
    instances when an execution context associated with an extension is
    created.
    
    This is added behind a new experiment intended to exist while this feature is worked on over a few further CLs.
    
    Currently each storage area shows as a new top-level entry. In a future
    CL we can update this to provide a tree-based layout.
    
    Additionally, there is no logic to hide storage areas if the storage API
    is not enabled for an extension. This will also be added as a follow-up.
    
    Bug: 40963428
    Change-Id: Ic0c532b4ae47c1a1e55fcbbff5a46708285f8648

---
+++ front_end/panels/application/ExtensionStorageModel.test.ts
@@ -0,0 +1,32 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type * as SDK from '../../core/sdk/sdk.js';
+import * as Protocol from '../../generated/protocol.js';
+import {createTarget} from '../../testing/EnvironmentHelpers.js';
+import {describeWithMockConnection} from '../../testing/MockConnection.js';
+
+import * as Resources from './application.js';
+
+describeWithMockConnection('ExtensionStorageModel', () => {
+  let extensionStorageModel: Resources.ExtensionStorageModel.ExtensionStorageModel;
+  let extensionStorage: Resources.ExtensionStorageModel.ExtensionStorage;
+  let target: SDK.Target.Target;
+  const initId = 'extensionid';
+  const initName = 'Test Extension';
+  const initStorageArea = Protocol.Extensions.StorageArea.Local;
+
+  beforeEach(() => {
+    target = createTarget();
+    extensionStorageModel = new Resources.ExtensionStorageModel.ExtensionStorageModel(target);
+    extensionStorage =
+        new Resources.ExtensionStorageModel.ExtensionStorage(extensionStorageModel, initId, initName, initStorageArea);
+  });
+
+  it('ExtensionStorage is instantiated correctly', () => {
+    assert.strictEqual(extensionStorage.extensionId, initId);
+    assert.strictEqual(extensionStorage.name, initName);
+    assert.strictEqual(extensionStorage.storageArea, initStorageArea);
+  });
+});
