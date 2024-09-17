0b5b9758f02431cb764fbf770bc91a68ee959941
Connor Clark <cjamcl@chromium.org>
2024-09-17 10:57:04 -0700

    [RPP] Add RemoteFont insight
    Bug: 352244504
    Change-Id: Ib8f3cfe4e5d9f3d986b35c9449aba47c392b33a3

--- front_end/models/trace/insights/InsightRunners.ts
+++ front_end/models/trace/insights/InsightRunners.ts
@@ -6,6 +6,7 @@ export * as CumulativeLayoutShift from './CumulativeLayoutShift.js';
 export * as DocumentLatency from './DocumentLatency.js';
 export * as InteractionToNextPaint from './InteractionToNextPaint.js';
 export * as LargestContentfulPaint from './LargestContentfulPaint.js';
+export * as RemoteFont from './RemoteFont.js';
 export * as RenderBlocking from './RenderBlocking.js';
 export * as SlowCSSSelector from './SlowCSSSelector.js';
 export * as ThirdPartyWeb from './ThirdPartyWeb.js';
