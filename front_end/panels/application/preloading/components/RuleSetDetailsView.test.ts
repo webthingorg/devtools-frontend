13bfc09dc0a74a008d151b0c9292e45328ceb828
HuanPo Lin <robertlin@chromium.org>
2024-09-17 08:39:01 +0000

    preload: Add a pretty print toggle to preloading panel in Devtools
    This CL adds a pretty print toggle to preloading panel in Devtools, and
    make pretty print by default. Also, pretty print can be disabled by
    `auto-pretty-print-minified` as source panel.
    
    Preloading panel pretty print on:
    https://drive.google.com/file/d/1R4ZAUhwpEB6cEIbz3P_LNtiqoGi3kpXY/view?usp=sharing
    
    Preloading panel pretty print off:
    https://drive.google.com/file/d/1-EIviebw2B5iZCWAz03atbcKVsFe2umW/view?usp=sharing
    
    Bug: 40279147
    Change-Id: I35f9b35a191b33db426021ef0c03e9f058e005a2

--- front_end/panels/application/preloading/components/RuleSetDetailsView.test.ts
+++ front_end/panels/application/preloading/components/RuleSetDetailsView.test.ts
@@ -14,9 +14,11 @@ import * as PreloadingComponents from './components.js';
 
 const coordinator = Coordinator.RenderCoordinator.RenderCoordinator.instance();
 
-async function renderRuleSetDetailsView(data: PreloadingComponents.RuleSetDetailsView.RuleSetDetailsViewData):
-    Promise<HTMLElement> {
+async function renderRuleSetDetailsView(
+    data: PreloadingComponents.RuleSetDetailsView.RuleSetDetailsViewData,
+    shouldPrettyPrint: boolean): Promise<HTMLElement> {
   const component = new PreloadingComponents.RuleSetDetailsView.RuleSetDetailsView();
+  component.shouldPrettyPrint = shouldPrettyPrint;
   component.data = data;
   renderElementIntoDOM(component);
   assert.isNotNull(component.shadowRoot);
@@ -29,7 +31,7 @@ describeWithEnvironment('RuleSetDetailsView', () => {
   it('renders nothing if not selected', async () => {
     const data = null;
 
-    const component = await renderRuleSetDetailsView(data);
+    const component = await renderRuleSetDetailsView(data, false);
     assert.isNotNull(component.shadowRoot);
     assert.strictEqual(component.shadowRoot.textContent, '');
   });
@@ -50,7 +52,7 @@ describeWithEnvironment('RuleSetDetailsView', () => {
 `,
       backendNodeId: 1 as Protocol.DOM.BackendNodeId,
     };
-    const component = await renderRuleSetDetailsView(data);
+    const component = await renderRuleSetDetailsView(data, false);
     assert.deepEqual(component.shadowRoot?.getElementById('error-message-text')?.textContent, undefined);
 
     const textEditor = component.shadowRoot?.querySelector('devtools-text-editor') as TextEditor.TextEditor.TextEditor;
@@ -74,7 +76,7 @@ describeWithEnvironment('RuleSetDetailsView', () => {
       url: 'https://example.com/speculationrules.json',
       requestId: 'reqeustId' as Protocol.Network.RequestId,
     };
-    const component = await renderRuleSetDetailsView(data);
+    const component = await renderRuleSetDetailsView(data, false);
     assert.deepEqual(component.shadowRoot?.getElementById('error-message-text')?.textContent, undefined);
     const textEditor = component.shadowRoot?.querySelector('devtools-text-editor') as TextEditor.TextEditor.TextEditor;
     assert.strictEqual(textEditor.state.doc.toString(), data.sourceText);
@@ -94,7 +96,7 @@ describeWithEnvironment('RuleSetDetailsView', () => {
       errorType: Protocol.Preload.RuleSetErrorType.SourceIsNotJsonObject,
       errorMessage: 'Line: 6, column: 1, Syntax error.',
     };
-    const component = await renderRuleSetDetailsView(data);
+    const component = await renderRuleSetDetailsView(data, false);
     assert.deepEqual(
         component.shadowRoot?.getElementById('error-message-text')?.textContent, 'Line: 6, column: 1, Syntax error.');
     const textEditor = component.shadowRoot?.querySelector('devtools-text-editor') as TextEditor.TextEditor.TextEditor;
@@ -118,7 +120,7 @@ describeWithEnvironment('RuleSetDetailsView', () => {
       errorType: Protocol.Preload.RuleSetErrorType.InvalidRulesSkipped,
       errorMessage: 'A list rule must have a "urls" array.',
     };
-    const component = await renderRuleSetDetailsView(data);
+    const component = await renderRuleSetDetailsView(data, false);
     assert.deepEqual(
         component.shadowRoot?.getElementById('error-message-text')?.textContent,
         'A list rule must have a "urls" array.');
