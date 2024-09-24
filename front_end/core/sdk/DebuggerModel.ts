6ccdc75783621070453b6aeb83b2c584d000dfa6
Kim-Anh Tran <kimanh@chromium.org>
2024-09-24 09:18:47 +0200

    Pick debug symbol to use when multiple are available for WebAssembly
    The v8 back-end now informs us about all available debug symbols
    that exist for a WebAssembly module. In this case, the front-end
    may now actively choose the debug symbol to use, and issues a warning
    to the developer that multiple symbols exist, and which symbol it
    ends up using.
    
    Bug: 40879198
    Change-Id: Ib5b73db6b34481823f42dfe697ad6ceab10a7605

--- front_end/core/sdk/DebuggerModel.ts
+++ front_end/core/sdk/DebuggerModel.ts
@@ -156,6 +156,12 @@ export const enum StepMode {
   STEP_OVER = 'StepOver',
 }
 
+export const WASM_SYMBOLS_PRIORITY = [
+  Protocol.Debugger.DebugSymbolsType.ExternalDWARF,
+  Protocol.Debugger.DebugSymbolsType.EmbeddedDWARF,
+  Protocol.Debugger.DebugSymbolsType.SourceMap,
+];
+
 export class DebuggerModel extends SDKModel<EventTypes> {
   readonly agent: ProtocolProxyApi.DebuggerApi;
   runtimeModelInternal: RuntimeModel;
@@ -243,6 +249,27 @@ export class DebuggerModel extends SDKModel<EventTypes> {
     if (resourceTreeModel) {
       resourceTreeModel.addEventListener(ResourceTreeModelEvents.FrameNavigated, this.onFrameNavigated, this);
     }
+  }
+
+  static selectSymbolSource(debugSymbols: Protocol.Debugger.DebugSymbols[]|null): Protocol.Debugger.DebugSymbols|null {
+    if (!debugSymbols || debugSymbols.length === 0) {
+      return null;
+    }
+    let debugSymbolsSource;
+    const symbolTypes = new Map(debugSymbols.map(symbol => [symbol.type, symbol]));
+    for (const symbol of WASM_SYMBOLS_PRIORITY) {
+      if (symbolTypes.has(symbol)) {
+        debugSymbolsSource = symbolTypes.get(symbol);
+        break;
+      }
+    }
+
+    Platform.assertNotNullOrUndefined(debugSymbolsSource);
+    if (debugSymbols.length > 1) {
+      Common.Console.Console.instance().warn(
+          `Multiple debug symbols for script were found. Using ${debugSymbolsSource.type}`);
+    }
+    return debugSymbolsSource;
   }
 
   sourceMapManager(): SourceMapManager<Script> {
@@ -719,11 +746,7 @@ export class DebuggerModel extends SDKModel<EventTypes> {
       isContentScript = !executionContextAuxData['isDefault'];
     }
 
-    let selectedDebugSymbol = null;
-    if (debugSymbols && debugSymbols.length > 0) {
-      // TODO(crbug.com/40879198): Adapt to prioritize DWARF information.
-      selectedDebugSymbol = debugSymbols[0];
-    }
+    const selectedDebugSymbol = DebuggerModel.selectSymbolSource(debugSymbols);
     const script = new Script(
         this, scriptId, sourceURL, startLine, startColumn, endLine, endColumn, executionContextId, hash,
         isContentScript, isLiveEdit, sourceMapURL, hasSourceURLComment, length, isModule, originStackTrace, codeOffset,
